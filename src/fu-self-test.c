31477cf4e7a56ffd202f431a424d59930a88a84f
Richard Hughes <richard@hughsie.com>
2015-07-25 14:51:36 +0100

    trivial: Fix make distcheck

--- src/fu-self-test.c
+++ src/fu-self-test.c
@@ -195,7 +195,13 @@ fu_cab_func (void)
 	/* extract firmware */
 	ret = fu_cab_extract (cab, FU_CAB_EXTRACT_FLAG_FIRMWARE |
 				   FU_CAB_EXTRACT_FLAG_SIGNATURE, &error);
+	g_assert_no_error (error);
+	g_assert (ret);
+	g_assert (g_str_has_suffix (fu_cab_get_filename_firmware (cab), "/firmware.bin"));
+	g_assert (g_file_test (fu_cab_get_filename_firmware (cab), G_FILE_TEST_EXISTS));
+
 	/* this is not available in make distcheck */
+	ret = fu_cab_verify (cab, &error);
 	if (g_error_matches (error, FWUPD_ERROR, FWUPD_ERROR_NOT_FOUND)) {
 		g_clear_error (&error);
 	} else {
@@ -203,13 +209,8 @@ fu_cab_func (void)
 		g_assert (ret);
 
 		/* check the certificate */
-		ret = fu_cab_verify (cab, &error);
-		g_assert_no_error (error);
-		g_assert (ret);
 		g_assert_cmpint (fu_cab_get_trust_flags (cab), ==, FWUPD_TRUST_FLAG_PAYLOAD);
 	}
-	g_assert (g_str_has_suffix (fu_cab_get_filename_firmware (cab), "/firmware.bin"));
-	g_assert (g_file_test (fu_cab_get_filename_firmware (cab), G_FILE_TEST_EXISTS));
 
 	/* clean up */
 	ret = fu_cab_delete_temp_files (cab, &error);
