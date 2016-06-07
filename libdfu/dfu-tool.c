11dc9ace51a4c048d81bd43c56d7dbf832ecee5d
Richard Hughes <richard@hughsie.com>
2016-06-07 10:11:00 +0100

    trivial: Fix up some warnings spotted by new GCC

--- libdfu/dfu-tool.c
+++ libdfu/dfu-tool.c
@@ -1177,8 +1177,7 @@ dfu_tool_device_added_cb (DfuContext *context,
 			  gpointer user_data)
 {
 	DfuToolPrivate *priv = (DfuToolPrivate *) user_data;
-	g_autofree gchar *tmp;
-	tmp = dfu_tool_get_device_string (priv, device);
+	g_autofree gchar *tmp = dfu_tool_get_device_string (priv, device);
 	/* TRANSLATORS: this is when a device is hotplugged */
 	dfu_tool_print_indent (_("Added"), tmp, 0);
 }
@@ -1192,8 +1191,7 @@ dfu_tool_device_removed_cb (DfuContext *context,
 			    gpointer user_data)
 {
 	DfuToolPrivate *priv = (DfuToolPrivate *) user_data;
-	g_autofree gchar *tmp;
-	tmp = dfu_tool_get_device_string (priv, device);
+	g_autofree gchar *tmp = dfu_tool_get_device_string (priv, device);
 	/* TRANSLATORS: this is when a device is hotplugged */
 	dfu_tool_print_indent (_("Removed"), tmp, 0);
 }
@@ -1205,8 +1203,7 @@ static void
 dfu_tool_device_changed_cb (DfuContext *context, DfuDevice *device, gpointer user_data)
 {
 	DfuToolPrivate *priv = (DfuToolPrivate *) user_data;
-	g_autofree gchar *tmp;
-	tmp = dfu_tool_get_device_string (priv, device);
+	g_autofree gchar *tmp = dfu_tool_get_device_string (priv, device);
 	/* TRANSLATORS: this is when a device is hotplugged */
 	dfu_tool_print_indent (_("Changed"), tmp, 0);
 }
