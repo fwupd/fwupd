535a5b974607023137a5fc4200964d54712183f9
Richard Hughes <richard@hughsie.com>
2016-08-11 15:36:42 +0100

    Get the firmware version correctly from 0Bitdo gamepads

---
+++ src/plugins/fu-plugin-ebitdo.c
@@ -0,0 +1,76 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2016 Richard Hughes <richard@hughsie.com>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "config.h"
+
+#include <gusb.h>
+#include <string.h>
+
+#include "fu-plugin.h"
+
+#include "ebitdo.h"
+
+const gchar *
+fu_plugin_get_name (void)
+{
+	return "ebitdo";
+}
+
+gboolean
+fu_plugin_device_probe (FuPlugin *plugin, FuDevice *device, GError **error)
+{
+	const gchar *platform_id;
+	g_autoptr(EbitdoDevice) ebitdo_dev = NULL;
+	g_autoptr(GUsbContext) usb_ctx = NULL;
+	g_autoptr(GUsbDevice) usb_device = NULL;
+
+	/* get version */
+	platform_id = fu_device_get_id (device);
+	usb_ctx = g_usb_context_new (NULL);
+	usb_device = g_usb_context_find_by_platform_id (usb_ctx,
+							platform_id,
+							error);
+	if (usb_device == NULL)
+		return FALSE;
+	ebitdo_dev = ebitdo_device_new (usb_device);
+	if (ebitdo_device_get_kind (ebitdo_dev) == EBITDO_DEVICE_KIND_UNKNOWN) {
+		g_set_error_literal (error,
+				     FWUPD_ERROR,
+				     FWUPD_ERROR_NOT_SUPPORTED,
+				     "invalid 0Bitdo device type detected");
+		return FALSE;
+	}
+
+	/* open the device */
+	if (!ebitdo_device_open (ebitdo_dev, error))
+		return FALSE;
+
+	/* get the version */
+	fu_device_set_version (device, ebitdo_device_get_version (ebitdo_dev));
+	g_debug ("overriding the version with %s",
+		 ebitdo_device_get_version (ebitdo_dev));
+
+	/* close the device */
+	if (!ebitdo_device_close (ebitdo_dev, error))
+		return FALSE;
+
+	return TRUE;
+}
