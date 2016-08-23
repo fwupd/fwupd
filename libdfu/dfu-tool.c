abe1de75ecfc169ca577597587e0612ada3da1d3
Richard Hughes <richard@hughsie.com>
2016-08-23 08:57:53 +0100

    trivial: Split out the XTEA cipher to a new file

--- libdfu/dfu-tool.c
+++ libdfu/dfu-tool.c
@@ -29,6 +29,7 @@
 #include <glib-unix.h>
 #include <appstream-glib.h>
 
+#include "dfu-cipher-xtea.h"
 #include "dfu-device-private.h"
 
 typedef struct {
@@ -1189,58 +1190,6 @@ dfu_tool_watch_cancelled_cb (GCancellable *cancellable, gpointer user_data)
 	g_main_loop_quit (loop);
 }
 
-static gboolean
-dfu_tool_parse_xtea_key (const gchar *key, guint32 *keys, GError **error)
-{
-	guint i;
-	gsize key_len;
-	g_autofree gchar *key_pad = NULL;
-
-	/* too long */
-	key_len = strlen (key);
-	if (key_len > 32) {
-		g_set_error (error,
-			     DFU_ERROR,
-			     DFU_ERROR_NOT_SUPPORTED,
-			     "Key string too long at %" G_GSIZE_FORMAT " chars, max 16",
-			     key_len);
-		return FALSE;
-	}
-
-	/* parse 4x32b values or generate a hash */
-	if (key_len == 32) {
-		for (i = 0; i < 4; i++) {
-			gchar buf[] = "xxxxxxxx";
-			gchar *endptr;
-			guint64 tmp;
-
-			/* copy to 4-char buf (with NUL) */
-			memcpy (buf, key + i*8, 8);
-			tmp = g_ascii_strtoull (buf, &endptr, 16);
-			if (endptr && endptr[0] != '\0') {
-				g_set_error (error,
-					     DFU_ERROR,
-					     DFU_ERROR_NOT_SUPPORTED,
-					     "Failed to parse key '%s'", key);
-				return FALSE;
-			}
-			keys[3-i] = (guint32) tmp;
-		}
-	} else {
-		gsize buf_len = 16;
-		g_autoptr(GChecksum) csum = NULL;
-		csum = g_checksum_new (G_CHECKSUM_MD5);
-		g_checksum_update (csum, (const guchar *) key, (gssize) key_len);
-		g_checksum_get_digest (csum, (guint8 *) keys, &buf_len);
-		g_assert (buf_len == 16);
-	}
-
-	/* success */
-	g_debug ("using XTEA key %04x%04x%04x%04x",
-		 keys[3], keys[2], keys[1], keys[0]);
-	return TRUE;
-}
-
 static guint8 *
 dfu_tool_get_firmware_contents_default (DfuFirmware *firmware,
 					gsize *length,
@@ -1275,57 +1224,6 @@ dfu_tool_get_firmware_contents_default (DfuFirmware *firmware,
 		return NULL;
 	}
 	return (guint8 *) g_bytes_get_data (contents, length);
-}
-
-#define XTEA_DELTA		0x9e3779b9
-#define XTEA_NUM_ROUNDS		32
-
-static void
-dfu_tool_encrypt_xtea (const guint32 key[4], guint8 *data, guint16 length)
-{
-	guint32 sum;
-	guint32 *tmp = (guint32 *) data;
-	guint32 v0;
-	guint32 v1;
-	guint8 i;
-	guint j;
-
-	for (j = 0; j < length / 4; j += 2) {
-		sum = 0;
-		v0 = tmp[j];
-		v1 = tmp[j+1];
-		for (i = 0; i < XTEA_NUM_ROUNDS; i++) {
-			v0 += (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + key[sum & 3]);
-			sum += XTEA_DELTA;
-			v1 += (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + key[(sum >> 11) & 3]);
-		}
-		tmp[j] = v0;
-		tmp[j+1] = v1;
-	}
-}
-
-static void
-dfu_tool_decrypt_xtea (const guint32 key[4], guint8 *data, guint16 length)
-{
-	guint32 sum;
-	guint32 *tmp = (guint32 *) data;
-	guint32 v0;
-	guint32 v1;
-	guint8 i;
-	guint j;
-
-	for (j = 0; j < length / 4; j += 2) {
-		v0 = tmp[j];
-		v1 = tmp[j+1];
-		sum = XTEA_DELTA * XTEA_NUM_ROUNDS;
-		for (i = 0; i < XTEA_NUM_ROUNDS; i++) {
-			v1 -= (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + key[(sum >> 11) & 3]);
-			sum -= XTEA_DELTA;
-			v0 -= (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + key[sum & 3]);
-		}
-		tmp[j] = v0;
-		tmp[j+1] = v1;
-	}
 }
 
 static gboolean
@@ -1383,10 +1281,8 @@ dfu_tool_encrypt (DfuToolPrivate *priv, gchar **values, GError **error)
 
 	/* check type */
 	if (g_strcmp0 (values[2], "xtea") == 0) {
-		guint32 key[4];
-		if (!dfu_tool_parse_xtea_key (values[3], key, error))
+		if (!dfu_cipher_encrypt_xtea (values[3], data, (guint32) len, error))
 			return FALSE;
-		dfu_tool_encrypt_xtea (key, data, (guint16) len);
 		dfu_firmware_set_metadata (firmware,
 					   DFU_METADATA_KEY_CIPHER_KIND,
 					   "XTEA");
@@ -1463,10 +1359,8 @@ dfu_tool_decrypt (DfuToolPrivate *priv, gchar **values, GError **error)
 
 	/* check type */
 	if (g_strcmp0 (values[2], "xtea") == 0) {
-		guint32 key[4];
-		if (!dfu_tool_parse_xtea_key (values[3], key, error))
+		if (!dfu_cipher_decrypt_xtea (values[3], data, (guint32) len, error))
 			return FALSE;
-		dfu_tool_decrypt_xtea (key, data, (guint16) len);
 		dfu_firmware_remove_metadata (firmware,
 					      DFU_METADATA_KEY_CIPHER_KIND);
 	} else {
