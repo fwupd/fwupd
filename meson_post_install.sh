57746cb83939f6b49754d4c918bd960fe4b3feb5
Richard Hughes <richard@hughsie.com>
2016-12-07 10:37:20 +0000

    Add the Meson build system as an alternate to autotools
    Automake and autoconf are impossible to fully understand and Meson now provides
    everything we need for a much smaller, faster, and more understandable build.
    
    See http://mesonbuild.com/ for more information.

---
+++ meson_post_install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ -z $MESON_INSTALL_PREFIX ]; then
+    echo 'This is meant to be ran from Meson only!'
+    exit 1
+fi
+
+#if [ -z $DESTDIR ]; then
+    echo 'Updating systemd deps'
+    mkdir -p $DESTDIR/$MESON_INSTALL_PREFIX/lib/systemd/system/system-update.target.wants
+    ln -sf ../fwupd-offline-update.service $DESTDIR/$MESON_INSTALL_PREFIX/lib/systemd/system/system-update.target.wants/fwupd-offline-update.service
+#fi
