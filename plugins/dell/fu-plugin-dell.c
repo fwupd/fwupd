fwupd/fwupd

commit cd87fcae5ab3d32d8e0616613d158f3e48d078fa

trivial: dell: don't disable plugin if UEFI capsule isn't supported
It now supports enabling things for other plugins too, so rather
mark any devices created by Dell as only readable, not flashable
when UEFI capsule is off.

plugins/dell/fu-plugin-dell.c
@@ -234,6 +234,27 @@ fu_plugin_get_dock_key (FuPlugin *plugin,
 }
 
 static gboolean
+fu_plugin_dell_capsule_supported (void)
+{
+	gint uefi_supported;
+
+	/* If ESRT is not turned on, fwupd will have already created an
+	 * unlock device (if compiled with support).
+	 *
+	 * Once unlocked, that will enable flashing capsules here too.
+	 *
+	 * that means we should only look for supported = 1
+	 */
+	uefi_supported = fwup_supported ();
+	if (uefi_supported != 1) {
+		g_debug("UEFI capsule firmware updating not supported (%x)",
+			(guint) uefi_supported);
+		return FALSE;
+	}
+	return TRUE;
+}
+
+static gboolean
 fu_plugin_dock_node (FuPlugin *plugin, GUsbDevice *device,
 		     guint8 type, const efi_guid_t *guid_raw,
 		     const gchar *component_desc, const gchar *version)
@@ -285,8 +306,9 @@ fu_plugin_dock_node (FuPlugin *plugin, GUsbDevice *device,
 	fu_device_add_flag (item->device, FWUPD_DEVICE_FLAG_REQUIRE_AC);
 	if (version != NULL) {
 		fu_device_set_version (item->device, version);
-		fu_device_add_flag (item->device,
-				    FWUPD_DEVICE_FLAG_ALLOW_OFFLINE);
+		if (fu_plugin_dell_capsule_supported ())
+			fu_device_add_flag (item->device,
+					    FWUPD_DEVICE_FLAG_ALLOW_OFFLINE);
 	}
 
 	g_hash_table_insert (data->devices, g_strdup (dock_key), item);
@@ -669,7 +691,8 @@ fu_plugin_dell_detect_tpm (FuPlugin *plugin, GError **error)
 	fu_device_add_flag (dev, FWUPD_DEVICE_FLAG_INTERNAL);
 	fu_device_add_flag (dev, FWUPD_DEVICE_FLAG_REQUIRE_AC);
 	if (out->flashes_left > 0) {
-		fu_device_add_flag (dev, FWUPD_DEVICE_FLAG_ALLOW_OFFLINE);
+		if (fu_plugin_dell_capsule_supported ())
+			fu_device_add_flag (dev, FWUPD_DEVICE_FLAG_ALLOW_OFFLINE);
 		fu_device_set_flashes_left (dev, out->flashes_left);
 	}
 	fu_plugin_device_add (plugin, dev);
@@ -902,7 +925,6 @@ fu_plugin_startup (FuPlugin *plugin, GError **error)
 {
 	FuPluginData *data = fu_plugin_get_data (plugin);
 	guint8 dell_supported = 0;
-	gint uefi_supported = 0;
 	struct smbios_struct *de_table;
 
 	/* get USB */
@@ -931,26 +953,6 @@ fu_plugin_startup (FuPlugin *plugin, GError **error)
 			     FWUPD_ERROR_NOT_SUPPORTED,
 			     "Firmware updating not supported (%x)",
 			     dell_supported);
-		return FALSE;
-	}
-	/* Check and make sure that ESRT is supported as well.
-	 *
-	 * This will indicate capsule support on the system.
-	 *
-	 * If ESRT is not turned on, fwupd will have already created an
-	 * unlock device (if compiled with support).
-	 *
-	 * Once unlocked, that will enable this plugin too.
-	 *
-	 * that means we should only look for supported = 1
-	 */
-	uefi_supported = fwup_supported ();
-	if (uefi_supported != 1) {
-		g_set_error (error,
-			     FWUPD_ERROR,
-			     FWUPD_ERROR_NOT_SUPPORTED,
-			     "UEFI capsule firmware updating not supported (%x)",
-			     (guint) uefi_supported);
 		return FALSE;
 	}
 

commit 72f905d8468950a31daf71fbd8cd7674c980bff7

trivial: dell: fix test suite
Commit cd87fca broke running the test suite on machines without
UEFI capsule support.

plugins/dell/fu-plugin-dell.c
@@ -234,9 +234,13 @@ fu_plugin_get_dock_key (FuPlugin *plugin,
 }
 
 static gboolean
-fu_plugin_dell_capsule_supported (void)
+fu_plugin_dell_capsule_supported (FuPlugin *plugin)
 {
+	FuPluginData *data = fu_plugin_get_data (plugin);
 	gint uefi_supported;
+
+	if (data->fake_smbios)
+		return TRUE;
 
 	/* If ESRT is not turned on, fwupd will have already created an
 	 * unlock device (if compiled with support).
@@ -306,7 +310,7 @@ fu_plugin_dock_node (FuPlugin *plugin, GUsbDevice *device,
 	fu_device_add_flag (item->device, FWUPD_DEVICE_FLAG_REQUIRE_AC);
 	if (version != NULL) {
 		fu_device_set_version (item->device, version);
-		if (fu_plugin_dell_capsule_supported ())
+		if (fu_plugin_dell_capsule_supported (plugin))
 			fu_device_add_flag (item->device,
 					    FWUPD_DEVICE_FLAG_ALLOW_OFFLINE);
 	}
@@ -691,7 +695,7 @@ fu_plugin_dell_detect_tpm (FuPlugin *plugin, GError **error)
 	fu_device_add_flag (dev, FWUPD_DEVICE_FLAG_INTERNAL);
 	fu_device_add_flag (dev, FWUPD_DEVICE_FLAG_REQUIRE_AC);
 	if (out->flashes_left > 0) {
-		if (fu_plugin_dell_capsule_supported ())
+		if (fu_plugin_dell_capsule_supported (plugin))
 			fu_device_add_flag (dev, FWUPD_DEVICE_FLAG_ALLOW_OFFLINE);
 		fu_device_set_flashes_left (dev, out->flashes_left);
 	}