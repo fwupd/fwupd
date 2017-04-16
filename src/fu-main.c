d9f6b10691351efd42d26ea3f44dcd6026e1036a
Richard Hughes <richard@hughsie.com>
2017-04-16 09:11:39 +0100

    Add a config option to allow runtime disabling plugins by name
    See https://github.com/hughsie/fwupd/issues/110

--- src/fu-main.c
+++ src/fu-main.c
@@ -2625,6 +2625,14 @@ fu_main_load_plugins (FuMainPrivate *priv, GError **error)
 	const gchar *fn;
 	g_autofree gchar *plugin_dir = NULL;
 	g_autoptr(GDir) dir = NULL;
+	g_auto(GStrv) blacklist = NULL;
+
+	/* get plugin blacklist */
+	blacklist = g_key_file_get_string_list (priv->config,
+						"fwupd",
+						"BlacklistPlugins",
+						NULL, /* length */
+						NULL);
 
 	/* search */
 	plugin_dir = g_build_filename (LIBDIR, "fwupd-plugins-2", NULL);
@@ -2650,6 +2658,18 @@ fu_main_load_plugins (FuMainPrivate *priv, GError **error)
 				   filename, error_local->message);
 			continue;
 		}
+
+		/* is blacklisted */
+		if (blacklist != NULL &&
+		    g_strv_contains ((const gchar * const *) blacklist,
+				     fu_plugin_get_name (plugin))) {
+			fu_plugin_set_enabled (plugin, FALSE);
+			g_debug ("%s blacklisted by config",
+				 fu_plugin_get_name (plugin));
+			continue;
+		}
+
+		/* watch for changes */
 		g_signal_connect (plugin, "device-added",
 				  G_CALLBACK (fu_main_plugin_device_added_cb),
 				  priv);
