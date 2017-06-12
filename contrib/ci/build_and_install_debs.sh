505a2e12b008b2899b877bcfca35c126973f34b4
Mario Limonciello <mario.limonciello@dell.com>
2017-06-12 11:27:18 -0500

    Generate deb & RPM packages as part of CI build (#134)

---
+++ contrib/ci/build_and_install_debs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+#build deb packages
+VERSION=`git describe --abbrev=0 --tags`
+rm -rf build/
+mkdir -p build && pushd build
+ln -s ../* .
+cp contrib/debian . -R
+sed s/quilt/native/ debian/source/format -i
+EDITOR=/bin/true dch --create --package fwupd -v $VERSION "CI Build"
+dpkg-buildpackage
+
+#test the packages install
+dpkg -i ../*.deb
