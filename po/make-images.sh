845ab8d18f989fa2a3e92e042c00f3095d540c0d
Peter Jones <pjones@redhat.com>
2017-06-12 14:47:27 -0400

    Actually make sure our firmware status string will get translated.
    Make the string we've gotten translated be the one we render, and also
    make it use ellipses consistently on all the current translations.
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- po/make-images.sh
+++ po/make-images.sh
@@ -5,8 +5,8 @@
 #
 # Distributed under terms of the GPLv2 license.
 #
-install -m 0755 -d ${MESON_INSTALL_DESTDIR_PREFIX}/share/fwupd/labels/
-${MESON_SOURCE_ROOT}/po/make-images "Please wait while we install a system update" ${MESON_INSTALL_DESTDIR_PREFIX}/share/locale/ ${MESON_SOURCE_ROOT}/po/LINGUAS
+install -m 0755 -d ${MESON_INSTALL_DESTDIR_PREFIX}/share/locale/
+${MESON_SOURCE_ROOT}/po/make-images "Installing firmware update…" ${MESON_INSTALL_DESTDIR_PREFIX}/share/locale/ ${MESON_SOURCE_ROOT}/po/LINGUAS
 for x in ${MESON_INSTALL_DESTDIR_PREFIX}/share/locale/*/LC_IMAGES/*.bmp ; do
     gzip -f ${x}
 done
