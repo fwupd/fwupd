954de52612bb0bcce5c76a9e751d1d69ec76a0ac
Richard Hughes <richard@hughsie.com>
2017-06-15 21:26:53 +0100

    trivial: Make --verbose work in fwupdmgr

--- src/fu-util.c
+++ src/fu-util.c
@@ -1634,7 +1634,7 @@ main (int argc, char *argv[])
 
 	/* set verbose? */
 	if (verbose) {
-		g_setenv ("FWUPD_VERBOSE", "1", FALSE);
+		g_setenv ("G_MESSAGES_DEBUG", "all", FALSE);
 	} else {
 		g_log_set_handler (G_LOG_DOMAIN, G_LOG_LEVEL_DEBUG,
 				   fu_util_ignore_cb, NULL);
