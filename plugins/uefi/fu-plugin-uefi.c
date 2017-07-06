2eb88a7d14c7f00460aca64664a13ef374ab765e
Mario Limonciello <mario.limonciello@dell.com>
2017-07-06 11:21:01 -0500

    uefi,dell: make error messages from installing capsules useful
    Before:
    $ fwupdmgr install XPS_test.cab
    Retrying as an offline update...
    Scheduling…            UEFI firmware update failed: -1
    
    After:
    $ fwupdmgr install XPS_test.cab
    Retrying as an offline update...
    Scheduling…            UEFI firmware update failed: libfwup.c:733 get_paths(): could not find shim or fwup on ESP: No such file or directory

--- plugins/uefi/fu-plugin-uefi.c
+++ plugins/uefi/fu-plugin-uefi.c
@@ -200,11 +200,25 @@ fu_plugin_update_offline (FuPlugin *plugin,
 					  g_bytes_get_data (blob_fw, NULL),
 					  g_bytes_get_size (blob_fw));
 	if (rc < 0) {
+		char *filename = NULL;
+		char *function = NULL;
+		char *message = NULL;
+		int line = 0;
+		int err = 0;
+		rc = efi_error_get (0, &filename, &function, &line, &message, &err);
+		if (rc <= 0) {
+			g_set_error_literal (error,
+				     FWUPD_ERROR,
+				     FWUPD_ERROR_NOT_SUPPORTED,
+				     "UEFI firmware update failed");
+			return FALSE;
+		}
+
 		g_set_error (error,
 			     FWUPD_ERROR,
 			     FWUPD_ERROR_NOT_SUPPORTED,
-			     "UEFI firmware update failed: %s",
-			     strerror (rc));
+			     "UEFI firmware update failed: %s:%d %s(): %s: %s",
+			     filename, line, function, message, strerror(err));
 		return FALSE;
 	}
 	return TRUE;
