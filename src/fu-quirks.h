9c028f06b5a68e5b96b497c11de448b2ca2d85d7
Richard Hughes <richard@hughsie.com>
2017-10-28 21:14:28 +0100

    Move the database of supported devices out into runtime loaded files
    When fwupd is installed in long-term support distros it's very hard to backport
    new versions as new hardware is released.
    
    There are several reasons why we can't just include the mapping and quirk
    information in the AppStream metadata:
    
     * The extra data is hugely specific to the installed fwupd plugin versions
     * The device-id is per-device, and the mapping is usually per-plugin
     * Often the information is needed before the FuDevice is created
     * There are security implications in allowing plugins to handle new devices
    
    The idea with quirks is that the end user can drop an additional (or replace
    an existing) file in a .d director with a simple format and the hardware will
    magically start working. This assumes no new quirks are required, as this would
    obviously need code changes, but allows us to get most existing devices working
    in an easy way without the user compiling anything.
    
    This allows us to fix issues like https://github.com/hughsie/fwupd/issues/265

--- src/fu-quirks.h
+++ src/fu-quirks.h
@@ -1,6 +1,7 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
  *
  * Copyright (C) 2016 Mario Limonciello <mario_limonciello@dell.com>
+ * Copyright (C) 2017 Richard Hughes <richard@hughsie.com>
  *
  * Licensed under the GNU General Public License Version 2
  *
@@ -22,6 +23,30 @@
 #ifndef __FU_QUIRKS_H
 #define __FU_QUIRKS_H
 
+G_BEGIN_DECLS
+
+#include <glib-object.h>
+#include <gusb.h>
+
+#define FU_TYPE_QUIRKS (fu_quirks_get_type ())
+G_DECLARE_FINAL_TYPE (FuQuirks, fu_quirks, FU, QUIRKS, GObject)
+
+FuQuirks	*fu_quirks_new				(void);
+gboolean	 fu_quirks_load				(FuQuirks	*self,
+							 GError		**error);
+const gchar	*fu_quirks_lookup_by_id			(FuQuirks	*self,
+							 const gchar	*prefix,
+							 const gchar	*id);
+const gchar	*fu_quirks_lookup_by_glob		(FuQuirks	*self,
+							 const gchar	*prefix,
+							 const gchar	*glob);
+const gchar	*fu_quirks_lookup_by_usb_device		(FuQuirks	*self,
+							 const gchar	*prefix,
+							 GUsbDevice	*dev);
+
+#include <appstream-glib.h>
+
+/* FIXME: port to above */
 typedef struct {
 	const gchar             *sys_vendor;
 	const gchar		*identifier;
@@ -35,5 +60,6 @@ static const FuVendorQuirks quirk_table[] = {
 	{ NULL,		NULL,			AS_VERSION_PARSE_FLAG_NONE }
 };
 
-#endif /* __FU_QUIRKS_H */
+G_END_DECLS
 
+#endif /* __FU_QUIRKS_H */
