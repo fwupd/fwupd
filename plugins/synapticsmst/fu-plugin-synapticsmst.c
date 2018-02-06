fwupd/fwupd

commit 4a036018f709e0782c4d828f7a9a8c9a9d8fe73c

Set the progress and state on the FuDevice, not the FuPlugin
This makes more sense; we're updating the device, not the plugin itself.

This also means we don't need to funnel everything through callbacks like
GFileProgressCallback and we can also update the state without adding an
explicit callback to each derived device type.

plugins/synapticsmst/fu-plugin-synapticsmst.c
@@ -285,13 +285,13 @@ fu_plugin_synapticsmst_enumerate (FuPlugin *plugin,
 static void
 fu_synapticsmst_write_progress_cb (goffset current, goffset total, gpointer user_data)
 {
-	FuPlugin *plugin = FU_PLUGIN (user_data);
+	FuDevice *device = FU_DEVICE (user_data);
 	gdouble percentage = -1.f;
 	if (total > 0)
 		percentage = (100.f * (gdouble) current) / (gdouble) total;
 	g_debug ("written %" G_GOFFSET_FORMAT "/%" G_GOFFSET_FORMAT "[%.1f%%]",
 		 current, total, percentage);
-	fu_plugin_set_percentage (plugin, (guint) percentage);
+	fu_device_set_progress (device, (guint) percentage);
 }
 
 gboolean
@@ -322,7 +322,7 @@ fu_plugin_update (FuPlugin *plugin,
 	/* sleep to allow device wakeup to complete */
 	g_debug ("waiting %d seconds for MST hub wakeup",
 		 SYNAPTICS_FLASH_MODE_DELAY);
-	fu_plugin_set_status (plugin, FWUPD_STATUS_DEVICE_BUSY);
+	fu_device_set_status (dev, FWUPD_STATUS_DEVICE_BUSY);
 	g_usleep (SYNAPTICS_FLASH_MODE_DELAY * 1000000);
 
 	device = synapticsmst_device_new (kind, aux_node, layer, rad);
@@ -331,10 +331,10 @@ fu_plugin_update (FuPlugin *plugin,
 						   data->system_type, error))
 		return FALSE;
 	if (synapticsmst_device_board_id_to_string (synapticsmst_device_get_board_id (device)) != NULL) {
-		fu_plugin_set_status (plugin, FWUPD_STATUS_DEVICE_WRITE);
+		fu_device_set_status (dev, FWUPD_STATUS_DEVICE_WRITE);
 		if (!synapticsmst_device_write_firmware (device, blob_fw,
 							 fu_synapticsmst_write_progress_cb,
-							 plugin,
+							 device,
 							 error)) {
 			g_prefix_error (error, "failed to flash firmware: ");
 			return FALSE;
@@ -348,7 +348,7 @@ fu_plugin_update (FuPlugin *plugin,
 	}
 
 	/* Re-run device enumeration to find the new device version */
-	fu_plugin_set_status (plugin, FWUPD_STATUS_DEVICE_RESTART);
+	fu_device_set_status (dev, FWUPD_STATUS_DEVICE_RESTART);
 	if (!synapticsmst_device_enumerate_device (device, data->dock_type,
 						   data->system_type, error)) {
 		return FALSE;

plugins/altos/fu-altos-device.c
@@ -429,12 +429,23 @@ fu_altos_device_write_page (FuAltosDevice *device,
 	return TRUE;
 }
 
+static void
+fu_altos_device_set_progress (FuAltosDevice *device, guint current, guint total)
+{
+	gdouble percentage = -1.f;
+	if (total > 0)
+		percentage = (100.f * (gdouble) current) / (gdouble) total;
+	if (g_getenv ("FWUPD_ALTOS_VERBOSE") != NULL) {
+		g_debug ("written %u/%u bytes [%.1f%%]",
+			 current, total, percentage);
+	}
+	fu_device_set_progress (FU_DEVICE (device), (guint) percentage);
+}
+
 gboolean
 fu_altos_device_write_firmware (FuAltosDevice *device,
 				GBytes *fw,
 				FuAltosDeviceWriteFirmwareFlag flags,
-				GFileProgressCallback progress_cb,
-				gpointer progress_data,
 				GError **error)
 {
 	FuAltosDevicePrivate *priv = GET_PRIVATE (device);
@@ -556,11 +567,7 @@ fu_altos_device_write_firmware (FuAltosDevice *device,
 		}
 
 		/* progress */
-		if (progress_cb != NULL) {
-			progress_cb ((goffset) i,
-				     (goffset) flash_len,
-				     progress_data);
-		}
+		fu_altos_device_set_progress (device, i, flash_len);
 		g_string_append_len (buf, str->str, str->len);
 	}
 
@@ -571,21 +578,14 @@ fu_altos_device_write_firmware (FuAltosDevice *device,
 	}
 
 	/* progress complete */
-	if (progress_cb != NULL) {
-		progress_cb ((goffset) flash_len,
-			     (goffset) flash_len,
-			     progress_data);
-	}
+	fu_altos_device_set_progress (device, flash_len, flash_len);
 
 	/* success */
 	return TRUE;
 }
 
 GBytes *
-fu_altos_device_read_firmware (FuAltosDevice *device,
-			       GFileProgressCallback progress_cb,
-			       gpointer progress_data,
-			       GError **error)
+fu_altos_device_read_firmware (FuAltosDevice *device, GError **error)
 {
 	FuAltosDevicePrivate *priv = GET_PRIVATE (device);
 	guint flash_len;
@@ -636,11 +636,9 @@ fu_altos_device_read_firmware (FuAltosDevice *device,
 			return NULL;
 
 		/* progress */
-		if (progress_cb != NULL) {
-			progress_cb ((goffset) (i - priv->addr_base),
-				     (goffset) (priv->addr_bound - priv->addr_base),
-				     progress_data);
-		}
+		fu_altos_device_set_progress (device,
+					      i - priv->addr_base,
+					      priv->addr_bound - priv->addr_base);
 		g_string_append_len (buf, str->str, str->len);
 	}
 

plugins/altos/fu-altos-device.h
@@ -61,12 +61,8 @@ gboolean	 fu_altos_device_probe			(FuAltosDevice	*device,
 gboolean	 fu_altos_device_write_firmware		(FuAltosDevice	*device,
 							 GBytes		*fw,
 							 FuAltosDeviceWriteFirmwareFlag flags,
-							 GFileProgressCallback progress_cb,
-							 gpointer	 progress_data,
 							 GError		**error);
 GBytes		*fu_altos_device_read_firmware		(FuAltosDevice	*device,
-							 GFileProgressCallback progress_cb,
-							 gpointer	 progress_data,
 							 GError		**error);
 
 G_END_DECLS

plugins/altos/fu-altos-tool.c
@@ -24,13 +24,9 @@
 #include "fu-altos-device.h"
 
 static void
-fu_altos_tool_write_progress_cb (goffset current, goffset total, gpointer user_data)
+fu_altos_tool_progress_cb (FuDevice *device, GParamSpec *pspec, gpointer user_data)
 {
-	gdouble percentage = -1.f;
-	if (total > 0)
-		percentage = (100.f * (gdouble) current) / (gdouble) total;
-	g_print ("Written %" G_GOFFSET_FORMAT "/%" G_GOFFSET_FORMAT " bytes [%.1f%%]\n",
-		 current, total, percentage);
+	g_print ("Written %u%%\n", fu_device_get_progress (device));
 }
 
 int
@@ -94,9 +90,10 @@ main (int argc, char **argv)
 
 	/* update with data blob */
 	fw = g_bytes_new (data, len);
+	g_signal_connect (dev, "notify::progress",
+			  G_CALLBACK (fu_altos_tool_progress_cb), NULL);
 	if (!fu_altos_device_write_firmware (dev, fw,
 					     FU_ALTOS_DEVICE_WRITE_FIRMWARE_FLAG_NONE,
-					     fu_altos_tool_write_progress_cb, NULL,
 					     &error)) {
 		g_print ("Failed to write firmware: %s\n", error->message);
 		return 1;

plugins/altos/fu-plugin-altos.c
@@ -64,18 +64,6 @@ fu_plugin_usb_device_added (FuPlugin *plugin, GUsbDevice *usb_device, GError **e
 	return TRUE;
 }
 
-static void
-fu_plugin_altos_progress_cb (goffset current, goffset total, gpointer user_data)
-{
-	FuPlugin *plugin = FU_PLUGIN (user_data);
-	gdouble percentage = -1.f;
-	if (total > 0)
-		percentage = (100.f * (gdouble) current) / (gdouble) total;
-	g_debug ("written %" G_GOFFSET_FORMAT "/%" G_GOFFSET_FORMAT " bytes [%.1f%%]",
-		 current, total, percentage);
-	fu_plugin_set_percentage (plugin, (guint) percentage);
-}
-
 gboolean
 fu_plugin_verify (FuPlugin *plugin,
 		  FuDevice *dev,
@@ -89,10 +77,8 @@ fu_plugin_verify (FuPlugin *plugin,
 		0 };
 
 	/* get data */
-	fu_plugin_set_status (plugin, FWUPD_STATUS_DEVICE_VERIFY);
+	fu_device_set_status (dev, FWUPD_STATUS_DEVICE_VERIFY);
 	blob_fw = fu_altos_device_read_firmware (FU_ALTOS_DEVICE (dev),
-						 fu_plugin_altos_progress_cb,
-						 plugin,
 						 error);
 	if (blob_fw == NULL)
 		return FALSE;
@@ -101,7 +87,6 @@ fu_plugin_verify (FuPlugin *plugin,
 		hash = g_compute_checksum_for_bytes (checksum_types[i], blob_fw);
 		fu_device_add_checksum (dev, hash);
 	}
-	fu_plugin_set_status (plugin, FWUPD_STATUS_IDLE);
 	return TRUE;
 }
 
@@ -112,15 +97,12 @@ fu_plugin_update (FuPlugin *plugin,
 		  FwupdInstallFlags flags,
 		  GError **error)
 {
-	fu_plugin_set_status (plugin, FWUPD_STATUS_DEVICE_WRITE);
+	fu_device_set_status (dev, FWUPD_STATUS_DEVICE_WRITE);
 	if (!fu_altos_device_write_firmware (FU_ALTOS_DEVICE (dev),
 					     blob_fw,
 					     FU_ALTOS_DEVICE_WRITE_FIRMWARE_FLAG_REBOOT,
-					     fu_plugin_altos_progress_cb,
-					     plugin,
 					     error)) {
 		return FALSE;
 	}
-	fu_plugin_set_status (plugin, FWUPD_STATUS_IDLE);
 	return TRUE;
 }

plugins/colorhug/fu-colorhug-device.c
@@ -31,8 +31,6 @@ typedef struct
 {
 	ChDeviceQueue		*device_queue;
 	gboolean		 is_bootloader;
-	GFileProgressCallback	 progress_cb;
-	gpointer		 progress_data;
 } FuColorhugDevicePrivate;
 
 G_DEFINE_TYPE_WITH_PRIVATE (FuColorhugDevice, fu_colorhug_device, FU_TYPE_USB_DEVICE)
@@ -55,10 +53,7 @@ fu_colorhug_device_progress_cb (ChDeviceQueue *device_queue,
 				guint percentage,
 				FuColorhugDevice *device)
 {
-	FuColorhugDevicePrivate *priv = GET_PRIVATE (device);
-	/* not ideal, but do as best we can */
-	if (priv->progress_cb != NULL)
-		priv->progress_cb (percentage, 100, priv->progress_data);
+	fu_device_set_progress (FU_DEVICE (device), percentage);
 }
 
 gboolean
@@ -75,11 +70,7 @@ fu_colorhug_device_detach (FuColorhugDevice *device, GError **error)
 	GUsbDevice *usb_device = fu_usb_device_get_dev (FU_USB_DEVICE (device));
 	g_autoptr(GError) error_local = NULL;
 
-	/* set up progress callback */
-	priv->progress_cb = NULL;
-	priv->progress_data = NULL;
-
-	g_debug ("rebooting...");
+	fu_device_set_status (FU_DEVICE (device), FWUPD_STATUS_DEVICE_RESTART);
 	ch_device_queue_reset (priv->device_queue, usb_device);
 	if (!ch_device_queue_process (priv->device_queue,
 				      CH_DEVICE_QUEUE_PROCESS_FLAGS_NONE,
@@ -101,11 +92,7 @@ fu_colorhug_device_attach (FuColorhugDevice *device, GError **error)
 	GUsbDevice *usb_device = fu_usb_device_get_dev (FU_USB_DEVICE (device));
 	g_autoptr(GError) error_local = NULL;
 
-	/* set up progress callback */
-	priv->progress_cb = NULL;
-	priv->progress_data = NULL;
-
-	g_debug ("rebooting...");
+	fu_device_set_status (FU_DEVICE (device), FWUPD_STATUS_DEVICE_RESTART);
 	ch_device_queue_boot_flash (priv->device_queue, usb_device);
 	if (!ch_device_queue_process (priv->device_queue,
 				      CH_DEVICE_QUEUE_PROCESS_FLAGS_NONE,
@@ -126,10 +113,6 @@ fu_colorhug_device_set_flash_success (FuColorhugDevice *device, GError **error)
 	FuColorhugDevicePrivate *priv = GET_PRIVATE (device);
 	GUsbDevice *usb_device = fu_usb_device_get_dev (FU_USB_DEVICE (device));
 	g_autoptr(GError) error_local = NULL;
-
-	/* set up progress callback */
-	priv->progress_cb = NULL;
-	priv->progress_data = NULL;
 
 	g_debug ("setting flash success");
 	ch_device_queue_set_flash_success (priv->device_queue,
@@ -224,10 +207,6 @@ fu_colorhug_device_open (FuUsbDevice *device, GError **error)
 	FuColorhugDevicePrivate *priv = GET_PRIVATE (self);
 	GUsbDevice *usb_device = fu_usb_device_get_dev (device);
 
-	/* set up progress callback */
-	priv->progress_cb = NULL;
-	priv->progress_data = NULL;
-
 	/* got the version using the HID API */
 	if (!g_usb_device_set_configuration (usb_device, CH_USB_CONFIG, error))
 		return FALSE;
@@ -260,10 +239,7 @@ fu_colorhug_device_open (FuUsbDevice *device, GError **error)
 }
 
 gboolean
-fu_colorhug_device_verify_firmware (FuColorhugDevice *device,
-				    GFileProgressCallback progress_cb,
-				    gpointer progress_data,
-				    GError **error)
+fu_colorhug_device_verify_firmware (FuColorhugDevice *device, GError **error)
 {
 	FuColorhugDevicePrivate *priv = GET_PRIVATE (device);
 	GUsbDevice *usb_device = fu_usb_device_get_dev (FU_USB_DEVICE (device));
@@ -275,12 +251,8 @@ fu_colorhug_device_verify_firmware (FuColorhugDevice *device,
 		G_CHECKSUM_SHA256,
 		0 };
 
-	/* set up progress callback */
-	priv->progress_cb = progress_cb;
-	priv->progress_data = progress_data;
-
 	/* get the firmware from the device */
-	g_debug ("verifying firmware");
+	fu_device_set_status (FU_DEVICE (device), FWUPD_STATUS_DEVICE_VERIFY);
 	ch_device_queue_read_firmware (priv->device_queue, usb_device,
 				       &data2, &len);
 	if (!ch_device_queue_process (priv->device_queue,
@@ -306,20 +278,14 @@ fu_colorhug_device_verify_firmware (FuColorhugDevice *device,
 }
 
 gboolean
-fu_colorhug_device_write_firmware (FuColorhugDevice *device, GBytes *fw,
-				   GFileProgressCallback progress_cb,
-				   gpointer progress_data,
-				   GError **error)
+fu_colorhug_device_write_firmware (FuColorhugDevice *device, GBytes *fw, GError **error)
 {
 	FuColorhugDevicePrivate *priv = GET_PRIVATE (device);
 	GUsbDevice *usb_device = fu_usb_device_get_dev (FU_USB_DEVICE (device));
 	g_autoptr(GError) error_local = NULL;
 
-	/* set up progress callback */
-	priv->progress_cb = progress_cb;
-	priv->progress_data = progress_data;
-
-	g_debug ("writing firmware");
+	/* write firmware */
+	fu_device_set_status (FU_DEVICE (device), FWUPD_STATUS_DEVICE_WRITE);
 	ch_device_queue_set_flash_success (priv->device_queue,
 					   usb_device,
 					   0x00);
@@ -338,7 +304,7 @@ fu_colorhug_device_write_firmware (FuColorhugDevice *device, GBytes *fw,
 	}
 
 	/* verify firmware */
-	g_debug ("verifying firmware");
+	fu_device_set_status (FU_DEVICE (device), FWUPD_STATUS_DEVICE_VERIFY);
 	ch_device_queue_verify_firmware (priv->device_queue, usb_device,
 					 g_bytes_get_data (fw, NULL),
 					 g_bytes_get_size (fw));

plugins/colorhug/fu-colorhug-device.h
@@ -49,12 +49,8 @@ gboolean	 fu_colorhug_device_set_flash_success	(FuColorhugDevice	*device,
 							 GError			**error);
 gboolean	 fu_colorhug_device_write_firmware	(FuColorhugDevice	*device,
 							 GBytes			*fw,
-							 GFileProgressCallback	 progress_cb,
-							 gpointer		 progress_data,
 							 GError			**error);
 gboolean	 fu_colorhug_device_verify_firmware	(FuColorhugDevice	*device,
-							 GFileProgressCallback	 progress_cb,
-							 gpointer		 progress_data,
 							 GError			**error);
 
 G_END_DECLS

plugins/colorhug/fu-plugin-colorhug.c
@@ -49,7 +49,6 @@ fu_plugin_update_detach (FuPlugin *plugin, FuDevice *device, GError **error)
 	}
 
 	/* reset */
-	fu_plugin_set_status (plugin, FWUPD_STATUS_DEVICE_RESTART);
 	if (!fu_colorhug_device_detach (colorhug_dev, error))
 		return FALSE;
 
@@ -90,7 +89,6 @@ fu_plugin_update_attach (FuPlugin *plugin, FuDevice *device, GError **error)
 	}
 
 	/* reset */
-	fu_plugin_set_status (plugin, FWUPD_STATUS_DEVICE_RESTART);
 	if (!fu_colorhug_device_attach (colorhug_dev, error))
 		return FALSE;
 
@@ -126,13 +124,6 @@ fu_plugin_update_reload (FuPlugin *plugin, FuDevice *device, GError **error)
 	return TRUE;
 }
 
-static void
-fu_plugin_colorhug_progress_cb (goffset current, goffset total, gpointer user_data)
-{
-	FuPlugin *plugin = FU_PLUGIN (user_data);
-	fu_plugin_set_percentage (plugin, (guint) current);
-}
-
 gboolean
 fu_plugin_update (FuPlugin *plugin,
 		  FuDevice *device,
@@ -162,11 +153,7 @@ fu_plugin_update (FuPlugin *plugin,
 	locker = fu_device_locker_new (device, error);
 	if (locker == NULL)
 		return FALSE;
-	fu_plugin_set_status (plugin, FWUPD_STATUS_DEVICE_WRITE);
-	return fu_colorhug_device_write_firmware (colorhug_dev, blob_fw,
-						  fu_plugin_colorhug_progress_cb,
-						  plugin,
-						  error);
+	return fu_colorhug_device_write_firmware (colorhug_dev, blob_fw, error);
 }
 
 gboolean
@@ -182,11 +169,7 @@ fu_plugin_verify (FuPlugin *plugin,
 	locker = fu_device_locker_new (device, error);
 	if (locker == NULL)
 		return FALSE;
-	fu_plugin_set_status (plugin, FWUPD_STATUS_DEVICE_VERIFY);
-	return fu_colorhug_device_verify_firmware (colorhug_dev,
-						   fu_plugin_colorhug_progress_cb,
-						   plugin,
-						   error);
+	return fu_colorhug_device_verify_firmware (colorhug_dev, error);
 }
 
 gboolean

commit 0a48529f164fdd4f9646a35e7367932ce22eebcc

synapticsmst: Correct the callback for progress
4a036018 broke the callback progress by putting the wrong device type in.

23:34:11:0715 GLib-GObject invalid cast from 'SynapticsMSTDevice' to 'FuDevice'
23:34:11:0715 Fu  fu_device_set_progress: assertion 'FU_IS_DEVICE (device)' failed

plugins/synapticsmst/fu-plugin-synapticsmst.c
@@ -333,7 +333,7 @@ fu_plugin_update (FuPlugin *plugin,
 		fu_device_set_status (dev, FWUPD_STATUS_DEVICE_WRITE);
 		if (!synapticsmst_device_write_firmware (device, blob_fw,
 							 fu_synapticsmst_write_progress_cb,
-							 device,
+							 dev,
 							 error)) {
 			g_prefix_error (error, "failed to flash firmware: ");
 			return FALSE;