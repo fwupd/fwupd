a076c81a07b0c8b55e9b64c617084978e6472a5a
Mario Limonciello <mario.limonciello@dell.com>
2018-02-13 13:42:13 -0500

    uefi: Log the efivar library version too into the report

--- plugins/uefi/fu-plugin-uefi.c
+++ plugins/uefi/fu-plugin-uefi.c
@@ -39,6 +39,7 @@ fu_plugin_init (FuPlugin *plugin)
 {
 	fu_plugin_add_rule (plugin, FU_PLUGIN_RULE_RUN_AFTER, "upower");
 	fu_plugin_add_report_metadata (plugin, "FwupdateVersion", LIBFWUP_LIBRARY_VERSION);
+	fu_plugin_add_report_metadata (plugin, "EfivarVersion", EFIVAR_LIBRARY_VERSION);
 }
 
 static gchar *
