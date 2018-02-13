41d5a1bb4f9f39ac5cf695eb75e307a9f83bc950
Mario Limonciello <mario.limonciello@dell.com>
2018-02-12 21:43:31 -0500

    uefi: Save SecureBoot status into debug log and LVFS report

--- plugins/uefi/fu-plugin-uefi.c
+++ plugins/uefi/fu-plugin-uefi.c
@@ -572,6 +572,26 @@ fu_plugin_uefi_coldplug_resource (FuPlugin *plugin, fwup_resource *re)
 	fu_plugin_device_add (plugin, dev);
 }
 
+static void
+fu_plugin_uefi_test_secure_boot (FuPlugin *plugin)
+{
+	const efi_guid_t guid = EFI_GLOBAL_GUID;
+	const gchar *result_str = "Disabled";
+	g_autofree guint8 *data = NULL;
+	gsize data_size = 0;
+	guint32 attributes = 0;
+	gint rc;
+
+	rc = efi_get_variable (guid, "SecureBoot", &data, &data_size, &attributes);
+	if (rc < 0)
+		return;
+	if (data_size >= 1 && data[0] & 1)
+		result_str = "Enabled";
+
+	g_debug ("SecureBoot is: %s", result_str);
+	fu_plugin_add_report_metadata (plugin, "SecureBoot", result_str);
+}
+
 gboolean
 fu_plugin_coldplug (FuPlugin *plugin, GError **error)
 {
@@ -621,5 +641,9 @@ fu_plugin_coldplug (FuPlugin *plugin, GError **error)
 	}
 	while (fwup_resource_iter_next (iter, &re) > 0)
 		fu_plugin_uefi_coldplug_resource (plugin, re);
+
+	/* for debugging problems later */
+	fu_plugin_uefi_test_secure_boot (plugin);
+
 	return TRUE;
 }
