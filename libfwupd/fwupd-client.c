f92d16f1383d3c3ff3da112b050e2947cfbf7748
Richard Hughes <richard@hughsie.com>
2018-02-19 16:47:45 +0000

    Fix running the self tests when no fwupd is installed
    Fixes https://github.com/hughsie/fwupd/issues/409

--- libfwupd/fwupd-client.c
+++ libfwupd/fwupd-client.c
@@ -334,6 +334,11 @@ fwupd_client_fixup_dbus_error (GError *error)
 				    G_DBUS_ERROR_SERVICE_UNKNOWN)) {
 		error->domain = FWUPD_ERROR;
 		error->code = FWUPD_ERROR_NOT_SUPPORTED;
+	} else if (g_error_matches (error,
+				    G_IO_ERROR,
+				    G_IO_ERROR_DBUS_ERROR)) {
+		error->domain = FWUPD_ERROR;
+		error->code = FWUPD_ERROR_NOT_SUPPORTED;
 	} else {
 		error->domain = FWUPD_ERROR;
 		error->code = FWUPD_ERROR_INTERNAL;
