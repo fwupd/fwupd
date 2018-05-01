2ee85143ed039612a418970e6a351d927a359206
Richard Hughes <richard@hughsie.com>
2018-05-01 20:39:10 +0100

    amt: Put the AMT device as a child under the system UEFI firmware

--- plugins/amt/fu-plugin-amt.c
+++ plugins/amt/fu-plugin-amt.c
@@ -435,6 +435,7 @@ fu_plugin_amt_create_device (GError **error)
 	fu_device_set_vendor (dev, "Intel Corporation");
 	fu_device_add_flag (dev, FWUPD_DEVICE_FLAG_INTERNAL);
 	fu_device_add_icon (dev, "computer");
+	fu_device_add_parent_guid (dev, "main-system-firmware");
 	if (!amt_get_provisioning_state (ctx, &state, error))
 		return NULL;
 	switch (state) {
