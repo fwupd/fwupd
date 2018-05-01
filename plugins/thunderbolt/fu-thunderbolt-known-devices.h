4b5d28655ccdbd1e126c0848cb6e4fb17962f032
Mario Limonciello <mario.limonciello@dell.com>
2018-05-01 12:02:29 -0500

    thunderbolt: Set known parent for Thunderbolt devices in Dell TB16

---
+++ plugins/thunderbolt/fu-thunderbolt-known-devices.h
@@ -0,0 +1,32 @@
+/* -*- mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2018 Dell Inc.
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef __FU_THUNDERBOLT_KNOWN_DEVICES_H__
+#define __FU_THUNDERBOLT_KNOWN_DEVICES_H__
+
+#define THUNDERBOLT_VENDOR_DELL			0x00d4
+
+/* Dell TB16 dock */
+#define THUNDERBOLT_DEVICE_DELL_TB16_CABLE	0xb051
+#define THUNDERBOLT_DEVICE_DELL_TB16_DOCK	0xb054
+#define PARENT_GUID_DELL_TB16			"e7ca1f36-bf73-4574-afe6-a4ccacabf479"
+
+#endif /* __FU_THUNDERBOLT_KNOWN_DEVICES_H__ */
