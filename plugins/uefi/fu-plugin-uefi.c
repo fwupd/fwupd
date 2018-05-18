fwupd/fwupd

commit cc664d7d36893295b2aa49c31e4cfec51849c9e7

amt: Put the AMT device as a child under the system UEFI firmware

plugins/uefi/fu-plugin-uefi.c
@@ -589,6 +589,7 @@ fu_plugin_uefi_coldplug_resource (FuPlugin *plugin, fwup_resource *re)
 	} else {
 		/* this is probably system firmware */
 		fu_device_add_icon (dev, "computer");
+		fu_device_add_guid (dev, "main-system-firmware");
 	}
 	fu_device_set_id (dev, id);
 	fu_device_add_guid (dev, guid);

plugins/amt/fu-plugin-amt.c
@@ -435,6 +435,7 @@ fu_plugin_amt_create_device (GError **error)
 	fu_device_set_vendor (dev, "Intel Corporation");
 	fu_device_add_flag (dev, FWUPD_DEVICE_FLAG_INTERNAL);
 	fu_device_add_icon (dev, "computer");
+	fu_device_add_parent_guid (dev, "main-system-firmware");
 	if (!amt_get_provisioning_state (ctx, &state, error))
 		return NULL;
 	switch (state) {

commit 3699be9658a0cc4239b4db6c72d2b58e92840628

uefi: Set the alternate GUID for parenting later
Several places in the UEFI plugin operate on the default GUID rather
than iterating a list of GUIDs.  This is normally fine since UEFI
GUIDs are tied to the ESRT and normally one FuDevice shouldn't
have multiple GUIDs.

The alternate GUID was added to set parents accordingly but this
caused no CAB files to be able to install.

Fixes: cc664d7d (amt: Put the AMT device as a child under the system UEFI firmware)

plugins/uefi/fu-plugin-uefi.c
@@ -583,14 +583,6 @@ fu_plugin_uefi_coldplug_resource (FuPlugin *plugin, fwup_resource *re)
 			      guid, hardware_instance);
 
 	dev = fu_device_new ();
-	if (uefi_type == FWUP_RESOURCE_TYPE_DEVICE_FIRMWARE) {
-		/* nothing better in the icon naming spec */
-		fu_device_add_icon (dev, "audio-card");
-	} else {
-		/* this is probably system firmware */
-		fu_device_add_icon (dev, "computer");
-		fu_device_add_guid (dev, "main-system-firmware");
-	}
 	fu_device_set_id (dev, id);
 	fu_device_add_guid (dev, guid);
 	fu_device_set_version (dev, version);
@@ -612,6 +604,14 @@ fu_plugin_uefi_coldplug_resource (FuPlugin *plugin, fwup_resource *re)
 		g_warning ("Kernel support for EFI variables missing");
 	}
 	fu_device_add_flag (dev, FWUPD_DEVICE_FLAG_REQUIRE_AC);
+	if (uefi_type == FWUP_RESOURCE_TYPE_DEVICE_FIRMWARE) {
+		/* nothing better in the icon naming spec */
+		fu_device_add_icon (dev, "audio-card");
+	} else {
+		/* this is probably system firmware */
+		fu_device_add_icon (dev, "computer");
+		fu_device_add_guid (dev, "main-system-firmware");
+	}
 	fu_plugin_device_add (plugin, dev);
 }
 