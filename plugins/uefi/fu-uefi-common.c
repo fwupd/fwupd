076685740763be371d213d93ad12c363be781a41
Richard Hughes <richard@hughsie.com>
2018-06-22 15:22:39 +0100

    uefi: Read the ESRT entries from a directory in sysfs

--- plugins/uefi/fu-uefi-common.c
+++ plugins/uefi/fu-uefi-common.c
@@ -9,6 +9,35 @@
 
 #include "fu-uefi-common.h"
 
+static gint
+fu_uefi_strcmp_sort_cb (gconstpointer a, gconstpointer b)
+{
+	const gchar *stra = *((const gchar **) a);
+	const gchar *strb = *((const gchar **) b);
+	return g_strcmp0 (stra, strb);
+}
+
+GPtrArray *
+fu_uefi_get_esrt_entry_paths (const gchar *esrt_path, GError **error)
+{
+	GPtrArray *entries = g_ptr_array_new_with_free_func (g_free);
+	const gchar *fn;
+	g_autofree gchar *esrt_entries = NULL;
+	g_autoptr(GDir) dir = NULL;
+
+	/* search ESRT */
+	esrt_entries = g_build_filename (esrt_path, "entries", NULL);
+	dir = g_dir_open (esrt_entries, 0, error);
+	if (dir == NULL)
+		return NULL;
+	while ((fn = g_dir_read_name (dir)) != NULL)
+		g_ptr_array_add (entries, g_build_filename (esrt_entries, fn, NULL));
+
+	/* sort by name */
+	g_ptr_array_sort (entries, fu_uefi_strcmp_sort_cb);
+	return entries;
+}
+
 guint64
 fu_uefi_read_file_as_uint64 (const gchar *path, const gchar *attr_name)
 {
