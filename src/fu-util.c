c3aa720cd8697c83f1a35105653e0a524d56f451
Richard Hughes <richard@hughsie.com>
2018-06-25 16:21:35 +0100

    Remove the libfwup dependency from fwupd
    In Fedora the only user of libfwupdate is fwupd and the fwupdate command line
    tool. It makes sense to absorb the libfwupdate library interface into the
    uefi plugin in fwupd. Benefits I can see include:
    
    * fwupd and fwupdate are very similar names; a lot of OEMs are confused
    
    * fwupd already depends on efivar for other things
    
    * We are maintaining an artificial library interface
    
    * The CI and translation hooks are already in place for fwupd
    
    * We don't need to check for features or versions in fwupd, we can just develop
    the feature (e.g. BGRT) all in one place.

--- src/fu-util.c
+++ src/fu-util.c
@@ -2491,10 +2491,6 @@ main (int argc, char *argv[])
 			G_USB_MAJOR_VERSION,
 			G_USB_MINOR_VERSION,
 			G_USB_MICRO_VERSION);
-#ifdef LIBFWUP_LIBRARY_VERSION
-		g_print ("\tfwupdate:\t%s\n",
-			LIBFWUP_LIBRARY_VERSION);
-#endif
 #ifdef EFIVAR_LIBRARY_VERSION
 		g_print ("\tefivar:\t%s\n",
 			EFIVAR_LIBRARY_VERSION);
