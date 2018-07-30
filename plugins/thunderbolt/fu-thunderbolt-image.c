bacb769c906d9018fe9039cad6a500a540915ac4
Mika Westerberg <mika.westerberg@linux.intel.com>
2018-07-30 10:19:50 +0300

    thunderbolt: Add native mode check for Titan Ridge host
    Titan Ridge has same set of bits than Alpine Ridge telling whether the
    Thunderbolt controller is in native or legacy mode. Add validation for
    that.
    
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>

--- plugins/thunderbolt/fu-thunderbolt-image.c
+++ plugins/thunderbolt/fu-thunderbolt-image.c
@@ -406,6 +406,7 @@ get_host_locations (guint16 id)
 		{ .offset = 0xB6,  .len = 1, .description = "PA/PB (USB2)", .mask = 0xC0 },
 		{ .offset = 0x5E,  .len = 1, .description = "Aux", .mask = 0x0F },
 		{ .offset = 0x45,  .len = 1, .description = "Flash Size", .mask = 0x07 },
+		{ .offset = 0x7B,  .len = 1, .description = "Native", .mask = 0x20 },
 		{ 0 },
 
 		{ .offset = 0x13,  .len = 1, .description = "PB", .mask = 0xCC, .section = DRAM_UCODE_SECTION },
