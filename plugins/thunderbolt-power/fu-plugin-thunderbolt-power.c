7154f6339745eea16e373d2269e4d4e2882cae0d
Mario Limonciello <mario.limonciello@dell.com>
2018-08-04 20:18:22 -0500

    trivial: Adjust syntax of varying copyright holders to unify across project

--- plugins/thunderbolt-power/fu-plugin-thunderbolt-power.c
+++ plugins/thunderbolt-power/fu-plugin-thunderbolt-power.c
@@ -1,6 +1,6 @@
 /* -*- mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
  *
- * Copyright (C) 2017 Dell Inc.
+ * Copyright (C) 2017 Dell, Inc.
  *
  * SPDX-License-Identifier: LGPL-2.1+
  */
