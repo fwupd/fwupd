f3fc6461488ba7b3dfad6c4ff33b953a3f1abb8f
Richard Hughes <richard@hughsie.com>
2018-08-13 08:57:54 +0100

    unifying: Allow flashing devices in recovery mode
    In version comparison, RQR24.xx_Bxxxx > RQR24.05_B0029 which is not what we
    wanted at all.
    
    See https://github.com/hughsie/fwupd/issues/655 for more details.

--- plugins/unifying/lu-device-peripheral.c
+++ plugins/unifying/lu-device-peripheral.c
@@ -361,7 +361,7 @@ lu_device_peripheral_probe (LuDevice *device, GError **error)
 		lu_device_add_flag (device, LU_DEVICE_FLAG_REQUIRES_ATTACH);
 		if (fu_device_get_version (device) == NULL) {
 			g_debug ("repairing device in bootloader mode");
-			fu_device_set_version (FU_DEVICE (device), "MPKxx.xx_Bxxxx");
+			fu_device_set_version (FU_DEVICE (device), "MPK00.00_B0000");
 		}
 	}
 
