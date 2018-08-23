2d9ba8d1a388cd9a0f9b6cb9ffb977b4038ab920
Richard Hughes <richard@hughsie.com>
2018-08-23 09:46:52 +0100

    trivial: Don't include the default GUID in the device ID if NULL

--- src/fu-device.c
+++ src/fu-device.c
@@ -839,10 +839,14 @@ fu_device_set_platform_id (FuDevice *device, const gchar *platform_id)
 
 	/* automatically use this */
 	if (fu_device_get_id (device) == NULL) {
-		g_autofree gchar *id_guid = NULL;
-		id_guid = g_strdup_printf ("%s:%s", platform_id,
-					   fu_device_get_guid_default (device));
-		fu_device_set_id (device, id_guid);
+		if (fu_device_get_guid_default (device) != NULL) {
+			g_autofree gchar *id_guid = NULL;
+			id_guid = g_strdup_printf ("%s:%s", platform_id,
+						   fu_device_get_guid_default (device));
+			fu_device_set_id (device, id_guid);
+		} else {
+			fu_device_set_id (device, platform_id);
+		}
 	}
 	fu_device_set_metadata (device, "platform-id", platform_id);
 }
