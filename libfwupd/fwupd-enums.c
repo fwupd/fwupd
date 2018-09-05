392ac029f7d10ee30e2827ae7040881cf965ff50
Mario Limonciello <mario.limonciello@dell.com>
2018-09-04 22:34:04 -0500

    Add a new device flag "ignore-validation" that will override validation checks
    This flag isn't really intended for production, but more for development
    to allow setting a quirk for a device during a transition period.

--- libfwupd/fwupd-enums.c
+++ libfwupd/fwupd-enums.c
@@ -147,6 +147,8 @@ fwupd_device_flag_to_string (FwupdDeviceFlags device_flag)
 		return "is-bootloader";
 	if (device_flag == FWUPD_DEVICE_FLAG_WAIT_FOR_REPLUG)
 		return "wait-for-replug";
+	if (device_flag == FWUPD_DEVICE_FLAG_IGNORE_VALIDATION)
+		return "ignore-validation";
 	if (device_flag == FWUPD_DEVICE_FLAG_UNKNOWN)
 		return "unknown";
 	return NULL;
@@ -199,6 +201,8 @@ fwupd_device_flag_from_string (const gchar *device_flag)
 		return FWUPD_DEVICE_FLAG_IS_BOOTLOADER;
 	if (g_strcmp0 (device_flag, "wait-for-replug") == 0)
 		return FWUPD_DEVICE_FLAG_WAIT_FOR_REPLUG;
+	if (g_strcmp0 (device_flag, "ignore-validation") == 0)
+		return FWUPD_DEVICE_FLAG_IGNORE_VALIDATION;
 	return FWUPD_DEVICE_FLAG_UNKNOWN;
 }
 
