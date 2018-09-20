7cc7b04d671940f2c91325d2b7d7b9cbf3a5137a
Mario Limonciello <mario.limonciello@dell.com>
2018-09-20 17:25:14 -0500

    ci: Run clang static analysis for Ubuntu build rather than just clang compiler

--- contrib/ci/ubuntu.sh
+++ contrib/ci/ubuntu.sh
@@ -1,1 +1,10 @@
-debian.sh
+#!/bin/sh
+set -e
+set -x
+
+rm -rf build
+meson build --werror
+#build with clang and -Werror
+ninja -C build -v
+#run static analysis (these mostly won't be critical)
+ninja -C build scan-build -v
