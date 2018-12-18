09cbacd771da5debcc2e109063bee43f2b525a09
Richard Hughes <richard@hughsie.com>
2018-12-18 09:32:10 +0000

    trivial: Move Fedora CI to f29
    We'll need this for some updated deps soon.

--- contrib/ci/fedora.sh
+++ contrib/ci/fedora.sh
@@ -49,12 +49,22 @@ dnf install -y $HOME/rpmbuild/RPMS/*/*.rpm
 
 cp $HOME/rpmbuild/RPMS/*/*.rpm dist
 
-# run the installed tests
 if [ "$CI" = "true" ]; then
 	sed "s,^BlacklistPlugins=test,BlacklistPlugins=," -i /etc/fwupd/daemon.conf
+
+	# set up enough PolicyKit and D-Bus to run the daemon
 	mkdir -p /run/dbus
 	mkdir -p /var
 	ln -s /var/run /run
 	dbus-daemon --system --fork
+	/usr/lib/polkit-1/polkitd &
+	sleep 5
+
+	# run the daemon startup to check it can start
+	/usr/libexec/fwupd/fwupd --immediate-exit --verbose
+
+	# run the installed tests whilst the daemon debugging
+	/usr/libexec/fwupd/fwupd --verbose &
+	sleep 10
 	gnome-desktop-testing-runner fwupd
 fi
