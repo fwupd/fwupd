44ce5aeae4a07fc661ecf1fae37aaa073c4b881c
Richard Hughes <richard@hughsie.com>
2019-01-18 09:51:47 +0000

    uefi: Add a quirk to use the legacy bootmgr description
    Some hardware from Lenovo deduplicates UEFI Boot entries, and uses the old
    string 'Linux-Firmware-Updater' to avoid removing the firmware update entry.
    Although this is forbidden in the UEFI specification we shouldn't break
    firmware updates from old firmware versions.
    
    Provide a quirk for this, and automatically whitelist anything with the LENOVO
    SMBIOS Manufacturer.

--- plugins/uefi/fu-plugin-uefi.c
+++ plugins/uefi/fu-plugin-uefi.c
@@ -511,6 +511,12 @@ fu_plugin_uefi_coldplug_device (FuPlugin *plugin, FuUefiDevice *dev, GError **er
 	if (!fu_device_probe (FU_DEVICE (dev), error))
 		return FALSE;
 
+	/* set this flag for all Lenovo hardware */
+	if (fu_plugin_check_hwid (plugin, "6de5d951-d755-576b-bd09-c5cf66b27234")) {
+		fu_device_set_custom_flags (FU_DEVICE (dev), "use-legacy-bootmgr-desc");
+		fu_plugin_add_report_metadata (plugin, "BootMgrDesc", "legacy");
+	}
+
 	/* set fallback name if nothing else is set */
 	if (fu_device_get_name (FU_DEVICE (dev)) == 0) {
 		g_autofree gchar *name = NULL;
