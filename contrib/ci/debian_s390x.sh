dc73a4756387188f36501e00b629a54e6d05872a
Mario Limonciello <mario.limonciello@dell.com>
2019-02-03 21:13:12 -0600

    trivial: read and use all build flags used by Debian and Ubuntu builds
    Filter out -Wl,-Bsymbolic-functions and run the test suite to make
    sure any future build flags introduced will cause errors.

--- contrib/ci/debian_s390x.sh
+++ contrib/ci/debian_s390x.sh
@@ -3,6 +3,12 @@ set -e
 set -x
 
 export LC_ALL=C.UTF-8
+
+#evaluate using Debian's build flags
+eval "$(dpkg-buildflags --export=sh)"
+#filter out -Bsymbolic-functions
+export LDFLAGS=$(dpkg-buildflags --get LDFLAGS | sed "s/-Wl,-Bsymbolic-functions\s//")
+
 rm -rf build
 mkdir -p build
 cp contrib/ci/s390x_cross.txt build/
--- contrib/ci/ubuntu.sh
+++ contrib/ci/ubuntu.sh
@@ -2,9 +2,14 @@
 set -e
 set -x
 
+#evaluate using Ubuntu's buildflags
+eval "$(dpkg-buildflags --export=sh)"
+#filter out -Bsymbolic-functions
+export LDFLAGS=$(dpkg-buildflags --get LDFLAGS | sed "s/-Wl,-Bsymbolic-functions\s//")
+
 rm -rf build
 meson build --werror
 #build with clang and -Werror
-ninja -C build -v
+ninja -C build test -v
 #run static analysis (these mostly won't be critical)
 ninja -C build scan-build -v
