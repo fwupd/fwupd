f28abe7fda25bab25a401fb8df94f57d6c0cd74f
Richard Hughes <richard@hughsie.com>
2019-03-07 16:01:02 +0000

    Allow signing and verifying data using a PKCS-7 self-signed client certificate
    This is a per-client certificate and private key that can be used to sign
    metadata sent to remote servers, for instance success/failure reports.

--- src/fu-keyring.h
+++ src/fu-keyring.h
@@ -16,6 +16,19 @@ G_BEGIN_DECLS
 #define FU_TYPE_KEYRING (fu_keyring_get_type ())
 G_DECLARE_DERIVABLE_TYPE (FuKeyring, fu_keyring, FU, KEYRING, GObject)
 
+typedef enum {
+	FU_KEYRING_VERIFY_FLAG_NONE		= 0,
+	FU_KEYRING_VERIFY_FLAG_USE_CLIENT_CERT	= 1 << 1,
+	FU_KEYRING_VERIFY_FLAG_LAST
+} FuKeyringVerifyFlags;
+
+typedef enum {
+	FU_KEYRING_SIGN_FLAG_NONE		= 0,
+	FU_KEYRING_SIGN_FLAG_ADD_TIMESTAMP	= 1 << 0,
+	FU_KEYRING_SIGN_FLAG_ADD_CERT		= 1 << 1,
+	FU_KEYRING_SIGN_FLAG_LAST
+} FuKeyringSignFlags;
+
 struct _FuKeyringClass
 {
 	GObjectClass		 parent_class;
@@ -27,6 +40,11 @@ struct _FuKeyringClass
 	FuKeyringResult		*(*verify_data)		(FuKeyring	*keyring,
 							 GBytes		*payload,
 							 GBytes		*payload_signature,
+							 FuKeyringVerifyFlags flags,
+							 GError		**error);
+	GBytes			*(*sign_data)		(FuKeyring	*keyring,
+							 GBytes		*payload,
+							 FuKeyringSignFlags flags,
 							 GError		**error);
 };
 
@@ -38,6 +56,11 @@ gboolean	 fu_keyring_add_public_keys		(FuKeyring	*keyring,
 FuKeyringResult	*fu_keyring_verify_data			(FuKeyring	*keyring,
 							 GBytes		*blob,
 							 GBytes		*blob_signature,
+							 FuKeyringVerifyFlags flags,
+							 GError		**error);
+GBytes		*fu_keyring_sign_data			(FuKeyring	*keyring,
+							 GBytes		*blob,
+							 FuKeyringSignFlags flags,
 							 GError		**error);
 const gchar	*fu_keyring_get_name			(FuKeyring	*self);
 void		 fu_keyring_set_name			(FuKeyring	*self,
