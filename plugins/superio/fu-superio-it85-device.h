ebedf62b15973fd2f53a673396db024e21784f9d
Richard Hughes <richard@hughsie.com>
2019-03-22 11:49:50 +0000

    superio: Move all the IT85xx code to a subclassed device object

---
+++ plugins/superio/fu-superio-it85-device.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2018-2019 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include "fu-superio-device.h"
+
+G_BEGIN_DECLS
+
+#define FU_TYPE_SUPERIO_IT85_DEVICE (fu_superio_it85_device_get_type ())
+G_DECLARE_FINAL_TYPE (FuSuperioIt85Device, fu_superio_it85_device, FU, SUPERIO_IT85_DEVICE, FuSuperioDevice)
+
+G_END_DECLS
