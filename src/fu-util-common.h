891284d7f17b76af09cad2556dc7ab5eb2ec34b9
Mario Limonciello <mario.limonciello@dell.com>
2019-05-07 15:52:39 -0500

    fu-util: Check that daemon is started by expected unit
    Unexpected behaviors can happen if:
    * a snapped daemon is running with a packaged frontend
    * a packaged daemon is running with a snapped frontend
    
    This should make sure that if the snap is installed on top of a
    packaged frontend that people don't try to mix and match as much.

--- src/fu-util-common.h
+++ src/fu-util-common.h
@@ -60,6 +60,7 @@ gboolean	 fu_util_cmd_array_run		(GPtrArray	*array,
 						 GError		**error);
 gchar		*fu_util_release_get_name	(FwupdRelease	*release);
 
+gboolean	 fu_util_using_correct_daemon	(GError		**error);
 gboolean	 fu_util_stop_daemon		(GError		**error);
 
 G_END_DECLS
