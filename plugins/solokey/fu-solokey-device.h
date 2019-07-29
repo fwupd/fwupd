d8f78d0b93f32194b3d94136b8954e85d1e10bbf
Richard Hughes <richard@hughsie.com>
2019-07-29 12:18:12 +0100

    Add support for the SoloKey Secure

---
+++ plugins/solokey/fu-solokey-device.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2019 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include "fu-plugin.h"
+
+G_BEGIN_DECLS
+
+#define FU_TYPE_SOLOKEY_DEVICE (fu_solokey_device_get_type ())
+G_DECLARE_FINAL_TYPE (FuSolokeyDevice, fu_solokey_device, FU, SOLOKEY_DEVICE, FuUsbDevice)
+
+FuSolokeyDevice *fu_solokey_device_new		(FuUsbDevice		*device);
+
+G_END_DECLS
