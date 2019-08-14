b962bc206bb27aff572cdc00263990423ec2f8cb
Richard Hughes <richard@hughsie.com>
2019-08-14 09:52:11 +0100

    synapticsmst: Use G_DECLARE_FINAL_TYPE to simplify the object
    No logic changes.

--- plugins/synapticsmst/synapticsmst-device.h
+++ plugins/synapticsmst/synapticsmst-device.h
@@ -13,14 +13,9 @@
 G_BEGIN_DECLS
 
 #define SYNAPTICSMST_TYPE_DEVICE (synapticsmst_device_get_type ())
-G_DECLARE_DERIVABLE_TYPE (SynapticsMSTDevice, synapticsmst_device, SYNAPTICSMST, DEVICE, GObject)
+G_DECLARE_FINAL_TYPE (SynapticsMSTDevice, synapticsmst_device, SYNAPTICSMST, DEVICE, GObject)
 
 #define SYSFS_DRM_DP_AUX "/sys/class/drm_dp_aux_dev"
-
-struct _SynapticsMSTDeviceClass
-{
-	GObjectClass		parent_class;
-};
 
 /**
  * SynapticsMSTDeviceKind:
@@ -54,27 +49,27 @@ SynapticsMSTDevice	*synapticsmst_device_new	(SynapticsMSTDeviceKind kind,
 /* helpers */
 SynapticsMSTDeviceKind synapticsmst_device_kind_from_string	(const gchar	*kind);
 const gchar	*synapticsmst_device_kind_to_string		(SynapticsMSTDeviceKind kind);
-GPtrArray 	*synapticsmst_device_get_guids 			(SynapticsMSTDevice *device);
-gboolean	 synapticsmst_device_scan_cascade_device 	(SynapticsMSTDevice *device,
+GPtrArray 	*synapticsmst_device_get_guids 			(SynapticsMSTDevice *self);
+gboolean	 synapticsmst_device_scan_cascade_device 	(SynapticsMSTDevice *self,
 								 GError **error,
 								 guint8 tx_port);
-gboolean	 synapticsmst_device_open 			(SynapticsMSTDevice *device,
+gboolean	 synapticsmst_device_open 			(SynapticsMSTDevice *self,
 								 GError	**error);
 
 /* getters */
-SynapticsMSTDeviceKind synapticsmst_device_get_kind		(SynapticsMSTDevice *device);
-guint16		 synapticsmst_device_get_board_id	 	(SynapticsMSTDevice *device);
-const gchar	*synapticsmst_device_get_version		(SynapticsMSTDevice *device);
-const gchar 	*synapticsmst_device_get_chip_id_str 		(SynapticsMSTDevice *device);
-const gchar 	*synapticsmst_device_get_aux_node		(SynapticsMSTDevice *device);
-guint16 	 synapticsmst_device_get_rad 			(SynapticsMSTDevice *device);
-guint8 		 synapticsmst_device_get_layer 			(SynapticsMSTDevice *device);
-gboolean	 synapticsmst_device_get_cascade		(SynapticsMSTDevice *device);
+SynapticsMSTDeviceKind synapticsmst_device_get_kind		(SynapticsMSTDevice *self);
+guint16		 synapticsmst_device_get_board_id	 	(SynapticsMSTDevice *self);
+const gchar	*synapticsmst_device_get_version		(SynapticsMSTDevice *self);
+const gchar 	*synapticsmst_device_get_chip_id_str 		(SynapticsMSTDevice *self);
+const gchar 	*synapticsmst_device_get_aux_node		(SynapticsMSTDevice *self);
+guint16 	 synapticsmst_device_get_rad 			(SynapticsMSTDevice *self);
+guint8 		 synapticsmst_device_get_layer 			(SynapticsMSTDevice *self);
+gboolean	 synapticsmst_device_get_cascade		(SynapticsMSTDevice *self);
 
 /* object methods */
 gboolean	 synapticsmst_device_enumerate_device 		(SynapticsMSTDevice *devices,
 								 GError **error);
-gboolean	 synapticsmst_device_write_firmware		(SynapticsMSTDevice *device,
+gboolean	 synapticsmst_device_write_firmware		(SynapticsMSTDevice *self,
 								 GBytes	*fw,
 								 GFileProgressCallback	progress_cb,
 								 gpointer user_data,
