bb59837ae0cd4ecfb7ab22e71cef172efdca4805
Richard Hughes <richard@hughsie.com>
2019-09-18 10:15:34 +0100

    Add a new plugin to update Conexant audio devices

---
+++ plugins/synaptics-cxaudio/fu-synaptics-cxaudio-device.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2019 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include "fu-plugin.h"
+
+G_BEGIN_DECLS
+
+#define FU_TYPE_SYNAPTICS_CXAUDIO_DEVICE (fu_synaptics_cxaudio_device_get_type ())
+G_DECLARE_FINAL_TYPE (FuSynapticsCxaudioDevice, fu_synaptics_cxaudio_device, FU, SYNAPTICS_CXAUDIO_DEVICE, FuUsbDevice)
+
+struct _FuSynapticsCxaudioDeviceClass
+{
+	FuUsbDeviceClass	parent_class;
+};
+
+FuSynapticsCxaudioDevice	*fu_synaptics_cxaudio_device_new	(FuUsbDevice	*usb_device);
+
+G_END_DECLS
---
+++ plugins/synaptics-cxaudio/fu-synaptics-cxaudio-firmware.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2005-2019 Synaptics Incorporated
+ * Copyright (C) 2019 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include "fu-srec-firmware.h"
+#include "fu-synaptics-cxaudio-common.h"
+
+G_BEGIN_DECLS
+
+#define FU_TYPE_SYNAPTICS_CXAUDIO_FIRMWARE (fu_synaptics_cxaudio_firmware_get_type ())
+G_DECLARE_FINAL_TYPE (FuSynapticsCxaudioFirmware, fu_synaptics_cxaudio_firmware, FU, SYNAPTICS_CXAUDIO_FIRMWARE, FuSrecFirmware)
+
+typedef enum {
+	FU_SYNAPTICS_CXAUDIO_FILE_KIND_UNKNOWN,
+	FU_SYNAPTICS_CXAUDIO_FILE_KIND_CX2070X_FW,
+	FU_SYNAPTICS_CXAUDIO_FILE_KIND_CX2070X_PATCH,
+	FU_SYNAPTICS_CXAUDIO_FILE_KIND_CX2077X_PATCH,
+	FU_SYNAPTICS_CXAUDIO_FILE_KIND_CX2076X_PATCH,
+	FU_SYNAPTICS_CXAUDIO_FILE_KIND_CX2085X_PATCH,
+	FU_SYNAPTICS_CXAUDIO_FILE_KIND_CX2089X_PATCH,
+	FU_SYNAPTICS_CXAUDIO_FILE_KIND_CX2098X_PATCH,
+	FU_SYNAPTICS_CXAUDIO_FILE_KIND_CX2198X_PATCH,
+	FU_SYNAPTICS_CXAUDIO_FILE_KIND_LAST
+} FuSynapticsCxaudioFileKind;
+
+FuFirmware			*fu_synaptics_cxaudio_firmware_new			(void);
+FuSynapticsCxaudioFileKind	 fu_synaptics_cxaudio_firmware_get_file_type		(FuSynapticsCxaudioFirmware	*self);
+FuSynapticsCxaudioDeviceKind	 fu_synaptics_cxaudio_firmware_get_devtype		(FuSynapticsCxaudioFirmware	*self);
+guint8				 fu_synaptics_cxaudio_firmware_get_layout_version	(FuSynapticsCxaudioFirmware	*self);
+
+G_END_DECLS
