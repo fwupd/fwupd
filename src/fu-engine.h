0917fb6aec177375a2241f57d63e21a71fe19cd6
Richard Hughes <richard@hughsie.com>
2019-09-21 12:55:37 +0100

    Export the salted machine ID as a daemon property

--- src/fu-engine.h
+++ src/fu-engine.h
@@ -49,6 +49,7 @@ gboolean	 fu_engine_load_plugins			(FuEngine	*self,
 							 GError		**error);
 gboolean	 fu_engine_get_tainted			(FuEngine	*self);
 const gchar	*fu_engine_get_host_product		(FuEngine *self);
+const gchar	*fu_engine_get_host_machine_id		(FuEngine *self);
 FwupdStatus	 fu_engine_get_status			(FuEngine	*self);
 XbSilo		*fu_engine_get_silo_from_blob		(FuEngine	*self,
 							 GBytes		*blob_cab,
