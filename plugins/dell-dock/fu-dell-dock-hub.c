fwupd/fwupd

commit 7afd7cba0d327b155b2201c9fbb5414ced9cfe06

Use FuFirmware as a container for firmware images
In many plugins we've wanted to use ->prepare_firmware() to parse the firmware
ahead of ->detach() and ->write_firmware() but this has the limitation that it
can only return a single blob of data.

For many devices, multiple binary blobs are required from one parsed image,
for instance providing signatures, config and data blobs that have to be pushed
to the device in different way.

This also means we parse the firmware *before* we ask the user to detach.

Break the internal FuDevice API to support these firmware types as they become
more popular.

This also allows us to move the Intel HEX and SREC parsing out of the dfu plugin
as they are used by a few plugins now, and resolving symbols between plugins
isn't exactly awesome.

plugins/dell-dock/fu-dell-dock-hub.c
@@ -46,22 +46,29 @@ fu_dell_dock_hub_probe (FuDevice *device, GError **error)
 
 static gboolean
 fu_dell_dock_hub_write_fw (FuDevice *device,
-			   GBytes *blob_fw,
+			   FuFirmware *firmware,
 			   FwupdInstallFlags flags,
 			   GError **error)
 {
 	FuDellDockHub *self = FU_DELL_DOCK_HUB (device);
 	gsize fw_size = 0;
-	const guint8 *data = g_bytes_get_data (blob_fw, &fw_size);
+	const guint8 *data;
 	gsize write_size =
 	    (fw_size / HIDI2C_MAX_WRITE) >= 1 ? HIDI2C_MAX_WRITE : fw_size;
 	gsize nwritten = 0;
 	guint32 address = 0;
 	gboolean result = FALSE;
 	g_autofree gchar *dynamic_version = NULL;
+	g_autoptr(GBytes) fw = NULL;
 
 	g_return_val_if_fail (device != NULL, FALSE);
-	g_return_val_if_fail (blob_fw != NULL, FALSE);
+	g_return_val_if_fail (FU_IS_FIRMWARE (firmware), FALSE);
+
+	/* get default image */
+	fw = fu_firmware_get_image_default_bytes (firmware, error);
+	if (fw == NULL)
+		return FALSE;
+	data = g_bytes_get_data (fw, &fw_size);
 
 	dynamic_version = g_strdup_printf ("%02x.%02x",
 					   data[self->blob_major_offset],

commit 9e3f5a859f9989fd4c649d8f3d0c5f4e093471f0

trivial: dell-dock: correct another write_size error
This was more fallout from 7afd7cba0d327b155b2201c9fbb5414ced9cfe06
which led to write_size being set to 0 and an endless loop.

plugins/dell-dock/fu-dell-dock-hub.c
@@ -53,8 +53,7 @@ fu_dell_dock_hub_write_fw (FuDevice *device,
 	FuDellDockHub *self = FU_DELL_DOCK_HUB (device);
 	gsize fw_size = 0;
 	const guint8 *data;
-	gsize write_size =
-	    (fw_size / HIDI2C_MAX_WRITE) >= 1 ? HIDI2C_MAX_WRITE : fw_size;
+	gsize write_size;
 	gsize nwritten = 0;
 	guint32 address = 0;
 	gboolean result = FALSE;
@@ -69,6 +68,8 @@ fu_dell_dock_hub_write_fw (FuDevice *device,
 	if (fw == NULL)
 		return FALSE;
 	data = g_bytes_get_data (fw, &fw_size);
+	write_size = (fw_size / HIDI2C_MAX_WRITE) >= 1 ?
+				 HIDI2C_MAX_WRITE : fw_size;
 
 	dynamic_version = g_strdup_printf ("%02x.%02x",
 					   data[self->blob_major_offset],