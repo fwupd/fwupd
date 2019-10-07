d6fc172ec9bd8e54931ca77d8cf3646df77a59c9
Richard Hughes <richard@hughsie.com>
2019-10-07 09:50:01 +0100

    synaptics-rmi: Check the correct register when querying bootloader mode

--- plugins/synaptics-rmi/fu-synaptics-rmi-device.c
+++ plugins/synaptics-rmi/fu-synaptics-rmi-device.c
@@ -456,7 +456,6 @@ fu_synaptics_rmi_device_setup (FuDevice *device, GError **error)
 	g_autofree gchar *fw_ver = NULL;
 	g_autofree gchar *instance_id = NULL;
 	g_autoptr(GByteArray) f01_basic = NULL;
-	g_autoptr(GByteArray) f01_db = NULL;
 	g_autoptr(GByteArray) f01_product_id = NULL;
 	g_autoptr(GByteArray) f01_ds4 = NULL;
 
@@ -578,18 +577,6 @@ fu_synaptics_rmi_device_setup (FuDevice *device, GError **error)
 	if (!klass_rmi->query_status (self, error)) {
 		g_prefix_error (error, "failed to read bootloader status: ");
 		return FALSE;
-	}
-
-	/* in bootloader mode */
-	f01_db = fu_synaptics_rmi_device_read (self, priv->f01->data_base, 0x1, error);
-	if (f01_db == NULL) {
-		g_prefix_error (error, "failed to read the f01 data base: ");
-		return FALSE;
-	}
-	if (f01_db->data[0] & 0x40) {
-		fu_device_add_flag (device, FWUPD_DEVICE_FLAG_IS_BOOTLOADER);
-	} else {
-		fu_device_remove_flag (device, FWUPD_DEVICE_FLAG_IS_BOOTLOADER);
 	}
 
 	/* set versions */
--- plugins/synaptics-rmi/fu-synaptics-rmi-v7-device.c
+++ plugins/synaptics-rmi/fu-synaptics-rmi-v7-device.c
@@ -533,19 +533,19 @@ fu_synaptics_rmi_v7_device_setup (FuSynapticsRmiDevice *self, GError **error)
 gboolean
 fu_synaptics_rmi_v7_device_query_status (FuSynapticsRmiDevice *self, GError **error)
 {
-	FuSynapticsRmiFunction *f01;
-	g_autoptr(GByteArray) f01_db = NULL;
+	FuSynapticsRmiFunction *f34;
+	g_autoptr(GByteArray) f34_data = NULL;
 
-	/* f01 */
-	f01 = fu_synaptics_rmi_device_get_function (self, 0x01, error);
-	if (f01 == NULL)
+	/* f34 */
+	f34 = fu_synaptics_rmi_device_get_function (self, 0x34, error);
+	if (f34 == NULL)
 		return FALSE;
-	f01_db = fu_synaptics_rmi_device_read (self, f01->data_base, 0x1, error);
-	if (f01_db == NULL) {
+	f34_data = fu_synaptics_rmi_device_read (self, f34->data_base, 0x1, error);
+	if (f34_data == NULL) {
 		g_prefix_error (error, "failed to read the f01 data base: ");
 		return FALSE;
 	}
-	if (f01_db->data[0] & 0x80) {
+	if (f34_data->data[0] & 0x80) {
 		fu_device_add_flag (FU_DEVICE (self), FWUPD_DEVICE_FLAG_IS_BOOTLOADER);
 	} else {
 		fu_device_remove_flag (FU_DEVICE (self), FWUPD_DEVICE_FLAG_IS_BOOTLOADER);
