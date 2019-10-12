e01532edc192466a9545c8a476b3bd7d99cfd071
Mario Limonciello <mario.limonciello@dell.com>
2019-10-12 18:01:55 -0500

    trivial: fu-tool: typo

--- src/fu-tool.c
+++ src/fu-tool.c
@@ -118,7 +118,7 @@ fu_util_start_engine (FuUtilPrivate *priv, FuEngineLoadFlags flags, GError **err
 
 #ifdef HAVE_SYSTEMD
 	if (!fu_systemd_unit_stop (fu_util_get_systemd_unit (), &error_local))
-		g_debug ("Failed top stop daemon: %s", error_local->message);
+		g_debug ("Failed to stop daemon: %s", error_local->message);
 #endif
 	if (!fu_engine_load (priv->engine, flags, error))
 		return FALSE;
