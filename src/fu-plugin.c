fwupd/fwupd

commit a9be536ef0d760a39b107b88b21ac89d4d5a1720

trivial: fu-plugin: quiet down debug output for optionrom plugin
All of these messages continually output are just noise for unsupported
devices:

```
18:11:52:0851 FuPlugin             using generic udev_device_added() on optionrom
18:11:52:0851 FuEngine             optionrom ignoring: no FWUPD_GUID property
```

src/fu-plugin.c
@@ -1512,8 +1512,14 @@ fu_plugin_runner_usb_device_added (FuPlugin *self, FuUsbDevice *device, GError *
 	if (func == NULL) {
 		if (priv->device_gtype != G_TYPE_INVALID ||
 		    fu_device_get_specialized_gtype (FU_DEVICE (device)) != G_TYPE_INVALID) {
-			g_debug ("using generic usb_device_added() on %s", priv->name);
-			return fu_plugin_usb_device_added (self, device, error);
+			if (!fu_plugin_usb_device_added (self, device, &error_local)) {
+				if (g_error_matches (error_local,
+						     FWUPD_ERROR,
+						     FWUPD_ERROR_NOT_SUPPORTED))
+					return TRUE;
+				g_propagate_error (error, error_local);
+				return FALSE;
+			}
 		}
 		return TRUE;
 	}
@@ -1555,8 +1561,14 @@ fu_plugin_runner_udev_device_added (FuPlugin *self, FuUdevDevice *device, GError
 	if (func == NULL) {
 		if (priv->device_gtype != G_TYPE_INVALID ||
 		    fu_device_get_specialized_gtype (FU_DEVICE (device)) != G_TYPE_INVALID) {
-			g_debug ("using generic udev_device_added() on %s", priv->name);
-			return fu_plugin_udev_device_added (self, device, error);
+			if (!fu_plugin_udev_device_added (self, device, &error_local)) {
+				if (g_error_matches (error_local,
+						     FWUPD_ERROR,
+						     FWUPD_ERROR_NOT_SUPPORTED))
+					return TRUE;
+				g_propagate_error (error, error_local);
+				return FALSE;
+			}
 		}
 		return TRUE;
 	}

commit cbf53fe52d052896b255a8848347ff5d0c39e254

trivial: Fix a recent regression causing a double-free
This was introduced accidentally in a9be536ef0d760a39b107b88b21ac89d4d5a1720.

Fixes https://github.com/fwupd/fwupd/issues/1461

src/fu-plugin.c
@@ -1517,7 +1517,7 @@ fu_plugin_runner_usb_device_added (FuPlugin *self, FuUsbDevice *device, GError *
 						     FWUPD_ERROR,
 						     FWUPD_ERROR_NOT_SUPPORTED))
 					return TRUE;
-				g_propagate_error (error, error_local);
+				g_propagate_error (error, g_steal_pointer (&error_local));
 				return FALSE;
 			}
 		}
@@ -1566,7 +1566,7 @@ fu_plugin_runner_udev_device_added (FuPlugin *self, FuUdevDevice *device, GError
 						     FWUPD_ERROR,
 						     FWUPD_ERROR_NOT_SUPPORTED))
 					return TRUE;
-				g_propagate_error (error, error_local);
+				g_propagate_error (error, g_steal_pointer (&error_local));
 				return FALSE;
 			}
 		}