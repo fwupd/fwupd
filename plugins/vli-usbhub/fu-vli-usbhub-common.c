f04be98153dbfb676b424dbaa209ae7c3fd4013d
Richard Hughes <richard@hughsie.com>
2019-10-24 13:14:07 +0100

    vli-usbhub: Add support for updating V2 devices

--- plugins/vli-usbhub/fu-vli-usbhub-common.c
+++ plugins/vli-usbhub/fu-vli-usbhub-common.c
@@ -67,4 +67,25 @@ void
 fu_vli_usbhub_header_to_string (FuVliUsbhubHeader *hdr, guint idt, GString *str)
 {
 	fu_common_string_append_kx (str, idt, "DevId", GUINT16_FROM_BE(hdr->dev_id));
+	fu_common_string_append_kx (str, idt, "Variant", hdr->variant);
+	if (hdr->usb2_fw_sz > 0) {
+		fu_common_string_append_kx (str, idt, "Usb2FwAddr",
+					    GUINT16_FROM_BE(hdr->usb2_fw_addr));
+		fu_common_string_append_kx (str, idt, "Usb2FwSz",
+					    GUINT16_FROM_BE(hdr->usb2_fw_sz));
+	}
+	fu_common_string_append_kx (str, idt, "Usb3FwAddr",
+				    GUINT16_FROM_BE(hdr->usb3_fw_addr));
+	fu_common_string_append_kx (str, idt, "Usb3FwSz",
+				    GUINT16_FROM_BE(hdr->usb3_fw_sz));
+	if (hdr->prev_ptr != VLI_USBHUB_FLASHMAP_IDX_INVALID) {
+		fu_common_string_append_kx (str, idt, "PrevPtr",
+					    VLI_USBHUB_FLASHMAP_IDX_TO_ADDR(hdr->prev_ptr));
+	}
+	if (hdr->next_ptr != VLI_USBHUB_FLASHMAP_IDX_INVALID) {
+		fu_common_string_append_kx (str, idt, "NextPtr",
+					    VLI_USBHUB_FLASHMAP_IDX_TO_ADDR(hdr->next_ptr));
+	}
+	fu_common_string_append_kb (str, idt, "ChecksumOK",
+				    hdr->checksum == fu_vli_usbhub_header_crc8 (hdr));
 }
