fwupd/fwupd

commit 2031ce3bf67885a4db247fa4088b36ae50011ca0

Ensure the device is open when doing the subclassed prepare() and cleanup()
It's not usual to open the device from inside the device, otherwise there is a
possibility of a deadlock. For devices without the ->prepare() virtual function
the ->open() should either be a NOP or very fast indeed.

src/fu-engine.c
@@ -1850,6 +1850,32 @@ fu_engine_get_device_by_id (FuEngine *self, const gchar *device_id, GError **err
 	return g_steal_pointer (&device2);
 }
 
+/* same as FuDevice->prepare, but with the device open */
+static gboolean
+fu_engine_device_prepare (FuEngine *self,
+			  FuDevice *device,
+			  FwupdInstallFlags flags,
+			  GError **error)
+{
+	g_autoptr(FuDeviceLocker) locker = fu_device_locker_new (device, error);
+	if (locker == NULL)
+		return FALSE;
+	return fu_device_prepare (device, flags, error);
+}
+
+/* same as FuDevice->cleanup, but with the device open */
+static gboolean
+fu_engine_device_cleanup (FuEngine *self,
+			  FuDevice *device,
+			  FwupdInstallFlags flags,
+			  GError **error)
+{
+	g_autoptr(FuDeviceLocker) locker = fu_device_locker_new (device, error);
+	if (locker == NULL)
+		return FALSE;
+	return fu_device_cleanup (device, flags, error);
+}
+
 static gboolean
 fu_engine_update_prepare (FuEngine *self,
 			  FwupdInstallFlags flags,
@@ -1866,7 +1892,7 @@ fu_engine_update_prepare (FuEngine *self,
 		return FALSE;
 	str = fu_device_to_string (device);
 	g_debug ("performing prepare on %s", str);
-	if (!fu_device_prepare (device, flags, error))
+	if (!fu_engine_device_prepare (self, device, flags, error))
 		return FALSE;
 	for (guint j = 0; j < plugins->len; j++) {
 		FuPlugin *plugin_tmp = g_ptr_array_index (plugins, j);
@@ -1900,7 +1926,7 @@ fu_engine_update_cleanup (FuEngine *self,
 		return FALSE;
 	str = fu_device_to_string (device);
 	g_debug ("performing cleanup on %s", str);
-	if (!fu_device_cleanup (device, flags, error))
+	if (!fu_engine_device_cleanup (self, device, flags, error))
 		return FALSE;
 	for (guint j = 0; j < plugins->len; j++) {
 		FuPlugin *plugin_tmp = g_ptr_array_index (plugins, j);

commit 6a9f9c0454e0f832d9f32ca7d2393a88ebba4f28

Skip cleanup after device is done updating if `will-disappear` is set (Fixes: #1529)
This fixes a regression introduced by 2031ce3bf67885a4db247fa4088b36ae50011ca0
that leads to:
```
USB error on device 2dc8:5750 : No such device (it may have been disconnected) [-4]
```

plugins/ebitdo/fu-ebitdo-device.c
@@ -512,8 +512,14 @@ fu_ebitdo_device_write_firmware (FuDevice *device,
 	/* when doing a soft-reboot the device does not re-enumerate properly
 	 * so manually reboot the GUsbDevice */
 	fu_device_set_status (device, FWUPD_STATUS_DEVICE_RESTART);
-	if (!g_usb_device_reset (usb_device, error)) {
-		g_prefix_error (error, "failed to force-reset device: ");
+	if (!g_usb_device_reset (usb_device, &error_local)) {
+		g_prefix_error (&error_local, "failed to force-reset device: ");
+		if (fu_device_has_flag (device, FWUPD_DEVICE_FLAG_WILL_DISAPPEAR)) {
+			fu_device_set_remove_delay (device, 0);
+			g_debug ("%s", error_local->message);
+			return TRUE;
+		}
+		g_propagate_error (error, g_steal_pointer (&error_local));
 		return FALSE;
 	}
 

src/fu-engine.c
@@ -1872,7 +1872,14 @@ fu_engine_device_cleanup (FuEngine *self,
 			  FwupdInstallFlags flags,
 			  GError **error)
 {
-	g_autoptr(FuDeviceLocker) locker = fu_device_locker_new (device, error);
+	g_autoptr(FuDeviceLocker) locker = NULL;
+
+	if (fu_device_has_flag (device, FWUPD_DEVICE_FLAG_WILL_DISAPPEAR)) {
+		g_debug ("skipping device cleanup due to will-disappear flag");
+		return TRUE;
+	}
+
+	locker = fu_device_locker_new (device, error);
 	if (locker == NULL)
 		return FALSE;
 	return fu_device_cleanup (device, flags, error);