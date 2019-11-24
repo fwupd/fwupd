4cc6b9947219f41c11264b705387d87c8c37b431
Leo <thinkabit.ukim@gmail.com>
2019-11-24 21:39:56 +0100

    Fix shebangs on scripts

--- data/tests/builder/startup.sh
+++ data/tests/builder/startup.sh
@@ -1,2 +1,2 @@
-#/bin/sh
+#!/bin/sh
 cat source.bin | rev > firmware.bin
--- plugins/flashrom/example/build.sh
+++ plugins/flashrom/example/build.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#!/bin/sh
 appstream-util validate-relax com.Flashrom.Laptop.metainfo.xml
 tar -cf firmware.tar startup.sh random-tool
 gcab --create --nopath Flashrom-Laptop-1.2.3.cab firmware.tar com.Flashrom.Laptop.metainfo.xml
