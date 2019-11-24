11503c23ee7bb4735ca46a879a0607cb7809f6a5
Mario Limonciello <mario.limonciello@dell.com>
2019-11-24 14:02:29 -0600

    Install development files for libfwupdplugin
    This splits out all development files, including headers into their
    own packages where relevant.
    
    Notably absent is `fu-hash.h` which is used for determining taint.
    Out of tree developed plugins should still taint the daemon.

---
+++ libfwupdplugin/fu-deprecated.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2017 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+G_BEGIN_DECLS
+
+/* indeed, nothing */
+
+G_END_DECLS
