8b05d448df80831d0032128682f4597ab6b0573c
Richard Hughes <richard@hughsie.com>
2019-11-27 10:55:16 +0000

    trivial: Unexport fu_test_loop_run_with_timeout()

--- libfwupdplugin/fu-test.h
+++ libfwupdplugin/fu-test.h
@@ -10,8 +10,6 @@
 
 gchar		*fu_test_get_filename		(const gchar	*testdatadirs,
 						 const gchar	*filename);
-void		 fu_test_loop_run_with_timeout	(guint		 timeout_ms);
-void		 fu_test_loop_quit		(void);
 gboolean	 fu_test_compare_lines		(const gchar	*txt1,
 						 const gchar	*txt2,
 						 GError		**error);
--- src/fu-self-test.c
+++ src/fu-self-test.c
@@ -34,6 +34,41 @@
 #include "fu-keyring-pkcs7.h"
 #endif
 
+static GMainLoop *_test_loop = NULL;
+static guint _test_loop_timeout_id = 0;
+
+static gboolean
+fu_test_hang_check_cb (gpointer user_data)
+{
+	g_main_loop_quit (_test_loop);
+	_test_loop_timeout_id = 0;
+	return G_SOURCE_REMOVE;
+}
+
+static void
+fu_test_loop_run_with_timeout (guint timeout_ms)
+{
+	g_assert (_test_loop_timeout_id == 0);
+	g_assert (_test_loop == NULL);
+	_test_loop = g_main_loop_new (NULL, FALSE);
+	_test_loop_timeout_id = g_timeout_add (timeout_ms, fu_test_hang_check_cb, NULL);
+	g_main_loop_run (_test_loop);
+}
+
+static void
+fu_test_loop_quit (void)
+{
+	if (_test_loop_timeout_id > 0) {
+		g_source_remove (_test_loop_timeout_id);
+		_test_loop_timeout_id = 0;
+	}
+	if (_test_loop != NULL) {
+		g_main_loop_quit (_test_loop);
+		g_main_loop_unref (_test_loop);
+		_test_loop = NULL;
+	}
+}
+
 static void
 fu_self_test_mkroot (void)
 {
