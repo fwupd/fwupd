fwupd/fwupd

commit e076d48afb74d1447936d353f12781755f13b047

trivial: Fix some invalid error handling in fwupdmgr
If somehow we forget to add a case section, return with an error as the code
suggests, rather than always returning TRUE. Found with Coverity.

src/fu-util.c
@@ -286,15 +286,15 @@ fu_util_maybe_enable_automatic (FuUtilPrivate *priv, GPtrArray *remotes, GError 
 							 NULL, error))
 				return FALSE;
 		}
-	/* fallthrough */
-	default:
 		return TRUE;
+	default:
+		break;
 	}
 
 	g_set_error_literal (error,
-				FWUPD_ERROR,
-				FWUPD_ERROR_NOTHING_TO_DO,
-				"no option selected");
+			     FWUPD_ERROR,
+			     FWUPD_ERROR_NOTHING_TO_DO,
+			     "invalid option selected");
 	return FALSE;
 }
 

commit d803f11f70ed7eafac88626ebb1be2112e48ea23

fu-util: fix a logic error in report uploading introduced in e076d48afb74d1447936d353f12781755f13b047
Fixes: #1770

src/fu-util.c
@@ -286,16 +286,12 @@ fu_util_maybe_enable_automatic (FuUtilPrivate *priv, GPtrArray *remotes, GError 
 							 NULL, error))
 				return FALSE;
 		}
-		return TRUE;
+		break;
 	default:
 		break;
 	}
 
-	g_set_error_literal (error,
-			     FWUPD_ERROR,
-			     FWUPD_ERROR_NOTHING_TO_DO,
-			     "invalid option selected");
-	return FALSE;
+	return TRUE;
 }
 
 static gboolean