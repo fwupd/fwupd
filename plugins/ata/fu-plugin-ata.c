dbcc8e11377142d804d29b97dfea7e61ca5597fb
Richard Hughes <richard@hughsie.com>
2020-02-13 10:27:13 +0000

    ata: Include a vendor ID for ATA hardware
    Some vendors want to ship updates for ATA hardware, but there are currently no
    lock-down restrictions in place for these kind of devices.
    
    There is the OUI from the WWN block which is supposed to identify the vendor,
    but this is not always set and so we have to be a little creative. We can match
    90% of hardware using the vendor name prefix, and the last 10% can be detected
    with a heuristic that was the result of comparing over 900 drive models.
    
    I'm not including very old drive models, media converters, raid controllers,
    or external 'portable' drives as I don't think it is useful. Also, if the drive
    contains a Dell vendor block just hardcode this as Dell rather than trying to
    be clever.
    
    Also ask the user to contribute OUI values if this data is found with no quirk
    data as this is the only real sane way to manage this data long term.
    The list of OUIs can be found here: http://standards-oui.ieee.org/oui.txt

--- plugins/ata/fu-plugin-ata.c
+++ plugins/ata/fu-plugin-ata.c
@@ -18,3 +18,11 @@ fu_plugin_init (FuPlugin *plugin)
 	fu_plugin_add_udev_subsystem (plugin, "block");
 	fu_plugin_set_device_gtype (plugin, FU_TYPE_ATA_DEVICE);
 }
+
+gboolean
+fu_plugin_device_created (FuPlugin *plugin, FuDevice *dev, GError **error)
+{
+	gboolean tmp = fu_plugin_get_config_value_boolean (plugin, "UnknownOuiReport");
+	fu_ata_device_set_unknown_oui_report (FU_ATA_DEVICE (dev), tmp);
+	return TRUE;
+}
--- plugins/ata/fu-self-test.c
+++ plugins/ata/fu-self-test.c
@@ -9,6 +9,7 @@
 #include <fwupd.h>
 
 #include "fu-ata-device.h"
+#include "fu-device-private.h"
 
 static void
 fu_ata_id_func (void)
@@ -34,6 +35,36 @@ fu_ata_id_func (void)
 	g_assert_cmpstr (fu_device_get_version (FU_DEVICE (dev)), ==, "SBFM61.2");
 }
 
+static void
+fu_ata_oui_func (void)
+{
+	gboolean ret;
+	gsize sz;
+	g_autofree gchar *data = NULL;
+	g_autofree gchar *path = NULL;
+	g_autofree gchar *str = NULL;
+	g_autoptr(FuAtaDevice) dev = NULL;
+	g_autoptr(GError) error = NULL;
+
+	path = g_build_filename (TESTDATADIR, "Samsung SSD 860 EVO 500GB.bin", NULL);
+	ret = g_file_get_contents (path, &data, &sz, &error);
+	g_assert_no_error (error);
+	g_assert (ret);
+	dev = fu_ata_device_new_from_blob ((guint8 *)data, sz, &error);
+	g_assert_no_error (error);
+	g_assert_nonnull (dev);
+	fu_ata_device_set_unknown_oui_report (dev, FALSE);
+	fu_device_convert_instance_ids (FU_DEVICE (dev));
+	str = fu_device_to_string (FU_DEVICE (dev));
+	g_debug ("%s", str);
+	g_assert_true (fu_device_has_guid (FU_DEVICE (dev), "OUI\\002538"));
+	g_assert_cmpint (fu_ata_device_get_transfer_mode (dev), ==, 0xe);
+	g_assert_cmpint (fu_ata_device_get_transfer_blocks (dev), ==, 0x1);
+	g_assert_cmpstr (fu_device_get_serial (FU_DEVICE (dev)), ==, "S3Z1NB0K862928X");
+	g_assert_cmpstr (fu_device_get_name (FU_DEVICE (dev)), ==, "SSD 860 EVO 500GB");
+	g_assert_cmpstr (fu_device_get_version (FU_DEVICE (dev)), ==, "RVT01B6Q");
+}
+
 int
 main (int argc, char **argv)
 {
@@ -43,6 +74,7 @@ main (int argc, char **argv)
 	g_log_set_fatal_mask (NULL, G_LOG_LEVEL_ERROR | G_LOG_LEVEL_CRITICAL);
 
 	/* tests go here */
-	g_test_add_func ("/fwupd/id", fu_ata_id_func);
+	g_test_add_func ("/fwupd/ata/id", fu_ata_id_func);
+	g_test_add_func ("/fwupd/ata/oui", fu_ata_oui_func);
 	return g_test_run ();
 }
