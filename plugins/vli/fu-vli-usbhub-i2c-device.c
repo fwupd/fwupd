18be33e706b08d22ee6145428e7d076b932f175a
Richard Hughes <richard@hughsie.com>
2020-02-27 08:51:42 +0000

    vli: Set the MSP430 version format to pair
    Apparently the Windows tool ignores the first byte, so we should too.
    
    Cherry pick from 6dd54fa9c4a91299e317194a59f740938302cef7 but adjust
    since f50ff2c27e37a2914703de09a39e1fa5a8296a7b is not backported.

--- plugins/vli/fu-vli-usbhub-i2c-device.c
+++ plugins/vli/fu-vli-usbhub-i2c-device.c
@@ -66,7 +66,7 @@ fu_vli_usbhub_i2c_device_setup (FuDevice *device, GError **error)
 
 	/* set version */
 	version = g_strdup_printf ("%x.%x.%x", buf[0], buf[1], buf[2]);
-	fu_device_set_version (device, version, FWUPD_VERSION_FORMAT_TRIPLET);
+	fu_device_set_version (device, version, FWUPD_VERSION_FORMAT_PAIR);
 	return TRUE;
 }
 
