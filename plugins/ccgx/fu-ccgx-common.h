666229fbeafe0cb248dd6286cbe5b8ada6329ead
Richard Hughes <richard@hughsie.com>
2020-03-18 18:33:08 +0000

    ccgx: Parse the metadata block in the firmware image

---
+++ plugins/ccgx/fu-ccgx-common.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2020 Cypress Semiconductor Corporation.
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include <glib-object.h>
+
+/* metadata valid signature "CY" */
+#define CCGX_METADATA_VALID_SIG 0x4359
+
+typedef struct __attribute__((packed)) {
+	guint8	fw_checksum;		/* firmware checksum */
+	guint32	fw_entry;		/* firmware entry address */
+	guint16	last_boot_row;		/* last flash row of bootloader or previous firmware */
+	guint8	reserved1[2];		/* reserved */
+	guint32	fw_size;		/* firmware size */
+	guint8	reserved2[9];		/* reserved */
+	guint16	metadata_valid;		/* meta data valid "CY" */
+	guint8	reserved3[4];		/* reserved */
+	guint32	boot_seq;		/* boot sequence number */
+} CCGxMetaData;
