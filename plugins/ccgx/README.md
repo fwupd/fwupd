b258e514c5f054a449fd49032e791359c23cb707
Richard Hughes <richard@hughsie.com>
2020-03-18 15:54:28 +0000

    ccgx: Add a firmare parser for cyacd files
    These are visually similar to Intel hex files, but different enough to demand
    their own parser. Multiple images can be stored in one firmware file, with the
    `addr` set to the SiliconID and the `idx` set to the position in the file.

---
+++ plugins/ccgx/README.md
@@ -0,0 +1,8 @@
+Cypress support
+===============
+
+Firmware Format
+---------------
+The daemon will decompress the cabinet archive and extract several firmware
+blobs in cyacd file format. See https://community.cypress.com/docs/DOC-10562
+for more details.
---
+++ plugins/ccgx/fu-plugin-ccgx.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2020 Cypress Semiconductor Corporation.
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#include "config.h"
+
+#include "fu-hash.h"
+#include "fu-plugin-vfuncs.h"
+
+#include "fu-ccgx-cyacd-firmware.h"
+
+void
+fu_plugin_init (FuPlugin *plugin)
+{
+	fu_plugin_set_build_hash (plugin, FU_BUILD_HASH);
+	fu_plugin_add_firmware_gtype (plugin, "ccgx-cyacd", FU_TYPE_CCGX_CYACD_FIRMWARE);
+}
