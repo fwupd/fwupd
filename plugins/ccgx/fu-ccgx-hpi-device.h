6e64bc00df317743e0abb18016488cbce67e71d3
Richard Hughes <richard@hughsie.com>
2020-03-19 22:10:41 +0000

    ccgx: Create devices when in HID and I2C modes

---
+++ plugins/ccgx/fu-ccgx-hpi-device.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2020 Cypress Semiconductor Corporation.
+ * Copyright (C) 2020 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include "fu-usb-device.h"
+
+#define FU_TYPE_CCGX_HPI_DEVICE (fu_ccgx_hpi_device_get_type ())
+G_DECLARE_FINAL_TYPE (FuCcgxHpiDevice, fu_ccgx_hpi_device, FU, CCGX_HPI_DEVICE, FuUsbDevice)
