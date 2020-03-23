fwupd/fwupd

commit c04f5a3e151cac589d48f7abee6faed4e4478d85

Add FuHidDevice abstraction
Quite a few plugins use HID commands to communicate with the hardware. At the
mement we have ~6 implementations of SET_REPORT and are soon to add one more.

Move this into common code.

plugins/csr/fu-csr-device.c
@@ -32,13 +32,13 @@ typedef enum {
 
 struct _FuCsrDevice
 {
-	FuUsbDevice		 parent_instance;
+	FuHidDevice		 parent_instance;
 	FuCsrDeviceQuirks	 quirks;
 	DfuState		 dfu_state;
 	guint32			 dnload_timeout;
 };
 
-G_DEFINE_TYPE (FuCsrDevice, fu_csr_device, FU_TYPE_USB_DEVICE)
+G_DEFINE_TYPE (FuCsrDevice, fu_csr_device, FU_TYPE_HID_DEVICE)
 
 #define FU_CSR_REPORT_ID_COMMAND		0x01
 #define FU_CSR_REPORT_ID_STATUS			0x02
@@ -69,73 +69,36 @@ fu_csr_device_to_string (FuDevice *device, guint idt, GString *str)
 static gboolean
 fu_csr_device_attach (FuDevice *device, GError **error)
 {
-	FuCsrDevice *self = FU_CSR_DEVICE (device);
-	GUsbDevice *usb_device = fu_usb_device_get_dev (FU_USB_DEVICE (self));
-	gsize sz = 0;
 	guint8 buf[] = { FU_CSR_REPORT_ID_CONTROL, FU_CSR_CONTROL_RESET };
-
-	if (g_getenv ("FWUPD_CSR_VERBOSE") != NULL)
-		fu_common_dump_raw (G_LOG_DOMAIN, "Reset", buf, sz);
-	if (!g_usb_device_control_transfer (usb_device,
-					    G_USB_DEVICE_DIRECTION_HOST_TO_DEVICE,
-					    G_USB_DEVICE_REQUEST_TYPE_CLASS,
-					    G_USB_DEVICE_RECIPIENT_INTERFACE,
-					    FU_HID_REPORT_SET,				/* bRequest */
-					    FU_HID_FEATURE | FU_CSR_REPORT_ID_CONTROL,	/* wValue */
-					    0x0000,					/* wIndex */
-					    buf, sizeof(buf), &sz,
-					    FU_CSR_DEVICE_TIMEOUT, /* timeout */
-					    NULL, error)) {
-		g_prefix_error (error, "Failed to ClearStatus: ");
+	if (!fu_hid_device_set_report (FU_HID_DEVICE (device),
+				       FU_CSR_REPORT_ID_CONTROL,
+				       buf, sizeof(buf),
+				       FU_CSR_DEVICE_TIMEOUT,
+				       FU_HID_DEVICE_FLAG_IS_FEATURE,
+				       error)) {
+		g_prefix_error (error, "failed to attach: ");
 		return FALSE;
 	}
-
-	/* check packet */
-	if (sz != FU_CSR_CONTROL_HEADER_SIZE) {
-		g_set_error (error,
-			     FWUPD_ERROR,
-			     FWUPD_ERROR_INTERNAL,
-			     "Reset packet was %" G_GSIZE_FORMAT " expected %i",
-			     sz, FU_CSR_CONTROL_HEADER_SIZE);
-		return FALSE;
-	}
-
 	return TRUE;
 }
 
 static gboolean
 fu_csr_device_get_status (FuCsrDevice *self, GError **error)
 {
-	GUsbDevice *usb_device = fu_usb_device_get_dev (FU_USB_DEVICE (self));
-	gsize sz = 0;
 	guint8 buf[64] = {0};
 
 	/* hit hardware */
-	if (!g_usb_device_control_transfer (usb_device,
-					    G_USB_DEVICE_DIRECTION_DEVICE_TO_HOST,
-					    G_USB_DEVICE_REQUEST_TYPE_CLASS,
-					    G_USB_DEVICE_RECIPIENT_INTERFACE,
-					    FU_HID_REPORT_GET,				/* bRequest */
-					    FU_HID_FEATURE | FU_CSR_REPORT_ID_STATUS,	/* wValue */
-					    0x0000,					/* wIndex */
-					    buf, sizeof(buf), &sz,
-					    FU_CSR_DEVICE_TIMEOUT,
-					    NULL, error)) {
-		g_prefix_error (error, "Failed to GetStatus: ");
+	if (!fu_hid_device_get_report (FU_HID_DEVICE (self),
+				       FU_CSR_REPORT_ID_STATUS,
+				       buf, sizeof(buf),
+				       FU_CSR_DEVICE_TIMEOUT,
+				       FU_HID_DEVICE_FLAG_IS_FEATURE,
+				       error)) {
+		g_prefix_error (error, "failed to GetStatus: ");
 		return FALSE;
 	}
-	if (g_getenv ("FWUPD_CSR_VERBOSE") != NULL)
-		fu_common_dump_raw (G_LOG_DOMAIN, "GetStatus", buf, sz);
 
 	/* check packet */
-	if (sz != FU_CSR_STATUS_HEADER_SIZE) {
-		g_set_error (error,
-			     FWUPD_ERROR,
-			     FWUPD_ERROR_INTERNAL,
-			     "GetStatus packet was %" G_GSIZE_FORMAT " expected %i",
-			     sz, FU_CSR_STATUS_HEADER_SIZE);
-		return FALSE;
-	}
 	if (buf[0] != FU_CSR_REPORT_ID_STATUS) {
 		g_set_error (error,
 			     FWUPD_ERROR,
@@ -157,8 +120,6 @@ fu_csr_device_get_status (FuCsrDevice *self, GError **error)
 static gboolean
 fu_csr_device_clear_status (FuCsrDevice *self, GError **error)
 {
-	GUsbDevice *usb_device = fu_usb_device_get_dev (FU_USB_DEVICE (self));
-	gsize sz = 0;
 	guint8 buf[] = { FU_CSR_REPORT_ID_CONTROL,
 			 FU_CSR_CONTROL_CLEAR_STATUS };
 
@@ -169,29 +130,13 @@ fu_csr_device_clear_status (FuCsrDevice *self, GError **error)
 		return TRUE;
 
 	/* hit hardware */
-	if (g_getenv ("FWUPD_CSR_VERBOSE") != NULL)
-		fu_common_dump_raw (G_LOG_DOMAIN, "ClearStatus", buf, sz);
-	if (!g_usb_device_control_transfer (usb_device,
-					    G_USB_DEVICE_DIRECTION_HOST_TO_DEVICE,
-					    G_USB_DEVICE_REQUEST_TYPE_CLASS,
-					    G_USB_DEVICE_RECIPIENT_INTERFACE,
-					    FU_HID_REPORT_SET,				/* bRequest */
-					    FU_HID_FEATURE | FU_CSR_REPORT_ID_CONTROL,	/* wValue */
-					    0x0000,					/* wIndex */
-					    buf, sizeof(buf), &sz,
-					    FU_CSR_DEVICE_TIMEOUT,
-					    NULL, error)) {
-		g_prefix_error (error, "Failed to ClearStatus: ");
-		return FALSE;
-	}
-
-	/* check packet */
-	if (sz != FU_CSR_CONTROL_HEADER_SIZE) {
-		g_set_error (error,
-			     FWUPD_ERROR,
-			     FWUPD_ERROR_INTERNAL,
-			     "ClearStatus packet was %" G_GSIZE_FORMAT " expected %i",
-			     sz, FU_CSR_CONTROL_HEADER_SIZE);
+	if (!fu_hid_device_set_report (FU_HID_DEVICE (self),
+				       FU_CSR_REPORT_ID_CONTROL,
+				       buf, sizeof(buf),
+				       FU_CSR_DEVICE_TIMEOUT,
+				       FU_HID_DEVICE_FLAG_IS_FEATURE,
+				       error)) {
+		g_prefix_error (error, "failed to ClearStatus: ");
 		return FALSE;
 	}
 
@@ -202,35 +147,17 @@ fu_csr_device_clear_status (FuCsrDevice *self, GError **error)
 static GBytes *
 fu_csr_device_upload_chunk (FuCsrDevice *self, GError **error)
 {
-	GUsbDevice *usb_device = fu_usb_device_get_dev (FU_USB_DEVICE (self));
-	gsize sz = 0;
 	guint16 data_sz;
 	guint8 buf[64] = {0};
 
 	/* hit hardware */
-	if (!g_usb_device_control_transfer (usb_device,
-					    G_USB_DEVICE_DIRECTION_DEVICE_TO_HOST,
-					    G_USB_DEVICE_REQUEST_TYPE_CLASS,
-					    G_USB_DEVICE_RECIPIENT_INTERFACE,
-					    FU_HID_REPORT_GET,				/* bRequest */
-					    FU_HID_FEATURE | FU_CSR_REPORT_ID_COMMAND,	/* wValue */
-					    0x0000,					/* wIndex */
-					    buf, sizeof(buf), &sz,
-					    FU_CSR_DEVICE_TIMEOUT,
-					    NULL, error)) {
-		g_prefix_error (error, "Failed to ReadFirmware: ");
-		return NULL;
-	}
-	if (g_getenv ("FWUPD_CSR_VERBOSE") != NULL)
-		fu_common_dump_raw (G_LOG_DOMAIN, "ReadFirmware", buf, sz);
-
-	/* too small to parse */
-	if (sz < FU_CSR_COMMAND_HEADER_SIZE) {
-		g_set_error (error,
-			     FWUPD_ERROR,
-			     FWUPD_ERROR_INTERNAL,
-			     "ReadFirmware packet too small, got %" G_GSIZE_FORMAT,
-			     sz);
+	if (!fu_hid_device_get_report (FU_HID_DEVICE (self),
+				       FU_CSR_REPORT_ID_COMMAND,
+				       buf, sizeof(buf),
+				       FU_CSR_DEVICE_TIMEOUT,
+				       FU_HID_DEVICE_FLAG_IS_FEATURE,
+				       error)) {
+		g_prefix_error (error, "failed to ReadFirmware: ");
 		return NULL;
 	}
 
@@ -245,7 +172,7 @@ fu_csr_device_upload_chunk (FuCsrDevice *self, GError **error)
 
 	/* check the length */
 	data_sz = fu_common_read_uint16 (&buf[1], G_LITTLE_ENDIAN);
-	if (data_sz + FU_CSR_COMMAND_HEADER_SIZE != (guint16) sz) {
+	if (data_sz + FU_CSR_COMMAND_HEADER_SIZE != (guint16) sizeof(buf)) {
 		g_set_error (error,
 			     FWUPD_ERROR,
 			     FWUPD_ERROR_INTERNAL,
@@ -256,7 +183,7 @@ fu_csr_device_upload_chunk (FuCsrDevice *self, GError **error)
 
 	/* return as bytes */
 	return g_bytes_new (buf + FU_CSR_COMMAND_HEADER_SIZE,
-			    sz - FU_CSR_COMMAND_HEADER_SIZE);
+			    sizeof(buf) - FU_CSR_COMMAND_HEADER_SIZE);
 }
 
 static FuFirmware *
@@ -331,10 +258,8 @@ fu_csr_device_upload (FuDevice *device, GError **error)
 static gboolean
 fu_csr_device_download_chunk (FuCsrDevice *self, guint16 idx, GBytes *chunk, GError **error)
 {
-	GUsbDevice *usb_device = fu_usb_device_get_dev (FU_USB_DEVICE (self));
 	const guint8 *chunk_data;
 	gsize chunk_sz = 0;
-	gsize write_sz = 0;
 	guint8 buf[FU_CSR_PACKET_DATA_SIZE] = {0};
 
 	/* too large? */
@@ -359,32 +284,13 @@ fu_csr_device_download_chunk (FuCsrDevice *self, guint16 idx, GBytes *chunk, GEr
 		return FALSE;
 
 	/* hit hardware */
-	if (g_getenv ("FWUPD_CSR_VERBOSE") != NULL)
-		fu_common_dump_raw (G_LOG_DOMAIN, "Upgrade", buf, sizeof(buf));
-	if (!g_usb_device_control_transfer (usb_device,
-					    G_USB_DEVICE_DIRECTION_HOST_TO_DEVICE,
-					    G_USB_DEVICE_REQUEST_TYPE_CLASS,
-					    G_USB_DEVICE_RECIPIENT_INTERFACE,
-					    FU_HID_REPORT_SET,				/* bRequest */
-					    FU_HID_FEATURE | FU_CSR_REPORT_ID_COMMAND,	/* wValue */
-					    0x0000,					/* wIndex */
-					    buf,
-					    sizeof(buf),
-					    &write_sz,
-					    FU_CSR_DEVICE_TIMEOUT,
-					    NULL, error)) {
-		g_prefix_error (error, "Failed to Upgrade: ");
-		return FALSE;
-	}
-
-	/* check packet */
-	if (write_sz != sizeof(buf)) {
-		g_set_error (error,
-			     FWUPD_ERROR,
-			     FWUPD_ERROR_INTERNAL,
-			     "Not all packet written for upgrade got "
-			     "%" G_GSIZE_FORMAT " expected %" G_GSIZE_FORMAT,
-			     write_sz, sizeof(buf));
+	if (!fu_hid_device_set_report (FU_HID_DEVICE (self),
+				       FU_CSR_REPORT_ID_COMMAND,
+				       buf, sizeof(buf),
+				       FU_CSR_DEVICE_TIMEOUT,
+				       FU_HID_DEVICE_FLAG_IS_FEATURE,
+				       error)) {
+		g_prefix_error (error, "failed to Upgrade: ");
 		return FALSE;
 	}
 
@@ -502,46 +408,12 @@ fu_csr_device_probe (FuUsbDevice *device, GError **error)
 }
 
 static gboolean
-fu_csr_device_open (FuUsbDevice *device, GError **error)
-{
-	GUsbDevice *usb_device = fu_usb_device_get_dev (device);
-
-	/* open device and clear status */
-	if (!g_usb_device_claim_interface (usb_device, 0x00, /* HID */
-					   G_USB_DEVICE_CLAIM_INTERFACE_BIND_KERNEL_DRIVER,
-					   error)) {
-		g_prefix_error (error, "failed to claim HID interface: ");
-		return FALSE;
-	}
-
-	/* success */
-	return TRUE;
-}
-
-static gboolean
 fu_csr_device_setup (FuDevice *device, GError **error)
 {
 	FuCsrDevice *self = FU_CSR_DEVICE (device);
 
 	if (!fu_csr_device_clear_status (self, error))
 		return FALSE;
-
-	/* success */
-	return TRUE;
-}
-
-static gboolean
-fu_csr_device_close (FuUsbDevice *device, GError **error)
-{
-	GUsbDevice *usb_device = fu_usb_device_get_dev (device);
-
-	/* we're done here */
-	if (!g_usb_device_release_interface (usb_device, 0x00, /* HID */
-					     G_USB_DEVICE_CLAIM_INTERFACE_BIND_KERNEL_DRIVER,
-					     error)) {
-		g_prefix_error (error, "failed to release interface: ");
-		return FALSE;
-	}
 
 	/* success */
 	return TRUE;
@@ -564,7 +436,5 @@ fu_csr_device_class_init (FuCsrDeviceClass *klass)
 	klass_device->prepare_firmware = fu_csr_device_prepare_firmware;
 	klass_device->attach = fu_csr_device_attach;
 	klass_device->setup = fu_csr_device_setup;
-	klass_usb_device->open = fu_csr_device_open;
-	klass_usb_device->close = fu_csr_device_close;
 	klass_usb_device->probe = fu_csr_device_probe;
 }

libfwupdplugin/fu-hid-device.c
@@ -1,0 +1,327 @@
+/*
+ * Copyright (C) 2017-2020 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#define G_LOG_DOMAIN				"FuHidDevice"
+
+#include "config.h"
+
+#include "fu-hid-device.h"
+
+#define FU_HID_REPORT_GET				0x01
+#define FU_HID_REPORT_SET				0x09
+
+#define FU_HID_REPORT_TYPE_INPUT			0x01
+#define FU_HID_REPORT_TYPE_OUTPUT			0x02
+#define FU_HID_REPORT_TYPE_FEATURE			0x03
+
+/**
+ * SECTION:fu-hid-device
+ * @short_description: a HID device
+ *
+ * An object that represents a HID device.
+ *
+ * See also: #FuDevice
+ */
+
+typedef struct
+{
+	FuUsbDevice		*usb_device;
+	guint8			 interface;
+} FuHidDevicePrivate;
+
+G_DEFINE_TYPE_WITH_PRIVATE (FuHidDevice, fu_hid_device, FU_TYPE_USB_DEVICE)
+
+enum {
+	PROP_0,
+	PROP_INTERFACE,
+	PROP_LAST
+};
+
+#define GET_PRIVATE(o) (fu_hid_device_get_instance_private (o))
+
+static void
+fu_hid_device_get_property (GObject *object, guint prop_id,
+			    GValue *value, GParamSpec *pspec)
+{
+	FuHidDevice *device = FU_HID_DEVICE (object);
+	FuHidDevicePrivate *priv = GET_PRIVATE (device);
+	switch (prop_id) {
+	case PROP_INTERFACE:
+		g_value_set_uint (value, priv->interface);
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+		break;
+	}
+}
+
+static void
+fu_hid_device_set_property (GObject *object, guint prop_id,
+			    const GValue *value, GParamSpec *pspec)
+{
+	FuHidDevice *device = FU_HID_DEVICE (object);
+	switch (prop_id) {
+	case PROP_INTERFACE:
+		fu_hid_device_set_interface (device, g_value_get_uint (value));
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+		break;
+	}
+}
+
+static gboolean
+fu_hid_device_open (FuUsbDevice *device, GError **error)
+{
+	FuHidDevice *self = FU_HID_DEVICE (device);
+	FuHidDeviceClass *klass = FU_HID_DEVICE_GET_CLASS (device);
+	FuHidDevicePrivate *priv = GET_PRIVATE (self);
+	GUsbDevice *usb_device = fu_usb_device_get_dev (device);
+
+	/* claim */
+	if (!g_usb_device_claim_interface (usb_device, priv->interface,
+					   G_USB_DEVICE_CLAIM_INTERFACE_BIND_KERNEL_DRIVER,
+					   error)) {
+		g_prefix_error (error, "failed to claim HID interface: ");
+		return FALSE;
+	}
+
+	/* subclassed */
+	if (klass->open != NULL) {
+		if (!klass->open (self, error))
+			return FALSE;
+	}
+
+	/* success */
+	return TRUE;
+}
+
+static gboolean
+fu_hid_device_close (FuUsbDevice *device, GError **error)
+{
+	FuHidDevice *self = FU_HID_DEVICE (device);
+	FuHidDeviceClass *klass = FU_HID_DEVICE_GET_CLASS (device);
+	FuHidDevicePrivate *priv = GET_PRIVATE (self);
+	GUsbDevice *usb_device = fu_usb_device_get_dev (device);
+
+	/* subclassed */
+	if (klass->close != NULL) {
+		if (!klass->close (self, error))
+			return FALSE;
+	}
+
+	/* release */
+	if (!g_usb_device_release_interface (usb_device, priv->interface,
+					     G_USB_DEVICE_CLAIM_INTERFACE_BIND_KERNEL_DRIVER,
+					     error)) {
+		g_prefix_error (error, "failed to release HID interface: ");
+		return FALSE;
+	}
+
+	/* success */
+	return TRUE;
+}
+
+/**
+ * fu_hid_device_set_interface:
+ * @self: A #FuHidDevice
+ * @interface: An interface number, e.g. 0x03
+ *
+ * Sets the HID USB interface number.
+ *
+ * Since: 1.4.0
+ **/
+void
+fu_hid_device_set_interface (FuHidDevice *self, guint8 interface)
+{
+	FuHidDevicePrivate *priv = GET_PRIVATE (self);
+	g_return_if_fail (FU_HID_DEVICE (self));
+	priv->interface = interface;
+}
+
+/**
+ * fu_hid_device_get_interface:
+ * @self: A #FuHidDevice
+ *
+ * Gets the HID USB interface number.
+ *
+ * Returns: integer
+ *
+ * Since: 1.4.0
+ **/
+guint8
+fu_hid_device_get_interface (FuHidDevice *self)
+{
+	FuHidDevicePrivate *priv = GET_PRIVATE (self);
+	g_return_val_if_fail (FU_HID_DEVICE (self), 0xff);
+	return priv->interface;
+}
+
+/**
+ * fu_hid_device_set_report:
+ * @self: A #FuHidDevice
+ * @value: low byte of wValue
+ * @buf: (nullable): a mutable buffer of data to send
+ * @bufsz: Size of @buf
+ * @timeout: timeout in ms
+ * @flags: #FuHidDeviceFlags e.g. %FU_HID_DEVICE_FLAG_ALLOW_TRUNC
+ * @error: a #GError or %NULL
+ *
+ * Calls SetReport on the hardware.
+ *
+ * Returns: %TRUE for success
+ *
+ * Since: 1.4.0
+ **/
+gboolean
+fu_hid_device_set_report (FuHidDevice *self,
+			  guint8 value,
+			  guint8 *buf,
+			  gsize bufsz,
+			  guint timeout,
+			  FuHidDeviceFlags flags,
+			  GError **error)
+{
+	FuHidDevicePrivate *priv = GET_PRIVATE (self);
+	GUsbDevice *usb_device = fu_usb_device_get_dev (FU_USB_DEVICE (self));
+	gsize actual_len = 0;
+	guint16 wvalue = (FU_HID_REPORT_TYPE_OUTPUT << 8) | value;
+
+	/* special case */
+	if (flags & FU_HID_DEVICE_FLAG_IS_FEATURE)
+		wvalue = (FU_HID_REPORT_TYPE_FEATURE << 8) | value;
+
+	g_return_val_if_fail (FU_HID_DEVICE (self), FALSE);
+	g_return_val_if_fail (buf != NULL, FALSE);
+	g_return_val_if_fail (bufsz != 0, FALSE);
+
+	if (g_getenv ("FU_HID_DEVICE_VERBOSE") != NULL)
+		fu_common_dump_raw (G_LOG_DOMAIN, "HID::SetReport", buf, bufsz);
+	if (!g_usb_device_control_transfer (usb_device,
+					    G_USB_DEVICE_DIRECTION_HOST_TO_DEVICE,
+					    G_USB_DEVICE_REQUEST_TYPE_CLASS,
+					    G_USB_DEVICE_RECIPIENT_INTERFACE,
+					    FU_HID_REPORT_SET,
+					    wvalue, priv->interface,
+					    buf, bufsz,
+					    &actual_len,
+					    timeout,
+					    NULL, error)) {
+		g_prefix_error (error, "failed to SetReport: ");
+		return FALSE;
+	}
+	if ((flags & FU_HID_DEVICE_FLAG_ALLOW_TRUNC) == 0 && actual_len != bufsz) {
+		g_set_error (error, G_IO_ERROR, G_IO_ERROR_INVALID_DATA,
+			     "only wrote %" G_GSIZE_FORMAT "bytes", actual_len);
+		return FALSE;
+	}
+	return TRUE;
+}
+
+/**
+ * fu_hid_device_get_report:
+ * @self: A #FuHidDevice
+ * @value: low byte of wValue
+ * @buf: (nullable): a mutable buffer of data to send
+ * @bufsz: Size of @buf
+ * @timeout: timeout in ms
+ * @flags: #FuHidDeviceFlags e.g. %FU_HID_DEVICE_FLAG_ALLOW_TRUNC
+ * @error: a #GError or %NULL
+ *
+ * Calls GetReport on the hardware.
+ *
+ * Returns: %TRUE for success
+ *
+ * Since: 1.4.0
+ **/
+gboolean
+fu_hid_device_get_report (FuHidDevice *self,
+			  guint8 value,
+			  guint8 *buf,
+			  gsize bufsz,
+			  guint timeout,
+			  FuHidDeviceFlags flags,
+			  GError **error)
+{
+	FuHidDevicePrivate *priv = GET_PRIVATE (self);
+	GUsbDevice *usb_device = fu_usb_device_get_dev (FU_USB_DEVICE (self));
+	gsize actual_len = 0;
+	guint16 wvalue = (FU_HID_REPORT_TYPE_INPUT << 8) | value;
+
+	g_return_val_if_fail (FU_HID_DEVICE (self), FALSE);
+	g_return_val_if_fail (buf != NULL, FALSE);
+	g_return_val_if_fail (bufsz != 0, FALSE);
+
+	/* special case */
+	if (flags & FU_HID_DEVICE_FLAG_IS_FEATURE)
+		wvalue = (FU_HID_REPORT_TYPE_FEATURE << 8) | value;
+
+	if (g_getenv ("FU_HID_DEVICE_VERBOSE") != NULL)
+		fu_common_dump_raw (G_LOG_DOMAIN, "HID::GetReport", buf, actual_len);
+	if (!g_usb_device_control_transfer (usb_device,
+					    G_USB_DEVICE_DIRECTION_DEVICE_TO_HOST,
+					    G_USB_DEVICE_REQUEST_TYPE_CLASS,
+					    G_USB_DEVICE_RECIPIENT_INTERFACE,
+					    FU_HID_REPORT_GET,
+					    wvalue, priv->interface,
+					    buf, bufsz,
+					    &actual_len, /* actual length */
+					    timeout,
+					    NULL, error)) {
+		g_prefix_error (error, "failed to GetReport: ");
+		return FALSE;
+	}
+	if (g_getenv ("FU_HID_DEVICE_VERBOSE") != NULL)
+		fu_common_dump_raw (G_LOG_DOMAIN, "HID::GetReport", buf, actual_len);
+	if ((flags & FU_HID_DEVICE_FLAG_ALLOW_TRUNC) == 0 && actual_len != bufsz) {
+		g_set_error (error, G_IO_ERROR, G_IO_ERROR_INVALID_DATA,
+			     "only read %" G_GSIZE_FORMAT "bytes", actual_len);
+		return FALSE;
+	}
+	return TRUE;
+}
+
+static void
+fu_hid_device_init (FuHidDevice *device)
+{
+}
+
+/**
+ * fu_hid_device_new:
+ * @usb_device: A #GUsbDevice
+ *
+ * Creates a new #FuHidDevice.
+ *
+ * Returns: (transfer full): a #FuHidDevice
+ *
+ * Since: 1.4.0
+ **/
+FuHidDevice *
+fu_hid_device_new (GUsbDevice *usb_device)
+{
+	FuHidDevice *device = g_object_new (FU_TYPE_HID_DEVICE, NULL);
+	fu_usb_device_set_dev (FU_USB_DEVICE (device), usb_device);
+	return FU_HID_DEVICE (device);
+}
+
+static void
+fu_hid_device_class_init (FuHidDeviceClass *klass)
+{
+	FuUsbDeviceClass *klass_usb_device = FU_USB_DEVICE_CLASS (klass);
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+	GParamSpec *pspec;
+
+	object_class->get_property = fu_hid_device_get_property;
+	object_class->set_property = fu_hid_device_set_property;
+	klass_usb_device->open = fu_hid_device_open;
+	klass_usb_device->close = fu_hid_device_close;
+
+	pspec = g_param_spec_uint ("interface", NULL, NULL,
+				   0x00, 0xff, 0x00,
+				   G_PARAM_READWRITE |
+				   G_PARAM_STATIC_NAME);
+	g_object_class_install_property (object_class, PROP_INTERFACE, pspec);
+}

libfwupdplugin/fu-hid-device.h
@@ -1,0 +1,58 @@
+/*
+ * Copyright (C) 2017-2020 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include <glib-object.h>
+
+#include "fu-usb-device.h"
+
+#define FU_TYPE_HID_DEVICE (fu_hid_device_get_type ())
+G_DECLARE_DERIVABLE_TYPE (FuHidDevice, fu_hid_device, FU, HID_DEVICE, FuUsbDevice)
+
+struct _FuHidDeviceClass
+{
+	FuUsbDeviceClass parent_class;
+	gboolean	 (*open)		(FuHidDevice		*device,
+						 GError			**error);
+	gboolean	 (*close)		(FuHidDevice		*device,
+						 GError			**error);
+	gpointer	__reserved[29];
+};
+
+/**
+ * FuHidDeviceFlags:
+ * @FU_HID_DEVICE_FLAG_NONE:			No flags set
+ * @FU_HID_DEVICE_FLAG_ALLOW_TRUNC:		Allow truncated reads and writes
+ * @FU_HID_DEVICE_FLAG_IS_FEATURE:		Use %FU_HID_REPORT_TYPE_FEATURE for wValue
+ *
+ * Flags used when calling fu_hid_device_get_report() and fu_hid_device_set_report().
+ **/
+typedef enum {
+	FU_HID_DEVICE_FLAG_NONE			= 0,
+	FU_HID_DEVICE_FLAG_ALLOW_TRUNC		= 1 << 0,
+	FU_HID_DEVICE_FLAG_IS_FEATURE		= 1 << 1,
+	FU_HID_DEVICE_FLAG_LAST
+} FuHidDeviceFlags;
+
+FuHidDevice	*fu_hid_device_new			(GUsbDevice	*usb_device);
+void		 fu_hid_device_set_interface		(FuHidDevice	*self,
+							 guint8		 interface);
+guint8		 fu_hid_device_get_interface		(FuHidDevice	*self);
+gboolean	 fu_hid_device_set_report		(FuHidDevice	*self,
+							 guint8		 value,
+							 guint8		*buf,
+							 gsize		 bufsz,
+							 guint		 timeout,
+							 FuHidDeviceFlags flags,
+							 GError		**error);
+gboolean	 fu_hid_device_get_report		(FuHidDevice	*self,
+							 guint8		 value,
+							 guint8		*buf,
+							 gsize		 bufsz,
+							 guint		 timeout,
+							 FuHidDeviceFlags flags,
+							 GError		**error);

libfwupdplugin/fu-plugin.h
@@ -17,6 +17,7 @@
 #include "fu-quirks.h"
 #include "fu-hwids.h"
 #include "fu-usb-device.h"
+//#include "fu-hid-device.h"
 #ifdef HAVE_GUDEV
 #include "fu-udev-device.h"
 #endif

libfwupdplugin/fu-usb-device.h
@@ -15,16 +15,6 @@
 #define FU_TYPE_USB_DEVICE (fu_usb_device_get_type ())
 G_DECLARE_DERIVABLE_TYPE (FuUsbDevice, fu_usb_device, FU, USB_DEVICE, FuDevice)
 
-/* HID */
-#define FU_HID_REPORT_GET				0x01
-#define FU_HID_REPORT_SET				0x09
-
-#define FU_HID_REPORT_TYPE_INPUT			0x01
-#define FU_HID_REPORT_TYPE_OUTPUT			0x02
-#define FU_HID_REPORT_TYPE_FEATURE			0x03
-
-#define FU_HID_FEATURE					0x0300
-
 struct _FuUsbDeviceClass
 {
 	FuDeviceClass	parent_class;

libfwupdplugin/fwupdplugin.map
@@ -556,6 +556,12 @@ LIBFWUPDPLUGIN_1.4.0 {
     fu_efivar_secure_boot_enabled;
     fu_efivar_set_data;
     fu_efivar_supported;
+    fu_hid_device_get_interface;
+    fu_hid_device_get_report;
+    fu_hid_device_get_type;
+    fu_hid_device_new;
+    fu_hid_device_set_interface;
+    fu_hid_device_set_report;
     fu_plugin_get_config_value_boolean;
     fu_plugin_runner_device_created;
   local: *;

libfwupdplugin/meson.build
@@ -22,6 +22,7 @@ fwupdplugin_src = [
   'fu-efivar.c',
   'fu-udev-device.c',
   'fu-usb-device.c',
+  'fu-hid-device.c',
 ]
 
 fwupdplugin_headers = [
@@ -49,6 +50,7 @@ fwupdplugin_headers = [
   'fu-efivar.h',
   'fu-udev-device.h',
   'fu-usb-device.h',
+  'fu-hid-device.h',
 ]
 install_headers(
   'fwupdplugin.h',

plugins/csr/fu-csr-device.h
@@ -6,7 +6,8 @@
 
 #pragma once
 
+#include "fu-hid-device.h"
 #include "fu-plugin.h"
 
 #define FU_TYPE_CSR_DEVICE (fu_csr_device_get_type ())
-G_DECLARE_FINAL_TYPE (FuCsrDevice, fu_csr_device, FU, CSR_DEVICE, FuUsbDevice)
+G_DECLARE_FINAL_TYPE (FuCsrDevice, fu_csr_device, FU, CSR_DEVICE, FuHidDevice)

commit 59fc93d910e1a6f7a55dd940a06643742cfe35df

csr: Allow truncated HID reads
The device is allowed to return short buffers during read operation. This fixes
a regression introduced in c04f5a3.

Fixes https://github.com/fwupd/fwupd/issues/1893

plugins/csr/fu-csr-device.c
@@ -92,6 +92,7 @@ fu_csr_device_get_status (FuCsrDevice *self, GError **error)
 				       FU_CSR_REPORT_ID_STATUS,
 				       buf, sizeof(buf),
 				       FU_CSR_DEVICE_TIMEOUT,
+				       FU_HID_DEVICE_FLAG_ALLOW_TRUNC |
 				       FU_HID_DEVICE_FLAG_IS_FEATURE,
 				       error)) {
 		g_prefix_error (error, "failed to GetStatus: ");
@@ -155,6 +156,7 @@ fu_csr_device_upload_chunk (FuCsrDevice *self, GError **error)
 				       FU_CSR_REPORT_ID_COMMAND,
 				       buf, sizeof(buf),
 				       FU_CSR_DEVICE_TIMEOUT,
+				       FU_HID_DEVICE_FLAG_ALLOW_TRUNC |
 				       FU_HID_DEVICE_FLAG_IS_FEATURE,
 				       error)) {
 		g_prefix_error (error, "failed to ReadFirmware: ");