647803aed7a799097dfcd8174f2d40a930e69350
Mario Limonciello <mario.limonciello@dell.com>
2020-03-27 15:22:37 -0500

    Modernize the thunderbolt plugin.
    Remove it's references to it's own GUdevclient and instead use
    FuUdevDevice.
    
    Some intentional casualties of the move:
    * Plugin metadata around native and safe mode dropped.
      - These haven't been useful in debugging anything and aren't relevant
        on new hardware
    * Extra GUID for 2 host controllers in same system dropped
      - Although this was normally static information BIOS operations like
        turning off PCI-E SD card reader or LAN controller changed things.
    * The NVM version is parsed directly instead of through gudev to prevent
      cached data breaking change events.
    
    Remaining TODO:
    * Force power w/ thunderbolt-power doesn't work

---
+++ plugins/thunderbolt/fu-thunderbolt-device.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2018 Richard Hughes <richard@hughsie.com>
+ * Copyright (C) 2020 Mario Limonciello <mario.limonciello@dell.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include "fu-plugin.h"
+
+#define FU_TYPE_THUNDERBOLT_DEVICE (fu_thunderbolt_device_get_type ())
+G_DECLARE_FINAL_TYPE (FuThunderboltDevice, fu_thunderbolt_device, FU, THUNDERBOLT_DEVICE, FuUdevDevice)
