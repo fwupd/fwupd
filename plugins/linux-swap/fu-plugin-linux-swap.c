d744fe8b67301be96504799f6b9864f52e4da7c2
Richard Hughes <richard@hughsie.com>
2020-05-07 12:17:33 +0100

    linux-swap: Rename the plugin as it is Linux specific
    We'll be adding other linux-foo plugins in the future, so it makes sense to get
    the namespace correct now.

--- plugins/linux-swap/fu-plugin-linux-swap.c
+++ plugins/linux-swap/fu-plugin-linux-swap.c
@@ -8,7 +8,7 @@
 
 #include "fu-plugin-vfuncs.h"
 #include "fu-hash.h"
-#include "fu-swap.h"
+#include "fu-linux-swap.h"
 
 struct FuPluginData {
 	GFileMonitor		*monitor;
@@ -30,11 +30,11 @@ fu_plugin_destroy (FuPlugin *plugin)
 }
 
 static void
-fu_plugin_swap_changed_cb (GFileMonitor *monitor,
-			   GFile *file,
-			   GFile *other_file,
-			   GFileMonitorEvent event_type,
-			   gpointer user_data)
+fu_plugin_linux_swap_changed_cb (GFileMonitor *monitor,
+				 GFile *file,
+				 GFile *other_file,
+				 GFileMonitorEvent event_type,
+				 gpointer user_data)
 {
 	g_debug ("swap changed");
 }
@@ -47,7 +47,7 @@ fu_plugin_startup (FuPlugin *plugin, GError **error)
 	g_autofree gchar *buf = NULL;
 	g_autofree gchar *fn = NULL;
 	g_autofree gchar *procfs = NULL;
-	g_autoptr(FuSwap) swap = NULL;
+	g_autoptr(FuLinuxSwap) swap = NULL;
 	g_autoptr(GFile) file = NULL;
 
 	procfs = fu_common_get_path (FU_PATH_KIND_PROCFS);
@@ -57,20 +57,20 @@ fu_plugin_startup (FuPlugin *plugin, GError **error)
 	if (data->monitor == NULL)
 		return FALSE;
 	g_signal_connect (data->monitor, "changed",
-			  G_CALLBACK (fu_plugin_swap_changed_cb), plugin);
+			  G_CALLBACK (fu_plugin_linux_swap_changed_cb), plugin);
 
-	/* load list of swaps */
+	/* load list of linux_swaps */
 	if (!g_file_get_contents (fn, &buf, &bufsz, error)) {
 		g_prefix_error (error, "could not open %s: ", fn);
 		return FALSE;
 	}
-	swap = fu_swap_new (buf, bufsz, error);
+	swap = fu_linux_swap_new (buf, bufsz, error);
 	if (swap == NULL) {
 		g_prefix_error (error, "could not parse %s: ", fn);
 		return FALSE;
 	}
 	g_debug ("swap %s and %s",
-		 fu_swap_get_enabled (swap) ? "enabled" : "disabled",
-		 fu_swap_get_encrypted (swap) ? "encrypted" : "unencrypted");
+		 fu_linux_swap_get_enabled (swap) ? "enabled" : "disabled",
+		 fu_linux_swap_get_encrypted (swap) ? "encrypted" : "unencrypted");
 	return TRUE;
 }
---
+++ plugins/linux-swap/fu-self-test.c
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2020 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#include "config.h"
+
+#include <fwupd.h>
+
+#include "fu-linux-swap.h"
+
+#include "fwupd-error.h"
+
+static void
+fu_linux_swap_none_func (void)
+{
+	g_autoptr(FuLinuxSwap) swap = NULL;
+	g_autoptr(GError) error = NULL;
+
+	swap = fu_linux_swap_new ("Filename\t\t\t\tType\t\tSize\tUsed\tPriority\n", 0, &error);
+	g_assert_no_error (error);
+	g_assert_nonnull (swap);
+	g_assert_false (fu_linux_swap_get_enabled (swap));
+	g_assert_false (fu_linux_swap_get_encrypted (swap));
+}
+
+static void
+fu_linux_swap_plain_func (void)
+{
+	g_autoptr(FuLinuxSwap) swap = NULL;
+	g_autoptr(GError) error = NULL;
+
+	swap = fu_linux_swap_new ("Filename\t\t\t\tType\t\tSize\tUsed\tPriority\n"
+			    "/dev/nvme0n1p4  partition\t5962748\t0\t-2\n",
+			    0, &error);
+	g_assert_no_error (error);
+	g_assert_nonnull (swap);
+	g_assert_true (fu_linux_swap_get_enabled (swap));
+	g_assert_false (fu_linux_swap_get_encrypted (swap));
+}
+
+static void
+fu_linux_swap_encrypted_func (void)
+{
+	g_autoptr(FuLinuxSwap) swap = NULL;
+	g_autoptr(GError) error = NULL;
+
+	swap = fu_linux_swap_new ("Filename\t\t\t\tType\t\tSize\tUsed\tPriority\n"
+			    "/dev/dm-1  partition\t5962748\t0\t-2\n",
+			    0, &error);
+	g_assert_no_error (error);
+	g_assert_nonnull (swap);
+	g_assert_true (fu_linux_swap_get_enabled (swap));
+	g_assert_true (fu_linux_swap_get_encrypted (swap));
+}
+
+int
+main (int argc, char **argv)
+{
+	g_test_init (&argc, &argv, NULL);
+
+	/* only critical and error are fatal */
+	g_log_set_fatal_mask (NULL, G_LOG_LEVEL_ERROR | G_LOG_LEVEL_CRITICAL);
+	g_setenv ("G_MESSAGES_DEBUG", "all", TRUE);
+
+	/* tests go here */
+	g_test_add_func ("/linux-swap/none", fu_linux_swap_none_func);
+	g_test_add_func ("/linux-swap/plain", fu_linux_swap_plain_func);
+	g_test_add_func ("/linux-swap/encrypted", fu_linux_swap_encrypted_func);
+	return g_test_run ();
+}
