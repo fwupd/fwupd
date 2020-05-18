3770e246da9ebf2eb9a0b2605e163b68b187f14e
Richard Hughes <richard@hughsie.com>
2020-05-18 21:02:06 +0100

    pcr-bpc: Don't show the 'Not found' message for BLE and SMM_BWP
    If we did not find the device for BIOSWP it is completely useless.

--- plugins/pci-bcr/fu-plugin-pci-bcr.c
+++ plugins/pci-bcr/fu-plugin-pci-bcr.c
@@ -63,18 +63,16 @@ fu_plugin_add_security_attr_ble (FuPlugin *plugin, FuSecurityAttrs *attrs)
 	FuPluginData *priv = fu_plugin_get_data (plugin);
 	g_autoptr(FwupdSecurityAttr) attr = NULL;
 
+	/* no device */
+	if (!priv->has_device)
+		return;
+
 	/* create attr */
 	attr = fwupd_security_attr_new (FWUPD_SECURITY_ATTR_ID_SPI_BLE);
 	fwupd_security_attr_set_plugin (attr, fu_plugin_get_name (plugin));
 	fwupd_security_attr_set_level (attr, FWUPD_SECURITY_ATTR_LEVEL_CRITICAL);
 	fwupd_security_attr_add_obsolete (attr, "linux_spi_lpc");
 	fu_security_attrs_append (attrs, attr);
-
-	/* no device */
-	if (!priv->has_device) {
-		fwupd_security_attr_set_result (attr, FWUPD_SECURITY_ATTR_RESULT_NOT_FOUND);
-		return;
-	}
 
 	/* load file */
 	if ((priv->bcr & BCR_BLE) == 0) {
@@ -93,18 +91,16 @@ fu_plugin_add_security_attr_smm_bwp (FuPlugin *plugin, FuSecurityAttrs *attrs)
 	FuPluginData *priv = fu_plugin_get_data (plugin);
 	g_autoptr(FwupdSecurityAttr) attr = NULL;
 
+	/* no device */
+	if (!priv->has_device)
+		return;
+
 	/* create attr */
 	attr = fwupd_security_attr_new (FWUPD_SECURITY_ATTR_ID_SPI_SMM_BWP);
 	fwupd_security_attr_set_plugin (attr, fu_plugin_get_name (plugin));
 	fwupd_security_attr_set_level (attr, FWUPD_SECURITY_ATTR_LEVEL_CRITICAL);
 	fwupd_security_attr_add_obsolete (attr, "linux_spi_lpc");
 	fu_security_attrs_append (attrs, attr);
-
-	/* no device */
-	if (!priv->has_device) {
-		fwupd_security_attr_set_result (attr, FWUPD_SECURITY_ATTR_RESULT_NOT_FOUND);
-		return;
-	}
 
 	/* load file */
 	if ((priv->bcr & BCR_SMM_BWP) == 0) {
