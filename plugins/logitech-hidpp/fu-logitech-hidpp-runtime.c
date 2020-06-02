fwupd/fwupd

commit c6ae0d998b7e5577b3de4763c25d9d90056bebf1

logitech: Correctly set the protocol
Fixes updates on signed receivers.

plugins/logitech-hidpp/fu-logitech-hidpp-runtime.c
@@ -229,6 +229,9 @@ fu_logitech_hidpp_runtime_setup_internal (FuDevice *device, GError **error)
 		if ((self->version_bl_major == 0x01 && config[8] >= 0x04) ||
 		    (self->version_bl_major == 0x03 && config[8] >= 0x02)) {
 			self->signed_firmware = TRUE;
+			fu_device_set_protocol (device, "com.logitech.unifyingsigned");
+		} else {
+			fu_device_set_protocol (device, "com.logitech.unifying");
 		}
 	}
 
@@ -325,5 +328,4 @@ fu_logitech_hidpp_runtime_init (FuLogitechHidPpRuntime *self)
 	fu_device_set_name (FU_DEVICE (self), "Unifying Receiver");
 	fu_device_set_summary (FU_DEVICE (self), "A miniaturised USB wireless receiver");
 	fu_device_set_remove_delay (FU_DEVICE (self), FU_DEVICE_REMOVE_DELAY_RE_ENUMERATE);
-	fu_device_set_protocol (FU_DEVICE (self), "com.logitech.unifying");
 }

commit d5e761fbf985c17454deec8921d1301dc1b7d97c

trivial: logitech_hidpp: set the protocol properly when bootloader unknown
This appears to be a regression from c6ae0d998b7e5577b3de4763c25d9d90056bebf1
where the case of bootloader version 0 didn't get protocol set.

Fixes: #2156

plugins/logitech-hidpp/fu-logitech-hidpp-runtime.c
@@ -230,10 +230,10 @@ fu_logitech_hidpp_runtime_setup_internal (FuDevice *device, GError **error)
 		    (self->version_bl_major == 0x03 && config[8] >= 0x02)) {
 			self->signed_firmware = TRUE;
 			fu_device_set_protocol (device, "com.logitech.unifyingsigned");
-		} else {
-			fu_device_set_protocol (device, "com.logitech.unifying");
 		}
 	}
+	if (!self->signed_firmware)
+		fu_device_set_protocol (device, "com.logitech.unifying");
 
 	/* enable HID++ notifications */
 	if (!fu_logitech_hidpp_runtime_enable_notifications (self, error)) {