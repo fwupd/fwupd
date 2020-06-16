a685f6f77ca022e4392f7a8f616b2a1318354a88
Benson Leung <bleung@chromium.org>
2020-06-16 11:22:02 -0700

    cros-ec: Add cros-ec-firmware
    cros-ec firmware are in fmap layout, and have two sections
    that this plugin will look for, the EC_RO, and EC_RW sections.
    
    Tested using a servo_micro firmware:
    $ fwupdtool firmware-parse servo_micro_v2.4.17-df61092c3.bin
    <select fmap option>
    FuCrosEcFirmware:
    Version:                 2.4.17
      FuFirmwareImage:
      ID:                    EC_RO
      Index:                 0x1
      Version:               servo_micro_v2.4.17-df61092c3
      Data:                  0xf000
      FuFirmwareImage:
      ID:                    FR_MAIN
      Index:                 0x2
      Data:                  0xf000
      FuFirmwareImage:
      ID:                    RO_FRID
      Index:                 0x3
      Address:               0xc4
      Data:                  0x20
      FuFirmwareImage:
      ID:                    FMAP
      Index:                 0x4
      Address:               0x9a40
      Version:               1.0
      Data:                  0x15e
      FuFirmwareImage:
      ID:                    WP_RO
      Index:                 0x5
      Data:                  0x10000
      FuFirmwareImage:
      ID:                    EC_RW
      Index:                 0x6
      Address:               0x10000
      Version:               servo_micro_v2.4.17-df61092c3
      Data:                  0x10000
      FuFirmwareImage:
      ID:                    RW_FWID
      Index:                 0x7
      Address:               0x100c4
      Data:                  0x20

---
+++ plugins/cros-ec/fu-cros-ec-firmware.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2020 Benson Leung <bleung@chromium.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include "fu-firmware.h"
+#include "fu-fmap-firmware.h"
+
+#define FU_TYPE_CROS_EC_FIRMWARE (fu_cros_ec_firmware_get_type ())
+G_DECLARE_FINAL_TYPE (FuCrosEcFirmware, fu_cros_ec_firmware, FU, CROS_EC_FIRMWARE, FuFmapFirmware)
+
+/*
+ * Each RO or RW section of the new image can be in one of the following
+ * states.
+ */
+typedef enum {
+	FU_CROS_EC_FW_NOT_NEEDED= 0,	/* Version below or equal that on the target. */
+	FU_CROS_EC_FW_NOT_POSSIBLE,	/*
+					 * RO is newer, but can't be transferred due to
+					 * target RW shortcomings.
+					 */
+	FU_CROS_EC_FW_NEEDED		/*
+					 * This section needs to be transferred to the
+					 * target.
+					 */
+} FuCrosEcFirmwareUpgradeStatus;
+
+typedef struct {
+	const gchar			*name;
+	guint32				offset;
+	gsize				size;
+	FuCrosEcFirmwareUpgradeStatus	ustatus;
+	gchar				version[FMAP_STRLEN];
+	gint32				rollback;
+	guint32				key_version;
+} FuCrosEcFirmwareSection;
+
+FuFirmware			*fu_cros_ec_firmware_new		(void);
