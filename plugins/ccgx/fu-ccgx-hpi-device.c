af8bbb88b070588d61eade8a69e710e0d070d43e
Ryan Lee <parangse1975@gmail.com>
2020-07-01 21:56:53 +0900

    ccgx: remove verify flag in plugin

--- plugins/ccgx/fu-ccgx-hpi-device.c
+++ plugins/ccgx/fu-ccgx-hpi-device.c
@@ -1582,7 +1582,6 @@ fu_ccgx_hpi_device_init (FuCcgxHpiDevice *self)
 	fu_device_set_version_format (FU_DEVICE (self), FWUPD_VERSION_FORMAT_TRIPLET);
 	fu_device_add_flag (FU_DEVICE (self), FWUPD_DEVICE_FLAG_REQUIRE_AC);
 	fu_device_add_flag (FU_DEVICE (self), FWUPD_DEVICE_FLAG_DUAL_IMAGE);
-	fu_device_add_flag (FU_DEVICE (self), FWUPD_DEVICE_FLAG_CAN_VERIFY_IMAGE);
 	fu_device_add_flag (FU_DEVICE (self), FWUPD_DEVICE_FLAG_SELF_RECOVERY);
 	fu_device_retry_set_delay (FU_DEVICE (self), HPI_CMD_RETRY_DELAY);
 
