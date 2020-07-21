840e84bcf2a8e77453849016ca1e2e269cc22990
Richard Hughes <richard@hughsie.com>
2020-07-21 15:20:21 +0100

    elantp: Add a plugin to update Elan Touchpads using HID

---
+++ plugins/elantp/fu-elantp-firmware.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2020 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include "fu-firmware.h"
+
+#define FU_TYPE_ELANTP_FIRMWARE (fu_elantp_firmware_get_type ())
+G_DECLARE_FINAL_TYPE (FuElantpFirmware, fu_elantp_firmware, FU, ELANTP_FIRMWARE, FuFirmware)
+
+FuFirmware	*fu_elantp_firmware_new			(void);
+guint16		 fu_elantp_firmware_get_module_id	(FuElantpFirmware	*self);
+guint16		 fu_elantp_firmware_get_iap_addr	(FuElantpFirmware	*self);
