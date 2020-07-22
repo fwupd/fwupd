4e13a790df695b83ae60c3b751654f16026afd5e
Richard Hughes <richard@hughsie.com>
2020-07-22 12:33:21 +0100

    vli: Rename FuVliUsbhubI2cDevice to FuVliUsbhubMsp430Device
    The I²C proxy specification is not always shared with all other devices as
    I originally hoped, instead there are other legacy devices that use different
    sets of I²C commands.
    
    Un-share various bits of code to allow for additional I²C devices to be added.
    
    No logic changes.

--- plugins/vli/fu-vli-usbhub-i2c-common.h
+++ plugins/vli/fu-vli-usbhub-i2c-common.h
@@ -18,17 +18,5 @@ typedef enum {
 	FU_VLI_USBHUB_I2C_STATUS_CHECKSUM	= 0x55,
 } FuVliUsbhubI2cStatus;
 
-/* Texas Instruments BSL */
-#define FU_VLI_USBHUB_I2C_ADDR_WRITE		0x18
-#define FU_VLI_USBHUB_I2C_ADDR_READ		0x19
-
-#define FU_VLI_USBHUB_I2C_CMD_WRITE		0x32
-#define FU_VLI_USBHUB_I2C_CMD_READ_STATUS	0x33
-#define FU_VLI_USBHUB_I2C_CMD_UPGRADE		0x34
-#define FU_VLI_USBHUB_I2C_CMD_READ_VERSIONS	0x40
-
-#define FU_VLI_USBHUB_I2C_R_VDR			0xa0	/* read vendor command */
-#define FU_VLI_USBHUB_I2C_W_VDR			0xb0	/* write vendor command */
-
 gboolean	 fu_vli_usbhub_i2c_check_status		(FuVliUsbhubI2cStatus	 status,
 							 GError			**error);
---
+++ plugins/vli/fu-vli-usbhub-msp430-device.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2019-2020 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include "fu-plugin.h"
+
+#define FU_TYPE_VLI_USBHUB_MSP430_DEVICE (fu_vli_usbhub_msp430_device_get_type ())
+G_DECLARE_FINAL_TYPE (FuVliUsbhubMsp430Device, fu_vli_usbhub_msp430_device, FU, VLI_USBHUB_MSP430_DEVICE, FuDevice)
+
+struct _FuVliUsbhubMsp430DeviceClass
+{
+	FuDeviceClass		parent_class;
+};
+
+FuDevice	*fu_vli_usbhub_msp430_device_new	(FuVliUsbhubDevice	*parent);
