e02bbc4062d10600b1f375dda5f1084cf55696e1
Richard Hughes <richard@hughsie.com>
2020-07-24 17:02:17 +0100

    nitrokey: Define the protocol on the runtime device
    This prevents a daemon warning.

--- plugins/nitrokey/fu-nitrokey-device.c
+++ plugins/nitrokey/fu-nitrokey-device.c
@@ -136,6 +136,7 @@ fu_nitrokey_device_init (FuNitrokeyDevice *device)
 	fu_device_add_flag (FU_DEVICE (device), FWUPD_DEVICE_FLAG_UPDATABLE);
 	fu_device_add_flag (FU_DEVICE (device), FWUPD_DEVICE_FLAG_ADD_COUNTERPART_GUIDS);
 	fu_device_set_version_format (FU_DEVICE (device), FWUPD_VERSION_FORMAT_PAIR);
+	fu_device_set_protocol (FU_DEVICE (device), "org.usb.dfu");
 	fu_device_retry_set_delay (FU_DEVICE (device), 100);
 }
 
