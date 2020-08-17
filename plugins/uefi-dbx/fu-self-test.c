1abb32c623d14581452c3e11bf238b498347b25f
Richard Hughes <richard@hughsie.com>
2020-08-17 14:13:54 +0100

    uefi-dbx: Validate the dbx update is safe to apply
    To do this mount all ESP partitions and check all the binaries there to see if
    they match any entries in the new dbx. If we applied the update when a hash
    matched, we would unintentially 'brick' the users machine, as the grub and shim
    binaries *have* to be updated first.
    
    This functionality does reimplement the PE hashing functionality found in
    sbsigntools and pesign. This was done for 4 main reasons:
    
     * There were some memory safety issues found when fuzzing random binaries
     * Executing the tools hundreds of times was a lot of overhead
     * Operating from a blob of immutable mmap'd memory is much faster
     * We only need a very small amount of functionality from both tools

--- plugins/uefi-dbx/fu-self-test.c
+++ plugins/uefi-dbx/fu-self-test.c
@@ -8,8 +8,40 @@
 
 #include <fwupd.h>
 
+#include "fu-common.h"
 #include "fu-uefi-dbx-common.h"
+#include "fu-efi-image.h"
 #include "fu-efi-signature-parser.h"
+
+static gchar *
+fu_test_get_filename (const gchar *filename)
+{
+	g_autofree gchar *path = NULL;
+	path = g_build_filename (TESTDATADIR, filename, NULL);
+	return fu_common_realpath (path, NULL);
+}
+
+static void
+fu_efi_image_func (void)
+{
+	const gchar *csum = NULL;
+	g_autofree gchar *fn = NULL;
+	g_autoptr(FuEfiImage) img = NULL;
+	g_autoptr(GBytes) bytes = NULL;
+	g_autoptr(GError) error = NULL;
+
+	fn = fu_test_get_filename ("fwupdx64.efi");
+	g_assert_nonnull (fn);
+	bytes = fu_common_get_contents_bytes (fn, &error);
+	g_assert_no_error (error);
+	g_assert_nonnull (bytes);
+
+	img = fu_efi_image_new (bytes, &error);
+	g_assert_no_error (error);
+	g_assert_nonnull (img);
+	csum = fu_efi_image_get_checksum (img);
+	g_assert_cmpstr (csum, ==, "e99707d4378140c01eb3f867240d5cc9e237b126d3db0c3b4bbcd3da1720ddff");
+}
 
 static void
 fu_efi_signature_list_parse_func (void)
@@ -55,6 +87,7 @@ main (int argc, char **argv)
 	g_setenv ("G_MESSAGES_DEBUG", "all", TRUE);
 
 	/* tests go here */
+	g_test_add_func ("/uefi-dbx/image", fu_efi_image_func);
 	g_test_add_func ("/uefi-dbx/file-parse", fu_efi_signature_list_parse_func);
 	return g_test_run ();
 }
