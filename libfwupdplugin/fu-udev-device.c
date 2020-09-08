a06a4809042acae2d7bfbd981fef9436432bcd05
Richard Hughes <richard@hughsie.com>
2020-09-08 10:32:22 +0100

    trivial: Allow using fu_udev_device_pread_full() at offset zero
    Do not enforce the port is nonzero when reading and writing to a device.

--- libfwupdplugin/fu-udev-device.c
+++ libfwupdplugin/fu-udev-device.c
@@ -1010,7 +1010,6 @@ fu_udev_device_pread_full (FuUdevDevice *self, goffset port,
 	FuUdevDevicePrivate *priv = GET_PRIVATE (self);
 
 	g_return_val_if_fail (FU_IS_UDEV_DEVICE (self), FALSE);
-	g_return_val_if_fail (port != 0x0, FALSE);
 	g_return_val_if_fail (buf != NULL, FALSE);
 	g_return_val_if_fail (priv->fd > 0, FALSE);
 
@@ -1056,7 +1055,6 @@ fu_udev_device_pwrite_full (FuUdevDevice *self, goffset port,
 	FuUdevDevicePrivate *priv = GET_PRIVATE (self);
 
 	g_return_val_if_fail (FU_IS_UDEV_DEVICE (self), FALSE);
-	g_return_val_if_fail (port != 0x0, FALSE);
 	g_return_val_if_fail (priv->fd > 0, FALSE);
 
 #ifdef HAVE_PWRITE
