406025ba567e905000a49a76784f1bc2e6b4901a
Mario Limonciello <mario.limonciello@dell.com>
2020-10-06 10:54:11 -0500

    trivial: ci: fix installed-tests for test plugin
    This has been broken for a while and missed various changes.
    * Reading firmware version
    * Passing `-y` into build
    * New verify / verify-image split

--- plugins/test/fu-plugin-test.c
+++ plugins/test/fu-plugin-test.c
@@ -38,7 +38,7 @@ fu_plugin_coldplug (FuPlugin *plugin, GError **error)
 	fu_device_set_name (device, "Integrated_Webcam(TM)");
 	fu_device_add_icon (device, "preferences-desktop-keyboard");
 	fu_device_add_flag (device, FWUPD_DEVICE_FLAG_UPDATABLE);
-	fu_device_add_flag (device, FWUPD_DEVICE_FLAG_CAN_VERIFY);
+	fu_device_add_flag (device, FWUPD_DEVICE_FLAG_CAN_VERIFY_IMAGE);
 	fu_device_set_protocol (device, "com.acme.test");
 	fu_device_set_summary (device, "A fake webcam");
 	fu_device_set_vendor (device, "ACME Corp.");
