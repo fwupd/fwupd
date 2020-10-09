2cbd478219f57b9d45a7067ec8d6cecedc9c7c5d
boger.wang <boger@goodix.com>
2020-10-09 16:32:44 +0800

    contrib : update uncrustify script, support  folder input to check

--- contrib/uncrustify/README.md
+++ contrib/uncrustify/README.md
@@ -1,8 +1,8 @@
 Uncrustify Check
 ================
-This tool is used for auto code beatuify and code check
+This tool is used for auto format or check C/C++ code style.
 ## How to use
 
- 1. install uncrustify tool `sudo apt-get install uncrustify`
- 2. run script for format code `./contrib/uncrustify/uncrustify.sh`
- 3. run script for check code `./contrib/uncrustify/uncrustify.sh -c`
+ 1. install uncrustify tool    `sudo apt-get install uncrustify`
+ 2. run script for format code `./contrib/uncrustify/uncrustify.sh -f folder`
+ 3. run script for check code  `./contrib/uncrustify/uncrustify.sh -c folder`
--- contrib/uncrustify/uncrustify.sh
+++ contrib/uncrustify/uncrustify.sh
@@ -1,20 +1,49 @@
 #!/bin/bash
+if [ ! $# == 2 ]; then
+        echo "Usage : $0 [options] [folder]"
+        echo "folder: the folder under fwupd src root path"
+        echo "-c: verify that nothing changes when files are processed by script"
+        echo "-f: process and replace source files"
+        echo "for example: ./contrib/uncrustify/uncrustify.sh  -f plugins/goodix-moc/"
+        exit
+fi
+
+DIRS=$2
 SRCROOT=`git rev-parse --show-toplevel`
 CFG="$SRCROOT/contrib/uncrustify/uncrustify.cfg"
-echo "srcroot: $SRCROOT"
+
+echo "dir: $DIRS"
 
 case "$1" in
     -c|--check)
 	OPTS="--check"
         ;;
+    -f|--folder)
+    OPTS="--replace --no-backup"
+        ;;
     *)
-	OPTS="--replace --no-backup"
+    echo "Usage : $0 [options] [folder]"
+    exit
         ;;
 esac
 
-pushd "$SRCROOT"
-## for test purpose, just check plugins/goodixmoc folder
-uncrustify -c "$CFG" $OPTS `git ls-tree --name-only -r HEAD | grep -E '.*\.[ch]$' | grep goodixmoc`
+function enumdir(){
+    for element in `ls $1`
+    do  
+        is_dir=$1"/"$element
+        if [ -d $is_dir ]
+        then 
+            enumdir $is_dir
+        else
+            cfile=`ls "$is_dir" | grep -E '.*\.[ch]$'`
+            if [ -n "$cfile" ]
+            then
+                uncrustify -c "$CFG" $OPTS $cfile
+            fi
+        fi  
+    done
+}
+enumdir  $SRCROOT/$DIRS
 RES=$?
-popd
+
 exit $RES
