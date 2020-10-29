be517f4ddb3a0f3d58fde2931ba4dbbd7bd9a82c
Richard Hughes <richard@hughsie.com>
2020-10-29 11:20:30 +0000

    Use a different Device ID for the OptionROM devices
    We want to continue to dedupe the ID, and the priority should be decided on the
    GUID match.
    
    Fixes https://github.com/fwupd/fwupd/issues/2510 in a different way

--- plugins/optionrom/fu-optionrom-device.c
+++ plugins/optionrom/fu-optionrom-device.c
@@ -103,6 +103,7 @@ fu_optionrom_device_init (FuOptionromDevice *self)
 	fu_device_add_flag (FU_DEVICE (self), FWUPD_DEVICE_FLAG_INTERNAL);
 	fu_device_add_icon (FU_DEVICE (self), "audio-card");
 	fu_device_add_flag (FU_DEVICE (self), FWUPD_DEVICE_FLAG_CAN_VERIFY_IMAGE);
+	fu_device_set_logical_id (FU_DEVICE (self), "rom");
 	fu_udev_device_set_flags (FU_UDEV_DEVICE (self),
 				  FU_UDEV_DEVICE_FLAG_OPEN_READ |
 				  FU_UDEV_DEVICE_FLAG_VENDOR_FROM_PARENT);
