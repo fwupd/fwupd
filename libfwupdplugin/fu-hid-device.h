8c415e05686651e528cd57acb35ecf8556345026
Richard Hughes <richard@hughsie.com>
2020-11-06 19:32:14 +0000

    trivia: Allow controlling HID kernel driver rebind

--- libfwupdplugin/fu-hid-device.h
+++ libfwupdplugin/fu-hid-device.h
@@ -29,6 +29,8 @@ struct _FuHidDeviceClass
  * @FU_HID_DEVICE_FLAG_ALLOW_TRUNC:		Allow truncated reads and writes
  * @FU_HID_DEVICE_FLAG_IS_FEATURE:		Use %FU_HID_REPORT_TYPE_FEATURE for wValue
  * @FU_HID_DEVICE_FLAG_RETRY_FAILURE:		Retry up to 10 times on failure
+ * @FU_HID_DEVICE_FLAG_NO_KERNEL_UNBIND:	Do not unbind the kernel driver on open
+ * @FU_HID_DEVICE_FLAG_NO_KERNEL_REBIND:	Do not rebind the kernel driver on close
  *
  * Flags used when calling fu_hid_device_get_report() and fu_hid_device_set_report().
  **/
@@ -37,6 +39,8 @@ typedef enum {
 	FU_HID_DEVICE_FLAG_ALLOW_TRUNC		= 1 << 0,
 	FU_HID_DEVICE_FLAG_IS_FEATURE		= 1 << 1,
 	FU_HID_DEVICE_FLAG_RETRY_FAILURE	= 1 << 2,
+	FU_HID_DEVICE_FLAG_NO_KERNEL_UNBIND	= 1 << 3,
+	FU_HID_DEVICE_FLAG_NO_KERNEL_REBIND	= 1 << 4,
 	FU_HID_DEVICE_FLAG_LAST
 } FuHidDeviceFlags;
 
