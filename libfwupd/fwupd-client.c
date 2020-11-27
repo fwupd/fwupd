fwupd/fwupd

commit 2f49da7f4e9ed300c91956531e3dbdb4da628da5

libfwupd: Restore recognizing gpg and pkcs7 types still
The snap-store intends to ship an updated libfwupd library but
to use it with whatever version daemon is on the host system.

This means that the library needs to still work with older metadata
signing types.

This fixes the following error in that scenario:
```Failed to update metadata for lvfs: Keyring kind jcat not supported```

libfwupd/fwupd-remote.c
@@ -154,6 +154,18 @@ fwupd_remote_set_filename_source (FwupdRemote *self, const gchar *filename_sourc
 	priv->filename_source = g_strdup (filename_source);
 }
 
+static const gchar *
+fwupd_remote_get_suffix_for_keyring_kind (FwupdKeyringKind keyring_kind)
+{
+	if (keyring_kind == FWUPD_KEYRING_KIND_JCAT)
+		return ".jcat";
+	if (keyring_kind == FWUPD_KEYRING_KIND_GPG)
+		return ".asc";
+	if (keyring_kind == FWUPD_KEYRING_KIND_PKCS7)
+		return ".p7b";
+	return NULL;
+}
+
 static CURLU *
 fwupd_remote_build_uri (FwupdRemote *self, const gchar *url, GError **error)
 {
@@ -224,13 +236,15 @@ static void
 fwupd_remote_set_metadata_uri (FwupdRemote *self, const gchar *metadata_uri)
 {
 	FwupdRemotePrivate *priv = GET_PRIVATE (self);
+	const gchar *suffix;
 
 	/* save this so we can export the object as a GVariant */
 	priv->metadata_uri = g_strdup (metadata_uri);
 
 	/* generate the signature URI too */
-	if (priv->keyring_kind == FWUPD_KEYRING_KIND_JCAT)
-		priv->metadata_uri_sig = g_strconcat (metadata_uri, ".jcat", NULL);
+	suffix = fwupd_remote_get_suffix_for_keyring_kind (priv->keyring_kind);
+	if (suffix != NULL)
+		priv->metadata_uri_sig = g_strconcat (metadata_uri, suffix, NULL);
 }
 
 /* note, this has to be set after MetadataURI */
@@ -303,6 +317,7 @@ static void
 fwupd_remote_set_filename_cache (FwupdRemote *self, const gchar *filename)
 {
 	FwupdRemotePrivate *priv = GET_PRIVATE (self);
+	const gchar *suffix;
 
 	g_return_if_fail (FWUPD_IS_REMOTE (self));
 
@@ -310,9 +325,10 @@ fwupd_remote_set_filename_cache (FwupdRemote *self, const gchar *filename)
 	priv->filename_cache = g_strdup (filename);
 
 	/* create for all remote types */
-	if (priv->keyring_kind == FWUPD_KEYRING_KIND_JCAT) {
+	suffix = fwupd_remote_get_suffix_for_keyring_kind (priv->keyring_kind);
+	if (suffix != NULL) {
 		g_free (priv->filename_cache_sig);
-		priv->filename_cache_sig = g_strconcat (filename, ".jcat", NULL);
+		priv->filename_cache_sig = g_strconcat (filename, suffix, NULL);
 	}
 }
 
@@ -376,11 +392,6 @@ fwupd_remote_load_from_filename (FwupdRemote *self,
 				     "Failed to parse type '%s'",
 				     keyring_kind);
 			return FALSE;
-		}
-		if (priv->keyring_kind == FWUPD_KEYRING_KIND_GPG ||
-		    priv->keyring_kind == FWUPD_KEYRING_KIND_PKCS7) {
-			g_debug ("converting Keyring value to Jcat");
-			priv->keyring_kind = FWUPD_KEYRING_KIND_JCAT;
 		}
 	}
 

data/tests/firmware-base-uri.conf
@@ -1,6 +1,6 @@
 [fwupd Remote]
 Enabled=true
 Type=download
-Keyring=gpg
+Keyring=jcat
 MetadataURI=https://s3.amazonaws.com/lvfsbucket/downloads/firmware.xml.gz
 FirmwareBaseURI=https://my.fancy.cdn/

data/tests/firmware-nopath.conf
@@ -1,5 +1,5 @@
 [fwupd Remote]
 Enabled=true
 Type=download
-Keyring=gpg
+Keyring=jcat
 MetadataURI=https://s3.amazonaws.com/lvfsbucket/downloads/firmware.xml.gz

commit 823b6c81414098c4ef43440cac7e87773ad502e6

Only parse the signature if the remote is JCat
This fixes a regression from 2f49da7f4e9ed300c91956531e3dbdb4da628da5 where we
tried to parse the GPG .asc file as a JcatFile even although the remote had
Keyring=gpg (which is the default for ODM accounts on the LVFS) which returned
the cryptic 'Invalid compressed data' message.

The workaround for 1.5.2 is to change the local remote.conf from 'Keyring=gpg'
to 'Keyring=jcat' and to save the file.

libfwupd/fwupd-client.c
@@ -2955,9 +2955,12 @@ fwupd_client_refresh_remote_signature_cb (GObject *source,
 		return;
 	}
 	data->signature = g_steal_pointer (&bytes);
-	if (!fwupd_remote_load_signature_bytes (data->remote, data->signature, &error)) {
-		g_task_return_error (task, g_steal_pointer (&error));
-		return;
+	if (fwupd_remote_get_keyring_kind (data->remote) == FWUPD_KEYRING_KIND_JCAT) {
+		if (!fwupd_remote_load_signature_bytes (data->remote, data->signature, &error)) {
+			g_prefix_error (&error, "Failed to load signature: ");
+			g_task_return_error (task, g_steal_pointer (&error));
+			return;
+		}
 	}
 
 	/* is the signature checksum the same? */

libfwupd/fwupd-remote.c
@@ -989,6 +989,9 @@ fwupd_remote_load_signature_jcat (FwupdRemote *self, JcatFile *jcat_file, GError
  *
  * Parses the signature, updating the metadata URI as appropriate.
  *
+ * This can only be called for remotes with `Keyring=jcat` which is
+ * the default for most remotes.
+ *
  * Returns: %TRUE for success
  *
  * Since: 1.4.5
@@ -996,12 +999,22 @@ fwupd_remote_load_signature_jcat (FwupdRemote *self, JcatFile *jcat_file, GError
 gboolean
 fwupd_remote_load_signature_bytes (FwupdRemote *self, GBytes *bytes, GError **error)
 {
+	FwupdRemotePrivate *priv = GET_PRIVATE (self);
 	g_autoptr(GInputStream) istr = NULL;
 	g_autoptr(JcatFile) jcat_file = jcat_file_new ();
 
 	g_return_val_if_fail (FWUPD_IS_REMOTE (self), FALSE);
 	g_return_val_if_fail (bytes != NULL, FALSE);
 	g_return_val_if_fail (error == NULL || *error == NULL, FALSE);
+
+	/* sanity check */
+	if (priv->keyring_kind != FWUPD_KEYRING_KIND_JCAT) {
+		g_set_error_literal (error,
+				     FWUPD_ERROR,
+				     FWUPD_ERROR_NOT_SUPPORTED,
+				     "only supported for JCat remotes");
+		return FALSE;
+	}
 
 	istr = g_memory_input_stream_new_from_bytes (bytes);
 	if (!jcat_file_import_stream (jcat_file, istr, JCAT_IMPORT_FLAG_NONE, NULL, error))