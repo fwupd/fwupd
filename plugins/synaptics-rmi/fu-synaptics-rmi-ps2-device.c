a6f5f80df11986fc95bdc1b8dad89a51d3980b8b
Richard Hughes <richard@hughsie.com>
2020-12-02 17:23:20 +0000

    More PS2

--- plugins/synaptics-rmi/fu-synaptics-rmi-ps2-device.c
+++ plugins/synaptics-rmi/fu-synaptics-rmi-ps2-device.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2020 Richard Hughes <richard@hughsie.com>
+ * Copyright (c) 2020 Synaptics Incorporated.
  *
  * SPDX-License-Identifier: LGPL-2.1+
  */
@@ -7,12 +8,93 @@
 #include "config.h"
 
 #include "fu-synaptics-rmi-ps2-device.h"
+#include "fu-io-channel.h"
 
 struct _FuSynapticsRmiPs2Device {
 	FuUdevDevice		 parent_instance;
+	FuIOChannel		*io_channel;
 };
 
 G_DEFINE_TYPE (FuSynapticsRmiPs2Device, fu_synaptics_rmi_ps2_device, FU_TYPE_UDEV_DEVICE)
+
+static gboolean
+ReadACK (FuSynapticsRmiPs2Device *self, guint8 *pbuf, GError **error) //FIXME: rename to fu_synaptics_rmi_ps2_device_read_ack
+{
+	for(guint i = 0 ; i < 60; i++) {
+		g_autoptr(GError) error_local = NULL;
+		if (!fu_io_channel_read_raw (self->io_channel, pbuf, 0x1,
+					     NULL, 60,
+					     FU_IO_CHANNEL_FLAG_NONE,
+					     &error_local)) {
+			if (g_error_matches (error_local, G_IO_ERROR, G_IO_ERROR_TIMED_OUT)) {
+				g_debug ("read timed out: %u", i);
+				g_usleep (30);
+				continue;
+			}
+			g_propagate_error (error, g_steal_pointer (&error_local));
+			return FALSE;
+		}
+		return TRUE;
+	}
+	g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED, "failed");
+	return FALSE;
+}
+
+/* read a single byte from the touchpad */
+static gboolean
+ReadByte (FuSynapticsRmiPs2Device *self, guint8 *pbuf, guint timeout, GError **error) //FIXME: rename to fu_synaptics_rmi_ps2_device_read_byte
+{
+	return fu_io_channel_read_raw (self->io_channel, pbuf, 0x1,
+				       NULL, timeout,
+				       FU_IO_CHANNEL_FLAG_NONE,
+				       error);
+}
+
+/* write a single byte to the touchpad and the read the acknowledge */
+static gboolean
+WriteByte (FuSynapticsRmiPs2Device *self, guint8 buf, guint timeout, GError **error) //FIXME: rename to fu_synaptics_rmi_ps2_device_write_byte
+{
+	gboolean do_write = TRUE;
+
+	for (guint i = 0; i < 3; i++) {
+		guint8 res = 0;
+		g_autoptr(GError) error_local = NULL;
+
+		if (do_write) {
+			if (!fu_io_channel_write_raw (self->io_channel, &buf, 0x1, timeout,
+						      FU_IO_CHANNEL_FLAG_FLUSH_INPUT, error))
+				return FALSE;
+		}
+		do_write = FALSE;
+		g_debug ("wrote byte: 0x%x, attempt to read acknowledge...", buf);
+
+		if(!ReadACK (self, &res, &error_local)) {
+			g_debug ("read Failed: %s", error_local->message);
+			continue;
+		}
+		if (res == edpsAcknowledge) {
+			g_debug ("write acknowledged");
+			return TRUE;
+		}
+		if (res == edpsResend) {
+			g_debug ("resend");
+			do_write = TRUE;
+			g_debug ("resend, sleep 1 sec");
+			g_usleep (1000*1000);
+			continue;
+		}
+		if (res == edpsError) {
+			g_debug ("PS2Device::WriteByte fail received error from touchpad");
+			do_write = TRUE;
+			g_usleep (1000 * 10);
+			continue;
+		}
+		g_debug ("other response : 0x%x, sleep 1 sec", res);
+		g_usleep (1000 * 10);
+	}
+	g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED, "failed");
+	return FALSE;
+}
 
 static void
 fu_synaptics_rmi_ps2_device_to_string (FuDevice *device, guint idt, GString *str)
@@ -47,7 +129,50 @@ fu_synaptics_rmi_ps2_device_setup (FuDevice *device, GError **error)
 static gboolean
 fu_synaptics_rmi_ps2_device_open (FuUdevDevice *device, GError **error)
 {
-//	FuSynapticsRmiPs2Device *self = FU_SYNAPTICS_RMI_PS2_DEVICE (device);
+	FuSynapticsRmiPs2Device *self = FU_SYNAPTICS_RMI_PS2_DEVICE (device);
+	guint8 buf[2] = { 0x0 };
+
+	/* create channel */
+	self->io_channel = fu_io_channel_unix_new (fu_udev_device_get_fd (device));
+
+	/* in serio_raw mode */
+	if (fu_device_has_flag (FU_DEVICE (self), FWUPD_DEVICE_FLAG_IS_BOOTLOADER)) {
+		/* send reset -- may take 300-500ms */
+		if (!WriteByte (self, edpAuxReset, 600, error)) {
+			g_prefix_error (error, "failed to reset: ");
+			return FALSE;
+		}
+
+		/* read the 0xAA 0x00 announcing the touchpad is ready */
+		if (!ReadByte(self, &buf[0], 500, error) ||
+		    !ReadByte(self, &buf[1], 500, error)) {
+			g_prefix_error (error, "failed to read 0xAA00: ");
+			return FALSE;
+		}
+		if (buf[0] != 0xAA || buf[1] != 0x00) {
+			g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED,
+				     "failed to read 0xAA00, got 0x%02X%02X: ",
+				     buf[0], buf[1]);
+			return FALSE;
+		}
+
+		/* disable the device so that it stops reporting finger data */
+		if (!WriteByte (self, edpAuxDisable, 50, error)) {
+			g_prefix_error (error, "failed to disable stream mode: ");
+			return FALSE;
+		}
+	}
+
+	/* success */
+	return TRUE;
+}
+
+static gboolean
+fu_synaptics_rmi_ps2_device_close (FuUdevDevice *device, GError **error)
+{
+	FuSynapticsRmiPs2Device *self = FU_SYNAPTICS_RMI_PS2_DEVICE (device);
+	fu_udev_device_set_fd (device, -1);
+	g_clear_object (&self->io_channel);
 	return TRUE;
 }
 
@@ -79,8 +204,63 @@ fu_synaptics_rmi_ps2_device_write_firmware (FuDevice *device,
 }
 
 static gboolean
+fu_synaptics_rmi_ps2_device_set_resolution_sequence (FuSynapticsRmiPs2Device *self,
+						     guint8 arg,
+						     gboolean send_e6s,
+						     GError **error)
+{
+	g_debug ("Set Resolution Sequence: arg = 0x%x", arg);
+
+	/* send set scaling twice if send_e6s */
+	for (gint i = send_e6s ? 2 : 1; i > 0; --i) {
+		if (!WriteByte (self, edpAuxSetScaling1To1, 50, error))
+			return FALSE;
+	}
+
+	for (gint i = 3; i >= 0; --i) {
+		guint8 ucTwoBitArg = (arg >> (i * 2)) & 0x3;
+		if (!WriteByte (self, edpAuxSetResolution, 50, error)) {
+			return FALSE;
+		}
+		g_debug ("Send ucTwoBitArg = 0x%x", ucTwoBitArg);
+		if (!WriteByte (self, ucTwoBitArg, 50, error))
+			return FALSE;
+	}
+	return TRUE;
+}
+
+static gboolean
+fu_synaptics_rmi_ps2_device_set_sample_rate_sequence (FuSynapticsRmiPs2Device *self,
+						      guint8 param,
+						      guint8 arg,
+						      gboolean send_e6s,
+						      GError **error)
+{
+	/* allow 3 retries */
+	for (guint i = 0; i < 3; i++) {
+		g_autoptr(GError) error_local = NULL;
+		if (i > 0) {
+			/* always send two E6s when retrying */
+			send_e6s = TRUE;
+		}
+		if (!fu_synaptics_rmi_ps2_device_set_resolution_sequence (self, arg, send_e6s, &error_local) ||
+		    !WriteByte (self, edpAuxSetSampleRate, 50, &error_local) ||
+		    !WriteByte (self, param, 50, &error_local)) {
+			g_warning ("failed, will retry: %s", error_local->message);
+			continue;
+		}
+		return TRUE;
+	}
+
+	g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED, "too many tries");
+	return FALSE;
+}
+
+static gboolean
 fu_synaptics_rmi_ps2_device_detach (FuDevice *device, GError **error)
 {
+	FuSynapticsRmiPs2Device *self = FU_SYNAPTICS_RMI_PS2_DEVICE (device);
+
 	/* sanity check */
 	if (fu_device_has_flag (device, FWUPD_DEVICE_FLAG_IS_BOOTLOADER)) {
 		g_debug ("already in bootloader mode, skipping");
@@ -95,7 +275,23 @@ fu_synaptics_rmi_ps2_device_detach (FuDevice *device, GError **error)
 	}
 
 	/* rescan device */
-	return fu_device_probe (device, error);
+	if (!fu_device_probe (device, error))
+		return FALSE;
+
+	/* disable stream */
+	if (!WriteByte (self, edpAuxDisable, 50, error)) {
+		g_prefix_error (error, "failed to disable stream mode: ");
+		return FALSE;
+	}
+
+	/* enable RMI mode */
+	if (!fu_synaptics_rmi_ps2_device_set_sample_rate_sequence (self, essrSetModeByte2, edpAuxFullRMIBackDoor, FALSE, error)) {
+		g_prefix_error (error, "failed to enter RMI mode: ");
+		return FALSE;
+	}
+
+	/* success */
+	return TRUE;
 }
 
 static gboolean
@@ -128,6 +324,9 @@ fu_synaptics_rmi_ps2_device_init (FuSynapticsRmiPs2Device *self)
 	fu_device_set_vendor (FU_DEVICE (self), "Synaptics");
 	fu_device_set_vendor_id (FU_DEVICE (self), "HIDRAW:0x06CB");
 	fu_device_set_version_format (FU_DEVICE (self), FWUPD_VERSION_FORMAT_HEX); //FIXME?
+	fu_udev_device_set_flags (FU_UDEV_DEVICE (self),
+				  FU_UDEV_DEVICE_FLAG_OPEN_READ |
+				  FU_UDEV_DEVICE_FLAG_OPEN_WRITE);
 }
 
 static void
@@ -152,4 +351,5 @@ fu_synaptics_rmi_ps2_device_class_init (FuSynapticsRmiPs2DeviceClass *klass)
 	klass_device->prepare_firmware = fu_synaptics_rmi_ps2_device_prepare_firmware;
 	klass_udev_device->probe = fu_synaptics_rmi_ps2_device_probe;
 	klass_udev_device->open = fu_synaptics_rmi_ps2_device_open;
+	klass_udev_device->close = fu_synaptics_rmi_ps2_device_close;
 }
--- plugins/synaptics-rmi/fu-synaptics-rmi-ps2-device.h
+++ plugins/synaptics-rmi/fu-synaptics-rmi-ps2-device.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2020 Richard Hughes <richard@hughsie.com>
+ * Copyright (c) 2020 Synaptics Incorporated.
  *
  * SPDX-License-Identifier: LGPL-2.1+
  */
@@ -10,3 +11,90 @@
 
 #define FU_TYPE_SYNAPTICS_RMI_PS2_DEVICE (fu_synaptics_rmi_ps2_device_get_type ())
 G_DECLARE_FINAL_TYPE (FuSynapticsRmiPs2Device, fu_synaptics_rmi_ps2_device, FU, SYNAPTICS_RMI_PS2_DEVICE, FuUdevDevice)
+
+//FIXME: typedef enum, and make upper case? align with tabs too!
+enum EPS2DataPortCommand {
+	edpAuxFullRMIBackDoor         = 0x7F,
+	edpAuxAccessModeByte1         = 0xE0,
+	edpAuxAccessModeByte2         = 0xE1,
+	edpAuxIBMReadSecondaryID      = 0xE1,
+	edpAuxSetScaling1To1          = 0xE6,
+	edpAuxSetScaling2To1          = 0xE7,
+	edpAuxSetResolution           = 0xE8,
+	edpAuxStatusRequest           = 0xE9,
+	edpAuxSetStreamMode           = 0xEA,
+	edpAuxReadData                = 0xEB,
+	edpAuxResetWrapMode           = 0xEC,
+	edpAuxSetWrapMode             = 0xEE,
+	edpAuxSetRemoteMode           = 0xF0,
+	edpAuxReadDeviceType          = 0xF2,
+	edpAuxSetSampleRate           = 0xF3,
+	edpAuxEnable                  = 0xF4,
+	edpAuxDisable                 = 0xF5,
+	edpAuxSetDefault              = 0xF6,
+	edpAuxResend                  = 0xFE,
+	edpAuxReset                   = 0xFF,
+};
+
+enum ESynapticsDeviceResponse {
+	esdrTouchPad			= 0x47,
+	esdrStyk			= 0x46,
+	esdrControlBar			= 0x44,
+	esdrRGBControlBar		= 0x43,
+};
+
+enum EStatusRequestSequence {
+	esrIdentifySynaptics          = 0x00,
+	esrReadTouchPadModes          = 0x01,
+	esrReadModeByte               = 0x01,
+	esrReadEdgeMargins            = 0x02,
+	esrReadCapabilities           = 0x02,
+	esrReadModelID                = 0x03,
+	esrReadCompilationDate        = 0x04,
+	esrReadSerialNumberPrefix     = 0x06,
+	esrReadSerialNumberSuffix     = 0x07,
+	esrReadResolutions            = 0x08,
+	esrReadExtraCapabilities1     = 0x09,
+	esrReadExtraCapabilities2     = 0x0A,
+	esrReadExtraCapabilities3     = 0x0B,
+	esrReadExtraCapabilities4     = 0x0C,
+	esrReadExtraCapabilities5     = 0x0D,
+	esrReadCoordinates            = 0x0D,
+	esrReadExtraCapabilities6     = 0x0E,
+	esrReadExtraCapabilities7     = 0x0F,
+};
+
+enum EPS2DataPortStatus {
+	edpsAcknowledge               = 0xFA,
+	edpsError                     = 0xFC,
+	edpsResend                    = 0xFE,
+	edpsTimeOut                   = 0x100
+};
+
+enum ESetSampleRateSequence {
+	essrSetModeByte1              = 0x0A,
+	essrSetModeByte2              = 0x14,
+	essrSetModeByte3              = 0x28,
+	essrSetModeByte4              = 0x3C,
+	essrSetDeluxeModeByte1        = 0x0A,
+	essrSetDeluxeModeByte2        = 0x3C,
+	essrSetDeluxeModeByte3        = 0xC8,
+	essrFastRecalibrate           = 0x50,
+	essrPassThroughCommandTunnel  = 0x28
+};
+
+enum EDeviceType {
+	edtUnknown,
+	edtTouchPad,
+};
+
+enum EStickDeviceType {
+	esdtNone = 0,
+	esdtIBM,
+	esdtALPS,
+	esdtELAN,
+	esdtNXP,
+	esdtJYTSyna,
+	esdtSynaptics,
+	esdtUnknown = 0xFFFFFFFF
+};
