bc9da04c2c8f5731d144fbc79d5a803e8e75708f
Sean Rhodes <sean@starlabs.systems>
2021-01-19 16:50:19 +0000

    Added AC flag

--- plugins/flashrom/fu-flashrom-device.c
+++ plugins/flashrom/fu-flashrom-device.c
@@ -21,6 +21,7 @@ fu_flashrom_device_init (FuFlashromDevice *self)
 	fu_device_add_flag (FU_DEVICE (self), FWUPD_DEVICE_FLAG_INTERNAL);
 	fu_device_add_flag (FU_DEVICE (self), FWUPD_DEVICE_FLAG_UPDATABLE);
 	fu_device_add_flag (FU_DEVICE (self), FWUPD_DEVICE_FLAG_NEEDS_REBOOT);
+	fu_device_add_flag (FU_DEVICE (self), FWUPD_DEVICE_FLAG_REQUIRE_AC);
 	fu_device_add_instance_id (FU_DEVICE (self), "main-system-firmware");
 	fu_device_add_internal_flag (FU_DEVICE (self), FU_DEVICE_INTERNAL_FLAG_ENSURE_SEMVER);
 	fu_device_set_version_format (FU_DEVICE (self), FWUPD_VERSION_FORMAT_TRIPLET);
