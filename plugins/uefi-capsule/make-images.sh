4a19138026f0f7140cfd600ac25d8d64bfd200a6
Richard Hughes <richard@hughsie.com>
2021-01-19 11:35:45 +0000

    trivial: Use argparse for make-images.py

--- plugins/uefi-capsule/make-images.sh
+++ plugins/uefi-capsule/make-images.sh
@@ -10,4 +10,7 @@ LOCALEDIR="${DESTDIR}$1"
 PYTHON3="$2"
 
 install -m 0755 -d $LOCALEDIR
-${PYTHON3} ${MESON_SOURCE_ROOT}/plugins/uefi-capsule/make-images.py "Installing firmware update…" $LOCALEDIR ${MESON_SOURCE_ROOT}/po/LINGUAS
+${PYTHON3} ${MESON_SOURCE_ROOT}/plugins/uefi-capsule/make-images.py \
+	--label="Installing firmware update…" \
+	--localedir=$LOCALEDIR \
+	--linguas=${MESON_SOURCE_ROOT}/po/LINGUAS
