378b00018b42bdaefe50e70fa546065a41ee0e42
Javier Martinez Canillas <javierm@redhat.com>
2021-02-04 14:53:25 +0000

    uefi-capsule: Add SBAT metadata to the fwupd EFI binary
    The Secure Boot Advanced Targeting (SBAT) [0] is a Generation Number Based
    Revocation mechanism that is meant to replace the DBX revocation file list.
    
    Binaries must contain a .sbat data section that has a set entries, each of
    them consisting of UTF-8 strings as comma separated values. Allow to embed
    this information into the fwupd EFI binary at build time.
    
    The SBAT metadata must contain at least two entries. One that defines the
    SBAT version used and another one that defines the component generation.
    
    Downstream users can add additional entries if have changes that make them
    diverge from the upstream code and potentially add other vulnerabilities.
    
    [0]: https://github.com/rhboot/shim/blob/sbat/SBAT.md

--- plugins/uefi-capsule/README.md
+++ plugins/uefi-capsule/README.md
@@ -25,6 +25,21 @@ scheduled for update the other UEFI devices found in the ESRT will be marked
 as `updatable-hidden` rather than `updatable`. Rebooting will restore them so
 they can be updated on next OS boot.
 
+UEFI SBAT Support
+-----------------
+
+If compiling with `-Dsupported_build=true` the packager must also specify the
+SBAT metadata required for the secure boot revocation support. See the
+specification for more information: https://github.com/rhboot/shim/blob/sbat/SBAT.md
+
+Typically, this will be set as part of the package build script, e.g.
+
+    -Defi_os_dir="fedora" \
+    -Defi_sbat_distro_summary="The Fedora Project" \
+    -Defi_sbat_distro_pkgname="%{name}" \
+    -Defi_sbat_distro_version="%{version}" \
+    -Defi_sbat_distro_url="https://src.fedoraproject.org/rpms/%{name}" \
+
 Firmware Format
 ---------------
 
--- plugins/uefi-capsule/efi/generate_binary.py
+++ plugins/uefi-capsule/efi/generate_binary.py
@@ -10,6 +10,62 @@
 import subprocess
 import sys
 import argparse
+import tempfile
+
+
+def _run_objcopy_sbat(args):
+    """ append SBAT metadata """
+    FWUPD_SUMMARY = "Firmware update daemon"
+    FWUPD_URL = "https://github.com/fwupd/fwupd"
+
+    with tempfile.NamedTemporaryFile() as sfd:
+
+        # spec
+        sfd.write(
+            "{0},{1},{2},{0},{1},{3}\n".format(
+                "sbat",
+                args.sbat_version,
+                "UEFI shim",
+                "https://github.com/rhboot/shim/blob/main/SBAT.md",
+            ).encode()
+        )
+
+        # fwupd
+        sfd.write(
+            "{0},{1},{2},{0},{3},{4}\n".format(
+                args.project_name,
+                args.sbat_generation,
+                "Firmware update daemon",
+                args.project_version,
+                FWUPD_URL,
+            ).encode()
+        )
+
+        # distro specifics, falling back to the project defaults
+        if args.sbat_distro_id:
+            sfd.write(
+                "{0}-{1},{2},{3},{4},{5},{6}\n".format(
+                    args.project_name,
+                    args.sbat_distro_id,
+                    args.sbat_distro_generation or args.sbat_generation,
+                    args.sbat_distro_summary or FWUPD_SUMMARY,
+                    args.sbat_distro_pkgname,
+                    args.sbat_distro_version or args.project_version,
+                    args.sbat_distro_url or FWUPD_URL,
+                ).encode()
+            )
+
+        # all written
+        sfd.seek(0)
+
+        # add a section to the object; use `objdump -s -j .sbat` to verify
+        argv = [
+            args.objcopy,
+            "--add-section",
+            ".sbat={}".format(sfd.name),
+            args.outfile,
+        ]
+        subprocess.run(argv, check=True)
 
 
 def _run_objcopy(args):
@@ -73,6 +129,57 @@ if __name__ == "__main__":
         help="EFI architecture",
     )
     parser.add_argument(
+        "--project-name",
+        help="SBAT project name",
+    )
+    parser.add_argument(
+        "--project-version",
+        help="SBAT project version",
+    )
+    parser.add_argument(
+        "--sbat-version",
+        default=1,
+        type=int,
+        help="SBAT version",
+    )
+    parser.add_argument(
+        "--sbat-generation",
+        default=0,
+        type=int,
+        help="SBAT generation",
+    )
+    parser.add_argument(
+        "--sbat-distro-id",
+        default=None,
+        help="SBAT distribution ID"
+    )
+    parser.add_argument(
+        "--sbat-distro-generation",
+        default=None,
+        type=int,
+        help="SBAT distribution generation",
+    )
+    parser.add_argument(
+        "--sbat-distro-summary",
+        default=None,
+        help="SBAT distribution summary",
+    )
+    parser.add_argument(
+        "--sbat-distro-pkgname",
+        default=None,
+        help="SBAT distribution package name",
+    )
+    parser.add_argument(
+        "--sbat-distro-version",
+        default=None,
+        help="SBAT distribution version",
+    )
+    parser.add_argument(
+        "--sbat-distro-url",
+        default=None,
+        help="SBAT distribution URL",
+    )
+    parser.add_argument(
         "infile",
         help="Input file",
     )
@@ -82,6 +189,7 @@ if __name__ == "__main__":
     )
     _args = parser.parse_args()
     _run_objcopy(_args)
+    _run_objcopy_sbat(_args)
     _run_genpeimg(_args)
 
     sys.exit(0)
