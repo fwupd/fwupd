63a9665107a29b7eeb64a432693a1e0160b8cc4c
Richard Hughes <richard@hughsie.com>
2021-02-15 10:59:08 +0000

    pixart: Fuzz the firmware parser

--- plugins/pixart-rf/fu-plugin-pixart-rf.c
+++ plugins/pixart-rf/fu-plugin-pixart-rf.c
@@ -18,5 +18,5 @@ fu_plugin_init (FuPlugin *plugin)
 	fu_plugin_set_build_hash (plugin, FU_BUILD_HASH);
 	fu_plugin_add_udev_subsystem (plugin, "hidraw");
 	fu_plugin_set_device_gtype (plugin, FU_TYPE_PXI_DEVICE);
-	fu_plugin_add_firmware_gtype (plugin, "pixart", FU_TYPE_PXI_RF_FIRMWARE);
+	fu_plugin_add_firmware_gtype (plugin, "pixart", FU_TYPE_PXI_FIRMWARE);
 }
