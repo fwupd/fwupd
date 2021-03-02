98c592fe5175436e041d8dce9b8626c39a455931
Richard Hughes <richard@hughsie.com>
2021-03-02 10:21:16 +0000

    ccgx: Use a plugin prefix for quirk keys

--- plugins/ccgx/fu-ccgx-hpi-device.c
+++ plugins/ccgx/fu-ccgx-hpi-device.c
@@ -70,9 +70,9 @@ fu_ccgx_hpi_device_to_string (FuDevice *device, guint idt, GString *str)
 	fu_common_string_append_kx (str, idt, "EpBulkOut", self->ep_bulk_out);
 	fu_common_string_append_kx (str, idt, "EpIntrIn", self->ep_intr_in);
 	if (self->flash_row_size > 0)
-		fu_common_string_append_kx (str, idt, "FlashRowSize", self->flash_row_size);
+		fu_common_string_append_kx (str, idt, "CcgxFlashRowSize", self->flash_row_size);
 	if (self->flash_size > 0)
-		fu_common_string_append_kx (str, idt, "FlashSize", self->flash_size);
+		fu_common_string_append_kx (str, idt, "CcgxFlashSize", self->flash_size);
 }
 
 typedef struct {
@@ -1486,7 +1486,7 @@ fu_ccgx_hpi_device_set_quirk_kv (FuDevice *device,
 				     "invalid SiliconId");
 		return FALSE;
 	}
-	if (g_strcmp0 (key, "FlashRowSize") == 0) {
+	if (g_strcmp0 (key, "CcgxFlashRowSize") == 0) {
 		guint64 tmp = fu_common_strtoull (value);
 		if (tmp < G_MAXUINT32) {
 			self->flash_row_size = tmp;
@@ -1495,10 +1495,10 @@ fu_ccgx_hpi_device_set_quirk_kv (FuDevice *device,
 		g_set_error_literal (error,
 				     G_IO_ERROR,
 				     G_IO_ERROR_INVALID_DATA,
-				     "invalid FlashRowSize");
+				     "invalid CcgxFlashRowSize");
 		return FALSE;
 	}
-	if (g_strcmp0 (key, "FlashSize") == 0) {
+	if (g_strcmp0 (key, "CcgxFlashSize") == 0) {
 		guint64 tmp = fu_common_strtoull (value);
 		if (tmp < G_MAXUINT32) {
 			self->flash_size = tmp;
@@ -1507,17 +1507,17 @@ fu_ccgx_hpi_device_set_quirk_kv (FuDevice *device,
 		g_set_error_literal (error,
 				     G_IO_ERROR,
 				     G_IO_ERROR_INVALID_DATA,
-				     "invalid FlashSize");
+				     "invalid CcgxFlashSize");
 		return FALSE;
 	}
-	if (g_strcmp0 (key, "ImageKind") == 0) {
+	if (g_strcmp0 (key, "CcgxImageKind") == 0) {
 		self->fw_image_type = fu_ccgx_fw_image_type_from_string (value);
 		if (self->fw_image_type != FW_IMAGE_TYPE_UNKNOWN)
 			return TRUE;
 		g_set_error_literal (error,
 				     G_IO_ERROR,
 				     G_IO_ERROR_INVALID_DATA,
-				     "invalid ImageKind");
+				     "invalid CcgxImageKind");
 		return FALSE;
 	}
 	g_set_error_literal (error,
