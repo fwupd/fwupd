8ae24984acf659994b85719c080c57c99298611a
Richard Hughes <richard@hughsie.com>
2021-03-10 19:37:26 +0000

    Generate the fuzzing targets using a script
    We cannot yet build them as part of the build process, as we need an installed
    tree so that the FuFirmware GTypes are registered. This halfway house at least
    verifies we can generate all the binary files we include in the git repo.

---
+++ src/fuzzing/generate.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# pylint: disable=invalid-name,missing-docstring
+#
+# Copyright (C) 2021 Richard Hughes <richard@hughsie.com>
+#
+# SPDX-License-Identifier: LGPL-2.1+
+
+import os
+import subprocess
+
+if __name__ == "__main__":
+
+    for fn_src, fn_dst in [
+        ("bcm57xx.builder.xml", "bcm57xx.bin"),
+        ("ccgx.builder.xml", "ccgx.cyacd"),
+        ("ccgx-dmc.builder.xml", "ccgx-dmc.bin"),
+        ("cros-ec.builder.xml", "cros-ec.bin"),
+        ("dfuse.builder.xml", "dfuse.dfu"),
+        ("ebitdo.builder.xml", "ebitdo.dat"),
+        ("elantp.builder.xml", "elantp.bin"),
+        ("fmap.builder.xml", "fmap.bin"),
+        ("fmap-offset.builder.xml", "fmap-offset.bin"),
+        ("ifd.builder.xml", "ifd.bin"),
+        ("ihex.builder.xml", "ihex.hex"),
+        ("pixart.builder.xml", "pixart.bin"),
+        ("rmi-0x.builder.xml", "synaptics-rmi-0x.img"),
+        ("rmi-10.builder.xml", "synaptics-rmi-10.img"),
+        ("solokey.builder.xml", "solokey.json"),
+        ("srec-addr32.builder.xml", "srec-addr32.srec"),
+        ("srec.builder.xml", "srec.srec"),
+        ("synaprom.builder.xml", "synaprom.bin"),
+        ("synaptics-mst.builder.xml", "synaptics-mst.dat"),
+        ("wacom.builder.xml", "wacom.wac"),
+    ]:
+        if not os.path.exists(fn_src):
+            print("WARNING: cannot find {}".format(fn_src))
+            continue
+        print("INFO: converting {} into {}".format(fn_src, fn_dst))
+        try:
+            argv = [
+                "../../build/src/fwupdtool",
+                "firmware-build",
+                fn_src,
+                os.path.join("firmware", fn_dst),
+            ]
+            subprocess.run(argv, check=True)
+        except subprocess.CalledProcessError as e:
+            print("tried to run: `{}` and got {}".format(" ".join(argv), str(e)))
