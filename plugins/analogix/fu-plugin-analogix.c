0f7f7f2cb5207f24dc5de6c05d9722b6f22718d8
xtcui <xtcui@analogix.corp-partner.google.com>
2021-04-01 17:38:41 +0800

    Add support for some Analogix hardware
    With much help from:
    
     * Ricardo Cañuelo <ricardo.canuelo@collabora.com>
     * Richard Hughes <richard@hughsie.com>

---
+++ plugins/analogix/fu-plugin-analogix.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2021 Xiaotian Cui <xtcui@analogixsemi.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#include "config.h"
+
+#include "fu-plugin-vfuncs.h"
+
+#include "fu-analogix-device.h"
+#include "fu-analogix-firmware.h"
+
+void
+fu_plugin_init (FuPlugin *plugin)
+{
+	fu_plugin_set_build_hash (plugin, FU_BUILD_HASH);
+	fu_plugin_set_device_gtype (plugin, FU_TYPE_ANALOGIX_DEVICE);
+	fu_plugin_add_firmware_gtype (plugin, NULL, FU_TYPE_ANALOGIX_FIRMWARE);
+}
