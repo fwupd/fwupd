65e26d7324961f90e6483964c2c392592c165bd5
Richard Hughes <richard@hughsie.com>
2021-04-06 17:10:02 +0100

    Fix quirks

--- plugins/analogix/fu-analogix-device.c
+++ plugins/analogix/fu-analogix-device.c
@@ -483,6 +483,7 @@ fu_analogix_device_init (FuAnalogixDevice *self)
 {
 	fu_device_add_protocol (FU_DEVICE (self), "com.analogix.bb");
 	fu_device_add_flag (FU_DEVICE (self), FWUPD_DEVICE_FLAG_UPDATABLE);
+	fu_device_add_flag (FU_DEVICE (self), FWUPD_DEVICE_FLAG_USABLE_DURING_UPDATE);
 	fu_device_set_version_format (FU_DEVICE (self), FWUPD_VERSION_FORMAT_PAIR);
 	fu_device_set_summary (FU_DEVICE (self), "Phoenix-Lite");
 	fu_device_set_vendor (FU_DEVICE (self), "Analogix Semiconductor Inc.");
