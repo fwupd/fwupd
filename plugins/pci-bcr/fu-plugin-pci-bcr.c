4678faa5508dbc435a9974ca2022a76b1a830d54
Richard Hughes <richard@hughsie.com>
2021-04-20 13:05:05 +0100

    pci-bcr: Use fu_device_inhibit() rather than removing UPDATABLE

--- plugins/pci-bcr/fu-plugin-pci-bcr.c
+++ plugins/pci-bcr/fu-plugin-pci-bcr.c
@@ -36,8 +36,9 @@ fu_plugin_pci_bcr_set_updatable (FuPlugin *plugin, FuDevice *dev)
 {
 	FuPluginData *priv = fu_plugin_get_data (plugin);
 	if ((priv->bcr & BCR_WPD) == 0 && (priv->bcr & BCR_BLE) > 0) {
-		fu_device_remove_flag (dev, FWUPD_DEVICE_FLAG_UPDATABLE);
-		fu_device_set_update_error (dev, "BIOS locked");
+		fu_device_inhibit (dev, "bcr-locked", "BIOS locked");
+	} else {
+		fu_device_uninhibit (dev, "bcr-locked");
 	}
 }
 
