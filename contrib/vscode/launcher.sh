d46bdb37881cbc5676a56ab4a6864202cac5db4c
Crag Wang <crag0715@gmail.com>
2021-05-03 15:59:28 +0800

    trivial: fixup runtime path for fwupdtool and libraries

--- contrib/vscode/launcher.sh
+++ contrib/vscode/launcher.sh
@@ -2,6 +2,7 @@
 export ROOT=#ROOT#
 export FWUPD_LOCALSTATEDIR=${ROOT}/dist
 export FWUPD_SYSCONFDIR=${ROOT}/dist/etc
+export LD_LIBRARY_PATH=${ROOT}/dist/lib/$(gcc -dumpmachine)
 if [ -n "${DEBUG}" ]; then
         DEBUG="gdbserver localhost:9091"
 fi
