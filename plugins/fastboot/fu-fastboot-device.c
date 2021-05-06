ddc40ab595caf67d4ce6b17447dcc8b85389cb1b
Richard Hughes <richard@hughsie.com>
2021-05-06 16:57:15 +0100

    fastboot: Add some trivial error prefix for the getvar failure

--- plugins/fastboot/fu-fastboot-device.c
+++ plugins/fastboot/fu-fastboot-device.c
@@ -243,8 +243,10 @@ fu_fastboot_device_getvar (FuDevice *device, const gchar *key, gchar **str, GErr
 	if (!fu_fastboot_device_writestr (device, tmp, error))
 		return FALSE;
 	if (!fu_fastboot_device_read (device, str,
-				      FU_FASTBOOT_DEVICE_READ_FLAG_NONE, error))
+				      FU_FASTBOOT_DEVICE_READ_FLAG_NONE, error)) {
+		g_prefix_error (error, "failed to getvar %s: ", key);
 		return FALSE;
+	}
 	return TRUE;
 }
 
