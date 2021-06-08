551b9708acba86e89b853e9c3ec597b729bf6c0d
Richard Hughes <richard@hughsie.com>
2021-06-08 14:19:31 +0100

    vli: Fix the i2c device enumeration, e.g. MSP430
    This regressed in f9e9e087d, as we're using FuDevice->setup() to check
    if the device is present, but without setting the physical IDs.

--- plugins/vli/fu-vli-usbhub-msp430-device.c
+++ plugins/vli/fu-vli-usbhub-msp430-device.c
@@ -308,6 +308,7 @@ fu_vli_usbhub_msp430_device_probe (FuDevice *device, GError **error)
 	g_autofree gchar *instance_id = NULL;
 
 	fu_device_set_name (device, fu_vli_common_device_kind_to_string (device_kind));
+	fu_device_set_physical_id (device, fu_device_get_physical_id (FU_DEVICE (parent)));
 
 	/* add instance ID */
 	instance_id = g_strdup_printf ("USB\\VID_%04X&PID_%04X&I2C_%s",
