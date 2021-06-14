7a125913bb4c3ce9925f24275e59e483e4284b5d
Ricardo Cañuelo <ricardo.canuelo@collabora.com>
2021-06-14 14:09:21 +0200

    logitech-hidpp: make receiver runtime class derivable

--- plugins/logitech-hidpp/fu-plugin-logitech-hidpp.c
+++ plugins/logitech-hidpp/fu-plugin-logitech-hidpp.c
@@ -12,7 +12,7 @@
 #include "fu-logitech-hidpp-bootloader-texas.h"
 #include "fu-logitech-hidpp-common.h"
 #include "fu-logitech-hidpp-peripheral.h"
-#include "fu-logitech-hidpp-runtime.h"
+#include "fu-logitech-hidpp-runtime-unifying.h"
 
 gboolean
 fu_plugin_startup (FuPlugin *plugin, GError **error)
@@ -37,6 +37,6 @@ fu_plugin_init (FuPlugin *plugin)
 	fu_plugin_add_rule (plugin, FU_PLUGIN_RULE_CONFLICTS, "unifying");
 	fu_plugin_add_device_gtype (plugin, FU_TYPE_UNIFYING_BOOTLOADER_NORDIC);
 	fu_plugin_add_device_gtype (plugin, FU_TYPE_UNIFYING_BOOTLOADER_TEXAS);
-	fu_plugin_add_device_gtype (plugin, FU_TYPE_UNIFYING_RUNTIME);
+	fu_plugin_add_device_gtype (plugin, FU_TYPE_HIDPP_RUNTIME_UNIFYING);
 	fu_plugin_add_device_gtype (plugin, FU_TYPE_HIDPP_PERIPHERAL);
 }
