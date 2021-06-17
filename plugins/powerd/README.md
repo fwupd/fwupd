379250996a3023447626cf598391a68f9ca5de61
Twain Byrnes <binarynewts@google.com>
2021-06-17 18:35:09 +0000

    Skeleton plugin for powerd
    This CL creates a mostly empty fu-plugin-powerd.c that
    has the file disable itself on startup with an error.
    
    The README.md file was re-created to include information on
    powerd and the specific interface it will use.
    
    A meson.build file re-created to call fu-plugin-powerd.c.
    It excludes a .conf install function was removed to be consistent with
    an upstream change.
    
    The general meson.build file includes the
    powerd subdirectory for powerd to compile.
    
    No .quirk file was included. There will be general daemon.quirk file for all plugins.
    
    FIXUP: Adjusted copyright format and whitespace in fu-plugin-powerd.c
    and plugin returns false on startup. Also added to "fu-plugin-powerd.so"
    entry fwupd.spec.in.

---
+++ plugins/powerd/README.md
@@ -0,0 +1,18 @@
+Powerd Support
+==============
+
+Introduction
+------------
+
+This plugin is used to ensure that some updates are not done on battery power
+and that there is sufficient battery power for certain updates that are.
+
+Vendor ID Security
+------------------
+
+This protocol does not create a device and thus requires no vendor ID set.
+
+External interface access
+-------------------------
+
+This plugin requires access to a dbus interface that is to be discussed.
---
+++ plugins/powerd/fu-plugin-powerd.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2021 Twain Byrnes <binarynewts@google.com>
+ * Copyright (C) 2021 George Popoola <gpopoola@google.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#include "config.h"
+
+#include <fwupdplugin.h>
+
+struct FuPluginData {
+	GDBusProxy *proxy; /* nullable */
+};
+
+void
+fu_plugin_init (FuPlugin *plugin)
+{
+	fu_plugin_set_build_hash (plugin, FU_BUILD_HASH);
+}
+
+gboolean
+fu_plugin_startup (FuPlugin *plugin, GError **error)
+{
+	g_set_error (
+	    error, G_IO_ERROR, G_IO_ERROR_NOT_SUPPORTED, "not supported yet");
+	return FALSE;
+}
+
+gboolean
+fu_plugin_update_prepare (FuPlugin *plugin,
+			  FwupdInstallFlags flags,
+			  FuDevice *device,
+			  GError **error)
+{
+	return TRUE;
+}
