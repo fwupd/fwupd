a12d7150385ec87d55663d768f5aed329dd63a0a
Peter Marheine <pmarheine@chromium.org>
2021-06-29 11:55:55 +1000

    New plugin: parade-lspcon

---
+++ plugins/parade-lspcon/fu-plugin-parade-lspcon.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2021 Peter Marheine <pmarheine@chromium.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#include "config.h"
+
+#include "fu-parade-lspcon-device.h"
+
+void
+fu_plugin_init (FuPlugin *plugin)
+{
+	fu_plugin_set_build_hash (plugin, FU_BUILD_HASH);
+	fu_plugin_add_udev_subsystem (plugin, "i2c");
+	fu_plugin_add_device_gtype (plugin, FU_TYPE_PARADE_LSPCON_DEVICE);
+}
