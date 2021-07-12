72a7699813f69a95e897d96063afdd6e52b863fb
Peter Marheine <pmarheine@chromium.org>
2021-07-12 13:57:34 +1000

    flashrom: remove PS175 support
    This is now implemented by the parade_lspcon plugin.

--- plugins/flashrom/fu-plugin-flashrom.c
+++ plugins/flashrom/fu-plugin-flashrom.c
@@ -26,7 +26,6 @@
 #include <string.h>
 
 #include "fu-flashrom-internal-device.h"
-#include "fu-flashrom-lspcon-i2c-spi-device.h"
 
 #include <libflashrom.h>
 
@@ -41,8 +40,6 @@ fu_plugin_init (FuPlugin *plugin)
 	fu_plugin_add_rule (plugin, FU_PLUGIN_RULE_METADATA_SOURCE, "linux_lockdown");
 	fu_plugin_add_rule (plugin, FU_PLUGIN_RULE_CONFLICTS, "coreboot"); /* obsoleted */
 	fu_plugin_add_flag (plugin, FWUPD_PLUGIN_FLAG_REQUIRE_HWID);
-	fu_plugin_add_device_gtype (plugin, FU_TYPE_FLASHROM_LSPCON_I2C_SPI_DEVICE);
-	fu_plugin_add_udev_subsystem (plugin, "i2c");
 	fu_context_add_quirk_key (ctx, "FlashromProgrammer");
 }
 
