9e9c858ddab31418ca0cc6efebc2a33d95c31c1c
Richard Hughes <richard@hughsie.com>
2021-07-19 14:33:28 +0100

    redfish: Automatically connect the BMC network interface at startup
    Some distributions like RHEL change the default to not autoconnect
    ethernet devices. This breaks connecting to the SMBIOS-defined BMC
    adapter which means all the reads fail after a 60s timemout.
    
    Autoconnect the internal USB ethernet adapter so we can use Redfish
    without user action.

--- plugins/redfish/fu-redfish-network.c
+++ plugins/redfish/fu-redfish-network.c
@@ -10,66 +10,33 @@
 
 #include "fu-redfish-network.h"
 
-#define NETWORK_MANAGER_SERVICE_NAME		"org.freedesktop.NetworkManager"
-#define NETWORK_MANAGER_INTERFACE		"org.freedesktop.NetworkManager"
-#define NETWORK_MANAGER_INTERFACE_IP4_CONFIG	"org.freedesktop.NetworkManager.IP4Config"
-#define NETWORK_MANAGER_INTERFACE_DEVICE	"org.freedesktop.NetworkManager.Device"
-
 typedef struct {
-	gchar		*address;
-	gboolean	 device_found;
+	FuRedfishNetworkDevice	*device;
 	const gchar	*mac_addr;
 	guint16		 vid;
 	guint16		 pid;
 } FuRedfishNetworkMatchHelper;
 
 static gboolean
-fu_redfish_network_ip_match_ip4_config (FuRedfishNetworkMatchHelper *helper,
+fu_redfish_network_device_match_device (FuRedfishNetworkMatchHelper *helper,
 					const gchar *object_path,
 					GError **error)
-{
-	g_autoptr(GDBusProxy) proxy = NULL;
-	g_autoptr(GVariant) addr_data = NULL;
-
-	proxy = g_dbus_proxy_new_for_bus_sync (G_BUS_TYPE_SYSTEM,
-					       G_DBUS_PROXY_FLAGS_DO_NOT_CONNECT_SIGNALS,
-					       NULL,
-					       NETWORK_MANAGER_SERVICE_NAME,
-					       object_path,
-					       NETWORK_MANAGER_INTERFACE_IP4_CONFIG,
-					       NULL,
-					       error);
-	if (proxy == NULL)
-		return FALSE;
-
-	addr_data = g_dbus_proxy_get_cached_property (proxy, "AddressData");
-	if (addr_data != NULL) {
-		g_autoptr(GVariant) addr_data0 = g_variant_get_child_value (addr_data, 0);
-		g_autoptr(GVariantDict) dict = g_variant_dict_new (addr_data0);
-		g_variant_dict_lookup (dict, "address", "s", &helper->address);
-	}
-
-	return TRUE;
-}
-
-static gboolean
-fu_redfish_network_ip_match_device (FuRedfishNetworkMatchHelper *helper,
-				    const gchar *object_path,
-				    GError **error)
 {
 	g_autoptr(GDBusProxy) proxy = NULL;
 
 	/* connect to device */
 	proxy = g_dbus_proxy_new_for_bus_sync (G_BUS_TYPE_SYSTEM,
-					       G_DBUS_PROXY_FLAGS_DO_NOT_CONNECT_SIGNALS,
+					       G_DBUS_PROXY_FLAGS_NONE,
 					       NULL,
 					       NETWORK_MANAGER_SERVICE_NAME,
 					       object_path,
 					       NETWORK_MANAGER_INTERFACE_DEVICE,
 					       NULL,
 					       error);
-	if (proxy == NULL)
+	if (proxy == NULL) {
+		g_prefix_error (error, "failed to connect to interface %s: ", object_path);
 		return FALSE;
+	}
 
 	/* compare MAC address different */
 	if (helper->mac_addr != NULL) {
@@ -85,7 +52,7 @@ fu_redfish_network_ip_match_device (FuRedfishNetworkMatchHelper *helper,
 		if (g_getenv ("FWUPD_REDFISH_VERBOSE") != NULL)
 			g_debug ("mac_addr=%s", mac_addr);
 		if (g_strcmp0 (mac_addr, helper->mac_addr) == 0)
-			helper->device_found = TRUE;
+			helper->device = fu_redfish_network_device_new (object_path);
 	}
 
 	/* compare VID:PID */
@@ -120,7 +87,7 @@ fu_redfish_network_ip_match_device (FuRedfishNetworkMatchHelper *helper,
 		if (g_getenv ("FWUPD_REDFISH_VERBOSE") != NULL)
 			g_debug ("%s: 0x%04x, 0x%04x", sysfs_path, vid, pid);
 		if (vid == helper->vid && pid == helper->pid)
-			helper->device_found = TRUE;
+			helper->device = fu_redfish_network_device_new (object_path);
 #else
 		g_set_error_literal (error,
 				     G_IO_ERROR,
@@ -130,25 +97,12 @@ fu_redfish_network_ip_match_device (FuRedfishNetworkMatchHelper *helper,
 #endif
 	}
 
-	/* look at the config */
-	if (helper->device_found) {
-		g_autoptr(GVariant) ip4_config = NULL;
-		ip4_config = g_dbus_proxy_get_cached_property (proxy, "Ip4Config");
-		if (ip4_config != NULL) {
-			const gchar *tmp = g_variant_get_string (ip4_config, NULL);
-			if (g_getenv ("FWUPD_REDFISH_VERBOSE") != NULL)
-				g_debug ("using IP config %s", tmp);
-			if (!fu_redfish_network_ip_match_ip4_config (helper, tmp, error))
-				return FALSE;
-		}
-	}
-
 	/* assume success */
 	return TRUE;
 }
 
 static gboolean
-fu_redfish_network_ip_match (FuRedfishNetworkMatchHelper *helper, GError **error)
+fu_redfish_network_device_match (FuRedfishNetworkMatchHelper *helper, GError **error)
 {
 	g_autoptr(GDBusProxy) proxy = NULL;
 	g_autoptr(GError) error_local = NULL;
@@ -161,24 +115,38 @@ fu_redfish_network_ip_match (FuRedfishNetworkMatchHelper *helper, GError **error
 					       G_DBUS_PROXY_FLAGS_DO_NOT_CONNECT_SIGNALS,
 					       NULL,
 					       NETWORK_MANAGER_SERVICE_NAME,
-					       "/org/freedesktop/NetworkManager",
+					       NETWORK_MANAGER_PATH,
 					       NETWORK_MANAGER_INTERFACE,
 					       NULL,
-					       error);
-	if (proxy == NULL)
+					       &error_local);
+	if (proxy == NULL) {
+		if (g_error_matches (error_local, G_IO_ERROR, G_IO_ERROR_DBUS_ERROR) ||
+		    g_error_matches (error_local, G_IO_ERROR, G_IO_ERROR_NOT_FOUND)) {
+			g_set_error_literal (error,
+					     FWUPD_ERROR,
+					     FWUPD_ERROR_NOT_SUPPORTED,
+					     "D-Bus is not running");
+			return FALSE;
+		}
+		g_propagate_prefixed_error (error, g_steal_pointer (&error_local),
+					    "failed to construct proxy for %s: ",
+					    NETWORK_MANAGER_SERVICE_NAME);
 		return FALSE;
+	}
 	devices = g_dbus_proxy_call_sync (proxy, "GetDevices",
 					  NULL, G_DBUS_CALL_FLAGS_NONE, -1,
 					  NULL, &error_local);
 	if (devices == NULL) {
 		if (g_error_matches (error_local, G_DBUS_ERROR, G_DBUS_ERROR_SERVICE_UNKNOWN)) {
 			g_set_error_literal (error,
-					     G_IO_ERROR,
-					     G_IO_ERROR_NOT_FOUND,
+					     FWUPD_ERROR,
+					     FWUPD_ERROR_NOT_SUPPORTED,
 					     "NetworkManager is not running");
 			return FALSE;
 		}
-		g_propagate_error (error, g_steal_pointer (&error_local));
+		g_propagate_prefixed_error (error, g_steal_pointer (&error_local),
+					    "failed to call GetDevices() on %s: ",
+					    g_dbus_proxy_get_name (proxy));
 		return FALSE;
 	}
 
@@ -187,51 +155,44 @@ fu_redfish_network_ip_match (FuRedfishNetworkMatchHelper *helper, GError **error
 	for (guint i = 0; paths[i] != NULL; i++) {
 		if (g_getenv ("FWUPD_REDFISH_VERBOSE") != NULL)
 			g_debug ("device %u: %s", i, paths[i]);
-		if (!fu_redfish_network_ip_match_device (helper, paths[i], error))
+		if (!fu_redfish_network_device_match_device (helper, paths[i], error))
 			return FALSE;
-		if (helper->device_found)
+		if (helper->device != NULL)
 			break;
 	}
-	if (!helper->device_found) {
+	if (helper->device == NULL) {
 		g_set_error_literal (error,
-				     G_IO_ERROR,
-				     G_IO_ERROR_NOT_FOUND,
+				     FWUPD_ERROR,
+				     FWUPD_ERROR_NOT_FOUND,
 				     "could not find device");
-		return FALSE;
-	}
-	if (helper->address == NULL) {
-		g_set_error_literal (error,
-				     G_IO_ERROR,
-				     G_IO_ERROR_NOT_CONNECTED,
-				     "could not find IP address for device");
 		return FALSE;
 	}
 	return TRUE;
 }
 
-gchar *
-fu_redfish_network_ip_for_mac_addr (const gchar *mac_addr, GError **error)
+FuRedfishNetworkDevice *
+fu_redfish_network_device_for_mac_addr (const gchar *mac_addr, GError **error)
 {
 	FuRedfishNetworkMatchHelper helper = {
 		.mac_addr = mac_addr,
 	};
-	if (!fu_redfish_network_ip_match (&helper, error)) {
+	if (!fu_redfish_network_device_match (&helper, error)) {
 		g_prefix_error (error, "missing %s: ", mac_addr);
 		return NULL;
 	}
-	return helper.address;
+	return helper.device;
 }
 
-gchar *
-fu_redfish_network_ip_for_vid_pid (guint16 vid, guint16 pid, GError **error)
+FuRedfishNetworkDevice *
+fu_redfish_network_device_for_vid_pid (guint16 vid, guint16 pid, GError **error)
 {
 	FuRedfishNetworkMatchHelper helper = {
 		.vid = vid,
 		.pid = pid,
 	};
-	if (!fu_redfish_network_ip_match (&helper, error)) {
+	if (!fu_redfish_network_device_match (&helper, error)) {
 		g_prefix_error (error, "missing 0x%04x:0x%04x: ", vid, pid);
 		return NULL;
 	}
-	return helper.address;
+	return helper.device;
 }
