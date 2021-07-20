19f924f6be8ec6fa428a131cd7a894a12ce8bd3c
Richard Hughes <richard@hughsie.com>
2021-07-20 20:46:30 +0100

    powerd: trivial style fixups

--- plugins/powerd/fu-plugin-powerd.c
+++ plugins/powerd/fu-plugin-powerd.c
@@ -12,14 +12,14 @@
 #define MINIMUM_BATTERY_PERCENTAGE_FALLBACK 10
 
 struct FuPluginData {
-	GDBusProxy *proxy; /* nullable */
+	GDBusProxy	*proxy; /* nullable */
 };
 
 void
 fu_plugin_init (FuPlugin *plugin)
 {
 	fu_plugin_set_build_hash (plugin, FU_BUILD_HASH);
-	fu_plugin_alloc_data (plugin, sizeof (FuPluginData));
+	fu_plugin_alloc_data (plugin, sizeof(FuPluginData));
 }
 
 void
@@ -34,40 +34,28 @@ gboolean
 fu_plugin_startup (FuPlugin *plugin, GError **error)
 {
 	FuPluginData *data = fu_plugin_get_data (plugin);
-	g_autoptr(GDBusConnection) connection = NULL;
 	g_autofree gchar *name_owner = NULL;
 
-	/* establishing DBus connection and proxy for method call to powerd */
-	connection = g_bus_get_sync (G_BUS_TYPE_SYSTEM, NULL, error);
-
-	if (connection == NULL) {
-		g_prefix_error (error,
-				"Connection with system was not established: ");
-		return FALSE;
-	}
-
-	data->proxy = g_dbus_proxy_new_sync (connection,
-					     G_DBUS_PROXY_FLAGS_NONE,
-					     NULL,
-					     "org.chromium.PowerManager",
-					     "/org/chromium/PowerManager",
-					     "org.chromium.PowerManager",
-					     NULL,
-					     error);
+	/* establish proxy for method call to powerd */
+	data->proxy = g_dbus_proxy_new_for_bus_sync (G_BUS_TYPE_SYSTEM,
+						     G_DBUS_PROXY_FLAGS_NONE,
+						     NULL,
+						     "org.chromium.PowerManager",
+						     "/org/chromium/PowerManager",
+						     "org.chromium.PowerManager",
+						     NULL,
+						     error);
 
 	if (data->proxy == NULL) {
-		g_prefix_error (
-		    error,
-		    "Failed to establish proxy for power manager server: ");
+		g_prefix_error (error, "failed to establish proxy: ");
 		return FALSE;
 	}
-
 	name_owner = g_dbus_proxy_get_name_owner (data->proxy);
 	if (name_owner == NULL) {
 		g_set_error (error,
 			     FWUPD_ERROR,
 			     FWUPD_ERROR_NOT_SUPPORTED,
-			     "No service that owns the name for %s",
+			     "no service that owns the name for %s",
 			     g_dbus_proxy_get_name (data->proxy));
 		return FALSE;
 	}
@@ -82,8 +70,8 @@ fu_plugin_update_prepare (FuPlugin *plugin,
 {
 	FuPluginData *data = fu_plugin_get_data (plugin);
 	guint32 power_type = 0;
-	guint32 current_status = 0;
-	gdouble current_percentage = 0;
+	guint32 current_state = 0;
+	gdouble current_level = 0;
 	g_autoptr(GVariant) powerd_response = NULL;
 
 	/* making method call to "GetBatteryState" through the proxy */
@@ -94,28 +82,22 @@ fu_plugin_update_prepare (FuPlugin *plugin,
 						  -1,
 						  NULL,
 						  error);
-
 	if (powerd_response == NULL) {
-		g_prefix_error (
-		    error,
-		    "Battery information from powerd was not loaded "
-		    "successfully: ");
+		g_prefix_error (error, "battery information was not loaded: ");
 		return FALSE;
 	}
 
-	/* parsing data in powerd_response and inputting it into battery-check
-	 * conditions */
+	/* parse and use for battery-check conditions */
 	g_variant_get (powerd_response,
 		       "(uud)",
 		       &power_type,
-		       &current_status,
-		       &current_percentage);
+		       &current_state,
+		       &current_level);
 
 	/* blocking updates if there is no AC power or if battery
-	 * percentage is too low
-	 */
+	 * percentage is too low */
 	if (fu_device_has_flag (device, FWUPD_DEVICE_FLAG_REQUIRE_AC) &&
-	    current_status == FU_BATTERY_STATE_DISCHARGING) {
+	    current_state == FU_BATTERY_STATE_DISCHARGING) {
 		g_set_error (error,
 			     FWUPD_ERROR,
 			     FWUPD_ERROR_AC_POWER_REQUIRED,
@@ -123,8 +105,7 @@ fu_plugin_update_prepare (FuPlugin *plugin,
 			     "unless forced ");
 		return FALSE;
 	}
-
-	if (current_percentage < MINIMUM_BATTERY_PERCENTAGE_FALLBACK) {
+	if (current_level < MINIMUM_BATTERY_PERCENTAGE_FALLBACK) {
 		g_set_error (error,
 			     FWUPD_ERROR,
 			     FWUPD_ERROR_BATTERY_LEVEL_TOO_LOW,
