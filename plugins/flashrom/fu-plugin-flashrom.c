4ff6a6dedb67e3efe01c2fcbbf5b842a100159d6
Mario Limonciello <superm1@gmail.com>
2021-08-06 14:23:01 -0500

    trivial: flashrom: Add SPDX license header
    Clarify that the license was intended to be LGPL2.1 when the project
    adjusted and this source file was just missed as well.

--- plugins/flashrom/fu-plugin-flashrom.c
+++ plugins/flashrom/fu-plugin-flashrom.c
@@ -3,21 +3,7 @@
  * Copyright (C) 2017 Richard Hughes <richard@hughsie.com>
  * Copyright (C) 2019 9elements Agency GmbH <patrick.rudolph@9elements.com>
  *
- * Licensed under the GNU General Public License Version 2
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * SPDX-License-Identifier: LGPL-2.1+
  */
 
 #include "config.h"
