915916e22a5091861f83e9df0d9a7cdc199787d9
Mario Limonciello <superm1@gmail.com>
2021-08-06 14:23:37 -0500

    Add a pre-commit rule to verify a SPDX license header is present

---
+++ contrib/ci/check-license.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+#
+# Copyright (C) 2021 Richard Hughes <richard@hughsie.com>
+# Copyright (C) 2021 Mario Limonciello <superm1@gmail.com>
+#
+# SPDX-License-Identifier: LGPL-2.1+
+
+import glob
+import sys
+
+
+def __get_license(fn: str) -> str:
+    with open(fn, "r") as f:
+        for line in f.read().split("\n"):
+            if line.find("SPDX-License-Identifier:") > 0:
+                return line.split(":")[1]
+    return ""
+
+
+def test_files() -> int:
+
+    rc: int = 0
+
+    for fn in glob.glob("**/*.[c|h|py|sh]", recursive=True):
+        if "meson-private" in fn:
+            continue
+        license = __get_license(fn)
+        if not license:
+            print("{} does not specify a license".format(fn))
+            rc = 1
+            continue
+        if not "GPL" in license:
+            print("{} does not contain LGPL or GPL ({})".format(fn, license))
+            rc = 1
+            continue
+    return rc
+
+
+if __name__ == "__main__":
+
+    # all done!
+    sys.exit(test_files())
