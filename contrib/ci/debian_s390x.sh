684bc038132977ff28e54550f95941cff5e4ee84
Mario Limonciello <superm1@gmail.com>
2021-08-17 09:19:11 -0500

    trivial: fix various shellcheck warnings

--- contrib/ci/debian_s390x.sh
+++ contrib/ci/debian_s390x.sh
@@ -7,7 +7,8 @@ export LC_ALL=C.UTF-8
 #evaluate using Debian's build flags
 eval "$(dpkg-buildflags --export=sh)"
 #filter out -Bsymbolic-functions
-export LDFLAGS=$(dpkg-buildflags --get LDFLAGS | sed "s/-Wl,-Bsymbolic-functions\s//")
+LDFLAGS=$(dpkg-buildflags --get LDFLAGS | sed "s/-Wl,-Bsymbolic-functions\s//")
+export LDFLAGS
 
 rm -rf build
 mkdir -p build
