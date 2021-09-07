6fb8d32aed549f219da1fa5c03bdd7cca16199d8
Mario Limonciello <mario.limonciello@amd.com>
2021-09-07 15:08:01 -0500

    trivial: create urlmap for each gi-docgen target rather than sharing

--- docs/urlmap.js
+++
@@ -1,5 +0,0 @@
-baseURLs = [
-    [ 'Gio', 'https://people.gnome.org/~ebassi/docs/_build/Gio/' ],
-    [ 'GObject', 'https://people.gnome.org/~ebassi/docs/_build/GObject/' ],
-    [ 'Fwupd', '../libfwupd/' ],
-]
