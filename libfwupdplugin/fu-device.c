fwupd/fwupd

commit 15b668c0c646216056033f358e80425e87bc8bdd

Open the device proxy in the engine where required
The benefit of using the proxy device is that we can 'use' the proxy
device for device access, but 'report' the progress on the passed
FuDevice instance.

This means the front-end reports the device status correctly when
updating composite devices that us proxies.

The comment always said we should move it to the daemon if another
plugin started doing this, and that is now.

libfwupdplugin/fu-device.c
@@ -1098,6 +1098,26 @@ fu_device_get_proxy (FuDevice *self)
 }
 
 /**
+ * fu_device_get_proxy_with_fallback:
+ * @self: a #FuDevice
+ *
+ * Gets the proxy device, falling back to the device itself.
+ *
+ * Returns: (transfer none): a device
+ *
+ * Since: 1.6.2
+ **/
+FuDevice *
+fu_device_get_proxy_with_fallback (FuDevice *self)
+{
+	FuDevicePrivate *priv = GET_PRIVATE (self);
+	g_return_val_if_fail (FU_IS_DEVICE (self), NULL);
+	if (priv->proxy != NULL)
+		return priv->proxy;
+	return self;
+}
+
+/**
  * fu_device_get_children:
  * @self: a #FuDevice
  *

libfwupdplugin/fu-device.h
@@ -310,6 +310,7 @@ void		 fu_device_add_counterpart_guid		(FuDevice	*self,
 FuDevice	*fu_device_get_proxy			(FuDevice	*self);
 void		 fu_device_set_proxy			(FuDevice	*self,
 							 FuDevice	*proxy);
+FuDevice	*fu_device_get_proxy_with_fallback	(FuDevice	*self);
 const gchar	*fu_device_get_metadata			(FuDevice	*self,
 							 const gchar	*key);
 gboolean	 fu_device_get_metadata_boolean		(FuDevice	*self,

plugins/vli/fu-vli-usbhub-device.c
@@ -299,8 +299,9 @@ fu_vli_usbhub_device_spi_write_data (FuVliDevice *self,
 #define VL817_ADDR_GPIO_GET_INPUT_DATA		0xF6A2	/* 0=low, 1=high */
 
 static gboolean
-fu_vli_usbhub_device_attach_full (FuDevice *device, FuDevice *proxy, GError **error)
+fu_vli_usbhub_device_attach (FuDevice *device, GError **error)
 {
+	FuDevice *proxy = fu_device_get_proxy_with_fallback (device);
 	g_autoptr(GError) error_local = NULL;
 
 	/* update UI */
@@ -360,25 +361,6 @@ fu_vli_usbhub_device_attach_full (FuDevice *device, FuDevice *proxy, GError **er
 	/* success */
 	fu_device_add_flag (device, FWUPD_DEVICE_FLAG_WAIT_FOR_REPLUG);
 	return TRUE;
-}
-
-static gboolean
-fu_vli_usbhub_device_attach (FuDevice *device, GError **error)
-{
-	FuDevice *proxy = fu_device_get_proxy (device);
-
-	/* if we do this in another plugin, perhaps move to the engine? */
-	if (proxy != NULL) {
-		g_autoptr(FuDeviceLocker) locker = NULL;
-		g_debug ("using proxy device %s", fu_device_get_id (proxy));
-		locker = fu_device_locker_new (proxy, error);
-		if (locker == NULL)
-			return FALSE;
-		return fu_vli_usbhub_device_attach_full (device, proxy, error);
-	}
-
-	/* normal case */
-	return fu_vli_usbhub_device_attach_full (device, device, error);
 }
 
 /* disable hub sleep states -- not really required by 815~ hubs */

libfwupdplugin/fu-plugin.c
@@ -655,8 +655,9 @@ fu_plugin_get_context (FuPlugin *self)
 static gboolean
 fu_plugin_device_attach (FuPlugin *self, FuDevice *device, GError **error)
 {
+	FuDevice *proxy = fu_device_get_proxy_with_fallback (device);
 	g_autoptr(FuDeviceLocker) locker = NULL;
-	locker = fu_device_locker_new (device, error);
+	locker = fu_device_locker_new (proxy, error);
 	if (locker == NULL)
 		return FALSE;
 	return fu_device_attach (device, error);
@@ -665,8 +666,9 @@ fu_plugin_device_attach (FuPlugin *self, FuDevice *device, GError **error)
 static gboolean
 fu_plugin_device_detach (FuPlugin *self, FuDevice *device, GError **error)
 {
+	FuDevice *proxy = fu_device_get_proxy_with_fallback (device);
 	g_autoptr(FuDeviceLocker) locker = NULL;
-	locker = fu_device_locker_new (device, error);
+	locker = fu_device_locker_new (proxy, error);
 	if (locker == NULL)
 		return FALSE;
 	return fu_device_detach (device, error);
@@ -675,8 +677,9 @@ fu_plugin_device_detach (FuPlugin *self, FuDevice *device, GError **error)
 static gboolean
 fu_plugin_device_activate (FuPlugin *self, FuDevice *device, GError **error)
 {
+	FuDevice *proxy = fu_device_get_proxy_with_fallback (device);
 	g_autoptr(FuDeviceLocker) locker = NULL;
-	locker = fu_device_locker_new (device, error);
+	locker = fu_device_locker_new (proxy, error);
 	if (locker == NULL)
 		return FALSE;
 	return fu_device_activate (device, error);
@@ -687,8 +690,9 @@ fu_plugin_device_write_firmware (FuPlugin *self, FuDevice *device,
 				 GBytes *fw, FwupdInstallFlags flags,
 				 GError **error)
 {
+	FuDevice *proxy = fu_device_get_proxy_with_fallback (device);
 	g_autoptr(FuDeviceLocker) locker = NULL;
-	locker = fu_device_locker_new (device, error);
+	locker = fu_device_locker_new (proxy, error);
 	if (locker == NULL)
 		return FALSE;
 
@@ -723,6 +727,7 @@ fu_plugin_device_write_firmware (FuPlugin *self, FuDevice *device,
 static gboolean
 fu_plugin_device_read_firmware (FuPlugin *self, FuDevice *device, GError **error)
 {
+	FuDevice *proxy = fu_device_get_proxy_with_fallback (device);
 	g_autoptr(FuDeviceLocker) locker = NULL;
 	g_autoptr(FuFirmware) firmware = NULL;
 	g_autoptr(GBytes) fw = NULL;
@@ -730,7 +735,7 @@ fu_plugin_device_read_firmware (FuPlugin *self, FuDevice *device, GError **error
 		G_CHECKSUM_SHA1,
 		G_CHECKSUM_SHA256,
 		0 };
-	locker = fu_device_locker_new (device, error);
+	locker = fu_device_locker_new (proxy, error);
 	if (locker == NULL)
 		return FALSE;
 	if (!fu_device_detach (device, error))
@@ -1219,6 +1224,7 @@ fu_plugin_runner_update_detach (FuPlugin *self, FuDevice *device, GError **error
 gboolean
 fu_plugin_runner_update_reload (FuPlugin *self, FuDevice *device, GError **error)
 {
+	FuDevice *proxy = fu_device_get_proxy_with_fallback (device);
 	g_autoptr(FuDeviceLocker) locker = NULL;
 
 	/* not enabled */
@@ -1226,7 +1232,7 @@ fu_plugin_runner_update_reload (FuPlugin *self, FuDevice *device, GError **error
 		return TRUE;
 
 	/* no object loaded */
-	locker = fu_device_locker_new (device, error);
+	locker = fu_device_locker_new (proxy, error);
 	if (locker == NULL)
 		return FALSE;
 	return fu_device_reload (device, error);
@@ -1407,6 +1413,7 @@ fu_plugin_check_supported_device (FuPlugin *self, FuDevice *device)
 static gboolean
 fu_plugin_backend_device_added (FuPlugin *self, FuDevice *device, GError **error)
 {
+	FuDevice *proxy;
 	FuPluginPrivate *priv = GET_PRIVATE (self);
 	GType device_gtype = fu_device_get_specialized_gtype (FU_DEVICE (device));
 	g_autoptr(FuDevice) dev = NULL;
@@ -1444,6 +1451,13 @@ fu_plugin_backend_device_added (FuPlugin *self, FuDevice *device, GError **error
 	}
 
 	/* open and add */
+	proxy = fu_device_get_proxy (device);
+	if (proxy != NULL) {
+		g_autoptr(FuDeviceLocker) locker_proxy = NULL;
+		locker_proxy = fu_device_locker_new (proxy, error);
+		if (locker_proxy == NULL)
+			return FALSE;
+	}
 	locker = fu_device_locker_new (dev, error);
 	if (locker == NULL)
 		return FALSE;

libfwupdplugin/fwupdplugin.map
@@ -829,6 +829,7 @@ LIBFWUPDPLUGIN_1.6.2 {
     fu_device_emit_request;
     fu_device_get_parent_physical_ids;
     fu_device_get_private_flags;
+    fu_device_get_proxy_with_fallback;
     fu_device_get_request_cnt;
     fu_device_has_parent_physical_id;
     fu_device_has_private_flag;

commit f75d68655700788857d3d26c64126879f86fb6f6

Fix a regression in flashing the Dell dock
This fixes a regression caused by 15b668c, we should have been more careful.

Fixes https://github.com/fwupd/fwupd/issues/3732

libfwupdplugin/fu-device.c
@@ -235,6 +235,8 @@ fu_device_internal_flag_to_string(FuDeviceInternalFlags flag)
 		return "use-parent-for-open";
 	if (flag == FU_DEVICE_INTERNAL_FLAG_USE_PARENT_FOR_BATTERY)
 		return "use-parent-for-battery";
+	if (flag == FU_DEVICE_INTERNAL_FLAG_USE_PROXY_FALLBACK)
+		return "use-proxy-fallback";
 	return NULL;
 }
 
@@ -287,6 +289,8 @@ fu_device_internal_flag_from_string(const gchar *flag)
 		return FU_DEVICE_INTERNAL_FLAG_USE_PARENT_FOR_OPEN;
 	if (g_strcmp0(flag, "use-parent-for-battery") == 0)
 		return FU_DEVICE_INTERNAL_FLAG_USE_PARENT_FOR_BATTERY;
+	if (g_strcmp0(flag, "use-proxy-fallback") == 0)
+		return FU_DEVICE_INTERNAL_FLAG_USE_PROXY_FALLBACK;
 	return FU_DEVICE_INTERNAL_FLAG_UNKNOWN;
 }
 
@@ -1092,7 +1096,8 @@ fu_device_get_proxy(FuDevice *self)
  * fu_device_get_proxy_with_fallback:
  * @self: a #FuDevice
  *
- * Gets the proxy device, falling back to the device itself.
+ * Gets the proxy device if %FU_DEVICE_INTERNAL_FLAG_USE_PROXY_FALLBACK is set, falling back to the
+ * device itself.
  *
  * Returns: (transfer none): a device
  *
@@ -1103,7 +1108,8 @@ fu_device_get_proxy_with_fallback(FuDevice *self)
 {
 	FuDevicePrivate *priv = GET_PRIVATE(self);
 	g_return_val_if_fail(FU_IS_DEVICE(self), NULL);
-	if (priv->proxy != NULL)
+	if (fu_device_has_internal_flag(self, FU_DEVICE_INTERNAL_FLAG_USE_PROXY_FALLBACK) &&
+	    priv->proxy != NULL)
 		return priv->proxy;
 	return self;
 }

libfwupdplugin/fu-device.h
@@ -378,6 +378,15 @@ typedef guint64 FuDeviceInternalFlags;
  */
 #define FU_DEVICE_INTERNAL_FLAG_USE_PARENT_FOR_BATTERY (1ull << 17)
 
+/**
+ * FU_DEVICE_INTERNAL_FLAG_USE_PROXY_FALLBACK:
+ *
+ * Use parent for the battery level and threshold.
+ *
+ * Since: 1.6.4
+ */
+#define FU_DEVICE_INTERNAL_FLAG_USE_PROXY_FALLBACK (1ull << 18)
+
 /* accessors */
 gchar *
 fu_device_to_string(FuDevice *self);

plugins/vli/fu-vli-usbhub-device.c
@@ -1128,6 +1128,7 @@ fu_vli_usbhub_device_init(FuVliUsbhubDevice *self)
 {
 	fu_device_add_icon(FU_DEVICE(self), "audio-card");
 	fu_device_add_protocol(FU_DEVICE(self), "com.vli.usbhub");
+	fu_device_add_internal_flag(FU_DEVICE(self), FU_DEVICE_INTERNAL_FLAG_USE_PROXY_FALLBACK);
 	fu_device_set_remove_delay(FU_DEVICE(self), FU_DEVICE_REMOVE_DELAY_RE_ENUMERATE);
 	fu_device_register_private_flag(FU_DEVICE(self),
 					FU_VLI_USBHUB_DEVICE_FLAG_ATTACH_WITH_GPIOB,