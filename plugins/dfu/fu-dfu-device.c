fwupd/fwupd

commit aaa77c6f513d65c38d70b95447460c8ed220dca5

Allow adding and removing custom flags on devices
The CustomFlags feature is a bit of a hack where we just join the flags
and store in the device metadata section as a string. This makes it
inefficient to check if just one flag exists as we have to split the
string to a temporary array each time.

Rather than adding to the hack by splitting, appending (if not exists)
then joining again, store the flags in the plugin privdata directly.

This allows us to support negating custom properties (e.g. ~hint) and
also allows quirks to append custom values without duplicating them on
each GUID match, e.g.

[USB\VID_17EF&PID_307F]
Plugin = customflag1
[USB\VID_17EF&PID_307F&HUB_0002]
Flags = customflag2

...would result in customflag1,customflag2 which is the same as you'd
get from an enumerated device flag doing the same thing.

plugins/dfu/fu-dfu-device.c
@@ -20,38 +20,6 @@
  */
 
 /**
- * FU_QUIRKS_DFU_FLAGS:
- * @key: the USB device ID, e.g. `USB\VID_0763&PID_2806`
- * @value: a string, separated using `|`, e.g. `ignore-polltimeout|no-pid-change`
- *
- * Assigns optional quirks to use for a DFU device which does not follow the
- * DFU 1.0 or 1.1 specification. The list of supported quirks is thus:
- *
- * * `none`:			No device quirks
- * * `attach-upload-download`:	An upload or download is required for attach
- * * `force-dfu-mode`:		Force DFU mode
- * * `ignore-polltimeout`:	Ignore the device download timeout
- * * `ignore-runtime`:		Device has broken DFU runtime support
- * * `ignore-upload`:		Uploading from the device is broken
- * * `no-dfu-runtime`:		No DFU runtime interface is provided
- * * `no-get-status-upload`:	Do not do GetStatus when uploading
- * * `no-pid-change`:		Accept the same VID:PID when changing modes
- * * `use-any-interface`:	Use any interface for DFU
- * * `use-atmel-avr`:		Device uses the ATMEL bootloader
- * * `use-protocol-zero`:	Fix up the protocol number
- * * `legacy-protocol`:		Use a legacy protocol version
- * * `detach-for-attach`:	Requires a FU_DFU_REQUEST_DETACH to attach
- * * `absent-sector-size`:	In absence of sector size, assume byte
- * * `manifest-poll`:		Requires polling via GetStatus in dfuManifest state
- * * `no-bus-reset-attach`:	Do not require a bus reset to attach to normal
- *
- * Default value: `none`
- *
- * Since: 1.0.1
- */
-#define	FU_QUIRKS_DFU_FLAGS			"Flags"
-
-/**
  * FU_QUIRKS_DFU_FORCE_VERSION:
  * @key: the USB device ID, e.g. `USB\VID_0763&PID_2806`
  * @value: the uint16_t DFU version, encoded in base 16, e.g. `0110`
@@ -259,7 +227,7 @@ static void
 fu_dfu_device_guess_state_from_iface (FuDfuDevice *self, GUsbInterface *iface)
 {
 	/* some devices use the wrong interface */
-	if (fu_device_has_custom_flag (FU_DEVICE (self), "force-dfu-mode")) {
+	if (fu_device_has_private_flag (FU_DEVICE (self), FU_DFU_DEVICE_FLAG_FORCE_DFU_MODE)) {
 		g_debug ("quirking device into DFU mode");
 		fu_dfu_device_set_state (self, FU_DFU_STATE_DFU_IDLE);
 		return;
@@ -300,7 +268,7 @@ fu_dfu_device_add_targets (FuDfuDevice *self, GError **error)
 		GUsbInterface *iface = g_ptr_array_index (ifaces, i);
 
 		/* some devices don't use the right class and subclass */
-		if (!fu_device_has_custom_flag (FU_DEVICE (self), "use-any-interface")) {
+		if (fu_device_has_private_flag (FU_DEVICE (self), FU_DFU_DEVICE_FLAG_USE_ANY_INTERFACE)) {
 			if (g_usb_interface_get_class (iface) != G_USB_DEVICE_CLASS_APPLICATION_SPECIFIC)
 				continue;
 			if (g_usb_interface_get_subclass (iface) != 0x01)
@@ -386,7 +354,7 @@ fu_dfu_device_add_targets (FuDfuDevice *self, GError **error)
 
 	/* save for reset */
 	if (priv->state == FU_DFU_STATE_APP_IDLE ||
-	    fu_device_has_custom_flag (FU_DEVICE (self), "no-pid-change")) {
+	    fu_device_has_private_flag (FU_DEVICE (self), FU_DFU_DEVICE_FLAG_NO_PID_CHANGE)) {
 		priv->runtime_vid = g_usb_device_get_vid (usb_device);
 		priv->runtime_pid = g_usb_device_get_pid (usb_device);
 		priv->runtime_release = g_usb_device_get_release (usb_device);
@@ -394,7 +362,7 @@ fu_dfu_device_add_targets (FuDfuDevice *self, GError **error)
 
 	/* the device has no DFU runtime, so cheat */
 	if (priv->targets->len == 0 &&
-	    fu_device_has_custom_flag (FU_DEVICE (self), "no-dfu-runtime")) {
+	    fu_device_has_private_flag (FU_DEVICE (self), FU_DFU_DEVICE_FLAG_NO_DFU_RUNTIME)) {
 		g_debug ("no DFU runtime, so faking device");
 		fu_dfu_device_set_state (self, FU_DFU_STATE_APP_IDLE);
 		priv->iface_number = 0xff;
@@ -416,7 +384,7 @@ fu_dfu_device_add_targets (FuDfuDevice *self, GError **error)
 	}
 
 	/* the device upload is broken */
-	if (fu_device_has_custom_flag (FU_DEVICE (self), "ignore-upload"))
+	if (fu_device_has_private_flag (FU_DEVICE (self), FU_DFU_DEVICE_FLAG_IGNORE_UPLOAD))
 		priv->attributes &= ~FU_DFU_DEVICE_ATTR_CAN_UPLOAD;
 
 	return TRUE;
@@ -863,7 +831,7 @@ fu_dfu_device_refresh (FuDfuDevice *self, GError **error)
 
 	/* the device has no DFU runtime, so cheat */
 	if (priv->state == FU_DFU_STATE_APP_IDLE &&
-	    fu_device_has_custom_flag (FU_DEVICE (self), "no-dfu-runtime"))
+	    fu_device_has_private_flag (FU_DEVICE (self), FU_DFU_DEVICE_FLAG_NO_DFU_RUNTIME))
 		return TRUE;
 
 	/* ensure interface is claimed */
@@ -906,7 +874,7 @@ fu_dfu_device_refresh (FuDfuDevice *self, GError **error)
 	}
 
 	/* some devices use the wrong state value */
-	if (fu_device_has_custom_flag (FU_DEVICE (self), "force-dfu-mode") &&
+	if (fu_device_has_private_flag (FU_DEVICE (self), FU_DFU_DEVICE_FLAG_FORCE_DFU_MODE) &&
 	    fu_dfu_device_get_state (self) != FU_DFU_STATE_DFU_IDLE) {
 		g_debug ("quirking device into DFU mode");
 		fu_dfu_device_set_state (self, FU_DFU_STATE_DFU_IDLE);
@@ -916,7 +884,7 @@ fu_dfu_device_refresh (FuDfuDevice *self, GError **error)
 
 	/* status or state changed */
 	fu_dfu_device_set_status (self, buf[0]);
-	if (fu_device_has_custom_flag (FU_DEVICE (self), "ignore-polltimeout")) {
+	if (fu_device_has_private_flag (FU_DEVICE (self), FU_DFU_DEVICE_FLAG_IGNORE_POLLTIMEOUT)) {
 		priv->dnload_timeout = DFU_DEVICE_DNLOAD_TIMEOUT_DEFAULT;
 	} else {
 		priv->dnload_timeout = buf[1] +
@@ -1011,7 +979,7 @@ fu_dfu_device_detach (FuDevice *device, GError **error)
 
 	/* the device has no DFU runtime, so cheat */
 	if (priv->state == FU_DFU_STATE_APP_IDLE &&
-	    fu_device_has_custom_flag (FU_DEVICE (self), "no-dfu-runtime"))
+	    fu_device_has_private_flag (FU_DEVICE (self), FU_DFU_DEVICE_FLAG_NO_DFU_RUNTIME))
 		return TRUE;
 
 	/* ensure interface is claimed */
@@ -1068,7 +1036,7 @@ fu_dfu_device_abort (FuDfuDevice *self, GError **error)
 
 	/* the device has no DFU runtime, so cheat */
 	if (priv->state == FU_DFU_STATE_APP_IDLE &&
-	    fu_device_has_custom_flag (FU_DEVICE (self), "no-dfu-runtime")) {
+	    fu_device_has_private_flag (FU_DEVICE (self), FU_DFU_DEVICE_FLAG_NO_DFU_RUNTIME)) {
 		g_set_error_literal (error,
 				     FWUPD_ERROR,
 				     FWUPD_ERROR_NOT_SUPPORTED,
@@ -1135,7 +1103,7 @@ fu_dfu_device_clear_status (FuDfuDevice *self, GError **error)
 
 	/* the device has no DFU runtime, so cheat */
 	if (priv->state == FU_DFU_STATE_APP_IDLE &&
-	    fu_device_has_custom_flag (FU_DEVICE (self), "no-dfu-runtime")) {
+	    fu_device_has_private_flag (FU_DEVICE (self), FU_DFU_DEVICE_FLAG_NO_DFU_RUNTIME)) {
 		g_set_error_literal (error,
 				     FWUPD_ERROR,
 				     FWUPD_ERROR_NOT_SUPPORTED,
@@ -1209,14 +1177,14 @@ fu_dfu_device_open (FuDevice *device, GError **error)
 
 	/* the device has no DFU runtime, so cheat */
 	if (priv->state == FU_DFU_STATE_APP_IDLE &&
-	    fu_device_has_custom_flag (device, "no-dfu-runtime")) {
+	    fu_device_has_private_flag (FU_DEVICE (self), FU_DFU_DEVICE_FLAG_NO_DFU_RUNTIME)) {
 		fu_dfu_device_set_state (self, FU_DFU_STATE_APP_IDLE);
 		priv->status = FU_DFU_STATUS_OK;
 	}
 
 	/* GD32VF103 encodes the serial number in UTF-8 (rather than UTF-16)
 	 * and also uses the first two bytes as the model identifier */
-	if (fu_device_has_custom_flag (FU_DEVICE (device), "gd32")) {
+	if (fu_device_has_private_flag (FU_DEVICE (self), FU_DFU_DEVICE_FLAG_GD32)) {
 #if G_USB_CHECK_VERSION(0,3,6)
 		GUsbDevice *usb_device = fu_usb_device_get_dev (FU_USB_DEVICE (device));
 		const guint8 *buf;
@@ -1335,7 +1303,7 @@ fu_dfu_device_probe (FuDevice *device, GError **error)
 
 	/* hardware from Jabra literally reboots if you try to retry a failed
 	 * write -- there's no way to avoid blocking the daemon like this... */
-	if (fu_device_has_internal_flag (device, FU_DEVICE_INTERNAL_FLAG_ATTACH_EXTRA_RESET))
+	if (fu_device_has_private_flag (FU_DEVICE (self), FU_DFU_DEVICE_FLAG_ATTACH_EXTRA_RESET))
 		fu_device_sleep_with_progress (device, 10); /* seconds */
 
 	/* success */
@@ -1404,7 +1372,7 @@ fu_dfu_device_attach (FuDevice *device, GError **error)
 	fu_device_set_status (device, FWUPD_STATUS_DEVICE_RESTART);
 
 	/* handle weirdness */
-	if (fu_device_has_custom_flag (device, "detach-for-attach")) {
+	if (fu_device_has_private_flag (FU_DEVICE (self), FU_DFU_DEVICE_FLAG_DETACH_FOR_ATTACH)) {
 		if (!fu_dfu_device_request_detach (self, error))
 			return FALSE;
 		fu_device_set_status (device, FWUPD_STATUS_IDLE);
@@ -1414,7 +1382,7 @@ fu_dfu_device_attach (FuDevice *device, GError **error)
 
 	/* handle m-stack DFU bootloaders */
 	if (!priv->done_upload_or_download &&
-	    fu_device_has_custom_flag (device, "attach-upload-download")) {
+	    fu_device_has_private_flag (FU_DEVICE (self), FU_DFU_DEVICE_FLAG_ATTACH_UPLOAD_DOWNLOAD)) {
 		g_autoptr(GBytes) chunk = NULL;
 		g_autoptr(FuDfuTarget) target_zero = NULL;
 		g_debug ("doing dummy upload to work around m-stack quirk");
@@ -1432,7 +1400,7 @@ fu_dfu_device_attach (FuDevice *device, GError **error)
 		return FALSE;
 
 	/* normal DFU mode just needs a bus reset */
-	if (fu_device_has_custom_flag (device, "no-bus-reset-attach") &&
+	if (fu_device_has_private_flag (FU_DEVICE (self), FU_DFU_DEVICE_FLAG_NO_BUS_RESET_ATTACH) &&
 	    fu_dfu_device_has_attribute (self, FU_DFU_DEVICE_ATTR_WILL_DETACH))
 		g_debug ("Bus reset is not required. Device will reboot to normal");
 	else if (!fu_dfu_target_attach (target, error))
@@ -1942,4 +1910,59 @@ fu_dfu_device_init (FuDfuDevice *self)
 	fu_device_add_flag (FU_DEVICE (self), FWUPD_DEVICE_FLAG_ADD_COUNTERPART_GUIDS);
 	fu_device_add_internal_flag (FU_DEVICE (self), FU_DEVICE_INTERNAL_FLAG_REPLUG_MATCH_GUID);
 	fu_device_set_remove_delay (FU_DEVICE (self), FU_DEVICE_REMOVE_DELAY_RE_ENUMERATE);
+
+	fu_device_register_private_flag (FU_DEVICE (self),
+					 FU_DFU_DEVICE_FLAG_ATTACH_EXTRA_RESET,
+					 "attach-extra-reset");
+	fu_device_register_private_flag (FU_DEVICE (self),
+					 FU_DFU_DEVICE_FLAG_ATTACH_UPLOAD_DOWNLOAD,
+					 "attach-upload-download");
+	fu_device_register_private_flag (FU_DEVICE (self),
+					 FU_DFU_DEVICE_FLAG_FORCE_DFU_MODE,
+					 "force-dfu-mode");
+	fu_device_register_private_flag (FU_DEVICE (self),
+					 FU_DFU_DEVICE_FLAG_IGNORE_POLLTIMEOUT,
+					 "ignore-polltimeout");
+	fu_device_register_private_flag (FU_DEVICE (self),
+					 FU_DFU_DEVICE_FLAG_IGNORE_RUNTIME,
+					 "ignore-runtime");
+	fu_device_register_private_flag (FU_DEVICE (self),
+					 FU_DFU_DEVICE_FLAG_IGNORE_UPLOAD,
+					 "ignore-upload");
+	fu_device_register_private_flag (FU_DEVICE (self),
+					 FU_DFU_DEVICE_FLAG_NO_DFU_RUNTIME,
+					 "no-dfu-runtime");
+	fu_device_register_private_flag (FU_DEVICE (self),
+					 FU_DFU_DEVICE_FLAG_NO_GET_STATUS_UPLOAD,
+					 "no-get-status-upload");
+	fu_device_register_private_flag (FU_DEVICE (self),
+					 FU_DFU_DEVICE_FLAG_NO_PID_CHANGE,
+					 "no-pid-change");
+	fu_device_register_private_flag (FU_DEVICE (self),
+					 FU_DFU_DEVICE_FLAG_USE_ANY_INTERFACE,
+					 "use-any-interface");
+	fu_device_register_private_flag (FU_DEVICE (self),
+					 FU_DFU_DEVICE_FLAG_USE_ATMEL_AVR,
+					 "use-atmel-avr");
+	fu_device_register_private_flag (FU_DEVICE (self),
+					 FU_DFU_DEVICE_FLAG_USE_PROTOCOL_ZERO,
+					 "use-protocol-zero");
+	fu_device_register_private_flag (FU_DEVICE (self),
+					 FU_DFU_DEVICE_FLAG_LEGACY_PROTOCOL,
+					 "legacy-protocol");
+	fu_device_register_private_flag (FU_DEVICE (self),
+					 FU_DFU_DEVICE_FLAG_DETACH_FOR_ATTACH,
+					 "detach-for-attach");
+	fu_device_register_private_flag (FU_DEVICE (self),
+					 FU_DFU_DEVICE_FLAG_ABSENT_SECTOR_SIZE,
+					 "absent-sector-size");
+	fu_device_register_private_flag (FU_DEVICE (self),
+					 FU_DFU_DEVICE_FLAG_MANIFEST_POLL,
+					 "manifest-poll");
+	fu_device_register_private_flag (FU_DEVICE (self),
+					 FU_DFU_DEVICE_FLAG_NO_BUS_RESET_ATTACH,
+					 "no-bus-reset-attach");
+	fu_device_register_private_flag (FU_DEVICE (self),
+					 FU_DFU_DEVICE_FLAG_GD32,
+					 "gd32");
 }

libfwupdplugin/fu-device-private.h
@@ -37,3 +37,6 @@ gchar		*fu_device_get_guids_as_str		(FuDevice	*self);
 GPtrArray	*fu_device_get_possible_plugins		(FuDevice	*self);
 void		 fu_device_add_possible_plugin		(FuDevice	*self,
 							 const gchar	*plugin);
+guint64		 fu_device_get_private_flags		(FuDevice	*self);
+void		 fu_device_set_private_flags		(FuDevice	*self,
+							 guint64	 flag);

libfwupdplugin/fu-device.c
@@ -70,6 +70,8 @@ typedef struct {
 	GPtrArray			*retry_recs;	/* of FuDeviceRetryRecovery */
 	guint				 retry_delay;
 	FuDeviceInternalFlags		 internal_flags;
+	guint64				 private_flags;
+	GPtrArray			*private_flag_items;	/* (nullable) */
 } FuDevicePrivate;
 
 typedef struct {
@@ -311,6 +313,92 @@ fu_device_has_internal_flag (FuDevice *self, FuDeviceInternalFlags flag)
 	FuDevicePrivate *priv = GET_PRIVATE (self);
 	g_return_val_if_fail (FU_IS_DEVICE (self), FALSE);
 	return (priv->internal_flags & flag) > 0;
+}
+
+/**
+ * fu_device_add_private_flag:
+ * @self: a #FuDevice
+ * @flag: a device flag
+ *
+ * Adds a private flag that can be used by the plugin for any purpose.
+ *
+ * Since: 1.6.2
+ **/
+void
+fu_device_add_private_flag (FuDevice *self, guint64 flag)
+{
+	FuDevicePrivate *priv = GET_PRIVATE (self);
+	g_return_if_fail (FU_IS_DEVICE (self));
+	priv->private_flags |= flag;
+}
+
+/**
+ * fu_device_remove_private_flag:
+ * @self: a #FuDevice
+ * @flag: a device flag
+ *
+ * Removes a private flag that can be used by the plugin for any purpose.
+ *
+ * Since: 1.6.2
+ **/
+void
+fu_device_remove_private_flag (FuDevice *self, guint64 flag)
+{
+	FuDevicePrivate *priv = GET_PRIVATE (self);
+	g_return_if_fail (FU_IS_DEVICE (self));
+	priv->private_flags &= ~flag;
+}
+
+/**
+ * fu_device_has_private_flag:
+ * @self: a #FuDevice
+ * @flag: a device flag
+ *
+ * Tests for a private flag that can be used by the plugin for any purpose.
+ *
+ * Since: 1.6.2
+ **/
+gboolean
+fu_device_has_private_flag (FuDevice *self, guint64 flag)
+{
+	FuDevicePrivate *priv = GET_PRIVATE (self);
+	g_return_val_if_fail (FU_IS_DEVICE (self), FALSE);
+	return (priv->private_flags & flag) > 0;
+}
+
+/**
+ * fu_device_get_private_flags:
+ * @self: a #FuDevice
+ *
+ * Returns all the private flags that can be used by the plugin for any purpose.
+ *
+ * Returns: flags
+ *
+ * Since: 1.6.2
+ **/
+guint64
+fu_device_get_private_flags (FuDevice *self)
+{
+	FuDevicePrivate *priv = GET_PRIVATE (self);
+	g_return_val_if_fail (FU_IS_DEVICE (self), G_MAXUINT64);
+	return priv->private_flags;
+}
+
+/**
+ * fu_device_set_private_flags:
+ * @self: a #FuDevice
+ * @flag: flags
+ *
+ * Sets private flags that can be used by the plugin for any purpose.
+ *
+ * Since: 1.6.2
+ **/
+void
+fu_device_set_private_flags (FuDevice *self, guint64 flag)
+{
+	FuDevicePrivate *priv = GET_PRIVATE (self);
+	g_return_if_fail (FU_IS_DEVICE (self));
+	priv->private_flags = flag;
 }
 
 /**
@@ -2568,30 +2656,124 @@ fu_device_add_flag (FuDevice *self, FwupdDeviceFlags flag)
 		fu_device_inhibit (self, "needs-activation", "Pending activation");
 }
 
+typedef struct {
+	guint64		 value;
+	gchar		*value_str;
+} FuDevicePrivateFlagItem;
+
+static void
+fu_device_private_flag_item_free (FuDevicePrivateFlagItem *item)
+{
+	g_free (item->value_str);
+	g_free (item);
+}
+
+static FuDevicePrivateFlagItem *
+fu_device_private_flag_item_find_by_str (FuDevice *self, const gchar *value_str)
+{
+	FuDevicePrivate *priv = GET_PRIVATE (self);
+	if (priv->private_flag_items == NULL)
+		return NULL;
+	for (guint i = 0; i < priv->private_flag_items->len; i++) {
+		FuDevicePrivateFlagItem *item = g_ptr_array_index (priv->private_flag_items, i);
+		if (g_strcmp0 (item->value_str, value_str) == 0)
+			return item;
+	}
+	return NULL;
+}
+
+static FuDevicePrivateFlagItem *
+fu_device_private_flag_item_find_by_val (FuDevice *self, guint64 value)
+{
+	FuDevicePrivate *priv = GET_PRIVATE (self);
+	if (priv->private_flag_items == NULL)
+		return NULL;
+	for (guint i = 0; i < priv->private_flag_items->len; i++) {
+		FuDevicePrivateFlagItem *item = g_ptr_array_index (priv->private_flag_items, i);
+		if (item->value == value)
+			return item;
+	}
+	return NULL;
+}
+
+/**
+ * fu_device_register_private_flag:
+ * @self: a #FuDevice
+ * @value: an integer value
+ * @value_str: a string that represents @value
+ *
+ * Registers a private device flag so that it can be set from quirk files.
+ *
+ * Since: 1.6.2
+ **/
+void
+fu_device_register_private_flag (FuDevice *self,
+				 guint64 value,
+				 const gchar *value_str)
+{
+	FuDevicePrivateFlagItem *item;
+	FuDevicePrivate *priv = GET_PRIVATE (self);
+
+	g_return_if_fail (FU_IS_DEVICE (self));
+	g_return_if_fail (value != 0);
+	g_return_if_fail (value_str != NULL);
+
+	/* ensure exists */
+	if (priv->private_flag_items == NULL)
+		priv->private_flag_items = g_ptr_array_new_with_free_func ((GDestroyNotify) fu_device_private_flag_item_free);
+
+	item = g_new0 (FuDevicePrivateFlagItem, 1);
+	item->value = value;
+	item->value_str = g_strdup (value_str);
+	g_ptr_array_add (priv->private_flag_items, item);
+}
+
 static void
 fu_device_set_custom_flag (FuDevice *self, const gchar *hint)
 {
 	FwupdDeviceFlags flag;
+	FuDevicePrivateFlagItem *item;
 	FuDeviceInternalFlags internal_flag;
+	FuDevicePrivate *priv = GET_PRIVATE (self);
 
 	/* is this a negated device flag */
 	if (g_str_has_prefix (hint, "~")) {
 		flag = fwupd_device_flag_from_string (hint + 1);
-		if (flag != FWUPD_DEVICE_FLAG_UNKNOWN)
+		if (flag != FWUPD_DEVICE_FLAG_UNKNOWN) {
 			fu_device_remove_flag (self, flag);
+			return;
+		}
 		internal_flag = fu_device_internal_flag_from_string (hint + 1);
-		if (internal_flag != FU_DEVICE_INTERNAL_FLAG_UNKNOWN)
+		if (internal_flag != FU_DEVICE_INTERNAL_FLAG_UNKNOWN) {
 			fu_device_remove_internal_flag (self, internal_flag);
+			return;
+		}
+		item = fu_device_private_flag_item_find_by_str (self, hint + 1);
+		if (item != NULL) {
+			priv->private_flags &= ~item->value;
+			return;
+		}
+		g_debug ("failed to find registered custom flag %s", hint + 1);
 		return;
 	}
 
 	/* is this a known device flag */
 	flag = fwupd_device_flag_from_string (hint);
-	if (flag != FWUPD_DEVICE_FLAG_UNKNOWN)
+	if (flag != FWUPD_DEVICE_FLAG_UNKNOWN) {
 		fu_device_add_flag (self, flag);
+		return;
+	}
 	internal_flag = fu_device_internal_flag_from_string (hint);
-	if (internal_flag != FU_DEVICE_INTERNAL_FLAG_UNKNOWN)
+	if (internal_flag != FU_DEVICE_INTERNAL_FLAG_UNKNOWN) {
 		fu_device_add_internal_flag (self, internal_flag);
+		return;
+	}
+	item = fu_device_private_flag_item_find_by_str (self, hint);
+	if (item != NULL) {
+		priv->private_flags |= item->value;
+		return;
+	}
+	g_debug ("failed to find registered custom flag %s", hint);
 }
 
 /**
@@ -3008,6 +3190,22 @@ fu_device_add_string (FuDevice *self, guint idt, GString *str)
 		if (tmp2->len > 0)
 			g_string_truncate (tmp2, tmp2->len - 1);
 		fu_common_string_append_kv (str, idt + 1, "InternalFlags", tmp2->str);
+	}
+	if (priv->private_flags > 0) {
+		g_autoptr(GPtrArray) tmpv = g_ptr_array_new ();
+		g_autofree gchar *tmps = NULL;
+		for (guint64 i = 0; i < 64; i++) {
+			FuDevicePrivateFlagItem *item;
+			guint64 value = 1ull << i;
+			if ((priv->private_flags & value) == 0)
+				continue;
+			item = fu_device_private_flag_item_find_by_val (self, value);
+			if (item == NULL)
+				continue;
+			g_ptr_array_add (tmpv, item->value_str);
+		}
+		tmps = fu_common_strjoin_array (",", tmpv);
+		fu_common_string_append_kv (str, idt + 1, "PrivateFlags", tmps);
 	}
 
 	/* subclassed */
@@ -4166,6 +4364,8 @@ fu_device_finalize (GObject *object)
 		g_hash_table_unref (priv->inhibits);
 	if (priv->parent_physical_ids != NULL)
 		g_ptr_array_unref (priv->parent_physical_ids);
+	if (priv->private_flag_items != NULL)
+		g_ptr_array_unref (priv->private_flag_items);
 	g_ptr_array_unref (priv->parent_guids);
 	g_ptr_array_unref (priv->possible_plugins);
 	g_ptr_array_unref (priv->retry_recs);

libfwupdplugin/fu-device.h
@@ -339,7 +339,8 @@ void		 fu_device_remove_flag			(FuDevice	*self,
 							 FwupdDeviceFlags flag);
 const gchar	*fu_device_get_custom_flags		(FuDevice	*self);
 gboolean	 fu_device_has_custom_flag		(FuDevice	*self,
-							 const gchar	*hint);
+							 const gchar	*hint)
+							 G_DEPRECATED_FOR(fu_device_has_private_flag);
 void		 fu_device_set_custom_flags		(FuDevice	*self,
 							 const gchar	*custom_flags);
 void		 fu_device_set_name			(FuDevice	*self,
@@ -476,3 +477,12 @@ GHashTable	*fu_device_report_metadata_pre		(FuDevice	*self);
 GHashTable	*fu_device_report_metadata_post		(FuDevice	*self);
 void		 fu_device_add_security_attrs		(FuDevice	*self,
 							 FuSecurityAttrs *attrs);
+void		 fu_device_register_private_flag	(FuDevice	*self,
+							 guint64	 value,
+							 const gchar	*value_str);
+void		 fu_device_add_private_flag		(FuDevice	*self,
+							 guint64	 flag);
+void		 fu_device_remove_private_flag		(FuDevice	*self,
+							 guint64	 flag);
+gboolean	 fu_device_has_private_flag		(FuDevice	*self,
+							 guint64	 flag);

libfwupdplugin/fu-self-test.c
@@ -1390,6 +1390,39 @@ fu_device_inhibit_func (void)
 	g_assert_false (fu_device_has_flag (device, FWUPD_DEVICE_FLAG_UPDATABLE_HIDDEN));
 }
 
+#define TEST_FLAG_FOO	(1 << 0)
+#define TEST_FLAG_BAR	(1 << 1)
+#define TEST_FLAG_BAZ	(1 << 2)
+
+static void
+fu_device_private_flags_func (void)
+{
+	g_autofree gchar *tmp = NULL;
+	g_autoptr(FuDevice) device = fu_device_new ();
+
+	fu_device_register_private_flag (device, TEST_FLAG_FOO, "foo");
+	fu_device_register_private_flag (device, TEST_FLAG_BAR, "bar");
+
+	fu_device_set_custom_flags (device, "foo");
+	g_assert_cmpint (fu_device_get_private_flags (device), ==, TEST_FLAG_FOO);
+	fu_device_set_custom_flags (device, "bar");
+	g_assert_cmpint (fu_device_get_private_flags (device), ==, TEST_FLAG_FOO | TEST_FLAG_BAR);
+	fu_device_set_custom_flags (device, "~bar");
+	g_assert_cmpint (fu_device_get_private_flags (device), ==, TEST_FLAG_FOO);
+	fu_device_set_custom_flags (device, "baz");
+	g_assert_cmpint (fu_device_get_private_flags (device), ==, TEST_FLAG_FOO);
+	fu_device_add_private_flag (device, TEST_FLAG_BAZ);
+	g_assert_cmpint (fu_device_get_private_flags (device), ==, TEST_FLAG_FOO | TEST_FLAG_BAZ);
+
+	tmp = fu_device_to_string (device);
+	g_assert_cmpstr (tmp, ==,
+		"FuDevice:\n"
+		"Unknown Device\n"
+		"  Flags:                none\n"
+		"  CustomFlags:          baz\n" /* compat */
+		"  PrivateFlags:         foo\n");
+}
+
 static void
 fu_device_flags_func (void)
 {
@@ -2942,6 +2975,7 @@ main (int argc, char **argv)
 	g_test_add_func ("/fwupd/device{instance-ids}", fu_device_instance_ids_func);
 	g_test_add_func ("/fwupd/device{composite-id}", fu_device_composite_id_func);
 	g_test_add_func ("/fwupd/device{flags}", fu_device_flags_func);
+	g_test_add_func ("/fwupd/device{custom-flags}", fu_device_private_flags_func);
 	g_test_add_func ("/fwupd/device{inhibit}", fu_device_inhibit_func);
 	g_test_add_func ("/fwupd/device{parent}", fu_device_parent_func);
 	g_test_add_func ("/fwupd/device{children}", fu_device_children_func);

libfwupdplugin/fwupdplugin.map
@@ -820,7 +820,13 @@ LIBFWUPDPLUGIN_1.6.2 {
   global:
     fu_common_check_kernel_version;
     fu_device_add_parent_physical_id;
+    fu_device_add_private_flag;
     fu_device_get_parent_physical_ids;
+    fu_device_get_private_flags;
     fu_device_has_parent_physical_id;
+    fu_device_has_private_flag;
+    fu_device_register_private_flag;
+    fu_device_remove_private_flag;
+    fu_device_set_private_flags;
   local: *;
 } LIBFWUPDPLUGIN_1.6.1;

plugins/ccgx/fu-ccgx-dmc-device.c
@@ -26,6 +26,14 @@ struct _FuCcgxDmcDevice {
 	DmcUpdateModel		update_model;
 };
 
+
+/**
+ * FU_CCGX_DMC_DEVICE_FLAG_HAS_MANUAL_REPLUG:
+ *
+ * Needs a manual replug from the end-user.
+ */
+#define FU_CCGX_DMC_DEVICE_FLAG_HAS_MANUAL_REPLUG		(1 << 0)
+
 G_DEFINE_TYPE (FuCcgxDmcDevice, fu_ccgx_dmc_device, FU_TYPE_USB_DEVICE)
 
 static gboolean
@@ -541,10 +549,9 @@ static gboolean
 fu_ccgx_dmc_device_attach (FuDevice *device, GError **error)
 {
 	FuCcgxDmcDevice *self = FU_CCGX_DMC_DEVICE (device);
-	gboolean manual_replug = FALSE;
+	gboolean manual_replug;
 
-	if (fu_device_has_custom_flag (device, "has-manual-replug"))
-		manual_replug = TRUE;
+	manual_replug = fu_device_has_private_flag (device, FU_CCGX_DMC_DEVICE_FLAG_HAS_MANUAL_REPLUG);
 
 	if (fu_device_get_update_state (self) != FWUPD_UPDATE_STATE_SUCCESS)
 		return TRUE;
@@ -657,6 +664,9 @@ fu_ccgx_dmc_device_init (FuCcgxDmcDevice *self)
 	fu_device_add_flag (FU_DEVICE (self), FWUPD_DEVICE_FLAG_DUAL_IMAGE);
 	fu_device_add_flag (FU_DEVICE (self), FWUPD_DEVICE_FLAG_SELF_RECOVERY);
 	fu_device_add_internal_flag (FU_DEVICE (self), FU_DEVICE_INTERNAL_FLAG_REPLUG_MATCH_GUID);
+	fu_device_register_private_flag (FU_DEVICE (self),
+					 FU_CCGX_DMC_DEVICE_FLAG_HAS_MANUAL_REPLUG,
+					 "has-manual-replug");
 }
 
 static void

plugins/colorhug/fu-colorhug-device.c
@@ -20,7 +20,7 @@
  *
  * Since: 1.0.3
  */
-#define FU_COLORHUG_DEVICE_FLAG_HALFSIZE	"halfsize"
+#define FU_COLORHUG_DEVICE_FLAG_HALFSIZE	(1 << 0)
 
 struct _FuColorhugDevice {
 	FuUsbDevice		 parent_instance;
@@ -321,7 +321,7 @@ fu_colorhug_device_probe (FuDevice *device, GError **error)
 		return FALSE;
 
 	/* compact memory layout */
-	if (fu_device_has_custom_flag (device, FU_COLORHUG_DEVICE_FLAG_HALFSIZE))
+	if (fu_device_has_private_flag (device, FU_COLORHUG_DEVICE_FLAG_HALFSIZE))
 		self->start_addr = CH_EEPROM_ADDR_RUNCODE_ALS;
 
 	/* add hardcoded bits */
@@ -534,6 +534,9 @@ fu_colorhug_device_init (FuColorhugDevice *self)
 				    FU_DEVICE_REMOVE_DELAY_RE_ENUMERATE);
 	fu_device_add_flag (FU_DEVICE (self), FWUPD_DEVICE_FLAG_ADD_COUNTERPART_GUIDS);
 	fu_device_add_internal_flag (FU_DEVICE (self), FU_DEVICE_INTERNAL_FLAG_REPLUG_MATCH_GUID);
+	fu_device_register_private_flag (FU_DEVICE (self),
+					 FU_COLORHUG_DEVICE_FLAG_HALFSIZE,
+					 "halfsize");
 }
 
 static void

commit 6a4d31f224b7bdd2b8af5a4006d16ccd8130e2b4

dfu: Fix all devices that rely on use-any-interface
This was a regression caused in aaa77c6f513d65c38d70b95447460c8ed220dca5.

plugins/dfu/fu-dfu-device.c
@@ -274,8 +274,8 @@ fu_dfu_device_add_targets(FuDfuDevice *self, GError **error)
 		GUsbInterface *iface = g_ptr_array_index(ifaces, i);
 
 		/* some devices don't use the right class and subclass */
-		if (fu_device_has_private_flag(FU_DEVICE(self),
-					       FU_DFU_DEVICE_FLAG_USE_ANY_INTERFACE)) {
+		if (!fu_device_has_private_flag(FU_DEVICE(self),
+						FU_DFU_DEVICE_FLAG_USE_ANY_INTERFACE)) {
 			if (g_usb_interface_get_class(iface) !=
 			    G_USB_DEVICE_CLASS_APPLICATION_SPECIFIC)
 				continue;