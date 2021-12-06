fwupd/fwupd

commit 5d337e72a68d3ead3f769e312ace7dbb9e97c887

vli: fix VL819, add placeholder VL821-VL822

plugins/vli/fu-vli-usbhub-firmware.c
@@ -57,6 +57,7 @@ fu_vli_usbhub_firmware_parse (FuFirmware *firmware,
 	guint16 adr_ofs = 0;
 	guint16 version = 0x0;
 	guint8 tmp = 0x0;
+	guint8 fwtype = 0x0;
 	const guint8 *buf = g_bytes_get_data (fw, &bufsz);
 
 	/* map into header */
@@ -174,16 +175,67 @@ fu_vli_usbhub_firmware_parse (FuFirmware *firmware,
 		self->device_kind = FU_VLI_DEVICE_KIND_VL211;
 		break;
 	case 0x0518:
+		/* VL819~VL822 == VT3518 */
 		if (!fu_common_read_uint8_safe (buf, bufsz, 0x8021, &tmp, error)) {
 			g_prefix_error (error, "failed to get 820/822 byte: ");
 			return FALSE;
 		}
-		/* VL820/VL822 == VT3518 */
+		/* Q5/Q7/Q8 requires searching two addresses for offset value */
+		if (!fu_common_read_uint16_safe(buf,
+						bufsz,
+						0x8018,
+						&adr_ofs,
+						G_BIG_ENDIAN,
+						error)) {
+			g_prefix_error(error, "failed to get Q7/Q8 offset mapping: ");
+			return FALSE;
+		}
+		/* VL819, VL821, VL822 */
 		if (tmp == 0xF0) {
-			self->device_kind = FU_VLI_DEVICE_KIND_VL822;
+			if (!fu_common_read_uint8_safe(buf, bufsz, adr_ofs + 0x2000, &tmp, error)) {
+				g_prefix_error(error, "failed to get offset version: ");
+				return FALSE;
+			}
+			/* VL819 */
+			if ((tmp == 0x05) || (tmp == 0x07))
+				fwtype = tmp & 0x7;
+			else
+				fwtype = (tmp & 0x1) << 1 | (tmp & 0x2) << 1 | (tmp & 0x4) >> 2;
+			/* matching Q5/Q7/Q8 */
+			switch (fwtype) {
+			case 0x00:
+				self->device_kind = FU_VLI_DEVICE_KIND_VL822Q7;
+				break;
+			case 0x01:
+				self->device_kind = FU_VLI_DEVICE_KIND_VL822Q5;
+				break;
+			case 0x02:
+				self->device_kind = FU_VLI_DEVICE_KIND_VL822Q8;
+				break;
+			case 0x04:
+				self->device_kind = FU_VLI_DEVICE_KIND_VL821Q7;
+				break;
+			case 0x05:
+				self->device_kind = FU_VLI_DEVICE_KIND_VL819Q7;
+				break;
+			case 0x06:
+				self->device_kind = FU_VLI_DEVICE_KIND_VL821Q8;
+				break;
+			case 0x07:
+				self->device_kind = FU_VLI_DEVICE_KIND_VL819Q8;
+				break;
+			default:
+				g_prefix_error(error, "failed to match Q5/Q7/Q8 fw type: ");
+				return FALSE;
+			}
+			/* VL820 */
 		} else {
-			if (!fu_common_read_uint8_safe (buf, bufsz, 0xf000, &tmp, error)) {
-				g_prefix_error (error, "failed to get Q7/Q8 difference: ");
+			if (!fu_common_read_uint8_safe(buf,
+						       bufsz,
+						       adr_ofs + 0x2000 + 0x05,
+						       &tmp,
+						       error)) {
+				g_prefix_error(error, "failed to get offset version: ");
 				return FALSE;
 			}
 			if (tmp & (1 << 0))
@@ -199,10 +251,6 @@ fu_vli_usbhub_firmware_parse (FuFirmware *firmware,
 	case 0x0553:
 		/* VL120 == VT3553 */
 		self->device_kind = FU_VLI_DEVICE_KIND_VL120;
-		break;
-	case 0x0557:
-		/* VL819 == VT3557 */
-		self->device_kind = FU_VLI_DEVICE_KIND_VL819;
 		break;
 	default:
 		break;

plugins/vli/fu-vli-common.c
@@ -44,14 +44,24 @@ fu_vli_common_device_kind_to_string (FuVliDeviceKind device_kind)
 		return "VL815";
 	if (device_kind == FU_VLI_DEVICE_KIND_VL817)
 		return "VL817";
-	if (device_kind == FU_VLI_DEVICE_KIND_VL819)
-		return "VL819";
+	if (device_kind == FU_VLI_DEVICE_KIND_VL819Q7)
+		return "VL819Q7";
+	if (device_kind == FU_VLI_DEVICE_KIND_VL819Q8)
+		return "VL819Q8";
 	if (device_kind == FU_VLI_DEVICE_KIND_VL820Q7)
 		return "VL820Q7";
 	if (device_kind == FU_VLI_DEVICE_KIND_VL820Q8)
 		return "VL820Q8";
-	if (device_kind == FU_VLI_DEVICE_KIND_VL822)
-		return "VL822";
+	if (device_kind == FU_VLI_DEVICE_KIND_VL821Q7)
+		return "VL821Q7";
+	if (device_kind == FU_VLI_DEVICE_KIND_VL821Q8)
+		return "VL821Q8";
+	if (device_kind == FU_VLI_DEVICE_KIND_VL822Q5)
+		return "VL822Q5";
+	if (device_kind == FU_VLI_DEVICE_KIND_VL822Q7)
+		return "VL822Q7";
+	if (device_kind == FU_VLI_DEVICE_KIND_VL822Q8)
+		return "VL822Q8";
 	if (device_kind == FU_VLI_DEVICE_KIND_VL120)
 		return "VL120";
 	if (device_kind == FU_VLI_DEVICE_KIND_VL210)
@@ -104,14 +114,24 @@ fu_vli_common_device_kind_from_string (const gchar *device_kind)
 		return FU_VLI_DEVICE_KIND_VL815;
 	if (g_strcmp0 (device_kind, "VL817") == 0)
 		return FU_VLI_DEVICE_KIND_VL817;
-	if (g_strcmp0 (device_kind, "VL819") == 0)
-		return FU_VLI_DEVICE_KIND_VL819;
+	if (g_strcmp0(device_kind, "VL819Q7") == 0)
+		return FU_VLI_DEVICE_KIND_VL819Q7;
+	if (g_strcmp0(device_kind, "VL819Q8") == 0)
+		return FU_VLI_DEVICE_KIND_VL819Q8;
 	if (g_strcmp0 (device_kind, "VL820Q7") == 0)
 		return FU_VLI_DEVICE_KIND_VL820Q7;
 	if (g_strcmp0 (device_kind, "VL820Q8") == 0)
 		return FU_VLI_DEVICE_KIND_VL820Q8;
-	if (g_strcmp0 (device_kind, "VL822") == 0)
-		return FU_VLI_DEVICE_KIND_VL822;
+	if (g_strcmp0(device_kind, "VL821Q7") == 0)
+		return FU_VLI_DEVICE_KIND_VL821Q7;
+	if (g_strcmp0(device_kind, "VL821Q8") == 0)
+		return FU_VLI_DEVICE_KIND_VL821Q8;
+	if (g_strcmp0(device_kind, "VL822Q5") == 0)
+		return FU_VLI_DEVICE_KIND_VL822Q5;
+	if (g_strcmp0(device_kind, "VL822Q7") == 0)
+		return FU_VLI_DEVICE_KIND_VL822Q7;
+	if (g_strcmp0(device_kind, "VL822Q8") == 0)
+		return FU_VLI_DEVICE_KIND_VL822Q8;
 	if (g_strcmp0 (device_kind, "VL120") == 0)
 		return FU_VLI_DEVICE_KIND_VL120;
 	if (g_strcmp0 (device_kind, "VL210") == 0)
@@ -170,13 +190,23 @@ fu_vli_common_device_kind_get_size (FuVliDeviceKind device_kind)
 		return 0x20000 * 2;
 	if (device_kind == FU_VLI_DEVICE_KIND_VL817)
 		return 0x20000 * 2;
-	if (device_kind == FU_VLI_DEVICE_KIND_VL819)
+	if (device_kind == FU_VLI_DEVICE_KIND_VL819Q7)
+		return 0x20000 * 2;
+	if (device_kind == FU_VLI_DEVICE_KIND_VL819Q8)
 		return 0x20000 * 2;
 	if (device_kind == FU_VLI_DEVICE_KIND_VL820Q7)
 		return 0x20000 * 2;
 	if (device_kind == FU_VLI_DEVICE_KIND_VL820Q8)
 		return 0x20000 * 2;
-	if (device_kind == FU_VLI_DEVICE_KIND_VL822)
+	if (device_kind == FU_VLI_DEVICE_KIND_VL821Q7)
+		return 0x20000 * 2;
+	if (device_kind == FU_VLI_DEVICE_KIND_VL821Q8)
+		return 0x20000 * 2;
+	if (device_kind == FU_VLI_DEVICE_KIND_VL822Q5)
+		return 0x20000 * 2;
+	if (device_kind == FU_VLI_DEVICE_KIND_VL822Q7)
+		return 0x20000 * 2;
+	if (device_kind == FU_VLI_DEVICE_KIND_VL822Q8)
 		return 0x20000 * 2;
 	if (device_kind == FU_VLI_DEVICE_KIND_PS186)
 		return 0x40000;

plugins/vli/fu-vli-common.h
@@ -10,34 +10,39 @@
 #include <fwupdplugin.h>
 
 typedef enum {
-	FU_VLI_DEVICE_KIND_UNKNOWN		= 0x0000,
-	FU_VLI_DEVICE_KIND_VL100		= 0x0100,
-	FU_VLI_DEVICE_KIND_VL101		= 0x0101,
-	FU_VLI_DEVICE_KIND_VL102		= 0x0102,
-	FU_VLI_DEVICE_KIND_VL103		= 0x0103,
-	FU_VLI_DEVICE_KIND_VL104		= 0x0104,
-	FU_VLI_DEVICE_KIND_VL105		= 0x0105,
-	FU_VLI_DEVICE_KIND_VL120		= 0x0120,
-	FU_VLI_DEVICE_KIND_VL210		= 0x0210,
-	FU_VLI_DEVICE_KIND_VL211		= 0x0211,
-	FU_VLI_DEVICE_KIND_VL212		= 0x0212,
-	FU_VLI_DEVICE_KIND_VL810		= 0x0810,
-	FU_VLI_DEVICE_KIND_VL811		= 0x0811,
-	FU_VLI_DEVICE_KIND_VL811PB0		= 0x8110,
-	FU_VLI_DEVICE_KIND_VL811PB3		= 0x8113,
-	FU_VLI_DEVICE_KIND_VL812B0		= 0xa812,
-	FU_VLI_DEVICE_KIND_VL812B3		= 0xb812,
-	FU_VLI_DEVICE_KIND_VL812Q4S		= 0xc812,
-	FU_VLI_DEVICE_KIND_VL813		= 0x0813,
-	FU_VLI_DEVICE_KIND_VL815		= 0x0815,
-	FU_VLI_DEVICE_KIND_VL817		= 0x0817,
-	FU_VLI_DEVICE_KIND_VL819		= 0x0819,
-	FU_VLI_DEVICE_KIND_VL820Q7		= 0xa820,
-	FU_VLI_DEVICE_KIND_VL820Q8		= 0xb820,
-	FU_VLI_DEVICE_KIND_VL822		= 0x0822,
-	FU_VLI_DEVICE_KIND_MSP430		= 0xf430, /* guessed */
-	FU_VLI_DEVICE_KIND_PS186		= 0xf186, /* guessed */
-	FU_VLI_DEVICE_KIND_RTD21XX		= 0xff00, /* guessed */
+	FU_VLI_DEVICE_KIND_UNKNOWN = 0x0000,
+	FU_VLI_DEVICE_KIND_VL100 = 0x0100,
+	FU_VLI_DEVICE_KIND_VL101 = 0x0101,
+	FU_VLI_DEVICE_KIND_VL102 = 0x0102,
+	FU_VLI_DEVICE_KIND_VL103 = 0x0103,
+	FU_VLI_DEVICE_KIND_VL104 = 0x0104,
+	FU_VLI_DEVICE_KIND_VL105 = 0x0105,
+	FU_VLI_DEVICE_KIND_VL120 = 0x0120,
+	FU_VLI_DEVICE_KIND_VL210 = 0x0210,
+	FU_VLI_DEVICE_KIND_VL211 = 0x0211,
+	FU_VLI_DEVICE_KIND_VL212 = 0x0212,
+	FU_VLI_DEVICE_KIND_VL810 = 0x0810,
+	FU_VLI_DEVICE_KIND_VL811 = 0x0811,
+	FU_VLI_DEVICE_KIND_VL811PB0 = 0x8110,
+	FU_VLI_DEVICE_KIND_VL811PB3 = 0x8113,
+	FU_VLI_DEVICE_KIND_VL812B0 = 0xa812,
+	FU_VLI_DEVICE_KIND_VL812B3 = 0xb812,
+	FU_VLI_DEVICE_KIND_VL812Q4S = 0xc812,
+	FU_VLI_DEVICE_KIND_VL813 = 0x0813,
+	FU_VLI_DEVICE_KIND_VL815 = 0x0815,
+	FU_VLI_DEVICE_KIND_VL817 = 0x0817,
+	FU_VLI_DEVICE_KIND_VL819Q7 = 0xa819, /* guessed */
+	FU_VLI_DEVICE_KIND_VL819Q8 = 0xb819, /* guessed */
+	FU_VLI_DEVICE_KIND_VL820Q7 = 0xa820,
+	FU_VLI_DEVICE_KIND_VL820Q8 = 0xb820,
+	FU_VLI_DEVICE_KIND_VL821Q7 = 0xa821, /* guessed */
+	FU_VLI_DEVICE_KIND_VL821Q8 = 0xb821, /* guessed */
+	FU_VLI_DEVICE_KIND_VL822Q5 = 0x0822, /* guessed */
+	FU_VLI_DEVICE_KIND_VL822Q7 = 0xa822, /* guessed */
+	FU_VLI_DEVICE_KIND_VL822Q8 = 0xb822, /* guessed */
+	FU_VLI_DEVICE_KIND_MSP430 = 0xf430,  /* guessed */
+	FU_VLI_DEVICE_KIND_PS186 = 0xf186,   /* guessed */
+	FU_VLI_DEVICE_KIND_RTD21XX = 0xff00, /* guessed */
 } FuVliDeviceKind;
 
 const gchar	*fu_vli_common_device_kind_to_string	(FuVliDeviceKind	 device_kind);

plugins/vli/fu-vli-usbhub-device.c
@@ -405,8 +405,7 @@ fu_vli_usbhub_device_guess_kind (FuVliUsbhubDevice *self, GError **error)
 {
 	GUsbDevice *usb_device = fu_usb_device_get_dev (FU_USB_DEVICE (self));
 	guint8 b811P812 = 0x0;
-	guint8 b820Q7Q8 = 0x0;
-	guint8 b820822 = 0x0;
+	guint8 pkgtype = 0x0;
 	guint8 chipid1 = 0x0;
 	guint8 chipid2 = 0x0;
 	guint8 chipid12 = 0x0;
@@ -443,12 +442,8 @@ fu_vli_usbhub_device_guess_kind (FuVliUsbhubDevice *self, GError **error)
 		g_prefix_error (error, "Read_ChipID22 failed: ");
 		return FALSE;
 	}
-	if (!fu_vli_usbhub_device_read_reg (self, 0xf651, &b820Q7Q8, error)) {
+	if (!fu_vli_usbhub_device_read_reg(self, 0xf651, &pkgtype, error)) {
 		g_prefix_error (error, "Read_820Q7Q8 failed: ");
-		return FALSE;
-	}
-	if (!fu_vli_usbhub_device_read_reg (self, 0xf88c, &b820822, error)) {
-		g_prefix_error (error, "Read_820822 failed: ");
 		return FALSE;
 	}
 	if (g_getenv ("FWUPD_VLI_USBHUB_VERBOSE") != NULL) {
@@ -459,17 +454,59 @@ fu_vli_usbhub_device_guess_kind (FuVliUsbhubDevice *self, GError **error)
 		g_debug ("chipid2 = 0x%02x", chipid2);
 		g_debug ("chipid12 = 0x%02x", chipid12);
 		g_debug ("chipid22 = 0x%02x", chipid22);
-		g_debug ("b820Q7Q8 = 0x%02x", b820Q7Q8);
-		g_debug ("b820822 = 0x%02x", b820822);
+		g_debug("pkgtype = 0x%02x", pkgtype);
 	}
 
 	if (chipid2 == 0x35 && chipid1 == 0x07) {
 		fu_vli_device_set_kind (FU_VLI_DEVICE (self), FU_VLI_DEVICE_KIND_VL210);
 	} else if (chipid2 == 0x35 && chipid1 == 0x18) {
-		if (b820822 == 0xF0) {
-			fu_vli_device_set_kind (FU_VLI_DEVICE (self), FU_VLI_DEVICE_KIND_VL822);
+		if (chipver == 0xF0) {
+			/* packet type determines device kind for VL819-VL822, minus VL820 */
+			switch ((pkgtype >> 1) & 0x07) {
+			/* VL822Q7 */
+			case 0x00:
+				fu_vli_device_set_kind(FU_VLI_DEVICE(self),
+						       FU_VLI_DEVICE_KIND_VL822Q7);
+				break;
+			/* VL822Q5 */
+			case 0x01:
+				fu_vli_device_set_kind(FU_VLI_DEVICE(self),
+						       FU_VLI_DEVICE_KIND_VL822Q5);
+				break;
+			/* VL822Q8 */
+			case 0x02:
+				fu_vli_device_set_kind(FU_VLI_DEVICE(self),
+						       FU_VLI_DEVICE_KIND_VL822Q8);
+				break;
+			/* VL821Q7 */
+			case 0x04:
+				fu_vli_device_set_kind(FU_VLI_DEVICE(self),
+						       FU_VLI_DEVICE_KIND_VL821Q7);
+				break;
+			/* VL819Q7 */
+			case 0x05:
+				fu_vli_device_set_kind(FU_VLI_DEVICE(self),
+						       FU_VLI_DEVICE_KIND_VL819Q7);
+				break;
+			/* VL821Q8 */
+			case 0x06:
+				fu_vli_device_set_kind(FU_VLI_DEVICE(self),
+						       FU_VLI_DEVICE_KIND_VL821Q8);
+				break;
+			/* VL819Q8 */
+			case 0x07:
+				fu_vli_device_set_kind(FU_VLI_DEVICE(self),
+						       FU_VLI_DEVICE_KIND_VL819Q8);
+				break;
+			default:
+				g_set_error(error,
+					    G_IO_ERROR,
+					    G_IO_ERROR_NOT_SUPPORTED,
+					    "Packet Type match failed: ");
+				return FALSE;
+			}
 		} else {
-			if (b820Q7Q8 & (1 << 2))
+			if (pkgtype & (1 << 2))
 				fu_vli_device_set_kind (FU_VLI_DEVICE (self), FU_VLI_DEVICE_KIND_VL820Q8);
 			else
 				fu_vli_device_set_kind (FU_VLI_DEVICE (self), FU_VLI_DEVICE_KIND_VL820Q7);
@@ -482,8 +519,6 @@ fu_vli_usbhub_device_guess_kind (FuVliUsbhubDevice *self, GError **error)
 		fu_vli_device_set_kind (FU_VLI_DEVICE (self), FU_VLI_DEVICE_KIND_VL211);
 	} else if (chipid22 == 0x35 && chipid12 == 0x53) {
 		fu_vli_device_set_kind (FU_VLI_DEVICE (self), FU_VLI_DEVICE_KIND_VL120);
-	} else if (chipid2 == 0x35 && chipid1 == 0x57) {
-		fu_vli_device_set_kind (FU_VLI_DEVICE (self), FU_VLI_DEVICE_KIND_VL819);
 	} else if (tPid == 0x810) {
 		fu_vli_device_set_kind (FU_VLI_DEVICE (self), FU_VLI_DEVICE_KIND_VL810);
 	} else if (tPid == 0x811) {

commit e662a757ee7eb8dfe4407efe488d6e3ffb729e42

vli: Fix LV820Q7 detection
This was changed accidentally in 5d337e72a68d3ead3f769e312ace7dbb9e97c887
which broke installing USB hub firmware on the Lenovo WS dock G2.

plugins/vli/fu-vli-usbhub-firmware.c
@@ -258,12 +258,8 @@ fu_vli_usbhub_firmware_parse(FuFirmware *firmware,
 			}
 			/* VL820 */
 		} else {
-			if (!fu_common_read_uint8_safe(buf,
-						       bufsz,
-						       adr_ofs + 0x2000 + 0x05,
-						       &tmp,
-						       error)) {
-				g_prefix_error(error, "failed to get offset version: ");
+			if (!fu_common_read_uint8_safe(buf, bufsz, 0xf000, &tmp, error)) {
+				g_prefix_error(error, "failed to get Q7/Q8 difference: ");
 				return FALSE;
 			}
 			if (tmp & (1 << 0))