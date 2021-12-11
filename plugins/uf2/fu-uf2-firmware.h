5cb5b1148b0c86b45a4cf3427940f0b84f820e38
Richard Hughes <richard@hughsie.com>
2021-12-11 09:02:38 +0000

    Add support for UF2 devices
    A UF2 device exposes a VFAT block device (sometimes called a Mass
    Storage Device) which has a virtual file called `INFO_UF2.TXT` where
    metadata can be read. It may also have a the current firmware exported
    as a file called `CURRENT.UF2` which is in a 512 byte-block UF2 format.
    
    Writing any file to the MSD will cause the firmware to be written.
    Sometimes the device will restart and the volume will be unmounted
    and then mounted again. In some cases the volume may not “come back”
    until the user manually puts the device back in programming mode.
    
    Match the block devices using the VID*PID, UUID or label, and then
    create a UF2 device which can be used to flash firmware.
    
    Note: We only read metadata from allow-listed IDs to avoid causing
    regressions on non-UF2 volumes. To get the UUID and label you can
    use commands like:
    
        udisksctl info -b /dev/sda1

---
+++ plugins/uf2/fu-uf2-firmware.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2021 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include <fwupdplugin.h>
+
+#define FU_TYPE_UF2_FIRMWARE (fu_uf2_firmware_get_type())
+G_DECLARE_FINAL_TYPE(FuUf2Firmware, fu_uf2_firmware, FU, UF2_FIRMWARE, FuFirmware)
+
+FuFirmware *
+fu_uf2_firmware_new(void);
