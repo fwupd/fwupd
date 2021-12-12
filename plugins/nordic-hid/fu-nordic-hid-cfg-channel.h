dbb7abb00fad204ea3f398996a756c3e3ac9558e
Denis Pynkin <denis.pynkin@collabora.com>
2021-12-12 21:18:42 +0100

    plugins/nordic-hid: add peers and bootloader type detection
    - added support of peer devices connected via dongle
    - added bootloader type detection from the new 'bootloader_var' option
    - fix annoying retry messages:
      mask replies stating the device is temporary busy.
      Allow to sync more precisely and without annoying messages.
    - multiple fixes and enhancements

--- plugins/nordic-hid/fu-nordic-hid-cfg-channel.h
+++ plugins/nordic-hid/fu-nordic-hid-cfg-channel.h
@@ -9,8 +9,11 @@
 #include <fwupdplugin.h>
 
 #define FU_TYPE_NORDIC_HID_CFG_CHANNEL (fu_nordic_hid_cfg_channel_get_type())
-G_DECLARE_FINAL_TYPE(FuNordicDeviceCfgChannel,
+G_DECLARE_FINAL_TYPE(FuNordicHidCfgChannel,
 		     fu_nordic_hid_cfg_channel,
 		     FU,
 		     NORDIC_HID_CFG_CHANNEL,
 		     FuUdevDevice)
+
+FuNordicHidCfgChannel *
+fu_nordic_hid_cfg_channel_new(guint8 id);
