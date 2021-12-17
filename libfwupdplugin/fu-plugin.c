17db067d1314a858fc6a0e24fae84389e3879866
Mario Limonciello <superm1@gmail.com>
2021-12-17 08:48:10 -0600

    libfwupdplugin: add a helper to set a config file to secure

--- libfwupdplugin/fu-plugin.c
+++ libfwupdplugin/fu-plugin.c
@@ -10,6 +10,7 @@
 
 #include <errno.h>
 #include <fwupd.h>
+#include <glib/gstdio.h>
 #include <gmodule.h>
 #include <string.h>
 #include <unistd.h>
@@ -2258,6 +2259,49 @@ fu_plugin_set_config_value(FuPlugin *self, const gchar *key, const gchar *value,
 		return FALSE;
 	g_key_file_set_string(keyfile, fu_plugin_get_name(self), key, value);
 	return g_key_file_save_to_file(keyfile, conf_path, error);
+}
+
+/**
+ * fu_plugin_set_secure_config_value:
+ * @self: a #FuPlugin
+ * @key: a settings key
+ * @value: (nullable): a settings value
+ * @error: (nullable): optional return location for an error
+ *
+ * Sets a plugin config file value and updates file so that non-privileged users
+ * cannot read it.
+ *
+ * Returns: %TRUE for success
+ *
+ * Since: 1.7.4
+ **/
+gboolean
+fu_plugin_set_secure_config_value(FuPlugin *self,
+				  const gchar *key,
+				  const gchar *value,
+				  GError **error)
+{
+	g_autofree gchar *conf_path = fu_plugin_get_config_filename(self);
+	gint ret;
+
+	g_return_val_if_fail(FU_IS_PLUGIN(self), FALSE);
+	g_return_val_if_fail(error == NULL || *error == NULL, FALSE);
+
+	if (!g_file_test(conf_path, G_FILE_TEST_EXISTS)) {
+		g_set_error(error, FWUPD_ERROR, FWUPD_ERROR_NOT_FOUND, "%s is missing", conf_path);
+		return FALSE;
+	}
+	ret = g_chmod(conf_path, 0660);
+	if (ret == -1) {
+		g_set_error(error,
+			    FWUPD_ERROR,
+			    FWUPD_ERROR_INTERNAL,
+			    "failed to set permissions on %s",
+			    conf_path);
+		return FALSE;
+	}
+
+	return fu_plugin_set_config_value(self, key, value, error);
 }
 
 /**
