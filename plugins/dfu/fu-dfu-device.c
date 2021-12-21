81f8b699b5855975085e4214db40decffe07ad40
Richard Hughes <richard@hughsie.com>
2021-12-21 12:24:49 +0000

    dfu: Always set a summary line for DFU peripherals

--- plugins/dfu/fu-dfu-device.c
+++ plugins/dfu/fu-dfu-device.c
@@ -1908,6 +1908,7 @@ fu_dfu_device_init(FuDfuDevice *self)
 	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_ADD_COUNTERPART_GUIDS);
 	fu_device_add_internal_flag(FU_DEVICE(self), FU_DEVICE_INTERNAL_FLAG_REPLUG_MATCH_GUID);
 	fu_device_set_remove_delay(FU_DEVICE(self), FU_DEVICE_REMOVE_DELAY_RE_ENUMERATE);
+	fu_device_set_summary(FU_DEVICE(self), "DFU peripheral");
 
 	fu_device_register_private_flag(FU_DEVICE(self),
 					FU_DFU_DEVICE_FLAG_ATTACH_EXTRA_RESET,
