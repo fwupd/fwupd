a258461926a5065f274f68533a16659077bec9b2
Richard Hughes <richard@hughsie.com>
2022-02-23 14:42:39 +0000

    trivial: Add unsigned flag for the Nitrokey Storage

--- plugins/nitrokey/fu-nitrokey-device.c
+++ plugins/nitrokey/fu-nitrokey-device.c
@@ -149,6 +149,7 @@ fu_nitrokey_device_init(FuNitrokeyDevice *self)
 	fu_device_set_remove_delay(FU_DEVICE(self), FU_DEVICE_REMOVE_DELAY_RE_ENUMERATE);
 	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_UPDATABLE);
 	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_ADD_COUNTERPART_GUIDS);
+	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_UNSIGNED_PAYLOAD);
 	fu_device_add_internal_flag(FU_DEVICE(self), FU_DEVICE_INTERNAL_FLAG_REPLUG_MATCH_GUID);
 	fu_device_set_version_format(FU_DEVICE(self), FWUPD_VERSION_FORMAT_PAIR);
 	fu_device_add_protocol(FU_DEVICE(self), "org.usb.dfu");
