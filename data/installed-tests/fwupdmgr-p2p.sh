b211ae4717663c94410e624f5b973acf185d83f8
Richard Hughes <richard@hughsie.com>
2022-03-07 13:08:37 +0000

    Do not use /var/run for the socket
    The FHS says to use /run everywhere now, so use that to avoid confusion.
    See https://refspecs.linuxfoundation.org/FHS_3.0/fhs/ch05s13.html

--- data/installed-tests/fwupdmgr-p2p.sh
+++ data/installed-tests/fwupdmgr-p2p.sh
@@ -8,7 +8,7 @@ if [ "$(id -u)" -ne 0 ]; then exit 0; fi
 
 # ---
 echo "Starting P2P daemon..."
-export FWUPD_DBUS_SOCKET="/var/run/fwupd.sock"
+export FWUPD_DBUS_SOCKET="/run/fwupd.sock"
 rm -rf ${FWUPD_DBUS_SOCKET}
 @libexecdir@/fwupd/fwupd --verbose --timed-exit --no-timestamp &
 while [ ! -e ${FWUPD_DBUS_SOCKET} ]; do sleep 1; done
