c920d5edda1d5fbb9407f89f71b938bcdd4867d2
Richard Hughes <richard@hughsie.com>
2022-03-08 16:38:02 +0000

    flashrom: Simplify the plugin to remove the indirection
    For a while we flirted with the idea of updating non-internal
    libflashrom-derived devices but the impedance mismatch was just too
    great.
    
    We removed the extra unused subclasses of FuFlashromDevice, but never
    removed the indirection we added for the writing.
    Remove the unused code now that we agree on the longer term plan.

--- plugins/flashrom/fu-flashrom-device.h
+++ plugins/flashrom/fu-flashrom-device.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2017 Richard Hughes <richard@hughsie.com>
+ * Copyright (C) 2021 Daniel Campello <campello@chromium.org>
  *
  * SPDX-License-Identifier: LGPL-2.1+
  */
@@ -8,20 +9,8 @@
 
 #include <fwupdplugin.h>
 
-struct _FuFlashromDeviceClass {
-	FuUdevDeviceClass parent_class;
-};
-
 #define FU_TYPE_FLASHROM_DEVICE (fu_flashrom_device_get_type())
-G_DECLARE_DERIVABLE_TYPE(FuFlashromDevice, fu_flashrom_device, FU, FLASHROM_DEVICE, FuUdevDevice)
+G_DECLARE_FINAL_TYPE(FuFlashromDevice, fu_flashrom_device, FU, FLASHROM_DEVICE, FuUdevDevice)
 
-void
-fu_flashrom_device_set_programmer_name(FuFlashromDevice *self, const gchar *name);
-const gchar *
-fu_flashrom_device_get_programmer_name(FuFlashromDevice *self);
-void
-fu_flashrom_device_set_programmer_args(FuFlashromDevice *self, const gchar *args);
-gsize
-fu_flashrom_device_get_flash_size(FuFlashromDevice *self);
-struct flashrom_flashctx *
-fu_flashrom_device_get_flashctx(FuFlashromDevice *self);
+FuDevice *
+fu_flashrom_device_new(FuContext *ctx);
