fwupd/fwupd

commit c4ca02629487d3f26278f47e4720167b1ae04eb4

Allow devices to build instance IDs more easily
Provide a device instance builder that allows plugins to easily
create multiple instance IDs based on parent attributes.

Also fix a lot of the instance ID orders, so that we add more generic
IDs first, and more specific IDs after.

plugins/scsi/fu-scsi-device.c
@@ -20,10 +20,7 @@ fu_scsi_device_probe(FuDevice *device, GError **error)
 	const gchar *name;
 	const gchar *vendor;
 	const gchar *version;
-	g_autofree gchar *name_safe = NULL;
-	g_autofree gchar *subsystem = NULL;
-	g_autofree gchar *vendor_safe = NULL;
-	g_autofree gchar *version_safe = NULL;
+	g_autofree gchar *vendor_id = NULL;
 	g_autoptr(GPtrArray) block_devs = NULL;
 
 	/* ignore */
@@ -37,8 +34,7 @@ fu_scsi_device_probe(FuDevice *device, GError **error)
 
 	/* vendor */
 	vendor = fu_udev_device_get_sysfs_attr(FU_UDEV_DEVICE(device), "vendor", NULL);
-	vendor_safe = fu_common_instance_id_strsafe(vendor);
-	if (vendor_safe == NULL || g_strcmp0(vendor_safe, "ATA") == 0) {
+	if (vendor == NULL || g_strcmp0(vendor, "ATA") == 0) {
 		g_set_error_literal(error,
 				    FWUPD_ERROR,
 				    FWUPD_ERROR_NOT_SUPPORTED,
@@ -46,57 +42,27 @@ fu_scsi_device_probe(FuDevice *device, GError **error)
 		return FALSE;
 	}
 	fu_device_set_vendor(device, vendor);
+	vendor_id = g_strdup_printf("SCSI:%s", vendor);
+	fu_device_add_vendor_id(device, vendor_id);
 
 	/* name */
 	name = fu_udev_device_get_sysfs_attr(FU_UDEV_DEVICE(device), "model", NULL);
-	name_safe = fu_common_instance_id_strsafe(name);
-	if (name_safe == NULL) {
-		g_set_error_literal(error,
-				    FWUPD_ERROR,
-				    FWUPD_ERROR_NOT_SUPPORTED,
-				    "no assigned name");
-		return FALSE;
-	}
 	fu_device_set_name(device, name);
 
 	/* version */
 	version = fu_udev_device_get_sysfs_attr(FU_UDEV_DEVICE(device), "rev", NULL);
-	version_safe = fu_common_instance_id_strsafe(version);
-	if (version_safe == NULL) {
-		g_set_error_literal(error,
-				    FWUPD_ERROR,
-				    FWUPD_ERROR_NOT_SUPPORTED,
-				    "no assigned version");
-		return FALSE;
-	}
 	fu_device_set_version(device, version);
 
 	/* add GUIDs */
-	subsystem = g_utf8_strup(fu_udev_device_get_subsystem(FU_UDEV_DEVICE(device)), -1);
-	if (subsystem != NULL && vendor_safe != NULL && name_safe != NULL && version_safe != NULL) {
-		g_autofree gchar *devid = NULL;
-		devid = g_strdup_printf("%s\\VEN_%s&DEV_%s&REV_%s",
-					subsystem,
-					vendor_safe,
-					name_safe,
-					version_safe);
-		fu_device_add_instance_id(device, devid);
-	}
-	if (subsystem != NULL && vendor_safe != NULL && name_safe != NULL) {
-		g_autofree gchar *devid = NULL;
-		devid = g_strdup_printf("%s\\VEN_%s&DEV_%s", subsystem, vendor_safe, name_safe);
-		fu_device_add_instance_id(device, devid);
-	}
-	if (subsystem != NULL && vendor_safe != NULL) {
-		g_autofree gchar *devid = NULL;
-		devid = g_strdup_printf("%s\\VEN_%s", subsystem, vendor_safe);
-		fu_device_add_instance_id_full(device, devid, FU_DEVICE_INSTANCE_FLAG_ONLY_QUIRKS);
-	}
-	if (vendor_safe != NULL) {
-		g_autofree gchar *vendor_id = NULL;
-		vendor_id = g_strdup_printf("SCSI:%s", vendor_safe);
-		fu_device_add_vendor_id(device, vendor_id);
-	}
+	fu_device_add_instance_strsafe(device, "VEN", vendor);
+	fu_device_add_instance_strsafe(device, "DEV", name);
+	fu_device_add_instance_strsafe(device, "REV", version);
+	if (!fu_device_build_instance_id_quirk(device, NULL, "SCSI", "VEN", NULL))
+		return FALSE;
+	if (!fu_device_build_instance_id(device, NULL, "SCSI", "VEN", "DEV", NULL))
+		return FALSE;
+	if (!fu_device_build_instance_id(device, NULL, "SCSI", "VEN", "DEV", "REV", NULL))
+		return FALSE;
 
 	/* check all block devices, although there should only be one */
 	block_devs = fu_udev_device_get_children_with_subsystem(FU_UDEV_DEVICE(device), "block");

libfwupdplugin/fu-bluez-device.c
@@ -171,24 +171,19 @@ fu_bluez_device_set_modalias(FuBluezDevice *self, const gchar *modalias)
 		fu_firmware_strparse_uint16_safe(modalias, modaliaslen, 21, &rev, NULL);
 	}
 
-	if (vid != 0x0 && pid != 0x0 && rev != 0x0) {
-		g_autofree gchar *devid = NULL;
-		devid = g_strdup_printf("BLUETOOTH\\VID_%04X&PID_%04X&REV_%04X", vid, pid, rev);
-		fu_device_add_instance_id(FU_DEVICE(self), devid);
-	}
-	if (vid != 0x0 && pid != 0x0) {
-		g_autofree gchar *devid = NULL;
-		devid = g_strdup_printf("BLUETOOTH\\VID_%04X&PID_%04X", vid, pid);
-		fu_device_add_instance_id(FU_DEVICE(self), devid);
-	}
+	/* add generated IDs */
+	if (vid != 0x0)
+		fu_device_add_instance_u16(FU_DEVICE(self), "VID", vid);
+	if (pid != 0x0)
+		fu_device_add_instance_u16(FU_DEVICE(self), "PID", pid);
+	fu_device_add_instance_u16(FU_DEVICE(self), "REV", rev);
+	fu_device_build_instance_id_quirk(FU_DEVICE(self), NULL, "BLUETOOTH", "VID", NULL);
+	fu_device_build_instance_id(FU_DEVICE(self), NULL, "BLUETOOTH", "VID", "PID", NULL);
+	fu_device_build_instance_id(FU_DEVICE(self), NULL, "BLUETOOTH", "VID", "PID", "REV", NULL);
+
+	/* set vendor ID */
 	if (vid != 0x0) {
-		g_autofree gchar *devid = NULL;
-		g_autofree gchar *vendor_id = NULL;
-		devid = g_strdup_printf("BLUETOOTH\\VID_%04X", vid);
-		fu_device_add_instance_id_full(FU_DEVICE(self),
-					       devid,
-					       FU_DEVICE_INSTANCE_FLAG_ONLY_QUIRKS);
-		vendor_id = g_strdup_printf("BLUETOOTH:%04X", vid);
+		g_autofree gchar *vendor_id = g_strdup_printf("BLUETOOTH:%04X", vid);
 		fu_device_add_vendor_id(FU_DEVICE(self), vendor_id);
 	}
 }

libfwupdplugin/fu-cfi-device.c
@@ -184,31 +184,19 @@ fu_cfi_device_probe(FuDevice *device, GError **error)
 {
 	FuCfiDevice *self = FU_CFI_DEVICE(device);
 	FuCfiDevicePrivate *priv = GET_PRIVATE(self);
+	g_autofree gchar *flash_id_jedec = NULL;
 
-	/* load the parameters from quirks */
-	if (priv->flash_id != NULL) {
-		g_autofree gchar *flash_id_jedec = NULL;
-		g_autofree gchar *instance_id0 = NULL;
-		g_autofree gchar *instance_id1 = NULL;
-
-		/* least specific so adding first */
-		flash_id_jedec = fu_cfi_device_get_flash_id_jedec(self);
-		if (flash_id_jedec != NULL) {
-			instance_id1 = g_strdup_printf("CFI\\FLASHID_%s", flash_id_jedec);
-			fu_device_add_instance_id_full(FU_DEVICE(self),
-						       instance_id1,
-						       FU_DEVICE_INSTANCE_FLAG_ONLY_QUIRKS);
-		}
-
-		/* this is most specific and can override keys of instance_id1 */
-		instance_id0 = g_strdup_printf("CFI\\FLASHID_%s", priv->flash_id);
-		fu_device_add_instance_id_full(FU_DEVICE(self),
-					       instance_id0,
-					       FU_DEVICE_INSTANCE_FLAG_ONLY_QUIRKS);
+	/* least specific so adding first */
+	flash_id_jedec = fu_cfi_device_get_flash_id_jedec(self);
+	if (flash_id_jedec != NULL) {
+		fu_device_add_instance_str(device, "FLASHID", flash_id_jedec);
+		if (!fu_device_build_instance_id_quirk(device, error, "CFI", "FLASHID", NULL))
+			return FALSE;
 	}
 
-	/* success */
-	return TRUE;
+	/* this is most specific and can override */
+	fu_device_add_instance_str(device, "FLASHID", priv->flash_id);
+	return fu_device_build_instance_id_quirk(device, error, "CFI", "FLASHID", NULL);
 }
 
 /**

libfwupdplugin/fu-device.c
@@ -76,6 +76,7 @@ typedef struct {
 	GPtrArray *private_flag_items; /* (nullable) */
 	gchar *custom_flags;
 	gulong notify_flags_handler_id;
+	GHashTable *instance_hash;
 } FuDevicePrivate;
 
 typedef struct {
@@ -4355,6 +4356,9 @@ fu_device_setup(FuDevice *self, GError **error)
 			return FALSE;
 	}
 
+	/* no longer needed */
+	g_hash_table_remove_all(priv->instance_hash);
+
 	priv->done_setup = TRUE;
 	return TRUE;
 }
@@ -4735,6 +4739,269 @@ fu_device_flags_notify_cb(FuDevice *self, GParamSpec *pspec, gpointer user_data)
 		fu_device_ensure_inhibits(self);
 }
 
+/**
+ * fu_device_add_instance_str:
+ * @self: a #FuDevice
+ * @key: (not nullable): string
+ * @value: (nullable): value
+ *
+ * Assign a value for the @key.
+ *
+ * Since: 1.7.7
+ **/
+void
+fu_device_add_instance_str(FuDevice *self, const gchar *key, const gchar *value)
+{
+	FuDevicePrivate *priv = GET_PRIVATE(self);
+	g_return_if_fail(FU_IS_DEVICE(self));
+	g_return_if_fail(key != NULL);
+	g_return_if_fail(!priv->done_setup);
+	g_hash_table_insert(priv->instance_hash, g_strdup(key), g_strdup(value));
+}
+
+/**
+ * fu_device_add_instance_strsafe:
+ * @self: a #FuDevice
+ * @key: (not nullable): string
+ * @value: (nullable): value
+ *
+ * Assign a sanitized value for the @key.
+ *
+ * Since: 1.7.7
+ **/
+void
+fu_device_add_instance_strsafe(FuDevice *self, const gchar *key, const gchar *value)
+{
+	FuDevicePrivate *priv = GET_PRIVATE(self);
+	g_return_if_fail(FU_IS_DEVICE(self));
+	g_return_if_fail(key != NULL);
+	g_return_if_fail(!priv->done_setup);
+	g_hash_table_insert(priv->instance_hash,
+			    g_strdup(key),
+			    fu_common_instance_id_strsafe(value));
+}
+
+/**
+ * fu_device_add_instance_strup:
+ * @self: a #FuDevice
+ * @key: (not nullable): string
+ * @value: (nullable): value
+ *
+ * Assign a uppercase value for the @key.
+ *
+ * Since: 1.7.7
+ **/
+void
+fu_device_add_instance_strup(FuDevice *self, const gchar *key, const gchar *value)
+{
+	FuDevicePrivate *priv = GET_PRIVATE(self);
+	g_return_if_fail(FU_IS_DEVICE(self));
+	g_return_if_fail(key != NULL);
+	g_return_if_fail(!priv->done_setup);
+	g_hash_table_insert(priv->instance_hash,
+			    g_strdup(key),
+			    value != NULL ? g_utf8_strup(value, -1) : NULL);
+}
+
+/**
+ * fu_device_add_instance_u4:
+ * @self: a #FuDevice
+ * @key: (not nullable): string
+ * @value: value
+ *
+ * Assign a value to the @key, which is padded as %1X.
+ *
+ * Since: 1.7.7
+ **/
+void
+fu_device_add_instance_u4(FuDevice *self, const gchar *key, guint8 value)
+{
+	FuDevicePrivate *priv = GET_PRIVATE(self);
+	g_return_if_fail(FU_IS_DEVICE(self));
+	g_return_if_fail(key != NULL);
+	g_return_if_fail(!priv->done_setup);
+	g_hash_table_insert(priv->instance_hash, g_strdup(key), g_strdup_printf("%01X", value));
+}
+
+/**
+ * fu_device_add_instance_u8:
+ * @self: a #FuDevice
+ * @key: (not nullable): string
+ * @value: value
+ *
+ * Assign a value to the @key, which is padded as %2X.
+ *
+ * Since: 1.7.7
+ **/
+void
+fu_device_add_instance_u8(FuDevice *self, const gchar *key, guint8 value)
+{
+	FuDevicePrivate *priv = GET_PRIVATE(self);
+	g_return_if_fail(FU_IS_DEVICE(self));
+	g_return_if_fail(key != NULL);
+	g_return_if_fail(!priv->done_setup);
+	g_hash_table_insert(priv->instance_hash, g_strdup(key), g_strdup_printf("%02X", value));
+}
+
+/**
+ * fu_device_add_instance_u16:
+ * @self: a #FuDevice
+ * @key: (not nullable): string
+ * @value: value
+ *
+ * Assign a value to the @key, which is padded as %4X.
+ *
+ * Since: 1.7.7
+ **/
+void
+fu_device_add_instance_u16(FuDevice *self, const gchar *key, guint16 value)
+{
+	FuDevicePrivate *priv = GET_PRIVATE(self);
+	g_return_if_fail(FU_IS_DEVICE(self));
+	g_return_if_fail(key != NULL);
+	g_return_if_fail(!priv->done_setup);
+	g_hash_table_insert(priv->instance_hash, g_strdup(key), g_strdup_printf("%04X", value));
+}
+
+/**
+ * fu_device_add_instance_u32:
+ * @self: a #FuDevice
+ * @key: (not nullable): string
+ * @value: value
+ *
+ * Assign a value to the @key, which is padded as %8X.
+ *
+ * Since: 1.7.7
+ **/
+void
+fu_device_add_instance_u32(FuDevice *self, const gchar *key, guint32 value)
+{
+	FuDevicePrivate *priv = GET_PRIVATE(self);
+	g_return_if_fail(FU_IS_DEVICE(self));
+	g_return_if_fail(key != NULL);
+	g_return_if_fail(!priv->done_setup);
+	g_hash_table_insert(priv->instance_hash, g_strdup(key), g_strdup_printf("%08X", value));
+}
+
+static const gchar *
+fu_device_instance_lookup(FuDevice *self, const gchar *key)
+{
+	FuDevicePrivate *priv = GET_PRIVATE(self);
+	return g_hash_table_lookup(priv->instance_hash, key);
+}
+
+/**
+ * fu_device_build_instance_id:
+ * @self: a #FuDevice
+ * @error: (nullable): optional return location for an error
+ * @subsystem: (not nullable): subsystem, e.g. `NVME`
+ * @...: pairs of string key values, ending with %NULL
+ *
+ * Creates an instance ID from a prefix and some key values.
+ * If the key value cannot be found, the parent and then proxy is also queried.
+ *
+ * If any of the key values remain unset then no instance ID is added.
+ *
+ *	fu_device_add_instance_str(dev, "VID", "1234");
+ *	fu_device_add_instance_u16(dev, "PID", 5678);
+ *	if (!fu_device_build_instance_id(dev, &error, "NVME", "VID", "PID", NULL))
+ *		g_warning("failed to add ID: %s", error->message);
+ *
+ * Returns: %TRUE if the instance ID was added.
+ *
+ * Since: 1.7.7
+ **/
+gboolean
+fu_device_build_instance_id(FuDevice *self, GError **error, const gchar *subsystem, ...)
+{
+	FuDevice *parent = fu_device_get_parent(self);
+	FuDevicePrivate *priv = GET_PRIVATE(self);
+	va_list args;
+	g_autoptr(GString) str = g_string_new(subsystem);
+
+	g_return_val_if_fail(FU_IS_DEVICE(self), FALSE);
+	g_return_val_if_fail(subsystem != NULL, FALSE);
+	g_return_val_if_fail(!priv->done_setup, FALSE);
+
+	va_start(args, subsystem);
+	for (guint i = 0;; i++) {
+		const gchar *key = va_arg(args, const gchar *);
+		const gchar *value;
+		if (key == NULL)
+			break;
+		value = fu_device_instance_lookup(self, key);
+		if (value == NULL && parent != NULL)
+			value = fu_device_instance_lookup(parent, key);
+		if (value == NULL && priv->proxy != NULL)
+			value = fu_device_instance_lookup(priv->proxy, key);
+		if (value == NULL) {
+			g_set_error(error,
+				    G_IO_ERROR,
+				    G_IO_ERROR_INVALID_DATA,
+				    "no value for %s",
+				    key);
+			return FALSE;
+		}
+		g_string_append(str, i == 0 ? "\\" : "&");
+		g_string_append_printf(str, "%s_%s", key, value);
+	}
+	va_end(args);
+
+	/* success */
+	fu_device_add_instance_id(self, str->str);
+	return TRUE;
+}
+
+/**
+ * fu_device_build_instance_id_quirk:
+ * @self: a #FuDevice
+ * @error: (nullable): optional return location for an error
+ * @subsystem: (not nullable): subsystem, e.g. `NVME`
+ * @...: pairs of string key values, ending with %NULL
+ *
+ * Creates an quirk-only instance ID from a prefix and some key values. If any of the key values
+ * are unset then no instance ID is added.
+ *
+ * Returns: %TRUE if the instance ID was added.
+ *
+ * Since: 1.7.7
+ **/
+gboolean
+fu_device_build_instance_id_quirk(FuDevice *self, GError **error, const gchar *subsystem, ...)
+{
+	FuDevicePrivate *priv = GET_PRIVATE(self);
+	va_list args;
+	g_autoptr(GString) str = g_string_new(subsystem);
+
+	g_return_val_if_fail(FU_IS_DEVICE(self), FALSE);
+	g_return_val_if_fail(subsystem != NULL, FALSE);
+	g_return_val_if_fail(!priv->done_setup, FALSE);
+
+	va_start(args, subsystem);
+	for (guint i = 0;; i++) {
+		const gchar *key = va_arg(args, const gchar *);
+		const gchar *value;
+		if (key == NULL)
+			break;
+		value = g_hash_table_lookup(priv->instance_hash, key);
+		if (value == NULL) {
+			g_set_error(error,
+				    G_IO_ERROR,
+				    G_IO_ERROR_INVALID_DATA,
+				    "no value for %s",
+				    key);
+			return FALSE;
+		}
+		g_string_append(str, i == 0 ? "\\" : "&");
+		g_string_append_printf(str, "%s_%s", key, value);
+	}
+	va_end(args);
+
+	/* success */
+	fu_device_add_instance_id_full(self, str->str, FU_DEVICE_INSTANCE_FLAG_ONLY_QUIRKS);
+	return TRUE;
+}
+
 static void
 fu_device_class_init(FuDeviceClass *klass)
 {
@@ -4929,6 +5196,7 @@ fu_device_init(FuDevice *self)
 	priv->parent_guids = g_ptr_array_new_with_free_func(g_free);
 	priv->possible_plugins = g_ptr_array_new_with_free_func(g_free);
 	priv->retry_recs = g_ptr_array_new_with_free_func(g_free);
+	priv->instance_hash = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, g_free);
 	g_rw_lock_init(&priv->parent_guids_mutex);
 	g_rw_lock_init(&priv->metadata_mutex);
 	priv->notify_flags_handler_id = g_signal_connect(FWUPD_DEVICE(self),
@@ -4972,6 +5240,7 @@ fu_device_finalize(GObject *object)
 	g_free(priv->backend_id);
 	g_free(priv->proxy_guid);
 	g_free(priv->custom_flags);
+	g_hash_table_unref(priv->instance_hash);
 
 	G_OBJECT_CLASS(fu_device_parent_class)->finalize(object);
 }

libfwupdplugin/fu-device.h
@@ -699,3 +699,22 @@ gboolean
 fu_device_has_private_flag(FuDevice *self, guint64 flag);
 void
 fu_device_emit_request(FuDevice *self, FwupdRequest *request);
+
+void
+fu_device_add_instance_str(FuDevice *self, const gchar *key, const gchar *value);
+void
+fu_device_add_instance_strsafe(FuDevice *self, const gchar *key, const gchar *value);
+void
+fu_device_add_instance_strup(FuDevice *self, const gchar *key, const gchar *value);
+void
+fu_device_add_instance_u4(FuDevice *self, const gchar *key, guint8 value);
+void
+fu_device_add_instance_u8(FuDevice *self, const gchar *key, guint8 value);
+void
+fu_device_add_instance_u16(FuDevice *self, const gchar *key, guint16 value);
+void
+fu_device_add_instance_u32(FuDevice *self, const gchar *key, guint32 value);
+gboolean
+fu_device_build_instance_id(FuDevice *self, GError **error, const gchar *subsystem, ...);
+gboolean
+fu_device_build_instance_id_quirk(FuDevice *self, GError **error, const gchar *subsystem, ...);

libfwupdplugin/fu-i2c-device.c
@@ -128,13 +128,9 @@ fu_i2c_device_probe(FuDevice *device, GError **error)
 #ifdef HAVE_GUDEV
 	/* i2c devices all expose a name */
 	tmp = g_udev_device_get_sysfs_attr(udev_device, "name");
-	if (tmp != NULL) {
-		g_autofree gchar *name_safe = fu_common_instance_id_strsafe(tmp);
-		if (name_safe != NULL) {
-			g_autofree gchar *devid = g_strdup_printf("I2C\\NAME_%s", name_safe);
-			fu_device_add_instance_id(FU_DEVICE(self), devid);
-		}
-	}
+	fu_device_add_instance_strsafe(device, "NAME", tmp);
+	if (!fu_device_build_instance_id(device, error, "I2C", "NAME", NULL))
+		return FALSE;
 
 	/* get bus number out of sysfs path */
 	regex = g_regex_new("/i2c-([0-9]+)/", 0, 0, error);

libfwupdplugin/fu-udev-device.c
@@ -300,15 +300,12 @@ fu_udev_device_probe_serio(FuUdevDevice *self, GError **error)
 	/* firmware ID */
 	tmp = g_udev_device_get_property(priv->udev_device, "SERIO_FIRMWARE_ID");
 	if (tmp != NULL) {
-		g_autofree gchar *id_safe = NULL;
 		/* this prefix is not useful */
 		if (g_str_has_prefix(tmp, "PNP: "))
 			tmp += 5;
-		id_safe = fu_common_instance_id_strsafe(tmp);
-		if (id_safe != NULL) {
-			g_autofree gchar *devid = g_strdup_printf("SERIO\\FWID_%s", id_safe);
-			fu_device_add_instance_id(FU_DEVICE(self), devid);
-		}
+		fu_device_add_instance_strsafe(FU_DEVICE(self), "FWID", tmp);
+		if (!fu_device_build_instance_id(FU_DEVICE(self), error, "SERIO", "FWID", NULL))
+			return FALSE;
 	}
 	return TRUE;
 }
@@ -459,71 +456,37 @@ fu_udev_device_probe(FuDevice *device, GError **error)
 	}
 
 	/* add GUIDs in order of priority */
-	if (priv->vendor != 0x0000 && priv->model != 0x0000 && priv->subsystem_vendor != 0x0000 &&
-	    priv->subsystem_model != 0x0000) {
-		g_autofree gchar *devid1 = NULL;
-		g_autofree gchar *devid2 = NULL;
-		devid1 = g_strdup_printf("%s\\VEN_%04X&DEV_%04X&SUBSYS_%04X%04X&REV_%02X",
-					 subsystem,
-					 priv->vendor,
-					 priv->model,
-					 priv->subsystem_vendor,
-					 priv->subsystem_model,
-					 priv->revision);
-		fu_device_add_instance_id(device, devid1);
-		devid2 = g_strdup_printf("%s\\VEN_%04X&DEV_%04X&SUBSYS_%04X%04X",
-					 subsystem,
-					 priv->vendor,
-					 priv->model,
-					 priv->subsystem_vendor,
-					 priv->subsystem_model);
-		fu_device_add_instance_id(device, devid2);
+	if (priv->vendor != 0x0000)
+		fu_device_add_instance_u16(device, "VEN", priv->vendor);
+	if (priv->model != 0x0000)
+		fu_device_add_instance_u16(device, "DEV", priv->model);
+	if (priv->subsystem_vendor != 0x0000 && priv->subsystem_model != 0x0000) {
+		g_autofree gchar *subsys =
+		    g_strdup_printf("%04X%04X", priv->subsystem_vendor, priv->subsystem_model);
+		fu_device_add_instance_str(device, "SUBSYS", subsys);
 	}
-	if (priv->vendor != 0x0000 && priv->model != 0x0000) {
-		g_autofree gchar *devid = NULL;
-		devid = g_strdup_printf("%s\\VEN_%04X&DEV_%04X&REV_%02X",
-					subsystem,
-					priv->vendor,
-					priv->model,
-					priv->revision);
-		fu_device_add_instance_id(device, devid);
-	}
-	if (priv->vendor != 0x0000 && priv->model != 0x0000) {
-		g_autofree gchar *devid = NULL;
-		devid =
-		    g_strdup_printf("%s\\VEN_%04X&DEV_%04X", subsystem, priv->vendor, priv->model);
-		fu_device_add_instance_id(device, devid);
-	}
-	if (priv->vendor != 0x0000) {
-		g_autofree gchar *devid = NULL;
-		devid = g_strdup_printf("%s\\VEN_%04X", subsystem, priv->vendor);
-		fu_device_add_instance_id_full(device, devid, FU_DEVICE_INSTANCE_FLAG_ONLY_QUIRKS);
-	}
+	fu_device_add_instance_u8(device, "REV", priv->revision);
+
+	fu_device_build_instance_id_quirk(device, NULL, subsystem, "VEN", NULL);
+	fu_device_build_instance_id(device, NULL, subsystem, "VEN", "DEV", NULL);
+	fu_device_build_instance_id(device, NULL, subsystem, "VEN", "DEV", "REV", NULL);
+	fu_device_build_instance_id(device, NULL, subsystem, "VEN", "DEV", "SUBSYS", NULL);
+	fu_device_build_instance_id(device, NULL, subsystem, "VEN", "DEV", "SUBSYS", "REV", NULL);
 
 	/* add device class */
 	tmp = g_udev_device_get_sysfs_attr(priv->udev_device, "class");
-	if (tmp != NULL && g_str_has_prefix(tmp, "0x")) {
-		g_autofree gchar *class_id = g_utf8_strup(tmp + 2, -1);
-		g_autofree gchar *devid = NULL;
-		devid = g_strdup_printf("%s\\VEN_%04X&CLASS_%s", subsystem, priv->vendor, class_id);
-		fu_device_add_instance_id_full(device, devid, FU_DEVICE_INSTANCE_FLAG_ONLY_QUIRKS);
-	}
+	if (tmp != NULL && g_str_has_prefix(tmp, "0x"))
+		tmp += 2;
+	fu_device_add_instance_strup(device, "CLASS", tmp);
+	fu_device_build_instance_id_quirk(device, NULL, subsystem, "VEN", "CLASS", NULL);
 
 	/* add devtype */
-	tmp = g_udev_device_get_devtype(priv->udev_device);
-	if (tmp != NULL) {
-		g_autofree gchar *devtype = g_utf8_strup(tmp, -1);
-		g_autofree gchar *devid = NULL;
-		devid = g_strdup_printf("%s\\TYPE_%s", subsystem, devtype);
-		fu_device_add_instance_id_full(device, devid, FU_DEVICE_INSTANCE_FLAG_ONLY_QUIRKS);
-	}
+	fu_device_add_instance_strup(device, "TYPE", g_udev_device_get_devtype(priv->udev_device));
+	fu_device_build_instance_id_quirk(device, NULL, subsystem, "TYPE", NULL);
 
 	/* add the driver */
-	if (priv->driver != NULL) {
-		g_autofree gchar *devid = NULL;
-		devid = g_strdup_printf("%s\\DRIVER_%s", subsystem, priv->driver);
-		fu_device_add_instance_id_full(device, devid, FU_DEVICE_INSTANCE_FLAG_ONLY_QUIRKS);
-	}
+	fu_device_add_instance_str(device, "DRIVER", priv->driver);
+	fu_device_build_instance_id_quirk(device, NULL, subsystem, "DRIVER", NULL);
 
 	/* add subsystem to match in plugins */
 	if (subsystem != NULL) {

libfwupdplugin/fu-usb-device.c
@@ -174,7 +174,6 @@ fu_usb_device_query_hub(FuUsbDevice *self, GError **error)
 	gsize sz = 0;
 	guint16 value = 0x29;
 	guint8 data[0x0c] = {0x0};
-	g_autofree gchar *devid = NULL;
 
 	/* longer descriptor for SuperSpeed */
 	if (fu_usb_device_get_spec(self) >= 0x0300)
@@ -200,20 +199,12 @@ fu_usb_device_query_hub(FuUsbDevice *self, GError **error)
 
 	/* see http://www.usblyzer.com/usb-hub-class-decoder.htm */
 	if (sz == 0x09) {
-		devid = g_strdup_printf("USB\\VID_%04X&PID_%04X&HUB_%02X",
-					g_usb_device_get_vid(priv->usb_device),
-					g_usb_device_get_pid(priv->usb_device),
-					data[7]);
-		fu_device_add_instance_id(FU_DEVICE(self), devid);
+		fu_device_add_instance_u8(FU_DEVICE(self), "HUB", data[7]);
 	} else if (sz == 0x0c) {
-		devid = g_strdup_printf("USB\\VID_%04X&PID_%04X&HUB_%02X%02X",
-					g_usb_device_get_vid(priv->usb_device),
-					g_usb_device_get_pid(priv->usb_device),
-					data[11],
-					data[10]);
-		fu_device_add_instance_id(FU_DEVICE(self), devid);
+		g_autofree gchar *hub = g_strdup_printf("%02X%02X", data[11], data[10]);
+		fu_device_add_instance_str(FU_DEVICE(self), "HUB", hub);
 	}
-	return TRUE;
+	return fu_device_build_instance_id(FU_DEVICE(self), error, "VID", "PID", "HUB", NULL);
 }
 #endif
 
@@ -395,9 +386,6 @@ fu_usb_device_probe(FuDevice *device, GError **error)
 	FuUsbDevice *self = FU_USB_DEVICE(device);
 	FuUsbDevicePrivate *priv = GET_PRIVATE(self);
 	guint16 release;
-	g_autofree gchar *devid0 = NULL;
-	g_autofree gchar *devid1 = NULL;
-	g_autofree gchar *devid2 = NULL;
 	g_autofree gchar *platform_id = NULL;
 	g_autofree gchar *vendor_id = NULL;
 	g_autoptr(GPtrArray) intfs = NULL;
@@ -417,17 +405,12 @@ fu_usb_device_probe(FuDevice *device, GError **error)
 	}
 
 	/* add GUIDs in order of priority */
-	devid2 = g_strdup_printf("USB\\VID_%04X&PID_%04X&REV_%04X",
-				 g_usb_device_get_vid(priv->usb_device),
-				 g_usb_device_get_pid(priv->usb_device),
-				 release);
-	fu_device_add_instance_id(device, devid2);
-	devid1 = g_strdup_printf("USB\\VID_%04X&PID_%04X",
-				 g_usb_device_get_vid(priv->usb_device),
-				 g_usb_device_get_pid(priv->usb_device));
-	fu_device_add_instance_id(device, devid1);
-	devid0 = g_strdup_printf("USB\\VID_%04X", g_usb_device_get_vid(priv->usb_device));
-	fu_device_add_instance_id_full(device, devid0, FU_DEVICE_INSTANCE_FLAG_ONLY_QUIRKS);
+	fu_device_add_instance_u16(device, "VID", g_usb_device_get_vid(priv->usb_device));
+	fu_device_add_instance_u16(device, "PID", g_usb_device_get_pid(priv->usb_device));
+	fu_device_add_instance_u16(device, "REV", release);
+	fu_device_build_instance_id_quirk(device, NULL, "USB", "VID", NULL);
+	fu_device_build_instance_id(device, NULL, "USB", "VID", "PID", NULL);
+	fu_device_build_instance_id(device, NULL, "USB", "VID", "PID", "REV", NULL);
 
 	/* add the interface GUIDs */
 	intfs = g_usb_device_get_interfaces(priv->usb_device, error);
@@ -435,20 +418,18 @@ fu_usb_device_probe(FuDevice *device, GError **error)
 		return FALSE;
 	for (guint i = 0; i < intfs->len; i++) {
 		GUsbInterface *intf = g_ptr_array_index(intfs, i);
-		g_autofree gchar *intid1 = NULL;
-		g_autofree gchar *intid2 = NULL;
-		g_autofree gchar *intid3 = NULL;
-		intid1 = g_strdup_printf("USB\\CLASS_%02X&SUBCLASS_%02X&PROT_%02X",
-					 g_usb_interface_get_class(intf),
-					 g_usb_interface_get_subclass(intf),
-					 g_usb_interface_get_protocol(intf));
-		fu_device_add_instance_id_full(device, intid1, FU_DEVICE_INSTANCE_FLAG_ONLY_QUIRKS);
-		intid2 = g_strdup_printf("USB\\CLASS_%02X&SUBCLASS_%02X",
-					 g_usb_interface_get_class(intf),
-					 g_usb_interface_get_subclass(intf));
-		fu_device_add_instance_id_full(device, intid2, FU_DEVICE_INSTANCE_FLAG_ONLY_QUIRKS);
-		intid3 = g_strdup_printf("USB\\CLASS_%02X", g_usb_interface_get_class(intf));
-		fu_device_add_instance_id_full(device, intid3, FU_DEVICE_INSTANCE_FLAG_ONLY_QUIRKS);
+		fu_device_add_instance_u8(device, "CLASS", g_usb_interface_get_class(intf));
+		fu_device_add_instance_u8(device, "SUBCLASS", g_usb_interface_get_subclass(intf));
+		fu_device_add_instance_u8(device, "PROT", g_usb_interface_get_protocol(intf));
+		fu_device_build_instance_id_quirk(device, NULL, "USB", "CLASS", NULL);
+		fu_device_build_instance_id_quirk(device, NULL, "USB", "CLASS", "SUBCLASS", NULL);
+		fu_device_build_instance_id_quirk(device,
+						  NULL,
+						  "USB",
+						  "CLASS",
+						  "SUBCLASS",
+						  "PROT",
+						  NULL);
 	}
 
 	/* add 2 levels of parent IDs */

commit 4080ef637e44cf19815829d1e0fa35778f45c8fe

trivial: Do not show SATA drives as SCSI devices
This fixes a regression in c4ca02629487d3f26278f47e4720167b1ae04eb4
which meant the 'vendor' sysfs attribute wasn't being stripped before
being used as the vendor ID.

plugins/scsi/fu-scsi-device.c
@@ -21,6 +21,7 @@ fu_scsi_device_probe(FuDevice *device, GError **error)
 	const gchar *vendor;
 	const gchar *version;
 	g_autofree gchar *vendor_id = NULL;
+	g_autofree gchar *vendor_safe = NULL;
 	g_autoptr(GPtrArray) block_devs = NULL;
 
 	/* ignore */
@@ -34,7 +35,8 @@ fu_scsi_device_probe(FuDevice *device, GError **error)
 
 	/* vendor */
 	vendor = fu_udev_device_get_sysfs_attr(FU_UDEV_DEVICE(device), "vendor", NULL);
-	if (vendor == NULL || g_strcmp0(vendor, "ATA") == 0) {
+	vendor_safe = fu_common_strstrip(vendor);
+	if (vendor_safe == NULL || g_strcmp0(vendor_safe, "ATA") == 0) {
 		g_set_error_literal(error,
 				    FWUPD_ERROR,
 				    FWUPD_ERROR_NOT_SUPPORTED,
@@ -42,7 +44,7 @@ fu_scsi_device_probe(FuDevice *device, GError **error)
 		return FALSE;
 	}
 	fu_device_set_vendor(device, vendor);
-	vendor_id = g_strdup_printf("SCSI:%s", vendor);
+	vendor_id = g_strdup_printf("SCSI:%s", vendor_safe);
 	fu_device_add_vendor_id(device, vendor_id);
 
 	/* name */