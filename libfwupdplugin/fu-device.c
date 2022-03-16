c460d9a85fa02403e211335c5e5b6de292837f02
Richard Hughes <richard@hughsie.com>
2022-03-16 17:45:47 +0000

    Set the physical ID automatically when setting the device proxy

--- libfwupdplugin/fu-device.c
+++ libfwupdplugin/fu-device.c
@@ -1116,6 +1116,8 @@ fu_device_set_proxy(FuDevice *self, FuDevice *proxy)
 	if (proxy != NULL) {
 		if (priv->ctx == NULL && fu_device_get_context(proxy) != NULL)
 			fu_device_set_context(self, fu_device_get_context(proxy));
+		if (priv->physical_id == NULL && fu_device_get_physical_id(proxy) != NULL)
+			fu_device_set_physical_id(self, fu_device_get_physical_id(proxy));
 	}
 
 	if (priv->proxy != NULL)
