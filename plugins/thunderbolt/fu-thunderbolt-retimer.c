1fd729dba439bd0ce5cad894ce2e3be75e7daddf
Mario Limonciello <superm1@gmail.com>
2022-03-24 11:04:55 -0500

    Don't export USB4 host controllers as updatable if they don't have unique GUIDs Fixes: #4438

--- plugins/thunderbolt/fu-thunderbolt-retimer.c
+++ plugins/thunderbolt/fu-thunderbolt-retimer.c
@@ -147,6 +147,7 @@ fu_thunderbolt_retimer_init(FuThunderboltRetimer *self)
 	    "that forms two separate electrical link segments");
 	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_DUAL_IMAGE);
 	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_INTERNAL);
+	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_UPDATABLE);
 	fu_device_add_internal_flag(FU_DEVICE(self), FU_DEVICE_INTERNAL_FLAG_NO_AUTO_REMOVE);
 }
 
