8412b2d59212ef1db2c8864d751ab80a841531d7
Andrii Dushko <andrii.dushko@developex.net>
2022-04-04 14:05:04 +0300

    Add a plugin for Corsair devices
    Supported devices:
     * SABRE RGB PRO WIRELESS
     * SLIPSTREAM WIRELESS USB Receiver

---
+++ plugins/corsair/fu-corsair-device.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2021 Andrii Dushko <andrii.dushko@developex.net>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include <fwupdplugin.h>
+
+#define FU_TYPE_CORSAIR_DEVICE (fu_corsair_device_get_type())
+G_DECLARE_FINAL_TYPE(FuCorsairDevice, fu_corsair_device, FU, CORSAIR_DEVICE, FuUsbDevice)
+
+struct _FuCorsairDeviceClass {
+	FuUsbDeviceClass parent_class;
+};
