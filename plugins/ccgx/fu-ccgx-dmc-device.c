4b2a452ac98f068e95fe1677af0743c3aa20a2ab
Ryan Lee <Ryan.Lee@infineon.com>
2022-04-27 17:29:43 +0900

    ccgx: add braces in attach

--- plugins/ccgx/fu-ccgx-dmc-device.c
+++ plugins/ccgx/fu-ccgx-dmc-device.c
@@ -631,22 +631,24 @@ fu_ccgx_dmc_device_attach(FuDevice *device, FuProgress *progress, GError **error
 			if (!fu_ccgx_dmc_device_send_download_trigger(self,
 								      self->trigger_code,
 								      error)) {
-				if (manual_replug)
+				if (manual_replug) {
 					fu_device_remove_flag(device,
 							      FWUPD_DEVICE_FLAG_NEEDS_ACTIVATION);
-				else
+				} else {
 					fu_device_remove_flag(device,
 							      FWUPD_DEVICE_FLAG_WAIT_FOR_REPLUG);
+				}
 				g_prefix_error(error, "download trigger error: ");
 				return FALSE;
 			}
 		}
 	} else if (self->update_model == DMC_UPDATE_MODEL_PENDING_RESET) {
 		if (!fu_ccgx_dmc_device_send_sort_reset(self, manual_replug, error)) {
-			if (manual_replug)
+			if (manual_replug) {
 				fu_device_remove_flag(device, FWUPD_DEVICE_FLAG_NEEDS_ACTIVATION);
-			else
+			} else {
 				fu_device_remove_flag(device, FWUPD_DEVICE_FLAG_WAIT_FOR_REPLUG);
+			}
 			g_prefix_error(error, "soft reset error: ");
 			return FALSE;
 		}
