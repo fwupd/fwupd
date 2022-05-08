a8dd34c7113e7e21829c4439b7e519b0bbe28ad7
Robert Ancell <robert.ancell@canonical.com>
2022-05-09 10:29:55 +1200

    Fix more copy-paste documentation errors

--- libfwupd/fwupd-release.c
+++ libfwupd/fwupd-release.c
@@ -1269,7 +1269,7 @@ fwupd_release_get_license(FwupdRelease *self)
 /**
  * fwupd_release_set_license:
  * @self: a #FwupdRelease
- * @license: (nullable): the description
+ * @license: (nullable): the update license.
  *
  * Sets the update license.
  *
@@ -1310,7 +1310,7 @@ fwupd_release_get_name(FwupdRelease *self)
 /**
  * fwupd_release_set_name:
  * @self: a #FwupdRelease
- * @name: (nullable): the description
+ * @name: (nullable): the update name.
  *
  * Sets the update name.
  *
