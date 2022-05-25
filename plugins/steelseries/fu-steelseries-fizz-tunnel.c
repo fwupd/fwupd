64f9c1d2d3ef7794bd74e1990a3ea840e610029d
Gaël PORTAY <gael.portay@collabora.com>
2022-05-25 07:48:27 +0200

    steelseries: fizz-tunnel: Enable polling

--- plugins/steelseries/fu-steelseries-fizz-tunnel.c
+++ plugins/steelseries/fu-steelseries-fizz-tunnel.c
@@ -16,6 +16,59 @@ struct _FuSteelseriesFizzTunnel {
 G_DEFINE_TYPE(FuSteelseriesFizzTunnel, fu_steelseries_fizz_tunnel, FU_TYPE_DEVICE)
 
 static gboolean
+fu_steelseries_fizz_tunnel_ping(FuDevice *device, gboolean *reached, GError **error)
+{
+	FuDevice *parent = fu_device_get_parent(device);
+	guint8 status;
+	guint8 level;
+	g_autoptr(GError) error_local = NULL;
+
+	if (!fu_steelseries_fizz_connection_status(parent, &status, error)) {
+		g_prefix_error(error, "failed to get connection status: ");
+		return FALSE;
+	}
+	g_debug("ConnectionStatus: %u", status);
+	*reached = status != STEELSERIES_FIZZ_CONNECTION_STATUS_NOT_CONNECTED;
+	if (!*reached) {
+		/* success */
+		return TRUE;
+	}
+
+	/* ping device anyway */
+	if (!fu_steelseries_fizz_battery_level(fu_device_get_parent(device),
+					       TRUE,
+					       &level,
+					       &error_local)) {
+		*reached = FALSE;
+
+		if (!g_error_matches(error_local,
+				     G_USB_DEVICE_ERROR,
+				     G_USB_DEVICE_ERROR_TIMED_OUT)) {
+			/* failure */
+			*error = g_steal_pointer(&error_local);
+			return FALSE;
+		}
+
+		/* success */
+		return TRUE;
+	}
+	g_debug("BatteryLevel: 0x%02x", level);
+	/*
+	 * CHARGING: Most significant bit. When bit is set to 1 it means battery is currently
+	 * charging/plugged in
+	 *
+	 * LEVEL: 7 least significant bit value of the battery. Values are between 2-21, to get %
+	 * you can do (LEVEL - 1) * 5
+	 */
+	fu_device_set_battery_level(device,
+				    ((level & STEELSERIES_FIZZ_BATTERY_LEVEL_STATUS_BITS) - 1U) *
+					5U);
+
+	/* success */
+	return TRUE;
+}
+
+static gboolean
 fu_steelseries_fizz_tunnel_wait_for_reconnect_cb(FuDevice *device,
 						 gpointer user_data,
 						 GError **error)
@@ -125,15 +178,20 @@ fu_steelseries_fizz_tunnel_setup(FuDevice *device, GError **error)
 	FuDevice *parent = fu_device_get_parent(device);
 	guint32 calculated_crc;
 	guint32 stored_crc;
-	guint8 status;
+	gboolean reached;
 	guint8 fs = STEELSERIES_FIZZ_FILESYSTEM_MOUSE;
 	guint8 id = STEELSERIES_FIZZ_MOUSE_FILESYSTEM_BACKUP_APP_ID;
+	g_autoptr(GError) error_local = NULL;
 	g_autofree gchar *version = NULL;
 
-	if (!fu_steelseries_fizz_connection_status(parent, &status, error))
-		return FALSE;
-	g_debug("ConnectionStatus: %u", status);
-	if (status == STEELSERIES_FIZZ_CONNECTION_STATUS_NOT_CONNECTED) {
+	/* ping */
+	if (!fu_steelseries_fizz_tunnel_ping(device, &reached, &error_local)) {
+		g_debug("ignoring error on ping: %s", error_local->message);
+
+		/* success */
+		return TRUE;
+	}
+	if (!reached) {
 		fu_device_add_flag(device, FWUPD_DEVICE_FLAG_UNREACHABLE);
 
 		/* success */
@@ -173,39 +231,68 @@ fu_steelseries_fizz_tunnel_setup(FuDevice *device, GError **error)
 }
 
 static gboolean
-fu_steelseries_fizz_tunnel_prepare(FuDevice *device,
-				   FuProgress *progress,
-				   FwupdInstallFlags flags,
-				   GError **error)
+fu_steelseries_fizz_tunnel_poll(FuDevice *device, GError **error)
 {
 	FuDevice *parent = fu_device_get_parent(device);
-	guint8 status;
-	guint8 level;
+	guint32 calculated_crc;
+	guint32 stored_crc;
+	gboolean reached;
+	guint8 fs = STEELSERIES_FIZZ_FILESYSTEM_MOUSE;
+	guint8 id = STEELSERIES_FIZZ_MOUSE_FILESYSTEM_BACKUP_APP_ID;
+	g_autoptr(GError) error_local = NULL;
+	g_autoptr(FuDeviceLocker) locker = NULL;
+	g_autofree gchar *version = NULL;
 
-	if (!fu_steelseries_fizz_connection_status(parent, &status, error)) {
-		g_prefix_error(error, "failed to get connection status: ");
+	/* open */
+	locker = fu_device_locker_new(parent, error);
+	if (locker == NULL)
+		return FALSE;
+
+	if (!fu_steelseries_fizz_tunnel_ping(device, &reached, error)) {
+		g_prefix_error(error, "failed to ping: ");
 		return FALSE;
 	}
-	g_debug("ConnectionStatus: %u", status);
-	if (status == STEELSERIES_FIZZ_CONNECTION_STATUS_NOT_CONNECTED) {
-		g_set_error(error, FWUPD_ERROR, FWUPD_ERROR_NOT_FOUND, "device is unreachable");
-		return FALSE;
+
+	if (!reached) {
+		fu_device_add_flag(device, FWUPD_DEVICE_FLAG_UNREACHABLE);
+
+		/* success */
+		return TRUE;
 	}
-	if (!fu_steelseries_fizz_battery_level(parent, TRUE, &level, error)) {
-		g_prefix_error(error, "failed to get battery level: ");
-		return FALSE;
+
+	/* "deferred" setup */
+	version = fu_steelseries_fizz_version(parent, TRUE, &error_local);
+	if (version == NULL) {
+		g_debug("ignoring error on version: %s", error_local->message);
+
+		/* success */
+		return TRUE;
 	}
-	g_debug("BatteryLevel: 0x%02x", level);
-	/*
-	 * CHARGING: Most significant bit. When bit is set to 1 it means battery is currently
-	 * charging/plugged in
-	 *
-	 * LEVEL: 7 least significant bit value of the battery. Values are between 2-21, to get %
-	 * you can do (LEVEL - 1) * 5
-	 */
-	fu_device_set_battery_level(device,
-				    ((level & STEELSERIES_FIZZ_BATTERY_LEVEL_STATUS_BITS) - 1U) *
-					5U);
+	fu_device_set_version(device, version);
+
+	if (!fu_steelseries_fizz_file_crc32(parent,
+					    TRUE,
+					    fs,
+					    id,
+					    &calculated_crc,
+					    &stored_crc,
+					    &error_local)) {
+		g_debug("ignoring error on get file CRC32 from FS 0x%02x ID 0x%02x: %s",
+			fs,
+			id,
+			error_local->message);
+
+		/* success */
+		return TRUE;
+	}
+
+	if (calculated_crc != stored_crc)
+		g_warning("%s: checksum mismatch, got 0x%08x, expected 0x%08x",
+			  fu_device_get_name(device),
+			  calculated_crc,
+			  stored_crc);
+
+	fu_device_remove_flag(device, FWUPD_DEVICE_FLAG_UNREACHABLE);
 
 	/* success */
 	return TRUE;
@@ -284,7 +371,7 @@ fu_steelseries_fizz_tunnel_class_init(FuSteelseriesFizzTunnelClass *klass)
 	klass_device->attach = fu_steelseries_fizz_tunnel_attach;
 	klass_device->probe = fu_steelseries_fizz_tunnel_probe;
 	klass_device->setup = fu_steelseries_fizz_tunnel_setup;
-	klass_device->prepare = fu_steelseries_fizz_tunnel_prepare;
+	klass_device->poll = fu_steelseries_fizz_tunnel_poll;
 	klass_device->write_firmware = fu_steelseries_fizz_tunnel_write_firmware;
 	klass_device->read_firmware = fu_steelseries_fizz_tunnel_read_firmware;
 	klass_device->set_progress = fu_steelseries_fizz_tunnel_set_progress;
@@ -297,6 +384,7 @@ fu_steelseries_fizz_tunnel_init(FuSteelseriesFizzTunnel *self)
 	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_UPDATABLE);
 	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_CAN_VERIFY_IMAGE);
 	fu_device_add_internal_flag(FU_DEVICE(self), FU_DEVICE_INTERNAL_FLAG_USE_PARENT_FOR_OPEN);
+	fu_device_add_internal_flag(FU_DEVICE(self), FU_DEVICE_INTERNAL_AUTO_PAUSE_POLLING);
 	fu_device_add_protocol(FU_DEVICE(self), "com.steelseries.fizz");
 	fu_device_set_logical_id(FU_DEVICE(self), "tunnel");
 	fu_device_set_install_duration(FU_DEVICE(self), 38);				  /* 38 s */
