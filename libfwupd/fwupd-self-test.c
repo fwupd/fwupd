25846ca7473b1a7931011b914a2f3fb908781588
Richard Hughes <richard@hughsie.com>
2022-06-30 15:49:08 +0100

    Increase the coverage of the self tests

--- libfwupd/fwupd-self-test.c
+++ libfwupd/fwupd-self-test.c
@@ -147,6 +147,11 @@ fwupd_enums_func(void)
 		g_assert_cmpstr(tmp, !=, NULL);
 		g_assert_cmpint(fwupd_version_format_from_string(tmp), ==, i);
 	}
+	for (guint i = 1; i < FWUPD_REMOTE_KIND_LAST; i++) {
+		const gchar *tmp = fwupd_remote_kind_to_string(i);
+		g_assert_cmpstr(tmp, !=, NULL);
+		g_assert_cmpint(fwupd_remote_kind_from_string(tmp), ==, i);
+	}
 
 	/* bitfield */
 	for (guint64 i = 1; i < FWUPD_DEVICE_FLAG_UNKNOWN; i *= 2) {
