fwupd/fwupd

commit 030a0d51c16c941b17f17f1922f33098c0bf4759

trivial: quiet the modem manager error about unable to probe
We don't show messages in the logs about unable to probe other
device types by default, MM should be no different.
This gets rid of this message coming up every fwupd startup on a
EM05-G:
"failed to probe MM device: modem cannot be put in programming mode"

plugins/modem-manager/fu-plugin-modem-manager.c
@@ -99,7 +99,7 @@ fu_plugin_mm_udev_device_ports_timeout(gpointer user_data)
 				     fu_device_get_physical_id(FU_DEVICE(priv->shadow_device)));
 	if (dev != NULL) {
 		if (!fu_device_probe(FU_DEVICE(dev), &error)) {
-			g_warning("failed to probe MM device: %s", error->message);
+			g_debug("failed to probe MM device: %s", error->message);
 		} else {
 			fu_plugin_device_add(plugin, FU_DEVICE(dev));
 		}

commit 611cf51fc8dde2f30d9889f790bf6d0a1c54d2e3

trivial: lower modem manager probing verbosity in one other place
Fixes: 030a0d51c ("trivial: quiet the modem manager error about unable to probe")

plugins/modem-manager/fu-plugin-modem-manager.c
@@ -259,7 +259,7 @@ fu_plugin_mm_device_add(FuPlugin *plugin, MMObject *modem)
 	}
 	dev = fu_mm_device_new(fu_plugin_get_context(plugin), priv->manager, modem);
 	if (!fu_device_setup(FU_DEVICE(dev), &error)) {
-		g_warning("failed to probe MM device: %s", error->message);
+		g_debug("failed to probe MM device: %s", error->message);
 		return;
 	}
 	fu_plugin_mm_ensure_modem_power_inhibit(plugin, FU_DEVICE(dev));