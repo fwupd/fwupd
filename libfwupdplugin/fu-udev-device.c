531f5db37487e22917b44cb92c7d505fe1ae596c
Richard Hughes <richard@hughsie.com>
2022-07-21 09:30:13 +0100

    Add UDev devices to the _REQUIRES_ACQUIESCE opt-in

--- libfwupdplugin/fu-udev-device.c
+++ libfwupdplugin/fu-udev-device.c
@@ -2119,6 +2119,7 @@ fu_udev_device_init(FuUdevDevice *self)
 {
 	FuUdevDevicePrivate *priv = GET_PRIVATE(self);
 	priv->flags = FU_UDEV_DEVICE_FLAG_OPEN_READ | FU_UDEV_DEVICE_FLAG_OPEN_WRITE;
+	fu_device_add_internal_flag(FU_DEVICE(self), FU_DEVICE_INTERNAL_FLAG_REQUIRES_ACQUIESCE);
 }
 
 static void
