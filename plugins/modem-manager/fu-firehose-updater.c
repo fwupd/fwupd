1dab4f67cfe54a91d29183af4e87f62f0608d1bb
Richard Hughes <richard@hughsie.com>
2022-09-09 12:01:15 +0100

    modem-manager: Fix debug format string
    PVS: The size_t type argument is expected.

--- plugins/modem-manager/fu-firehose-updater.c
+++ plugins/modem-manager/fu-firehose-updater.c
@@ -691,9 +691,9 @@ fu_firehose_updater_run_action_program(FuFirehoseUpdater *self,
 	while ((payload_size + (guint)program_sector_size) <= max_payload_size)
 		payload_size += (guint)program_sector_size;
 
-	g_debug("sending program file '%s' (%zu bytes)",
+	g_debug("sending program file '%s' (0x%x bytes)",
 		program_filename,
-		g_bytes_get_size(program_file));
+		(guint)g_bytes_get_size(program_file));
 	if (!fu_firehose_updater_send_program_file(self,
 						   program_filename,
 						   program_file,
