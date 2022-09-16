fwupd/fwupd

commit 0c51630991725d29255a4e4774f92185f6a24aaa

Check firmware magic in a more standard way
Some parsers are ignoring the magic when using _FLAG_IGNORE_CHECKSUM
(which is wrong; fuzzers have no problem with enforcing a static prefix)
and other either disregard the offset or check the magic in an unsafe
way. Also, use FWUPD_ERROR_INVALID_FILE consistently for magic failure.

Add a vfunc, and move all the clever code into one place.

plugins/pixart-rf/fu-pxi-firmware.c
@@ -13,6 +13,8 @@
 #define PIXART_RF_FW_HEADER_SIZE       32 /* bytes */
 #define PIXART_RF_FW_HEADER_TAG_OFFSET 24
 
+#define PIXART_RF_FW_HEADER_MAGIC 0x55AA55AA55AA55AA
+
 struct _FuPxiFirmware {
 	FuFirmware parent_instance;
 	gchar *model_name;
@@ -35,6 +37,36 @@ fu_pxi_firmware_export(FuFirmware *firmware, FuFirmwareExportFlags flags, XbBuil
 }
 
 static gboolean
+fu_pxi_firmware_check_magic(FuFirmware *firmware, GBytes *fw, gsize offset, GError **error)
+{
+	guint64 magic = 0;
+
+	/* is a footer */
+	if (!fu_memread_uint64_safe(g_bytes_get_data(fw, NULL),
+				    g_bytes_get_size(fw),
+				    g_bytes_get_size(fw) - PIXART_RF_FW_HEADER_SIZE +
+					PIXART_RF_FW_HEADER_TAG_OFFSET,
+				    &magic,
+				    G_LITTLE_ENDIAN,
+				    error)) {
+		g_prefix_error(error, "failed to read magic: ");
+		return FALSE;
+	}
+	if (magic != PIXART_RF_FW_HEADER_MAGIC) {
+		g_set_error(error,
+			    FWUPD_ERROR,
+			    FWUPD_ERROR_INVALID_FILE,
+			    "invalid magic, expected 0x%08X got 0x%08X",
+			    (guint32)PIXART_RF_FW_HEADER_MAGIC,
+			    (guint32)magic);
+		return FALSE;
+	}
+
+	/* success */
+	return TRUE;
+}
+
+static gboolean
 fu_pxi_firmware_parse(FuFirmware *firmware,
 		      GBytes *fw,
 		      gsize offset,
@@ -43,30 +75,14 @@ fu_pxi_firmware_parse(FuFirmware *firmware,
 {
 	FuPxiFirmware *self = FU_PXI_FIRMWARE(firmware);
 	const guint8 *buf;
-	const guint8 tag[] = {
-	    0x55,
-	    0xAA,
-	    0x55,
-	    0xAA,
-	    0x55,
-	    0xAA,
-	    0x55,
-	    0xAA,
-	};
 	gsize bufsz = 0;
 	guint32 version_raw = 0;
 	guint8 fw_header[PIXART_RF_FW_HEADER_SIZE];
 	guint8 model_name[FU_PXI_DEVICE_MODEL_NAME_LEN] = {0x0};
 	g_autofree gchar *version = NULL;
 
-	/* get buf */
-	buf = g_bytes_get_data(fw, &bufsz);
-	if (bufsz < sizeof(fw_header)) {
-		g_set_error(error, G_IO_ERROR, G_IO_ERROR_FAILED, "firmware invalid, too small!");
-		return FALSE;
-	}
-
 	/* get fw header from buf */
+	buf = g_bytes_get_data(fw, &bufsz);
 	if (!fu_memcpy_safe(fw_header,
 			    sizeof(fw_header),
 			    0x0,
@@ -80,21 +96,6 @@ fu_pxi_firmware_parse(FuFirmware *firmware,
 	}
 	if (g_getenv("FWUPD_PIXART_RF_VERBOSE") != NULL) {
 		fu_dump_raw(G_LOG_DOMAIN, "fw header", fw_header, sizeof(fw_header));
-	}
-
-	/* check the tag from fw header is correct */
-	for (guint32 i = 0x0; i < sizeof(tag); i++) {
-		guint8 tmp = 0;
-		if (!fu_memread_uint8_safe(fw_header,
-					   sizeof(fw_header),
-					   i + PIXART_RF_FW_HEADER_TAG_OFFSET,
-					   &tmp,
-					   error))
-			return FALSE;
-		if (tmp != tag[i]) {
-			g_set_error(error, G_IO_ERROR, G_IO_ERROR_FAILED, "Fw tag is incorrect");
-			return FALSE;
-		}
 	}
 
 	/* set fw version */
@@ -226,6 +227,7 @@ fu_pxi_firmware_class_init(FuPxiFirmwareClass *klass)
 	GObjectClass *object_class = G_OBJECT_CLASS(klass);
 	FuFirmwareClass *klass_firmware = FU_FIRMWARE_CLASS(klass);
 	object_class->finalize = fu_pxi_firmware_finalize;
+	klass_firmware->check_magic = fu_pxi_firmware_check_magic;
 	klass_firmware->parse = fu_pxi_firmware_parse;
 	klass_firmware->build = fu_pxi_firmware_build;
 	klass_firmware->write = fu_pxi_firmware_write;

libfwupdplugin/fu-self-test.c
@@ -1834,13 +1834,13 @@ fu_firmware_raw_aligned_func(void)
 	g_autoptr(GBytes) blob = g_bytes_new_static("hello", 5);
 
 	/* no alignment */
-	ret = fu_firmware_parse(firmware1, blob, FWUPD_INSTALL_FLAG_NONE, &error);
+	ret = fu_firmware_parse(firmware1, blob, FWUPD_INSTALL_FLAG_NO_SEARCH, &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 
 	/* invalid alignment */
 	fu_firmware_set_alignment(firmware2, FU_FIRMWARE_ALIGNMENT_4K);
-	ret = fu_firmware_parse(firmware2, blob, FWUPD_INSTALL_FLAG_NONE, &error);
+	ret = fu_firmware_parse(firmware2, blob, FWUPD_INSTALL_FLAG_NO_SEARCH, &error);
 	g_assert_error(error, FWUPD_ERROR, FWUPD_ERROR_INVALID_FILE);
 	g_assert_false(ret);
 }
@@ -1866,7 +1866,7 @@ fu_firmware_ihex_func(void)
 	data_file = fu_bytes_get_contents(filename_hex, &error);
 	g_assert_no_error(error);
 	g_assert_nonnull(data_file);
-	ret = fu_firmware_parse(firmware, data_file, FWUPD_INSTALL_FLAG_NONE, &error);
+	ret = fu_firmware_parse(firmware, data_file, FWUPD_INSTALL_FLAG_NO_SEARCH, &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 	data_fw = fu_firmware_get_bytes(firmware, &error);
@@ -1922,7 +1922,7 @@ fu_firmware_ihex_signed_func(void)
 	data_file = fu_bytes_get_contents(filename_shex, &error);
 	g_assert_no_error(error);
 	g_assert_nonnull(data_file);
-	ret = fu_firmware_parse(firmware, data_file, FWUPD_INSTALL_FLAG_NONE, &error);
+	ret = fu_firmware_parse(firmware, data_file, FWUPD_INSTALL_FLAG_NO_SEARCH, &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 	data_fw = fu_firmware_get_bytes(firmware, &error);
@@ -1970,7 +1970,7 @@ fu_firmware_ihex_offset_func(void)
 			":00000001FF\n");
 
 	/* check we can load it too */
-	ret = fu_firmware_parse(firmware_verify, data_bin, FWUPD_INSTALL_FLAG_NONE, &error);
+	ret = fu_firmware_parse(firmware_verify, data_bin, FWUPD_INSTALL_FLAG_NO_SEARCH, &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 	g_assert_cmpint(fu_firmware_get_addr(firmware_verify), ==, 0x80000000);
@@ -1996,7 +1996,7 @@ fu_firmware_srec_func(void)
 	data_srec = fu_bytes_get_contents(filename_srec, &error);
 	g_assert_no_error(error);
 	g_assert_nonnull(data_srec);
-	ret = fu_firmware_parse(firmware, data_srec, FWUPD_INSTALL_FLAG_NONE, &error);
+	ret = fu_firmware_parse(firmware, data_srec, FWUPD_INSTALL_FLAG_NO_SEARCH, &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 	data_bin = fu_firmware_get_bytes(firmware, &error);
@@ -2033,7 +2033,7 @@ fu_firmware_fdt_func(void)
 	data = fu_bytes_get_contents(filename, &error);
 	g_assert_no_error(error);
 	g_assert_nonnull(data);
-	ret = fu_firmware_parse(firmware, data, FWUPD_INSTALL_FLAG_NONE, &error);
+	ret = fu_firmware_parse(firmware, data, FWUPD_INSTALL_FLAG_NO_SEARCH, &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 	g_assert_cmpint(fu_fdt_firmware_get_cpuid(FU_FDT_FIRMWARE(firmware)), ==, 0x0);
@@ -2081,7 +2081,7 @@ fu_firmware_fit_func(void)
 	data = fu_bytes_get_contents(filename, &error);
 	g_assert_no_error(error);
 	g_assert_nonnull(data);
-	ret = fu_firmware_parse(firmware, data, FWUPD_INSTALL_FLAG_NONE, &error);
+	ret = fu_firmware_parse(firmware, data, FWUPD_INSTALL_FLAG_NO_SEARCH, &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 	g_assert_cmpint(fu_fit_firmware_get_timestamp(FU_FIT_FIRMWARE(firmware)), ==, 0x629D4ABD);
@@ -2246,7 +2246,7 @@ fu_firmware_dfuse_func(void)
 	roundtrip_orig = fu_bytes_get_contents(filename, &error);
 	g_assert_no_error(error);
 	g_assert_nonnull(roundtrip_orig);
-	ret = fu_firmware_parse(firmware, roundtrip_orig, FWUPD_INSTALL_FLAG_NONE, &error);
+	ret = fu_firmware_parse(firmware, roundtrip_orig, FWUPD_INSTALL_FLAG_NO_SEARCH, &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 	g_assert_cmpint(fu_dfu_firmware_get_vid(FU_DFU_FIRMWARE(firmware)), ==, 0x1234);
@@ -2390,7 +2390,7 @@ fu_firmware_linear_func(void)
 	g_assert_cmpint(g_bytes_get_size(blob3), ==, 1024);
 
 	/* parse them back */
-	ret = fu_firmware_parse(firmware2, blob3, FWUPD_INSTALL_FLAG_NONE, &error);
+	ret = fu_firmware_parse(firmware2, blob3, FWUPD_INSTALL_FLAG_NO_SEARCH, &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 	str = fu_firmware_to_string(firmware2);
@@ -2417,7 +2417,7 @@ fu_firmware_dfu_func(void)
 	data_dfu = fu_bytes_get_contents(filename_dfu, &error);
 	g_assert_no_error(error);
 	g_assert_nonnull(data_dfu);
-	ret = fu_firmware_parse(firmware, data_dfu, FWUPD_INSTALL_FLAG_NONE, &error);
+	ret = fu_firmware_parse(firmware, data_dfu, FWUPD_INSTALL_FLAG_NO_SEARCH, &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 	g_assert_cmpint(fu_dfu_firmware_get_vid(FU_DFU_FIRMWARE(firmware)), ==, 0x1234);
@@ -2454,7 +2454,7 @@ fu_firmware_ifwi_cpd_func(void)
 	data_ifwi_cpd = fu_bytes_get_contents(filename_ifwi_cpd, &error);
 	g_assert_no_error(error);
 	g_assert_nonnull(data_ifwi_cpd);
-	ret = fu_firmware_parse(firmware, data_ifwi_cpd, FWUPD_INSTALL_FLAG_NONE, &error);
+	ret = fu_firmware_parse(firmware, data_ifwi_cpd, FWUPD_INSTALL_FLAG_NO_SEARCH, &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 	g_assert_cmpint(fu_firmware_get_idx(firmware), ==, 0x1234);
@@ -2492,7 +2492,7 @@ fu_firmware_ifwi_fpt_func(void)
 	data_ifwi_fpt = fu_bytes_get_contents(filename_ifwi_fpt, &error);
 	g_assert_no_error(error);
 	g_assert_nonnull(data_ifwi_fpt);
-	ret = fu_firmware_parse(firmware, data_ifwi_fpt, FWUPD_INSTALL_FLAG_NONE, &error);
+	ret = fu_firmware_parse(firmware, data_ifwi_fpt, FWUPD_INSTALL_FLAG_NO_SEARCH, &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 	data_bin = fu_firmware_write(firmware, &error);
@@ -2528,7 +2528,7 @@ fu_firmware_oprom_func(void)
 	data_oprom = fu_bytes_get_contents(filename_oprom, &error);
 	g_assert_no_error(error);
 	g_assert_nonnull(data_oprom);
-	ret = fu_firmware_parse(firmware, data_oprom, FWUPD_INSTALL_FLAG_NONE, &error);
+	ret = fu_firmware_parse(firmware, data_oprom, FWUPD_INSTALL_FLAG_NO_SEARCH, &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 	g_assert_cmpint(fu_firmware_get_idx(firmware), ==, 0x1);
@@ -2561,7 +2561,7 @@ fu_firmware_dfu_patch_func(void)
 	data_dfu = fu_bytes_get_contents(filename_dfu, &error);
 	g_assert_no_error(error);
 	g_assert_nonnull(data_dfu);
-	ret = fu_firmware_parse(firmware, data_dfu, FWUPD_INSTALL_FLAG_NONE, &error);
+	ret = fu_firmware_parse(firmware, data_dfu, FWUPD_INSTALL_FLAG_NO_SEARCH, &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 
@@ -3102,7 +3102,9 @@ fu_firmware_builder_round_trip_func(void)
 		blob = fu_firmware_write(firmware1, &error);
 		g_assert_no_error(error);
 		g_assert_nonnull(blob);
-		ret = fu_firmware_parse(firmware3, blob, FWUPD_INSTALL_FLAG_NONE, &error);
+		ret = fu_firmware_parse(firmware3, blob, FWUPD_INSTALL_FLAG_NO_SEARCH, &error);
+		if (!ret)
+			g_prefix_error(&error, "%s: ", map[i].xml_fn);
 		g_assert_no_error(error);
 		g_assert_true(ret);
 

plugins/acpi-phat/fu-self-test.c
@@ -28,7 +28,10 @@ fu_acpi_phat_parse_func(void)
 	blob = fu_bytes_get_contents(fn, &error);
 	g_assert_no_error(error);
 	g_assert_nonnull(blob);
-	ret = fu_firmware_parse(phat, blob, FWUPD_INSTALL_FLAG_FORCE, &error);
+	ret = fu_firmware_parse(phat,
+				blob,
+				FWUPD_INSTALL_FLAG_FORCE | FWUPD_INSTALL_FLAG_NO_SEARCH,
+				&error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 	str = fu_acpi_phat_to_report_string(FU_ACPI_PHAT(phat));

plugins/bcm57xx/fu-self-test.c
@@ -73,7 +73,7 @@ fu_bcm57xx_firmware_talos_func(void)
 	blob = fu_bytes_get_contents(fn, &error);
 	g_assert_no_error(error);
 	g_assert_nonnull(blob);
-	ret = fu_firmware_parse(firmware, blob, FWUPD_INSTALL_FLAG_NONE, &error);
+	ret = fu_firmware_parse(firmware, blob, FWUPD_INSTALL_FLAG_NO_SEARCH, &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 	images = fu_firmware_get_images(firmware);

plugins/dell/fu-self-test.c
@@ -241,19 +241,20 @@ fu_plugin_dell_tpm_func(gconstpointer user_data)
 					      device_v20,
 					      blob_fw,
 					      progress,
-					      FWUPD_INSTALL_FLAG_NONE,
+					      FWUPD_INSTALL_FLAG_NO_SEARCH,
 					      &error);
 	g_assert_error(error, FWUPD_ERROR, FWUPD_ERROR_NOT_SUPPORTED);
 	g_assert_false(ret);
 	g_clear_error(&error);
 
 	/* test override */
-	ret = fu_plugin_runner_write_firmware(self->plugin_uefi_capsule,
-					      device_v20,
-					      blob_fw,
-					      progress,
-					      FWUPD_INSTALL_FLAG_FORCE,
-					      &error);
+	ret =
+	    fu_plugin_runner_write_firmware(self->plugin_uefi_capsule,
+					    device_v20,
+					    blob_fw,
+					    progress,
+					    FWUPD_INSTALL_FLAG_NO_SEARCH | FWUPD_INSTALL_FLAG_FORCE,
+					    &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 

plugins/redfish/fu-self-test.c
@@ -337,7 +337,7 @@ fu_test_redfish_update_func(gconstpointer user_data)
 					      dev,
 					      blob_fw,
 					      progress,
-					      FWUPD_INSTALL_FLAG_NONE,
+					      FWUPD_INSTALL_FLAG_NO_SEARCH,
 					      &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
@@ -348,7 +348,7 @@ fu_test_redfish_update_func(gconstpointer user_data)
 					      dev,
 					      blob_fw,
 					      progress,
-					      FWUPD_INSTALL_FLAG_NONE,
+					      FWUPD_INSTALL_FLAG_NO_SEARCH,
 					      &error);
 	g_assert_error(error, FWUPD_ERROR, FWUPD_ERROR_WRITE);
 	g_assert_false(ret);

plugins/synaptics-prometheus/fu-self-test.c
@@ -40,7 +40,7 @@ fu_test_synaprom_firmware_func(void)
 	g_assert_cmpint(sz, ==, 294);
 	g_assert_cmpint(buf[0], ==, 0x01);
 	g_assert_cmpint(buf[1], ==, 0x00);
-	ret = fu_firmware_parse(firmware, fw, 0, &error);
+	ret = fu_firmware_parse(firmware, fw, FWUPD_INSTALL_FLAG_NO_SEARCH, &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 

plugins/thunderbolt/fu-self-test.c
@@ -1143,8 +1143,10 @@ test_image_validation(ThunderboltTest *tt, gconstpointer user_data)
 	g_assert_nonnull(ctl_data);
 
 	/* parse controller image */
-	ret =
-	    fu_firmware_parse(FU_FIRMWARE(firmware_ctl), ctl_data, FWUPD_INSTALL_FLAG_NONE, &error);
+	ret = fu_firmware_parse(FU_FIRMWARE(firmware_ctl),
+				ctl_data,
+				FWUPD_INSTALL_FLAG_NO_SEARCH,
+				&error);
 	g_assert_true(ret);
 	g_assert_no_error(error);
 
@@ -1158,8 +1160,10 @@ test_image_validation(ThunderboltTest *tt, gconstpointer user_data)
 	g_assert_nonnull(fwi_data);
 
 	/* parse */
-	ret =
-	    fu_firmware_parse(FU_FIRMWARE(firmware_fwi), fwi_data, FWUPD_INSTALL_FLAG_NONE, &error);
+	ret = fu_firmware_parse(FU_FIRMWARE(firmware_fwi),
+				fwi_data,
+				FWUPD_INSTALL_FLAG_NO_SEARCH,
+				&error);
 	g_assert_true(ret);
 	g_assert_no_error(error);
 
@@ -1173,8 +1177,10 @@ test_image_validation(ThunderboltTest *tt, gconstpointer user_data)
 	g_assert_nonnull(bad_data);
 
 	/* parse; should fail, bad image */
-	ret =
-	    fu_firmware_parse(FU_FIRMWARE(firmware_bad), bad_data, FWUPD_INSTALL_FLAG_NONE, &error);
+	ret = fu_firmware_parse(FU_FIRMWARE(firmware_bad),
+				bad_data,
+				FWUPD_INSTALL_FLAG_NO_SEARCH,
+				&error);
 	g_assert_false(ret);
 	g_assert_error(error, FWUPD_ERROR, FWUPD_ERROR_READ);
 	g_debug("expected image validation error [ctl]: %s", error->message);
@@ -1234,8 +1240,12 @@ test_update_working(ThunderboltTest *tt, gconstpointer user_data)
 	 * after the time in the last parameter (given in ms) */
 	up_ctx = mock_tree_prepare_for_update(tree, plugin, "42.23", fw_data, 1000);
 	g_assert_nonnull(up_ctx);
-	ret =
-	    fu_plugin_runner_write_firmware(plugin, tree->fu_device, fw_data, progress, 0, &error);
+	ret = fu_plugin_runner_write_firmware(plugin,
+					      tree->fu_device,
+					      fw_data,
+					      progress,
+					      FWUPD_INSTALL_FLAG_NO_SEARCH,
+					      &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 
@@ -1284,8 +1294,12 @@ test_update_wd19(ThunderboltTest *tt, gconstpointer user_data)
 	fu_device_add_flag(tree->fu_device, FWUPD_DEVICE_FLAG_USABLE_DURING_UPDATE);
 	version_before = fu_device_get_version(tree->fu_device);
 
-	ret =
-	    fu_plugin_runner_write_firmware(plugin, tree->fu_device, fw_data, progress, 0, &error);
+	ret = fu_plugin_runner_write_firmware(plugin,
+					      tree->fu_device,
+					      fw_data,
+					      progress,
+					      FWUPD_INSTALL_FLAG_NO_SEARCH,
+					      &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 
@@ -1318,8 +1332,12 @@ test_update_fail(ThunderboltTest *tt, gconstpointer user_data)
 	up_ctx = mock_tree_prepare_for_update(tree, plugin, "42.23", fw_data, 1000);
 	up_ctx->result = UPDATE_FAIL_DEVICE_INTERNAL;
 
-	ret =
-	    fu_plugin_runner_write_firmware(plugin, tree->fu_device, fw_data, progress, 0, &error);
+	ret = fu_plugin_runner_write_firmware(plugin,
+					      tree->fu_device,
+					      fw_data,
+					      progress,
+					      FWUPD_INSTALL_FLAG_NO_SEARCH,
+					      &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 
@@ -1367,8 +1385,12 @@ test_update_fail_nowshow(ThunderboltTest *tt, gconstpointer user_data)
 	up_ctx = mock_tree_prepare_for_update(tree, plugin, "42.23", fw_data, 1000);
 	up_ctx->result = UPDATE_FAIL_DEVICE_NOSHOW;
 
-	ret =
-	    fu_plugin_runner_write_firmware(plugin, tree->fu_device, fw_data, progress, 0, &error);
+	ret = fu_plugin_runner_write_firmware(plugin,
+					      tree->fu_device,
+					      fw_data,
+					      progress,
+					      FWUPD_INSTALL_FLAG_NO_SEARCH,
+					      &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
 

commit 16def47f5b48b3a46f313d467151b873db775479

pixart-rf: Fix the endianness of the magic
This was probably broken in https://github.com/fwupd/fwupd/commit/0c51630991725d29255a4e4774f92185f6a24aaa

Mea culpa.

plugins/pixart-rf/fu-pxi-firmware.c
@@ -47,7 +47,7 @@ fu_pxi_firmware_check_magic(FuFirmware *firmware, GBytes *fw, gsize offset, GErr
 				    g_bytes_get_size(fw) - PIXART_RF_FW_HEADER_SIZE +
 					PIXART_RF_FW_HEADER_TAG_OFFSET,
 				    &magic,
-				    G_LITTLE_ENDIAN,
+				    G_BIG_ENDIAN,
 				    error)) {
 		g_prefix_error(error, "failed to read magic: ");
 		return FALSE;

plugins/pixart-rf/fu-self-test.c
@@ -21,6 +21,8 @@ fu_pxi_firmware_xml_func(void)
 	g_autofree gchar *xml_src = NULL;
 	g_autoptr(FuFirmware) firmware1 = fu_pxi_firmware_new();
 	g_autoptr(FuFirmware) firmware2 = fu_pxi_firmware_new();
+	g_autoptr(FuFirmware) firmware3 = fu_pxi_firmware_new();
+	g_autoptr(GBytes) fw = NULL;
 	g_autoptr(GError) error = NULL;
 
 	/* build and write */
@@ -31,9 +33,17 @@ fu_pxi_firmware_xml_func(void)
 	ret = fu_firmware_build_from_xml(firmware1, xml_src, &error);
 	g_assert_no_error(error);
 	g_assert_true(ret);
+	fw = fu_firmware_write(firmware1, &error);
+	g_assert_no_error(error);
+	g_assert_nonnull(fw);
 	csum1 = fu_firmware_get_checksum(firmware1, G_CHECKSUM_SHA1, &error);
 	g_assert_no_error(error);
 	g_assert_cmpstr(csum1, ==, "2aae6c35c94fcfb415dbe95f408b9ce91ee846ed");
+
+	/* ensure we can parse */
+	ret = fu_firmware_parse(firmware3, fw, FWUPD_INSTALL_FLAG_NONE, &error);
+	g_assert_no_error(error);
+	g_assert_true(ret);
 
 	/* ensure we can round-trip */
 	xml_out = fu_firmware_export_to_xml(firmware1, FU_FIRMWARE_EXPORT_FLAG_NONE, &error);