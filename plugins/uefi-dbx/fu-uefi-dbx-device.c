c0b8673faa6473a584a1d38e5b37fdce20a840f4
Richard Hughes <richard@hughsie.com>
2022-09-20 10:37:39 +0100

    dbx: Do not show the 'connected devices may not be usable while updating' message

--- plugins/uefi-dbx/fu-uefi-dbx-device.c
+++ plugins/uefi-dbx/fu-uefi-dbx-device.c
@@ -150,6 +150,7 @@ fu_uefi_dbx_device_init(FuUefiDbxDevice *self)
 	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_NEEDS_REBOOT);
 	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_ONLY_VERSION_UPGRADE);
 	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_SIGNED_PAYLOAD);
+	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_USABLE_DURING_UPDATE);
 	fu_device_add_parent_guid(FU_DEVICE(self), "main-system-firmware");
 	if (!fu_common_is_live_media())
 		fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_UPDATABLE);
