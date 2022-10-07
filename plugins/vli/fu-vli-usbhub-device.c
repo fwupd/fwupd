c621c723679dd01c12cc486833c618e1b5903ab2
Richard Hughes <richard@hughsie.com>
2022-10-07 21:05:03 +0100

    vli: Set auto-parent-children flag for all USB hubs

--- plugins/vli/fu-vli-usbhub-device.c
+++ plugins/vli/fu-vli-usbhub-device.c
@@ -1211,6 +1211,7 @@ fu_vli_usbhub_device_init(FuVliUsbhubDevice *self)
 	fu_device_add_icon(FU_DEVICE(self), "usb-hub");
 	fu_device_add_protocol(FU_DEVICE(self), "com.vli.usbhub");
 	fu_device_add_internal_flag(FU_DEVICE(self), FU_DEVICE_INTERNAL_FLAG_USE_PROXY_FALLBACK);
+	fu_device_add_internal_flag(FU_DEVICE(self), FU_DEVICE_INTERNAL_FLAG_AUTO_PARENT_CHILDREN);
 	fu_device_set_remove_delay(FU_DEVICE(self), FU_DEVICE_REMOVE_DELAY_RE_ENUMERATE);
 	fu_device_register_private_flag(FU_DEVICE(self),
 					FU_VLI_USBHUB_DEVICE_FLAG_ATTACH_WITH_GPIOB,
