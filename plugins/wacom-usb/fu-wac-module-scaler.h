513bad1e9347b244acb3e677d868cb0f0a782186
Aaron Skomra <aaron.skomra@wacom.com>
2022-10-11 16:27:24 -0700

    wacom-usb: add scaler support to wacom-usb
    Support for updating Wacom scalar firmware is being introduced
    with the Cintiq Pro 27.

---
+++ plugins/wacom-usb/fu-wac-module-scaler.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2018 Richard Hughes <richard@hughsie.com>
+ * Copyright (C) 2021 Jason Gerecke <killertofu@gmail.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include "fu-wac-module.h"
+
+#define FU_TYPE_WAC_MODULE_SCALER (fu_wac_module_scaler_get_type())
+G_DECLARE_FINAL_TYPE(FuWacModuleScaler, fu_wac_module_scaler, FU, WAC_MODULE_SCALER, FuWacModule)
+
+FuWacModule *
+fu_wac_module_scaler_new(FuContext *context, GUsbDevice *usb_device);
