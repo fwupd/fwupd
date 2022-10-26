a447fca10181c4e86a65789cdf95e1c8f3c3d22e
Kevin Chen <hsinfu.chen@qsitw.com>
2022-10-26 13:55:02 +0800

    qsi-dock: Create a new plugin to update docks from QSI

---
+++ plugins/qsi-dock/README.md
@@ -0,0 +1,32 @@
+# QSI Dock
+
+## Introduction
+
+This plugin uses the MCU to write all the dock firmware components. The MCU version
+is provided by the DMC bcdDevice.
+
+This plugin supports the following protocol ID:
+
+* com.qsi.dock
+
+## GUID Generation
+
+These devices use the standard USB DeviceInstanceId values, e.g.
+
+* `USB\VID_047D&PID_808D&REV_0001`
+* `USB\VID_047D&PID_808D`
+* `USB\VID_047D`
+
+## Update Behavior
+
+The device usually presents in runtime mode, but on detach re-enumerates with
+the same USB PID in an unlocked mode. On attach the device again re-enumerates
+back to the runtime locked mode.
+
+## Vendor ID Security
+
+The vendor ID is set from the USB vendor.
+
+## External Interface Access
+
+This plugin requires read/write access to `/dev/bus/usb`.
---
+++ plugins/qsi-dock/fu-qsi-dock-plugin.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2021 Richard Hughes <richard@hughsie.com>
+ * Copyright (C) 2022 Kevin Chen <hsinfu.chen@qsitw.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#include "config.h"
+
+#include "fu-qsi-dock-mcu-device.h"
+#include "fu-qsi-dock-plugin.h"
+
+struct _FuQsiDockPlugin {
+	FuPlugin parent_instance;
+};
+
+G_DEFINE_TYPE(FuQsiDockPlugin, fu_qsi_dock_plugin, FU_TYPE_PLUGIN)
+
+static void
+fu_qsi_dock_plugin_init(FuQsiDockPlugin *self)
+{
+}
+
+static void
+fu_qsi_dock_plugin_constructed(GObject *obj)
+{
+	FuPlugin *plugin = FU_PLUGIN(obj);
+	fu_plugin_add_device_gtype(plugin, FU_TYPE_QSI_DOCK_MCU_DEVICE);
+}
+
+static void
+fu_qsi_dock_plugin_class_init(FuQsiDockPluginClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS(klass);
+	object_class->constructed = fu_qsi_dock_plugin_constructed;
+}
