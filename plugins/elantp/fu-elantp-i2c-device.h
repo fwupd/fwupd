93599e316c89a1c3e4af3122154d0b0bf478a7f7
Richard Hughes <richard@hughsie.com>
2022-10-28 14:02:33 +0100

    elantp: Derive from FuI2cDevice to allow a I2C\NAME_ quirk match

--- plugins/elantp/fu-elantp-i2c-device.h
+++ plugins/elantp/fu-elantp-i2c-device.h
@@ -11,4 +11,4 @@
 #define FU_ELANTP_I2C_DEVICE_ABSOLUTE (1 << 0)
 
 #define FU_TYPE_ELANTP_I2C_DEVICE (fu_elantp_i2c_device_get_type())
-G_DECLARE_FINAL_TYPE(FuElantpI2cDevice, fu_elantp_i2c_device, FU, ELANTP_I2C_DEVICE, FuUdevDevice)
+G_DECLARE_FINAL_TYPE(FuElantpI2cDevice, fu_elantp_i2c_device, FU, ELANTP_I2C_DEVICE, FuI2cDevice)
