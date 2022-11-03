1986e32671d54d5e94111ce89affa4bdac935ea5
Sanjay Sheth <ssheth@logitech.com>
2022-11-03 14:58:46 -0700

    New plugin for Logitech Whiteboard camera

---
+++ plugins/logitech-scribe/fu-logitech-scribe-plugin.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2022 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include <fwupdplugin.h>
+
+G_DECLARE_FINAL_TYPE(FuLogitechScribePlugin,
+		     fu_logitech_scribe_plugin,
+		     FU,
+		     LOGITECH_SCRIBE_PLUGIN,
+		     FuPlugin)
