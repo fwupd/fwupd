1521e9aec2702299d371c82e5dd40d48f1620c65
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-12-27 13:32:06 -0500

    spelling: transfer
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- plugins/logitech-bulkcontroller/fu-logitech-bulkcontroller-device.c
+++ plugins/logitech-bulkcontroller/fu-logitech-bulkcontroller-device.c
@@ -795,7 +795,7 @@ fu_logitech_bulkcontroller_device_write_firmware(FuDevice *device,
 							    CMD_END_TRANSFER,
 							    end_pkt,
 							    error)) {
-		g_prefix_error(error, "failed to write end transfer transfer packet: ");
+		g_prefix_error(error, "failed to write end transfer packet: ");
 		return FALSE;
 	}
 	fu_progress_step_done(progress);
