6124b07a2a44311dfbfc5c7d55503a2c9842c46d
Richard Hughes <richard@hughsie.com>
2023-02-24 10:05:40 +0000

    trivial: Actually run fwupdtool in the OpenBMC CI target

--- contrib/build-openbmc.sh
+++ contrib/build-openbmc.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-meson setup ../ \
+rm -rf build
+
+meson setup build \
     -Dauto_features=disabled \
+    -Ddocs=disabled \
     -Dbash_completion=false \
     -Dcompat_cli=false \
     -Dfish_completion=false \
@@ -13,3 +16,7 @@ meson setup ../ \
     -Dudevdir=/tmp \
     -Dsystemd_root_prefix=/tmp \
     $@
+
+ninja install -C build
+build/src/fwupdtool get-devices --verbose
+test $? -eq 2 || exit 1
