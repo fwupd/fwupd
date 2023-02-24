fwupd/fwupd

commit 34743ff6f48510839ca3ef5f4ad6fe51ffd3425c

Allow downloading files in fwupdtool
The useragent was being set as 'fwupdtool/1.8.11 fwupd/(NULL)' which the LVFS
did not like. As we're using fwupdtool without a daemon proxy just set the
fwupd version manually.

Fixes https://github.com/fwupd/fwupd/issues/5519

libfwupd/fwupd-client.c
@@ -321,7 +321,16 @@ fwupd_client_set_host_security_id(FwupdClient *self, const gchar *host_security_
 	fwupd_client_object_notify(self, "host-security-id");
 }
 
-static void
+/**
+ * fwupd_client_set_daemon_version:
+ * @self: a #FwupdClient
+ * @daemon_version: A semantic version, e.g. "1.2.3"
+ *
+ * Sets the daemon version number.
+ *
+ * Since: 1.8.11
+ **/
+void
 fwupd_client_set_daemon_version(FwupdClient *self, const gchar *daemon_version)
 {
 	FwupdClientPrivate *priv = GET_PRIVATE(self);
@@ -5035,6 +5044,7 @@ fwupd_client_set_user_agent_for_package(FwupdClient *self,
 	g_return_if_fail(FWUPD_IS_CLIENT(self));
 	g_return_if_fail(package_name != NULL);
 	g_return_if_fail(package_version != NULL);
+	g_return_if_fail(priv->daemon_version != NULL);
 
 	/* application name and version */
 	g_string_append_printf(str, "%s/%s", package_name, package_version);

libfwupd/fwupd-client.h
@@ -435,6 +435,8 @@ guint
 fwupd_client_get_percentage(FwupdClient *self);
 const gchar *
 fwupd_client_get_daemon_version(FwupdClient *self);
+void
+fwupd_client_set_daemon_version(FwupdClient *self, const gchar *daemon_version);
 const gchar *
 fwupd_client_get_host_bkc(FwupdClient *self);
 const gchar *

libfwupd/fwupd.map
@@ -916,6 +916,7 @@ LIBFWUPD_1.8.11 {
     fwupd_client_quit;
     fwupd_client_quit_async;
     fwupd_client_quit_finish;
+    fwupd_client_set_daemon_version;
     fwupd_client_uninhibit;
     fwupd_client_uninhibit_async;
     fwupd_client_uninhibit_finish;

src/fu-tool.c
@@ -22,6 +22,7 @@
 #include <unistd.h>
 
 #include "fwupd-bios-setting-private.h"
+#include "fwupd-client-private.h"
 #include "fwupd-common-private.h"
 #include "fwupd-device-private.h"
 #include "fwupd-enums-private.h"
@@ -3657,6 +3658,7 @@ main(int argc, char *argv[])
 	/* used for monitoring and downloading */
 	priv->client = fwupd_client_new();
 	fwupd_client_set_main_context(priv->client, priv->main_ctx);
+	fwupd_client_set_daemon_version(priv->client, PACKAGE_VERSION);
 	fwupd_client_set_user_agent_for_package(priv->client, "fwupdtool", PACKAGE_VERSION);
 	g_signal_connect(FWUPD_CLIENT(priv->client),
 			 "notify::percentage",

commit 1a1f72ba9b971ba7a8595a0d0eea9c46f9cdabd4

Allow setting the package user agent before the client has connected
This fixes a regression introduced in https://github.com/fwupd/fwupd/pull/5528
where we added a critical warning if setting the package user agent before we
retrieved the fwupd daemon version.

If we rebuild the user agent whenever we get new information it does not matter
in which order these are set.

libfwupd/fwupd-client.c
@@ -72,6 +72,8 @@ typedef struct {
 	GMutex proxy_mutex; /* for @proxy */
 	GDBusProxy *proxy;
 	GProxyResolver *proxy_resolver;
+	gchar *package_name;
+	gchar *package_version;
 	gchar *user_agent;
 	GHashTable *hints; /* str:str */
 #ifdef SOUP_SESSION_COMPAT
@@ -266,6 +268,31 @@ fwupd_client_signal_emit_object(FwupdClient *self, guint signal_id, GObject *pay
 }
 
 static void
+fwupd_client_rebuild_user_agent(FwupdClient *self)
+{
+	FwupdClientPrivate *priv = GET_PRIVATE(self);
+	g_autoptr(GString) str = g_string_new(NULL);
+	g_autofree gchar *system = NULL;
+
+	/* application name and version */
+	if (priv->package_name != NULL && priv->package_version != NULL)
+		g_string_append_printf(str, "%s/%s ", priv->package_name, priv->package_version);
+
+	/* system information */
+	system = fwupd_build_user_agent_system();
+	if (system != NULL)
+		g_string_append_printf(str, "(%s) ", system);
+
+	/* platform, unless the application name is fwupd itself */
+	if (priv->daemon_version != NULL && g_strcmp0(priv->package_name, "fwupd") != 0)
+		g_string_append_printf(str, "fwupd/%s", priv->daemon_version);
+
+	/* success */
+	g_free(priv->user_agent);
+	priv->user_agent = g_string_free(g_steal_pointer(&str), FALSE);
+}
+
+static void
 fwupd_client_set_host_vendor(FwupdClient *self, const gchar *host_vendor)
 {
 	FwupdClientPrivate *priv = GET_PRIVATE(self);
@@ -342,6 +369,7 @@ fwupd_client_set_daemon_version(FwupdClient *self, const gchar *daemon_version)
 	g_free(priv->daemon_version);
 	priv->daemon_version = g_strdup(daemon_version);
 	fwupd_client_object_notify(self, "daemon-version");
+	fwupd_client_rebuild_user_agent(self);
 }
 
 static void
@@ -5038,29 +5066,16 @@ fwupd_client_set_user_agent_for_package(FwupdClient *self,
 					const gchar *package_version)
 {
 	FwupdClientPrivate *priv = GET_PRIVATE(self);
-	g_autoptr(GString) str = g_string_new(NULL);
-	g_autofree gchar *system = NULL;
 
 	g_return_if_fail(FWUPD_IS_CLIENT(self));
 	g_return_if_fail(package_name != NULL);
 	g_return_if_fail(package_version != NULL);
-	g_return_if_fail(priv->daemon_version != NULL);
 
-	/* application name and version */
-	g_string_append_printf(str, "%s/%s", package_name, package_version);
-
-	/* system information */
-	system = fwupd_build_user_agent_system();
-	if (system != NULL)
-		g_string_append_printf(str, " (%s)", system);
-
-	/* platform, which in our case is just fwupd */
-	if (g_strcmp0(package_name, "fwupd") != 0)
-		g_string_append_printf(str, " fwupd/%s", priv->daemon_version);
-
-	/* success */
-	g_free(priv->user_agent);
-	priv->user_agent = g_string_free(g_steal_pointer(&str), FALSE);
+	g_free(priv->package_name);
+	g_free(priv->package_version);
+	priv->package_name = g_strdup(package_name);
+	priv->package_version = g_strdup(package_version);
+	fwupd_client_rebuild_user_agent(self);
 }
 
 #ifdef HAVE_LIBCURL
@@ -6346,6 +6361,8 @@ fwupd_client_finalize(GObject *object)
 
 	g_clear_pointer(&priv->main_ctx, g_main_context_unref);
 	g_free(priv->user_agent);
+	g_free(priv->package_name);
+	g_free(priv->package_version);
 	g_free(priv->daemon_version);
 	g_free(priv->host_bkc);
 	g_free(priv->host_vendor);