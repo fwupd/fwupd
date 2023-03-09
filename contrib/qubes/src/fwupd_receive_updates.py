fwupd/fwupd

commit 63903c82d9c661f2466316abe1972c5248d947de

contrib/qubes: Do not dynamically create shell commands
The security reasons for this are obvious.  When shell commands must be
generated dynamically, use shlex.join() to ensure that proper quoting is
done.

contrib/qubes/src/fwupd_receive_updates.py
@@ -143,12 +143,17 @@ class FwupdReceiveUpdates:
             shutil.rmtree(FWUPD_DOM0_UNTRUSTED_DIR)
         self._create_dirs(FWUPD_DOM0_UPDATES_DIR, FWUPD_DOM0_UNTRUSTED_DIR)
 
-        cmd_copy = "qvm-run --pass-io %s %s > %s" % (
-            updatevm,
-            "'cat %s'" % updatevm_firmware_file_path,
-            dom0_firmware_untrusted_path,
-        )
-        p = subprocess.Popen(cmd_copy, shell=True)
+        cmd_copy = [
+            "qvm-run",
+            "--pass-io",
+            "--no-shell",
+            "--",
+            "cat",
+            "--",
+            updatevm_firmware_file_path,
+        ]
+        with open(dom0_firmware_untrusted_path, "wbx") as untrusted_file:
+            p = subprocess.Popen(cmd_copy, stdout=untrusted_file, shell=False)
         p.wait()
         if p.returncode != 0:
             raise Exception("qvm-run: Copying firmware file failed!!")
@@ -192,27 +197,39 @@ class FwupdReceiveUpdates:
         )
         self._check_domain(updatevm)
         self._create_dirs(FWUPD_DOM0_METADATA_DIR)
-        cmd_file = "'cat %s'" % self.metadata_file_updatevm
-        cmd_jcat = "'cat %s'" % self.metadata_file_jcat_updatevm
-        cmd_copy_metadata_file = "qvm-run --pass-io %s %s > %s" % (
+        cmd_copy_metadata_file = [
+            "qvm-run",
+            "--pass-io",
+            "--no-shell",
+            "--",
             updatevm,
-            cmd_file,
-            self.metadata_file,
-        )
-        cmd_copy_metadata_jcat = "qvm-run --pass-io %s %s > %s" % (
+            "cat",
+            "--",
+            self.metadata_file_updatevm,
+        ]
+        cmd_copy_metadata_file_jcat = [
+            "qvm-run",
+            "--pass-io",
+            "--no-shell",
+            "--",
             updatevm,
-            cmd_jcat,
-            self.metadata_file_jcat,
-        )
-
-        p = subprocess.Popen(cmd_copy_metadata_file, shell=True)
-        p.wait()
+            "cat",
+            "--",
+            self.metadata_file_jcat_updatevm,
+        ]
+        with open(self.metadata_file, "wbx") as untrusted_file_1, open(
+            self.metadata_file_jcat, "wbx"
+        ) as untrusted_file_2, subprocess.Popen(
+            cmd_copy_metadata_file, stdout=untrusted_file_1
+        ) as p, subprocess.Popen(
+            cmd_copy_metadata_file_jcat, stdout=untrusted_file_2
+        ) as q:
+            p.wait()
+            q.wait()
         if p.returncode != 0:
             raise Exception("qvm-run: Copying metadata file failed!!")
-        p = subprocess.Popen(cmd_copy_metadata_jcat, shell=True)
-        p.wait()
         if p.returncode != 0:
-            raise Exception('qvm-run": Copying metadata jcat failed!!')
+            raise Exception("qvm-run: Copying metadata jcat failed!!")
 
         self._verify_received(
             FWUPD_DOM0_METADATA_DIR, FWUPD_METADATA_FILES_REGEX, updatevm

contrib/qubes/src/qubes_fwupd_update.py
@@ -22,6 +22,39 @@ UPDATEVM_REGEX = re.compile(r"^sys-")
 
 WARNING_COLOR = "\033[93m"
 
+run_cmd = (
+    "qvm-run",
+    "--no-gui",
+    "--no-shell",
+    "-q",
+    "-a",
+    "--filter-escape-chars",
+    "--color-output=31",
+    "--color-stderr=31",
+    "--",
+)
+
+
+def usbvm_run(args, **kwargs):
+    return subprocess.check_call((*run_cmd, *args), **kwargs)
+
+
+def run_in_tty(updatevm, args, **kwargs):
+    return subprocess.check_call(
+        (
+            *run_cmd,
+            updatevm,
+            "script",
+            "--quiet",
+            "--return",
+            "--command",
+            shlex.join(args),
+            "/dev/null",
+        ),
+        stdin=subprocess.DEVNULL,
+        **kwargs,
+    )
+
 
 class FwupdUpdate:
     def _create_dirs(self, *args):
@@ -64,19 +97,8 @@ class FwupdUpdate:
 
     def _encrypt_update_url(self, url):
         self.enc_url = url
-        self.arch_name = url.replace(FWUPD_DOWNLOAD_PREFIX, "")
         self.arch_path = os.path.join(FWUPD_DOM0_UPDATES_DIR, self.arch_name)
-        if "&" in url:
-            self.enc_url = self.enc_url.replace("&", "--and--")
-            self.arch_name = "untrusted.cab"
-        if "|" in url:
-            self.enc_url = self.enc_url.replace("|", "--or--")
-            self.arch_name = "untrusted.cab"
-        if "#" in url:
-            self.enc_url = self.enc_url.replace("#", "--hash--")
-            self.arch_name = "untrusted.cab"
-        if "%20" in url:
-            self.arch_name = "untrusted.cab"
+        self.arch_name = "untrusted.cab"
 
     def download_metadata(self, whonix=False, metadata_url=None):
         """Initialize downloading metadata files.
@@ -93,30 +115,12 @@ class FwupdUpdate:
             raise Exception(f"{self.updatevm} is not running!!")
         if not os.path.exists(FWUPD_DOM0_DIR):
             self._create_dirs(FWUPD_DOM0_DIR)
+        cmd_metadata = [FWUPD_VM_DOWNLOAD, "--metadata"]
         if metadata_url:
-            cmd_metadata = [
-                "qvm-run",
-                "--pass-io",
-                self.updatevm,
-                (
-                    "script --quiet --return --command "
-                    f'"{FWUPD_VM_DOWNLOAD} --metadata'
-                    f' --url={metadata_url}"'
-                ),
-            ]
-        else:
-            cmd_metadata = [
-                "qvm-run",
-                "--pass-io",
-                self.updatevm,
-                (
-                    "script --quiet --return --command "
-                    f'"{FWUPD_VM_DOWNLOAD} --metadata"'
-                ),
-            ]
-        p = subprocess.Popen(cmd_metadata)
-        p.wait()
-        if p.returncode != 0:
+            cmd_metadata.append("--url=" + metadata_url)
+        try:
+            run_in_tty(updatevm, cmd_metadata)
+        except subprocess.CalledProcessError:
             raise Exception("Metadata download failed.")
 
     def download_firmware_updates(self, url, sha, whonix=False):
@@ -140,14 +144,22 @@ class FwupdUpdate:
             cmd_firmware_download = [
                 "qvm-run",
                 "--pass-io",
+                "--quiet",
+                "--autostart",
+                "--no-shell",
+                "--color-output=31",
+                "--color-stderr=31",
+                "--",
                 self.updatevm,
-                (
-                    "script --quiet --return --command "
-                    f'"{FWUPD_VM_DOWNLOAD} --url={self.enc_url}'
-                    f' --sha={sha}"'
+                "script",
+                "--quiet",
+                "--return",
+                "--command",
+                shlex.join(
+                    (FWUPD_VM_DOWNLOAD, f"--url={self.enc_url}", f"--sha={sha}")
                 ),
             ]
-            p = subprocess.Popen(cmd_firmware_download)
+            p = subprocess.Popen(cmd_firmware_download, stdin=subprocess.DEVNULL)
             p.wait()
             if p.returncode != 0:
                 raise Exception("Firmware download failed.")

contrib/qubes/src/qubes_fwupdmgr.py
@@ -19,6 +19,7 @@ from pathlib import Path
 from packaging.version import Version
 
 FWUPD_QUBES_DIR = "/usr/share/qubes-fwupd"
+USBVM_N = "sys-usb"
 
 # Check if script is run by tests and append sys path properly
 if __name__ == "__main__":
@@ -28,12 +29,17 @@ else:
 
 try:
     from qubes_fwupd_heads import FwupdHeads
-    from qubes_fwupd_update import FwupdUpdate
+    from qubes_fwupd_update import FwupdUpdate, usbvm_run, run_in_tty
     from fwupd_receive_updates import FwupdReceiveUpdates
 except ModuleNotFoundError:
     raise ModuleNotFoundError(
-        "qubes-fwupd modules not found. " "You may need to reinstall package."
+        "qubes-fwupd modules not found.  You may need to reinstall package."
     )
+
+
+def usbvm_run_in_tty(args, **kwargs):
+    return run_in_tty(USBVM_N, args, **kwargs)
+
 
 FWUPD_DOM0_DIR = "/var/cache/qubes-fwupd"
 FWUPD_DOM0_METADATA_DIR = os.path.join(FWUPD_DOM0_DIR, "metadata")
@@ -55,7 +61,16 @@ FWUPD_DOWNLOAD_PREFIX = "https://fwupd.org/downloads/"
 
 FWUPDMGR = "/bin/fwupdmgr"
 
-USBVM_N = "sys-usb"
+
+def usbvm_create_file(fname, dest, **kwargs):
+    with open(fname, "rb") as stdin:
+        subprocess.check_call(
+            [*usbvm_run_cmd, "dd", "of=" + dest, "bs=128K", "conv=fsync"],
+            stdin=stdin,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+
 
 BIOS_UPDATE_FLAG = os.path.join(FWUPD_DOM0_DIR, "bios_update")
 LVFS_TESTING_DOM0_FLAG = os.path.join(FWUPD_DOM0_DIR, "lvfs_testing")
@@ -111,30 +126,17 @@ class QubesFwupdmgr(FwupdHeads, FwupdUpdate, FwupdReceiveUpdates):
 
     def _validate_usbvm_dirs(self):
         """Validates if sys-ubs updates and metadata directories exist."""
-        cmd_validate_dirs = [
-            "qvm-run",
-            "--pass-io",
-            USBVM_N,
-            f'script --quiet --return --command "{FWUPD_VM_VALIDATE} dirs"',
-        ]
-        p = subprocess.Popen(cmd_validate_dirs)
-        p.wait()
-        if p.returncode != 0:
+        try:
+            usbvm_run_in_tty((FWUPD_VM_VALIDATE, "dirs"))
+        except subprocess.CalledProcessError:
             raise Exception("Validation of usbvm directories failed.")
 
     def _validate_usbvm_archive(self, arch_name, sha):
         """Validates checksum and gpg signature of the archive file."""
         arch_path = os.path.join(FWUPD_VM_UPDATES_DIR, arch_name)
-        arch_validate = f"{FWUPD_VM_VALIDATE} updates {arch_path} {sha}"
-        cmd_validate_arch = [
-            "qvm-run",
-            "--pass-io",
-            USBVM_N,
-            f'script --quiet --return --command "{arch_validate}"',
-        ]
-        p = subprocess.Popen(cmd_validate_arch)
-        p.wait()
-        if p.returncode != 0:
+        try:
+            usbvm_run_in_tty((FWUPD_VM_VALIDATE, "updates", arch_path, sha))
+        except subprocess.CalledProcessError:
             raise Exception("Validation of the archive file failed.")
 
     def _copy_usbvm_metadata(self):
@@ -143,58 +145,33 @@ class QubesFwupdmgr(FwupdHeads, FwupdUpdate, FwupdReceiveUpdates):
             FWUPD_DOM0_METADATA_DIR, FWUPD_VM_METADATA_DIR
         )
         self.metadata_file_jcat_usbvm = self.metadata_file_usbvm + ".jcat"
-        cat_file = f"cat > {self.metadata_file_usbvm}"
-        cmd_copy_file = (
-            f"cat {self.metadata_file} | "
-            f'qvm-run --nogui --pass-io {USBVM_N} "{cat_file}"'
-        )
-        cat_jcat = f"cat > {self.metadata_file_jcat_usbvm}"
-        cmd_copy_jcat = (
-            f"cat {self.metadata_file_jcat} | "
-            f'qvm-run --nogui --pass-io {USBVM_N} "{cat_jcat}"'
-        )
-        p = subprocess.Popen(cmd_copy_file, shell=True)
-        p.wait()
-        if p.returncode != 0:
-            raise Exception("Copying metadata file failed.")
-        p = subprocess.Popen(cmd_copy_jcat, shell=True)
-        p.wait()
-        if p.returncode != 0:
-            raise Exception("Copying metadata jcat failed.")
+        usbvm_create_file(self.metadata_file, self.metadata_file_usbvm)
+        usbvm_create_file(self.metadata_file_jcat, self.metadata_file_jcat_usbvm)
 
     def _validate_usbvm_metadata(self, metadata_url=None):
         """Checks GPG signature of metadata files in usbvm."""
         usbvm_cmd = f'"{FWUPD_VM_VALIDATE} metadata"'
+        cmd_metadata = [FWUPD_VM_DOWNLOAD, "metadata"]
         if metadata_url:
-            usbvm_cmd = f'"{FWUPD_VM_VALIDATE} metadata --url={metadata_url}"'
-        cmd_validate_metadata = [
-            "qvm-run",
-            "--pass-io",
-            USBVM_N,
-            "script --quiet --return --command" f" {usbvm_cmd}",
-        ]
+            cmd_metadata.append("--url=" + metadata_url)
         p = subprocess.Popen(cmd_validate_metadata)
-        p.wait()
-        if p.returncode != 0:
-            raise Exception("Metadata validation failed")
+        try:
+            usbvm_run_in_tty(cmd_metadata)
+        except subprocess.CalledProcessError:
+            raise Exception("Metadata validation failed.")
 
     def _refresh_usbvm_metadata(self):
         """Refreshes metadata in usbvm."""
         sig_metadata_file = self.metadata_file_jcat_usbvm
-        cmd_refresh_metadata = [
-            "qvm-run",
-            "--pass-io",
-            USBVM_N,
+        usbvm_run_in_tty(
             (
-                "script --quiet --return --command "
-                f'"{FWUPDMGR} refresh {self.metadata_file_usbvm} '
-                f'{sig_metadata_file} {self.lvfs}"'
-            ),
-        ]
-        p = subprocess.Popen(cmd_refresh_metadata)
-        p.wait()
-        if p.returncode != 0:
-            raise Exception("Metadata refresh in usbvm failed")
+                FWUPDMGR,
+                "refresh",
+                self.metadata_file_usbvm,
+                sig_metadata_file,
+                self.lvfs,
+            )
+        )
 
     def _copy_firmware_updates(self, arch_name):
         """Copies updates files to usbvm.
@@ -204,14 +181,7 @@ class QubesFwupdmgr(FwupdHeads, FwupdUpdate, FwupdReceiveUpdates):
         """
         arch_path = os.path.join(FWUPD_DOM0_UPDATES_DIR, arch_name)
         output_path = os.path.join(FWUPD_VM_UPDATES_DIR, arch_name)
-        cat_file = f"cat > {output_path}"
-        cmd_copy_file = (
-            f"cat {arch_path} | " f'qvm-run --nogui --pass-io {USBVM_N} "{cat_file}"'
-        )
-        p = subprocess.Popen(cmd_copy_file, shell=True)
-        p.wait()
-        if p.returncode != 0:
-            raise Exception("Copying metadata file failed.")
+        usbvm_create_file(arch_path, output_path)
 
     def _install_usbvm_firmware_update(self, arch_name):
         """Installs firmware update for specified device in dom0.
@@ -220,17 +190,7 @@ class QubesFwupdmgr(FwupdHeads, FwupdUpdate, FwupdReceiveUpdates):
         arch_name - name of the archive file
         """
         arch_path = os.path.join(FWUPD_VM_UPDATES_DIR, arch_name)
-        CMD_update = [
-            "qvm-run",
-            "--pass-io",
-            USBVM_N,
-            f"script --quiet --return --command"
-            f' "{FWUPDMGR} install {arch_path}" /dev/null',
-        ]
-        p = subprocess.Popen(CMD_update)
-        p.wait()
-        if p.returncode != 0:
-            raise Exception("fwupd-qubes: Firmware update failed")
+        usbvm_run_in_tty((FWUPDMGR, "--", "install", arch_path))
 
     def _install_usbvm_firmware_downgrade(self, arch_name):
         """Installs firmware downgrades for specified device in dom0.
@@ -239,29 +199,13 @@ class QubesFwupdmgr(FwupdHeads, FwupdUpdate, FwupdReceiveUpdates):
         arch_name - name of the archive file
         """
         arch_path = os.path.join(FWUPD_VM_UPDATES_DIR, arch_name)
-        CMD_downgrade = [
-            "qvm-run",
-            "--pass-io",
-            USBVM_N,
-            f"script --quiet --return --command"
-            f' "{FWUPDMGR} --allow-older install {arch_path}" /dev/null',
-        ]
-        p = subprocess.Popen(CMD_downgrade)
-        p.wait()
-        if p.returncode != 0:
-            raise Exception("fwupd-qubes: Firmware downgrade failed")
+        usbvm_run_in_tty((FWUPDMGR, "--allow-older", "--", "install", arch_path))
 
     def _clean_usbvm(self):
         """Cleans usbvm directories."""
-        cmd_clean = [
-            "qvm-run",
-            "--pass-io",
-            USBVM_N,
-            f'script --quiet --return --command "{FWUPD_VM_VALIDATE} clean"',
-        ]
-        p = subprocess.Popen(cmd_clean)
-        p.wait()
-        if p.returncode != 0:
+        try:
+            usbvm_run_in_tty((FWUPDMGR, "clean"))
+        except subprocess.CalledProcessError:
             raise Exception("Cleaning usbvm directories failed")
 
     def _enable_lvfs_testing_dom0(self):
@@ -277,42 +221,11 @@ class QubesFwupdmgr(FwupdHeads, FwupdUpdate, FwupdReceiveUpdates):
     def _enable_lvfs_testing_usbvm(self, usbvm=False):
         """Checks and enable lvfs-testing for custom metadata in usbvm"""
         if not usbvm:
-            return 0
-        cmd_refresh_metadata = [
-            "qvm-run",
-            "--pass-io",
-            USBVM_N,
-            (
-                "script --quiet --return --command "
-                f'"{FWUPDMGR} enable-remote -y lvfs-testing"'
-            ),
-        ]
-        cmd_validate_flag = [
-            "qvm-run",
-            "--pass-io",
-            USBVM_N,
-            (
-                "script --quiet --return --command "
-                f'"ls {LVFS_TESTING_USBVM_FLAG} &>/dev/null"'
-            ),
-        ]
-        cmd_touch_flag = [
-            "qvm-run",
-            "--pass-io",
-            USBVM_N,
-            ("script --quiet --return --command " f'"touch {LVFS_TESTING_USBVM_FLAG}"'),
-        ]
-        flag = subprocess.Popen(cmd_validate_flag)
-        flag.wait()
-        if flag.returncode != 0:
-            p = subprocess.Popen(cmd_refresh_metadata)
-            p.wait()
-            if p.returncode != 0:
-                raise Exception("Enabling usbvm lvfs-testing failed!!")
-            p = subprocess.Popen(cmd_touch_flag)
-            p.wait()
-            if p.returncode != 0:
-                raise Exception("Creating flag failed!!")
+            return
+        try:
+            usbvm_run_in_tty((FWUPDMGR, "enable-remote", "-y", "lvfs-testing"))
+        except subprocess.CalledProcessError:
+            raise Exception("Enabling usbvm lvfs-testing failed!!")
 
     def refresh_metadata(self, usbvm=False, whonix=False, metadata_url=None):
         """Updates metadata with downloaded files.
@@ -553,13 +466,13 @@ class QubesFwupdmgr(FwupdHeads, FwupdUpdate, FwupdReceiveUpdates):
         """Gathers information about devices connected in usbvm."""
         if os.path.exists(FWUPD_VM_LOG):
             os.remove(FWUPD_VM_LOG)
-        usbvm_cmd = f'"{FWUPDAGENT} get-devices"'
-        log_file = f" > {FWUPD_VM_LOG}"
-        cmd_get_usbvm_devices = (
-            f"qvm-run --nogui --pass-io {USBVM_N} {usbvm_cmd}{log_file}"
-        )
-        p = subprocess.Popen(cmd_get_usbvm_devices, shell=True)
-        p.wait()
+        with open(FWUPD_VM_LOG, "wb") as log:
+            p = usbvm_run(
+                [FWUPDAGENT, "get-devices"],
+                stdout=log,
+                stderr=log,
+                stdin=subprocess.DEVNULL,
+            )
         if p.returncode != 0 and p.returncode != 2 and not os.path.exists(FWUPD_VM_LOG):
             raise Exception("fwupd-qubes: Getting usbvm devices info failed")
         if not os.path.exists(FWUPD_VM_LOG):

commit 76b3951f7258be7923106635c6de561c372f5b67

qubes: several small changes
1. qubes: simplify updatevm running check

Use `xl list <name>` to just check a single VM, instead of listing them
all and then looking for expected VM. This fixes the case when updatevm
name is prefix/suffix of another VM running in the system.

2. qubes: re-add showing download progress

Re-add --pass-io option that was erroneously removed during refactor.

3. qubes: drop 'script' call

'script' tool doesn't play well with qubes.VMExec service. Specifically,
it loops indefinitely when launched with unexpected (pre-existing)
children - which is the case for qrexec service.
The simplest solution is to drop it, the only downside is less nice
progress information (but still real-time progress is presented).

4. qubes: use qvm-run --no-gui

Downloading update doesn't need to wait for the GUI session. This change
makes it work even if user hasn't logged to the graphical session yet.

5. qubes: don't fail if ~/.cache doesn't exist in updatevm

Create parent directory automatically too.

6. qubes: remove remainings of manual shell command escaping

Since all the places use now call target commands without passing it
through shell, manual escaping is not needed anymore.
While at it, simplify filename calculation - use just base name of the
update (which on lvfs contains its hash), instead of assuming specific
URL prefix and cutting it off.

7. qubes: use consistent cache dir

Use /var/cache/fwupd/qubes as cache dir in all the scripts. Previously
some used /var/cache/qubes-fwupd and some /var/cache/fwupd.

Fixes: 63903c82d "ontrib/qubes: Do not dynamically create shell commands"
Fixes: 3150f04ac "trivial: contrib/qubes: Place cache in /var/cache"
Signed-off-by: Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>

contrib/qubes/src/fwupd_receive_updates.py
@@ -16,7 +16,7 @@ import re
 import shutil
 import subprocess
 
-FWUPD_DOM0_DIR = "/var/cache/fwupd"
+FWUPD_DOM0_DIR = "/var/cache/fwupd/qubes"
 FWUPD_DOM0_UPDATES_DIR = os.path.join(FWUPD_DOM0_DIR, "updates")
 FWUPD_DOM0_UNTRUSTED_DIR = os.path.join(FWUPD_DOM0_UPDATES_DIR, "untrusted")
 FWUPD_DOM0_METADATA_DIR = os.path.join(FWUPD_DOM0_DIR, "metadata")
@@ -202,6 +202,7 @@ class FwupdReceiveUpdates:
         cmd_copy = [
             "qvm-run",
             "--pass-io",
+            "--no-gui",
             "-q",
             "-a",
             "--no-shell",
@@ -254,6 +255,7 @@ class FwupdReceiveUpdates:
         cmd_copy_metadata_file = [
             "qvm-run",
             "--pass-io",
+            "--no-gui",
             "--no-shell",
             "--",
             updatevm,
@@ -265,6 +267,7 @@ class FwupdReceiveUpdates:
         cmd_copy_metadata_file_signature = [
             "qvm-run",
             "--pass-io",
+            "--no-gui",
             "--no-shell",
             "--",
             updatevm,

contrib/qubes/src/qubes_fwupd_update.py
@@ -13,7 +13,7 @@ import re
 import shlex
 import subprocess
 
-FWUPD_DOM0_DIR = "/var/cache/fwupd"
+FWUPD_DOM0_DIR = "/var/cache/fwupd/qubes"
 FWUPD_VM_DOWNLOAD = "/usr/libexec/qubes-fwupd/fwupd_download_updates.py"
 FWUPD_DOM0_UPDATES_DIR = os.path.join(FWUPD_DOM0_DIR, "updates")
 FWUPD_DOWNLOAD_PREFIX = "https://fwupd.org/downloads/"
@@ -25,6 +25,7 @@ WARNING_COLOR = "\033[93m"
 
 run_cmd = (
     "qvm-run",
+    "--pass-io",
     "--no-gui",
     "--no-shell",
     "-q",
@@ -41,12 +42,7 @@ def run_in_tty(updatevm, args, **kwargs):
         (
             *run_cmd,
             updatevm,
-            "script",
-            "--quiet",
-            "--return",
-            "--command",
-            shlex.join(args),
-            "/dev/null",
+            *args,
         ),
         stdin=subprocess.DEVNULL,
         **kwargs,
@@ -85,17 +81,12 @@ class FwupdUpdate:
 
     def _check_updatevm(self):
         """Checks if updatevm is running"""
-        cmd_xl_list = ["xl", "list"]
-        p = subprocess.Popen(cmd_xl_list, stdout=subprocess.PIPE)
+        cmd_xl_list = ["xl", "list", "--", self.updatevm]
+        p = subprocess.Popen(
+            cmd_xl_list, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
         output = p.communicate()[0].decode()
-        if p.returncode != 0:
-            raise Exception("fwupd-qubes: updatevm check failed")
-        return self.updatevm in output
-
-    def _encrypt_update_url(self, url):
-        self.enc_url = url
-        self.arch_name = "untrusted.cab"
-        self.arch_path = os.path.join(FWUPD_DOM0_UPDATES_DIR, self.arch_name)
+        return p.returncode == 0
 
     def download_metadata(self, whonix=False, metadata_url=None):
         """Initialize downloading metadata files.
@@ -136,7 +127,8 @@ class FwupdUpdate:
             raise Exception(f"{self.updatevm} is not running!!")
         if not os.path.exists(FWUPD_DOM0_DIR):
             self._create_dirs(FWUPD_DOM0_DIR)
-        self._encrypt_update_url(url)
+        self.arch_name = os.path.basename(url)
+        self.arch_path = os.path.join(FWUPD_DOM0_UPDATES_DIR, self.arch_name)
         if not os.path.exists(self.arch_path):
             cmd_firmware_download = [
                 "qvm-run",
@@ -148,13 +140,9 @@ class FwupdUpdate:
                 "--color-stderr=31",
                 "--",
                 self.updatevm,
-                "script",
-                "--quiet",
-                "--return",
-                "--command",
-                shlex.join(
-                    (FWUPD_VM_DOWNLOAD, f"--url={self.enc_url}", f"--sha={sha}")
-                ),
+                FWUPD_VM_DOWNLOAD,
+                f"--url={url}",
+                f"--sha={sha}",
             ]
             p = subprocess.Popen(cmd_firmware_download, stdin=subprocess.DEVNULL)
             p.wait()

contrib/qubes/src/qubes_fwupdmgr.py
@@ -35,7 +35,7 @@ except ModuleNotFoundError:
         "qubes-fwupd modules not found.  You may need to reinstall package."
     )
 
-FWUPD_DOM0_DIR = "/var/cache/qubes-fwupd"
+FWUPD_DOM0_DIR = "/var/cache/fwupd/qubes"
 FWUPD_DOM0_METADATA_DIR = os.path.join(FWUPD_DOM0_DIR, "metadata")
 FWUPD_DOM0_UPDATES_DIR = os.path.join(FWUPD_DOM0_DIR, "updates")
 FWUPD_DOM0_METADATA_SIGNATURE = os.path.join(

contrib/qubes/src/vms/fwupd_common_vm.py
@@ -33,7 +33,7 @@ class FwupdVmCommon:
             raise Exception("Creating directories failed, no paths given.")
         for file_path in args:
             if not os.path.exists(file_path):
-                os.mkdir(file_path)
+                os.makedirs(file_path)
                 os.chown(file_path, -1, qubes_gid)
             elif os.stat(file_path).st_gid != qubes_gid:
                 print(

contrib/qubes/src/vms/fwupd_download_updates.py
@@ -21,20 +21,6 @@ METADATA_URL_JCAT = "https://fwupd.org/downloads/firmware.xml.gz.jcat"
 
 
 class DownloadData(FwupdVmCommon):
-    def _decrypt_update_url(self, url):
-        self.dec_url = url
-        if "--and--" in url:
-            self.dec_url = self.dec_url.replace("--and--", "&")
-            self.arch_name = "untrusted.cab"
-        if "--or--" in url:
-            self.dec_url = self.dec_url.replace("--or--", "|")
-            self.arch_name = "untrusted.cab"
-        if "--hash--" in url:
-            self.dec_url = self.dec_url.replace("--hash--", "#")
-            self.arch_name = "untrusted.cab"
-        if "%20" in url:
-            self.arch_name = "untrusted.cab"
-
     def _download_metadata_file(self):
         """Download metadata file"""
         if self.custom_url is None:
@@ -114,10 +100,9 @@ class DownloadData(FwupdVmCommon):
         url - url address of the update
         """
         self.validate_vm_dirs()
-        self.arch_name = url.replace("https://fwupd.org/downloads/", "")
-        self._decrypt_update_url(url)
+        self.arch_name = os.path.basename(url)
         update_path = os.path.join(FWUPD_VM_UPDATES_DIR, self.arch_name)
-        cmd_update = ["wget", "-O", update_path, self.dec_url]
+        cmd_update = ["wget", "-O", update_path, url]
         p = subprocess.Popen(cmd_update)
         p.wait()
         if p.returncode != 0:

contrib/qubes/test/test_qubes_fwupdmgr.py
@@ -30,7 +30,7 @@ if os.path.exists(QUBES_FWUPDMGR_REPO):
 elif os.path.exists(QUBES_FWUPDMGR_BINDIR):
     qfwupd = imp.load_source("qubes_fwupdmgr", QUBES_FWUPDMGR_BINDIR)
 
-FWUPD_DOM0_DIR = "/var/cache/qubes-fwupd"
+FWUPD_DOM0_DIR = "/var/cache/fwupd/qubes"
 FWUPD_DOM0_UPDATES_DIR = os.path.join(FWUPD_DOM0_DIR, "updates")
 FWUPD_DOM0_UNTRUSTED_DIR = os.path.join(FWUPD_DOM0_UPDATES_DIR, "untrusted")
 FWUPD_DOM0_METADATA_DIR = os.path.join(FWUPD_DOM0_DIR, "metadata")