fwupd/fwupd

commit 4110e356bfac4be97060c27c014b4785f4692a3f

contrib/qubes: Do not perform manual .cab verification
fwupd already does this, and besides the file has been checked against
the metadata already.

contrib/qubes/src/fwupd_receive_updates.py
@@ -107,20 +107,6 @@ class FwupdReceiveUpdates:
                     f"the personal data!!{WARNING_COLOR}"
                 )
 
-    def _extract_archive(self, archive_path, output_path):
-        """Extracts archive file to the specified directory.
-
-        Keyword arguments:
-        archive_path -- absolute path to archive file
-        output_path -- absolute path to the output directory
-        """
-        cmd_extract = ["gcab", "-x", f"--directory={output_path}", f"{archive_path}"]
-        shutil.copy(archive_path, FWUPD_DOM0_UPDATES_DIR)
-        p = subprocess.Popen(cmd_extract, stdout=subprocess.PIPE)
-        p.communicate()[0].decode("ascii")
-        if p.returncode != 0:
-            raise Exception(f"gcab: Error while extracting {archive_path}.")
-
     def _jcat_verification(self, file_path, file_directory):
         """Verifies sha1 and sha256 checksum, GPG signature,
         and PKCS#7 signature.
@@ -171,13 +157,7 @@ class FwupdReceiveUpdates:
             FWUPD_DOM0_UNTRUSTED_DIR, fwupd_firmware_file_regex, updatevm
         )
         self._check_shasum(dom0_firmware_untrusted_path, sha)
-        untrusted_dir_name = filename.replace(".cab", "")
-        self._extract_archive(dom0_firmware_untrusted_path, FWUPD_DOM0_UNTRUSTED_DIR)
-        signature_name = os.path.join(FWUPD_DOM0_UNTRUSTED_DIR, "firmware*.jcat")
-        file_path = glob.glob(signature_name)
-        if not file_path:
-            raise FileNotFoundError("jcat file not found!")
-        self._jcat_verification(file_path[0], FWUPD_DOM0_UNTRUSTED_DIR)
+        # jcat verification will be done by fwupd itself
         os.umask(self.old_umask)
         if untrusted_dir_name == "untrusted":
             untrusted_dir_name = "trusted"

contrib/qubes/src/vms/fwupd_usbvm_validate.py
@@ -44,19 +44,6 @@ class FwupdUsbvmUpdates(FwupdVmCommon):
             if os.path.islink(path_f) or not os.path.isfile(path_f):
                 raise Exception("Dom0 sent not regular file")
 
-    def _extract_archive(self, archive_path, output_path):
-        """Extracts archive file to the specified directory.
-
-        Keyword arguments:
-        archive_path -- absolute path to archive file
-        output_path -- absolute path to the output directory
-        """
-        cmd_extract = ["gcab", "-x", f"--directory={output_path}", f"{archive_path}"]
-        p = subprocess.Popen(cmd_extract, stdout=subprocess.PIPE)
-        p.communicate()[0].decode("ascii")
-        if p.returncode != 0:
-            raise Exception("gcab: Error while extracting %s." % archive_path)
-
     def validate_metadata(self, metadata_url=None):
         """Validates received the metadata files."""
         print("Running validation of the metadata files")
@@ -85,17 +72,8 @@ class FwupdUsbvmUpdates(FwupdVmCommon):
         output_path = archive_path.replace(".cab", "")
         arch_temp = os.path.join(output_path, archive_name)
         os.mkdir(output_path)
+        # jcat verification will be done by fwupd itself
         shutil.copyfile(archive_path, arch_temp)
-        self._extract_archive(arch_temp, output_path)
-        signature_name = os.path.join(output_path, "firmware*.jcat")
-        file_path = glob.glob(signature_name)
-        try:
-            self._jcat_verification(file_path[0], output_path)
-            shutil.rmtree(output_path)
-        except Exception as e:
-            print(str(e), file=sys.stderr)
-            self.clean_vm_cache()
-            exit(1)
 
 
 def main():

commit 8979d5a6aa2f48a21f871162b3f8dd287cb6e6ed

qubes: fix update archive handling
After removing archive extraction, it's the cab file that needs to be
stored, not its extracted content. Adjust the handle_fw_update()
function accordingly.
But applying Heads update is done manually, not through fwupd plugin, so
still extract the cab archive there manually. DMI check also needs
extracted archive.
Update tests too.

Fixes: 4110e356b "contrib/qubes: Do not perform manual .cab verification"
Signed-off-by: Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>

contrib/qubes/src/fwupd_receive_updates.py
@@ -167,6 +167,7 @@ class FwupdReceiveUpdates:
             "-a",
             "--no-shell",
             "--",
+            updatevm,
             "cat",
             "--",
             updatevm_firmware_file_path,
@@ -180,17 +181,9 @@ class FwupdReceiveUpdates:
         self._check_shasum(dom0_firmware_untrusted_path, sha)
         # jcat verification will be done by fwupd itself
         os.umask(self.old_umask)
-        if untrusted_dir_name == "untrusted":
-            untrusted_dir_name = "trusted"
-            verified_file = os.path.join(FWUPD_DOM0_UPDATES_DIR, filename)
-            self.arch_name = "trusted.cab"
-            self.arch_path = os.path.join(FWUPD_DOM0_UPDATES_DIR, self.arch_name)
-            shutil.move(verified_file, self.arch_path)
-        else:
-            self.arch_path = os.path.join(FWUPD_DOM0_UPDATES_DIR, filename)
-        dir_name = os.path.join(FWUPD_DOM0_UPDATES_DIR, untrusted_dir_name)
-        os.remove(dom0_firmware_untrusted_path)
-        shutil.move(FWUPD_DOM0_UNTRUSTED_DIR, dir_name)
+        self.arch_name = filename
+        self.arch_path = os.path.join(FWUPD_DOM0_UPDATES_DIR, filename)
+        shutil.move(dom0_firmware_untrusted_path, self.arch_path)
 
     def handle_metadata_update(self, updatevm, metadata_url=None):
         """Copies metadata files from the updateVM.
@@ -247,7 +240,6 @@ class FwupdReceiveUpdates:
 
         self._pgp_verification(self.metadata_file + '.asc', self.metadata_file)
         self._reconstruct_jcat(self.metadata_file_jcat, self.metadata_file)
-        os.umask(self.old_umask)
 
     def clean_cache(self):
         """Removes updates data"""

contrib/qubes/src/qubes_fwupd_heads.py
@@ -8,6 +8,7 @@
 #
 
 import subprocess
+import tempfile
 import os
 import shutil
 import xml.etree.ElementTree as ET
@@ -99,7 +100,6 @@ class FwupdHeads:
         Copies heads update to the boot path
         """
         heads_boot_path = os.path.join(HEADS_UPDATES_DIR, self.heads_update_version)
-        update_path = arch_path.replace(".cab", "/firmware.rom")
 
         heads_update_path = os.path.join(heads_boot_path, "firmware.rom")
         if not os.path.exists(HEADS_UPDATES_DIR):
@@ -109,7 +109,14 @@ class FwupdHeads:
             return EXIT_CODES["NOTHING_TO_DO"]
         else:
             os.mkdir(heads_boot_path)
-            shutil.copyfile(update_path, heads_update_path)
+            with tempfile.TemporaryDirectory() as tmpdir:
+                cmd_extract = ["gcab", "-x", f"--directory={tmpdir}", "--", arch_path]
+                p = subprocess.Popen(cmd_extract, stdout=subprocess.PIPE)
+                p.communicate()
+                if p.returncode != 0:
+                    raise Exception(f"gcab: Error while extracting {arch_path}.")
+                update_path = os.path.join(tmpdir, "firmware.rom")
+                shutil.copyfile(update_path, heads_update_path)
             print(
                 f"Heads Update == {self.heads_update_version} "
                 f"available at {heads_boot_path}"

contrib/qubes/src/qubes_fwupdmgr.py
@@ -12,6 +12,7 @@ import os
 import re
 import shutil
 import subprocess
+import tempfile
 import sys
 import xml.etree.ElementTree as ET
 
@@ -170,9 +171,8 @@ class QubesFwupdmgr(FwupdHeads, FwupdUpdate, FwupdReceiveUpdates):
         self.download_firmware_updates(url, sha, whonix=whonix)
         if not self.cached:
             self.handle_fw_update(self.updatevm, sha, self.arch_name)
-        update_path = self.arch_path.replace(".cab", "")
-        if not os.path.exists(update_path):
-            raise NotADirectoryError("Firmware update files do not exist")
+        if not os.path.exists(self.arch_path):
+            raise FileNotFoundError("Firmware update files do not exist")
 
     def _user_input(self, updates_list, downgrade=False):
         """UI for update process.
@@ -276,17 +276,23 @@ class QubesFwupdmgr(FwupdHeads, FwupdUpdate, FwupdReceiveUpdates):
             raise Exception("dmidecode: Reading DMI failed")
         return p.communicate()[0].decode()
 
-    def _verify_dmi(self, path, version, downgrade=False):
+    def _verify_dmi(self, arch_path, version, downgrade=False):
         """Verifies DMI tables for BIOS updates.
 
         Keywords arguments:
-        path -- absolute path of the updates files
+        arch_path -- absolute path of the update archive
         version -- version of the update
         downgrade -- downgrade flag
         """
         dmi_info = self._read_dmi()
-        path_metainfo = os.path.join(path, "firmware.metainfo.xml")
-        tree = ET.parse(path_metainfo)
+        with tempfile.TemporaryDirectory() as tmpdir:
+            cmd_extract = ["gcab", "-x", f"--directory={tmpdir}", "--", arch_path]
+            p = subprocess.Popen(cmd_extract, stdout=subprocess.PIPE)
+            p.communicate()
+            if p.returncode != 0:
+                raise Exception(f"gcab: Error while extracting {arch_path}.")
+            path_metainfo = os.path.join(tmpdir, "firmware.metainfo.xml")
+            tree = ET.parse(path_metainfo)
         root = tree.getroot()
         vendor = root.find("developer_name").text
         if vendor is None:
@@ -323,8 +329,7 @@ class QubesFwupdmgr(FwupdHeads, FwupdUpdate, FwupdReceiveUpdates):
         self._download_firmware_updates(self.url, self.sha, whonix=whonix)
         if self.name == "System Firmware":
             Path(BIOS_UPDATE_FLAG).touch(mode=0o644, exist_ok=True)
-            extracted_path = self.arch_path.replace(".cab", "")
-            self._verify_dmi(extracted_path, self.version)
+            self._verify_dmi(self.arch_path, self.version)
         self._install_dom0_firmware_update(self.arch_path)
 
     def _parse_downgrades(self, device_list):
@@ -393,9 +398,8 @@ class QubesFwupdmgr(FwupdHeads, FwupdUpdate, FwupdReceiveUpdates):
         self._download_firmware_updates(downgrade_url, downgrade_sha, whonix=whonix)
         if downgrade["Name"] == "System Firmware":
             Path(BIOS_UPDATE_FLAG).touch(mode=0o644, exist_ok=True)
-            extracted_path = self.arch_path.replace(".cab", "")
             self._verify_dmi(
-                extracted_path,
+                self.arch_path,
                 downgrade["Version"],
                 downgrade=True,
             )

contrib/qubes/test/test_qubes_fwupdmgr.py
@@ -15,6 +15,7 @@ import sys
 import imp
 import io
 import platform
+import tempfile
 from packaging.version import Version
 from pathlib import Path
 from .fwupd_logs import UPDATE_INFO, GET_DEVICES, DMI_DECODE
@@ -278,7 +279,12 @@ class TestQubesFwupdmgr(unittest.TestCase):
     )
     def test_verify_dmi(self, output):
         self.q.dmi_version = "P.1.0"
-        self.q._verify_dmi("test/logs/", "P1.1")
+        with tempfile.TemporaryDirectory() as tmpdir:
+            arch_name = tmpdir + "/firmware.cab"
+            subprocess.check_call(
+                ["gcab", "-c", arch_name, "firmware.metainfo.xml"], cwd="test/logs"
+            )
+            self.q._verify_dmi(arch_name, "P1.1")
 
     @patch(
         "test.test_qubes_fwupdmgr.qfwupd.QubesFwupdmgr._read_dmi",
@@ -287,7 +293,13 @@ class TestQubesFwupdmgr(unittest.TestCase):
     def test_verify_dmi_wrong_vendor(self, output):
         with self.assertRaises(ValueError) as wrong_vendor:
             self.q.dmi_version = "P.1.0"
-            self.q._verify_dmi("test/logs/metainfo_name/", "P1.1")
+            with tempfile.TemporaryDirectory() as tmpdir:
+                arch_name = tmpdir + "/firmware.cab"
+                subprocess.check_call(
+                    ["gcab", "-c", arch_name, "firmware.metainfo.xml"],
+                    cwd="test/logs/metainfo_name",
+                )
+                self.q._verify_dmi(arch_name, "P1.1")
         self.assertIn("Wrong firmware provider.", str(wrong_vendor.exception))
 
     @patch(
@@ -297,7 +309,13 @@ class TestQubesFwupdmgr(unittest.TestCase):
     def test_verify_dmi_version(self, output):
         self.q.dmi_version = "P1.0"
         with self.assertRaises(ValueError) as downgrade:
-            self.q._verify_dmi("test/logs/metainfo_version/", "P0.1")
+            with tempfile.TemporaryDirectory() as tmpdir:
+                arch_name = tmpdir + "/firmware.cab"
+                subprocess.check_call(
+                    ["gcab", "-c", arch_name, "firmware.metainfo.xml"],
+                    cwd="test/logs/metainfo_version",
+                )
+                self.q._verify_dmi(arch_name, "P0.1")
         self.assertIn("P0.1 < P1.0 Downgrade not allowed", str(downgrade.exception))
 
     @unittest.skipUnless(device_connected_dom0(), REQUIRED_DEV)