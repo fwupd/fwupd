fwupd/fwupd

commit e70e067e4014e4a3969b7de4ad75861c1fc78fb2

Record the ESP type in the firmware report sent to the LVFS
We have a failure where setting the parition as msftdata means that the update
fails, and setting is as ESP succeeds.

We want to see how common this failure is, and provide a known-failure matching
rule on the LVFS so we can redirect the user to a wiki page.

Fixes https://github.com/fwupd/fwupd/issues/5612

plugins/uefi-capsule/fu-uefi-device.c
@@ -122,6 +122,7 @@ fu_uefi_device_to_string(FuDevice *device, guint idt, GString *str)
 {
 	FuUefiDevice *self = FU_UEFI_DEVICE(device);
 	FuUefiDevicePrivate *priv = GET_PRIVATE(self);
+
 	fu_string_append(str, idt, "Kind", fu_uefi_device_kind_to_string(priv->kind));
 	fu_string_append(str, idt, "FwClass", priv->fw_class);
 	fu_string_append_kx(str, idt, "CapsuleFlags", priv->capsule_flags);
@@ -133,7 +134,17 @@ fu_uefi_device_to_string(FuDevice *device, guint idt, GString *str)
 			 fu_uefi_device_status_to_string(priv->last_attempt_status));
 	fu_string_append_kx(str, idt, "LastAttemptVersion", priv->last_attempt_version);
 	if (priv->esp != NULL) {
+		g_autofree gchar *mount_point = fu_volume_get_mount_point(priv->esp);
 		fu_string_append(str, idt, "EspId", fu_volume_get_id(priv->esp));
+		if (mount_point != NULL)
+			fu_string_append(str, idt, "EspPath", mount_point);
+		if (fu_volume_get_partition_kind(priv->esp) != NULL) {
+			const gchar *kind = fu_volume_get_partition_kind(priv->esp);
+			const gchar *guid = fu_volume_kind_convert_to_gpt(kind);
+			fu_string_append(str, idt, "EspKind", kind);
+			if (g_strcmp0(kind, guid) != 0)
+				fu_string_append(str, idt, "EspGuid", guid);
+		}
 	}
 	fu_string_append_ku(str, idt, "RequireESPFreeSpace", priv->require_esp_free_space);
 }
@@ -143,14 +154,20 @@ fu_uefi_device_report_metadata_pre(FuDevice *device, GHashTable *metadata)
 {
 	FuUefiDevice *self = FU_UEFI_DEVICE(device);
 	FuUefiDevicePrivate *priv = GET_PRIVATE(self);
+	g_autofree gchar *mount_point = fu_volume_get_mount_point(priv->esp);
 
 	/* record if we had an invalid header during update */
 	g_hash_table_insert(metadata,
 			    g_strdup("MissingCapsuleHeader"),
 			    g_strdup(priv->missing_header ? "True" : "False"));
 
-	/* where the ESP was mounted during installation */
+	/* where and how the ESP was mounted during installation */
 	g_hash_table_insert(metadata, g_strdup("EspPath"), fu_volume_get_mount_point(priv->esp));
+	if (fu_volume_get_partition_kind(priv->esp) != NULL) {
+		g_hash_table_insert(metadata,
+				    g_strdup("EspKind"),
+				    g_strdup(fu_volume_get_partition_kind(priv->esp)));
+	}
 }
 
 static void

libfwupdplugin/fu-volume.c
@@ -24,12 +24,20 @@
 
 struct _FuVolume {
 	GObject parent_instance;
+	gchar *partition_kind;
 	GDBusProxy *proxy_blk;
 	GDBusProxy *proxy_fs;
 	gchar *mount_path; /* only when mounted ourselves */
 };
 
-enum { PROP_0, PROP_MOUNT_PATH, PROP_PROXY_BLOCK, PROP_PROXY_FILESYSTEM, PROP_LAST };
+enum {
+	PROP_0,
+	PROP_PARTITION_KIND,
+	PROP_MOUNT_PATH,
+	PROP_PROXY_BLOCK,
+	PROP_PROXY_FILESYSTEM,
+	PROP_LAST
+};
 
 G_DEFINE_TYPE(FuVolume, fu_volume, G_TYPE_OBJECT)
 
@@ -37,6 +45,7 @@ static void
 fu_volume_finalize(GObject *obj)
 {
 	FuVolume *self = FU_VOLUME(obj);
+	g_free(self->partition_kind);
 	g_free(self->mount_path);
 	if (self->proxy_blk != NULL)
 		g_object_unref(self->proxy_blk);
@@ -50,6 +59,9 @@ fu_volume_get_property(GObject *object, guint prop_id, GValue *value, GParamSpec
 {
 	FuVolume *self = FU_VOLUME(object);
 	switch (prop_id) {
+	case PROP_PARTITION_KIND:
+		g_value_set_string(value, self->partition_kind);
+		break;
 	case PROP_MOUNT_PATH:
 		g_value_set_string(value, self->mount_path);
 		break;
@@ -70,6 +82,9 @@ fu_volume_set_property(GObject *object, guint prop_id, const GValue *value, GPar
 {
 	FuVolume *self = FU_VOLUME(object);
 	switch (prop_id) {
+	case PROP_PARTITION_KIND:
+		self->partition_kind = g_value_dup_string(value);
+		break;
 	case PROP_MOUNT_PATH:
 		self->mount_path = g_value_dup_string(value);
 		break;
@@ -139,6 +154,21 @@ fu_volume_class_init(FuVolumeClass *klass)
 				NULL,
 				G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE | G_PARAM_STATIC_NAME);
 	g_object_class_install_property(object_class, PROP_MOUNT_PATH, pspec);
+
+	/**
+	 * FuVolume:partition-kind:
+	 *
+	 * The partition kind.
+	 *
+	 * Since: 1.8.13
+	 */
+	pspec =
+	    g_param_spec_string("partition-kind",
+				NULL,
+				NULL,
+				NULL,
+				G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE | G_PARAM_STATIC_NAME);
+	g_object_class_install_property(object_class, PROP_PARTITION_KIND, pspec);
 }
 
 static void
@@ -165,6 +195,26 @@ fu_volume_get_id(FuVolume *self)
 	if (self->proxy_blk != NULL)
 		return g_dbus_proxy_get_object_path(self->proxy_blk);
 	return NULL;
+}
+
+/**
+ * fu_volume_get_partition_kind:
+ * @self: a @FuVolume
+ *
+ * Gets the partition kind of the volume mount point.
+ *
+ * NOTE: If you want this to be converted to a GPT-style GUID then use
+ * fu_volume_kind_convert_to_gpt() on the return value of this function.
+ *
+ * Returns: partition kind, e.g. `0x06`, `vfat` or a GUID like `FU_VOLUME_KIND_ESP`
+ *
+ * Since: 1.8.13
+ **/
+const gchar *
+fu_volume_get_partition_kind(FuVolume *self)
+{
+	g_return_val_if_fail(FU_IS_VOLUME(self), NULL);
+	return self->partition_kind;
 }
 
 /**
@@ -565,6 +615,8 @@ fu_volume_new_by_kind(const gchar *kind, GError **error)
 			continue;
 		}
 		vol = g_object_new(FU_TYPE_VOLUME,
+				   "partition-kind",
+				   type_str,
 				   "proxy-block",
 				   proxy_blk,
 				   "proxy-filesystem",

libfwupdplugin/fu-volume.h
@@ -40,6 +40,8 @@ gboolean
 fu_volume_is_mounted(FuVolume *self);
 gboolean
 fu_volume_is_encrypted(FuVolume *self);
+const gchar *
+fu_volume_get_partition_kind(FuVolume *self);
 gchar *
 fu_volume_get_mount_point(FuVolume *self);
 gboolean

libfwupdplugin/fwupdplugin.map
@@ -1189,5 +1189,6 @@ LIBFWUPDPLUGIN_1.8.12 {
 LIBFWUPDPLUGIN_1.8.13 {
   global:
     fu_device_ensure_from_component;
+    fu_volume_get_partition_kind;
   local: *;
 } LIBFWUPDPLUGIN_1.8.12;

commit 0eec0c4a66a5e8a5957172e34fb675cfce646617

trivial: Fix tiny memory leak when getting the ESP
This was introduced in e70e067e4014e4a3969b7de4ad75861c1fc78fb2

plugins/uefi-capsule/fu-uefi-device.c
@@ -162,7 +162,7 @@ fu_uefi_device_report_metadata_pre(FuDevice *device, GHashTable *metadata)
 			    g_strdup(priv->missing_header ? "True" : "False"));
 
 	/* where and how the ESP was mounted during installation */
-	g_hash_table_insert(metadata, g_strdup("EspPath"), fu_volume_get_mount_point(priv->esp));
+	g_hash_table_insert(metadata, g_strdup("EspPath"), mount_point);
 	if (fu_volume_get_partition_kind(priv->esp) != NULL) {
 		g_hash_table_insert(metadata,
 				    g_strdup("EspKind"),