ed1b2bce85363f8efeb08da683678c4848a35b86
Richard Hughes <richard@hughsie.com>
2023-04-18 12:51:14 +0100

    aver-hid: Add a new plugin to update the AVer FONE540

---
+++ plugins/aver-hid/fu-aver-hid-plugin.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2023 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include <fwupdplugin.h>
+
+G_DECLARE_FINAL_TYPE(FuAverHidPlugin, fu_aver_hid_plugin, FU, AVER_HID_PLUGIN, FuPlugin)
