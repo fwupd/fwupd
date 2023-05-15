9fb95a3a9a75c07b6378cdb2c5fcc7362f190d26
Richard Hughes <richard@hughsie.com>
2023-05-15 18:47:21 +0100

    trivial: Ignore more things when running with Tartan

--- contrib/tartan.sh
+++ contrib/tartan.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 /usr/bin/scan-build -load-plugin /usr/local/lib64/tartan/16.0/libtartan.so \
-	-disable-checker unix.Malloc \
 	-disable-checker core.CallAndMessage \
+	-disable-checker core.NullDereference \
 	-disable-checker deadcode.DeadStores \
+	-disable-checker unix.Malloc \
 	-enable-checker tartan.GErrorChecker \
-	-v --view "$@"
+	--status-bugs -v "$@"
