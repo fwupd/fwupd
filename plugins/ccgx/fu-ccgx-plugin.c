fb25a537a6ab212251086e963703ddaa3ea349a3
Daniel Schaefer <dhs@frame.work>
2023-05-17 23:10:15 +0800

    ccgx: Add quirk for Framework HDMI/DP cards
    Signed-off-by: Daniel Schaefer <dhs@frame.work>
    
    ccgx: Detect firmware on HID only CCG3
    
    ```
    > ninja -C build && sudo ./build/src/fwupdtool --plugins ccgx get-devices --show-all
    ninja: Entering directory `build'
    ninja: no work to do.
    Loading…                 [************************************** ]
    WARNING: This package has not been validated, it may not work properly.
    Framework Laptop (12th Gen Intel Core)
    │
    └─DisplayPort Expansion Card:
          Device ID:          54dda0fbfd5ac0dee5efc0ec4baefc9190d666ac
          Current version:    3.0.17 Build 100
          Vendor:             Framework (USB:0x32AC)
          Serial Number:      11AD1D00C9D73E0223190B00
          GUIDs:              7e137108-1fe0-5820-b962-6603fa7b2070 ← USB\VID_32AC&PID_0003
                              a671c02b-adb7-5f2d-b7b6-818cb56c9deb ← USB\VID_32AC&PID_0003&REV_0000
          Device Flags:       • Updatable
                              • System requires external power source
                              • Unsigned Payload
    ```
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>
    
    ccgx: Implement pure HID firmware update
    
    Currently only handles one image, so you need to run it twice to update
    both sides.
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>
    
    ccgx: Fix detailed version
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>
    
    ccgx: Add more info to pure hid device
    
    ```
    > ninja -C build && sudo -E ./build/src/fwupdtool --plugins ccgx get-devices
    ninja: Entering directory `build'
    [5/8] Generating symbol file src/libfwupdengine.so.p/libfwupdengine.so.symbols
    Place your right index finger on the fingerprint reader
    Loading…                 [************************************** ]
    WARNING: This package has not been validated, it may not work properly.
    Framework Laptop (12th Gen Intel Core)
    │
    └─DisplayPort Expansion Card:
          Device ID:          131395051f4ab8ccb1a73e24b5cb70ec2a60bdd7
          Current version:    3.0.17.100
          Bootloader Version: 3.0.2.900
          Vendor:             Framework (USB:0x32AC)
          Serial Number:      11AD1D00C9D73E0223190B00
          GUIDs:              7e137108-1fe0-5820-b962-6603fa7b2070 ← USB\VID_32AC&PID_0003
                              a671c02b-adb7-5f2d-b7b6-818cb56c9deb ← USB\VID_32AC&PID_0003&REV_0000
          Device Flags:       • Updatable
                              • Unsigned Payload
    ```
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- plugins/ccgx/fu-ccgx-plugin.c
+++ plugins/ccgx/fu-ccgx-plugin.c
@@ -10,6 +10,7 @@
 #include "fu-ccgx-hid-device.h"
 #include "fu-ccgx-hpi-device.h"
 #include "fu-ccgx-plugin.h"
+#include "fu-ccgx-pure-hid-device.h"
 
 struct _FuCcgxPlugin {
 	FuPlugin parent_instance;
@@ -32,6 +33,7 @@ fu_ccgx_plugin_constructed(GObject *obj)
 	fu_context_add_quirk_key(ctx, "CcgxImageKind");
 	fu_plugin_add_firmware_gtype(plugin, NULL, FU_TYPE_CCGX_FIRMWARE);
 	fu_plugin_add_device_gtype(plugin, FU_TYPE_CCGX_HID_DEVICE);
+	fu_plugin_add_device_gtype(plugin, FU_TYPE_CCGX_PURE_HID_DEVICE);
 	fu_plugin_add_device_gtype(plugin, FU_TYPE_CCGX_HPI_DEVICE);
 }
 
---
+++ plugins/ccgx/fu-ccgx-pure-hid-device.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2023 Framework Computer Inc
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include <fwupdplugin.h>
+
+#define FU_TYPE_CCGX_PURE_HID_DEVICE (fu_ccgx_pure_hid_device_get_type())
+G_DECLARE_FINAL_TYPE(FuCcgxPureHidDevice,
+		     fu_ccgx_pure_hid_device,
+		     FU,
+		     CCGX_PURE_HID_DEVICE,
+		     FuHidDevice)
