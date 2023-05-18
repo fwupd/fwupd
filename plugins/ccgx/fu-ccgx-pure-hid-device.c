1a70e951ce621c76d895b3279bac707531d56e48
Daniel Schaefer <dhs@frame.work>
2023-05-18 13:06:58 +0800

    ccgx: Cleanup
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- plugins/ccgx/fu-ccgx-pure-hid-device.c
+++ plugins/ccgx/fu-ccgx-pure-hid-device.c
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2020 Cypress Semiconductor Corporation.
- * Copyright (C) 2020 Richard Hughes <richard@hughsie.com>
+ * Copyright (C) 2023 Framework Computer Inc
  *
  * SPDX-License-Identifier: LGPL-2.1+
  */
@@ -11,46 +10,44 @@
 
 #include "fu-ccgx-common.h"
 #include "fu-ccgx-firmware.h"
-#include "fu-ccgx-hpi-common.h"
 #include "fu-ccgx-pure-hid-device.h"
 
 struct _FuCcgxPureHidDevice {
 	FuHidDevice parent_instance;
-        //guint8 operating_mode;
-        //guint8 bootloader_info;
-        //guint8 bootmode_reason;
-        //guint32 silicon_id;
-        //bl_version: // [u8; 8],
-        //image_1_ver: // [u8; 8],
-        //image_2_ver: // [u8; 8],
-        //guint32 image_1_row;
-        //guint32 image_2_row;
-        //device_uid: // [u8; 6],
+	guint8 operating_mode;
+	//guint8 bootloader_info;
+	//guint8 bootmode_reason;
+	//guint32 silicon_id;
+	//bl_version: // [u8; 8],
+	//image_1_ver: // [u8; 8],
+	//image_2_ver: // [u8; 8],
+	//guint32 image_1_row;
+	//guint32 image_2_row;
+	//device_uid: // [u8; 6],
 };
+G_DEFINE_TYPE(FuCcgxPureHidDevice, fu_ccgx_pure_hid_device, FU_TYPE_HID_DEVICE)
 
 
 typedef struct __attribute__((packed)) {
 	guint8 report_id;
 	guint8 reserved_1;
-        guint16 signature;
-        guint8 operating_mode;
-        guint8 bootloader_info;
-        guint8 bootmode_reason;
+	guint16 signature;
+	guint8 operating_mode;
+	guint8 bootloader_info;
+	guint8 bootmode_reason;
 	guint8 reserved_2;
-        guint32 silicon_id;
-        guint32 bl_version;
-        guint8 bl_version_reserved[4];
-        guint32 image_1_ver;
-        guint8 image_1_ver_reserved[4];
-        guint32 image_2_ver;
-        guint8 image_2_ver_reserved[4];
-        guint32 image_1_row;
-        guint32 image_2_row;
-        guint8 device_uid[6];
-        guint8 reserved_3[10];
+	guint32 silicon_id;
+	guint32 bl_version;
+	guint8 bl_version_reserved[4];
+	guint32 image_1_ver;
+	guint8 image_1_ver_reserved[4];
+	guint32 image_2_ver;
+	guint8 image_2_ver_reserved[4];
+	guint32 image_1_row;
+	guint32 image_2_row;
+	guint8 device_uid[6];
+	guint8 reserved_3[10];
 } HidFwInfo;
-
-G_DEFINE_TYPE(FuCcgxPureHidDevice, fu_ccgx_pure_hid_device, FU_TYPE_HID_DEVICE)
 
 #define FU_CCGX_PURE_HID_DEVICE_TIMEOUT     5000 /* ms */
 #define FU_CCGX_PURE_HID_DEVICE_RETRY_DELAY 30   /* ms */
@@ -73,12 +70,15 @@ fu_ccgx_pure_hid_flashing_mode(FuDevice *device, gpointer user_data, GError **er
 		return FALSE;
 	}
 	g_debug("After flashing mode");
+	return TRUE;
 }
 
 static gboolean
 fu_ccgx_pure_hid_magic_unlock(FuDevice *device, gpointer user_data, GError **error)
 {
 	guint8 magic_buf[8] = {0xE4, 0x42, 0x43, 0x59, 0x00, 0x00, 0x00, 0x0B};
+	guint8 bridge_buf[8] = {0xE1, 0x06, 0x42, 0x00, 0xCC, 0xCC, 0xCC, 0xCC};
+	gboolean err;
 
 	if (!fu_hid_device_set_report(FU_HID_DEVICE(device),
 				      magic_buf[0],
@@ -91,16 +91,16 @@ fu_ccgx_pure_hid_magic_unlock(FuDevice *device, gpointer user_data, GError **err
 		return FALSE;
 	}
 
-	guint8 bridge_buf[8] = {0xE1, 0x06, 0x42, 0x00, 0xCC, 0xCC, 0xCC, 0xCC};
 
-	// Ignoring error
-	fu_hid_device_set_report(FU_HID_DEVICE(device),
+	err = fu_hid_device_set_report(FU_HID_DEVICE(device),
 				      bridge_buf[0],
 				      bridge_buf,
 				      sizeof(bridge_buf),
 				      FU_CCGX_PURE_HID_DEVICE_TIMEOUT,
 				      FU_HID_DEVICE_FLAG_NONE,
 				      NULL);
+	// Ignoring error
+	err = err;
 
 	return TRUE;
 }
@@ -108,8 +108,12 @@ fu_ccgx_pure_hid_magic_unlock(FuDevice *device, gpointer user_data, GError **err
 static gboolean
 fu_ccgx_pure_hid_get_fw_info(FuDevice *device, gpointer user_data, GError **error)
 {
+	FuCcgxPureHidDevice *self = FU_CCGX_PURE_HID_DEVICE(device);
 	gsize bufsz = 0x40;
 	g_autofree guint8 *buf = g_malloc0(bufsz);
+	HidFwInfo *info;
+	g_autofree gchar *ver1;
+	g_autofree gchar *ver2;
 
 	buf[0] = 0xE0;
 	if (!fu_hid_device_get_report(FU_HID_DEVICE(device),
@@ -121,10 +125,11 @@ fu_ccgx_pure_hid_get_fw_info(FuDevice *device, gpointer user_data, GError **erro
 				      error))
 		return FALSE;
 
-        fu_ccgx_pure_hid_flashing_mode(device, user_data, error);
+	fu_ccgx_pure_hid_flashing_mode(device, user_data, error);
 
-	HidFwInfo *info = (HidFwInfo *)(void *)buf;
+	info = (HidFwInfo *)(void *)buf;
 
+	self->operating_mode = info->operating_mode;
 	g_debug("Report ID:       0x%02x", info->report_id);
 	g_debug("Signature:       0x%02x", info->signature);
 	g_debug("Operating Mode:  0x%02x", info->operating_mode);
@@ -134,16 +139,16 @@ fu_ccgx_pure_hid_get_fw_info(FuDevice *device, gpointer user_data, GError **erro
 	g_debug("BL Version:      0x%08x", info->bl_version);
 	//g_debug("Image 1 Ver:     0x%08x", info->image_1_ver);
 	//g_debug("Image 2 Ver:     0x%08x", info->image_2_ver);
-	g_autofree gchar *ver1 = fu_ccgx_detailed_version_to_string(info->image_1_ver);
+	ver1 = fu_ccgx_detailed_version_to_string(info->image_1_ver);
 	fu_device_set_version(FU_DEVICE(device), ver1);
 	fu_device_set_version(FU_DEVICE(device), ver1);
 	fu_device_set_version_raw(FU_DEVICE(device), info->image_1_ver);
 	g_debug("Image 1 Ver:     %s", ver1);
-	g_autofree gchar *ver2 = fu_ccgx_detailed_version_to_string(info->image_2_ver);
+	ver2 = fu_ccgx_detailed_version_to_string(info->image_2_ver);
 	g_debug("Image 2 Ver:     %s", ver2);
 	g_debug("Image 1 Row:     0x%08x", info->image_1_row);
 	g_debug("Image 2 Row:     0x%08x", info->image_2_row);
-	g_debug("Device UID:      0x%012x", info->device_uid);
+	g_debug("Device UID:      0x%012x", (guint32)info->device_uid[0]);
 
 	return TRUE;
 }
@@ -151,8 +156,8 @@ fu_ccgx_pure_hid_get_fw_info(FuDevice *device, gpointer user_data, GError **erro
 static gboolean
 fu_ccgx_pure_hid_get_fw_info_wrapper(FuDevice *device, gpointer user_data, GError **error)
 {
-        fu_ccgx_pure_hid_magic_unlock(device, user_data, error);
-        return fu_ccgx_pure_hid_get_fw_info(device, user_data, error);
+	fu_ccgx_pure_hid_magic_unlock(device, user_data, error);
+	return fu_ccgx_pure_hid_get_fw_info(device, user_data, error);
 }
 
 static gboolean
@@ -171,17 +176,15 @@ fu_ccgx_pure_hid_device_detach(FuDevice *device, FuProgress *progress, GError **
 static gboolean
 fu_ccgx_pure_hid_device_setup(FuDevice *device, GError **error)
 {
-	FuCcgxPureHidDevice *self = FU_CCGX_PURE_HID_DEVICE(device);
 	g_debug("Setup Start");
 
 	/* FuUsbDevice->setup */
 	if (!FU_DEVICE_CLASS(fu_ccgx_pure_hid_device_parent_class)->setup(device, error))
 		return FALSE;
 
-        if (!fu_ccgx_pure_hid_get_fw_info_wrapper(device, NULL, error)) {
+	if (!fu_ccgx_pure_hid_get_fw_info_wrapper(device, NULL, error)) {
 		return FALSE;
 	}
-        //self->asdfa = ;
 
 	/* set summary */
 	//summary = g_strdup_printf("CX%u USB audio device", self->chip_id);
@@ -211,8 +214,6 @@ fu_ccgx_pure_hid_device_init(FuCcgxPureHidDevice *self)
 	fu_device_add_protocol(FU_DEVICE(self), "com.infineon.ccgx");
 	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_UNSIGNED_PAYLOAD);
 	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_UPDATABLE);
-	//fu_device_add_internal_flag(FU_DEVICE(self), FU_DEVICE_INTERNAL_FLAG_REPLUG_MATCH_GUID);
-	//fu_device_retry_set_delay(FU_DEVICE(self), FU_CCGX_PURE_HID_DEVICE_RETRY_DELAY);
 }
 
 static void
