1d4f14dd2687911321e0e71c64bec9196a572732
Simon Ho <simon.ho@synaptics.com>
2023-05-18 14:26:08 +0100

    synaptics-cape: Add initial support for sngl firmware

--- plugins/synaptics-cape/fu-synaptics-cape-plugin.c
+++ plugins/synaptics-cape/fu-synaptics-cape-plugin.c
@@ -9,6 +9,7 @@
 #include "fu-synaptics-cape-device.h"
 #include "fu-synaptics-cape-hid-firmware.h"
 #include "fu-synaptics-cape-plugin.h"
+#include "fu-synaptics-cape-sngl-firmware.h"
 
 struct _FuSynapticsCapePlugin {
 	FuPlugin parent_instance;
@@ -27,6 +28,7 @@ fu_synaptics_cape_plugin_constructed(GObject *obj)
 	FuPlugin *plugin = FU_PLUGIN(obj);
 	fu_plugin_add_device_gtype(plugin, FU_TYPE_SYNAPTICS_CAPE_DEVICE);
 	fu_plugin_add_firmware_gtype(plugin, NULL, FU_TYPE_SYNAPTICS_CAPE_HID_FIRMWARE);
+	fu_plugin_add_firmware_gtype(plugin, NULL, FU_TYPE_SYNAPTICS_CAPE_SNGL_FIRMWARE);
 }
 
 static void
