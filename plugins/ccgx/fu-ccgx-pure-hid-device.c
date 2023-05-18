ef5ce8f28a8b2def0a5cd21b800be80485d9c107
Daniel Schaefer <dhs@frame.work>
2023-05-18 13:25:23 +0800

    ccgx: Implement pure HID firmware update
    Currently only handles one image, so you need to run it twice to update
    both sides.
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- plugins/ccgx/fu-ccgx-pure-hid-device.c
+++ plugins/ccgx/fu-ccgx-pure-hid-device.c
@@ -12,6 +12,13 @@
 #include "fu-ccgx-firmware.h"
 #include "fu-ccgx-pure-hid-device.h"
 
+#define ROW_SIZE 128
+#define FW1_START 0x0030
+#define FW2_START 0x0200
+#define FW1_METADATA 0x03FF
+#define FW2_METADATA 0x03FE
+#define WRITE_HEADER_SIZE 4
+
 struct _FuCcgxPureHidDevice {
 	FuHidDevice parent_instance;
 	guint8 operating_mode;
@@ -79,6 +86,7 @@ fu_ccgx_pure_hid_magic_unlock(FuDevice *device, gpointer user_data, GError **err
 	guint8 magic_buf[8] = {0xE4, 0x42, 0x43, 0x59, 0x00, 0x00, 0x00, 0x0B};
 	guint8 bridge_buf[8] = {0xE1, 0x06, 0x42, 0x00, 0xCC, 0xCC, 0xCC, 0xCC};
 	gboolean err;
+	g_debug("magic unlock");
 
 	if (!fu_hid_device_set_report(FU_HID_DEVICE(device),
 				      magic_buf[0],
@@ -114,6 +122,7 @@ fu_ccgx_pure_hid_get_fw_info(FuDevice *device, gpointer user_data, GError **erro
 	HidFwInfo *info;
 	g_autofree gchar *ver1;
 	g_autofree gchar *ver2;
+	g_debug("get fw info");
 
 	buf[0] = 0xE0;
 	if (!fu_hid_device_get_report(FU_HID_DEVICE(device),
@@ -196,6 +205,179 @@ fu_ccgx_pure_hid_device_setup(FuDevice *device, GError **error)
 	return TRUE;
 }
 
+static gboolean
+fu_ccgx_pure_hid_write_row(FuDevice *device,
+				guint16 row_no,
+				const guint8 *row,
+				GError **error)
+{
+	guint8 buf[ROW_SIZE + WRITE_HEADER_SIZE];
+	buf[0] = 0xE2;
+	buf[1] = (guint8)'F'; // 0x46
+	buf[2] = row_no & 0xFF;
+	buf[3] = (row_no >> 8) & 0xFF;
+	if (!fu_memcpy_safe(buf,
+			    sizeof(buf),
+			    WRITE_HEADER_SIZE,
+			    row,
+			    ROW_SIZE,
+			    0,
+			    ROW_SIZE,
+			    error))
+		return FALSE;
+	g_debug("before write row");
+	if (!fu_hid_device_set_report(FU_HID_DEVICE(device),
+				      buf[0],
+				      buf,
+				      sizeof(buf),
+				      FU_CCGX_PURE_HID_DEVICE_TIMEOUT,
+				      FU_HID_DEVICE_FLAG_NONE,
+				      error)) {
+		g_prefix_error(error, "write row command error: ");
+		return FALSE;
+	}
+	g_debug("After write row");
+
+	return TRUE;
+}
+
+static gboolean
+fu_ccgx_pure_hid_flash_firmware_image(FuDevice *device,
+					const guint8 *fw_bytes,
+					gsize fw_len,
+					guint16 start_row,
+					guint16 metadata_row,
+					guint8 fw_img_no,
+					GError **error)
+{
+	gsize rows;
+	guint8 cmd_buf[8] = {0xE1, 0x00, 0x00, 0x00, 0xCC, 0xCC, 0xCC, 0xCC};
+	if (!fu_ccgx_pure_hid_get_fw_info_wrapper(device, NULL, error)) {
+		return FALSE;
+	}
+
+	g_assert(fw_len % ROW_SIZE == 0);
+	rows = fw_len / ROW_SIZE;
+
+	for (gsize row_no = 0; row_no < rows; row_no++) {
+		const guint8 *row = fw_bytes + row_no*ROW_SIZE;
+		if (row_no == 0) {
+			g_debug("Writing first row");
+		}
+		if (row_no == rows-1) {
+			g_debug("Writing metadata row");
+			// Last row is the metadata row. It's at a different offset
+			if (!fu_ccgx_pure_hid_write_row(device, metadata_row, row, error))
+				return FALSE;
+		} else {
+			g_debug("Writing row #%lu 0x%04lx", row_no, start_row+row_no);
+			if (!fu_ccgx_pure_hid_write_row(device, start_row + row_no, row, error))
+				return FALSE;
+		}
+	}
+
+	cmd_buf[1] = 0x04;
+	cmd_buf[2] = fw_img_no;
+	g_debug("before bootswitch");
+	if (!fu_hid_device_set_report(FU_HID_DEVICE(device),
+				      cmd_buf[0],
+				      cmd_buf,
+				      sizeof(cmd_buf),
+				      FU_CCGX_PURE_HID_DEVICE_TIMEOUT,
+				      FU_HID_DEVICE_FLAG_NONE,
+				      error)) {
+		g_prefix_error(error, "bootswitch command error: ");
+		return FALSE;
+	}
+	g_debug("After bootswitch");
+
+	cmd_buf[1] = 0x01;
+	cmd_buf[2] = 0x52;
+	g_debug("before reset");
+	if (!fu_hid_device_set_report(FU_HID_DEVICE(device),
+				      cmd_buf[0],
+				      cmd_buf,
+				      sizeof(cmd_buf),
+				      FU_CCGX_PURE_HID_DEVICE_TIMEOUT,
+				      FU_HID_DEVICE_FLAG_NONE,
+				      error)) {
+		g_prefix_error(error, "reset command error: ");
+		return FALSE;
+	}
+	g_debug("After reset");
+
+	return TRUE;
+}
+
+static gboolean
+fu_ccgx_pure_hid_device_write_firmware(FuDevice *device,
+					   FuFirmware *firmware,
+					   FuProgress *progress,
+					   FwupdInstallFlags flags,
+					   GError **error)
+{
+	FuCcgxPureHidDevice *self = FU_CCGX_PURE_HID_DEVICE(device);
+	g_autoptr(GBytes) fw = NULL;
+	gsize fw_total_size;
+	gsize fw_size;
+	const guint8 *fw_bytes;
+	const guint8 *fw1_binary;
+	const guint8 *fw2_binary;
+
+	/* get blob and read */
+	fw = fu_firmware_get_bytes(FU_FIRMWARE(firmware), error);
+	if (fw == NULL)
+		return FALSE;
+	
+	fw_bytes = g_bytes_get_data(fw, NULL);
+	fw_total_size = g_bytes_get_size(fw);
+	fw_size = fw_total_size / 2;
+
+	// TODO: Do this in prepare_firmware
+	// Make sure there are two firmware images composed of full rows
+	g_assert(fw_total_size % 2 * ROW_SIZE == 0);
+	fw1_binary = fw_bytes;
+	fw2_binary = &fw_bytes[fw_size];
+
+	fu_ccgx_pure_hid_magic_unlock(device, NULL, error);
+	if (!fu_ccgx_pure_hid_get_fw_info_wrapper(device, NULL, error)) {
+		return FALSE;
+	}
+
+	g_debug("Operating Mode:  0x%02x", self->operating_mode);
+	switch (self->operating_mode) {
+		case 0:
+		case 2:
+			// Update Image 1
+			g_debug("Flashing Image 1");
+			fu_ccgx_pure_hid_flash_firmware_image(device, fw1_binary, fw_size, FW1_START, FW1_METADATA, 1, error);
+
+			// TODO: Wait 5s
+			// TODO: Refresh HID devices
+			//fu_ccgx_pure_hid_magic_unlock(device, user_data, error);
+			// Update Image 2
+			// flash_firmware_image(device, fw2_binary, fw_size, FW2_START, FW2_METADATA, 2);
+			break;
+		case 1:
+			// Update Image 2
+			g_debug("Flashing Image 2");
+			fu_ccgx_pure_hid_flash_firmware_image(device, fw2_binary, fw_size, FW2_START, FW2_METADATA, 2, error);
+
+			// TODO: Wait 5s
+			// TODO: Refresh HID devices
+			//fu_ccgx_pure_hid_magic_unlock(device, user_data, error);
+			// Update Image 1
+			//flash_firmware_image(device, fw1_binary, fw_size, FW1_START, FW1_METADATA, 1);
+			break;
+		default:
+			// Invalid value
+			g_assert(self->operating_mode < 3);
+			break;
+	}
+
+	return TRUE;
+}
+
 //static void
 //fu_ccgx_pure_hid_device_set_progress(FuDevice *self, FuProgress *progress)
 //{
@@ -222,5 +404,6 @@ fu_ccgx_pure_hid_device_class_init(FuCcgxPureHidDeviceClass *klass)
 	FuDeviceClass *klass_device = FU_DEVICE_CLASS(klass);
 	klass_device->detach = fu_ccgx_pure_hid_device_detach;
 	klass_device->setup = fu_ccgx_pure_hid_device_setup;
+	klass_device->write_firmware = fu_ccgx_pure_hid_device_write_firmware;
 	//klass_device->set_progress = fu_ccgx_pure_hid_device_set_progress;
 }
