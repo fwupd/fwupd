2a4623422ead819443e10a23258f2a258a0abd69
Simon Ho <simon.ho@synaptics.com>
2023-05-18 12:46:34 +0100

    synaptics-cape: Add error codes, enums and structs for future devices

--- plugins/synaptics-cape/fu-synaptics-cape.rs
+++ plugins/synaptics-cape/fu-synaptics-cape.rs
@@ -15,3 +15,35 @@ struct SynapticsCapeFileHdr {
     reserved: u32le,
 }
 
+struct SynapticsCapeSnglHdr {
+    magic: u32le,
+    file_crc: u32le,
+    file_size: u32le,
+    magic2: u32le,
+    img_type: u32le,
+    fw_version: u32le,
+    vid: u16le,
+    pid: u16le,
+    fw_file_num: u32le,
+    version: u32le,
+    fw_crc: u32le,
+    _reserved: [u8; 8],
+    machine_name: [char; 16],
+    time_stamp: [char; 16],
+}
+
+enum SynapticsCapeSnglImgTypeId {
+    Hid0 = 0x30444948, // hid file for partition 0
+    Hid1 = 0x31444948, // hid file for partition 1
+    Hof0 = 0x30464F48, // hid + offer file for partition 0
+    Hof1 = 0x31464F48, // hid + offer file for partition 1
+    Sfsx = 0x58534653, // sfs file
+    Sofx = 0x58464F53, // sfs + offer file
+    Sign = 0x4e474953, // digital signature file
+}
+
+enum SynapticsCapeFirmwarePartition {
+    Auto,
+    1,
+    2,
+}
