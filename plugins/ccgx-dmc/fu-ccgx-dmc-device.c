7ee9722200f8caa558549307e1f10012abba5dae
Richard Hughes <richard@hughsie.com>
2023-06-13 08:10:26 +0100

    ccgx-dmc: Default to TriggerCode of 0x1
    The trigger code is only used when `CcgxDmcUpdateModel` is `DownloadTrigger`.
    
    This is on advice of Infineon.

--- plugins/ccgx-dmc/fu-ccgx-dmc-device.c
+++ plugins/ccgx-dmc/fu-ccgx-dmc-device.c
@@ -800,6 +800,7 @@ fu_ccgx_dmc_device_init(FuCcgxDmcDevice *self)
 {
 	self->ep_intr_in = DMC_INTERRUPT_PIPE_ID;
 	self->ep_bulk_out = DMC_BULK_PIPE_ID;
+	self->trigger_code = 0x1;
 	fu_device_add_protocol(FU_DEVICE(self), "com.cypress.ccgx.dmc");
 	fu_device_add_protocol(FU_DEVICE(self), "com.infineon.ccgx.dmc");
 	fu_device_set_version_format(FU_DEVICE(self), FWUPD_VERSION_FORMAT_QUAD);
