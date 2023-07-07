fwupd/fwupd

commit 16b463334e0119374a5bd3e31e8dc6432221072d

Export the generic Intel Thunderbolt firmware format
This is being used for other products, e.g. USB4 docks.

If non-Intel firmware is being used (e.g. ASMedia) then the explicit
calls to `fu_intel_thunderbolt_nvm_new()` can be changed to something
like `fu_firmware_new_from_gtypes()` with all the formats listed.

plugins/thunderbolt/fu-thunderbolt-controller.c
@@ -12,7 +12,6 @@
 
 #include "fu-thunderbolt-common.h"
 #include "fu-thunderbolt-controller.h"
-#include "fu-thunderbolt-firmware.h"
 
 typedef enum {
 	FU_THUNDERBOLT_CONTROLLER_KIND_DEVICE,
@@ -29,6 +28,10 @@ struct _FuThunderboltController {
 };
 
 G_DEFINE_TYPE(FuThunderboltController, fu_thunderbolt_controller, FU_TYPE_THUNDERBOLT_DEVICE)
+
+/* byte offsets in firmware image */
+#define FU_TBT_OFFSET_NATIVE 0x7B
+#define FU_TBT_CHUNK_SZ	     0x40
 
 static void
 fu_thunderbolt_controller_check_safe_mode(FuThunderboltController *self)
@@ -108,7 +111,7 @@ fu_thunderbolt_controller_read_status_block(FuThunderboltController *self, GErro
 	g_autoptr(GFile) nvmem = NULL;
 	g_autoptr(GBytes) controller_fw = NULL;
 	g_autoptr(GInputStream) istr = NULL;
-	g_autoptr(FuThunderboltFirmware) firmware = fu_thunderbolt_firmware_new();
+	g_autoptr(FuFirmware) firmware = NULL;
 
 	nvmem = fu_thunderbolt_device_find_nvmem(FU_THUNDERBOLT_DEVICE(self), TRUE, error);
 	if (nvmem == NULL)
@@ -122,12 +125,18 @@ fu_thunderbolt_controller_read_status_block(FuThunderboltController *self, GErro
 	controller_fw = g_input_stream_read_bytes(istr, nr_chunks * FU_TBT_CHUNK_SZ, NULL, error);
 	if (controller_fw == NULL)
 		return FALSE;
-	if (!fu_firmware_parse(FU_FIRMWARE(firmware),
-			       controller_fw,
-			       FWUPD_INSTALL_FLAG_NO_SEARCH,
-			       error))
+	firmware = fu_firmware_new_from_gtypes(controller_fw,
+					       FWUPD_INSTALL_FLAG_NO_SEARCH,
+					       error,
+					       FU_TYPE_INTEL_THUNDERBOLT_NVM,
+					       FU_TYPE_FIRMWARE,
+					       G_TYPE_INVALID);
+	if (firmware == NULL)
 		return FALSE;
-	self->is_native = fu_thunderbolt_firmware_is_native(firmware);
+	if (FU_IS_INTEL_THUNDERBOLT_FIRMWARE(firmware)) {
+		self->is_native =
+		    fu_intel_thunderbolt_nvm_is_native(FU_INTEL_THUNDERBOLT_NVM(firmware));
+	}
 	return TRUE;
 }
 

contrib/ci/oss-fuzz.py
@@ -352,6 +352,7 @@ def _build(bld: Builder) -> None:
         Fuzzer("fmap"),
         Fuzzer("ihex"),
         Fuzzer("srec"),
+        Fuzzer("intel-thunderbolt"),
         Fuzzer("ifwi-cpd"),
         Fuzzer("ifwi-fpt"),
         Fuzzer("oprom"),

libfwupdplugin/fu-intel-thunderbolt-firmware.c
@@ -1,0 +1,129 @@
+/*
+ * Copyright (C) 2021 Dell Inc.
+ * Copyright (C) 2020 Richard Hughes <richard@hughsie.com>
+ * Copyright (C) 2020 Mario Limonciello <mario.limonciello@dell.com>
+ * Copyright (C) 2017 Intel Corporation.
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#define G_LOG_DOMAIN "FuFirmware"
+
+#include "config.h"
+
+#include "fu-byte-array.h"
+#include "fu-intel-thunderbolt-firmware.h"
+#include "fu-mem.h"
+
+/**
+ * FuIntelThunderboltFirmware:
+ *
+ * The Non-Volatile-Memory file-format specification. This is what you would find as the update
+ * payload.
+ *
+ * See also: [class@FuFirmware]
+ */
+
+G_DEFINE_TYPE(FuIntelThunderboltFirmware,
+	      fu_intel_thunderbolt_firmware,
+	      FU_TYPE_INTEL_THUNDERBOLT_NVM)
+
+static gboolean
+fu_intel_thunderbolt_nvm_valid_farb_pointer(guint32 pointer)
+{
+	return pointer != 0 && pointer != 0xFFFFFF;
+}
+
+static gboolean
+fu_intel_thunderbolt_firmware_parse(FuFirmware *firmware,
+				    GBytes *fw,
+				    gsize offset,
+				    FwupdInstallFlags flags,
+				    GError **error)
+{
+	const guint32 farb_offsets[] = {0x0, 0x1000};
+	gboolean valid = FALSE;
+	guint32 farb_pointer = 0x0;
+
+	/* get header offset */
+	for (guint i = 0; i < G_N_ELEMENTS(farb_offsets); i++) {
+		if (!fu_memread_uint24_safe(g_bytes_get_data(fw, NULL),
+					    g_bytes_get_size(fw),
+					    offset + farb_offsets[i],
+					    &farb_pointer,
+					    G_LITTLE_ENDIAN,
+					    error))
+			return FALSE;
+		if (fu_intel_thunderbolt_nvm_valid_farb_pointer(farb_pointer)) {
+			valid = TRUE;
+			break;
+		}
+	}
+	if (!valid) {
+		g_set_error_literal(error,
+				    FWUPD_ERROR,
+				    FWUPD_ERROR_INVALID_FILE,
+				    "no valid farb pointer found");
+		return FALSE;
+	}
+	g_debug("detected digital section begins at 0x%x", farb_pointer);
+	fu_firmware_set_offset(firmware, farb_pointer);
+
+	/* FuIntelThunderboltNvm->parse */
+	return FU_FIRMWARE_CLASS(fu_intel_thunderbolt_firmware_parent_class)
+	    ->parse(firmware, fw, offset + farb_pointer, flags, error);
+}
+
+static GBytes *
+fu_intel_thunderbolt_firmware_write(FuFirmware *firmware, GError **error)
+{
+	g_autoptr(GByteArray) buf = g_byte_array_new();
+	g_autoptr(GBytes) blob = NULL;
+
+	/* sanity check */
+	if (fu_firmware_get_offset(firmware) < 0x08) {
+		g_set_error_literal(error,
+				    FWUPD_ERROR,
+				    FWUPD_ERROR_INVALID_FILE,
+				    "not valid offset");
+		return NULL;
+	}
+
+	/* offset */
+	fu_byte_array_append_uint32(buf, fu_firmware_get_offset(firmware), G_LITTLE_ENDIAN);
+	fu_byte_array_set_size(buf, fu_firmware_get_offset(firmware), 0x00);
+
+	/* FuIntelThunderboltNvm->write */
+	blob =
+	    FU_FIRMWARE_CLASS(fu_intel_thunderbolt_firmware_parent_class)->write(firmware, error);
+	fu_byte_array_append_bytes(buf, blob);
+
+	/* success */
+	return g_byte_array_free_to_bytes(g_steal_pointer(&buf));
+}
+
+static void
+fu_intel_thunderbolt_firmware_init(FuIntelThunderboltFirmware *self)
+{
+}
+
+static void
+fu_intel_thunderbolt_firmware_class_init(FuIntelThunderboltFirmwareClass *klass)
+{
+	FuFirmwareClass *klass_firmware = FU_FIRMWARE_CLASS(klass);
+	klass_firmware->parse = fu_intel_thunderbolt_firmware_parse;
+	klass_firmware->write = fu_intel_thunderbolt_firmware_write;
+}
+
+/**
+ * fu_intel_thunderbolt_firmware_new:
+ *
+ * Creates a new #FuFirmware of Intel NVM format
+ *
+ * Since: 1.8.5
+ **/
+FuFirmware *
+fu_intel_thunderbolt_firmware_new(void)
+{
+	return FU_FIRMWARE(g_object_new(FU_TYPE_INTEL_THUNDERBOLT_FIRMWARE, NULL));
+}

libfwupdplugin/fu-intel-thunderbolt-firmware.h
@@ -1,0 +1,26 @@
+/*
+ * Copyright (C) 2021 Dell Inc.
+ * Copyright (C) 2020 Richard Hughes <richard@hughsie.com>
+ * Copyright (C) 2020 Mario Limonciello <mario.limonciello@dell.com>
+ * Copyright (C) 2017 Intel Corporation.
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include "fu-intel-thunderbolt-nvm.h"
+
+#define FU_TYPE_INTEL_THUNDERBOLT_FIRMWARE (fu_intel_thunderbolt_firmware_get_type())
+G_DECLARE_DERIVABLE_TYPE(FuIntelThunderboltFirmware,
+			 fu_intel_thunderbolt_firmware,
+			 FU,
+			 INTEL_THUNDERBOLT_FIRMWARE,
+			 FuIntelThunderboltNvm)
+
+struct _FuIntelThunderboltFirmwareClass {
+	FuIntelThunderboltNvmClass parent_class;
+};
+
+FuFirmware *
+fu_intel_thunderbolt_firmware_new(void);

libfwupdplugin/fu-intel-thunderbolt-nvm.h
@@ -1,0 +1,41 @@
+/*
+ * Copyright (C) 2021 Dell Inc.
+ * Copyright (C) 2020 Richard Hughes <richard@hughsie.com>
+ * Copyright (C) 2020 Mario Limonciello <mario.limonciello@dell.com>
+ * Copyright (C) 2017 Intel Corporation.
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include "fu-firmware.h"
+
+#define FU_TYPE_INTEL_THUNDERBOLT_NVM (fu_intel_thunderbolt_nvm_get_type())
+G_DECLARE_DERIVABLE_TYPE(FuIntelThunderboltNvm,
+			 fu_intel_thunderbolt_nvm,
+			 FU,
+			 INTEL_THUNDERBOLT_NVM,
+			 FuFirmware)
+
+struct _FuIntelThunderboltNvmClass {
+	FuFirmwareClass parent_class;
+};
+
+guint16
+fu_intel_thunderbolt_nvm_get_vendor_id(FuIntelThunderboltNvm *self);
+guint16
+fu_intel_thunderbolt_nvm_get_device_id(FuIntelThunderboltNvm *self);
+guint16
+fu_intel_thunderbolt_nvm_get_model_id(FuIntelThunderboltNvm *self);
+gboolean
+fu_intel_thunderbolt_nvm_is_host(FuIntelThunderboltNvm *self);
+gboolean
+fu_intel_thunderbolt_nvm_is_native(FuIntelThunderboltNvm *self);
+gboolean
+fu_intel_thunderbolt_nvm_has_pd(FuIntelThunderboltNvm *self);
+guint8
+fu_intel_thunderbolt_nvm_get_flash_size(FuIntelThunderboltNvm *self);
+
+FuFirmware *
+fu_intel_thunderbolt_nvm_new(void);

libfwupdplugin/fu-self-test.c
@@ -3249,6 +3249,9 @@ fu_firmware_builder_round_trip_func(void)
 	    {FU_TYPE_OPROM_FIRMWARE,
 	     "oprom.builder.xml",
 	     "2aae6c35c94fcfb415dbe95f408b9ce91ee846ed"},
+	    {FU_TYPE_INTEL_THUNDERBOLT_NVM,
+	     "intel-thunderbolt.builder.xml",
+	     "e858000646fecb5223b41df57647c005b495749b"},
 #ifdef HAVE_CBOR
 	    {FU_TYPE_USWID_FIRMWARE,
 	     "uswid.builder.xml",

libfwupdplugin/fwupdplugin.h
@@ -52,6 +52,8 @@
 #include <libfwupdplugin/fu-ifwi-cpd-firmware.h>
 #include <libfwupdplugin/fu-ifwi-fpt-firmware.h>
 #include <libfwupdplugin/fu-ihex-firmware.h>
+#include <libfwupdplugin/fu-intel-thunderbolt-firmware.h>
+#include <libfwupdplugin/fu-intel-thunderbolt-nvm.h>
 #include <libfwupdplugin/fu-io-channel.h>
 #include <libfwupdplugin/fu-kernel.h>
 #include <libfwupdplugin/fu-linear-firmware.h>

commit d31e15690a47325a40ed2023c0b3676bf801fd7c

thunderbolt: Correctly obtain the is_native controller attribute
This regressed in 16b463334e.

Fixes https://github.com/fwupd/fwupd/issues/5975

plugins/thunderbolt/fu-thunderbolt-controller.c
@@ -130,7 +130,7 @@ fu_thunderbolt_controller_read_status_block(FuThunderboltController *self, GErro
 					       G_TYPE_INVALID);
 	if (firmware == NULL)
 		return FALSE;
-	if (FU_IS_INTEL_THUNDERBOLT_FIRMWARE(firmware)) {
+	if (FU_IS_INTEL_THUNDERBOLT_NVM(firmware)) {
 		self->is_native =
 		    fu_intel_thunderbolt_nvm_is_native(FU_INTEL_THUNDERBOLT_NVM(firmware));
 	}