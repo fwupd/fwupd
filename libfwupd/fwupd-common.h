ca7adbfa81d3c5cf9f93edd678e7daed58a11b1a
Richard Hughes <richard@hughsie.com>
2023-07-12 12:28:41 +0100

    trivial: Include fwupd_guid_t in the libfwupd introspection

--- libfwupd/fwupd-common.h
+++ libfwupd/fwupd-common.h
@@ -59,7 +59,9 @@ typedef enum {
 } FwupdGuidFlags;
 
 /* GObject Introspection does not understand typedefs with sizes */
-#ifndef __GI_SCANNER__
+#ifdef __GI_SCANNER__
+typedef guint8 *fwupd_guid_t;
+#else
 typedef guint8 fwupd_guid_t[16];
 #endif
 
