9d46f11d6c2e9d6b07fd9d10907a8b169ecc85ea
Richard Hughes <richard@hughsie.com>
2023-07-12 12:31:07 +0100

    Allow rustgen-provided types to be used by plugins

--- libfwupdplugin/fu-efi.rs
+++ libfwupdplugin/fu-efi.rs
@@ -1,6 +1,13 @@
 // Copyright (C) 2023 Richard Hughes <richard@hughsie.com>
 // SPDX-License-Identifier: LGPL-2.1+
 
+#[derive(ToString)]
+enum EfiSignatureKind {
+    Unknown,
+    Sha256,
+    X509,
+}
+
 #[repr(u8)]
 enum EfiFileAttrib {
     None = 0x00,
