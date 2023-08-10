fwupd/fwupd

commit 946cfd63ac27fd2d27052801b27f7de8d7c3e43a

trivial: synaptics-mst: make self tests not write
The self tests shouldn't be writing the source directory, make sure
they open up files in read-only mode.

plugins/synaptics-mst/fu-synaptics-mst-device.c
@@ -79,15 +79,17 @@ fu_synaptics_mst_device_finalize(GObject *object)
 static void
 fu_synaptics_mst_device_init(FuSynapticsMstDevice *self)
 {
+	FuUdevDeviceFlags flags =
+	    FU_UDEV_DEVICE_FLAG_OPEN_READ | FU_UDEV_DEVICE_FLAG_VENDOR_FROM_PARENT;
+	if (fu_udev_device_get_dev(FU_UDEV_DEVICE(self)) != NULL)
+		flags |= FU_UDEV_DEVICE_FLAG_OPEN_WRITE;
+	fu_udev_device_set_flags(FU_UDEV_DEVICE(self), flags);
 	fu_device_add_protocol(FU_DEVICE(self), "com.synaptics.mst");
 	fu_device_set_vendor(FU_DEVICE(self), "Synaptics");
 	fu_device_add_vendor_id(FU_DEVICE(self), "DRM_DP_AUX_DEV:0x06CB");
 	fu_device_set_summary(FU_DEVICE(self), "Multi-stream transport device");
 	fu_device_add_icon(FU_DEVICE(self), "video-display");
 	fu_device_set_version_format(FU_DEVICE(self), FWUPD_VERSION_FORMAT_TRIPLET);
-	fu_udev_device_set_flags(FU_UDEV_DEVICE(self),
-				 FU_UDEV_DEVICE_FLAG_OPEN_READ | FU_UDEV_DEVICE_FLAG_OPEN_WRITE |
-				     FU_UDEV_DEVICE_FLAG_VENDOR_FROM_PARENT);
 	fu_device_register_private_flag(FU_DEVICE(self),
 					FU_SYNAPTICS_MST_DEVICE_FLAG_IGNORE_BOARD_ID,
 					"ignore-board-id");

commit 491c7c1018b56ca2f654aca5a88b1cc270d2651f

synaptics-mst: Enumerate devices correctly
This fixes a regression from 946cfd63 which broke enumerating devices. The
GUdevDevice is only set from ->incorporate() and so checking in _init() is much
too early.

plugins/synaptics-mst/fu-synaptics-mst-device.c
@@ -80,13 +80,26 @@ fu_synaptics_mst_device_finalize(GObject *object)
 }
 
 static void
+fu_synaptics_mst_device_udev_device_notify_cb(FuUdevDevice *udev_device,
+					      GParamSpec *pspec,
+					      gpointer user_data)
+{
+	FuSynapticsMstDevice *self = FU_SYNAPTICS_MST_DEVICE(user_data);
+	if (fu_udev_device_get_dev(FU_UDEV_DEVICE(self)) != NULL) {
+		fu_udev_device_set_flags(FU_UDEV_DEVICE(self),
+					 FU_UDEV_DEVICE_FLAG_OPEN_READ |
+					     FU_UDEV_DEVICE_FLAG_OPEN_WRITE |
+					     FU_UDEV_DEVICE_FLAG_VENDOR_FROM_PARENT);
+	} else {
+		fu_udev_device_set_flags(FU_UDEV_DEVICE(self),
+					 FU_UDEV_DEVICE_FLAG_OPEN_READ |
+					     FU_UDEV_DEVICE_FLAG_VENDOR_FROM_PARENT);
+	}
+}
+
+static void
 fu_synaptics_mst_device_init(FuSynapticsMstDevice *self)
 {
-	FuUdevDeviceFlags flags =
-	    FU_UDEV_DEVICE_FLAG_OPEN_READ | FU_UDEV_DEVICE_FLAG_VENDOR_FROM_PARENT;
-	if (fu_udev_device_get_dev(FU_UDEV_DEVICE(self)) != NULL)
-		flags |= FU_UDEV_DEVICE_FLAG_OPEN_WRITE;
-	fu_udev_device_set_flags(FU_UDEV_DEVICE(self), flags);
 	fu_device_add_protocol(FU_DEVICE(self), "com.synaptics.mst");
 	fu_device_set_vendor(FU_DEVICE(self), "Synaptics");
 	fu_device_add_vendor_id(FU_DEVICE(self), "DRM_DP_AUX_DEV:0x06CB");
@@ -98,6 +111,12 @@ fu_synaptics_mst_device_init(FuSynapticsMstDevice *self)
 					"ignore-board-id");
 	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_UPDATABLE);
 	fu_device_add_internal_flag(FU_DEVICE(self), FU_DEVICE_INTERNAL_FLAG_NO_PROBE_COMPLETE);
+
+	/* this is set from ->incorporate() */
+	g_signal_connect(FU_UDEV_DEVICE(self),
+			 "notify::udev-device",
+			 G_CALLBACK(fu_synaptics_mst_device_udev_device_notify_cb),
+			 self);
 }
 
 static void