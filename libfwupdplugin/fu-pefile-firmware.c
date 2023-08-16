6dfef7ea3cbd077f2fee9cfcfb27ea0b8cc9c7b4
Valentin David <valentin.david@canonical.com>
2023-08-16 12:28:30 +0200

    fu-pefile-firmware: Load effective size of section instead of raw size

--- libfwupdplugin/fu-pefile-firmware.c
+++ libfwupdplugin/fu-pefile-firmware.c
@@ -102,7 +102,7 @@ fu_pefile_firmware_parse_section(FuFirmware *firmware,
 	fu_firmware_set_offset(img, sect_offset);
 	blob = fu_bytes_new_offset(fw,
 				   sect_offset,
-				   fu_struct_pe_coff_section_get_size_of_raw_data(st),
+				   fu_struct_pe_coff_section_get_virtual_size(st),
 				   error);
 	if (blob == NULL) {
 		g_prefix_error(error, "failed to get raw data for %s: ", sect_id);
