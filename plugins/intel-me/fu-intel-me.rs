eb6c2e23ae2e0d9c8a9a8889fbcb039e0ca932e8
Richard Hughes <richard@hughsie.com>
2023-08-17 20:32:07 +0100

    intel-me: Only query one set of FILE_IDs
    According to Intel sections 0x4 and 0x8 are only used for development testing.
    Using 0x0 is sufficient to get the value from either the FPF *or* NVRAM.

--- plugins/intel-me/fu-intel-me.rs
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2023 Richard Hughes <richard@hughsie.com>
-// SPDX-License-Identifier: LGPL-2.1+
-
-#[derive(ToString)]
-enum IntelMeMcaSection {
-    Me = 0x00,
-    Uep = 0x04,
-    Fpf = 0x08,
-}
