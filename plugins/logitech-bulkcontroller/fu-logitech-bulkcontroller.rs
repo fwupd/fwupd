76a8898456a36323ca4ebc64edc2e9cf1285fac0
Richard Hughes <richard@hughsie.com>
2023-09-15 16:20:41 +0100

    logitech-bulkcontroller: Do less in the hot path to speed up firmware updates
    fu_logitech_bulkcontroller_device_send_upd_cmd() is called over ~200,000 times
    and so it makes sense to do as little as possible.

--- plugins/logitech-bulkcontroller/fu-logitech-bulkcontroller.rs
+++ plugins/logitech-bulkcontroller/fu-logitech-bulkcontroller.rs
@@ -24,3 +24,46 @@ enum LogitechBulkcontrollerUpdateState {
     Scheduled,
     Error,
 }
+
+#[repr(u32le)]
+#[derive(ToString)]
+enum LogitechBulkcontrollerCmd {
+    CheckBuffersize = 0xCC00,
+    Init = 0xCC01,
+    StartTransfer = 0xCC02,
+    DataTransfer = 0xCC03,
+    EndTransfer = 0xCC04,
+    Uninit = 0xCC05,
+    BufferRead = 0xCC06,
+    BufferWrite = 0xCC07,
+    UninitBuffer = 0xCC08,
+    Ack = 0xFF01,
+    Timeout = 0xFF02,
+    Nack = 0xFF03,
+}
+
+#[derive(New)]
+struct LogitechBulkcontrollerSendSyncReq {
+    cmd: LogitechBulkcontrollerCmd,
+    payload_length: u32le,
+    sequence_id: u32le,
+}
+
+#[derive(New)]
+struct LogitechBulkcontrollerUpdateReq {
+    cmd: LogitechBulkcontrollerCmd,
+    payload_length: u32le,
+}
+
+#[derive(Getters)]
+struct LogitechBulkcontrollerUpdateRes {
+    cmd: LogitechBulkcontrollerCmd,
+    _payload_length: u32le,
+    cmd_req: LogitechBulkcontrollerCmd,
+}
+
+enum LogitechBulkcontrollerChecksumType {
+    Sha256,
+    Sha512,
+    Md5,
+}
