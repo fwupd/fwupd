cc1c05464e582a435e0739c675aa3ce4912694a2
Richard Hughes <richard@hughsie.com>
2023-09-18 13:40:29 +0100

    redfish: Convert more of the SMBIOS type 42 table to rustgen
    This ended up being a bug in the table itself, but the new code is easier to read.

--- plugins/redfish/fu-redfish.rs
+++ plugins/redfish/fu-redfish.rs
@@ -17,6 +17,45 @@ struct RedfishProtocolOverIp {
     service_hostname_len: u8,
 // optional service_hostname goes here
 }
+
+#[derive(ToString)]
+enum RedfishInterfaceType {
+    UsbNetwork = 0x02,
+    PciNetwork = 0x03,
+    UsbNetworkV2 = 0x04,
+    PciNetworkV2 = 0x05,
+}
+
+enum RedfishIpAssignmentType {
+    Static = 0x00,
+    Dhcp = 0x02,
+    AutoConfig = 0x03,
+    HostSelect = 0x04,
+}
+
+enum RedfishIpAddressFormat {
+    Unknown = 0x00,
+    V4 = 0x01,
+    V6 = 0x02,
+}
+
+#[repr(u8)]
+enum RedfishControllerInterfaceType {
+    NetworkHost = 0x40,
+}
+
+#[derive(Parse)]
+struct RedfishSmbiosType42 {
+    type: u8 == 42,
+    length: u8,
+    handle: u16le,
+    interface_type: RedfishControllerInterfaceType == NetworkHost,
+    data_length: u8,
+    // data: [u8; data_length],
+    // protocol_cnt: u8,
+    // protocol_records
+}
+
 #[derive(ToString)]
 enum RedfishNetworkDeviceState {
     Unknown = 0,
