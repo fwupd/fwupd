ff37f8b01cb19de7cd70a52256e2758fa8ea916c
Emily Miller <m_emily@berkeley.edu>
2023-09-26 16:26:19 +0800

    vli: add pd tags, defines, minor fujitsu fix

--- plugins/vli/fu-vli-pd-common.c
+++ plugins/vli/fu-vli-pd-common.c
@@ -14,25 +14,29 @@ FuVliDeviceKind
 fu_vli_pd_common_guess_device_kind(guint32 fwver)
 {
 	guint32 tmp = (fwver & 0xFF000000) >> 24;
-	if (tmp == 0x01 || tmp == 0x02 || tmp == 0x03)
+	if (tmp < 0xA0)
+		tmp = tmp & 0x0F;
+	if (tmp == FU_VLI_DEVICE_FW_TAG_VL100A || tmp == FU_VLI_DEVICE_FW_TAG_VL100B ||
+	    tmp == FU_VLI_DEVICE_FW_TAG_VL100C)
 		return FU_VLI_DEVICE_KIND_VL100;
-	if (tmp == 0x04 || tmp == 0x05 || tmp == 0x06)
+	if (tmp == FU_VLI_DEVICE_FW_TAG_VL101A || tmp == FU_VLI_DEVICE_FW_TAG_VL101B ||
+	    tmp == FU_VLI_DEVICE_FW_TAG_VL101C)
 		return FU_VLI_DEVICE_KIND_VL101;
-	if (tmp == 0x07 || tmp == 0x08)
+	if (tmp == FU_VLI_DEVICE_FW_TAG_VL102A || tmp == FU_VLI_DEVICE_FW_TAG_VL102B)
 		return FU_VLI_DEVICE_KIND_VL102;
-	if (tmp == 0x09 || tmp == 0x0A)
+	if (tmp == FU_VLI_DEVICE_FW_TAG_VL103A || tmp == FU_VLI_DEVICE_FW_TAG_VL103B)
 		return FU_VLI_DEVICE_KIND_VL103;
-	if (tmp == 0x0B)
+	if (tmp == FU_VLI_DEVICE_FW_TAG_VL104)
 		return FU_VLI_DEVICE_KIND_VL104;
-	if (tmp == 0x0C)
+	if (tmp == FU_VLI_DEVICE_FW_TAG_VL105)
 		return FU_VLI_DEVICE_KIND_VL105;
-	if (tmp == 0x0D)
+	if (tmp == FU_VLI_DEVICE_FW_TAG_VL106)
 		return FU_VLI_DEVICE_KIND_VL106;
-	if (tmp == 0x0E)
+	if (tmp == FU_VLI_DEVICE_FW_TAG_VL107)
 		return FU_VLI_DEVICE_KIND_VL107;
-	if (tmp == 0xA1 || tmp == 0xB1)
+	if (tmp == FU_VLI_DEVICE_FW_TAG_VL108A || tmp == FU_VLI_DEVICE_FW_TAG_VL108B)
 		return FU_VLI_DEVICE_KIND_VL108;
-	if (tmp == 0xA2 || tmp == 0xB2)
+	if (tmp == FU_VLI_DEVICE_FW_TAG_VL109A || tmp == FU_VLI_DEVICE_FW_TAG_VL109B)
 		return FU_VLI_DEVICE_KIND_VL109;
 	return FU_VLI_DEVICE_KIND_UNKNOWN;
 }
