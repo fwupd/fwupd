c0e8fa97b370f808960218ab9b86209b73a6313e
Richard Hughes <richard@hughsie.com>
2023-11-01 13:58:00 +0000

    limit the number of HID descriptors to be smaller when fuzzing
    Fixes https://oss-fuzz.com/testcase-detail/6263577335693312

--- libfwupdplugin/fu-hid-descriptor.c
+++ libfwupdplugin/fu-hid-descriptor.c
@@ -281,7 +281,8 @@ static void
 fu_hid_descriptor_init(FuHidDescriptor *self)
 {
 	fu_firmware_add_flag(FU_FIRMWARE(self), FU_FIRMWARE_FLAG_NO_AUTO_DETECTION);
-	fu_firmware_set_images_max(FU_FIRMWARE(self), 1024);
+	fu_firmware_set_images_max(FU_FIRMWARE(self),
+				   g_getenv("FWUPD_FUZZER_RUNNING") != NULL ? 10 : 1024);
 	g_type_ensure(FU_TYPE_HID_REPORT);
 	g_type_ensure(FU_TYPE_HID_REPORT_ITEM);
 }
