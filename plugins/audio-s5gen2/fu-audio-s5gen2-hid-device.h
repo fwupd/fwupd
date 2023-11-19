8f99d58e1ebf0477f1099e13cc093f16c97c2c1a
Denis Pynkin <denis.pynkin@collabora.com>
2023-11-19 20:57:34 +0100

    audio-s5gen2: plugin for updating Qualcomm audio devices
    Firmware Update Plug-in for Qualcomm Voice & Music Series 5 Gen 1 and
    Gen 2, and Series 3 Gen 1, Gen 2 and Gen 3.
    
    The initial variant of the plugin allows to update devices based
    on Qualcomm 5 series via USB HID. Tested with development board only.
    
    Co-authored-by: Richard Hughes <richard@hughsie.com>

---
+++ plugins/audio-s5gen2/fu-audio-s5gen2-hid-device.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2023 Denis Pynkin <denis.pynkin@collabora.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#pragma once
+
+#include <fwupdplugin.h>
+
+#define FU_TYPE_QC_S5GEN2_HID_DEVICE (fu_qc_s5gen2_hid_device_get_type())
+G_DECLARE_FINAL_TYPE(FuQcS5gen2HidDevice,
+		     fu_qc_s5gen2_hid_device,
+		     FU,
+		     QC_S5GEN2_HID_DEVICE,
+		     FuHidDevice)
