f45c9c09114430fcc343802b6a19b9b0b9aacc7b
Richard Hughes <richard@hughsie.com>
2023-11-21 19:57:33 +0000

    synaptics-cxaudio: Move more of the defines to rust format
    Text output is way more helpful when debugging.

--- plugins/synaptics-cxaudio/fu-synaptics-cxaudio.rs
+++ plugins/synaptics-cxaudio/fu-synaptics-cxaudio.rs
@@ -1,6 +1,38 @@
 // Copyright (C) 2023 Richard Hughes <richard@hughsie.com>
 // SPDX-License-Identifier: LGPL-2.1+
 
+#[derive(ToString)]
+enum SynapticsCxaudioDeviceKind {
+    Unknown,
+    Cx20562 = 20562,
+    Cx2070x = 20700,
+    Cx2077x = 20770,
+    Cx2076x = 20760,
+    Cx2085x = 20850,
+    Cx2089x = 20890,
+    Cx2098x = 20980,
+    Cx2198x = 21980,
+}
+
+enum SynapticsCxaudioMemKind {
+    Eeprom,
+    CpxRam,
+    CpxRom,
+}
+
+#[derive(ToString)]
+enum SynapticsCxaudioFileKind {
+    Unknown,
+    Cx2070xFw,
+    Cx2070xPatch,
+    Cx2077xPatch,
+    Cx2076xPatch,
+    Cx2085xPatch,
+    Cx2089xPatch,
+    Cx2098xPatch,
+    Cx2198xPatch,
+}
+
 #[derive(Parse)]
 struct SynapticsCxaudioCustomInfo {
     patch_version_string_address: u16le,
