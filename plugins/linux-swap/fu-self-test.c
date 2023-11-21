7ec2053a79a0df926657641ab3bf30cad77d5598
Richard Hughes <richard@hughsie.com>
2023-11-21 09:41:47 +0000

    trivial: Handle G_DBUS_ERROR_NAME_HAS_NO_OWNER in the self tests
    Fixes some of https://github.com/fwupd/fwupd/issues/6418

--- plugins/linux-swap/fu-self-test.c
+++ plugins/linux-swap/fu-self-test.c
@@ -32,18 +32,13 @@ fu_linux_swap_plain_func(void)
 			      "/dev/nvme0n1p4                          partition\t5962748\t0\t-2\n",
 			      0,
 			      &error);
-	if (g_error_matches(error, G_IO_ERROR, G_IO_ERROR_NOT_FOUND) ||
-	    g_error_matches(error, G_IO_ERROR, G_IO_ERROR_NOT_DIRECTORY) ||
+	if (g_error_matches(error, G_DBUS_ERROR, G_DBUS_ERROR_NAME_HAS_NO_OWNER) ||
+	    g_error_matches(error, G_DBUS_ERROR, G_DBUS_ERROR_SERVICE_UNKNOWN) ||
 	    g_error_matches(error, G_DBUS_ERROR, G_DBUS_ERROR_SPAWN_EXEC_FAILED) ||
-	    g_error_matches(error, G_IO_ERROR, G_IO_ERROR_INVALID_ARGUMENT)) {
-		g_test_skip(error->message);
-		return;
-	}
-	if (g_error_matches(error, G_DBUS_ERROR, G_DBUS_ERROR_SERVICE_UNKNOWN)) {
-		g_test_skip(error->message);
-		return;
-	}
-	if (g_error_matches(error, G_IO_ERROR, G_IO_ERROR_CONNECTION_REFUSED)) {
+	    g_error_matches(error, G_IO_ERROR, G_IO_ERROR_CONNECTION_REFUSED) ||
+	    g_error_matches(error, G_IO_ERROR, G_IO_ERROR_INVALID_ARGUMENT) ||
+	    g_error_matches(error, G_IO_ERROR, G_IO_ERROR_NOT_DIRECTORY) ||
+	    g_error_matches(error, G_IO_ERROR, G_IO_ERROR_NOT_FOUND)) {
 		g_test_skip(error->message);
 		return;
 	}
