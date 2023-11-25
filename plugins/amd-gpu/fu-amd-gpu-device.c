fwupd/fwupd

commit 5bb7ab86954e08b6e3e7cfa9d92c2070f739742f

trivial: amd-gpu: fix a casting error on 32 bit x86

plugins/amd-gpu/fu-amd-gpu-device.c
@@ -111,7 +111,7 @@ fu_amd_gpu_device_setup(FuDevice *device, GError **error)
 	struct drm_amdgpu_info_vbios vbios_info;
 	struct drm_amdgpu_info request = {
 	    .query = AMDGPU_INFO_VBIOS,
-	    .return_pointer = (__u64)&vbios_info,
+	    .return_pointer = GPOINTER_TO_UINT(&vbios_info),
 	    .return_size = sizeof(struct drm_amdgpu_info_vbios),
 	    .vbios_info.type = AMDGPU_INFO_VBIOS_INFO,
 	};

commit d446b3fb12aa284e7d91fef0519a2ca085a11da1

amd-gpu: Fix probing mistake
While trying to fix a casting error, I accidentally broke 64 bit return
addresses entirely.

Fixes: 5bb7ab869 ("trivial: amd-gpu: fix a casting error on 32 bit x86")

plugins/amd-gpu/fu-amd-gpu-device.c
@@ -111,7 +111,7 @@ fu_amd_gpu_device_setup(FuDevice *device, GError **error)
 	struct drm_amdgpu_info_vbios vbios_info;
 	struct drm_amdgpu_info request = {
 	    .query = AMDGPU_INFO_VBIOS,
-	    .return_pointer = GPOINTER_TO_UINT(&vbios_info),
+	    .return_pointer = GPOINTER_TO_SIZE(&vbios_info),
 	    .return_size = sizeof(struct drm_amdgpu_info_vbios),
 	    .vbios_info.type = AMDGPU_INFO_VBIOS_INFO,
 	};