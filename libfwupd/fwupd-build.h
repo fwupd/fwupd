fbafb3dc3c783e80ace9ea392a8dd4abe8210113
Richard Hughes <richard@hughsie.com>
2023-12-01 21:12:41 +0000

    trivial: Fix oss-fuzz CI

--- libfwupd/fwupd-build.h
+++ libfwupd/fwupd-build.h
@@ -8,10 +8,6 @@
 
 #include <glib.h>
 
-#if !defined(__FWUPD_H_INSIDE__) && !defined(FWUPD_COMPILATION)
-#error "Only <fwupd.h> can be included directly."
-#endif
-
 /* see https://bugzilla.gnome.org/show_bug.cgi?id=113075 */
 #ifndef G_GNUC_NON_NULL
 #if !defined(_WIN32) && (__GNUC__ > 3) || (__GNUC__ == 3 && __GNUC_MINOR__ >= 3)
