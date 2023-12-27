363e9545d787a67896f322679e229a491d816912
Mario Limonciello <superm1@gmail.com>
2023-12-27 13:28:42 -0600

    trivial: keep udev object around for PSP and MEI devices
    In order to populate security attributes it will be required to still
    exist.
    
    Fixes: https://github.com/fwupd/fwupd/issues/6576

--- libfwupdplugin/fu-mei-device.c
+++ libfwupdplugin/fu-mei-device.c
@@ -497,6 +497,7 @@ static void
 fu_mei_device_init(FuMeiDevice *self)
 {
 	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_INTERNAL);
+	fu_device_add_internal_flag(FU_DEVICE(self), FU_DEVICE_INTERNAL_FLAG_NO_PROBE_COMPLETE);
 	fu_udev_device_set_flags(FU_UDEV_DEVICE(self),
 				 FU_UDEV_DEVICE_FLAG_OPEN_READ | FU_UDEV_DEVICE_FLAG_OPEN_WRITE |
 				     FU_UDEV_DEVICE_FLAG_VENDOR_FROM_PARENT);
