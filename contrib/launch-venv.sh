14ba64c1caf52dbaf9ee4a68f0c4c09e5f5f62b7
Mario Limonciello <superm1@gmail.com>
2024-01-03 21:35:02 -0600

    Use virtualenv for development
    This moves the wrappers around to encourage the use of `contrib/setup`
    for development instead.
    
    It also makes the existing gdbserver wrappers work by default and
    users won't need to install into /usr/local anymore.

--- contrib/launch-venv.sh
+++ contrib/launch-venv.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/sh -e
 gcc=$(gcc -dumpmachine)
-DIST="$(dirname $0)"
+DIST="$(dirname $0)/../dist"
 BIN="$(basename $0)"
 export FWUPD_LOCALSTATEDIR=${DIST}
 export FWUPD_SYSCONFDIR=${DIST}/etc
@@ -17,4 +17,16 @@ if [ -f ${DIST}/libexec/fwupd/${BIN} ]; then
 else
         EXE=${DIST}/bin/${BIN}
 fi
-${DEBUG} ${EXE} "$@"
+if [ ! -f ${EXE} ]; then
+        echo "Not yet built! Please run:"
+        echo ""
+        echo "# build-fwupd"
+        exit 1
+fi
+if [ "${BIN}" != fwupdgmr ]; then
+        SUDO="$(which sudo) \
+              LD_LIBRARY_PATH=${LD_LIBRARY_PATH} \
+              FWUPD_LOCALSTATEDIR=${FWUPD_LOCALSTATEDIR} \
+              FWUPD_SYSCONFDIR=${FWUPD_SYSCONFDIR}"
+fi
+${SUDO} ${DEBUG} ${EXE} "$@"
