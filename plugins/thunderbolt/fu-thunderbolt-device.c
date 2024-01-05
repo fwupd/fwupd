c131446acfedbe8bc9c377363ffe8f65c223faf5
Crag Wang <crag.wang@dell.com>
2024-01-05 14:36:14 +0800

    bugfix: keep udev object data for thunderbolt device
    Fixes: https://github.com/fwupd/fwupd/issues/6615
    (cherry picked from commit 41bb24b800e12c38c4b92cfe60133ffd9dacd86d)

--- plugins/thunderbolt/fu-thunderbolt-device.c
+++ plugins/thunderbolt/fu-thunderbolt-device.c
@@ -428,6 +428,7 @@ fu_thunderbolt_device_init(FuThunderboltDevice *self)
 	priv->auth_method = "nvm_authenticate";
 	fu_device_add_icon(FU_DEVICE(self), "thunderbolt");
 	fu_device_add_protocol(FU_DEVICE(self), "com.intel.thunderbolt");
+	fu_device_add_internal_flag(FU_DEVICE(self), FU_DEVICE_INTERNAL_FLAG_NO_PROBE_COMPLETE);
 	fu_device_set_version_format(FU_DEVICE(self), FWUPD_VERSION_FORMAT_PAIR);
 }
 
