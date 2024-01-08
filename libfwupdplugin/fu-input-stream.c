fwupd/fwupd

commit 62f6f19d269212871493ba8343bf0d53204c7fb7

Reduce memory usage when loading large firmware
Some multi-GB firmware is impractical to load in memory and should instead be
streamed from disk or the device node.

plugins/vli/fu-vli-pd-firmware.c
@@ -57,34 +57,45 @@ fu_vli_pd_firmware_export(FuFirmware *firmware, FuFirmwareExportFlags flags, XbB
 }
 
 static gboolean
+fu_vli_pd_firmware_compute_crc16_cb(const guint8 *buf,
+				    gsize bufsz,
+				    gpointer user_data,
+				    GError **error)
+{
+	guint16 *value = (guint16 *)user_data;
+	*value = fu_crc16_full(buf, bufsz, *value, 0xA001);
+	return TRUE;
+}
+
+static gboolean
 fu_vli_pd_firmware_parse(FuFirmware *firmware,
-			 GBytes *fw,
+			 GInputStream *stream,
 			 gsize offset,
 			 FwupdInstallFlags flags,
 			 GError **error)
 {
 	FuVliPdFirmware *self = FU_VLI_PD_FIRMWARE(firmware);
-	gsize bufsz = 0;
+	gsize streamsz = 0;
 	guint32 fwver;
-	const guint8 *buf = g_bytes_get_data(fw, &bufsz);
 	g_autofree gchar *fwver_str = NULL;
 	g_autoptr(GByteArray) st = NULL;
 
 	/* parse */
-	st = fu_struct_vli_pd_hdr_parse(buf, bufsz, VLI_USBHUB_PD_FLASHMAP_ADDR, error);
+	st = fu_struct_vli_pd_hdr_parse_stream(stream, VLI_USBHUB_PD_FLASHMAP_ADDR, error);
 	if (st == NULL) {
 		g_prefix_error(error, "failed to read header: ");
 		return FALSE;
 	}
 	self->vid = fu_struct_vli_pd_hdr_get_vid(st);
+	if (!fu_input_stream_size(stream, &streamsz, error))
+		return FALSE;
 
 	/* fall back to legacy location */
 	if (!fu_vli_pd_firmware_validate_header(self)) {
 		g_byte_array_unref(st);
-		st = fu_struct_vli_pd_hdr_parse(buf,
-						bufsz,
-						VLI_USBHUB_PD_FLASHMAP_ADDR_LEGACY,
-						error);
+		st = fu_struct_vli_pd_hdr_parse_stream(stream,
+						       VLI_USBHUB_PD_FLASHMAP_ADDR_LEGACY,
+						       error);
 		if (st == NULL) {
 			g_prefix_error(error, "failed to read header: ");
 			return FALSE;
@@ -117,30 +128,36 @@ fu_vli_pd_firmware_parse(FuFirmware *firmware,
 	fu_firmware_set_version_raw(firmware, fwver);
 
 	/* check size */
-	if (bufsz != fu_vli_common_device_kind_get_size(self->device_kind)) {
+	if (streamsz != fu_vli_common_device_kind_get_size(self->device_kind)) {
 		g_set_error(error,
 			    FWUPD_ERROR,
 			    FWUPD_ERROR_INVALID_FILE,
 			    "size invalid, got 0x%x expected 0x%x",
-			    (guint)bufsz,
+			    (guint)streamsz,
 			    fu_vli_common_device_kind_get_size(self->device_kind));
 		return FALSE;
 	}
 
 	/* check CRC */
 	if ((flags & FWUPD_INSTALL_FLAG_IGNORE_CHECKSUM) == 0) {
-		guint16 crc_actual;
+		guint16 crc_actual = 0xFFFF;
 		guint16 crc_file = 0x0;
-		if (!fu_memread_uint16_safe(buf,
-					    bufsz,
-					    bufsz - 2,
-					    &crc_file,
-					    G_LITTLE_ENDIAN,
-					    error)) {
+		g_autoptr(GInputStream) stream_tmp = NULL;
+
+		if (!fu_input_stream_read_u16(stream,
+					      streamsz - 2,
+					      &crc_file,
+					      G_LITTLE_ENDIAN,
+					      error)) {
 			g_prefix_error(error, "failed to read file CRC: ");
 			return FALSE;
 		}
-		crc_actual = fu_crc16(buf, bufsz - 2);
+		stream_tmp = fu_partial_input_stream_new(stream, 0, streamsz);
+		if (!fu_input_stream_chunkify(stream_tmp,
+					      fu_vli_pd_firmware_compute_crc16_cb,
+					      &crc_actual,
+					      error))
+			return FALSE;
 		if (crc_actual != crc_file) {
 			g_set_error(error,
 				    FWUPD_ERROR,

commit 5ef9056460a532efef90c0f38af734bf29d5e37f

vli: Fix a regression when calculating the PD firmware CRC
This regressed in 62f6f19d.

Found by Comic Cho, many thanks.

libfwupdplugin/fu-input-stream.c
@@ -479,12 +479,12 @@ fu_input_stream_compute_sum16(GInputStream *stream, guint16 *value, GError **err
 typedef struct {
 	guint32 crc;
 	guint32 polynomial;
-} FuInputStreamComputeCrcHelper;
+} FuInputStreamComputeCrc32Helper;
 
 static gboolean
 fu_input_stream_compute_crc32_cb(const guint8 *buf, gsize bufsz, gpointer user_data, GError **error)
 {
-	FuInputStreamComputeCrcHelper *helper = (FuInputStreamComputeCrcHelper *)user_data;
+	FuInputStreamComputeCrc32Helper *helper = (FuInputStreamComputeCrc32Helper *)user_data;
 	helper->crc = fu_crc32_full(buf, bufsz, ~helper->crc, helper->polynomial);
 	return TRUE;
 }
@@ -511,11 +511,56 @@ fu_input_stream_compute_crc32(GInputStream *stream,
 			      guint32 polynomial,
 			      GError **error)
 {
-	FuInputStreamComputeCrcHelper helper = {.crc = *crc, .polynomial = polynomial};
+	FuInputStreamComputeCrc32Helper helper = {.crc = *crc, .polynomial = polynomial};
 	g_return_val_if_fail(G_IS_INPUT_STREAM(stream), FALSE);
 	g_return_val_if_fail(crc != NULL, FALSE);
 	g_return_val_if_fail(error == NULL || *error == NULL, FALSE);
 	if (!fu_input_stream_chunkify(stream, fu_input_stream_compute_crc32_cb, &helper, error))
+		return FALSE;
+	*crc = helper.crc;
+	return TRUE;
+}
+
+typedef struct {
+	guint16 crc;
+	guint16 polynomial;
+} FuInputStreamComputeCrc16Helper;
+
+static gboolean
+fu_input_stream_compute_crc16_cb(const guint8 *buf, gsize bufsz, gpointer user_data, GError **error)
+{
+	FuInputStreamComputeCrc16Helper *helper = (FuInputStreamComputeCrc16Helper *)user_data;
+	helper->crc = fu_crc16_full(buf, bufsz, ~helper->crc, helper->polynomial);
+	return TRUE;
+}
+
+/**
+ * fu_input_stream_compute_crc16:
+ * @stream: a #GInputStream
+ * @crc: (out): initial and final CRC value, typically 0x0
+ * @polynomial: CRC polynomial, typically 0xA001
+ * @error: (nullable): optional return location for an error
+ *
+ * Returns the cyclic redundancy check value for the given memory buffer.
+ *
+ * NOTE: The initial @crc differs from fu_crc16_full() in that it is inverted (to make it
+ * symmetrical, and chainable), so for most uses you want to use the value of 0x0, not 0xFFFF.
+ *
+ * Returns: %TRUE for success
+ *
+ * Since: 2.0.0
+ **/
+gboolean
+fu_input_stream_compute_crc16(GInputStream *stream,
+			      guint16 *crc,
+			      guint16 polynomial,
+			      GError **error)
+{
+	FuInputStreamComputeCrc16Helper helper = {.crc = *crc, .polynomial = polynomial};
+	g_return_val_if_fail(G_IS_INPUT_STREAM(stream), FALSE);
+	g_return_val_if_fail(crc != NULL, FALSE);
+	g_return_val_if_fail(error == NULL || *error == NULL, FALSE);
+	if (!fu_input_stream_chunkify(stream, fu_input_stream_compute_crc16_cb, &helper, error))
 		return FALSE;
 	*crc = helper.crc;
 	return TRUE;

libfwupdplugin/fu-input-stream.h
@@ -74,6 +74,11 @@ fu_input_stream_compute_sum16(GInputStream *stream,
 			      guint16 *value,
 			      GError **error) G_GNUC_WARN_UNUSED_RESULT G_GNUC_NON_NULL(1, 2);
 gboolean
+fu_input_stream_compute_crc16(GInputStream *stream,
+			      guint16 *crc,
+			      guint16 polynomial,
+			      GError **error) G_GNUC_WARN_UNUSED_RESULT G_GNUC_NON_NULL(1, 2);
+gboolean
 fu_input_stream_compute_crc32(GInputStream *stream,
 			      guint32 *crc,
 			      guint32 polynomial,

plugins/vli/fu-vli-pd-firmware.c
@@ -57,17 +57,6 @@ fu_vli_pd_firmware_export(FuFirmware *firmware, FuFirmwareExportFlags flags, XbB
 }
 
 static gboolean
-fu_vli_pd_firmware_compute_crc16_cb(const guint8 *buf,
-				    gsize bufsz,
-				    gpointer user_data,
-				    GError **error)
-{
-	guint16 *value = (guint16 *)user_data;
-	*value = fu_crc16_full(buf, bufsz, *value, 0xA001);
-	return TRUE;
-}
-
-static gboolean
 fu_vli_pd_firmware_parse(FuFirmware *firmware,
 			 GInputStream *stream,
 			 gsize offset,
@@ -140,7 +129,7 @@ fu_vli_pd_firmware_parse(FuFirmware *firmware,
 
 	/* check CRC */
 	if ((flags & FWUPD_INSTALL_FLAG_IGNORE_CHECKSUM) == 0) {
-		guint16 crc_actual = 0xFFFF;
+		guint16 crc_actual = 0x0;
 		guint16 crc_file = 0x0;
 		g_autoptr(GInputStream) stream_tmp = NULL;
 
@@ -152,11 +141,8 @@ fu_vli_pd_firmware_parse(FuFirmware *firmware,
 			g_prefix_error(error, "failed to read file CRC: ");
 			return FALSE;
 		}
-		stream_tmp = fu_partial_input_stream_new(stream, 0, streamsz);
-		if (!fu_input_stream_chunkify(stream_tmp,
-					      fu_vli_pd_firmware_compute_crc16_cb,
-					      &crc_actual,
-					      error))
+		stream_tmp = fu_partial_input_stream_new(stream, 0, streamsz - 2);
+		if (!fu_input_stream_compute_crc16(stream_tmp, &crc_actual, 0xA001, error))
 			return FALSE;
 		if (crc_actual != crc_file) {
 			g_set_error(error,