8a29ac3cd2eadae11f44a9fc580ac3b5a3787a17
Richard Hughes <richard@hughsie.com>
2024-01-11 09:25:58 +0000

    trivial: Remove duplicate key in redfish tests

--- plugins/redfish/tests/redfish.py
+++ plugins/redfish/tests/redfish.py
@@ -199,7 +199,6 @@ def firmware_inventory_bios():
         "LowestSupportedVersion": "P79 v1.10",
         "Name": "Contoso BIOS Firmware",
         "RelatedItem": [{"@odata.id": "/redfish/v1/Systems/437XR1138R2"}],
-        "ReleaseDate": "2017-12-06T12:00:00",
         "SoftwareId": "FEE82A67-6CE2-4625-9F44-237AD2402C28",
         "Updateable": True,
         "Version": "P79 v1.45",
