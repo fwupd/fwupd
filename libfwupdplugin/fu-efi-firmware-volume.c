9058214645e550c4f0260204c6548746e40bab14
Richard Hughes <richard@hughsie.com>
2024-01-13 08:42:03 +0000

    trivial: Fix an infinite loop when parsing an invalid EFI volume
    Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=65698

--- libfwupdplugin/fu-efi-firmware-volume.c
+++ libfwupdplugin/fu-efi-firmware-volume.c
@@ -155,6 +155,13 @@ fu_efi_firmware_volume_parse(FuFirmware *firmware,
 			    fu_struct_efi_volume_ext_entry_parse_stream(stream, offset_ext, error);
 			if (st_ext_entry == NULL)
 				return FALSE;
+			if (fu_struct_efi_volume_ext_entry_get_size(st_ext_entry) == 0x0) {
+				g_set_error_literal(error,
+						    G_IO_ERROR,
+						    G_IO_ERROR_INVALID_DATA,
+						    "EFI_VOLUME_EXT_ENTRY invalid size");
+				return FALSE;
+			}
 			if (fu_struct_efi_volume_ext_entry_get_size(st_ext_entry) == 0xFFFF)
 				break;
 			offset_ext += fu_struct_efi_volume_ext_entry_get_size(st_ext_entry);
