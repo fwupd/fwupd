fwupd/fwupd

commit 62f6f19d269212871493ba8343bf0d53204c7fb7

Reduce memory usage when loading large firmware
Some multi-GB firmware is impractical to load in memory and should instead be
streamed from disk or the device node.

plugins/pixart-rf/fu-pxi-ble-device.c
@@ -87,40 +87,29 @@ fu_pxi_ble_device_to_string(FuDevice *device, guint idt, GString *str)
 
 static FuFirmware *
 fu_pxi_ble_device_prepare_firmware(FuDevice *device,
-				   GBytes *fw,
+				   GInputStream *stream,
 				   FwupdInstallFlags flags,
 				   GError **error)
 {
 	FuPxiBleDevice *self = FU_PXI_BLE_DEVICE(device);
 	g_autoptr(FuFirmware) firmware = fu_pxi_firmware_new();
+	g_autoptr(FuFirmware) firmware_new = NULL;
 
-	if (!fu_firmware_parse(firmware, fw, flags, error))
+	if (!fu_firmware_parse_stream(firmware, stream, 0x0, flags, error))
 		return NULL;
 
 	if (fu_device_has_private_flag(device, FU_PXI_DEVICE_FLAG_IS_HPAC) &&
 	    fu_pxi_firmware_is_hpac(FU_PXI_FIRMWARE(firmware))) {
-		g_autoptr(GBytes) fw_tmp = NULL;
 		guint32 hpac_fw_size = 0;
-		const guint8 *fw_ptr = g_bytes_get_data(fw, NULL);
+		g_autoptr(GInputStream) stream_fw = NULL;
 
-		if (!fu_memread_uint32_safe(fw_ptr,
-					    g_bytes_get_size(fw),
-					    9,
-					    &hpac_fw_size,
-					    G_LITTLE_ENDIAN,
-					    error))
+		if (!fu_input_stream_read_u32(stream, 9, &hpac_fw_size, G_LITTLE_ENDIAN, error))
 			return NULL;
-		hpac_fw_size += 264;
-		fw_tmp = fu_bytes_new_offset(fw, 9, hpac_fw_size, error);
-		if (fw_tmp == NULL) {
-			g_set_error(error,
-				    FWUPD_ERROR,
-				    FWUPD_ERROR_INVALID_FILE,
-				    "HPAC F/W preparation failed.");
+		stream_fw = fu_partial_input_stream_new(stream, 9, hpac_fw_size + 264);
+		firmware_new = fu_pxi_firmware_new();
+		if (!fu_firmware_parse_stream(firmware, stream_fw, 0x0, flags, error))
 			return NULL;
-		}
 
-		fu_firmware_set_bytes(firmware, fw_tmp);
 	} else if (!fu_device_has_private_flag(device, FU_PXI_DEVICE_FLAG_IS_HPAC) &&
 		   !fu_pxi_firmware_is_hpac(FU_PXI_FIRMWARE(firmware))) {
 		const gchar *model_name;
@@ -146,6 +135,7 @@ fu_pxi_ble_device_prepare_firmware(FuDevice *device,
 				return NULL;
 			}
 		}
+		firmware_new = g_object_ref(firmware);
 	} else {
 		g_set_error(error,
 			    FWUPD_ERROR,
@@ -154,7 +144,7 @@ fu_pxi_ble_device_prepare_firmware(FuDevice *device,
 		return NULL;
 	}
 
-	return g_steal_pointer(&firmware);
+	return g_steal_pointer(&firmware_new);
 }
 
 #ifdef HAVE_HIDRAW_H

plugins/pixart-rf/fu-pxi-receiver-device.c
@@ -55,39 +55,27 @@ fu_pxi_receiver_device_to_string(FuDevice *device, guint idt, GString *str)
 
 static FuFirmware *
 fu_pxi_receiver_device_prepare_firmware(FuDevice *device,
-					GBytes *fw,
+					GInputStream *stream,
 					FwupdInstallFlags flags,
 					GError **error)
 {
 	g_autoptr(FuFirmware) firmware = fu_pxi_firmware_new();
+	g_autoptr(FuFirmware) firmware_new = NULL;
 
-	if (!fu_firmware_parse(firmware, fw, flags, error))
+	if (!fu_firmware_parse_stream(firmware, stream, 0x0, flags, error))
 		return NULL;
 
 	if (fu_device_has_private_flag(device, FU_PXI_DEVICE_FLAG_IS_HPAC) &&
 	    fu_pxi_firmware_is_hpac(FU_PXI_FIRMWARE(firmware))) {
-		g_autoptr(GBytes) fw_tmp = NULL;
 		guint32 hpac_fw_size = 0;
-		const guint8 *fw_ptr = g_bytes_get_data(fw, NULL);
+		g_autoptr(GInputStream) stream_fw = NULL;
 
-		if (!fu_memread_uint32_safe(fw_ptr,
-					    g_bytes_get_size(fw),
-					    9,
-					    &hpac_fw_size,
-					    G_LITTLE_ENDIAN,
-					    error))
+		if (!fu_input_stream_read_u32(stream, 9, &hpac_fw_size, G_LITTLE_ENDIAN, error))
 			return NULL;
-		hpac_fw_size += 264;
-		fw_tmp = fu_bytes_new_offset(fw, 9, hpac_fw_size, error);
-		if (fw_tmp == NULL) {
-			g_set_error(error,
-				    FWUPD_ERROR,
-				    FWUPD_ERROR_INVALID_FILE,
-				    "HPAC F/W preparation failed.");
+		stream_fw = fu_partial_input_stream_new(stream, 9, hpac_fw_size + 264);
+		firmware_new = fu_pxi_firmware_new();
+		if (!fu_firmware_parse_stream(firmware, stream_fw, 0x0, flags, error))
 			return NULL;
-		}
-
-		fu_firmware_set_bytes(firmware, fw_tmp);
 	} else if (fu_device_has_private_flag(device, FU_PXI_DEVICE_FLAG_IS_HPAC) !=
 		   fu_pxi_firmware_is_hpac(FU_PXI_FIRMWARE(firmware))) {
 		g_set_error(error,
@@ -95,9 +83,11 @@ fu_pxi_receiver_device_prepare_firmware(FuDevice *device,
 			    FWUPD_ERROR_INVALID_FILE,
 			    "The firmware is incompatible with the device");
 		return NULL;
+	} else {
+		firmware_new = g_object_ref(firmware);
 	}
 
-	return g_steal_pointer(&firmware);
+	return g_steal_pointer(&firmware_new);
 }
 
 static gboolean

plugins/pixart-rf/fu-pxi-wireless-device.c
@@ -52,44 +52,32 @@ fu_pxi_wireless_device_get_parent(FuDevice *self, GError **error)
 
 static FuFirmware *
 fu_pxi_wireless_device_prepare_firmware(FuDevice *device,
-					GBytes *fw,
+					GInputStream *stream,
 					FwupdInstallFlags flags,
 					GError **error)
 {
 	FuPxiReceiverDevice *parent;
 	g_autoptr(FuFirmware) firmware = fu_pxi_firmware_new();
+	g_autoptr(FuFirmware) firmware_new = NULL;
 
 	parent = fu_pxi_wireless_device_get_parent(device, error);
 	if (parent == NULL)
 		return NULL;
 
-	if (!fu_firmware_parse(firmware, fw, flags, error))
+	if (!fu_firmware_parse_stream(firmware, stream, 0x0, flags, error))
 		return NULL;
 
 	if (fu_device_has_private_flag(FU_DEVICE(parent), FU_PXI_DEVICE_FLAG_IS_HPAC) &&
 	    fu_pxi_firmware_is_hpac(FU_PXI_FIRMWARE(firmware))) {
-		g_autoptr(GBytes) fw_tmp = NULL;
 		guint32 hpac_fw_size = 0;
-		const guint8 *fw_ptr = g_bytes_get_data(fw, NULL);
+		g_autoptr(GInputStream) stream_fw = NULL;
 
-		if (!fu_memread_uint32_safe(fw_ptr,
-					    g_bytes_get_size(fw),
-					    9,
-					    &hpac_fw_size,
-					    G_LITTLE_ENDIAN,
-					    error))
+		if (!fu_input_stream_read_u32(stream, 9, &hpac_fw_size, G_LITTLE_ENDIAN, error))
 			return NULL;
-		hpac_fw_size += 264;
-		fw_tmp = fu_bytes_new_offset(fw, 9, hpac_fw_size, error);
-		if (fw_tmp == NULL) {
-			g_set_error(error,
-				    FWUPD_ERROR,
-				    FWUPD_ERROR_INVALID_FILE,
-				    "HPAC F/W preparation failed.");
+		stream_fw = fu_partial_input_stream_new(stream, 9, hpac_fw_size + 264);
+		firmware_new = fu_pxi_firmware_new();
+		if (!fu_firmware_parse_stream(firmware, stream_fw, 0x0, flags, error))
 			return NULL;
-		}
-
-		fu_firmware_set_bytes(firmware, fw_tmp);
 	} else if (fu_device_has_private_flag(FU_DEVICE(parent), FU_PXI_DEVICE_FLAG_IS_HPAC) !=
 		   fu_pxi_firmware_is_hpac(FU_PXI_FIRMWARE(firmware))) {
 		g_set_error(error,
@@ -97,9 +85,11 @@ fu_pxi_wireless_device_prepare_firmware(FuDevice *device,
 			    FWUPD_ERROR_INVALID_FILE,
 			    "The firmware is incompatible with the device");
 		return NULL;
+	} else {
+		firmware_new = g_object_ref(firmware);
 	}
 
-	return g_steal_pointer(&firmware);
+	return g_steal_pointer(&firmware_new);
 }
 
 static gboolean

commit 58c838cf552b56d0b641df3836df2c72bae161bc

pixart-rf: Fix a regression when installing receiver device firmware
This regressed in 62f6f19d2, mea culpa.

Fixes https://github.com/fwupd/fwupd/issues/6743

plugins/pixart-rf/fu-pxi-ble-device.c
@@ -89,7 +89,6 @@ fu_pxi_ble_device_prepare_firmware(FuDevice *device,
 {
 	FuPxiBleDevice *self = FU_PXI_BLE_DEVICE(device);
 	g_autoptr(FuFirmware) firmware = fu_pxi_firmware_new();
-	g_autoptr(FuFirmware) firmware_new = NULL;
 
 	if (!fu_firmware_parse_stream(firmware, stream, 0x0, flags, error))
 		return NULL;
@@ -97,13 +96,12 @@ fu_pxi_ble_device_prepare_firmware(FuDevice *device,
 	if (fu_device_has_private_flag(device, FU_PXI_DEVICE_FLAG_IS_HPAC) &&
 	    fu_pxi_firmware_is_hpac(FU_PXI_FIRMWARE(firmware))) {
 		guint32 hpac_fw_size = 0;
-		g_autoptr(GInputStream) stream_fw = NULL;
+		g_autoptr(GInputStream) stream_new = NULL;
 
 		if (!fu_input_stream_read_u32(stream, 9, &hpac_fw_size, G_LITTLE_ENDIAN, error))
 			return NULL;
-		stream_fw = fu_partial_input_stream_new(stream, 9, hpac_fw_size + 264);
-		firmware_new = fu_pxi_firmware_new();
-		if (!fu_firmware_parse_stream(firmware, stream_fw, 0x0, flags, error))
+		stream_new = fu_partial_input_stream_new(stream, 9, hpac_fw_size + 264);
+		if (!fu_firmware_set_stream(firmware, stream_new, error))
 			return NULL;
 
 	} else if (!fu_device_has_private_flag(device, FU_PXI_DEVICE_FLAG_IS_HPAC) &&
@@ -131,7 +129,6 @@ fu_pxi_ble_device_prepare_firmware(FuDevice *device,
 				return NULL;
 			}
 		}
-		firmware_new = g_object_ref(firmware);
 	} else {
 		g_set_error(error,
 			    FWUPD_ERROR,
@@ -140,7 +137,7 @@ fu_pxi_ble_device_prepare_firmware(FuDevice *device,
 		return NULL;
 	}
 
-	return g_steal_pointer(&firmware_new);
+	return g_steal_pointer(&firmware);
 }
 
 #ifdef HAVE_HIDRAW_H

plugins/pixart-rf/fu-pxi-receiver-device.c
@@ -60,7 +60,6 @@ fu_pxi_receiver_device_prepare_firmware(FuDevice *device,
 					GError **error)
 {
 	g_autoptr(FuFirmware) firmware = fu_pxi_firmware_new();
-	g_autoptr(FuFirmware) firmware_new = NULL;
 
 	if (!fu_firmware_parse_stream(firmware, stream, 0x0, flags, error))
 		return NULL;
@@ -68,13 +67,12 @@ fu_pxi_receiver_device_prepare_firmware(FuDevice *device,
 	if (fu_device_has_private_flag(device, FU_PXI_DEVICE_FLAG_IS_HPAC) &&
 	    fu_pxi_firmware_is_hpac(FU_PXI_FIRMWARE(firmware))) {
 		guint32 hpac_fw_size = 0;
-		g_autoptr(GInputStream) stream_fw = NULL;
+		g_autoptr(GInputStream) stream_new = NULL;
 
 		if (!fu_input_stream_read_u32(stream, 9, &hpac_fw_size, G_LITTLE_ENDIAN, error))
 			return NULL;
-		stream_fw = fu_partial_input_stream_new(stream, 9, hpac_fw_size + 264);
-		firmware_new = fu_pxi_firmware_new();
-		if (!fu_firmware_parse_stream(firmware, stream_fw, 0x0, flags, error))
+		stream_new = fu_partial_input_stream_new(stream, 9, hpac_fw_size + 264);
+		if (!fu_firmware_set_stream(firmware, stream_new, error))
 			return NULL;
 	} else if (fu_device_has_private_flag(device, FU_PXI_DEVICE_FLAG_IS_HPAC) !=
 		   fu_pxi_firmware_is_hpac(FU_PXI_FIRMWARE(firmware))) {
@@ -83,11 +81,9 @@ fu_pxi_receiver_device_prepare_firmware(FuDevice *device,
 			    FWUPD_ERROR_INVALID_FILE,
 			    "The firmware is incompatible with the device");
 		return NULL;
-	} else {
-		firmware_new = g_object_ref(firmware);
 	}
 
-	return g_steal_pointer(&firmware_new);
+	return g_steal_pointer(&firmware);
 }
 
 static gboolean

plugins/pixart-rf/fu-pxi-wireless-device.c
@@ -58,7 +58,6 @@ fu_pxi_wireless_device_prepare_firmware(FuDevice *device,
 {
 	FuPxiReceiverDevice *parent;
 	g_autoptr(FuFirmware) firmware = fu_pxi_firmware_new();
-	g_autoptr(FuFirmware) firmware_new = NULL;
 
 	parent = fu_pxi_wireless_device_get_parent(device, error);
 	if (parent == NULL)
@@ -70,13 +69,12 @@ fu_pxi_wireless_device_prepare_firmware(FuDevice *device,
 	if (fu_device_has_private_flag(FU_DEVICE(parent), FU_PXI_DEVICE_FLAG_IS_HPAC) &&
 	    fu_pxi_firmware_is_hpac(FU_PXI_FIRMWARE(firmware))) {
 		guint32 hpac_fw_size = 0;
-		g_autoptr(GInputStream) stream_fw = NULL;
+		g_autoptr(GInputStream) stream_new = NULL;
 
 		if (!fu_input_stream_read_u32(stream, 9, &hpac_fw_size, G_LITTLE_ENDIAN, error))
 			return NULL;
-		stream_fw = fu_partial_input_stream_new(stream, 9, hpac_fw_size + 264);
-		firmware_new = fu_pxi_firmware_new();
-		if (!fu_firmware_parse_stream(firmware, stream_fw, 0x0, flags, error))
+		stream_new = fu_partial_input_stream_new(stream, 9, hpac_fw_size + 264);
+		if (!fu_firmware_set_stream(firmware, stream_new, error))
 			return NULL;
 	} else if (fu_device_has_private_flag(FU_DEVICE(parent), FU_PXI_DEVICE_FLAG_IS_HPAC) !=
 		   fu_pxi_firmware_is_hpac(FU_PXI_FIRMWARE(firmware))) {
@@ -85,11 +83,9 @@ fu_pxi_wireless_device_prepare_firmware(FuDevice *device,
 			    FWUPD_ERROR_INVALID_FILE,
 			    "The firmware is incompatible with the device");
 		return NULL;
-	} else {
-		firmware_new = g_object_ref(firmware);
 	}
 
-	return g_steal_pointer(&firmware_new);
+	return g_steal_pointer(&firmware);
 }
 
 static gboolean