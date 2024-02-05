fwupd/fwupd

commit 54cbef45b9955b9c2b7071bd3c7467793b67323d

Do not warn the user about ESP when using MBR

plugins/uefi-capsule/fu-uefi-capsule-plugin.c
@@ -727,7 +727,7 @@ fu_uefi_capsule_plugin_validate_esp(FuUefiCapsulePlugin *self)
 
 	/* found *something* but it wasn't quite an ESP... */
 	kind = fu_volume_get_partition_kind(self->esp);
-	if (g_strcmp0(kind, FU_VOLUME_KIND_ESP) != 0) {
+	if (g_strcmp0(fu_volume_kind_convert_to_gpt(kind), FU_VOLUME_KIND_ESP) != 0) {
 		fu_plugin_add_flag(FU_PLUGIN(self), FWUPD_PLUGIN_FLAG_ESP_NOT_VALID);
 		fu_plugin_add_flag(FU_PLUGIN(self), FWUPD_PLUGIN_FLAG_USER_WARNING);
 	}

commit e46b77dd4a5b9ed5f1b4886cd28a00fb38f4de73

trivial: Fix a UndefinedBehaviorSanitizer: undefined-behavior warning
This regressed in 54cbef4.

plugins/uefi-capsule/fu-uefi-capsule-plugin.c
@@ -711,7 +711,8 @@ fu_uefi_capsule_plugin_validate_esp(FuUefiCapsulePlugin *self)
 
 	/* found *something* but it wasn't quite an ESP... */
 	kind = fu_volume_get_partition_kind(self->esp);
-	if (g_strcmp0(fu_volume_kind_convert_to_gpt(kind), FU_VOLUME_KIND_ESP) != 0) {
+	if (kind != NULL &&
+	    g_strcmp0(fu_volume_kind_convert_to_gpt(kind), FU_VOLUME_KIND_ESP) != 0) {
 		fu_plugin_add_flag(FU_PLUGIN(self), FWUPD_PLUGIN_FLAG_ESP_NOT_VALID);
 		fu_plugin_add_flag(FU_PLUGIN(self), FWUPD_PLUGIN_FLAG_USER_WARNING);
 	}