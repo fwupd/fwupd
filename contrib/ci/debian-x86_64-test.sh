ea75b94b302622632968968ec87048f0b2d670ba
Mario Limonciello <mario.limonciello@amd.com>
2024-02-05 23:23:54 -0600

    Add support for code coverage reporting with Coveralls
    Due to various CI complexities this also rejoins the separated
    build/test phases and ONLY runs on Debian builds at the moment.

--- contrib/ci/debian-x86_64-test.sh
+++ contrib/ci/debian-x86_64-test.sh
@@ -1,8 +1,20 @@
 #!/bin/bash -e
 
-apt install -y ./*.deb
+# install
+apt update
+apt install -y gcovr ./dist/*.deb
+
+# run tests
 ./contrib/ci/get_test_firmware.sh
 cp fwupd-test-firmware/installed-tests/* /usr/share/installed-tests/fwupd/ -LRv
 service dbus restart
 gnome-desktop-testing-runner fwupd
+
+# generate coverage report
+if [ "$CI" = "true" ]; then
+	gcovr -x -o coverage.xml
+	sed "s,build/,,g" coverage.xml -i
+fi
+
+# cleanup
 apt purge -y fwupd fwupd-doc libfwupd3 libfwupd-dev
