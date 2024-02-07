e60d8471bfe2bc2a2ec2fac61e8b6be3075a7870
Mario Limonciello <mario.limonciello@amd.com>
2024-02-07 14:05:12 -0600

    trivial: ci: Add a test flow for verify update

--- data/installed-tests/fwupdtool.sh
+++ data/installed-tests/fwupdtool.sh
@@ -6,6 +6,12 @@ CAB=fakedevice124.cab
 INPUT="@installedtestsdir@/fakedevice124.bin \
        @installedtestsdir@/fakedevice124.jcat \
        @installedtestsdir@/fakedevice124.metainfo.xml"
+DEVICE=08d460be0f1f9f128413f816022a6439e0078018
+
+# ---
+echo "Enabling test device..."
+fwupdtool enable-test-devices
+rc=$?; if [ $rc != 0 ]; then exit $rc; fi
 
 # ---
 echo "Building ${CAB}..."
@@ -20,3 +26,8 @@ rc=$?; if [ $rc != 0 ]; then exit $rc; fi
 # ---
 echo "Cleaning ${CAB} generated cabinet ..."
 rm -f ${CAB}
+
+# ---
+echo "Verifying update..."
+fwupdtool verify-update ${DEVICE}
+rc=$?; if [ $rc != 0 ]; then exit $rc; fi
