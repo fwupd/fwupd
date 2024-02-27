8073c9ff581d1a3206295ff4e3e301ee3913ef76
Richard Hughes <richard@hughsie.com>
2024-02-27 07:56:36 +0000

    wacom-raw: trivial: Fix some progress values

--- plugins/wacom-raw/fu-wacom-aes-device.c
+++ plugins/wacom-raw/fu-wacom-aes-device.c
@@ -258,9 +258,8 @@ fu_wacom_aes_device_write_firmware(FuDevice *device,
 
 	/* progress */
 	fu_progress_set_id(progress, G_STRLOC);
-	fu_progress_add_flag(progress, FU_PROGRESS_FLAG_GUESSED);
-	fu_progress_add_step(progress, FWUPD_STATUS_DEVICE_ERASE, 20, NULL);
-	fu_progress_add_step(progress, FWUPD_STATUS_DEVICE_WRITE, 80, NULL);
+	fu_progress_add_step(progress, FWUPD_STATUS_DEVICE_ERASE, 28, NULL);
+	fu_progress_add_step(progress, FWUPD_STATUS_DEVICE_WRITE, 72, NULL);
 
 	/* erase */
 	if (!fu_wacom_aes_device_erase_all(self, fu_progress_get_child(progress), error))
