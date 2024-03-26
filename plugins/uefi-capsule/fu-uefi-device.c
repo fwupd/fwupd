fd497690f86a76fe466c03f0d1ba8f5ed4656954
Richard Hughes <richard@hughsie.com>
2024-03-26 14:43:00 +0000

    Put the fwupd-efi verbose debugging in the journal
    Reusing the device error string is both confusing and looks terrible in the UI.

--- plugins/uefi-capsule/fu-uefi-device.c
+++ plugins/uefi-capsule/fu-uefi-device.c
@@ -547,19 +547,19 @@ fu_uefi_device_capture_efi_debugging(FuDevice *device)
 				       NULL,
 				       &error_local);
 	if (buf == NULL) {
-		fu_device_set_update_error(device, error_local->message);
+		g_warning("failed to capture EFI debugging: %s", error_local->message);
 		return;
 	}
 
 	/* convert from UCS-2 to UTF-8 */
 	str = fu_utf16_to_utf8_bytes(buf, G_LITTLE_ENDIAN, &error_local);
 	if (str == NULL) {
-		fu_device_set_update_error(device, error_local->message);
+		g_warning("failed to capture EFI debugging: %s", error_local->message);
 		return;
 	}
 
-	/* success */
-	fu_device_set_update_error(device, str);
+	/* success, dump into journal */
+	g_info("EFI debugging: %s", str);
 }
 
 gboolean
