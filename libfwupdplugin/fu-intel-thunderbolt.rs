a3a6ef2470b0c5c69aa5aea9cdb4e5f1c3677cc5
Richard Hughes <richard@hughsie.com>
2024-04-05 17:19:38 +0100

    trivial: Build the thunderbolt self test binaries from builder XML

--- libfwupdplugin/fu-intel-thunderbolt.rs
+++ libfwupdplugin/fu-intel-thunderbolt.rs
@@ -10,6 +10,11 @@ enum FuIntelThunderboltNvmSection {
     Last,
 }
 
+#[repr(u8)]
+enum FuIntelThunderboltNvmSectionFlag {
+    Dram = 1 << 6,
+}
+
 #[derive(ToString, FromString)]
 enum FuIntelThunderboltNvmFamily {
     Unknown,
@@ -21,4 +26,44 @@ enum FuIntelThunderboltNvmFamily {
     Bb,
     MapleRidge,
     GoshenRidge,
+}
+
+#[derive(New)]
+struct FuIntelThunderboltNvmDigital {
+    reserved: [u8; 2],
+    available_sections: u8, // FuIntelThunderboltNvmSectionFlag
+    ucode: u16, // addr
+    device_id: u32,
+    version: u16,
+    reserved: [u8; 5],
+    flags_host: u8,
+    reserved: [u8; 52],
+    flash_size: u8,
+    reserved: [u8; 47],
+    arc_params: u32,
+    reserved: [u8; 2],
+    flags_is_native: u8,
+    reserved: [u8; 146],
+    drom: u32,
+    reserved: [u8; 14],
+}
+
+#[derive(New)]
+struct FuIntelThunderboltNvmDrom {
+    reserved: [u8; 16],
+    vendor_id: u16,
+    model_id: u16,
+    reserved: [u8; 12],
+}
+
+#[derive(New)]
+struct FuIntelThunderboltNvmArcParams {
+    reserved: [u8; 268],
+    pd_pointer: u32,
+    reserved: [u8; 16],
+}
+
+#[derive(New)]
+struct FuIntelThunderboltNvmDram {
+    reserved: [u8; 16],
 }
