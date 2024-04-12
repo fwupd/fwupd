fwupd/fwupd

commit 4d96224c42db2ad18c63f9a713f4068479016700

Convert GUsbError to FwupdError domains
See https://github.com/fwupd/fwupd/discussions/6907

libfwupdplugin/fu-usb-device.c
@@ -706,8 +706,10 @@ fu_usb_device_probe_bos_descriptors(FuUsbDevice *self, GError **error)
 	}
 
 	gusb_locker = fu_device_locker_new(priv->usb_device, error);
-	if (gusb_locker == NULL)
+	if (gusb_locker == NULL) {
+		fu_error_convert(error);
 		return FALSE;
+	}
 	bos_descriptors = g_usb_device_get_bos_descriptors(priv->usb_device, &error_local);
 	if (bos_descriptors == NULL) {
 		if (g_error_matches(error_local, G_USB_DEVICE_ERROR, G_USB_DEVICE_ERROR_IO)) {
@@ -715,6 +717,7 @@ fu_usb_device_probe_bos_descriptors(FuUsbDevice *self, GError **error)
 			return TRUE;
 		}
 		g_propagate_error(error, g_steal_pointer(&error_local));
+		fu_error_convert(error);
 		return FALSE;
 	}
 	for (guint i = 0; i < bos_descriptors->len; i++) {

libfwupdplugin/fu-common.c
@@ -11,6 +11,9 @@
 #ifdef HAVE_CPUID_H
 #include <cpuid.h>
 #endif
+#ifdef HAVE_GUSB
+#include <gusb.h>
+#endif
 
 #include "fu-common-private.h"
 #include "fu-firmware.h"
@@ -268,6 +271,70 @@ fu_power_state_is_ac(FuPowerState power_state)
 	    power_state == FU_POWER_STATE_AC_FULLY_CHARGED)
 		return TRUE;
 	return FALSE;
+}
+
+/**
+ * fu_error_convert:
+ * @perror: (nullable): A #GError, perhaps with domain #GIOError
+ *
+ * Convert the error to a #FwupdError, if required.
+ *
+ * Since: 2.0.0
+ **/
+void
+fu_error_convert(GError **perror)
+{
+	GError *error = (perror != NULL) ? *perror : NULL;
+	struct {
+		GQuark domain;
+		gint code;
+		FwupdError fwupd_code;
+	} map[] = {
+#ifdef HAVE_GUSB
+		{G_USB_DEVICE_ERROR, G_USB_DEVICE_ERROR_ALREADY_OPEN, FWUPD_ERROR_NOTHING_TO_DO},
+		{G_USB_DEVICE_ERROR, G_USB_DEVICE_ERROR_CANCELLED, FWUPD_ERROR_NOTHING_TO_DO},
+		{G_USB_DEVICE_ERROR, G_USB_DEVICE_ERROR_FAILED, FWUPD_ERROR_INTERNAL},
+		{G_USB_DEVICE_ERROR, G_USB_DEVICE_ERROR_INTERNAL, FWUPD_ERROR_INTERNAL},
+		{G_USB_DEVICE_ERROR, G_USB_DEVICE_ERROR_IO, FWUPD_ERROR},
+		{G_USB_DEVICE_ERROR, G_USB_DEVICE_ERROR_NO_DEVICE, FWUPD_ERROR_NOT_FOUND},
+		{G_USB_DEVICE_ERROR, G_USB_DEVICE_ERROR_NOT_OPEN, FWUPD_ERROR_INTERNAL},
+		{G_USB_DEVICE_ERROR, G_USB_DEVICE_ERROR_NOT_SUPPORTED, FWUPD_ERROR_NOT_SUPPORTED},
+		{G_USB_DEVICE_ERROR, G_USB_DEVICE_ERROR_TIMED_OUT, FWUPD_ERROR_TIMED_OUT},
+		{G_USB_DEVICE_ERROR,
+		 G_USB_DEVICE_ERROR_PERMISSION_DENIED,
+		 FWUPD_ERROR_PERMISSION_DENIED},
+#if G_USB_CHECK_VERSION(0, 4, 8)
+		{G_USB_DEVICE_ERROR, G_USB_DEVICE_ERROR_BUSY, FWUPD_ERROR_BUSY},
+#endif
+#endif
+	};
+
+	/* sanity check */
+	if (error == NULL)
+		return;
+
+	/* convert GIOError and GFileError */
+	fwupd_error_convert(perror);
+	if (error->domain == FWUPD_ERROR)
+		return;
+
+	/* find match */
+	for (guint i = 0; i < G_N_ELEMENTS(map); i++) {
+		if (g_error_matches(error, map[i].domain, map[i].code)) {
+			error->domain = FWUPD_ERROR;
+			error->code = map[i].fwupd_code;
+			return;
+		}
+	}
+
+	/* fallback */
+#ifndef SUPPORTED_BUILD
+	g_critical("GError %s:%i sending over D-Bus was not converted to FwupdError",
+		   g_quark_to_string(error->domain),
+		   error->code);
+#endif
+	error->domain = FWUPD_ERROR;
+	error->code = FWUPD_ERROR_INTERNAL;
 }
 
 /**

libfwupdplugin/fu-common.h
@@ -68,6 +68,8 @@ gsize
 fu_common_align_up(gsize value, guint8 alignment);
 gboolean
 fu_power_state_is_ac(FuPowerState power_state);
+void
+fu_error_convert(GError **perror);
 
 void
 fu_xmlb_builder_insert_kv(XbBuilderNode *bn, const gchar *key, const gchar *value)

libfwupdplugin/fu-device-locker.c
@@ -87,9 +87,7 @@ fu_device_locker_close(FuDeviceLocker *self, GError **error)
 		return TRUE;
 	if (!self->close_func(self->device, &error_local)) {
 #ifdef HAVE_GUSB
-		if (g_error_matches(error_local,
-				    G_USB_DEVICE_ERROR,
-				    G_USB_DEVICE_ERROR_NO_DEVICE)) {
+		if (g_error_matches(error_local, FWUPD_ERROR, FWUPD_ERROR_NOT_FOUND)) {
 			g_debug("ignoring: %s", error_local->message);
 			return TRUE;
 		}

libfwupdplugin/fu-hid-device.c
@@ -423,6 +423,7 @@ fu_hid_device_set_report_internal(FuHidDevice *self, FuHidDeviceRetryHelper *hel
 						     helper->timeout,
 						     NULL, /* cancellable */
 						     error)) {
+			fu_error_convert(error);
 			return FALSE;
 		}
 	} else {
@@ -451,6 +452,7 @@ fu_hid_device_set_report_internal(FuHidDevice *self, FuHidDeviceRetryHelper *hel
 						   NULL,
 						   error)) {
 			g_prefix_error(error, "failed to SetReport: ");
+			fu_error_convert(error);
 			return FALSE;
 		}
 	}
@@ -554,6 +556,7 @@ fu_hid_device_get_report_internal(FuHidDevice *self, FuHidDeviceRetryHelper *hel
 						     helper->timeout,
 						     NULL, /* cancellable */
 						     error)) {
+			fu_error_convert(error);
 			return FALSE;
 		}
 		title = g_strdup_printf("HID::GetReport [EP=0x%02x]", priv->ep_addr_in);
@@ -584,6 +587,7 @@ fu_hid_device_get_report_internal(FuHidDevice *self, FuHidDeviceRetryHelper *hel
 						   NULL,
 						   error)) {
 			g_prefix_error(error, "failed to GetReport: ");
+			fu_error_convert(error);
 			return FALSE;
 		}
 		fu_dump_raw(G_LOG_DOMAIN, title, helper->buf, actual_len);

plugins/aver-hid/fu-aver-hid-device.c
@@ -90,9 +90,7 @@ fu_aver_hid_device_ensure_version(FuAverHidDevice *self, GError **error)
 	g_autoptr(GError) error_local = NULL;
 
 	if (!fu_aver_hid_device_transfer(self, req, res, &error_local)) {
-		if (g_error_matches(error_local,
-				    G_USB_DEVICE_ERROR,
-				    G_USB_DEVICE_ERROR_TIMED_OUT)) {
+		if (g_error_matches(error_local, FWUPD_ERROR, FWUPD_ERROR_TIMED_OUT)) {
 			g_debug("ignoring %s", error_local->message);
 			fu_device_set_version(FU_DEVICE(self), "0.0.0000.00");
 			return TRUE;

plugins/cros-ec/fu-cros-ec-usb-device.c
@@ -185,6 +185,7 @@ fu_cros_ec_usb_device_do_xfer(FuCrosEcUsbDevice *self,
 						BULK_SEND_TIMEOUT_MS,
 						NULL,
 						error)) {
+			fu_error_convert(error);
 			return FALSE;
 		}
 		if (actual != outlen) {
@@ -209,6 +210,7 @@ fu_cros_ec_usb_device_do_xfer(FuCrosEcUsbDevice *self,
 						BULK_RECV_TIMEOUT_MS,
 						NULL,
 						error)) {
+			fu_error_convert(error);
 			return FALSE;
 		}
 		if (actual != inlen && !allow_less) {
@@ -850,9 +852,7 @@ fu_cros_ec_usb_device_write_firmware(FuDevice *device,
 							    section,
 							    fu_progress_get_child(progress),
 							    &error_local)) {
-			if (g_error_matches(error_local,
-					    G_USB_DEVICE_ERROR,
-					    G_USB_DEVICE_ERROR_NOT_SUPPORTED)) {
+			if (g_error_matches(error_local, FWUPD_ERROR, FWUPD_ERROR_NOT_SUPPORTED)) {
 				g_debug("failed to transfer section, trying another write, "
 					"ignoring error: %s",
 					error_local->message);

plugins/fresco-pd/fu-fresco-pd-device.c
@@ -54,6 +54,7 @@ fu_fresco_pd_device_transfer_read(FuFrescoPdDevice *self,
 					   NULL,
 					   error)) {
 		g_prefix_error(error, "failed to read from offset 0x%x: ", offset);
+		fu_error_convert(error);
 		return FALSE;
 	}
 	if (bufsz != actual_length) {
@@ -99,6 +100,7 @@ fu_fresco_pd_device_transfer_write(FuFrescoPdDevice *self,
 					   NULL,
 					   error)) {
 		g_prefix_error(error, "failed to write offset 0x%x: ", offset);
+		fu_error_convert(error);
 		return FALSE;
 	}
 	if (bufsz != actual_length) {
@@ -222,7 +224,7 @@ fu_fresco_pd_device_panther_reset_device(FuFrescoPdDevice *self, GError **error)
 
 	/* ignore when the device reset before completing the transaction */
 	if (!fu_fresco_pd_device_or_byte(self, 0xA003, 1 << 3, &error_local)) {
-		if (g_error_matches(error_local, G_USB_DEVICE_ERROR, G_USB_DEVICE_ERROR_FAILED)) {
+		if (g_error_matches(error_local, FWUPD_ERROR, FWUPD_ERROR_INTERNAL)) {
 			g_debug("ignoring %s", error_local->message);
 			return TRUE;
 		}

commit b9f1154d767262d25d8251335fd8f78873965ded

Add the FwupdError codes to auto-abort FuUsbDevices
This regressed in 4d96224c42db and fixes updating CalDigit TS4.

libfwupdplugin/fu-usb-device.c
@@ -119,6 +119,11 @@ fu_usb_device_init(FuUsbDevice *device)
 				     G_USB_DEVICE_ERROR_PERMISSION_DENIED,
 				     NULL);
 #endif
+	fu_device_retry_add_recovery(FU_DEVICE(device), FWUPD_ERROR, FWUPD_ERROR_NOT_FOUND, NULL);
+	fu_device_retry_add_recovery(FU_DEVICE(device),
+				     FWUPD_ERROR,
+				     FWUPD_ERROR_PERMISSION_DENIED,
+				     NULL);
 }
 
 static void