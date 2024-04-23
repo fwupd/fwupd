4962d3b5be83592df19fef2edf2c3e41d5538697
Richard Hughes <richard@hughsie.com>
2024-04-23 10:52:07 +0100

    trivial: Relax the fwupd_client_disconnect() return value requirement

--- libfwupd/fwupd-client.h
+++ libfwupd/fwupd-client.h
@@ -81,8 +81,7 @@ fwupd_client_connect_finish(FwupdClient *self,
 			    GAsyncResult *res,
 			    GError **error) G_GNUC_WARN_UNUSED_RESULT G_GNUC_NON_NULL(1, 2);
 gboolean
-fwupd_client_disconnect(FwupdClient *self, GError **error) G_GNUC_WARN_UNUSED_RESULT
-    G_GNUC_NON_NULL(1);
+fwupd_client_disconnect(FwupdClient *self, GError **error) G_GNUC_NON_NULL(1);
 void
 fwupd_client_quit_async(FwupdClient *self,
 			GCancellable *cancellable,
