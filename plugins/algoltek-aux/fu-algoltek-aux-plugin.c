05c591777800c98f4e98d51fa88199707b521947
Richard Hughes <richard@hughsie.com>
2024-05-10 14:51:25 +0100

    algoltek-aux: Remove the extra locker
    This is already special-cased in fu-udev-backend.c for FuDpauxDevice devices.

--- plugins/algoltek-aux/fu-algoltek-aux-plugin.c
+++ plugins/algoltek-aux/fu-algoltek-aux-plugin.c
@@ -22,10 +22,7 @@ fu_algoltek_aux_plugin_backend_device_added(FuPlugin *plugin,
 					    FuProgress *progress,
 					    GError **error)
 {
-	FuAlgoltekAuxPlugin *self = FU_ALGOLTEK_AUX_PLUGIN(plugin);
 	g_autoptr(FuAlgoltekAuxDevice) dev = NULL;
-	g_autoptr(FuDeviceLocker) locker = NULL;
-	g_autoptr(GError) error_local = NULL;
 
 	/* interesting device? */
 	if (!FU_IS_DPAUX_DEVICE(device))
@@ -35,19 +32,7 @@ fu_algoltek_aux_plugin_backend_device_added(FuPlugin *plugin,
 	dev = fu_algoltek_aux_device_new(FU_DPAUX_DEVICE(device));
 	if (dev == NULL)
 		return FALSE;
-
-	/* open */
-	locker = fu_device_locker_new(dev, &error_local);
-	if (locker == NULL) {
-		if (g_error_matches(error_local, G_IO_ERROR, G_IO_ERROR_NOT_SUPPORTED) ||
-		    g_error_matches(error_local, FWUPD_ERROR, FWUPD_ERROR_READ)) {
-			g_debug("no device found: %s", error_local->message);
-			return TRUE;
-		}
-		g_propagate_error(error, g_steal_pointer(&error_local));
-		return FALSE;
-	}
-	fu_plugin_device_add(FU_PLUGIN(self), FU_DEVICE(dev));
+	fu_plugin_device_add(plugin, FU_DEVICE(dev));
 	return TRUE;
 }
 
@@ -62,6 +47,7 @@ fu_algoltek_aux_plugin_constructed(GObject *obj)
 	FuPlugin *plugin = FU_PLUGIN(obj);
 	fu_plugin_add_udev_subsystem(plugin, "drm"); /* used for uevent only */
 	fu_plugin_add_device_udev_subsystem(plugin, "drm_dp_aux_dev");
+	fu_plugin_add_device_gtype(plugin, FU_TYPE_ALGOLTEK_AUX_DEVICE);
 	fu_plugin_add_firmware_gtype(plugin, NULL, FU_TYPE_ALGOLTEK_AUX_FIRMWARE);
 }
 
