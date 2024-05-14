0eb06d9a65bb7207bad5f8e59d97b7d9fb35ce90
Sanjay Sheth <ssheth@logitech.com>
2024-05-14 11:51:30 -0700

    Incorporated code review feedback

--- plugins/logitech-rallysystem/fu-logitech-rallysystem-audio-device.c
+++ plugins/logitech-rallysystem/fu-logitech-rallysystem-audio-device.c
@@ -194,15 +194,13 @@ fu_logitech_rallysystem_audio_device_set_progress(FuDevice *self, FuProgress *pr
 static gchar *
 fu_logitech_rallysystem_audio_device_convert_version(FuDevice *device, guint64 version_raw)
 {
-	guint32 fwversion = 0;
 	guint8 major = 0;
 	guint8 minor = 0;
 	guint8 build = 0;
-	fwversion = version_raw;
-	major = (fwversion >> 16) & 0xFF;
-	minor = (fwversion >> 8) & 0xFF;
-	build = (fwversion >> 0) & 0xFF;
-	fwversion = ((major << 24) | (minor << 16) | 0 | (build << 0));
+	/*
+	 * device reports system version in 3 bytes: major.minor.build
+	 * convert major.minor.build -> major.minor.0.build
+	 */
 	major = (version_raw >> 16) & 0xFF;
 	minor = (version_raw >> 8) & 0xFF;
 	build = (version_raw >> 0) & 0xFF;
