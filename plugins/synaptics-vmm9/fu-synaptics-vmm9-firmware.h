3b546aa704419183f20f998514dab3a379cf5326
Richard Hughes <richard@hughsie.com>
2024-05-14 09:24:21 +0100

    synaptics-mst: Add support for VMM9xxx 'Carrera' devices

---
+++ plugins/synaptics-vmm9/fu-synaptics-vmm9-firmware.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2024 Richard hughes <Richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#pragma once
+
+#include <fwupdplugin.h>
+
+#define FU_TYPE_SYNAPTICS_VMM9_FIRMWARE (fu_synaptics_vmm9_firmware_get_type())
+G_DECLARE_FINAL_TYPE(FuSynapticsVmm9Firmware,
+		     fu_synaptics_vmm9_firmware,
+		     FU,
+		     SYNAPTICS_VMM9_FIRMWARE,
+		     FuFirmware)
+
+FuFirmware *
+fu_synaptics_vmm9_firmware_new(void);
+guint16
+fu_synaptics_vmm9_firmware_get_board_id(FuSynapticsVmm9Firmware *self);
---
+++ plugins/synaptics-vmm9/fu-synaptics-vmm9-plugin.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2024 Richard hughes <Richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#pragma once
+
+#include <fwupdplugin.h>
+
+G_DECLARE_FINAL_TYPE(FuSynapticsVmm9Plugin,
+		     fu_synaptics_vmm9_plugin,
+		     FU,
+		     SYNAPTICS_VMM9_PLUGIN,
+		     FuPlugin)
