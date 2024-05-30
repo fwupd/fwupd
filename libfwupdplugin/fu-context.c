fwupd/fwupd

commit b42ad1d450ae022e8b98709e92947c5c3a535b38

trivial: catch miscategorized ESP partitions
Fedora 38 appears to have set up an ESP within an md array that is
btrfs.  This isn't possible for a BIOS to use.

Reject this combination.

Fixes: #7277

plugins/uefi-capsule/fu-uefi-capsule-plugin.c
@@ -633,6 +633,7 @@ fu_uefi_capsule_plugin_get_default_esp(FuPlugin *plugin, GError **error)
 			guint score = 0;
 			g_autofree gchar *name = NULL;
 			g_autofree gchar *kind = NULL;
+			g_autofree gchar *id_type = NULL;
 			g_autoptr(FuDeviceLocker) locker = NULL;
 			g_autoptr(GError) error_local = NULL;
 
@@ -671,6 +672,13 @@ fu_uefi_capsule_plugin_get_default_esp(FuPlugin *plugin, GError **error)
 					}
 					continue;
 				}
+			}
+
+			/* in case an installer messes up the kind */
+			id_type = fu_volume_get_id_type(esp);
+			if (g_strcmp0(id_type, "btrfs") == 0) {
+				g_debug("skipping %s as it's a %s type", name, id_type);
+				continue;
 			}
 
 			/* big partitions are better than small partitions */

commit b8589de2a9c1d5c37fe856e450c0d415e7e17a12

trivial: switch to allowlist for partition kinds
A user has reported an md array that is an xfs kind that with the wrong
partition table flags.  Explicitly only allow vfat for ESPs.

Fixes: b42ad1d45 ("trivial: catch miscategorized ESP partitions")
Fixes https://github.com/fwupd/fwupd/issues/7298

libfwupdplugin/fu-context.c
@@ -1447,7 +1447,7 @@ fu_context_get_esp_volumes(FuContext *self, GError **error)
 		for (guint i = 0; i < volumes_esp->len; i++) {
 			FuVolume *vol = g_ptr_array_index(volumes_esp, i);
 			g_autofree gchar *type = fu_volume_get_id_type(vol);
-			if (g_strcmp0(type, "ext4") == 0)
+			if (g_strcmp0(type, "vfat") != 0)
 				continue;
 			fu_context_add_esp_volume(self, vol);
 		}

plugins/uefi-capsule/fu-uefi-capsule-plugin.c
@@ -633,7 +633,6 @@ fu_uefi_capsule_plugin_get_default_esp(FuPlugin *plugin, GError **error)
 			guint score = 0;
 			g_autofree gchar *name = NULL;
 			g_autofree gchar *kind = NULL;
-			g_autofree gchar *id_type = NULL;
 			g_autoptr(FuDeviceLocker) locker = NULL;
 			g_autoptr(GError) error_local = NULL;
 
@@ -672,13 +671,6 @@ fu_uefi_capsule_plugin_get_default_esp(FuPlugin *plugin, GError **error)
 					}
 					continue;
 				}
-			}
-
-			/* in case an installer messes up the kind */
-			id_type = fu_volume_get_id_type(esp);
-			if (g_strcmp0(id_type, "btrfs") == 0) {
-				g_debug("skipping %s as it's a %s type", name, id_type);
-				continue;
 			}
 
 			/* big partitions are better than small partitions */