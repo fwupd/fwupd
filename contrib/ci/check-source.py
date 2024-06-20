0cea5417c9cbb48c68236ea6ba4ca440554b0d7d
Richard Hughes <richard@hughsie.com>
2024-06-20 08:35:01 +0100

    trivial: Add checks for direct GUsb calls

--- contrib/ci/check-source.py
+++ contrib/ci/check-source.py
@@ -34,6 +34,22 @@ def test_files() -> int:
                     "cbor_get_uint32(": "Use cbor_get_int() instead",
                     "g_error(": "Use GError instead",
                     "g_byte_array_free_to_bytes(": "Use g_bytes_new() instead",
+                    "g_usb_device_bulk_transfer(": "Use fu_usb_device_bulk_transfer() instead",
+                    "g_usb_device_claim_interface(": "Use fu_usb_device_claim_interface() instead",
+                    "g_usb_device_control_transfer(": "Use fu_usb_device_control_transfer() instead",
+                    "g_usb_device_get_configuration_index(": "Use fu_usb_device_get_configuration_index() instead",
+                    "g_usb_device_get_custom_index(": "Use fu_usb_device_get_custom_index() instead",
+                    "g_usb_device_get_device_class(": "Use fu_usb_device_get_device_class() instead",
+                    "g_usb_device_get_interface(": "Use fu_usb_device_get_interface() instead",
+                    "g_usb_device_get_interfaces(": "Use fu_usb_device_get_interfaces() instead",
+                    "g_usb_device_get_release(": "Use fu_usb_device_get_release() instead",
+                    "g_usb_device_get_serial_number_index(": "Use fu_usb_device_get_serial_number_index() instead",
+                    "g_usb_device_get_string_descriptor_bytes_full(": "Use fu_usb_device_get_string_descriptor_bytes_full() instead",
+                    "g_usb_device_get_string_descriptor desc_index(": "Use fu_usb_device_get_string_descriptor desc_index() instead",
+                    "g_usb_device_interrupt_transfer(": "Use fu_usb_device_interrupt_transfer() instead",
+                    "g_usb_device_release_interface(": "Use fu_usb_device_release_interface() instead",
+                    "g_usb_device_reset error(": "Use fu_usb_device_reset error() instead",
+                    "g_usb_device_set_interface_alt(": "Use fu_usb_device_set_interface_alt() instead",
                 }.items():
                     if line.find(token) != -1:
                         print(
