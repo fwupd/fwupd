fwupd/fwupd

commit e9da9eca36a6bb8e53fc885d4ada60b8149f0984

Add the devtype to fu_udev_device_get_parent_with_subsystem() for future use
Longer term I'd like plugins to actually set `subsystem` and `devtype` in more
cases in case the sysfs tree structure changes.

libfwupdplugin/fu-udev-device.c
@@ -1273,22 +1273,6 @@ fu_udev_device_match_subsystem_devtype(GUdevDevice *udev_device,
 	}
 	return TRUE;
 }
-
-static GUdevDevice *
-fu_udev_device_get_parent_with_subsystem_devtype(GUdevDevice *udev_device,
-						 const gchar *subsystem,
-						 const gchar *devtype)
-{
-	g_autoptr(GUdevDevice) udev_device_tmp = g_object_ref(udev_device);
-	while (udev_device_tmp != NULL) {
-		g_autoptr(GUdevDevice) parent = NULL;
-		if (fu_udev_device_match_subsystem_devtype(udev_device_tmp, subsystem, devtype))
-			return g_object_ref(udev_device_tmp);
-		parent = g_udev_device_get_parent(udev_device_tmp);
-		g_set_object(&udev_device_tmp, parent);
-	}
-	return NULL;
-}
 #endif
 
 /**
@@ -1332,13 +1316,16 @@ fu_udev_device_set_physical_id(FuUdevDevice *self, const gchar *subsystems, GErr
 	split = g_strsplit(subsystems, ",", -1);
 	for (guint i = 0; split[i] != NULL; i++) {
 		g_auto(GStrv) subsys_devtype = g_strsplit(split[i], ":", 2);
+		g_autoptr(FuUdevDevice) device_parent = NULL;
 
 		/* matching on devtype is optional */
-		udev_device = fu_udev_device_get_parent_with_subsystem_devtype(priv->udev_device,
-									       subsys_devtype[0],
-									       subsys_devtype[1]);
-		if (udev_device != NULL) {
+		device_parent = fu_udev_device_get_parent_with_subsystem(self,
+									 subsys_devtype[0],
+									 subsys_devtype[1],
+									 NULL);
+		if (device_parent != NULL) {
 			subsystem = g_strdup(subsys_devtype[0]);
+			udev_device = g_object_ref(fu_udev_device_get_dev(device_parent));
 			break;
 		}
 	}
@@ -2244,17 +2231,21 @@ fu_udev_device_get_siblings_with_subsystem(FuUdevDevice *self,
 /**
  * fu_udev_device_get_parent_with_subsystem
  * @self: a #FuUdevDevice
- * @subsystem: (nullable): the name of a udev subsystem
+ * @subsystem: (nullable): the name of a udev subsystem, or %NULL for any
+ * @devtype: (nullable): the name of a udev devtype, e.g. `usb_interface`, or %NULL for any
  * @error: (nullable): optional return location for an error
  *
- * Get the device that is a parent of self and has the provided subsystem.
+ * Get the device that is a ancestor of self and has the provided subsystem and device type.
  *
  * Returns: (transfer full): device, or %NULL
  *
  * Since: 2.0.0
  */
 FuUdevDevice *
-fu_udev_device_get_parent_with_subsystem(FuUdevDevice *self, const gchar *subsystem, GError **error)
+fu_udev_device_get_parent_with_subsystem(FuUdevDevice *self,
+					 const gchar *subsystem,
+					 const gchar *devtype,
+					 GError **error)
 {
 #ifdef HAVE_GUDEV
 	FuUdevDevicePrivate *priv = GET_PRIVATE(self);
@@ -2265,13 +2256,24 @@ fu_udev_device_get_parent_with_subsystem(FuUdevDevice *self, const gchar *subsys
 		g_set_error_literal(error, FWUPD_ERROR, FWUPD_ERROR_NOT_FOUND, "not initialized");
 		return NULL;
 	}
-	if (subsystem == NULL) {
-		device_tmp = g_udev_device_get_parent(priv->udev_device);
-	} else {
-		device_tmp =
-		    g_udev_device_get_parent_with_subsystem(priv->udev_device, subsystem, NULL);
+	device_tmp = g_object_ref(priv->udev_device);
+	while (device_tmp != NULL) {
+		g_autoptr(GUdevDevice) parent = NULL;
+		if (fu_udev_device_match_subsystem_devtype(device_tmp, subsystem, devtype))
+			break;
+		parent = g_udev_device_get_parent(device_tmp);
+		g_set_object(&device_tmp, parent);
 	}
 	if (device_tmp == NULL) {
+		if (devtype != NULL) {
+			g_set_error(error,
+				    FWUPD_ERROR,
+				    FWUPD_ERROR_NOT_SUPPORTED,
+				    "no parent with subsystem %s and devtype %s",
+				    subsystem,
+				    devtype);
+			return NULL;
+		}
 		g_set_error(error,
 			    FWUPD_ERROR,
 			    FWUPD_ERROR_NOT_SUPPORTED,

libfwupdplugin/fu-drm-device.c
@@ -159,7 +159,10 @@ fu_drm_device_probe(FuDevice *device, GError **error)
 	}
 
 	/* set the parent */
-	parent = fu_udev_device_get_parent_with_subsystem(FU_UDEV_DEVICE(self), "pci", NULL);
+	parent = fu_udev_device_get_parent_with_subsystem(FU_UDEV_DEVICE(self),
+							  "pci",
+							  NULL, /* devtype */
+							  NULL);
 	if (parent != NULL)
 		fu_device_add_parent_backend_id(device, fu_udev_device_get_sysfs_path(parent));
 

libfwupdplugin/fu-mei-device.c
@@ -75,7 +75,10 @@ fu_mei_device_ensure_parent_device_file(FuMeiDevice *self, GError **error)
 	g_autoptr(GDir) dir = NULL;
 
 	/* get direct parent */
-	parent = fu_udev_device_get_parent_with_subsystem(FU_UDEV_DEVICE(self), NULL, error);
+	parent = fu_udev_device_get_parent_with_subsystem(FU_UDEV_DEVICE(self),
+							  NULL, /* subsystem */
+							  NULL, /* devtype */
+							  error);
 	if (parent == NULL)
 		return FALSE;
 

libfwupdplugin/fu-udev-device.h
@@ -196,8 +196,10 @@ GPtrArray *
 fu_udev_device_get_children_with_subsystem(FuUdevDevice *self, const gchar *subsystem)
     G_GNUC_NON_NULL(1, 2);
 FuUdevDevice *
-fu_udev_device_get_parent_with_subsystem(FuUdevDevice *self, const gchar *subsystem, GError **error)
-    G_GNUC_NON_NULL(1);
+fu_udev_device_get_parent_with_subsystem(FuUdevDevice *self,
+					 const gchar *subsystem,
+					 const gchar *devtype,
+					 GError **error) G_GNUC_NON_NULL(1);
 
 FuDevice *
 fu_udev_device_find_usb_device(FuUdevDevice *self, GError **error) G_GNUC_WARN_UNUSED_RESULT

plugins/elantp/fu-elantp-i2c-device.c
@@ -112,7 +112,10 @@ fu_elantp_i2c_device_probe(FuDevice *device, GError **error)
 	if (g_strcmp0(fu_udev_device_get_subsystem(FU_UDEV_DEVICE(device)), "i2c") == 0) {
 		g_autoptr(GPtrArray) i2c_buses = NULL;
 		FuUdevDevice *i2c_device =
-		    fu_udev_device_get_parent_with_subsystem(FU_UDEV_DEVICE(device), "i2c", error);
+		    fu_udev_device_get_parent_with_subsystem(FU_UDEV_DEVICE(device),
+							     "i2c",
+							     NULL, /* devtype */
+							     error);
 		if (i2c_device == NULL)
 			return FALSE;
 

plugins/intel-gsc/fu-igsc-device.c
@@ -761,7 +761,7 @@ fu_igsc_device_set_pci_power_policy(FuIgscDevice *self, const gchar *val, GError
 	g_autoptr(FuUdevDevice) parent = NULL;
 
 	/* get PCI parent */
-	parent = fu_udev_device_get_parent_with_subsystem(FU_UDEV_DEVICE(self), "pci", error);
+	parent = fu_udev_device_get_parent_with_subsystem(FU_UDEV_DEVICE(self), "pci", NULL, error);
 	if (parent == NULL)
 		return FALSE;
 	return fu_udev_device_write_sysfs(parent, "power/control", val, error);

plugins/mediatek-scaler/fu-mediatek-scaler-device.c
@@ -495,7 +495,7 @@ fu_mediatek_scaler_device_probe(FuDevice *device, GError **error)
 
 	/* set vid and pid from PCI bus */
 	udev_parent =
-	    fu_udev_device_get_parent_with_subsystem(FU_UDEV_DEVICE(device), "pci", error);
+	    fu_udev_device_get_parent_with_subsystem(FU_UDEV_DEVICE(device), "pci", NULL, error);
 	if (udev_parent == NULL)
 		return FALSE;
 	if (!fu_device_probe(FU_DEVICE(udev_parent), error))

plugins/pixart-rf/fu-pxi-receiver-device.c
@@ -901,7 +901,10 @@ fu_pxi_receiver_device_probe(FuDevice *device, GError **error)
 	g_autoptr(FuUdevDevice) usb_parent = NULL;
 
 	/* check USB interface number */
-	usb_parent = fu_udev_device_get_parent_with_subsystem(FU_UDEV_DEVICE(device), "usb", error);
+	usb_parent = fu_udev_device_get_parent_with_subsystem(FU_UDEV_DEVICE(device),
+							      "usb",
+							      NULL, /* devtype */
+							      error);
 	if (usb_parent == NULL)
 		return FALSE;
 	if (!fu_udev_device_get_sysfs_attr_uint64(usb_parent, "bInterfaceNumber", &iface_nr, error))

commit de969ee6910585f4fbdb34c9a933b6d6703e64a9

trivial: Never return the original device when the subsystem matches
This regressed in e9da9eca...

libfwupdplugin/fu-udev-device.c
@@ -2265,7 +2265,7 @@ fu_udev_device_get_parent_with_subsystem(FuUdevDevice *self,
 		g_set_error_literal(error, FWUPD_ERROR, FWUPD_ERROR_NOT_FOUND, "not initialized");
 		return NULL;
 	}
-	device_tmp = g_object_ref(priv->udev_device);
+	device_tmp = g_udev_device_get_parent(priv->udev_device);
 	while (device_tmp != NULL) {
 		g_autoptr(GUdevDevice) parent = NULL;
 		if (fu_udev_device_match_subsystem_devtype(device_tmp, subsystem, devtype))