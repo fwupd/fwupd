3214cde0d1f0534ec9c20c1c5fa5d86ae3505740
Richard Hughes <richard@hughsie.com>
2024-07-07 15:38:18 +0100

    kinetic-dp: Fix a reported integer overflow from Coverity

--- plugins/kinetic-dp/fu-kinetic-dp-puma-firmware.c
+++ plugins/kinetic-dp/fu-kinetic-dp-puma-firmware.c
@@ -255,6 +255,13 @@ fu_kinetic_dp_puma_firmware_parse(FuFirmware *firmware,
 	/* add App FW as a new image into firmware */
 	if (!fu_input_stream_size(stream, &streamsz, error))
 		return FALSE;
+	if (streamsz < HEADER_LEN_ISP_DRV_SIZE + isp_drv_size) {
+		g_set_error_literal(error,
+				    FWUPD_ERROR,
+				    FWUPD_ERROR_INVALID_FILE,
+				    "stream was too small");
+		return FALSE;
+	}
 	app_fw_size = streamsz - HEADER_LEN_ISP_DRV_SIZE - isp_drv_size;
 	app_fw_stream = fu_partial_input_stream_new(stream,
 						    HEADER_LEN_ISP_DRV_SIZE + isp_drv_size,
