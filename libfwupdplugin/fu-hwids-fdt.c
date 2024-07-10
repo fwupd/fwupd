fwupd/fwupd

commit 2b0f92506b51ffa230266b843e64b9230574da89

Refactor the hwids functionality
This refactors the code as it was getting very confusing; before FuSmbios was
reading both SMBIOS and the kernel-provided DT -- and various things were
injecting overrides in three different place. To properly support FDT remove
one layer of indirection.

This also lets us use the compatible strings to enable plugins specifying the
flag _REQUIRE_HWID -- which means we only load the plugin if it's got a chance
of working. e.g.

    [aspeed,ast2500]

libfwupdplugin/fu-hwids-fdt.c
@@ -1,0 +1,73 @@
+/*
+ * Copyright (C) 2023 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#define G_LOG_DOMAIN "FuContext"
+
+#include "config.h"
+
+#include "fu-context-private.h"
+#include "fu-fdt-firmware.h"
+#include "fu-hwids-private.h"
+
+gboolean
+fu_hwids_fdt_setup(FuContext *ctx, FuHwids *self, GError **error)
+{
+	g_auto(GStrv) compatible = NULL;
+	g_autoptr(FuFirmware) fdt_img = NULL;
+	g_autoptr(FuFdtImage) fdt_img_fwver = NULL;
+	g_autoptr(FuFirmware) fdt = NULL;
+	struct {
+		const gchar *hwid;
+		const gchar *key;
+	} map[] = {{FU_HWIDS_KEY_MANUFACTURER, "vendor"},
+		   {FU_HWIDS_KEY_FAMILY, "model-name"},
+		   {FU_HWIDS_KEY_PRODUCT_NAME, "model"},
+		   {NULL, NULL}};
+
+	/* adds compatible GUIDs */
+	fdt = fu_context_get_fdt(ctx, error);
+	if (fdt == NULL)
+		return FALSE;
+	fdt_img = fu_firmware_get_image_by_id(fdt, NULL, error);
+	if (fdt_img == NULL)
+		return FALSE;
+	if (!fu_fdt_image_get_attr_strlist(FU_FDT_IMAGE(fdt_img), "compatible", &compatible, error))
+		return FALSE;
+	for (guint i = 0; compatible[i] != NULL; i++) {
+		g_autofree gchar *guid = fwupd_guid_hash_string(compatible[i]);
+		g_debug("using %s for DT compatible %s", guid, compatible[i]);
+		fu_hwids_add_guid(self, guid);
+	}
+
+	/* root node */
+	for (guint i = 0; map[i].key != NULL; i++) {
+		g_autofree gchar *tmp = NULL;
+		fu_fdt_image_get_attr_str(FU_FDT_IMAGE(fdt_img), map[i].key, &tmp, NULL);
+		fu_hwids_add_value(self, map[i].hwid, tmp);
+	}
+
+	/* fallback */
+	if (g_strv_length(compatible) > 0)
+		fu_hwids_add_value(self, FU_HWIDS_KEY_MANUFACTURER, compatible[0]);
+	if (g_strv_length(compatible) > 1)
+		fu_hwids_add_value(self, FU_HWIDS_KEY_PRODUCT_NAME, compatible[1]);
+	if (g_strv_length(compatible) > 2)
+		fu_hwids_add_value(self, FU_HWIDS_KEY_FAMILY, compatible[2]);
+	if (fu_context_get_chassis_kind(ctx) == FU_SMBIOS_CHASSIS_KIND_UNKNOWN) {
+		if (fu_fdt_image_get_attr_str(FU_FDT_IMAGE(fdt_img), "battery", NULL, NULL))
+			fu_context_set_chassis_kind(ctx, FU_SMBIOS_CHASSIS_KIND_PORTABLE);
+	}
+	fdt_img_fwver =
+	    fu_fdt_firmware_get_image_by_path(FU_FDT_FIRMWARE(fdt), "ibm,firmware-versions", NULL);
+	if (fdt_img_fwver != NULL) {
+		g_autofree gchar *version = NULL;
+		fu_fdt_image_get_attr_str(FU_FDT_IMAGE(fdt_img), "version", &version, NULL);
+		fu_hwids_add_value(self, FU_HWIDS_KEY_BIOS_VERSION, version);
+	}
+
+	/* success */
+	return TRUE;
+}

contrib/fwupd.spec.in
@@ -463,7 +463,8 @@ done
 %if 0%{?have_uefi}
 %{_datadir}/installed-tests/fwupd/efi
 %endif
-%{_datadir}/installed-tests/fwupd/dmi
+%{_datadir}/installed-tests/fwupd/chassis_type
+%{_datadir}/installed-tests/fwupd/sys_vendor
 %{_datadir}/fwupd/device-tests/*.json
 %{_libexecdir}/installed-tests/fwupd/*
 %dir %{_sysconfdir}/fwupd/remotes.d

docs/fwupdplugin.toml.in
@@ -46,6 +46,7 @@ content_files = [
   "tutorial.md",
   "hsi.md",
   "ds20.md",
+  "hwids.md",
   "bios-settings.md",
   @plugin_readme_outputs@,
 ]

docs/index.html
@@ -46,6 +46,9 @@
       <li><a href="libfwupdplugin/tutorial.html">Plugin tutorial</a></li>
     </ul>
     <ul>
+      <li><a href="libfwupdplugin/hwids.html">Hardware IDs</a></li>
+    </ul>
+    <ul>
       <li><a href="libfwupdplugin/env.html">Environment variables</a></li>
     </ul>
     <header>

libfwupdplugin/fu-context.c
@@ -11,7 +11,7 @@
 #include "fu-bios-settings-private.h"
 #include "fu-context-private.h"
 #include "fu-fdt-firmware.h"
-#include "fu-hwids.h"
+#include "fu-hwids-private.h"
 #include "fu-path.h"
 #include "fu-smbios-private.h"
 #include "fu-volume-private.h"
@@ -455,8 +455,11 @@ gchar *
 fu_context_get_hwid_replace_value(FuContext *self, const gchar *keys, GError **error)
 {
 	FuContextPrivate *priv = GET_PRIVATE(self);
+
 	g_return_val_if_fail(FU_IS_CONTEXT(self), NULL);
 	g_return_val_if_fail(keys != NULL, NULL);
+	g_return_val_if_fail(error == NULL || *error == NULL, NULL);
+
 	if (!priv->loaded_hwinfo) {
 		g_critical("cannot use HWIDs before calling ->load_hwinfo()");
 		g_set_error_literal(error, G_IO_ERROR, G_IO_ERROR_NOT_INITIALIZED, "no data");
@@ -786,21 +789,64 @@ fu_context_load_hwinfo(FuContext *self, FuContextHwidFlags flags, GError **error
 	g_return_val_if_fail(FU_IS_CONTEXT(self), FALSE);
 	g_return_val_if_fail(error == NULL || *error == NULL, FALSE);
 
+	if ((flags & FU_CONTEXT_HWID_FLAG_LOAD_CONFIG) > 0) {
+		g_autoptr(GError) error_local = NULL;
+		if (!fu_hwids_config_setup(self, priv->hwids, &error_local)) {
+			if (!g_error_matches(error_local, FWUPD_ERROR, FWUPD_ERROR_NOT_SUPPORTED)) {
+				g_propagate_prefixed_error(error,
+							   g_steal_pointer(&error_local),
+							   "Failed to load HWIDs config: ");
+				return FALSE;
+			}
+		}
+	}
+	if ((flags & FU_CONTEXT_HWID_FLAG_LOAD_DMI) > 0) {
+		g_autoptr(GError) error_local = NULL;
+		if (!fu_hwids_dmi_setup(self, priv->hwids, &error_local)) {
+			if (!g_error_matches(error_local, FWUPD_ERROR, FWUPD_ERROR_NOT_SUPPORTED)) {
+				g_propagate_prefixed_error(error,
+							   g_steal_pointer(&error_local),
+							   "Failed to load HWIDs DMI: ");
+				return FALSE;
+			}
+		}
+	}
+	if ((flags & FU_CONTEXT_HWID_FLAG_LOAD_FDT) > 0) {
+		g_autoptr(GError) error_local = NULL;
+		if (!fu_hwids_fdt_setup(self, priv->hwids, &error_local)) {
+			if (!g_error_matches(error_local, FWUPD_ERROR, FWUPD_ERROR_NOT_SUPPORTED)) {
+				g_propagate_prefixed_error(error,
+							   g_steal_pointer(&error_local),
+							   "Failed to load HWIDs FDT: ");
+				return FALSE;
+			}
+		}
+	}
+	if ((flags & FU_CONTEXT_HWID_FLAG_LOAD_KENV) > 0) {
+		g_autoptr(GError) error_local = NULL;
+		if (!fu_hwids_kenv_setup(self, priv->hwids, &error_local)) {
+			if (!g_error_matches(error_local, FWUPD_ERROR, FWUPD_ERROR_NOT_SUPPORTED)) {
+				g_propagate_prefixed_error(error,
+							   g_steal_pointer(&error_local),
+							   "Failed to load HWIDs kenv: ");
+				return FALSE;
+			}
+		}
+	}
 	if ((flags & FU_CONTEXT_HWID_FLAG_LOAD_SMBIOS) > 0) {
 		g_autoptr(GError) error_local = NULL;
-		if (!fu_smbios_setup(priv->smbios, &error_local)) {
-			if (!g_error_matches(error_local, FWUPD_ERROR, FWUPD_ERROR_NOT_SUPPORTED))
-				g_warning("Failed to load SMBIOS: %s", error_local->message);
+		if (!fu_hwids_smbios_setup(self, priv->hwids, &error_local)) {
+			if (!g_error_matches(error_local, FWUPD_ERROR, FWUPD_ERROR_NOT_SUPPORTED)) {
+				g_propagate_prefixed_error(error,
+							   g_steal_pointer(&error_local),
+							   "Failed to load SMBIOS: ");
+				return FALSE;
+			}
 		}
-		fu_context_set_chassis_kind(self,
-					    fu_smbios_get_integer(priv->smbios,
-								  FU_SMBIOS_STRUCTURE_TYPE_CHASSIS,
-								  0x05,
-								  NULL));
 	}
-	if (!fu_hwids_setup(priv->hwids, priv->smbios, &error_hwids))
-		g_warning("Failed to load HWIDs: %s", error_hwids->message);
 	priv->loaded_hwinfo = TRUE;
+	if (!fu_hwids_setup(priv->hwids, &error_hwids))
+		g_warning("Failed to load HWIDs: %s", error_hwids->message);
 
 	/* set the hwid flags */
 	guids = fu_context_get_hwid_guids(self);

libfwupdplugin/fu-hwids-config.c
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2021 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#define G_LOG_DOMAIN "FuContext"
+
+#include "config.h"
+
+#include "fu-context-private.h"
+#include "fu-hwids-private.h"
+#include "fu-path.h"
+
+gboolean
+fu_hwids_config_setup(FuContext *ctx, FuHwids *self, GError **error)
+{
+	g_autofree gchar *localstatedir = fu_path_from_kind(FU_PATH_KIND_LOCALSTATEDIR_PKG);
+	g_autofree gchar *sysconfigdir = fu_path_from_kind(FU_PATH_KIND_SYSCONFDIR_PKG);
+	g_autoptr(GKeyFile) kf = g_key_file_new();
+	g_autoptr(GPtrArray) fns = g_ptr_array_new_with_free_func(g_free);
+	g_autoptr(GPtrArray) keys = fu_hwids_get_keys(self);
+
+	/* per-system configuration and optional overrides */
+	g_ptr_array_add(fns, g_build_filename(sysconfigdir, "daemon.conf", NULL));
+	g_ptr_array_add(fns, g_build_filename(localstatedir, "daemon.conf", NULL));
+	for (guint i = 0; i < fns->len; i++) {
+		const gchar *fn = g_ptr_array_index(fns, i);
+		if (g_file_test(fn, G_FILE_TEST_EXISTS)) {
+			g_debug("loading HwId overrides from %s", fn);
+			if (!g_key_file_load_from_file(kf, fn, G_KEY_FILE_NONE, error))
+				return FALSE;
+		} else {
+			g_debug("not loading HwId overrides from %s", fn);
+		}
+	}
+
+	/* all keys are optional */
+	for (guint i = 0; i < keys->len; i++) {
+		const gchar *key = g_ptr_array_index(keys, i);
+		g_autofree gchar *value = g_key_file_get_string(kf, "fwupd", key, NULL);
+		if (value != NULL)
+			fu_hwids_add_value(self, key, value);
+	}
+
+	/* success */
+	return TRUE;
+}

libfwupdplugin/fu-hwids-dmi.c
@@ -1,0 +1,71 @@
+/*
+ * Copyright (C) 2021 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#define G_LOG_DOMAIN "FuContext"
+
+#include "config.h"
+
+#include "fu-context-private.h"
+#include "fu-hwids-private.h"
+#include "fu-path.h"
+#include "fu-string.h"
+
+gboolean
+fu_hwids_dmi_setup(FuContext *ctx, FuHwids *self, GError **error)
+{
+	g_autofree gchar *path = fu_path_from_kind(FU_PATH_KIND_SYSFSDIR_DMI);
+	struct {
+		const gchar *hwid;
+		const gchar *key;
+	} map[] = {{FU_HWIDS_KEY_BASEBOARD_MANUFACTURER, "board_vendor"},
+		   {FU_HWIDS_KEY_BASEBOARD_PRODUCT, "board_name"},
+		   {FU_HWIDS_KEY_BIOS_VENDOR, "bios_vendor"},
+		   {FU_HWIDS_KEY_BIOS_VERSION, "bios_version"},
+		   {FU_HWIDS_KEY_FAMILY, "product_family"},
+		   {FU_HWIDS_KEY_MANUFACTURER, "sys_vendor"},
+		   {FU_HWIDS_KEY_PRODUCT_NAME, "product_name"},
+		   {FU_HWIDS_KEY_PRODUCT_SKU, "product_sku"},
+		   {FU_HWIDS_KEY_ENCLOSURE_KIND, "chassis_type"},
+		   {NULL, NULL}};
+
+	/* the values the kernel parsed; these are world-readable */
+	if (!g_file_test(path, G_FILE_TEST_IS_DIR)) {
+		g_set_error(error, FWUPD_ERROR, FWUPD_ERROR_NOT_SUPPORTED, "no %s", path);
+		return FALSE;
+	}
+	for (guint i = 0; map[i].key != NULL; i++) {
+		gsize bufsz = 0;
+		g_autofree gchar *buf = NULL;
+		g_autofree gchar *fn = g_build_filename(path, map[i].key, NULL);
+		g_autoptr(GError) error_local = NULL;
+
+		if (!g_file_get_contents(fn, &buf, &bufsz, &error_local)) {
+			g_debug("unable to read SMBIOS data from %s: %s", fn, error_local->message);
+			continue;
+		}
+
+		/* trim trailing newline added by kernel */
+		if (buf[bufsz - 1] == '\n')
+			buf[bufsz - 1] = 0;
+		fu_hwids_add_value(self, map[i].hwid, buf);
+
+		if (g_strcmp0(map[i].hwid, FU_HWIDS_KEY_ENCLOSURE_KIND) == 0) {
+			guint64 val = 0;
+			if (!fu_strtoull(buf,
+					 &val,
+					 FU_SMBIOS_CHASSIS_KIND_OTHER,
+					 FU_SMBIOS_CHASSIS_KIND_LAST,
+					 &error_local)) {
+				g_warning("ignoring enclosure kind %s", buf);
+				continue;
+			}
+			fu_context_set_chassis_kind(ctx, val);
+		}
+	}
+
+	/* success */
+	return TRUE;
+}

libfwupdplugin/fu-hwids-kenv.c
@@ -1,0 +1,44 @@
+/*
+ * Copyright (C) 2021 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1+
+ */
+
+#define G_LOG_DOMAIN "FuContext"
+
+#include "config.h"
+
+#include "fu-context-private.h"
+#include "fu-hwids-private.h"
+#include "fu-kenv.h"
+
+gboolean
+fu_hwids_kenv_setup(FuContext *ctx, FuHwids *self, GError **error)
+{
+#ifdef HAVE_KENV_H
+	struct {
+		const gchar *hwid;
+		const gchar *key;
+	} map[] = {{FU_HWIDS_KEY_BASEBOARD_MANUFACTURER, "smbios.planar.maker"},
+		   {FU_HWIDS_KEY_BASEBOARD_PRODUCT, "smbios.planar.product"},
+		   {FU_HWIDS_KEY_BIOS_VENDOR, "smbios.bios.vendor"},
+		   {FU_HWIDS_KEY_BIOS_VERSION, "smbios.bios.version"},
+		   {FU_HWIDS_KEY_FAMILY, "smbios.system.family"},
+		   {FU_HWIDS_KEY_MANUFACTURER, "smbios.system.maker"},
+		   {FU_HWIDS_KEY_PRODUCT_NAME, "smbios.system.product"},
+		   {FU_HWIDS_KEY_PRODUCT_SKU, "smbios.system.sku"},
+		   {{NULL, NULL}}};
+	for (guint i = 0; map[i].key != NULL; i++) {
+		g_autoptr(GError) error_local = NULL;
+		g_autofree gchar *value = fu_kenv_get_string(map[i].key, error_local);
+		if (value == NULL) {
+			g_debug("ignoring: %s", error_local->message);
+			continue;
+		}
+		fu_hwids_add_value(self, map[i].hwid, value);
+	}
+#endif
+
+	/* success */
+	return TRUE;
+}

commit 48c48b1d6cafc9a346069c59f20c2e51567d8163

Set the Blackbird and Talos II baseboard details again
This regressed in 2b0f925.

Fixes https://github.com/fwupd/fwupd/issues/7437

libfwupdplugin/fu-hwids-fdt.c
@@ -18,6 +18,7 @@ fu_hwids_fdt_setup(FuContext *ctx, FuHwids *self, GError **error)
 	g_autofree gchar *chassis_type = NULL;
 	g_auto(GStrv) compatible = NULL;
 	g_autoptr(FuFirmware) fdt_img = NULL;
+	g_autoptr(FuFdtImage) fdt_img_baseb = NULL;
 	g_autoptr(FuFdtImage) fdt_img_fwver = NULL;
 	g_autoptr(FuFirmware) fdt = NULL;
 	struct {
@@ -91,6 +92,23 @@ fu_hwids_fdt_setup(FuContext *ctx, FuHwids *self, GError **error)
 		g_autofree gchar *version = NULL;
 		fu_fdt_image_get_attr_str(FU_FDT_IMAGE(fdt_img), "version", &version, NULL);
 		fu_hwids_add_value(self, FU_HWIDS_KEY_BIOS_VERSION, version);
+	}
+	fdt_img_baseb = fu_fdt_firmware_get_image_by_path(
+	    FU_FDT_FIRMWARE(fdt),
+	    "/vpd/root-node-vpd@a000/enclosure@1e00/backplane@800",
+	    NULL);
+	if (fdt_img_baseb != NULL) {
+		g_autofree gchar *vendor = NULL;
+		g_autofree gchar *product = NULL;
+		fu_fdt_image_get_attr_str(FU_FDT_IMAGE(fdt_img_baseb), "vendor", &vendor, NULL);
+		fu_fdt_image_get_attr_str(FU_FDT_IMAGE(fdt_img_baseb),
+					  "part-number",
+					  &product,
+					  NULL);
+		if (vendor != NULL)
+			fu_hwids_add_value(self, FU_HWIDS_KEY_BASEBOARD_MANUFACTURER, vendor);
+		if (product != NULL)
+			fu_hwids_add_value(self, FU_HWIDS_KEY_BASEBOARD_PRODUCT, product);
 	}
 
 	/* success */