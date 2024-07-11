eaf752c75f4ad3dd674a708c257c1f51ee1296ea
Richard Hughes <richard@hughsie.com>
2024-07-11 17:19:06 +0100

    logitech-bulkcontroller: Add support for Logitech Sight
    The JSON blob is as follows:
    
    	{
    	  "id" : "3ea70281-f11c-4718-a72e-b8ec32285b75",
    	  "type" : 2,
    	  "payload" : {
    		"devices" : [
    		  {
    			"av" : "1.0.763",
    			"bootStatus" : 0,
    			"hkhw" : "170.170.48059",
    			"hkv" : "1.1.79",
    			"hw" : "4.4",
    			"make" : "Logitech",
    			"manifest" : "10.12.556",
    			"model" : "VR0019",
    			"name" : "Rally Bar",
    			"os" : "Logi CollabOS",
    			"osv" : "0.917.642",
    			"pid" : "0x899",
    			"pthw" : "16.0.0",
    			"ptv" : "1.0.80",
    			"serial" : "XXXXXXXXXXXX",
    			"status" : 2,
    			"sw" : "1.9.556",
    			"type" : "HostedKong",
    			"updateStatus" : 0,
    			"vid" : "0x046d",
    			"zfhw" : "16.0.0",
    			"zfv" : "1.0.79"
    		  },
    		  {
    			"av" : "0.901.50",
    			"bootStatus" : 0,
    			"hw" : "mt8195.4.0",
    			"make" : "Logitech",
    			"manifest" : "0.0.0",
    			"model" : "VU0067",
    			"name" : "Sight",
    			"os" : "Logi CollabOS",
    			"osv" : "0.901.50",
    			"serial" : "XXXXXXXXXXXX",
    			"sw" : "0.5.0",
    			"type" : "Sentinel",
    			"updateStatus" : 0,
    			"vid" : "0x046d",
    			"vv" : "0.901.50",
    			"uid" : "2325LZ51XY58",
    			"status" : 1
    		  }
    		]
    	  }
    	}

---
+++ plugins/logitech-bulkcontroller/fu-logitech-bulkcontroller-child.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2024 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#pragma once
+
+#include <fwupdplugin.h>
+
+#define FU_TYPE_LOGITECH_BULKCONTROLLER_CHILD (fu_logitech_bulkcontroller_child_get_type())
+G_DECLARE_FINAL_TYPE(FuLogitechBulkcontrollerChild,
+		     fu_logitech_bulkcontroller_child,
+		     FU,
+		     LOGITECH_BULKCONTROLLER_CHILD,
+		     FuDevice)
