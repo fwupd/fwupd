c189caaabd55946ed73810d764b945b96821a58e
Richard Hughes <richard@hughsie.com>
2024-07-11 12:03:27 +0100

    algoltek-aux: Only do RDV on devices that *could* be Algoltek devices

--- plugins/algoltek-aux/fu-algoltek-aux-plugin.c
+++ plugins/algoltek-aux/fu-algoltek-aux-plugin.c
@@ -16,26 +16,6 @@ struct _FuAlgoltekAuxPlugin {
 
 G_DEFINE_TYPE(FuAlgoltekAuxPlugin, fu_algoltek_aux_plugin, FU_TYPE_PLUGIN)
 
-static gboolean
-fu_algoltek_aux_plugin_backend_device_added(FuPlugin *plugin,
-					    FuDevice *device,
-					    FuProgress *progress,
-					    GError **error)
-{
-	g_autoptr(FuAlgoltekAuxDevice) dev = NULL;
-
-	/* interesting device? */
-	if (!FU_IS_DPAUX_DEVICE(device))
-		return TRUE;
-
-	/* instantiate a new device */
-	dev = fu_algoltek_aux_device_new(FU_DPAUX_DEVICE(device));
-	if (dev == NULL)
-		return FALSE;
-	fu_plugin_device_add(plugin, FU_DEVICE(dev));
-	return TRUE;
-}
-
 static void
 fu_algoltek_aux_plugin_init(FuAlgoltekAuxPlugin *self)
 {
@@ -46,7 +26,7 @@ fu_algoltek_aux_plugin_constructed(GObject *obj)
 {
 	FuPlugin *plugin = FU_PLUGIN(obj);
 	fu_plugin_add_udev_subsystem(plugin, "drm"); /* used for uevent only */
-	fu_plugin_add_device_udev_subsystem(plugin, "drm_dp_aux_dev");
+	fu_plugin_add_udev_subsystem(plugin, "drm_dp_aux_dev");
 	fu_plugin_add_device_gtype(plugin, FU_TYPE_ALGOLTEK_AUX_DEVICE);
 	fu_plugin_add_firmware_gtype(plugin, NULL, FU_TYPE_ALGOLTEK_AUX_FIRMWARE);
 }
@@ -56,5 +36,4 @@ fu_algoltek_aux_plugin_class_init(FuAlgoltekAuxPluginClass *klass)
 {
 	FuPluginClass *plugin_class = FU_PLUGIN_CLASS(klass);
 	plugin_class->constructed = fu_algoltek_aux_plugin_constructed;
-	plugin_class->backend_device_added = fu_algoltek_aux_plugin_backend_device_added;
 }
