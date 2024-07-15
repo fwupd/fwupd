b2714e29d3fc21234b7b9e87a64d746ba523574d
Denis Pynkin <denis.pynkin@collabora.com>
2024-07-15 18:44:05 +0200

    steelseries: initial support of Nova 5 headset

--- plugins/steelseries/fu-steelseries-plugin.c
+++ plugins/steelseries/fu-steelseries-plugin.c
@@ -8,6 +8,7 @@
 #include "config.h"
 
 #include "fu-steelseries-fizz-gen1.h"
+#include "fu-steelseries-fizz-gen2.h"
 #include "fu-steelseries-fizz-hid.h"
 #include "fu-steelseries-fizz-tunnel.h"
 #include "fu-steelseries-fizz.h"
@@ -31,8 +32,11 @@ static void
 fu_steelseries_plugin_constructed(GObject *obj)
 {
 	FuPlugin *plugin = FU_PLUGIN(obj);
+	FuContext *ctx = fu_plugin_get_context(plugin);
+	fu_context_add_quirk_key(ctx, "SteelSeriesFizzInterface");
 	fu_plugin_add_device_gtype(plugin, FU_TYPE_STEELSERIES_FIZZ);
 	fu_plugin_add_device_gtype(plugin, FU_TYPE_STEELSERIES_FIZZ_GEN1);
+	fu_plugin_add_device_gtype(plugin, FU_TYPE_STEELSERIES_FIZZ_GEN2);
 	fu_plugin_add_device_gtype(plugin, FU_TYPE_STEELSERIES_FIZZ_HID);
 	fu_plugin_add_device_gtype(plugin, FU_TYPE_STEELSERIES_FIZZ_TUNNEL);
 	fu_plugin_add_device_gtype(plugin, FU_TYPE_STEELSERIES_GAMEPAD);
