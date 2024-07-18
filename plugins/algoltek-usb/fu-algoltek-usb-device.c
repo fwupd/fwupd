fwupd/fwupd

commit bf223e2ddea4c598ed24ca6c6b864480751c0280

algoltek-usb: Fix untrusted value as argument
Spotted by Coverity.

plugins/algoltek-usb/fu-algoltek-usb-device.c
@@ -151,6 +151,15 @@ fu_algoltek_usb_device_rst(FuAlgoltekUsbDevice *self, guint16 address, GError **
 	fu_struct_algoltek_cmd_address_pkt_set_address(st, address);
 	fu_struct_algoltek_cmd_address_pkt_set_checksum(st, ~fu_sum8(st->data, st->len) + 1);
 
+	if (st->data[0] < st->len) {
+		g_set_error(error,
+			    FWUPD_ERROR,
+			    FWUPD_ERROR_INVALID_DATA,
+			    "rst length invalid, 0x%x < 0x%x",
+			    st->data[0],
+			    st->len);
+		return FALSE;
+	}
 	if (!fu_algoltek_usb_device_ctrl_transfer(self,
 						  FU_USB_DIRECTION_HOST_TO_DEVICE,
 						  FU_ALGOLTEK_CMD_RST,
@@ -177,6 +186,15 @@ fu_algoltek_usb_device_wrr(FuAlgoltekUsbDevice *self, int address, int value, GE
 	fu_struct_algoltek_cmd_address_pkt_set_value(st, value);
 	fu_struct_algoltek_cmd_address_pkt_set_checksum(st, ~fu_sum8(st->data, st->len) + 1);
 
+	if (st->data[0] < st->len) {
+		g_set_error(error,
+			    FWUPD_ERROR,
+			    FWUPD_ERROR_INVALID_DATA,
+			    "wrr length invalid, 0x%x < 0x%x",
+			    st->data[0],
+			    st->len);
+		return FALSE;
+	}
 	if (!fu_algoltek_usb_device_ctrl_transfer(self,
 						  FU_USB_DIRECTION_HOST_TO_DEVICE,
 						  FU_ALGOLTEK_CMD_WRR,
@@ -229,7 +247,15 @@ fu_algoltek_usb_device_isp(FuAlgoltekUsbDevice *self,
 		}
 		fu_struct_algoltek_cmd_transfer_pkt_set_checksum(st,
 								 ~fu_sum8(st->data, st->len) + 1);
-
+		if (st->data[0] < st->len) {
+			g_set_error(error,
+				    FWUPD_ERROR,
+				    FWUPD_ERROR_INVALID_DATA,
+				    "isp length invalid, 0x%x < 0x%x",
+				    st->data[0],
+				    st->len);
+			return FALSE;
+		}
 		if (!fu_algoltek_usb_device_ctrl_transfer(self,
 							  FU_USB_DIRECTION_HOST_TO_DEVICE,
 							  FU_ALGOLTEK_CMD_ISP,
@@ -257,6 +283,15 @@ fu_algoltek_usb_device_bot(FuAlgoltekUsbDevice *self, int address, GError **erro
 	fu_struct_algoltek_cmd_address_pkt_set_address(st, address);
 	fu_struct_algoltek_cmd_address_pkt_set_checksum(st, ~fu_sum8(st->data, st->len) + 1);
 
+	if (st->data[0] < st->len) {
+		g_set_error(error,
+			    FWUPD_ERROR,
+			    FWUPD_ERROR_INVALID_DATA,
+			    "bot length invalid, 0x%x < 0x%x",
+			    st->data[0],
+			    st->len);
+		return FALSE;
+	}
 	if (!fu_algoltek_usb_device_ctrl_transfer(self,
 						  FU_USB_DIRECTION_HOST_TO_DEVICE,
 						  FU_ALGOLTEK_CMD_BOT,

commit 5558fd912cc6be6b1c3bde28c9c58a0d2755c715

algoltek-usb: Fix untrusted value as argument, harder
This regressed in bf223e2ddea4c598ed24ca6c6b864480751c0280.

plugins/algoltek-usb/fu-algoltek-usb-device.c
@@ -151,11 +151,11 @@ fu_algoltek_usb_device_rst(FuAlgoltekUsbDevice *self, guint16 address, GError **
 	fu_struct_algoltek_cmd_address_pkt_set_address(st, address);
 	fu_struct_algoltek_cmd_address_pkt_set_checksum(st, ~fu_sum8(st->data, st->len) + 1);
 
-	if (st->data[0] < st->len) {
+	if (st->data[0] > st->len) {
 		g_set_error(error,
 			    FWUPD_ERROR,
 			    FWUPD_ERROR_INVALID_DATA,
-			    "rst length invalid, 0x%x < 0x%x",
+			    "rst length invalid, 0x%x > 0x%x",
 			    st->data[0],
 			    st->len);
 		return FALSE;
@@ -186,11 +186,11 @@ fu_algoltek_usb_device_wrr(FuAlgoltekUsbDevice *self, int address, int value, GE
 	fu_struct_algoltek_cmd_address_pkt_set_value(st, value);
 	fu_struct_algoltek_cmd_address_pkt_set_checksum(st, ~fu_sum8(st->data, st->len) + 1);
 
-	if (st->data[0] < st->len) {
+	if (st->data[0] > st->len) {
 		g_set_error(error,
 			    FWUPD_ERROR,
 			    FWUPD_ERROR_INVALID_DATA,
-			    "wrr length invalid, 0x%x < 0x%x",
+			    "wrr length invalid, 0x%x > 0x%x",
 			    st->data[0],
 			    st->len);
 		return FALSE;
@@ -247,11 +247,11 @@ fu_algoltek_usb_device_isp(FuAlgoltekUsbDevice *self,
 		}
 		fu_struct_algoltek_cmd_transfer_pkt_set_checksum(st,
 								 ~fu_sum8(st->data, st->len) + 1);
-		if (st->data[0] < st->len) {
+		if (st->data[0] > st->len) {
 			g_set_error(error,
 				    FWUPD_ERROR,
 				    FWUPD_ERROR_INVALID_DATA,
-				    "isp length invalid, 0x%x < 0x%x",
+				    "isp length invalid, 0x%x > 0x%x",
 				    st->data[0],
 				    st->len);
 			return FALSE;
@@ -283,11 +283,11 @@ fu_algoltek_usb_device_bot(FuAlgoltekUsbDevice *self, int address, GError **erro
 	fu_struct_algoltek_cmd_address_pkt_set_address(st, address);
 	fu_struct_algoltek_cmd_address_pkt_set_checksum(st, ~fu_sum8(st->data, st->len) + 1);
 
-	if (st->data[0] < st->len) {
+	if (st->data[0] > st->len) {
 		g_set_error(error,
 			    FWUPD_ERROR,
 			    FWUPD_ERROR_INVALID_DATA,
-			    "bot length invalid, 0x%x < 0x%x",
+			    "bot length invalid, 0x%x > 0x%x",
 			    st->data[0],
 			    st->len);
 		return FALSE;