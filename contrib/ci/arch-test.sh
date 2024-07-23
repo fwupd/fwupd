febe6cbb70cc07946e763560a0a08f85cad83c34
Richard Hughes <richard@hughsie.com>
2024-07-23 15:02:14 +0100

    trivial: Start polkitd manually in the arch CI tests

--- contrib/ci/arch-test.sh
+++ contrib/ci/arch-test.sh
@@ -14,6 +14,9 @@ cp fwupd-test-firmware/installed-tests/* /usr/share/installed-tests/fwupd/ -LRv
 # gnome-desktop-testing is missing, so manually run these tests
 export G_TEST_SRCDIR=/usr/share/installed-tests/fwupd G_TEST_BUILDDIR=/usr/share/installed-tests/fwupd
 /usr/bin/dbus-daemon --system
+/usr/lib/polkit-1/polkitd &
+sleep 5
+
 fwupdtool enable-test-devices
 /usr/lib/fwupd/fwupd --verbose &
 sleep 10
