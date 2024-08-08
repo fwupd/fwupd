a4723ecb160da3df53b0e107864ebe1f98fdb068
Richard Hughes <richard@hughsie.com>
2024-08-08 11:31:45 +0100

    Fix a thinko when recording emulation of USB devices
    We want to record the emaultion of the superclass device, not the backend
    device -- i.e. we need to capture the plugin-specific setup() actions, not just
    the generic backend ones...

---
+++ libfwupdplugin/fu-backend-private.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2024 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#pragma once
+
+#include "fu-backend.h"
+
+gchar *
+fu_backend_get_emulation_array_member_name(FuBackend *self);
