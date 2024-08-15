66aafd0cc54f93fa7b2febae0b2cab96c9c85b24
Richard Hughes <richard@hughsie.com>
2024-08-15 16:35:30 +0100

    Read from the input stream when loading archives
    This saves 64MB of RSS when loading the AVER emulation data.

--- plugins/aver-hid/fu-aver-safeisp-device.c
+++ plugins/aver-hid/fu-aver-safeisp-device.c
@@ -269,8 +269,8 @@ fu_aver_safeisp_device_write_firmware(FuDevice *device,
 	g_autoptr(FuArchive) archive = NULL;
 	g_autoptr(FuChunkArray) chunks = NULL;
 	g_autoptr(GBytes) cx3_fw = NULL;
-	g_autoptr(GBytes) fw = NULL;
 	g_autoptr(GBytes) m12_fw = NULL;
+	g_autoptr(GInputStream) stream = NULL;
 
 	/* progress */
 	fu_progress_set_id(progress, G_STRLOC);
@@ -282,12 +282,12 @@ fu_aver_safeisp_device_write_firmware(FuDevice *device,
 	fu_progress_add_step(progress, FWUPD_STATUS_DEVICE_VERIFY, 34, NULL);
 
 	/* get default image */
-	fw = fu_firmware_get_bytes(firmware, error);
-	if (fw == NULL)
+	stream = fu_firmware_get_stream(firmware, error);
+	if (stream == NULL)
 		return FALSE;
 
 	/* decompress */
-	archive = fu_archive_new(fw, FU_ARCHIVE_FLAG_NONE, error);
+	archive = fu_archive_new_stream(stream, FU_ARCHIVE_FLAG_NONE, error);
 	if (archive == NULL)
 		return FALSE;
 	cx3_fw = fu_archive_lookup_by_fn(archive, "update/cx3uvc.img", error);
