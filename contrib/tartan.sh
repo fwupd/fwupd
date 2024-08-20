63a3f42cc62aad042a9f836a8ce759db0e980884
Richard Hughes <richard@hughsie.com>
2024-08-20 17:22:54 +0100

    trivial: Force tartan to build with llvm 17

--- contrib/tartan.sh
+++ contrib/tartan.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-/usr/bin/scan-build -load-plugin /usr/local/lib64/tartan/16.0/libtartan.so \
+/usr/bin/scan-build-17 \
+	-load-plugin /usr/lib64/tartan/17.0/libtartan.so \
 	-disable-checker core.CallAndMessage \
 	-disable-checker core.NullDereference \
 	-disable-checker deadcode.DeadStores \
