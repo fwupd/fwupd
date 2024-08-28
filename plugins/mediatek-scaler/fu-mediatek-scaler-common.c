0b6b102efc8edf49c0e5d6090d7ee2886a8d3ebd
Crag Wang <crag_wang@dell.com>
2024-08-28 23:04:36 +0800

    mediatek_scaler: bugfix for device firmware version

--- plugins/mediatek-scaler/fu-mediatek-scaler-common.c
+++ plugins/mediatek-scaler/fu-mediatek-scaler-common.c
@@ -12,5 +12,5 @@
 gchar *
 mediatek_scaler_device_version_to_string(guint32 val)
 {
-	return g_strdup_printf("%x.%x.%x", val & 0xff, (val >> 24) & 0xff, (val >> 16) & 0xff);
+	return g_strdup_printf("%u.%u.%u", val & 0xff, (val >> 24) & 0xff, (val >> 16) & 0xff);
 }
