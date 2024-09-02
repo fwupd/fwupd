89f51752d219a95212f57f19e17df2fdfc50dc01
Richard Hughes <richard@hughsie.com>
2024-09-02 18:47:07 +0100

    Do not incorporate proxies if refcounted
    Should fix a regression in the qualcomm code.

--- libfwupdplugin/fu-device.c
+++ libfwupdplugin/fu-device.c
@@ -5576,8 +5576,11 @@ fu_device_incorporate(FuDevice *self, FuDevice *donor)
 		fu_device_set_backend_id(self, priv_donor->backend_id);
 	if (priv->update_request_id == NULL && priv_donor->update_request_id != NULL)
 		fu_device_set_update_request_id(self, priv_donor->update_request_id);
-	if (priv->proxy == NULL && priv_donor->proxy != NULL)
-		fu_device_set_proxy(self, priv_donor->proxy);
+	if (!fu_device_has_private_flag(self, FU_DEVICE_PRIVATE_FLAG_REFCOUNTED_PROXY) &&
+	    !fu_device_has_private_flag(donor, FU_DEVICE_PRIVATE_FLAG_REFCOUNTED_PROXY)) {
+		if (priv->proxy == NULL && priv_donor->proxy != NULL)
+			fu_device_set_proxy(self, priv_donor->proxy);
+	}
 	if (priv->proxy_guid == NULL && priv_donor->proxy_guid != NULL)
 		fu_device_set_proxy_guid(self, priv_donor->proxy_guid);
 	if (priv->custom_flags == NULL && priv_donor->custom_flags != NULL)
