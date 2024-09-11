830b01e9d5035051beacc471e9f866329f488b2a
Richard Hughes <richard@hughsie.com>
2024-09-11 10:59:23 +0100

    trivial: Be explicit when finding source files for the CI checks
    Using `recursive=True` is slow, and breaks horribly if there are symlinks.

--- contrib/ci/check-finalizers.py
+++ contrib/ci/check-finalizers.py
@@ -42,9 +42,12 @@ class ReturnValidator:
 def test_files():
     # test all C source files
     validator = ReturnValidator()
-    for fn in glob.glob("**/*.c", recursive=True):
-        if fn.startswith("dist/") or fn.startswith("subprojects/"):
-            continue
+    for fn in (
+        glob.glob("libfwupd/*.c")
+        + glob.glob("libfwupdplugin/*.c")
+        + glob.glob("plugins/*/*.c")
+        + glob.glob("src/*.c")
+    ):
         validator.parse(fn)
     for warning in validator.warnings:
         print(warning)
