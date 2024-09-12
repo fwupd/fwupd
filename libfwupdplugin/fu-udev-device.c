8a32d280a752911d902db22b0dcb137760cb4dc0
Richard Hughes <richard@hughsie.com>
2024-09-12 11:00:25 +0100

    Set the PCI vendor and name explicitly for SCSI devices
    It's was all too magic before.

--- libfwupdplugin/fu-udev-device.c
+++ libfwupdplugin/fu-udev-device.c
@@ -1332,9 +1332,8 @@ fu_udev_device_set_physical_id(FuUdevDevice *self, const gchar *subsystems, GErr
 		physical_id = g_strdup_printf("PCI_SLOT_NAME=%s", tmp);
 	} else if (g_strcmp0(subsystem, "usb") == 0 || g_strcmp0(subsystem, "mmc") == 0 ||
 		   g_strcmp0(subsystem, "i2c") == 0 || g_strcmp0(subsystem, "platform") == 0 ||
-		   g_strcmp0(subsystem, "scsi") == 0 || g_strcmp0(subsystem, "mtd") == 0 ||
-		   g_strcmp0(subsystem, "block") == 0 || g_strcmp0(subsystem, "gpio") == 0 ||
-		   g_strcmp0(subsystem, "video4linux") == 0) {
+		   g_strcmp0(subsystem, "mtd") == 0 || g_strcmp0(subsystem, "block") == 0 ||
+		   g_strcmp0(subsystem, "gpio") == 0 || g_strcmp0(subsystem, "video4linux") == 0) {
 		tmp = g_udev_device_get_property(udev_device, "DEVPATH"); /* nocheck:blocked */
 		if (tmp == NULL) {
 			g_set_error_literal(error,
