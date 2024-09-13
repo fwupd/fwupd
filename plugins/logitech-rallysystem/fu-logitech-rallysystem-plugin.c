cbb019d77428af0419bba1b46f844903ddbf63e7
Sanjay Sheth <ssheth@logitech.com>
2024-09-13 13:05:18 -0700

    overwrite tablehub version with system version

--- plugins/logitech-rallysystem/fu-logitech-rallysystem-plugin.c
+++ plugins/logitech-rallysystem/fu-logitech-rallysystem-plugin.c
@@ -12,6 +12,7 @@
 
 struct _FuLogitechRallysystemPlugin {
 	FuPlugin parent_instance;
+	gchar *system_version;
 };
 
 G_DEFINE_TYPE(FuLogitechRallysystemPlugin, fu_logitech_rallysystem_plugin, FU_TYPE_PLUGIN)
@@ -19,6 +20,49 @@ G_DEFINE_TYPE(FuLogitechRallysystemPlugin, fu_logitech_rallysystem_plugin, FU_TY
 static void
 fu_logitech_rallysystem_plugin_init(FuLogitechRallysystemPlugin *self)
 {
+}
+
+static void
+fu_logitech_rallysystem_plugin_finalize(GObject *object)
+{
+	FuLogitechRallysystemPlugin *self = FU_LOGITECH_RALLYSYSTEM_PLUGIN(object);
+	g_free(self->system_version);
+	G_OBJECT_CLASS(fu_logitech_rallysystem_plugin_parent_class)->finalize(object);
+}
+
+static void
+fu_logitech_rallysystem_plugin_device_added(FuPlugin *plugin, FuDevice *device)
+{
+	FuLogitechRallysystemPlugin *self = FU_LOGITECH_RALLYSYSTEM_PLUGIN(plugin);
+	GPtrArray *devices;
+
+	/*
+	 * composite device is composed of multiple sub-devices: audio, video, tablehub, speakers.
+	 * Each sub-device has their own unique firmware version.
+	 * Only audio sub-device has topology information of all sub-devices.
+	 * Only audio sub-device has information of overall/unique system version.
+	 * Only tablehub device is responsible to push firmware images to all sub-devices,
+	 * all sub-devices update firmware independently, and provide status to tablehub.
+	 * Since only tablehub can accept firmware image, its guid is used in .metainfo.xml file.
+	 * Ask here is for tablehub, to provide system version, so that application can use single
+	 * guid to query system version and check .metainfo.xml to determine if upgraded is needed
+	 * or not. Following logic preserve the system version information from audio sub-device and
+	 * overwrite local version information of tablehub with this system version
+	 */
+	if (g_strcmp0(fu_device_get_plugin(device), "logitech_rallysystem") == 0) {
+		if (FU_IS_LOGITECH_RALLYSYSTEM_AUDIO_DEVICE(device))
+			self->system_version = strdup(fu_device_get_version(device));
+
+		devices = fu_plugin_get_devices(plugin);
+		for (guint i = 0; i < devices->len; i++) {
+			FuDevice *device_tmp = g_ptr_array_index(devices, i);
+			if (FU_IS_LOGITECH_RALLYSYSTEM_TABLEHUB_DEVICE(device_tmp)) {
+				fu_device_set_version(device_tmp, self->system_version);
+				g_debug("overwriting tablehub version to %s", self->system_version);
+				break;
+			}
+		}
+	}
 }
 
 static void
@@ -34,5 +78,9 @@ static void
 fu_logitech_rallysystem_plugin_class_init(FuLogitechRallysystemPluginClass *klass)
 {
 	FuPluginClass *plugin_class = FU_PLUGIN_CLASS(klass);
+	GObjectClass *object_class = G_OBJECT_CLASS(klass);
+
+	object_class->finalize = fu_logitech_rallysystem_plugin_finalize;
 	plugin_class->constructed = fu_logitech_rallysystem_plugin_constructed;
+	plugin_class->device_added = fu_logitech_rallysystem_plugin_device_added;
 }
