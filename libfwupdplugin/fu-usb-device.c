fwupd/fwupd

commit ec5edab33963587888f7c3b8169ffd36801f90c7

Store the USB VID/PID in the FuUdevDevice vendor and model properties
It doesn't make sense to have two different fields now that FuUsbDevice is a
superclass of FuUdevDevice.

libfwupdplugin/fu-usb-device.c
@@ -1224,9 +1224,8 @@ fu_usb_device_probe(FuDevice *device, GError **error)
 guint16
 fu_usb_device_get_vid(FuUsbDevice *self)
 {
-	FuUsbDevicePrivate *priv = GET_PRIVATE(self);
 	g_return_val_if_fail(FU_IS_USB_DEVICE(self), 0x0000);
-	return priv->desc.idVendor;
+	return fu_udev_device_get_vendor(FU_UDEV_DEVICE(self));
 }
 
 /**
@@ -1242,9 +1241,8 @@ fu_usb_device_get_vid(FuUsbDevice *self)
 guint16
 fu_usb_device_get_pid(FuUsbDevice *self)
 {
-	FuUsbDevicePrivate *priv = GET_PRIVATE(self);
 	g_return_val_if_fail(FU_IS_USB_DEVICE(self), 0x0000);
-	return priv->desc.idProduct;
+	return fu_udev_device_get_model(FU_UDEV_DEVICE(self));
 }
 
 /**
@@ -1319,7 +1317,7 @@ fu_usb_device_incorporate(FuDevice *device, FuDevice *device_donor)
 	fu_usb_device_set_dev(self, priv_donor->usb_device);
 
 	/* all descriptor fields */
-	if (priv->desc.idVendor == 0x0)
+	if (priv->desc.bLength == 0x0)
 		memcpy(&priv->desc, &priv_donor->desc, sizeof(priv->desc)); /* nocheck:blocked */
 
 	if (priv->interfaces->len == 0) {
@@ -1745,8 +1743,8 @@ fu_usb_device_parse_descriptor(FuUsbDevice *self, GInputStream *stream, GError *
 	priv->desc.bDeviceSubClass = fu_usb_device_hdr_get_device_sub_class(st);
 	priv->desc.bDeviceProtocol = fu_usb_device_hdr_get_device_protocol(st);
 	priv->desc.bMaxPacketSize0 = fu_usb_device_hdr_get_max_packet_size0(st);
-	priv->desc.idVendor = fu_usb_device_hdr_get_vendor(st);
-	priv->desc.idProduct = fu_usb_device_hdr_get_product(st);
+	fu_udev_device_set_vendor(FU_UDEV_DEVICE(self), fu_usb_device_hdr_get_vendor(st));
+	fu_udev_device_set_model(FU_UDEV_DEVICE(self), fu_usb_device_hdr_get_product(st));
 	priv->desc.bcdDevice = fu_usb_device_hdr_get_device(st);
 	priv->desc.iManufacturer = fu_usb_device_hdr_get_manufacturer_idx(st);
 	priv->desc.iProduct = fu_usb_device_hdr_get_product_idx(st);
@@ -2715,9 +2713,12 @@ fu_usb_device_from_json(FwupdCodec *codec, JsonNode *json_node, GError **error)
 		fu_device_set_created_usec(FU_DEVICE(self), g_date_time_to_unix_usec(created_new));
 	}
 #endif
-	priv->desc.idVendor = json_object_get_int_member_with_default(json_object, "IdVendor", 0x0);
-	priv->desc.idProduct =
-	    json_object_get_int_member_with_default(json_object, "IdProduct", 0x0);
+	fu_udev_device_set_vendor(
+	    FU_UDEV_DEVICE(self),
+	    json_object_get_int_member_with_default(json_object, "IdVendor", 0x0));
+	fu_udev_device_set_model(
+	    FU_UDEV_DEVICE(self),
+	    json_object_get_int_member_with_default(json_object, "IdProduct", 0x0));
 	priv->desc.bcdDevice = json_object_get_int_member_with_default(json_object, "Device", 0x0);
 	priv->desc.bcdUSB = json_object_get_int_member_with_default(json_object, "USB", 0x0);
 	priv->desc.iManufacturer =
@@ -2830,10 +2831,16 @@ fu_usb_device_add_json(FwupdCodec *codec, JsonBuilder *builder, FwupdCodecFlags 
 		fwupd_codec_json_append(builder, "Created", str);
 	}
 #endif
-	if (priv->desc.idVendor != 0)
-		fwupd_codec_json_append_int(builder, "IdVendor", priv->desc.idVendor);
-	if (priv->desc.idProduct != 0)
-		fwupd_codec_json_append_int(builder, "IdProduct", priv->desc.idProduct);
+	if (fu_udev_device_get_vendor(FU_UDEV_DEVICE(self)) != 0) {
+		fwupd_codec_json_append_int(builder,
+					    "IdVendor",
+					    fu_udev_device_get_vendor(FU_UDEV_DEVICE(self)));
+	}
+	if (fu_udev_device_get_model(FU_UDEV_DEVICE(self)) != 0) {
+		fwupd_codec_json_append_int(builder,
+					    "IdProduct",
+					    fu_udev_device_get_model(FU_UDEV_DEVICE(self)));
+	}
 	if (priv->desc.bcdDevice != 0)
 		fwupd_codec_json_append_int(builder, "Device", priv->desc.bcdDevice);
 	if (priv->desc.bcdUSB != 0)

commit 4d6e5af3abd112f63298a8f9398665b06c492f4e

trivial: Fix an enumeration regression on macOS
This regressed in ec5edab.

libfwupdplugin/fu-usb-device.c
@@ -1037,6 +1037,8 @@ fu_usb_device_probe_internal(FuUsbDevice *self, GError **error)
 		}
 		priv->busnum = libusb_get_bus_number(priv->usb_device);
 		priv->devnum = libusb_get_device_address(priv->usb_device);
+		fu_udev_device_set_vendor(FU_UDEV_DEVICE(self), priv->desc.idVendor);
+		fu_udev_device_set_model(FU_UDEV_DEVICE(self), priv->desc.idProduct);
 	} else {
 		guint64 busnum = 0;
 		guint64 devnum = 0;