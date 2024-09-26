33e4dd0457dc710d53af833a4a9479c6f1dbfee4
Mario Limonciello <mario.limonciello@amd.com>
2024-09-26 14:40:29 -0500

    Add a plugin for updating Legion Hid 2 devices

---
+++ plugins/legion-hid2/fu-legion-hid2-firmware.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2024 Mario Limonciello <superm1@gmail.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#pragma once
+
+#include <fwupdplugin.h>
+
+#define FU_TYPE_LEGION_HID2_FIRMWARE (fu_legion_hid2_firmware_get_type())
+G_DECLARE_FINAL_TYPE(FuLegionHid2Firmware,
+		     fu_legion_hid2_firmware,
+		     FU,
+		     LEGION_HID2_FIRMWARE,
+		     FuFirmware)
+
+FuFirmware *
+fu_legion_hid2_firmware_new(void);
+
+guint32
+fu_legion_hid2_firmware_get_version(FuFirmware *firmware);
