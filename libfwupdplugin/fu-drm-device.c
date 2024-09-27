9e9c0ccef727f0e70c2af4fef254f51d29a2f066
Richard Hughes <richard@hughsie.com>
2024-09-27 08:08:36 +0100

    Add fu_drm_device_get_crtc_{x|y|width|height} for future use

--- libfwupdplugin/fu-drm-device.c
+++ libfwupdplugin/fu-drm-device.c
@@ -8,6 +8,10 @@
 
 #include "config.h"
 
+#ifdef HAVE_LIBDRM
+#include <xf86drmMode.h>
+#endif
+
 #include "fu-bytes.h"
 #include "fu-common-struct.h"
 #include "fu-drm-device.h"
@@ -26,11 +30,20 @@ typedef struct {
 	gboolean enabled;
 	FuDisplayState display_state;
 	FuEdid *edid;
+	guint32 crtc_x;
+	guint32 crtc_y;
+	guint32 crtc_width;
+	guint32 crtc_height;
 } FuDrmDevicePrivate;
 
 G_DEFINE_TYPE_WITH_PRIVATE(FuDrmDevice, fu_drm_device, FU_TYPE_UDEV_DEVICE)
 
 #define GET_PRIVATE(o) (fu_drm_device_get_instance_private(o))
+
+#ifdef HAVE_LIBDRM
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(drmModeCrtc, drmModeFreeCrtc)
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(drmModeRes, drmModeFreeResources)
+#endif
 
 static void
 fu_drm_device_to_string(FuDevice *device, guint idt, GString *str)
@@ -39,6 +52,10 @@ fu_drm_device_to_string(FuDevice *device, guint idt, GString *str)
 	FuDrmDevicePrivate *priv = GET_PRIVATE(self);
 	fwupd_codec_string_append(str, idt, "ConnectorId", priv->connector_id);
 	fwupd_codec_string_append_bool(str, idt, "Enabled", priv->enabled);
+	fwupd_codec_string_append_int(str, idt, "CrctX", priv->crtc_x);
+	fwupd_codec_string_append_int(str, idt, "CrctY", priv->crtc_y);
+	fwupd_codec_string_append_int(str, idt, "CrctWidth", priv->crtc_width);
+	fwupd_codec_string_append_int(str, idt, "CrctHeight", priv->crtc_height);
 	fwupd_codec_string_append(str,
 				  idt,
 				  "State",
@@ -97,6 +114,78 @@ fu_drm_device_get_connector_id(FuDrmDevice *self)
 	FuDrmDevicePrivate *priv = GET_PRIVATE(self);
 	g_return_val_if_fail(FU_IS_DRM_DEVICE(self), NULL);
 	return priv->connector_id;
+}
+
+/**
+ * fu_drm_device_get_crtc_x:
+ * @self: a #FuDrmDevice
+ *
+ * Gets the X position of the preferred CRTC of the DRM device.
+ *
+ * Returns: pixels
+ *
+ * Since: 2.0.0
+ **/
+guint32
+fu_drm_device_get_crtc_x(FuDrmDevice *self)
+{
+	FuDrmDevicePrivate *priv = GET_PRIVATE(self);
+	g_return_val_if_fail(FU_IS_DRM_DEVICE(self), 0);
+	return priv->crtc_x;
+}
+
+/**
+ * fu_drm_device_get_crtc_y:
+ * @self: a #FuDrmDevice
+ *
+ * Gets the Y position of the preferred CRTC of the DRM device.
+ *
+ * Returns: pixels
+ *
+ * Since: 2.0.0
+ **/
+guint32
+fu_drm_device_get_crtc_y(FuDrmDevice *self)
+{
+	FuDrmDevicePrivate *priv = GET_PRIVATE(self);
+	g_return_val_if_fail(FU_IS_DRM_DEVICE(self), 0);
+	return priv->crtc_y;
+}
+
+/**
+ * fu_drm_device_get_crtc_width:
+ * @self: a #FuDrmDevice
+ *
+ * Gets the width of the preferred CRTC of the DRM device.
+ *
+ * Returns: pixels
+ *
+ * Since: 2.0.0
+ **/
+guint32
+fu_drm_device_get_crtc_width(FuDrmDevice *self)
+{
+	FuDrmDevicePrivate *priv = GET_PRIVATE(self);
+	g_return_val_if_fail(FU_IS_DRM_DEVICE(self), 0);
+	return priv->crtc_width;
+}
+
+/**
+ * fu_drm_device_get_crtc_height:
+ * @self: a #FuDrmDevice
+ *
+ * Gets the height of the preferred CRTC of the DRM device.
+ *
+ * Returns: pixels
+ *
+ * Since: 2.0.0
+ **/
+guint32
+fu_drm_device_get_crtc_height(FuDrmDevice *self)
+{
+	FuDrmDevicePrivate *priv = GET_PRIVATE(self);
+	g_return_val_if_fail(FU_IS_DRM_DEVICE(self), 0);
+	return priv->crtc_height;
 }
 
 /**
@@ -202,6 +291,43 @@ fu_drm_device_probe(FuDevice *device, GError **error)
 	return TRUE;
 }
 
+static gboolean
+fu_drm_device_setup(FuDevice *device, GError **error)
+{
+#ifdef HAVE_LIBDRM
+	FuDrmDevice *self = FU_DRM_DEVICE(device);
+	FuDrmDevicePrivate *priv = GET_PRIVATE(self);
+	FuIOChannel *io_channel = fu_udev_device_get_io_channel(FU_UDEV_DEVICE(device));
+
+	/* get crtcs */
+	if (io_channel != NULL) {
+		gint fd = fu_io_channel_unix_get_fd(io_channel);
+		g_autoptr(drmModeRes) res = drmModeGetResources(fd);
+		if (res == NULL) {
+			g_set_error_literal(error,
+					    FWUPD_ERROR,
+					    FWUPD_ERROR_NOT_SUPPORTED,
+					    "failed to get DRM resources");
+			return FALSE;
+		}
+		for (gint i = 0; i < res->count_crtcs; i++) {
+			g_autoptr(drmModeCrtc) crtc = drmModeGetCrtc(fd, res->crtcs[i]);
+			if (crtc == NULL)
+				continue;
+			if (crtc->mode_valid && (crtc->mode.type & DRM_MODE_TYPE_PREFERRED) > 0) {
+				priv->crtc_x = crtc->x;
+				priv->crtc_y = crtc->y;
+				priv->crtc_width = crtc->width;
+				priv->crtc_height = crtc->height;
+				break;
+			}
+		}
+	}
+#endif
+	/* success */
+	return TRUE;
+}
+
 static void
 fu_drm_device_incorporate(FuDevice *device, FuDevice *donor)
 {
@@ -219,11 +345,20 @@ fu_drm_device_incorporate(FuDevice *device, FuDevice *donor)
 		priv->connector_id = g_strdup(priv_donor->connector_id);
 	if (priv->edid == NULL && priv_donor->edid != NULL)
 		priv->edid = g_object_ref(priv_donor->edid);
+	if (priv->crtc_x == 0 && priv_donor->crtc_x > 0)
+		priv->crtc_x = priv_donor->crtc_x;
+	if (priv->crtc_y == 0 && priv_donor->crtc_y > 0)
+		priv->crtc_y = priv_donor->crtc_y;
+	if (priv->crtc_width == 0 && priv_donor->crtc_width > 0)
+		priv->crtc_width = priv_donor->crtc_width;
+	if (priv->crtc_height == 0 && priv_donor->crtc_height > 0)
+		priv->crtc_height = priv_donor->crtc_height;
 }
 
 static void
 fu_drm_device_init(FuDrmDevice *self)
 {
+	fu_udev_device_add_open_flag(FU_UDEV_DEVICE(self), FU_IO_CHANNEL_OPEN_FLAG_READ);
 }
 
 static void
@@ -247,5 +382,6 @@ fu_drm_device_class_init(FuDrmDeviceClass *klass)
 	object_class->finalize = fu_drm_device_finalize;
 	device_class->probe = fu_drm_device_probe;
 	device_class->incorporate = fu_drm_device_incorporate;
+	device_class->setup = fu_drm_device_setup;
 	device_class->to_string = fu_drm_device_to_string;
 }
