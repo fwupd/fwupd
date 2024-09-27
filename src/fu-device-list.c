fwupd/fwupd

commit 20c1173277333a5fcd56aec9594eceb9fbfd3b41

trivial: Add sysfs tests for FuV4lDevice
Also, standardize on how to handle the donor physical ID as different plugins
are doing different things.

src/fu-device-list.c
@@ -668,27 +668,9 @@ _fu_device_incorporate_problem_update_in_progress(FuDevice *self, FuDevice *dono
 }
 
 static void
-_fu_device_incorporate_update_state(FuDevice *self, FuDevice *donor)
-{
-	if (fu_device_get_update_error(donor) != NULL && fu_device_get_update_error(self) == NULL) {
-		const gchar *update_error = fu_device_get_update_error(donor);
-		g_info("copying update error %s to new device", update_error);
-		fu_device_set_update_error(self, update_error);
-	}
-	if (fu_device_get_update_state(donor) != FWUPD_UPDATE_STATE_UNKNOWN &&
-	    fu_device_get_update_state(self) == FWUPD_UPDATE_STATE_UNKNOWN) {
-		FwupdUpdateState update_state = fu_device_get_update_state(donor);
-		g_info("copying update state %s to new device",
-		       fwupd_update_state_to_string(update_state));
-		fu_device_set_update_state(self, update_state);
-	}
-}
-
-static void
 fu_device_list_replace(FuDeviceList *self, FuDeviceItem *item, FuDevice *device)
 {
 	GPtrArray *children = fu_device_get_children(item->device);
-	GPtrArray *vendor_ids;
 	g_autofree gchar *str = NULL;
 
 	/* run the optional device-specific subclass */
@@ -697,13 +679,12 @@ fu_device_list_replace(FuDeviceList *self, FuDeviceItem *item, FuDevice *device)
 	/* copy over any GUIDs that used to exist */
 	fu_device_list_add_missing_guids(device, item->device);
 
-	/* enforce the vendor ID if specified */
-	vendor_ids = fu_device_get_vendor_ids(item->device);
-	for (guint i = 0; i < vendor_ids->len; i++) {
-		const gchar *vendor_id = g_ptr_array_index(vendor_ids, i);
-		g_info("copying old vendor ID %s to new device", vendor_id);
-		fwupd_device_add_vendor_id(FWUPD_DEVICE(device), vendor_id);
-	}
+	/* incorporate properties from the old device */
+	fu_device_incorporate(device,
+			      item->device,
+			      FU_DEVICE_INCORPORATE_FLAG_VENDOR_IDS |
+				  FU_DEVICE_INCORPORATE_FLAG_UPDATE_ERROR |
+				  FU_DEVICE_INCORPORATE_FLAG_UPDATE_ERROR);
 
 	/* copy inhibit */
 	_fu_device_incorporate_problem_update_in_progress(item->device, device);
@@ -726,24 +707,15 @@ fu_device_list_replace(FuDeviceList *self, FuDeviceItem *item, FuDevice *device)
 	}
 
 	/* allow another plugin to handle the write too */
-	if (fu_device_has_flag(item->device, FWUPD_DEVICE_FLAG_ANOTHER_WRITE_REQUIRED)) {
-		g_debug("copying another-write-required to new device");
-		fu_device_add_flag(device, FWUPD_DEVICE_FLAG_ANOTHER_WRITE_REQUIRED);
-	}
+	fu_device_incorporate_flag(device, item->device, FWUPD_DEVICE_FLAG_ANOTHER_WRITE_REQUIRED);
 
 	/* seems like a sane assumption if we've tagged the runtime mode as signed */
-	if (fu_device_has_flag(item->device, FWUPD_DEVICE_FLAG_SIGNED_PAYLOAD))
-		fu_device_add_flag(device, FWUPD_DEVICE_FLAG_SIGNED_PAYLOAD);
-	if (fu_device_has_flag(item->device, FWUPD_DEVICE_FLAG_UNSIGNED_PAYLOAD))
-		fu_device_add_flag(device, FWUPD_DEVICE_FLAG_UNSIGNED_PAYLOAD);
-	if (fu_device_has_flag(item->device, FWUPD_DEVICE_FLAG_EMULATION_TAG))
-		fu_device_add_flag(device, FWUPD_DEVICE_FLAG_EMULATION_TAG);
+	fu_device_incorporate_flag(device, item->device, FWUPD_DEVICE_FLAG_SIGNED_PAYLOAD);
+	fu_device_incorporate_flag(device, item->device, FWUPD_DEVICE_FLAG_UNSIGNED_PAYLOAD);
+	fu_device_incorporate_flag(device, item->device, FWUPD_DEVICE_FLAG_EMULATION_TAG);
 
 	/* device won't come back in right mode */
-	if (fu_device_has_flag(item->device, FWUPD_DEVICE_FLAG_WILL_DISAPPEAR)) {
-		g_info("copying will-disappear to new device");
-		fu_device_add_flag(device, FWUPD_DEVICE_FLAG_WILL_DISAPPEAR);
-	}
+	fu_device_incorporate_flag(device, item->device, FWUPD_DEVICE_FLAG_WILL_DISAPPEAR);
 
 	/* copy the parent if not already set */
 	if (fu_device_get_parent(item->device) != NULL &&
@@ -760,9 +732,6 @@ fu_device_list_replace(FuDeviceList *self, FuDeviceItem *item, FuDevice *device)
 		g_info("copying child %s to new device", fu_device_get_id(child));
 		fu_device_add_child(device, child);
 	}
-
-	/* copy the update state if known */
-	_fu_device_incorporate_update_state(item->device, device);
 
 	/* assign the new device */
 	g_set_object(&item->device_old, item->device);
@@ -824,7 +793,10 @@ fu_device_list_add(FuDeviceList *self, FuDevice *device)
 			fu_device_remove_private_flag(item->device,
 						      FU_DEVICE_PRIVATE_FLAG_UNCONNECTED);
 			_fu_device_incorporate_problem_update_in_progress(device, item->device);
-			_fu_device_incorporate_update_state(device, item->device);
+			fu_device_incorporate(item->device,
+					      device,
+					      FU_DEVICE_INCORPORATE_FLAG_UPDATE_ERROR |
+						  FU_DEVICE_INCORPORATE_FLAG_UPDATE_ERROR);
 			g_set_object(&item->device_old, item->device);
 			fu_device_list_item_set_device(item, device);
 			fu_device_list_clear_wait_for_replug(self, item);

libfwupdplugin/fu-block-device.c
@@ -59,7 +59,7 @@ fu_block_device_probe(FuDevice *device, GError **error)
 			return FALSE;
 		if (!fu_device_build_instance_id(device, error, "BLOCK", "VEN", "DEV", NULL))
 			return FALSE;
-		fu_device_incorporate_vendor_ids(device, usb_device);
+		fu_device_incorporate(device, usb_device, FU_DEVICE_INCORPORATE_FLAG_VENDOR_IDS);
 
 		/* USB devpath as physical ID */
 		physical_id =

libfwupdplugin/fu-device.c
@@ -1109,13 +1109,7 @@ fu_device_set_proxy(FuDevice *self, FuDevice *proxy)
 
 	/* copy from proxy */
 	if (proxy != NULL) {
-		if (fu_device_get_context(self) == NULL && fu_device_get_context(proxy) != NULL)
-			fu_device_set_context(self, fu_device_get_context(proxy));
-		if (fu_device_get_backend(self) == NULL && fu_device_get_backend(proxy) != NULL)
-			fu_device_set_backend(self, fu_device_get_backend(proxy));
-		if (fu_device_get_physical_id(self) == NULL &&
-		    fu_device_get_physical_id(proxy) != NULL)
-			fu_device_set_physical_id(self, fu_device_get_physical_id(proxy));
+		fu_device_incorporate(self, proxy, FU_DEVICE_INCORPORATE_FLAG_PHYSICAL_ID);
 		priv->notify_flags_proxy_id =
 		    g_signal_connect(FWUPD_DEVICE(proxy),
 				     "notify::flags",
@@ -1215,7 +1209,6 @@ void
 fu_device_add_child(FuDevice *self, FuDevice *child)
 {
 	FuDevicePrivate *priv = GET_PRIVATE(self);
-	FuDevicePrivate *priv_child = GET_PRIVATE(child);
 	GPtrArray *children;
 	g_autoptr(GError) error = NULL;
 
@@ -1271,32 +1264,13 @@ fu_device_add_child(FuDevice *self, FuDevice *child)
 	}
 
 	/* copy from main device if unset */
-	if (fu_device_get_physical_id(child) == NULL && fu_device_get_physical_id(self) != NULL)
-		fu_device_set_physical_id(child, fu_device_get_physical_id(self));
-	if (priv_child->backend_id == NULL && priv->backend_id != NULL)
-		fu_device_set_backend_id(child, priv->backend_id);
-	if (priv_child->ctx == NULL && priv->ctx != NULL)
-		fu_device_set_context(child, priv->ctx);
-	if (fu_device_get_vendor(child) == NULL)
-		fu_device_set_vendor(child, fu_device_get_vendor(self));
-	if (priv_child->remove_delay == 0 && priv->remove_delay != 0)
-		fu_device_set_remove_delay(child, priv->remove_delay);
-	if (priv_child->acquiesce_delay == 0 && priv->acquiesce_delay != 0)
-		fu_device_set_acquiesce_delay(child, priv->acquiesce_delay);
-	if (fu_device_get_vendor_ids(child)->len == 0) {
-		GPtrArray *vendor_ids = fu_device_get_vendor_ids(self);
-		for (guint i = 0; i < vendor_ids->len; i++) {
-			const gchar *vendor_id = g_ptr_array_index(vendor_ids, i);
-			fu_device_add_vendor_id(child, vendor_id);
-		}
-	}
-	if (fu_device_get_icons(child)->len == 0) {
-		GPtrArray *icons = fu_device_get_icons(self);
-		for (guint i = 0; i < icons->len; i++) {
-			const gchar *icon_name = g_ptr_array_index(icons, i);
-			fu_device_add_icon(child, icon_name);
-		}
-	}
+	fu_device_incorporate(
+	    child,
+	    self,
+	    FU_DEVICE_INCORPORATE_FLAG_PHYSICAL_ID | FU_DEVICE_INCORPORATE_FLAG_BACKEND_ID |
+		FU_DEVICE_INCORPORATE_FLAG_REMOVE_DELAY |
+		FU_DEVICE_INCORPORATE_FLAG_ACQUIESCE_DELAY | FU_DEVICE_INCORPORATE_FLAG_VENDOR_IDS |
+		FU_DEVICE_INCORPORATE_FLAG_ICONS | FU_DEVICE_INCORPORATE_FLAG_VENDOR);
 
 	/* ensure the ID is converted */
 	if (!fu_device_ensure_id(child, &error))
@@ -1599,8 +1573,7 @@ fu_device_add_child_by_type_guid(FuDevice *self, GType type, const gchar *guid, 
 	g_autoptr(FuDevice) child = NULL;
 	child = g_object_new(type, "context", priv->ctx, "logical-id", guid, NULL);
 	fu_device_add_guid(child, guid);
-	if (fu_device_get_physical_id(self) != NULL)
-		fu_device_set_physical_id(child, fu_device_get_physical_id(self));
+	fu_device_incorporate(child, self, FU_DEVICE_INCORPORATE_FLAG_PHYSICAL_ID);
 	if (!fu_device_ensure_id(self, error))
 		return FALSE;
 	if (!fu_device_probe(child, error))
@@ -5587,198 +5560,224 @@ fu_device_build_vendor_id_u16(FuDevice *self, const gchar *prefix, guint16 value
 	fwupd_device_add_vendor_id(FWUPD_DEVICE(self), vendor_id);
 }
 
-static void
-fu_device_incorporate_subclasses(FuDevice *self, FuDevice *donor)
-{
-	gpointer device_class_incorporate_last = NULL;
-	g_autoptr(GList) device_class_list = NULL;
-
-	/* run every unique ->incorporate() in each subclass */
-	for (GType gtype = G_OBJECT_TYPE(self); gtype != FWUPD_TYPE_DEVICE;
-	     gtype = g_type_parent(gtype)) {
-		FuDeviceClass *device_class = g_type_class_peek(gtype);
-		for (GType gtype_donor = G_OBJECT_TYPE(donor); gtype_donor != FWUPD_TYPE_DEVICE;
-		     gtype_donor = g_type_parent(gtype_donor)) {
-			if (gtype == gtype_donor)
-				device_class_list = g_list_prepend(device_class_list, device_class);
-		}
-	}
-	for (GList *l = device_class_list; l != NULL; l = l->next) {
-		FuDeviceClass *device_class = FU_DEVICE_CLASS(l->data);
-		if (device_class->incorporate != NULL &&
-		    device_class->incorporate != device_class_incorporate_last) {
-			device_class->incorporate(self, donor);
-			device_class_incorporate_last = device_class->incorporate;
-		}
-	}
-}
-
-/**
- * fu_device_incorporate_vendor_ids:
- * @self: a #FuDevice
- * @donor: Another #FuDevice
- *
- * Copy all vendor IDs from the donor object.
- *
- * Since: 2.0.0
- **/
-void
-fu_device_incorporate_vendor_ids(FuDevice *self, FuDevice *donor)
-{
-	GPtrArray *vendor_ids;
-
-	g_return_if_fail(FU_IS_DEVICE(self));
-	g_return_if_fail(FU_IS_DEVICE(donor));
-
-	vendor_ids = fu_device_get_vendor_ids(donor);
-	for (guint i = 0; i < vendor_ids->len; i++) {
-		const gchar *vendor_id = g_ptr_array_index(vendor_ids, i);
-		fu_device_add_vendor_id(self, vendor_id);
-	}
-}
-
 /**
  * fu_device_incorporate:
  * @self: a #FuDevice
  * @donor: Another #FuDevice
+ * @flag: Some #FuDeviceIncorporateFlags, e.g. %FU_DEVICE_INCORPORATE_FLAG_PHYSICAL_ID
  *
- * Copy all properties from the donor object if they have not already been set.
+ * Copy some properties from the donor object if they have not already been set.
  *
- * Since: 1.1.0
+ * Since: 2.0.0
  **/
 void
-fu_device_incorporate(FuDevice *self, FuDevice *donor)
+fu_device_incorporate(FuDevice *self, FuDevice *donor, FuDeviceIncorporateFlags flag)
 {
 	FuDevicePrivate *priv = GET_PRIVATE(self);
 	FuDevicePrivate *priv_donor = GET_PRIVATE(donor);
-	GPtrArray *instance_ids = fu_device_get_instance_ids(donor);
-	GPtrArray *parent_physical_ids = fu_device_get_parent_physical_ids(donor);
-	GPtrArray *parent_backend_ids = fu_device_get_parent_backend_ids(donor);
-	GHashTableIter iter;
-	gpointer key, value;
 
 	g_return_if_fail(FU_IS_DEVICE(self));
 	g_return_if_fail(FU_IS_DEVICE(donor));
 
-	/* copy from donor FuDevice if has not already been set */
-	if (priv_donor->private_flags != NULL) {
-		for (guint i = 0; i < priv_donor->private_flags->len; i++) {
-			const gchar *item_donor = g_ptr_array_index(priv_donor->private_flags, i);
-			fu_device_add_private_flag(self, item_donor);
-		}
-	}
-	if (priv->created_usec == 0 && priv_donor->created_usec != 0)
-		fu_device_set_created_usec(self, priv_donor->created_usec);
-	if (priv->modified_usec == 0 && priv_donor->modified_usec != 0)
-		fu_device_set_modified_usec(self, priv_donor->modified_usec);
-	if (priv->equivalent_id == NULL && fu_device_get_equivalent_id(donor) != NULL)
-		fu_device_set_equivalent_id(self, fu_device_get_equivalent_id(donor));
-	if (priv->physical_id == NULL && priv_donor->physical_id != NULL)
-		fu_device_set_physical_id(self, priv_donor->physical_id);
-	if (priv->logical_id == NULL && priv_donor->logical_id != NULL)
-		fu_device_set_logical_id(self, priv_donor->logical_id);
-	if (priv->backend_id == NULL && priv_donor->backend_id != NULL)
-		fu_device_set_backend_id(self, priv_donor->backend_id);
-	if (priv->update_request_id == NULL && priv_donor->update_request_id != NULL)
-		fu_device_set_update_request_id(self, priv_donor->update_request_id);
-	if (fu_device_has_private_flag(self, FU_DEVICE_PRIVATE_FLAG_REFCOUNTED_PROXY) &&
-	    fu_device_has_private_flag(donor, FU_DEVICE_PRIVATE_FLAG_REFCOUNTED_PROXY)) {
-		if (priv->proxy == NULL && priv_donor->proxy != NULL)
-			fu_device_set_proxy(self, priv_donor->proxy);
-	}
-	if (priv->proxy_guid == NULL && priv_donor->proxy_guid != NULL)
-		fu_device_set_proxy_guid(self, priv_donor->proxy_guid);
-	if (priv->custom_flags == NULL && priv_donor->custom_flags != NULL)
-		fu_device_set_custom_flags(self, priv_donor->custom_flags);
+	/* do these unconditionally */
+	if (priv->ctx == NULL && priv_donor->ctx != NULL)
+		fu_device_set_context(self, priv_donor->ctx);
 	if (priv->backend == NULL && priv_donor->backend != NULL)
 		fu_device_set_backend(self, priv_donor->backend);
-	if (priv->ctx == NULL)
-		fu_device_set_context(self, fu_device_get_context(donor));
-	if (priv_donor->parent_guids != NULL) {
-		for (guint i = 0; i < priv_donor->parent_guids->len; i++) {
-			const gchar *guid = g_ptr_array_index(priv_donor->parent_guids, i);
-			fu_device_add_parent_guid(self, guid);
+
+	/* bitflags */
+	if (flag & FU_DEVICE_INCORPORATE_FLAG_BASECLASS) {
+		fwupd_device_incorporate(FWUPD_DEVICE(self), FWUPD_DEVICE(donor));
+		if (fu_device_get_id(self) != NULL)
+			priv->device_id_valid = TRUE;
+		/* remove the baseclass-added serial number and GUIDs if set */
+		if (fu_device_has_private_flag(self, FU_DEVICE_PRIVATE_FLAG_NO_SERIAL_NUMBER))
+			fwupd_device_set_serial(FWUPD_DEVICE(self), NULL);
+		if (fu_device_has_private_flag(self, FU_DEVICE_PRIVATE_FLAG_NO_GENERIC_GUIDS))
+			g_ptr_array_set_size(fwupd_device_get_instance_ids(FWUPD_DEVICE(self)), 0);
+	}
+	if (flag & FU_DEVICE_INCORPORATE_FLAG_VENDOR) {
+		if (fu_device_get_vendor(self) == NULL && fu_device_get_vendor(donor) != NULL)
+			fu_device_set_vendor(self, fu_device_get_vendor(donor));
+	}
+	if (flag & FU_DEVICE_INCORPORATE_FLAG_PHYSICAL_ID) {
+		if (priv->physical_id == NULL && priv_donor->physical_id != NULL)
+			fu_device_set_physical_id(self, priv_donor->physical_id);
+	}
+	if (flag & FU_DEVICE_INCORPORATE_FLAG_LOGICAL_ID) {
+		if (priv->logical_id == NULL && priv_donor->logical_id != NULL)
+			fu_device_set_logical_id(self, priv_donor->logical_id);
+	}
+	if (flag & FU_DEVICE_INCORPORATE_FLAG_BACKEND_ID) {
+		if (priv->backend_id == NULL && priv_donor->backend_id != NULL)
+			fu_device_set_backend_id(self, priv_donor->backend_id);
+	}
+	if (flag & FU_DEVICE_INCORPORATE_FLAG_VENDOR_IDS) {
+		GPtrArray *vendor_ids = fu_device_get_vendor_ids(donor);
+		for (guint i = 0; i < vendor_ids->len; i++) {
+			const gchar *vendor_id = g_ptr_array_index(vendor_ids, i);
+			fu_device_add_vendor_id(self, vendor_id);
 		}
 	}
-	if (parent_physical_ids != NULL) {
-		for (guint i = 0; i < parent_physical_ids->len; i++) {
-			const gchar *tmp = g_ptr_array_index(parent_physical_ids, i);
-			fu_device_add_parent_physical_id(self, tmp);
+	if (flag & FU_DEVICE_INCORPORATE_FLAG_REMOVE_DELAY) {
+		if (priv->remove_delay == 0 && priv_donor->remove_delay != 0)
+			fu_device_set_remove_delay(self, priv_donor->remove_delay);
+	}
+	if (flag & FU_DEVICE_INCORPORATE_FLAG_ACQUIESCE_DELAY) {
+		if (priv->acquiesce_delay == 0 && priv_donor->acquiesce_delay != 0)
+			fu_device_set_acquiesce_delay(self, priv_donor->acquiesce_delay);
+	}
+	if (flag & FU_DEVICE_INCORPORATE_FLAG_ICONS) {
+		if (fu_device_get_icons(self)->len == 0) {
+			GPtrArray *icons = fu_device_get_icons(donor);
+			for (guint i = 0; i < icons->len; i++) {
+				const gchar *icon_name = g_ptr_array_index(icons, i);
+				fu_device_add_icon(self, icon_name);
+			}
 		}
 	}
-	if (parent_backend_ids != NULL) {
-		for (guint i = 0; i < parent_backend_ids->len; i++) {
-			const gchar *tmp = g_ptr_array_index(parent_backend_ids, i);
-			fu_device_add_parent_backend_id(self, tmp);
+	if (flag & FU_DEVICE_INCORPORATE_FLAG_UPDATE_ERROR) {
+		if (fu_device_get_update_error(self) == NULL &&
+		    fu_device_get_update_error(donor) != NULL) {
+			fu_device_set_update_error(self, fu_device_get_update_error(donor));
 		}
 	}
-	if (priv_donor->counterpart_guids != NULL) {
-		for (guint i = 0; i < priv_donor->counterpart_guids->len; i++) {
-			const gchar *tmp = g_ptr_array_index(priv_donor->counterpart_guids, i);
-			fu_device_add_counterpart_guid(self, tmp);
+	if (flag & FU_DEVICE_INCORPORATE_FLAG_UPDATE_STATE) {
+		if (fu_device_get_update_state(self) == FWUPD_UPDATE_STATE_UNKNOWN &&
+		    fu_device_get_update_state(donor) != FWUPD_UPDATE_STATE_UNKNOWN) {
+			fu_device_set_update_state(self, fu_device_get_update_state(donor));
 		}
 	}
-	if (priv_donor->metadata != NULL) {
-		g_hash_table_iter_init(&iter, priv_donor->metadata);
-		while (g_hash_table_iter_next(&iter, &key, &value)) {
-			if (fu_device_get_metadata(self, key) == NULL)
-				fu_device_set_metadata(self, key, value);
+	if (flag & FU_DEVICE_INCORPORATE_FLAG_SUPERCLASS) {
+		gpointer device_class_incorporate_last = NULL;
+		g_autoptr(GList) device_class_list = NULL;
+
+		/* run every unique ->incorporate() in each subclass */
+		for (GType gtype = G_OBJECT_TYPE(self); gtype != FWUPD_TYPE_DEVICE;
+		     gtype = g_type_parent(gtype)) {
+			FuDeviceClass *device_class = g_type_class_peek(gtype);
+			for (GType gtype_donor = G_OBJECT_TYPE(donor);
+			     gtype_donor != FWUPD_TYPE_DEVICE;
+			     gtype_donor = g_type_parent(gtype_donor)) {
+				if (gtype == gtype_donor)
+					device_class_list =
+					    g_list_prepend(device_class_list, device_class);
+			}
+		}
+		for (GList *l = device_class_list; l != NULL; l = l->next) {
+			FuDeviceClass *device_class = FU_DEVICE_CLASS(l->data);
+			if (device_class->incorporate != NULL &&
+			    device_class->incorporate != device_class_incorporate_last) {
+				device_class->incorporate(self, donor);
+				device_class_incorporate_last = device_class->incorporate;
+			}
 		}
 	}
-	if (priv_donor->events != NULL && donor != priv->proxy) {
-		for (guint i = 0; i < priv_donor->events->len; i++) {
-			FuDeviceEvent *event = g_ptr_array_index(priv_donor->events, i);
-			fu_device_add_event(self, event);
+	/* everything else */
+	if (flag == FU_DEVICE_INCORPORATE_FLAG_ALL) {
+		GPtrArray *instance_ids = fu_device_get_instance_ids(donor);
+		GPtrArray *parent_physical_ids = fu_device_get_parent_physical_ids(donor);
+		GPtrArray *parent_backend_ids = fu_device_get_parent_backend_ids(donor);
+
+		/* copy from donor FuDevice if has not already been set */
+		if (priv_donor->private_flags != NULL) {
+			for (guint i = 0; i < priv_donor->private_flags->len; i++) {
+				const gchar *item_donor =
+				    g_ptr_array_index(priv_donor->private_flags, i);
+				fu_device_add_private_flag(self, item_donor);
+			}
 		}
-	}
-
-	/* probably not required, but seems safer */
-	for (guint i = 0; i < priv_donor->possible_plugins->len; i++) {
-		const gchar *possible_plugin = g_ptr_array_index(priv_donor->possible_plugins, i);
-		fu_device_add_possible_plugin(self, possible_plugin);
-	}
-	if (priv_donor->instance_id_quirks != NULL) {
-		for (guint i = 0; i < priv_donor->instance_id_quirks->len; i++) {
-			const gchar *instance_id =
-			    g_ptr_array_index(priv_donor->instance_id_quirks, i);
-			fu_device_add_instance_id_full(self,
-						       instance_id,
-						       FU_DEVICE_INSTANCE_FLAG_QUIRKS);
+		if (priv->created_usec == 0 && priv_donor->created_usec != 0)
+			fu_device_set_created_usec(self, priv_donor->created_usec);
+		if (priv->modified_usec == 0 && priv_donor->modified_usec != 0)
+			fu_device_set_modified_usec(self, priv_donor->modified_usec);
+		if (priv->equivalent_id == NULL && fu_device_get_equivalent_id(donor) != NULL)
+			fu_device_set_equivalent_id(self, fu_device_get_equivalent_id(donor));
+		if (priv->update_request_id == NULL && priv_donor->update_request_id != NULL)
+			fu_device_set_update_request_id(self, priv_donor->update_request_id);
+		if (fu_device_has_private_flag(self, FU_DEVICE_PRIVATE_FLAG_REFCOUNTED_PROXY) &&
+		    fu_device_has_private_flag(donor, FU_DEVICE_PRIVATE_FLAG_REFCOUNTED_PROXY)) {
+			if (priv->proxy == NULL && priv_donor->proxy != NULL)
+				fu_device_set_proxy(self, priv_donor->proxy);
 		}
-	}
-
-	/* copy all instance ID keys if not already set */
-	if (priv_donor->instance_hash != NULL) {
-		g_hash_table_iter_init(&iter, priv_donor->instance_hash);
-		while (g_hash_table_iter_next(&iter, &key, &value)) {
-			if (fu_device_get_instance_str(self, key) == NULL)
-				fu_device_add_instance_str(self, key, value);
+		if (priv->proxy_guid == NULL && priv_donor->proxy_guid != NULL)
+			fu_device_set_proxy_guid(self, priv_donor->proxy_guid);
+		if (priv->custom_flags == NULL && priv_donor->custom_flags != NULL)
+			fu_device_set_custom_flags(self, priv_donor->custom_flags);
+		if (priv_donor->parent_guids != NULL) {
+			for (guint i = 0; i < priv_donor->parent_guids->len; i++) {
+				const gchar *guid = g_ptr_array_index(priv_donor->parent_guids, i);
+				fu_device_add_parent_guid(self, guid);
+			}
 		}
-	}
+		if (parent_physical_ids != NULL) {
+			for (guint i = 0; i < parent_physical_ids->len; i++) {
+				const gchar *tmp = g_ptr_array_index(parent_physical_ids, i);
+				fu_device_add_parent_physical_id(self, tmp);
+			}
+		}
+		if (parent_backend_ids != NULL) {
+			for (guint i = 0; i < parent_backend_ids->len; i++) {
+				const gchar *tmp = g_ptr_array_index(parent_backend_ids, i);
+				fu_device_add_parent_backend_id(self, tmp);
+			}
+		}
+		if (priv_donor->counterpart_guids != NULL) {
+			for (guint i = 0; i < priv_donor->counterpart_guids->len; i++) {
+				const gchar *tmp =
+				    g_ptr_array_index(priv_donor->counterpart_guids, i);
+				fu_device_add_counterpart_guid(self, tmp);
+			}
+		}
+		if (priv_donor->metadata != NULL) {
+			GHashTableIter iter;
+			gpointer key, value;
+			g_hash_table_iter_init(&iter, priv_donor->metadata);
+			while (g_hash_table_iter_next(&iter, &key, &value)) {
+				if (fu_device_get_metadata(self, key) == NULL)
+					fu_device_set_metadata(self, key, value);
+			}
+		}
+		if (priv_donor->events != NULL && donor != priv->proxy) {
+			for (guint i = 0; i < priv_donor->events->len; i++) {
+				FuDeviceEvent *event = g_ptr_array_index(priv_donor->events, i);
+				fu_device_add_event(self, event);
+			}
+		}
 
-	/* now the base class, where all the interesting bits are */
-	fwupd_device_incorporate(FWUPD_DEVICE(self), FWUPD_DEVICE(donor));
+		/* probably not required, but seems safer */
+		for (guint i = 0; i < priv_donor->possible_plugins->len; i++) {
+			const gchar *possible_plugin =
+			    g_ptr_array_index(priv_donor->possible_plugins, i);
+			fu_device_add_possible_plugin(self, possible_plugin);
+		}
+		if (priv_donor->instance_id_quirks != NULL) {
+			for (guint i = 0; i < priv_donor->instance_id_quirks->len; i++) {
+				const gchar *instance_id =
+				    g_ptr_array_index(priv_donor->instance_id_quirks, i);
+				fu_device_add_instance_id_full(self,
+							       instance_id,
+							       FU_DEVICE_INSTANCE_FLAG_QUIRKS);
+			}
+		}
 
-	/* remove the baseclass-added serial number if set */
-	if (fu_device_has_private_flag(self, FU_DEVICE_PRIVATE_FLAG_NO_SERIAL_NUMBER))
-		fwupd_device_set_serial(FWUPD_DEVICE(self), NULL);
+		/* copy all instance ID keys if not already set */
+		if (priv_donor->instance_hash != NULL) {
+			GHashTableIter iter;
+			gpointer key, value;
+			g_hash_table_iter_init(&iter, priv_donor->instance_hash);
+			while (g_hash_table_iter_next(&iter, &key, &value)) {
+				if (fu_device_get_instance_str(self, key) == NULL)
+					fu_device_add_instance_str(self, key, value);
+			}
+		}
 
-	/* remove the baseclass-added GUIDs */
-	if (fu_device_has_private_flag(self, FU_DEVICE_PRIVATE_FLAG_NO_GENERIC_GUIDS))
-		g_ptr_array_set_size(fwupd_device_get_instance_ids(FWUPD_DEVICE(self)), 0);
-
-	/* set by the superclass */
-	if (fu_device_get_id(self) != NULL)
-		priv->device_id_valid = TRUE;
-
-	/* optional subclass */
-	fu_device_incorporate_subclasses(self, donor);
-
-	/* call the set_quirk_kv() vfunc for the superclassed object */
-	for (guint i = 0; i < instance_ids->len; i++) {
-		const gchar *instance_id = g_ptr_array_index(instance_ids, i);
-		g_autofree gchar *guid = fwupd_guid_hash_string(instance_id);
-		fu_device_add_guid_quirks(self, guid);
+		/* call the set_quirk_kv() vfunc for the superclassed object */
+		for (guint i = 0; i < instance_ids->len; i++) {
+			const gchar *instance_id = g_ptr_array_index(instance_ids, i);
+			g_autofree gchar *guid = fwupd_guid_hash_string(instance_id);
+			fu_device_add_guid_quirks(self, guid);
+		}
 	}
 }
 

libfwupdplugin/fu-device.h
@@ -111,6 +111,112 @@ typedef enum {
 } FuDeviceInstanceFlags;
 
 /**
+ * FuDeviceIncorporateFlags:
+ *
+ * The flags to use when incorporating a device instance.
+ **/
+typedef enum {
+	/**
+	 * FU_DEVICE_INCORPORATE_FLAG_BASECLASS:
+	 *
+	 * Set baseclass properties.
+	 *
+	 * Since: 2.0.0
+	 **/
+	FU_DEVICE_INCORPORATE_FLAG_BASECLASS = 1ull << 0,
+	/**
+	 * FU_DEVICE_INCORPORATE_FLAG_SUPERCLASS:
+	 *
+	 * Set superclass properties, implemented using `->incorporate()`.
+	 *
+	 * Since: 2.0.0
+	 **/
+	FU_DEVICE_INCORPORATE_FLAG_SUPERCLASS = 1ull << 1,
+	/**
+	 * FU_DEVICE_INCORPORATE_FLAG_VENDOR:
+	 *
+	 * Set vendor.
+	 *
+	 * Since: 2.0.0
+	 **/
+	FU_DEVICE_INCORPORATE_FLAG_VENDOR = 1ull << 2,
+	/**
+	 * FU_DEVICE_INCORPORATE_FLAG_VENDOR_IDS:
+	 *
+	 * Set vendor IDs.
+	 *
+	 * Since: 2.0.0
+	 **/
+	FU_DEVICE_INCORPORATE_FLAG_VENDOR_IDS = 1ull << 3,
+	/**
+	 * FU_DEVICE_INCORPORATE_FLAG_PHYSICAL_ID:
+	 *
+	 * Set the physical ID.
+	 *
+	 * Since: 2.0.0
+	 **/
+	FU_DEVICE_INCORPORATE_FLAG_PHYSICAL_ID = 1ull << 4,
+	/**
+	 * FU_DEVICE_INCORPORATE_FLAG_LOGICAL_ID:
+	 *
+	 * Set the logical ID.
+	 *
+	 * Since: 2.0.0
+	 **/
+	FU_DEVICE_INCORPORATE_FLAG_LOGICAL_ID = 1ull << 5,
+	/**
+	 * FU_DEVICE_INCORPORATE_FLAG_BACKEND_ID:
+	 *
+	 * Set the backend ID.
+	 *
+	 * Since: 2.0.0
+	 **/
+	FU_DEVICE_INCORPORATE_FLAG_BACKEND_ID = 1ull << 6,
+	/**
+	 * FU_DEVICE_INCORPORATE_FLAG_REMOVE_DELAY:
+	 *
+	 * Set the remove delay.
+	 *
+	 * Since: 2.0.0
+	 **/
+	FU_DEVICE_INCORPORATE_FLAG_REMOVE_DELAY = 1ull << 7,
+	/**
+	 * FU_DEVICE_INCORPORATE_FLAG_ACQUIESCE_DELAY:
+	 *
+	 * Set the acquiesce delay.
+	 *
+	 * Since: 2.0.0
+	 **/
+	FU_DEVICE_INCORPORATE_FLAG_ACQUIESCE_DELAY = 1ull << 8,
+	/**
+	 * FU_DEVICE_INCORPORATE_FLAG_ICONS:
+	 *
+	 * Set the icons.
+	 *
+	 * Since: 2.0.0
+	 **/
+	FU_DEVICE_INCORPORATE_FLAG_ICONS = 1ull << 9,
+	/**
+	 * FU_DEVICE_INCORPORATE_FLAG_UPDATE_ERROR:
+	 *
+	 * Set the update error.
+	 *
+	 * Since: 2.0.0
+	 **/
+	FU_DEVICE_INCORPORATE_FLAG_UPDATE_ERROR = 1ull << 10,
+	/**
+	 * FU_DEVICE_INCORPORATE_FLAG_UPDATE_STATE:
+	 *
+	 * Set the update state.
+	 *
+	 * Since: 2.0.0
+	 **/
+	FU_DEVICE_INCORPORATE_FLAG_UPDATE_STATE = 1ull << 11,
+	/*< private >*/
+	FU_DEVICE_INCORPORATE_FLAG_ALL = G_MAXUINT64,
+} FuDeviceIncorporateFlags;
+
+/**
  * FU_DEVICE_REMOVE_DELAY_RE_ENUMERATE:
  *
  * The default removal delay for device re-enumeration taking into account a
@@ -850,9 +956,8 @@ gboolean
 fu_device_cleanup(FuDevice *self, FuProgress *progress, FwupdInstallFlags flags, GError **error)
     G_GNUC_WARN_UNUSED_RESULT G_GNUC_NON_NULL(1, 2);
 void
-fu_device_incorporate(FuDevice *self, FuDevice *donor) G_GNUC_NON_NULL(1);
-void
-fu_device_incorporate_vendor_ids(FuDevice *self, FuDevice *donor) G_GNUC_NON_NULL(1, 2);
+fu_device_incorporate(FuDevice *self, FuDevice *donor, FuDeviceIncorporateFlags flag)
+    G_GNUC_NON_NULL(1, 2);
 void
 fu_device_incorporate_flag(FuDevice *self, FuDevice *donor, FwupdDeviceFlags flag)
     G_GNUC_NON_NULL(1);

libfwupdplugin/fu-mei-device.c
@@ -130,12 +130,14 @@ fu_mei_device_pci_probe(FuMeiDevice *self, GError **error)
 		return FALSE;
 	if (!fu_device_probe(pci_donor, error))
 		return FALSE;
-	fu_device_set_physical_id(FU_DEVICE(self), fu_device_get_physical_id(pci_donor));
 	fu_udev_device_set_vendor(FU_UDEV_DEVICE(self),
 				  fu_udev_device_get_vendor(FU_UDEV_DEVICE(pci_donor)));
 	fu_udev_device_set_model(FU_UDEV_DEVICE(self),
 				 fu_udev_device_get_model(FU_UDEV_DEVICE(pci_donor)));
-	fu_device_incorporate_vendor_ids(FU_DEVICE(self), pci_donor);
+	fu_device_incorporate(FU_DEVICE(self),
+			      pci_donor,
+			      FU_DEVICE_INCORPORATE_FLAG_VENDOR_IDS |
+				  FU_DEVICE_INCORPORATE_FLAG_PHYSICAL_ID);
 
 	/* success */
 	return TRUE;

commit 7986c845f969a50e9c5a3e03a69971f20385834d

Never unset the emulation-tag flag
This regressed in 20c11732...

src/fu-device-list.c
@@ -720,7 +720,10 @@ fu_device_list_replace(FuDeviceList *self, FuDeviceItem *item, FuDevice *device)
 	/* seems like a sane assumption if we've tagged the runtime mode as signed */
 	fu_device_incorporate_flag(device, item->device, FWUPD_DEVICE_FLAG_SIGNED_PAYLOAD);
 	fu_device_incorporate_flag(device, item->device, FWUPD_DEVICE_FLAG_UNSIGNED_PAYLOAD);
-	fu_device_incorporate_flag(device, item->device, FWUPD_DEVICE_FLAG_EMULATION_TAG);
+
+	/* never unset */
+	if (fu_device_has_flag(item->device, FWUPD_DEVICE_FLAG_EMULATION_TAG))
+		fu_device_add_flag(device, FWUPD_DEVICE_FLAG_EMULATION_TAG);
 
 	/* device won't come back in right mode */
 	fu_device_incorporate_flag(device, item->device, FWUPD_DEVICE_FLAG_WILL_DISAPPEAR);