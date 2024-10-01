3f2f727eee89b1e0d59cb3a08fa868c843d6e71c
Richard Hughes <richard@hughsie.com>
2024-10-01 19:35:00 +0100

    trivial: Check the quirk GTypes are valid

--- contrib/ci/check-quirks.py
+++ contrib/ci/check-quirks.py
@@ -3,14 +3,41 @@
 # Copyright 2021 Richard Hughes <richard@hughsie.com>
 #
 # SPDX-License-Identifier: LGPL-2.1-or-later
+#
+# pylint: disable=missing-module-docstring,missing-function-docstring,invalid-name,too-many-branches
 
 import glob
 import sys
+from typing import List
+
+
+def gtype_is_valid(gtype: str) -> bool:
+    if not gtype:
+        return False
+    for token in ["Plugin", "Firmware", "Backend", "Image"]:
+        if gtype.endswith(token):
+            return False
+    return True
 
 
 def test_files() -> int:
     rc: int = 0
 
+    gtypes: List[str] = []
+
+    # find the possible GTypes
+    for fn in glob.glob("libfwupdplugin/fu-*-device.c") + glob.glob("plugins/*/fu-*.c"):
+        with open(fn) as f:
+            for line in f.read().split("\n"):
+                gtype: str = ""
+                if line.startswith("G_DEFINE_TYPE("):
+                    gtype = line[14:].split(",")[0]
+                if line.startswith("G_DEFINE_TYPE_WITH_PRIVATE("):
+                    gtype = line[27:].split(",")[0]
+                if gtype_is_valid(gtype):
+                    gtypes.append(gtype)
+
+    # check each quirk file
     for fn in glob.glob("data/*.quirk") + glob.glob("plugins/*/*.quirk"):
         with open(fn) as f:
             for line in f.read().split("\n"):
@@ -29,6 +56,10 @@ def test_files() -> int:
                     sections = line.split(" = ")
                     if len(sections) != 2:
                         print(f"{fn} has invalid line: {line}")
+                        rc = 1
+                        continue
+                    if sections[0] == "GType" and sections[1] not in gtypes:
+                        print(f"{fn} has invalid GType: {line}")
                         rc = 1
                         continue
                     for section in sections:
