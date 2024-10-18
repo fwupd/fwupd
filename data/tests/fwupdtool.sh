71222704229a673000a0f09176beefc870e5cb84
Mario Limonciello <mario.limonciello@amd.com>
2024-10-18 00:08:20 -0500

    trivial: add unit tests for version format commands

--- data/tests/fwupdtool.sh
+++ data/tests/fwupdtool.sh
@@ -56,3 +56,18 @@ rc=$?; if [ $rc != 2 ]; then exit $rc; fi
 echo "Resetting config..."
 fwupdtool reset-config test
 rc=$?; if [ $rc != 0 ]; then exit $rc; fi
+
+# ---
+echo "Testing good version compare"
+fwupdtool vercmp 1.0.0 1.0.0 triplet
+rc=$?; if [ $rc != 0 ]; then exit $rc; fi
+
+# ---
+echo "Testing bad version compare"
+fwupdtool vercmp 1.0.0 1.0.1 foo
+rc=$?; if [ $rc != 1 ]; then exit $rc; fi
+
+# ---
+echo "Getting supported version formats..."
+fwupdtool get-version-formats --json
+rc=$?; if [ $rc != 0 ]; then exit $rc; fi
