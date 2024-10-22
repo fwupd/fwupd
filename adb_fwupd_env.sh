a1b80376a6d270799b225c12c4b564ee7c2b8f1d
Colin Kinloch <colin.kinloch@collabora.com>
2024-10-22 13:48:36 +0100

    Add build instructions for NDK
    * Add script to push builds to android
    * Remove extra environment variables from adb env script

--- adb_fwupd_env.sh
+++ adb_fwupd_env.sh
@@ -7,14 +7,19 @@
 # ./adb_fwupd_env.sh fwupdtool get-devices -vv
 # ./adb_fwupd_env.sh fwupd-binder --verbose
 
-FWUPD_DIST_PATH="/system_ext/bin/fwupd"
+#FWUPD_DIST_PATH="/system_ext/bin/fwupd"
+FWUPD_DIST_PATH="/data/fwupd"
+
+
+# These were set but fwupd exits if prefix cache dir is `ro`
+#  so better to use meson install --distdir instead
+# FWUPD_SYSCONFDIR="${FWUPD_DIST_PATH}/etc" \
+# CACHE_DIRECTORY="${FWUPD_DIST_PATH}/var/cache/fwupd" \
+# FWUPD_DATADIR="${FWUPD_DIST_PATH}/usr/share/fwupd" \
+# FWUPD_LIBEXECDIR="${FWUPD_DIST_PATH}/libexec" \
 
 adb -e shell -t \
  FWUPD_POLKIT_NOCHECK=1 \
- FWUPD_SYSCONFDIR="${FWUPD_DIST_PATH}/etc" \
- CACHE_DIRECTORY="${FWUPD_DIST_PATH}/var/cache/fwupd" \
- FWUPD_DATADIR="${FWUPD_DIST_PATH}/usr/share/fwupd" \
- FWUPD_LIBEXECDIR="${FWUPD_DIST_PATH}/libexec" \
  LD_LIBRARY_PATH="${FWUPD_DIST_PATH}/lib/fwupd-2.0.2:${FWUPD_DIST_PATH}/lib64:${FWUPD_DIST_PATH}/lib" \
  PATH="\${PATH}:${FWUPD_DIST_PATH}/bin:${FWUPD_DIST_PATH}/libexec/fwupd" \
  "${*}"
