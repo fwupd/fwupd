ad3075202db3d821f7fa4de73e021cd2d792f5c1
Richard Hughes <richard@hughsie.com>
2024-11-06 10:14:45 +0000

    trivial: Ignore more impossible coverage lines

--- contrib/ci/coverage.sh
+++ contrib/ci/coverage.sh
@@ -11,6 +11,6 @@ gcovr -x \
 	--filter build/libfwupdplugin \
 	--filter build/plugins \
 	--filter build/src \
-	--exclude-lines-by-pattern '^.*(g_assert_cmpfloat_with_epsilon|g_assert_cmpint|g_assert_cmpstr|g_assert_cmpuint|g_assert_error|g_assert_false|g_assert_no_error|g_assert_nonnull|g_assert_not_reached|g_assert_null|g_assert_true|g_return_if_fail|g_return_val_if_fail).*$' \
+	--exclude-lines-by-pattern '^.*(G_OBJECT_WARN_INVALID|G_DEFINE_TYPE|g_autoptr|g_assert_cmpfloat_with_epsilon|g_assert_cmpint|g_assert_cmpstr|g_assert_cmpuint|g_assert_error|g_assert_false|g_assert_no_error|g_assert_nonnull|g_assert_not_reached|g_assert_null|g_assert_true|g_return_if_fail|g_return_val_if_fail).*$' \
 	-o coverage.xml
 sed "s,build/,,g" coverage.xml -i
