a995a9412cb6f7c7cc5fa16561948561e256b258
Richard Hughes <richard@hughsie.com>
2024-11-07 19:08:08 +0000

    tpm: Never set the report TpmFamily if the family is unknown
    (cherry picked from commit b88480a87fa2cf10e0781cd81ecf43a0f6d1237b)

--- plugins/tpm/fu-tpm-plugin.c
+++ plugins/tpm/fu-tpm-plugin.c
@@ -68,11 +68,11 @@ fu_tpm_plugin_device_added(FuPlugin *plugin, FuDevice *dev)
 {
 	FuTpmPlugin *self = FU_TPM_PLUGIN(plugin);
 	g_autoptr(GPtrArray) pcr0s = NULL;
+	const gchar *family = fu_tpm_device_get_family(FU_TPM_DEVICE(dev));
 
 	g_set_object(&self->tpm_device, FU_TPM_DEVICE(dev));
-	fu_plugin_add_report_metadata(plugin,
-				      "TpmFamily",
-				      fu_tpm_device_get_family(FU_TPM_DEVICE(dev)));
+	if (family != NULL)
+		fu_plugin_add_report_metadata(plugin, "TpmFamily", family);
 
 	/* ensure */
 	fu_tpm_plugin_set_bios_pcr0s(plugin);
