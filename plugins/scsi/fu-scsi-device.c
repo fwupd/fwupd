fwupd/fwupd

commit e915154a8bffa40afb8055b20415e0b4e774b3c6

scsi: Do SG_IO INQUIRY_CMD rather than relying on the udev prober
This allows us to emulate the device, and also means we can work without udev.

plugins/scsi/fu-scsi-device.c
@@ -18,6 +18,10 @@ struct _FuScsiDevice {
 
 G_DEFINE_TYPE(FuScsiDevice, fu_scsi_device, FU_TYPE_UDEV_DEVICE)
 
+#define INQUIRY_CMD	  0x12
+#define INQUIRY_CMDLEN	  6
+#define SCSI_INQ_BUFF_LEN 254
+
 #define BUFFER_VENDOR_MODE 0x01
 #define BUFFER_DUFS_MODE   0x02
 #define BUFFER_FFU_MODE	   0x0E
@@ -133,22 +137,6 @@ fu_scsi_device_probe(FuDevice *device, GError **error)
 		}
 	}
 
-	/* add GUIDs */
-	fu_device_add_instance_strsafe(device, "VEN", fu_device_get_vendor(device));
-	fu_device_add_instance_strsafe(device, "DEV", fu_device_get_name(device));
-	fu_device_add_instance_strsafe(device, "REV", fu_device_get_version(device));
-	if (!fu_device_build_instance_id_full(device,
-					      FU_DEVICE_INSTANCE_FLAG_QUIRKS,
-					      error,
-					      "SCSI",
-					      "VEN",
-					      NULL))
-		return FALSE;
-	if (!fu_device_build_instance_id(device, error, "SCSI", "VEN", "DEV", NULL))
-		return FALSE;
-	if (!fu_device_build_instance_id(device, error, "SCSI", "VEN", "DEV", "REV", NULL))
-		return FALSE;
-
 	/* is internal? */
 	attr_removable = fu_udev_device_read_sysfs(FU_UDEV_DEVICE(self),
 						   "removable",
@@ -229,6 +217,67 @@ fu_scsi_device_send_scsi_cmd_v3(FuScsiDevice *self,
 			    sense_buffer[13]);
 		return FALSE;
 	}
+
+	/* success */
+	return TRUE;
+}
+
+static gboolean
+fu_scsi_device_setup(FuDevice *device, GError **error)
+{
+	FuScsiDevice *self = FU_SCSI_DEVICE(device);
+	guint8 buf[SCSI_INQ_BUFF_LEN] = {0};
+	guint8 cdb[INQUIRY_CMDLEN] = {
+	    INQUIRY_CMD,
+	    0, /* evpd */
+	    0, /* page */
+	    0,
+	    sizeof(buf),
+	    0,
+	};
+	g_autofree gchar *model = NULL;
+	g_autofree gchar *revision = NULL;
+	g_autofree gchar *vendor = NULL;
+
+	/* prepare chunk */
+	if (!fu_scsi_device_send_scsi_cmd_v3(self,
+					     cdb,
+					     sizeof(cdb),
+					     buf,
+					     sizeof(buf),
+					     SG_DXFER_FROM_DEV,
+					     error)) {
+		g_prefix_error(error, "SG_IO INQUIRY_CMD data error: ");
+		return FALSE;
+	}
+	fu_dump_raw(G_LOG_DOMAIN, "INQUIRY", buf, sizeof(buf));
+
+	/* parse */
+	vendor = fu_strsafe((const gchar *)buf + 8, 8);
+	if (vendor != NULL)
+		fu_device_set_vendor(device, vendor);
+	model = fu_strsafe((const gchar *)buf + 16, 8);
+	if (model != NULL)
+		fu_device_set_name(device, model);
+	revision = fu_strsafe((const gchar *)buf + 32, 4);
+	if (revision != NULL)
+		fu_device_set_version(device, revision);
+
+	/* add GUIDs */
+	fu_device_add_instance_str(device, "VEN", vendor);
+	fu_device_add_instance_str(device, "DEV", model);
+	fu_device_add_instance_str(device, "REV", revision);
+	if (!fu_device_build_instance_id_full(device,
+					      FU_DEVICE_INSTANCE_FLAG_QUIRKS,
+					      error,
+					      "SCSI",
+					      "VEN",
+					      NULL))
+		return FALSE;
+	if (!fu_device_build_instance_id(device, error, "SCSI", "VEN", "DEV", NULL))
+		return FALSE;
+	if (!fu_device_build_instance_id(device, error, "SCSI", "VEN", "DEV", "REV", NULL))
+		return FALSE;
 
 	/* success */
 	return TRUE;
@@ -325,6 +374,7 @@ fu_scsi_device_class_init(FuScsiDeviceClass *klass)
 	FuDeviceClass *device_class = FU_DEVICE_CLASS(klass);
 	device_class->to_string = fu_scsi_device_to_string;
 	device_class->probe = fu_scsi_device_probe;
+	device_class->setup = fu_scsi_device_setup;
 	device_class->prepare_firmware = fu_scsi_device_prepare_firmware;
 	device_class->write_firmware = fu_scsi_device_write_firmware;
 	device_class->set_progress = fu_scsi_device_set_progress;

commit 9b2bef89002c61776ade9b017cc5f9f42ff871d3

plugins: scsi: Fix SCSI INQUIRY DATA decoding
The Product ID is 16 bytes long.

Using Samsung UFS KLUDG4UHGC-B0E1 as an example:

Before:
??KLUDG4UH:
      Device ID:          645dd3eabfbffd6d82917cc4a4cddc0487c3187d
      Vendor:             SAMSUNG (SCSI:SAMSUNG)
      GUIDs:              906223b2-9aee-5f6a-b78c-facb87a09056 ?  SCSI\VEN_SAMSUNG&DEV_KLUDG4UH
After: (as expected and like 1.9.22 branch)
??KLUDG4UHGC-B0E1:
      Device ID:          645dd3eabfbffd6d82917cc4a4cddc0487c3187d
      Vendor:             SAMSUNG (SCSI:SAMSUNG)
      GUIDs:              41a057a2-a1ff-5764-bc6f-71aa045706ff ?  SCSI\VEN_SAMSUNG&DEV_KLUDG4UHGC-B0E1

fixes: e915154a8bf ("scsi: Do SG_IO INQUIRY_CMD rather than relying on the udev prober")
Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

plugins/scsi/fu-scsi-device.c
@@ -304,7 +304,7 @@ fu_scsi_device_setup(FuDevice *device, GError **error)
 	vendor = fu_strsafe((const gchar *)buf + 8, 8);
 	if (vendor != NULL)
 		fu_device_set_vendor(device, vendor);
-	model = fu_strsafe((const gchar *)buf + 16, 8);
+	model = fu_strsafe((const gchar *)buf + 16, 16);
 	if (model != NULL)
 		fu_device_set_name(device, model);
 	revision = fu_strsafe((const gchar *)buf + 32, 4);