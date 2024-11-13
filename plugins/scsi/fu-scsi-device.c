fwupd/fwupd

commit afb22f7cc0990fe9d57dbabd3e5ecc8743b27108

trivial: Convert from fu_udev_device_get_sysfs_attr_uint64() to fu_udev_device_read_sysfs()
This allows the caller to convert the value in whatever base is more appropriate.

plugins/scsi/fu-scsi-device.c
@@ -43,7 +43,7 @@ fu_scsi_device_probe(FuDevice *device, GError **error)
 {
 	FuScsiDevice *self = FU_SCSI_DEVICE(device);
 	GUdevDevice *udev_device = fu_udev_device_get_dev(FU_UDEV_DEVICE(device));
-	guint64 removable = 0;
+	g_autofree gchar *attr_removable = NULL;
 	g_autofree gchar *vendor_id = NULL;
 	g_autoptr(FuUdevDevice) ufshci_parent = NULL;
 	const gchar *subsystem_parents[] = {"pci", "platform", NULL};
@@ -85,30 +85,51 @@ fu_scsi_device_probe(FuDevice *device, GError **error)
 									 NULL);
 	}
 	if (ufshci_parent != NULL) {
-		guint64 ufs_features = 0;
+		g_autofree gchar *attr_ufs_features = NULL;
+		g_autofree gchar *attr_ffu_timeout = NULL;
 
 		/* check if this is a UFS device */
 		g_info("found ufshci controller at %s",
 		       fu_udev_device_get_sysfs_path(ufshci_parent));
-		if (fu_udev_device_get_sysfs_attr_uint64(ufshci_parent,
-							 "device_descriptor/ufs_features",
-							 &ufs_features,
-							 NULL)) {
+
+		attr_ufs_features =
+		    fu_udev_device_read_sysfs(FU_UDEV_DEVICE(self),
+					      "device_descriptor/ufs_features",
+					      FU_UDEV_DEVICE_ATTR_READ_TIMEOUT_DEFAULT,
+					      NULL);
+		if (attr_ufs_features != NULL) {
+			guint64 ufs_features = 0;
 			fu_device_set_summary(device, "UFS device");
 			/* least significant bit specifies FFU capability */
+			if (!fu_strtoull(attr_ufs_features,
+					 &ufs_features,
+					 0,
+					 G_MAXUINT64,
+					 FU_INTEGER_BASE_AUTO,
+					 error))
+				return FALSE;
 			if (ufs_features & 0x1) {
 				fu_device_add_flag(device, FWUPD_DEVICE_FLAG_UPDATABLE);
 				fu_device_add_internal_flag(FU_DEVICE(self),
 							    FU_DEVICE_INTERNAL_FLAG_MD_SET_SIGNED);
 				fu_device_add_protocol(device, "org.jedec.ufs");
 			}
-			if (!fu_udev_device_get_sysfs_attr_uint64(ufshci_parent,
-								  "device_descriptor/ffu_timeout",
-								  &self->ffu_timeout,
-								  error)) {
+			attr_ffu_timeout =
+			    fu_udev_device_read_sysfs(FU_UDEV_DEVICE(self),
+						      "device_descriptor/ffu_timeout",
+						      FU_UDEV_DEVICE_ATTR_READ_TIMEOUT_DEFAULT,
+						      error);
+			if (attr_ffu_timeout == NULL) {
 				g_prefix_error(error, "no ffu timeout specified: ");
 				return FALSE;
 			}
+			if (!fu_strtoull(attr_ffu_timeout,
+					 &self->ffu_timeout,
+					 0,
+					 G_MAXUINT64,
+					 FU_INTEGER_BASE_AUTO,
+					 error))
+				return FALSE;
 		}
 	}
 
@@ -129,10 +150,19 @@ fu_scsi_device_probe(FuDevice *device, GError **error)
 		return FALSE;
 
 	/* is internal? */
-	if (fu_udev_device_get_sysfs_attr_uint64(FU_UDEV_DEVICE(device),
-						 "removable",
-						 &removable,
-						 NULL)) {
+	attr_removable = fu_udev_device_read_sysfs(FU_UDEV_DEVICE(self),
+						   "removable",
+						   FU_UDEV_DEVICE_ATTR_READ_TIMEOUT_DEFAULT,
+						   NULL);
+	if (attr_removable != NULL) {
+		guint64 removable = 0;
+		if (!fu_strtoull(attr_removable,
+				 &removable,
+				 0,
+				 G_MAXUINT64,
+				 FU_INTEGER_BASE_AUTO,
+				 error))
+			return FALSE;
 		if (removable == 0x0)
 			fu_device_add_flag(device, FWUPD_DEVICE_FLAG_INTERNAL);
 	}

libfwupdplugin/fu-udev-device.c
@@ -2226,37 +2226,6 @@ fu_udev_device_get_sysfs_attr(FuUdevDevice *self, const gchar *attr, GError **er
 }
 
 /**
- * fu_udev_device_get_sysfs_attr_uint64:
- * @self: a #FuUdevDevice
- * @attr: name of attribute to get
- * @value: (out) (optional): value to return
- * @error: (nullable): optional return location for an error
- *
- * Reads an arbitrary sysfs attribute 'attr' associated with UDEV device as a uint64.
- *
- * Returns: %TRUE for success
- *
- * Since: 1.7.2
- **/
-gboolean
-fu_udev_device_get_sysfs_attr_uint64(FuUdevDevice *self,
-				     const gchar *attr,
-				     guint64 *value,
-				     GError **error)
-{
-	const gchar *tmp;
-
-	g_return_val_if_fail(FU_IS_UDEV_DEVICE(self), FALSE);
-	g_return_val_if_fail(attr != NULL, FALSE);
-	g_return_val_if_fail(error == NULL || *error == NULL, FALSE);
-
-	tmp = fu_udev_device_get_sysfs_attr(self, attr, error);
-	if (tmp == NULL)
-		return FALSE;
-	return fu_strtoull(tmp, value, 0, G_MAXUINT64, FU_INTEGER_BASE_AUTO, error);
-}
-
-/**
  * fu_udev_device_write_sysfs:
  * @self: a #FuUdevDevice
  * @attr: sysfs attribute name

libfwupdplugin/fu-udev-device.h
@@ -195,11 +195,6 @@ fu_udev_device_seek(FuUdevDevice *self, goffset offset, GError **error) G_GNUC_W
 const gchar *
 fu_udev_device_get_sysfs_attr(FuUdevDevice *self, const gchar *attr, GError **error)
     G_GNUC_NON_NULL(1);
-gboolean
-fu_udev_device_get_sysfs_attr_uint64(FuUdevDevice *self,
-				     const gchar *attr,
-				     guint64 *value,
-				     GError **error) G_GNUC_NON_NULL(1);
 gchar *
 fu_udev_device_read_sysfs(FuUdevDevice *self, const gchar *attr, guint timeout_ms, GError **error)
     G_GNUC_WARN_UNUSED_RESULT G_GNUC_NON_NULL(1, 2);

plugins/amd-pmc/fu-amd-pmc-device.c
@@ -21,7 +21,8 @@ G_DEFINE_TYPE(FuAmdPmcDevice, fu_amd_pmc_device, FU_TYPE_UDEV_DEVICE)
 static gboolean
 fu_amd_pmc_device_probe(FuDevice *device, GError **error)
 {
-	guint64 program;
+	guint64 program = 0;
+	g_autofree gchar *attr_smu_program = NULL;
 	const gchar *version;
 	g_autoptr(GError) error_local = NULL;
 	g_autofree gchar *summary = NULL;
@@ -40,10 +41,13 @@ fu_amd_pmc_device_probe(FuDevice *device, GError **error)
 		return FALSE;
 	}
 
-	if (!fu_udev_device_get_sysfs_attr_uint64(FU_UDEV_DEVICE(device),
-						  "smu_program",
-						  &program,
-						  error))
+	attr_smu_program = fu_udev_device_read_sysfs(FU_UDEV_DEVICE(device),
+						     "smu_program",
+						     FU_UDEV_DEVICE_ATTR_READ_TIMEOUT_DEFAULT,
+						     error);
+	if (attr_smu_program == NULL)
+		return FALSE;
+	if (!fu_strtoull(attr_smu_program, &program, 0, G_MAXUINT64, FU_INTEGER_BASE_AUTO, error))
 		return FALSE;
 
 	fu_device_set_version(device, version);

plugins/emmc/fu-emmc-device.c
@@ -109,66 +109,58 @@ fu_emmc_device_get_manufacturer(guint64 mmc_id)
 }
 
 static gboolean
-fu_emmc_device_get_sysattr_guint64(GUdevDevice *device,
+fu_emmc_device_get_sysattr_guint64(FuUdevDevice *device,
 				   const gchar *name,
 				   guint64 *val_out,
 				   GError **error)
 {
-	const gchar *sysfs;
+	g_autofree gchar *value = NULL;
 
-	sysfs = g_udev_device_get_sysfs_attr(device, name);
-	if (sysfs == NULL) {
-		g_set_error(error, FWUPD_ERROR, FWUPD_ERROR_INTERNAL, "failed get %s", name);
+	value = fu_udev_device_read_sysfs(device,
+					  name,
+					  FU_UDEV_DEVICE_ATTR_READ_TIMEOUT_DEFAULT,
+					  error);
+	if (value == NULL)
 		return FALSE;
-	}
-	return fu_strtoull(sysfs, val_out, 0, G_MAXUINT64, FU_INTEGER_BASE_16, error);
+	return fu_strtoull(value, val_out, 0, G_MAXUINT64, FU_INTEGER_BASE_16, error);
 }
 
 static gboolean
 fu_emmc_device_probe(FuDevice *device, GError **error)
 {
-	GUdevDevice *udev_device = fu_udev_device_get_dev(FU_UDEV_DEVICE(device));
 	guint64 flag;
 	guint64 oemid = 0;
 	guint64 manfid = 0;
-	const gchar *tmp;
-	g_autoptr(GUdevDevice) udev_parent = NULL;
+	g_autofree gchar *attr_fwrev = NULL;
+	g_autofree gchar *attr_manfid = NULL;
+	g_autofree gchar *attr_name = NULL;
 	g_autofree gchar *man_oem_name = NULL;
 	g_autofree gchar *vendor_id = NULL;
+	g_autoptr(FuUdevDevice) udev_parent = NULL;
 	g_autoptr(GRegex) dev_regex = NULL;
 
-	udev_parent = g_udev_device_get_parent_with_subsystem(udev_device, "mmc", NULL);
+	udev_parent =
+	    fu_udev_device_get_parent_with_subsystem(FU_UDEV_DEVICE(device), "mmc", "disk", NULL);
 	if (udev_parent == NULL) {
 		g_set_error_literal(error, FWUPD_ERROR, FWUPD_ERROR_NOT_SUPPORTED, "no MMC parent");
 		return FALSE;
 	}
 
-	/* look for only the parent node */
-	if (g_strcmp0(g_udev_device_get_devtype(udev_device), "disk") != 0) {
-		g_set_error(error,
-			    FWUPD_ERROR,
-			    FWUPD_ERROR_NOT_SUPPORTED,
-			    "is not correct devtype=%s, expected disk",
-			    g_udev_device_get_devtype(udev_device));
-		return FALSE;
-	}
-
 	/* ignore *rpmb and *boot* mmc block devices */
 	dev_regex = g_regex_new("mmcblk\\d$", 0, 0, NULL);
-	tmp = g_udev_device_get_name(udev_device);
-	if (tmp == NULL) {
+	if (fu_device_get_name(device) == NULL) {
 		g_set_error_literal(error,
 				    FWUPD_ERROR,
 				    FWUPD_ERROR_NOT_SUPPORTED,
 				    "device has no name");
 		return FALSE;
 	}
-	if (!g_regex_match(dev_regex, tmp, 0, NULL)) {
+	if (!g_regex_match(dev_regex, fu_device_get_name(device), 0, NULL)) {
 		g_set_error(error,
 			    FWUPD_ERROR,
 			    FWUPD_ERROR_NOT_SUPPORTED,
 			    "is not raw mmc block device, devname=%s",
-			    g_udev_device_get_name(udev_device));
+			    fu_device_get_name(device));
 		return FALSE;
 	}
 
@@ -185,8 +177,11 @@ fu_emmc_device_probe(FuDevice *device, GError **error)
 	}
 
 	/* name */
-	tmp = g_udev_device_get_sysfs_attr(udev_parent, "name");
-	if (tmp == NULL) {
+	attr_name = fu_udev_device_read_sysfs(udev_parent,
+					      "name",
+					      FU_UDEV_DEVICE_ATTR_READ_TIMEOUT_DEFAULT,
+					      NULL);
+	if (attr_name == NULL) {
 		g_set_error(error,
 			    FWUPD_ERROR,
 			    FWUPD_ERROR_NOT_SUPPORTED,
@@ -194,17 +189,20 @@ fu_emmc_device_probe(FuDevice *device, GError **error)
 			    fu_device_get_name(device));
 		return FALSE;
 	}
-	fu_device_add_instance_strsafe(device, "NAME", tmp);
+	fu_device_add_instance_strsafe(device, "NAME", attr_name);
 	fu_device_build_instance_id(device, NULL, "EMMC", "NAME", NULL);
-	fu_device_set_name(device, tmp);
+	fu_device_set_name(device, attr_name);
 
 	/* firmware version */
-	tmp = g_udev_device_get_sysfs_attr(udev_parent, "fwrev");
-	if (tmp != NULL) {
+	attr_fwrev = fu_udev_device_read_sysfs(udev_parent,
+					       "fwrev",
+					       FU_UDEV_DEVICE_ATTR_READ_TIMEOUT_DEFAULT,
+					       NULL);
+	if (attr_fwrev != NULL) {
 		fu_device_set_version_format(device, FWUPD_VERSION_FORMAT_NUMBER);
-		fu_device_set_version(device, tmp);
+		fu_device_set_version(device, attr_fwrev);
 	}
-	fu_device_add_instance_strsafe(device, "REV", tmp);
+	fu_device_add_instance_strsafe(device, "REV", attr_fwrev);
 	if (fu_device_has_internal_flag(device, FU_DEVICE_INTERNAL_FLAG_ADD_INSTANCE_ID_REV))
 		fu_device_build_instance_id(device, NULL, "EMMC", "NAME", "REV", NULL);
 
@@ -234,8 +232,13 @@ fu_emmc_device_probe(FuDevice *device, GError **error)
 	fu_device_add_instance_id(device, man_oem_name);
 
 	/* set the vendor */
-	tmp = g_udev_device_get_sysfs_attr(udev_parent, "manfid");
-	vendor_id = g_strdup_printf("EMMC:%s", tmp);
+	attr_manfid = fu_udev_device_read_sysfs(udev_parent,
+						"manfid",
+						FU_UDEV_DEVICE_ATTR_READ_TIMEOUT_DEFAULT,
+						error);
+	if (attr_manfid == NULL)
+		return FALSE;
+	vendor_id = g_strdup_printf("EMMC:%s", attr_manfid);
 	fu_device_add_vendor_id(device, vendor_id);
 	fu_device_set_vendor(device, fu_emmc_device_get_manufacturer(manfid));
 
@@ -244,7 +247,7 @@ fu_emmc_device_probe(FuDevice *device, GError **error)
 		return FALSE;
 
 	/* internal */
-	if (!fu_emmc_device_get_sysattr_guint64(udev_device, "removable", &flag, error))
+	if (!fu_emmc_device_get_sysattr_guint64(FU_UDEV_DEVICE(device), "removable", &flag, error))
 		return FALSE;
 	if (flag == 0)
 		fu_device_add_flag(device, FWUPD_DEVICE_FLAG_INTERNAL);

plugins/logitech-scribe/fu-logitech-scribe-device.c
@@ -341,8 +341,8 @@ fu_logitech_scribe_device_to_string(FuDevice *device, guint idt, GString *str)
 static gboolean
 fu_logitech_scribe_device_probe(FuDevice *device, GError **error)
 {
+	g_autofree gchar *attr_index = NULL;
 	const gchar *id_v4l_capabilities;
-	const gchar *index;
 	GUdevDevice *udev_device = fu_udev_device_get_dev(FU_UDEV_DEVICE(device));
 
 	/* check is valid */
@@ -367,8 +367,11 @@ fu_logitech_scribe_device_probe(FuDevice *device, GError **error)
 
 	/* interested in lowest index only e,g, video0, ignore low format siblings like
 	 * video1/video2/video3 etc */
-	index = g_udev_device_get_sysfs_attr(udev_device, "index");
-	if (g_strcmp0(index, "0") != 0) {
+	attr_index = fu_udev_device_read_sysfs(FU_UDEV_DEVICE(device),
+					       "index",
+					       FU_UDEV_DEVICE_ATTR_READ_TIMEOUT_DEFAULT,
+					       NULL);
+	if (g_strcmp0(attr_index, "0") != 0) {
 		g_set_error_literal(error,
 				    FWUPD_ERROR,
 				    FWUPD_ERROR_NOT_SUPPORTED,

plugins/logitech-tap/fu-logitech-tap-hdmi-device.c
@@ -446,8 +446,8 @@ fu_logitech_tap_hdmi_device_setup(FuDevice *device, GError **error)
 static gboolean
 fu_logitech_tap_hdmi_device_probe(FuDevice *device, GError **error)
 {
+	g_autofree gchar *attr_index = NULL;
 	const gchar *id_v4l_capabilities;
-	const gchar *index;
 	GUdevDevice *udev_device = fu_udev_device_get_dev(FU_UDEV_DEVICE(device));
 
 	/* FuUdevDevice->probe */
@@ -476,8 +476,11 @@ fu_logitech_tap_hdmi_device_probe(FuDevice *device, GError **error)
 
 	/* interested in lowest index only e,g, video0, ignore low format siblings like
 	 * video1/video2/video3 etc */
-	index = g_udev_device_get_sysfs_attr(udev_device, "index");
-	if (g_strcmp0(index, "0") != 0) {
+	attr_index = fu_udev_device_read_sysfs(FU_UDEV_DEVICE(device),
+					       "index",
+					       FU_UDEV_DEVICE_ATTR_READ_TIMEOUT_DEFAULT,
+					       NULL);
+	if (g_strcmp0(attr_index, "0") != 0) {
 		g_set_error_literal(error,
 				    FWUPD_ERROR,
 				    FWUPD_ERROR_NOT_SUPPORTED,

plugins/mtd/fu-mtd-device.c
@@ -189,6 +189,8 @@ fu_mtd_device_probe(FuDevice *device, GError **error)
 	const gchar *vendor;
 	guint64 flags = 0;
 	guint64 size = 0;
+	g_autofree gchar *attr_flags = NULL;
+	g_autofree gchar *attr_size = NULL;
 	g_autoptr(GError) error_local = NULL;
 
 	/* set physical ID */
@@ -196,10 +198,11 @@ fu_mtd_device_probe(FuDevice *device, GError **error)
 		return FALSE;
 
 	/* flags have to exist */
-	if (!fu_udev_device_get_sysfs_attr_uint64(FU_UDEV_DEVICE(device),
-						  "flags",
-						  &flags,
-						  &error_local)) {
+	attr_flags = fu_udev_device_read_sysfs(FU_UDEV_DEVICE(self),
+					       "flags",
+					       FU_UDEV_DEVICE_ATTR_READ_TIMEOUT_DEFAULT,
+					       &error_local);
+	if (attr_flags == NULL) {
 		if (g_error_matches(error_local, FWUPD_ERROR, FWUPD_ERROR_NOT_FOUND)) {
 			g_set_error_literal(error,
 					    FWUPD_ERROR,
@@ -210,6 +213,8 @@ fu_mtd_device_probe(FuDevice *device, GError **error)
 		g_propagate_error(error, g_steal_pointer(&error_local));
 		return FALSE;
 	}
+	if (!fu_strtoull(attr_flags, &flags, 0, G_MAXUINT64, FU_INTEGER_BASE_AUTO, error))
+		return FALSE;
 
 	/* get name */
 	name = fu_udev_device_get_sysfs_attr(FU_UDEV_DEVICE(device), "name", NULL);
@@ -234,15 +239,30 @@ fu_mtd_device_probe(FuDevice *device, GError **error)
 	fu_device_build_instance_id(device, NULL, "MTD", "VENDOR", "PRODUCT", "NAME", NULL);
 
 	/* get properties about the device */
-	if (!fu_udev_device_get_sysfs_attr_uint64(FU_UDEV_DEVICE(device), "size", &size, error))
+	attr_size = fu_udev_device_read_sysfs(FU_UDEV_DEVICE(self),
+					      "size",
+					      FU_UDEV_DEVICE_ATTR_READ_TIMEOUT_DEFAULT,
+					      error);
+	if (attr_size == NULL)
+		return FALSE;
+	if (!fu_strtoull(attr_size, &size, 0, G_MAXUINT64, FU_INTEGER_BASE_AUTO, error))
 		return FALSE;
 	fu_device_set_firmware_size_max(device, size);
 #ifdef HAVE_MTD_USER_H
 	if ((flags & MTD_NO_ERASE) == 0) {
-		if (!fu_udev_device_get_sysfs_attr_uint64(FU_UDEV_DEVICE(device),
-							  "erasesize",
-							  &self->erasesize,
-							  error))
+		g_autofree gchar *attr_erasesize = NULL;
+		attr_erasesize = fu_udev_device_read_sysfs(FU_UDEV_DEVICE(self),
+							   "erasesize",
+							   FU_UDEV_DEVICE_ATTR_READ_TIMEOUT_DEFAULT,
+							   error);
+		if (attr_erasesize == NULL)
+			return FALSE;
+		if (!fu_strtoull(attr_erasesize,
+				 &self->erasesize,
+				 0,
+				 G_MAXUINT64,
+				 FU_INTEGER_BASE_AUTO,
+				 error))
 			return FALSE;
 	}
 	if (flags & MTD_WRITEABLE) {

commit bdff9e98ffe99df4d71f1d6d8190e8d8073a1d9b

plugins: scsi: Fix UFS device feature discovery
Properties "device_descriptor" are attached to the UFS host controller,
not the UFS memory device. Using Samsung UFS KLUDG4UHGC-B0E1 as an
example:

Before:
??KLUDG4UH:
      Device ID:          645dd3eabfbffd6d82917cc4a4cddc0487c3187d
      Summary:            SCSI device
      ...
      Device Flags:       ? Internal device
                          ? Can tag for emulation

After:
      Device ID:          645dd3eabfbffd6d82917cc4a4cddc0487c3187d
      Summary:            UFS device
      ...
      Device Flags:       ? Internal device
                          ? Updatable
                          ? Can tag for emulation

fixes: afb22f7cc0 ("trivial: Convert from fu_udev_device_get_sysfs_attr_uint64() to fu_udev_device_read_sysfs()")
Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

plugins/scsi/fu-scsi-device.c
@@ -69,7 +69,7 @@ fu_scsi_device_probe(FuDevice *device, GError **error)
 		       fu_udev_device_get_sysfs_path(FU_UDEV_DEVICE(ufshci_parent)));
 
 		attr_ufs_features =
-		    fu_udev_device_read_sysfs(FU_UDEV_DEVICE(self),
+		    fu_udev_device_read_sysfs(FU_UDEV_DEVICE(ufshci_parent),
 					      "device_descriptor/ufs_features",
 					      FU_UDEV_DEVICE_ATTR_READ_TIMEOUT_DEFAULT,
 					      NULL);
@@ -91,7 +91,7 @@ fu_scsi_device_probe(FuDevice *device, GError **error)
 				fu_device_add_protocol(device, "org.jedec.ufs");
 			}
 			attr_ffu_timeout =
-			    fu_udev_device_read_sysfs(FU_UDEV_DEVICE(self),
+			    fu_udev_device_read_sysfs(FU_UDEV_DEVICE(ufshci_parent),
 						      "device_descriptor/ffu_timeout",
 						      FU_UDEV_DEVICE_ATTR_READ_TIMEOUT_DEFAULT,
 						      error);