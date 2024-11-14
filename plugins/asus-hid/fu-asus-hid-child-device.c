3a3957699a37d17d72a59e4f60785b26b3ae5055
Mario Limonciello <mario.limonciello@amd.com>
2024-11-14 00:00:51 -0600

    trivial: asus-hid: set the version format in the child

--- plugins/asus-hid/fu-asus-hid-child-device.c
+++ plugins/asus-hid/fu-asus-hid-child-device.c
@@ -264,6 +264,7 @@ fu_asus_hid_child_device_init(FuAsusHidChildDevice *self)
 	// TODO: is it?
 	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_UNSIGNED_PAYLOAD);
 	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_INTERNAL);
+	fu_device_set_version_format(FU_DEVICE(self), FWUPD_VERSION_FORMAT_PLAIN);
 }
 
 static void
