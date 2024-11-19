e2e1492138a17e48b992ae5e231f29874e7f28cc
Richard Hughes <richard@hughsie.com>
2024-11-19 20:43:17 +0000

    trivial: Fix one translated string

--- src/fu-security-attr-common.c
+++ src/fu-security-attr-common.c
@@ -602,7 +602,7 @@ fu_security_attr_get_description(FwupdSecurityAttr *attr)
 	}
 	if (g_strcmp0(appstream_id, FWUPD_SECURITY_ATTR_ID_AMD_SMM_LOCKED) == 0) {
 		/* TRANSLATORS: Title: Whether firmware is locked down */
-		return g_strdup(_("System management mode is used by the firmware to access"
+		return g_strdup(_("System management mode is used by the firmware to access "
 				  "resident BIOS code and data."));
 	}
 	return NULL;
