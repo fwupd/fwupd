eba98b69250b8871beaa93d868c4bb90b54a0974
Richard Hughes <richard@hughsie.com>
2024-11-20 08:57:55 +0000

    trivial: Fix a translator description

--- src/fu-security-attr-common.c
+++ src/fu-security-attr-common.c
@@ -601,7 +601,7 @@ fu_security_attr_get_description(FwupdSecurityAttr *attr)
 		return _("Enabling firmware updates for the BIOS allows fixing security issues.");
 	}
 	if (g_strcmp0(appstream_id, FWUPD_SECURITY_ATTR_ID_AMD_SMM_LOCKED) == 0) {
-		/* TRANSLATORS: Title: Whether firmware is locked down */
+		/* TRANSLATORS: longer description */
 		return g_strdup(_("System management mode is used by the firmware to access "
 				  "resident BIOS code and data."));
 	}
