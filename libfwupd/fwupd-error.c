55ae4311df099f80472ed53282e87c844ef7ccf0
Richard Hughes <richard@hughsie.com>
2024-12-03 10:05:53 +0000

    trivial: Backport support for FWUPD_ERROR_INVALID_DATA
    This will make plugin backports much easier.

--- libfwupd/fwupd-error.c
+++ libfwupd/fwupd-error.c
@@ -61,6 +61,8 @@ fwupd_error_to_string(FwupdError error)
 		return FWUPD_DBUS_INTERFACE ".NeedsUserAction";
 	if (error == FWUPD_ERROR_AUTH_EXPIRED)
 		return FWUPD_DBUS_INTERFACE ".AuthExpired";
+	if (error == FWUPD_ERROR_INVALID_DATA)
+		return FWUPD_DBUS_INTERFACE ".InvalidData";
 	return NULL;
 }
 
@@ -113,6 +115,8 @@ fwupd_error_from_string(const gchar *error)
 		return FWUPD_ERROR_NEEDS_USER_ACTION;
 	if (g_strcmp0(error, FWUPD_DBUS_INTERFACE ".AuthExpired") == 0)
 		return FWUPD_ERROR_AUTH_EXPIRED;
+	if (g_strcmp0(error, FWUPD_DBUS_INTERFACE ".InvalidData") == 0)
+		return FWUPD_ERROR_INVALID_DATA;
 	return FWUPD_ERROR_LAST;
 }
 
