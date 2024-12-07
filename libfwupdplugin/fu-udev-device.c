fwupd/fwupd

commit 325d87ea749810c00660df58d40ecd7952b1c7cf

optionrom: Set the device file that can be used post-probe
The GUdevDevice is not available post probe, and using the device flag
`FU_DEVICE_INTERNAL_FLAG_NO_PROBE_COMPLETE` is very "expensive" for potentially
a lot of option ROM devices.

This is required for verifying the image.

libfwupdplugin/fu-udev-device.c
@@ -2389,7 +2389,8 @@ fu_udev_device_find_usb_device(FuUdevDevice *self, GError **error)
 static GBytes *
 fu_udev_device_dump_firmware(FuDevice *device, FuProgress *progress, GError **error)
 {
-	FuUdevDevice *udev_device = FU_UDEV_DEVICE(device);
+	FuUdevDevice *self = FU_UDEV_DEVICE(device);
+	FuUdevDevicePrivate *priv = GET_PRIVATE(self);
 	guint number_reads = 0;
 	g_autofree gchar *fn = NULL;
 	g_autofree gchar *rom_fn = NULL;
@@ -2399,8 +2400,7 @@ fu_udev_device_dump_firmware(FuDevice *device, FuProgress *progress, GError **er
 	g_autoptr(GInputStream) stream = NULL;
 
 	/* open the file */
-	rom_fn = g_build_filename(fu_udev_device_get_sysfs_path(udev_device), "rom", NULL);
-	if (rom_fn == NULL) {
+	if (priv->device_file == NULL) {
 		g_set_error_literal(error,
 				    FWUPD_ERROR,
 				    FWUPD_ERROR_INTERNAL,
@@ -2409,7 +2409,7 @@ fu_udev_device_dump_firmware(FuDevice *device, FuProgress *progress, GError **er
 	}
 
 	/* open file */
-	file = g_file_new_for_path(rom_fn);
+	file = g_file_new_for_path(priv->device_file);
 	stream = G_INPUT_STREAM(g_file_read(file, NULL, &error_local));
 	if (stream == NULL) {
 		g_set_error_literal(error,

plugins/optionrom/fu-optionrom-device.c
@@ -28,6 +28,7 @@ fu_optionrom_device_probe(FuDevice *device, GError **error)
 				    "Unable to read firmware from device");
 		return FALSE;
 	}
+	fu_udev_device_set_device_file(FU_UDEV_DEVICE(device), fn);
 
 	/* FuUdevDevice->probe -- needed by FU_UDEV_DEVICE_FLAG_VENDOR_FROM_PARENT */
 	if (!FU_DEVICE_CLASS(fu_optionrom_device_parent_class)->probe(device, error))

commit fdf926fe7c2e314087069a88ba1a013168621f33

Fix dumping rom images
Dumping doesn't happen from /dev/dri/card0, but from sysfs in the 'rom' file.

Fixes: 325d87ea7 ("optionrom: Set the device file that can be used post-probe")
Closes: https://github.com/fwupd/fwupd/issues/7964
Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

libfwupdplugin/fu-udev-device.c
@@ -1847,25 +1847,17 @@ static GBytes *
 fu_udev_device_dump_firmware(FuDevice *device, FuProgress *progress, GError **error)
 {
 	FuUdevDevice *self = FU_UDEV_DEVICE(device);
-	FuUdevDevicePrivate *priv = GET_PRIVATE(self);
 	guint number_reads = 0;
 	g_autofree gchar *fn = NULL;
 	g_autoptr(GByteArray) buf = g_byte_array_new();
 	g_autoptr(GError) error_local = NULL;
 	g_autoptr(GFile) file = NULL;
 	g_autoptr(GInputStream) stream = NULL;
-
-	/* open the file */
-	if (priv->device_file == NULL) {
-		g_set_error_literal(error,
-				    FWUPD_ERROR,
-				    FWUPD_ERROR_INTERNAL,
-				    "Unable to read firmware from device");
-		return NULL;
-	}
+	g_autofree gchar *rom_fn = NULL;
 
 	/* open file */
-	file = g_file_new_for_path(priv->device_file);
+	rom_fn = g_build_filename(fu_udev_device_get_sysfs_path(self), "rom", NULL);
+	file = g_file_new_for_path(rom_fn);
 	stream = G_INPUT_STREAM(g_file_read(file, NULL, &error_local));
 	if (stream == NULL) {
 		g_set_error_literal(error,