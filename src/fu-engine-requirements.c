fwupd/fwupd

commit bfa5fe696c3ae5e5cc5e9455c922f80a5793093d

trivial: Check the device is updatable before doing release requirement checks

src/fu-engine-requirements.c
@@ -821,6 +821,15 @@ fu_engine_requirements_check(FuEngine *self,
 	if (fwupd_version == NULL)
 		return FALSE;
 
+	/* sanity check */
+	if (device != NULL && !fu_device_has_flag(device, FWUPD_DEVICE_FLAG_UPDATABLE)) {
+		g_set_error_literal(error,
+				    FWUPD_ERROR,
+				    FWUPD_ERROR_NOT_SUPPORTED,
+				    "device is not updatable");
+		return FALSE;
+	}
+
 	/* hard requirements */
 	reqs = fu_release_get_hard_reqs(release);
 	if (reqs != NULL) {

src/fu-self-test.c
@@ -1734,6 +1734,8 @@ fu_engine_device_unlock_func(gconstpointer user_data)
 	fu_device_add_protocol(device, "com.acme");
 	fu_device_add_guid(device, "2d47f29b-83a2-4f31-a2e8-63474f4d4c2e");
 	fu_device_add_flag(device, FWUPD_DEVICE_FLAG_LOCKED);
+	fu_device_add_flag(device, FWUPD_DEVICE_FLAG_UPDATABLE);
+	fu_device_add_flag(device, FWUPD_DEVICE_FLAG_UNSIGNED_PAYLOAD);
 	fu_device_set_version_format(device, FWUPD_VERSION_FORMAT_PLAIN);
 	fu_engine_add_device(engine, device);
 

commit 9a6333fccdac30bf457907ee3f8e26a65feff652

Show updates with problems when using 'fwupdmgr get-releases'
This regressed in bfa5fe6.

Fixes https://github.com/fwupd/fwupd/issues/8219

src/fu-engine-requirements.c
@@ -822,7 +822,8 @@ fu_engine_requirements_check(FuEngine *self,
 		return FALSE;
 
 	/* sanity check */
-	if (device != NULL && !fu_device_has_flag(device, FWUPD_DEVICE_FLAG_UPDATABLE)) {
+	if (device != NULL && !fu_device_has_flag(device, FWUPD_DEVICE_FLAG_UPDATABLE) &&
+	    !fu_device_has_flag(device, FWUPD_DEVICE_FLAG_UPDATABLE_HIDDEN)) {
 		g_set_error(error,
 			    FWUPD_ERROR,
 			    FWUPD_ERROR_NOT_SUPPORTED,