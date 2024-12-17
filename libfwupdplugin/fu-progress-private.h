4b810dbc858d97e27e0172f5f98a0be89879041e
Richard Hughes <richard@hughsie.com>
2024-12-17 13:51:45 +0000

    trivial: Do not connect percentage-changed if the fraction is less than 0.1%
    This reduces startup time a little.

---
+++ libfwupdplugin/fu-progress-private.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2024 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#pragma once
+
+#include "fu-progress.h"
+
+gdouble
+fu_progress_get_global_fraction(FuProgress *self) G_GNUC_NON_NULL(1);
