5d0c9a2a72f7630bf9cbeaf6de4188248da473b5
Richard Hughes <richard@hughsie.com>
2025-01-02 21:17:12 +0000

    trivial: Fix an integer overflow issue when parsing EFI DPs
    Spotted by Coverity.

--- libfwupdplugin/fu-efi-device-path.c
+++ libfwupdplugin/fu-efi-device-path.c
@@ -119,7 +119,7 @@ fu_efi_device_path_parse(FuFirmware *firmware,
 	if (!fu_input_stream_size(stream, &streamsz, error))
 		return FALSE;
 	dp_length = fu_struct_efi_device_path_get_length(st);
-	if (dp_length > streamsz) {
+	if (streamsz > 4 && dp_length > streamsz) {
 		dp_length = streamsz - 0x4;
 		g_debug("fixing up DP length from 0x%x to 0x%x, because of a bug in efiboot",
 			fu_struct_efi_device_path_get_length(st),
