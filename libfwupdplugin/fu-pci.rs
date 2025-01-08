5525cb29b824d0ee1103a8f8f77abf95b7c809ba
Richard Hughes <richard@hughsie.com>
2025-01-08 10:22:31 +0000

    trivial: Move FuPciDeviceBaseCls to rust format

---
+++ libfwupdplugin/fu-pci.rs
@@ -0,0 +1,26 @@
+// Copyright 2025 Richard Hughes <richard@hughsie.com>
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+enum FuPciDeviceBaseCls {
+    Old,
+    MassStorage,
+    Network,
+    Display,
+    Multimedia,
+    Memory,
+    Bridge,
+    SimpleCommunication,
+    Base,
+    Input,
+    Docking,
+    Processors,
+    SerialBus,
+    Wireless,
+    IntelligentIo,
+    Satellite,
+    Encryption,
+    SignalProcessing,
+    Accelerator,
+    NonEssential,
+    Undefined = 0xFF,
+}
