b58ed90fd0edfe3fcfb0da5cdf04ea672f45ccef
Richard Hughes <richard@hughsie.com>
2025-01-09 17:20:53 +0000

    trivial: Update the tartan build helper

--- contrib/tartan.sh
+++ contrib/tartan.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-/usr/bin/scan-build-17 \
-	-load-plugin /usr/lib64/tartan/17.0/libtartan.so \
+/usr/bin/scan-build \
+	-load-plugin /usr/lib64/tartan/19.1/libtartan.so \
 	-disable-checker core.CallAndMessage \
 	-disable-checker core.NullDereference \
 	-disable-checker deadcode.DeadStores \
