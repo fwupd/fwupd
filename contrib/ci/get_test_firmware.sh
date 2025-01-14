d0ccafba8c1bfb8753372078236d2aab2d666c11
Mario Limonciello <mario.limonciello@amd.com>
2025-01-14 12:01:09 -0600

    trivial: ci: clean up test firmware clone
    Simplify the script to take an argument to put it into installed
    tests location and remove the clone when done

--- contrib/ci/get_test_firmware.sh
+++ contrib/ci/get_test_firmware.sh
@@ -4,6 +4,12 @@ if [ "$CI_NETWORK" = "true" ]; then
         #clone fwupd-test-firmware
         rm -rf fwupd-test-firmware
         git clone https://github.com/fwupd/fwupd-test-firmware
+        #If argument is set copy for installed tests
+        if [ -n "$1" ]; then
+                cp fwupd-test-firmware/installed-tests/* $1 -LRv
         #copy data for self-tests into the source tree
-        cp fwupd-test-firmware/ci-tests/* . -R
+        else
+                cp fwupd-test-firmware/ci-tests/* . -R
+        fi
+        rm -rf fwupd-test-firmware
 fi
