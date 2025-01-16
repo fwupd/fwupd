fwupd/fwupd

commit 785288abb63ac7ad3c627eaf892ddbcaca5b9de8

libfwupd: Detect if network is reachable before downloading metadata
If the network address isn't reachable, return NOTHING_TO_DO.

Fixes: https://github.com/fwupd/fwupd/issues/8144

libfwupd/fwupd-client.c
@@ -5645,8 +5645,26 @@ fwupd_client_download_http_retry(FwupdClient *self, CURL *curl, const gchar *url
 	FwupdClientPrivate *priv = GET_PRIVATE(self);
 	gulong delay_ms = 2500;
 	for (guint i = 0;; i++, delay_ms *= 2) {
+		GSocketConnectable *address;
+		GNetworkMonitor *monitor = g_network_monitor_get_default();
 		g_autoptr(GBytes) blob = NULL;
 		g_autoptr(GError) error_local = NULL;
+		g_autoptr(GUri) uri = NULL;
+
+		uri = g_uri_parse(url, G_URI_FLAGS_NONE, error);
+		if (uri == NULL)
+			return NULL;
+		address = g_network_address_parse(g_uri_get_host(uri), g_uri_get_port(uri), error);
+		if (address == NULL)
+			return NULL;
+		if (!g_network_monitor_can_reach(monitor, address, NULL, &error_local)) {
+			g_set_error(error,
+				    FWUPD_ERROR,
+				    FWUPD_ERROR_NOTHING_TO_DO,
+				    "Failed to download, network is unreachable: %s",
+				    error_local->message);
+			return NULL;
+		}
 
 		blob = fwupd_client_download_http(self, curl, url, &error_local);
 		if (blob != NULL)

commit efc9e1885f6dbdef4935a7ccbe172a233594e589

trivial: fix a small memory leak
The address returned from `g_network_address_parse()` needs to be freed.

Fixes: 785288abb ("libfwupd: Detect if network is reachable before downloading metadata")
Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

libfwupd/fwupd-client.c
@@ -5645,11 +5645,11 @@ fwupd_client_download_http_retry(FwupdClient *self, CURL *curl, const gchar *url
 	FwupdClientPrivate *priv = GET_PRIVATE(self);
 	gulong delay_ms = 2500;
 	for (guint i = 0;; i++, delay_ms *= 2) {
-		GSocketConnectable *address;
 		GNetworkMonitor *monitor = g_network_monitor_get_default();
 		g_autoptr(GBytes) blob = NULL;
 		g_autoptr(GError) error_local = NULL;
 		g_autoptr(GUri) uri = NULL;
+		g_autoptr(GSocketConnectable) address = NULL;
 
 		uri = g_uri_parse(url, G_URI_FLAGS_NONE, error);
 		if (uri == NULL)