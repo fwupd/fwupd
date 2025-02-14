7ffb7d02a60ed1ff392096dd2d3055f596c40049
Richard Hughes <richard@hughsie.com>
2025-02-14 14:41:50 +0000

    trivial: Add fu_udev_device_read_bytes() for future use

--- libfwupdplugin/fu-udev-device.h
+++ libfwupdplugin/fu-udev-device.h
@@ -76,6 +76,12 @@ fu_udev_device_write(FuUdevDevice *self,
 		     FuIOChannelFlags flags,
 		     GError **error) G_GNUC_WARN_UNUSED_RESULT G_GNUC_NON_NULL(1);
 gboolean
+fu_udev_device_write_bytes(FuUdevDevice *self,
+			   GBytes *blob,
+			   guint timeout_ms,
+			   FuIOChannelFlags flags,
+			   GError **error) G_GNUC_WARN_UNUSED_RESULT G_GNUC_NON_NULL(1, 2);
+gboolean
 fu_udev_device_read(FuUdevDevice *self,
 		    guint8 *buf,
 		    gsize bufsz,
@@ -83,6 +89,12 @@ fu_udev_device_read(FuUdevDevice *self,
 		    guint timeout_ms,
 		    FuIOChannelFlags flags,
 		    GError **error) G_GNUC_WARN_UNUSED_RESULT G_GNUC_NON_NULL(1);
+GBytes *
+fu_udev_device_read_bytes(FuUdevDevice *self,
+			  gsize count,
+			  guint timeout_ms,
+			  FuIOChannelFlags flags,
+			  GError **error) G_GNUC_WARN_UNUSED_RESULT G_GNUC_NON_NULL(1);
 gboolean
 fu_udev_device_seek(FuUdevDevice *self, goffset offset, GError **error) G_GNUC_WARN_UNUSED_RESULT
     G_GNUC_NON_NULL(1);
