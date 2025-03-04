0639fd62c847304c7cd182ba9d87c7ac4e9439f5
Richard Hughes <richard@hughsie.com>
2025-03-04 15:33:43 +0000

    amd-kria: Do not auto-detect amd-kria image firmware
    This matches any text firmware and makes `fwupdtool firmware-parse foo.bin auto`
    much less useful.

--- plugins/amd-kria/fu-amd-kria-image-firmware.c
+++ plugins/amd-kria/fu-amd-kria-image-firmware.c
@@ -56,6 +56,7 @@ fu_amd_kria_image_firmware_parse(FuFirmware *firmware,
 static void
 fu_amd_kria_image_firmware_init(FuAmdKriaImageFirmware *self)
 {
+	fu_firmware_add_flag(FU_FIRMWARE(self), FU_FIRMWARE_FLAG_NO_AUTO_DETECTION);
 }
 
 static void
