3e09ea0a3b94ca7d1cae7b766b22aee8eeb7baef
Sanjay Sheth <ssheth@logitech.com>
2025-03-13 16:30:36 -0700

    Raised retry attempts during setup

--- plugins/logitech-bulkcontroller/fu-logitech-bulkcontroller-device.c
+++ plugins/logitech-bulkcontroller/fu-logitech-bulkcontroller-device.c
@@ -21,6 +21,7 @@
 #define BULK_TRANSFER_TIMEOUT	      2500
 #define HASH_VALUE_SIZE		      16
 #define MAX_RETRIES		      5
+#define MAX_SETUP_RETRIES	      50
 #define MAX_WAIT_COUNT		      150
 #define POST_INSTALL_SLEEP_DURATION   80 * 1000 /* ms */
 
@@ -860,7 +861,7 @@ fu_logitech_bulkcontroller_device_ensure_info(FuLogitechBulkcontrollerDevice *se
 {
 	return fu_device_retry(FU_DEVICE(self),
 			       fu_logitech_bulkcontroller_device_ensure_info_cb,
-			       MAX_RETRIES,
+			       MAX_SETUP_RETRIES,
 			       &send_req,
 			       error);
 }
@@ -1157,7 +1158,7 @@ fu_logitech_bulkcontroller_device_set_time(FuLogitechBulkcontrollerDevice *self,
 {
 	return fu_device_retry(FU_DEVICE(self),
 			       fu_logitech_bulkcontroller_device_set_time_cb,
-			       MAX_RETRIES,
+			       MAX_SETUP_RETRIES,
 			       NULL,
 			       error);
 }
@@ -1206,7 +1207,7 @@ fu_logitech_bulkcontroller_device_transition_to_device_mode(FuLogitechBulkcontro
 {
 	return fu_device_retry(FU_DEVICE(self),
 			       fu_logitech_bulkcontroller_device_transition_to_device_mode_cb,
-			       MAX_RETRIES,
+			       MAX_SETUP_RETRIES,
 			       NULL,
 			       error);
 }
@@ -1288,6 +1289,14 @@ fu_logitech_bulkcontroller_device_setup(FuDevice *device, GError **error)
 {
 	FuLogitechBulkcontrollerDevice *self = FU_LOGITECH_BULKCONTROLLER_DEVICE(device);
 
+	/* the hardware is unable to handle requests -- firmware issue */
+	if (fu_device_has_private_flag(device,
+				       FU_LOGITECH_BULKCONTROLLER_DEVICE_FLAG_POST_INSTALL)) {
+		fu_device_sleep(device, POST_INSTALL_SLEEP_DURATION);
+		fu_device_remove_private_flag(device,
+					      FU_LOGITECH_BULKCONTROLLER_DEVICE_FLAG_POST_INSTALL);
+	}
+
 	/* FuUsbDevice->setup */
 	if (!FU_DEVICE_CLASS(fu_logitech_bulkcontroller_device_parent_class)
 		 ->setup(device, error)) {
@@ -1314,14 +1323,6 @@ fu_logitech_bulkcontroller_device_setup(FuDevice *device, GError **error)
 	if (!fu_logitech_bulkcontroller_device_transition_to_device_mode(self, error)) {
 		g_prefix_error(error, "failed to transition to device_mode: ");
 		return FALSE;
-	}
-
-	/* the hardware is unable to handle requests -- firmware issue */
-	if (fu_device_has_private_flag(device,
-				       FU_LOGITECH_BULKCONTROLLER_DEVICE_FLAG_POST_INSTALL)) {
-		fu_device_sleep(device, POST_INSTALL_SLEEP_DURATION);
-		fu_device_remove_private_flag(device,
-					      FU_LOGITECH_BULKCONTROLLER_DEVICE_FLAG_POST_INSTALL);
 	}
 
 	/* set device time */
