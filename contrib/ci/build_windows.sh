550679d83858a6d8d032dd51041109518299250e
Mario Limonciello <mario.limonciello@amd.com>
2025-04-07 12:30:26 -0500

    trivial: add a hack for readline mingw packaging bug in Fedora

--- contrib/ci/build_windows.sh
+++ contrib/ci/build_windows.sh
@@ -23,6 +23,9 @@ build=$root/build-win32
 rm -rf $DESTDIR $build
 mkdir -p $build $DESTDIR && cd $build
 
+# Hack for Fedora bug
+sed -i '/^Requires.private: termcap/d'  /usr/x86_64-w64-mingw32/sys-root/mingw/lib/pkgconfig/readline.pc
+
 # run before using meson
 export WINEPREFIX=$build/.wine
 
