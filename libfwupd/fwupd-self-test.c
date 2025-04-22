f0a4d879b578105e98a5e32b4ee3031a22c1ceea
Richard Hughes <richard@hughsie.com>
2025-04-22 16:46:04 +0100

    Constructing the authenticated URI properly when using FirmwareBaseURI

--- libfwupd/fwupd-self-test.c
+++ libfwupd/fwupd-self-test.c
@@ -392,6 +392,36 @@ fwupd_plugin_func(void)
 }
 
 static void
+fwupd_remote_func(void)
+{
+	g_autofree gchar *uri1 = NULL;
+	g_autofree gchar *uri2 = NULL;
+	g_autofree gchar *uri3 = NULL;
+	g_autoptr(FwupdRemote) remote = fwupd_remote_new();
+	g_autoptr(GError) error = NULL;
+
+	uri1 = fwupd_remote_build_firmware_uri(remote,
+					       "https://example.org/downloads/foo.cab",
+					       &error);
+	g_assert_no_error(error);
+	g_assert_cmpstr(uri1, ==, "https://example.org/downloads/foo.cab");
+
+	fwupd_remote_set_firmware_base_uri(remote, "https://example.org/mirror");
+	uri2 = fwupd_remote_build_firmware_uri(remote,
+					       "https://example.org/downloads/foo.cab",
+					       &error);
+	g_assert_no_error(error);
+	g_assert_cmpstr(uri2, ==, "https://example.org/mirror/foo.cab");
+
+	fwupd_remote_set_username(remote, "admin");
+	uri3 = fwupd_remote_build_firmware_uri(remote,
+					       "https://example.org/downloads/foo.cab",
+					       &error);
+	g_assert_no_error(error);
+	g_assert_cmpstr(uri3, ==, "https://admin@example.org/mirror/foo.cab/auth");
+}
+
+static void
 fwupd_request_func(void)
 {
 	gboolean ret;
@@ -1449,6 +1479,7 @@ main(int argc, char **argv)
 	g_test_add_func("/fwupd/release", fwupd_release_func);
 	g_test_add_func("/fwupd/report", fwupd_report_func);
 	g_test_add_func("/fwupd/plugin", fwupd_plugin_func);
+	g_test_add_func("/fwupd/remote", fwupd_remote_func);
 	g_test_add_func("/fwupd/request", fwupd_request_func);
 	g_test_add_func("/fwupd/device", fwupd_device_func);
 	g_test_add_func("/fwupd/device{filter}", fwupd_device_filter_func);
