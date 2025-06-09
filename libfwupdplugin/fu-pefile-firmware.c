db291693923e360cd680223f1c6160dbae7453cf
Richard Hughes <richard@hughsie.com>
2025-06-09 12:13:08 +0100

    pefile: Use the virtual size when cutting PE sections
    In *most* cases, the SizeOfRawData is padded out to the sector size, and so is
    actually more data than recorded. In this case, use the virtual size which is
    usually the byte-specific size value.
    
    (cherry picked from commit 8bd5f7e1b3e5187e9a140ac9f28a368a7eb76603)

--- libfwupdplugin/fu-pefile-firmware.c
+++ libfwupdplugin/fu-pefile-firmware.c
@@ -91,17 +91,24 @@ fu_pefile_firmware_parse_section(FuFirmware *firmware,
 	fu_firmware_set_idx(img, idx);
 
 	/* add data */
-	if (fu_struct_pe_coff_section_get_size_of_raw_data(st) > 0) {
+	if (fu_struct_pe_coff_section_get_virtual_size(st) > 0) {
 		guint32 sect_offset = fu_struct_pe_coff_section_get_pointer_to_raw_data(st);
+		guint32 sect_size = fu_struct_pe_coff_section_get_virtual_size(st);
 		g_autoptr(GBytes) blob = NULL;
 
+		/* use the raw data size if the section is compressed */
+		if (fu_struct_pe_coff_section_get_virtual_size(st) >
+		    fu_struct_pe_coff_section_get_size_of_raw_data(st)) {
+			g_debug("virtual size 0x%x bigger than raw data, truncating to 0x%x",
+				sect_size,
+				fu_struct_pe_coff_section_get_size_of_raw_data(st));
+			sect_size = fu_struct_pe_coff_section_get_size_of_raw_data(st);
+		}
+
 		fu_firmware_set_offset(img, sect_offset);
-		blob = fu_bytes_new_offset(fw,
-					   sect_offset,
-					   fu_struct_pe_coff_section_get_size_of_raw_data(st),
-					   error);
+		blob = fu_bytes_new_offset(fw, sect_offset, sect_size, error);
 		if (blob == NULL) {
-			g_prefix_error(error, "failed to get raw data for %s: ", sect_id);
+			g_prefix_error(error, "failed to cut raw PE data %s: ", sect_id);
 			return FALSE;
 		}
 		if (!fu_firmware_parse(img, blob, flags, error)) {
