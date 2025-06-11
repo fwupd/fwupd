263dae99a5ceba340337407346c65faf07410256
Mario Limonciello <mario.limonciello@amd.com>
2025-06-10 22:43:44 -0700

    trivial: fu-tool: don't coldplug refresh command
    coldplugging refresh is unnecessary when refreshing metadata with
    fwupdtool.

--- src/fu-tool.c
+++ src/fu-tool.c
@@ -3746,11 +3746,7 @@ fu_util_refresh(FuUtilPrivate *priv, gchar **values, GError **error)
 	g_autoptr(GPtrArray) remotes = NULL;
 
 	/* load engine */
-	if (!fu_util_start_engine(priv,
-				  FU_ENGINE_LOAD_FLAG_COLDPLUG | FU_ENGINE_LOAD_FLAG_REMOTES |
-				      FU_ENGINE_LOAD_FLAG_HWINFO,
-				  priv->progress,
-				  error))
+	if (!fu_util_start_engine(priv, FU_ENGINE_LOAD_FLAG_REMOTES, priv->progress, error))
 		return FALSE;
 
 	/* download new metadata */
