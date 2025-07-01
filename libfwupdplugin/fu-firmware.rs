8d47c6068bacbde150fab0e817c1486ef0ad093d
Richard Hughes <richard@hughsie.com>
2025-07-01 11:48:25 +0100

    trivial: Move FuFirmwareParseFlags to rustgen format

--- libfwupdplugin/fu-firmware.rs
+++ libfwupdplugin/fu-firmware.rs
@@ -6,3 +6,12 @@ enum FuFirmwareExportFlags {
     IncludeDebug = 1 << 0,
     AsciiData = 1 << 1, // as UTF-8 strings
 }
+
+enum FuFirmwareParseFlags {
+    None = 0,
+    IgnoreChecksum = 1 << 6,
+    IgnoreVidPid = 1 << 7,
+    NoSearch = 1 << 8, // no heuristics
+    CacheStream = 1 << 10,
+    CacheBlob = 1 << 11,
+}
