4e0c5ce64abf6640024bcb9bff9eb7506a4234ca
Richard Hughes <richard@hughsie.com>
2025-07-14 09:59:51 +0100

    Add regression checks for CPU and RSS in CI
    This should catch changes that accidentally slow down coldplug or increase
    memory usage significantly.

--- contrib/ci/ubuntu.sh
+++ contrib/ci/ubuntu.sh
@@ -43,6 +43,11 @@ sudo -u nobody meson ${BUILD}               \
 sudo -u nobody ninja -C ${BUILD} -v
 sudo -u nobody meson test -C ${BUILD} --print-errorlogs --verbose
 
+# check we've not become a CPU or memory hog
+ninja -C ${BUILD} install -v
+./contrib/ci/check-rss.py --limit 3072 ${BUILD}/src/fwupdtool get-devices
+./contrib/ci/check-cpu.py --limit 300 ${BUILD}/src/fwupdtool get-devices
+
 # check for unused symbols
 ./contrib/ci/check-unused.py
 
@@ -55,7 +60,6 @@ if [ $? -ne 1 ] ; then
 fi
 
 #make docs available outside of docker
-ninja -C ${BUILD} install -v
 mkdir -p ${root}/dist/share
 mv ${root}/target/share/doc ${root}/dist/share
 
