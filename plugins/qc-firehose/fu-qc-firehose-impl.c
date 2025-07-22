211e9b202f604d587a4709e11d7bea6a4767789c
Richard Hughes <richard@hughsie.com>
2025-07-22 15:50:12 +0100

    qc-firehose: Fix the progress steps

--- plugins/qc-firehose/fu-qc-firehose-impl.c
+++ plugins/qc-firehose/fu-qc-firehose-impl.c
@@ -863,10 +863,9 @@ fu_qc_firehose_impl_write_firmware(FuQcFirehoseImpl *self,
 
 	/* progress */
 	fu_progress_set_id(progress, G_STRLOC);
-	fu_progress_add_flag(progress, FU_PROGRESS_FLAG_GUESSED);
 	fu_progress_add_step(progress, FWUPD_STATUS_DEVICE_BUSY, 1, NULL);
-	fu_progress_add_step(progress, FWUPD_STATUS_DEVICE_ERASE, 20, NULL);
-	fu_progress_add_step(progress, FWUPD_STATUS_DEVICE_WRITE, 80, NULL);
+	fu_progress_add_step(progress, FWUPD_STATUS_DEVICE_ERASE, 4, NULL);
+	fu_progress_add_step(progress, FWUPD_STATUS_DEVICE_WRITE, 94, NULL);
 	fu_progress_add_step(progress, FWUPD_STATUS_DEVICE_WRITE, 1, "patch");
 
 	/* load XML */
