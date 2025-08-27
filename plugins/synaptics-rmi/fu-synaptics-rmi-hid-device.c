c8fe011fa5737693f3a4a20f09322b4f4df16488
Vincent Huang <vincenth@synaptics.com>
2025-08-27 19:47:07 +0800

    synaptics-rmi: add required flags to synaptics-rmi hid device initialization
    Add both FU_IO_CHANNEL_OPEN_FLAG_READ and FU_IO_CHANNEL_OPEN_FLAG_WRITE
    to the Synaptics RMI HID device initialization to ensure proper device
    communication.

--- plugins/synaptics-rmi/fu-synaptics-rmi-hid-device.c
+++ plugins/synaptics-rmi/fu-synaptics-rmi-hid-device.c
@@ -538,6 +538,8 @@ fu_synaptics_rmi_hid_device_init(FuSynapticsRmiHidDevice *self)
 	fu_device_set_name(FU_DEVICE(self), "Touchpad");
 	fu_device_set_remove_delay(FU_DEVICE(self), FU_DEVICE_REMOVE_DELAY_RE_ENUMERATE);
 	fu_synaptics_rmi_device_set_max_page(FU_SYNAPTICS_RMI_DEVICE(self), 0xff);
+	fu_udev_device_add_open_flag(FU_UDEV_DEVICE(self), FU_IO_CHANNEL_OPEN_FLAG_READ);
+	fu_udev_device_add_open_flag(FU_UDEV_DEVICE(self), FU_IO_CHANNEL_OPEN_FLAG_WRITE);
 }
 
 static void
