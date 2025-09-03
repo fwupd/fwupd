2d7c4f6172716f7f24a089e6e0d7de47da5e9aaf
Richard Hughes <richard@hughsie.com>
2025-09-03 08:05:16 +0100

    elantp: Remove hardcoded vendor that is now set from a quirk

--- plugins/elantp/fu-elantp-hid-device.c
+++ plugins/elantp/fu-elantp-hid-device.c
@@ -966,7 +966,6 @@ fu_elantp_hid_device_init(FuElantpHidDevice *self)
 	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_UNSIGNED_PAYLOAD);
 	fu_device_add_icon(FU_DEVICE(self), FU_DEVICE_ICON_INPUT_TOUCHPAD);
 	fu_device_add_protocol(FU_DEVICE(self), "tw.com.emc.elantp");
-	fu_device_set_vendor(FU_DEVICE(self), "Elan");
 	fu_device_set_version_format(FU_DEVICE(self), FWUPD_VERSION_FORMAT_HEX);
 	fu_device_set_priority(FU_DEVICE(self), 1); /* better than i2c */
 	fu_udev_device_add_open_flag(FU_UDEV_DEVICE(self), FU_IO_CHANNEL_OPEN_FLAG_READ);
