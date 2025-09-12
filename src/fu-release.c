fwupd/fwupd

commit dd56b98a57c148c01ddaf6359b76d88ca72c7be0

Allow installing modern archives named as .CAB rather than .cab

src/fu-release.c
@@ -437,11 +437,14 @@ fu_release_load_artifact(FuRelease *self, XbNode *artifact, GError **error)
 
 	/* filename */
 	filename = xb_node_query_text(artifact, "filename", NULL);
-	if (filename != NULL && !g_str_has_suffix(filename, ".cab")) {
-		/* some firmware archives was signed with <artifact type="binary"> where the
-		 * checksums were the *content* checksums, not the *container* checksum */
-		g_debug("ignoring non-binary artifact entry: %s", filename);
-		return TRUE;
+	if (filename != NULL) {
+		g_autofree gchar *fn_lowercase = g_ascii_strdown(filename, -1);
+		if (g_str_has_suffix(fn_lowercase, ".cab")) {
+			/* some firmware archives was signed with <artifact type="binary"> where the
+			 * checksums were the *content* checksums, not the *container* checksum */
+			g_debug("ignoring non-binary artifact entry: %s", filename);
+			return TRUE;
+		}
 	}
 	if (filename != NULL)
 		fu_release_set_filename(self, filename);

commit e03c4a55e3b53236bb741a7869a84e071271d5e4

Fix missing release locations when loading from artifact
This regressed in dd56b98a.

Fixes https://github.com/fwupd/fwupd/issues/9231

src/fu-release.c
@@ -439,7 +439,7 @@ fu_release_load_artifact(FuRelease *self, XbNode *artifact, GError **error)
 	filename = xb_node_query_text(artifact, "filename", NULL);
 	if (filename != NULL) {
 		g_autofree gchar *fn_lowercase = g_ascii_strdown(filename, -1);
-		if (g_str_has_suffix(fn_lowercase, ".cab")) {
+		if (!g_str_has_suffix(fn_lowercase, ".cab")) {
 			/* some firmware archives was signed with <artifact type="binary"> where the
 			 * checksums were the *content* checksums, not the *container* checksum */
 			g_debug("ignoring non-binary artifact entry: %s", filename);