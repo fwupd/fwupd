48453ac0e1bd4d39bc4843f2dd7ae0c5e3b4ce52
Richard Hughes <richard@hughsie.com>
2025-09-25 14:41:37 +0100

    trivial: Add has-stored-size to the new NVRAM objects

--- libfwupdplugin/fu-efi-vss-auth-variable.c
+++ libfwupdplugin/fu-efi-vss-auth-variable.c
@@ -230,6 +230,7 @@ fu_efi_vss_auth_variable_build(FuFirmware *firmware, XbNode *n, GError **error)
 static void
 fu_efi_vss_auth_variable_init(FuEfiVssAuthVariable *self)
 {
+	fu_firmware_add_flag(FU_FIRMWARE(self), FU_FIRMWARE_FLAG_HAS_STORED_SIZE);
 }
 
 static void
