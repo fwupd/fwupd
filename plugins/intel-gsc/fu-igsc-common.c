fwupd/fwupd

commit da2b084929190f337a1f8e096283e6e31eedfbb4

Use a custom wrapper for the rustgen structs
This allows us to typecheck the structure -- as in C typedefs to the same
base type can be compared freely without any kind of warning.

This means putting the GByteArray as a pointer inside the defined struct type;
we can't put in the deref struct as the public type is upcast to _GRealArray
inside Glib so that the internal implementation details are hidden. This means
we have to dereference the GByteArray as `->buf` which is a little clunky, but
then also frees us to put other stuff (e.g. a GType) inside the defined rustgen
structure in the future.

Also, to make clear that the rustgen types are not GByteArrays anymore, enforce
that any rustgen types start with a `st` prefix. At least now, the compiler is
on our side.

plugins/intel-gsc/fu-igsc-common.c
@@ -80,7 +80,7 @@ fu_igsc_fwdata_device_info_parse_device_id_array(GPtrArray *device_infos,
 		st = fu_igsc_fwdata_device_info4_parse_stream(stream, offset, error);
 		if (st == NULL)
 			return FALSE;
-		g_ptr_array_add(device_infos, g_steal_pointer(&st));
+		g_ptr_array_add(device_infos, g_steal_pointer(&st->buf));
 	}
 
 	/* success */

commit e54e6eac2a19be96a719018bba19955b89da1afa

igsc: Fix a regression when installing oprom firmware
This was introduced in da2b0849.

plugins/intel-gsc/fu-igsc-common.c
@@ -80,7 +80,7 @@ fu_igsc_fwdata_device_info_parse_device_id_array(GPtrArray *device_infos,
 		st = fu_igsc_fwdata_device_info4_parse_stream(stream, offset, error);
 		if (st == NULL)
 			return FALSE;
-		g_ptr_array_add(device_infos, g_steal_pointer(&st->buf));
+		g_ptr_array_add(device_infos, g_steal_pointer(&st));
 	}
 
 	/* success */