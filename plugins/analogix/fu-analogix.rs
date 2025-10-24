20ccf4ed035324d593821a1e6f31a3d13a0486f9
Richard Hughes <richard@hughsie.com>
2025-10-24 14:09:22 +0100

    analogix: Move some constants to Rust format

--- plugins/analogix/fu-analogix.rs
+++ plugins/analogix/fu-analogix.rs
@@ -8,3 +8,22 @@ enum FuAnalogixUpdateStatus {
     Finish,
     Error = 0xFF,
 }
+
+// bRequest for Phoenix-Lite Billboard
+enum FuAnalogixBbRqt {
+    SendUpdateData = 0x01,
+    ReadUpdateData = 0x02,
+    GetUpdateStatus = 0x10,
+    ReadFwVer = 0x12,
+    ReadCusVer = 0x13,
+    ReadFwRver = 0x19,
+    ReadCusRver = 0x1C,
+}
+
+// wValue low byte
+enum FuAnalogixBbWval {
+    UpdateOcm = 0x06,
+    UpdateCustomDef = 0x07,
+    UpdateSecureTx = 0x08,
+    UpdateSecureRx = 0x09,
+}
