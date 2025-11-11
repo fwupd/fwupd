7f2817bbdcdfe8d543af499b1895991b3f787156
Richard Hughes <richard@hughsie.com>
2025-11-11 12:20:37 +0000

    trivial: Add EFI_STATUS defines for future use

--- libfwupdplugin/fu-efi.rs
+++ libfwupdplugin/fu-efi.rs
@@ -1,6 +1,45 @@
 // Copyright 2023 Richard Hughes <richard@hughsie.com>
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
+// EFI_STATUS is u64le on 64-bit and u32le on 32-bit -- but be pragmatic
+#[repr(u64le)]
+enum FuEfiStatus {
+    Success             = 0,
+    LoadError           = 1,
+    InvalidParameter    = 2,
+    Unsupported         = 3,
+    BadBufferSize       = 4,
+    BufferTooSmall      = 5,
+    NotReady            = 6,
+    DeviceError         = 7,
+    WriteProtected      = 8,
+    OutOfResources      = 9,
+    VolumeCorrupted     = 10,
+    VolumeFull          = 11,
+    NoMedia             = 12,
+    MediaChanged        = 13,
+    NotFound            = 14,
+    AccessDenied        = 15,
+    NoResponse          = 16,
+    NoMapping           = 17,
+    Timeout             = 18,
+    NotStarted          = 19,
+    AlreadyStarted      = 20,
+    Aborted             = 21,
+    IcmpError           = 22,
+    TftpError           = 23,
+    ProtocolError       = 24,
+    IncompatibleVersion = 25,
+    SecurityViolation   = 26,
+    CrcError            = 27,
+    EndOfMedia          = 28,
+    EndOfFile           = 31,
+    InvalidLanguage     = 32,
+    CompromisedData     = 33,
+    IpAddressConflict   = 34,
+    HttpError           = 35,
+}
+
 #[derive(ToString, FromString)]
 enum FuEfiSignatureKind {
     Unknown,
