925987493f15688e0eb02f74ddbdce4a9bdd961f
Richard Hughes <richard@hughsie.com>
2025-11-18 18:52:04 +0000

    wacom-usb: Fix a small memory leak when failing to parse firmware

--- plugins/wacom-usb/fu-wac-module-sub-cpu.c
+++ plugins/wacom-usb/fu-wac-module-sub-cpu.c
@@ -212,8 +212,8 @@ fu_wac_module_sub_cpu_prepare_firmware(FuDevice *device,
 				       FuFirmwareParseFlags flags,
 				       GError **error)
 {
-	FuFirmware *firmware_srec = fu_srec_firmware_new();
-	if (!fu_firmware_parse_stream(firmware_srec,
+	g_autoptr(FuFirmware) firmware = fu_srec_firmware_new();
+	if (!fu_firmware_parse_stream(firmware,
 				      stream,
 				      0,
 				      flags | FU_FIRMWARE_PARSE_FLAG_NO_SEARCH,
@@ -221,7 +221,7 @@ fu_wac_module_sub_cpu_prepare_firmware(FuDevice *device,
 		g_prefix_error_literal(error, "wacom sub_cpu failed to parse firmware: ");
 		return NULL;
 	}
-	return firmware_srec;
+	return g_steal_pointer(&firmware);
 }
 
 static void
