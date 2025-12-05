3119a50a19a29c66ddf4d7fdb53d7883d4285bcb
Richard Hughes <richard@hughsie.com>
2025-12-05 15:40:19 +0000

    Use rustgen in libfwupd too

---
+++ libfwupd/fwupd-client.rs
@@ -0,0 +1,7 @@
+// Copyright 2025 Richard Hughes <richard@hughsie.com>
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+enum FwupdClientDownloadFlags {
+    None,                   // Since: 1.4.5
+    OnlyP2p = 1 << 0,       // Since: 1.9.4
+}
