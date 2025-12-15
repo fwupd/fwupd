ec6502c6751fa829c7f2f0b67ce3116807bd2a28
Richard Hughes <richard@hughsie.com>
2025-12-15 10:28:58 +0000

    trivial: Move FuCorsairBpProperty to Rust format

--- plugins/corsair/fu-corsair.rs
+++ plugins/corsair/fu-corsair.rs
@@ -7,3 +7,16 @@ enum FuCorsairDeviceKind {
     Mouse,
     Receiver,
 }
+
+enum FuCorsairBpProperty {
+    Mode = 0x03,
+    BatteryLevel = 0x0F,
+    Version = 0x13,
+    BootloaderVersion = 0x14,
+    Subdevices = 0x36,
+}
+
+enum FuCorsairDeviceMode {
+    Application = 0x01,
+    Bootloader = 0x03,
+}
