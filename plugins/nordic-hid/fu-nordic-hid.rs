c32c80d9486f9d6fefe747289022c4c38f8a00a7
Richard Hughes <richard@hughsie.com>
2025-12-15 11:44:42 +0000

    trivial: Move FuNordicHidCfgStatus to Rust format

--- plugins/nordic-hid/fu-nordic-hid.rs
+++ plugins/nordic-hid/fu-nordic-hid.rs
@@ -1,6 +1,31 @@
 // Copyright 2025 Richard Hughes <richard@hughsie.com>
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
+enum FuNordicHidCfgStatus {
+    Pending,
+    GetMaxModId,
+    GetHwid,
+    GetBoardName,
+    IndexPeers,
+    GetPeer,
+    Set,
+    Fetch,
+    Success,
+    Timeout,
+    Reject,
+    WriteFail,
+    Disconnected,
+    GetPeersCache,
+    Fault = 99,
+}
+
+enum FuNordicHidCfgSyncState {
+    Inactive,
+    Active,
+    Storing,
+    Cleaning,
+}
+
 #[derive(Parse)]
 #[repr(C, packed)]
 struct FuStructNordicHidDfuFwinfo {
