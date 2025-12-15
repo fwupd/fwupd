e64ef9f593157c63ff1d0f9cd334bb4a213eccb0
Richard Hughes <richard@hughsie.com>
2025-12-15 10:12:06 +0000

    trivial: Move FuHidDeviceFlags to Rust format

--- libfwupdplugin/fu-hid-device.h
+++ libfwupdplugin/fu-hid-device.h
@@ -7,6 +7,7 @@
 #pragma once
 
 #include "fu-hid-descriptor.h"
+#include "fu-hid-struct.h"
 #include "fu-usb-device.h"
 
 #define FU_TYPE_HID_DEVICE (fu_hid_device_get_type())
@@ -15,32 +16,6 @@ G_DECLARE_DERIVABLE_TYPE(FuHidDevice, fu_hid_device, FU, HID_DEVICE, FuUsbDevice
 struct _FuHidDeviceClass {
 	FuUsbDeviceClass parent_class;
 };
-
-/**
- * FuHidDeviceFlags:
- * @FU_HID_DEVICE_FLAG_NONE:			No flags set
- * @FU_HID_DEVICE_FLAG_ALLOW_TRUNC:		Allow truncated reads and writes
- * @FU_HID_DEVICE_FLAG_IS_FEATURE:		Use %FU_HID_REPORT_TYPE_FEATURE for wValue
- * @FU_HID_DEVICE_FLAG_RETRY_FAILURE:		Retry up to 10 times on failure
- * @FU_HID_DEVICE_FLAG_NO_KERNEL_UNBIND:	Do not unbind the kernel driver on open
- * @FU_HID_DEVICE_FLAG_NO_KERNEL_REBIND:	Do not rebind the kernel driver on close
- * @FU_HID_DEVICE_FLAG_USE_INTERRUPT_TRANSFER:	Use interrupt transfers, not control transfers
- * @FU_HID_DEVICE_FLAG_AUTODETECT_EPS:		Autodetect interface endpoints
- *
- * Flags used when calling fu_hid_device_get_report() and fu_hid_device_set_report().
- **/
-typedef enum {
-	FU_HID_DEVICE_FLAG_NONE = 0,
-	FU_HID_DEVICE_FLAG_ALLOW_TRUNC = 1 << 0,
-	FU_HID_DEVICE_FLAG_IS_FEATURE = 1 << 1,
-	FU_HID_DEVICE_FLAG_RETRY_FAILURE = 1 << 2,
-	FU_HID_DEVICE_FLAG_NO_KERNEL_UNBIND = 1 << 3,
-	FU_HID_DEVICE_FLAG_NO_KERNEL_REBIND = 1 << 4,
-	FU_HID_DEVICE_FLAG_USE_INTERRUPT_TRANSFER = 1 << 5,
-	FU_HID_DEVICE_FLAG_AUTODETECT_EPS = 1 << 6,
-	/*< private >*/
-	FU_HID_DEVICE_FLAG_LAST
-} G_GNUC_FLAG_ENUM FuHidDeviceFlags;
 
 void
 fu_hid_device_add_flag(FuHidDevice *self, FuHidDeviceFlags flag) G_GNUC_NON_NULL(1);
