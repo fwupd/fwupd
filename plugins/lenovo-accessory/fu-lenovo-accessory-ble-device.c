3b05731b6abcef45d8891abf680f6f37716aeaaa
yuchao li <liyc44@lenovo.com>
2026-01-08 12:02:24 +0000

    Add a new plugin for Lenovo accessories

---
+++ plugins/lenovo-accessory/fu-lenovo-accessory-ble-device.c
@@ -0,0 +1,269 @@
+/*
+ * Copyright 2026 Yuchao Li <liyc44@lenovo.com>
+ * Copyright 2026 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+#include "config.h"
+
+#include "fu-lenovo-accessory-ble-device.h"
+#include "fu-lenovo-accessory-impl.h"
+
+struct _FuLenovoAccessoryBleDevice {
+	FuBluezDevice parent_instance;
+};
+
+static void
+fu_lenovo_accessory_ble_device_impl_iface_init(FuLenovoAccessoryImplInterface *iface);
+
+G_DEFINE_TYPE_WITH_CODE(FuLenovoAccessoryBleDevice,
+			fu_lenovo_accessory_ble_device,
+			FU_TYPE_BLUEZ_DEVICE,
+			G_IMPLEMENT_INTERFACE(FU_TYPE_LENOVO_ACCESSORY_IMPL,
+					      fu_lenovo_accessory_ble_device_impl_iface_init))
+
+#define UUID_WRITE "c1d02501-2d1f-400a-95d2-6a2f7bca0c25"
+#define UUID_READ  "c1d02502-2d1f-400a-95d2-6a2f7bca0c25"
+
+static gboolean
+fu_lenovo_accessory_ble_device_write_files(FuLenovoAccessoryBleDevice *self,
+					   FuLenovoDfuFileType file_type,
+					   GInputStream *stream,
+					   FuProgress *progress,
+					   GError **error)
+{
+	g_autoptr(FuChunkArray) chunks = NULL;
+
+	chunks = fu_chunk_array_new_from_stream(stream, 0, 0, 32, error);
+	if (chunks == NULL)
+		return FALSE;
+	fu_progress_set_id(progress, G_STRLOC);
+	fu_progress_set_steps(progress, fu_chunk_array_length(chunks));
+	for (guint32 i = 0; i < fu_chunk_array_length(chunks); i++) {
+		g_autoptr(FuChunk) chk = NULL;
+
+		chk = fu_chunk_array_index(chunks, i, error);
+		if (chk == NULL)
+			return FALSE;
+		if (!fu_lenovo_accessory_impl_dfu_file(FU_LENOVO_ACCESSORY_IMPL(self),
+						       file_type,
+						       fu_chunk_get_address(chk),
+						       fu_chunk_get_data(chk),
+						       fu_chunk_get_data_sz(chk),
+						       error))
+			return FALSE;
+		fu_progress_step_done(progress);
+	}
+
+	/* success */
+	return TRUE;
+}
+
+static gboolean
+fu_lenovo_accessory_ble_device_write_firmware(FuDevice *device,
+					      FuFirmware *firmware,
+					      FuProgress *progress,
+					      FwupdInstallFlags flags,
+					      GError **error)
+{
+	FuLenovoAccessoryBleDevice *self = FU_LENOVO_ACCESSORY_BLE_DEVICE(device);
+	gsize fw_size = 0;
+	guint32 file_crc = 0xFFFFFFFF;
+	guint32 device_crc = 0;
+	g_autoptr(GInputStream) stream = NULL;
+
+	/* progress */
+	fu_progress_set_id(progress, G_STRLOC);
+	fu_progress_add_step(progress, FWUPD_STATUS_DEVICE_WRITE, 5, "prepare");
+	fu_progress_add_step(progress, FWUPD_STATUS_DEVICE_WRITE, 95, "write");
+
+	stream = fu_firmware_get_stream(firmware, error);
+	if (stream == NULL)
+		return FALSE;
+	if (!fu_input_stream_size(stream, &fw_size, error))
+		return FALSE;
+	if (!fu_input_stream_compute_crc32(stream, FU_CRC_KIND_B32_STANDARD, &file_crc, error))
+		return FALSE;
+	if (!fu_lenovo_accessory_impl_dfu_entry(FU_LENOVO_ACCESSORY_IMPL(device), error))
+		return FALSE;
+	if (!fu_lenovo_accessory_impl_dfu_prepare(FU_LENOVO_ACCESSORY_IMPL(device),
+						  FU_LENOVO_DFU_FILE_TYPE_BIN_FILE,
+						  0x0,
+						  (guint32)fw_size,
+						  file_crc,
+						  error))
+		return FALSE;
+	fu_progress_step_done(progress);
+	if (!fu_lenovo_accessory_ble_device_write_files(self,
+							FU_LENOVO_DFU_FILE_TYPE_BIN_FILE,
+							stream,
+							fu_progress_get_child(progress),
+							error))
+		return FALSE;
+	if (!fu_lenovo_accessory_impl_dfu_crc(FU_LENOVO_ACCESSORY_IMPL(device),
+					      &device_crc,
+					      error)) {
+		g_prefix_error(error, "BLE CRC Error (device 0x%08x): ", device_crc);
+		return FALSE;
+	}
+	if (device_crc != file_crc) {
+		g_set_error(error,
+			    FWUPD_ERROR,
+			    FWUPD_ERROR_WRITE,
+			    "CRC mismatch: device 0x%08x != file 0x%08x",
+			    device_crc,
+			    file_crc);
+		return FALSE;
+	}
+	fu_progress_step_done(progress);
+
+	/* success */
+	return TRUE;
+}
+
+static gboolean
+fu_lenovo_accessory_ble_device_attach(FuDevice *device, FuProgress *progress, GError **error)
+{
+	if (!fu_lenovo_accessory_impl_dfu_exit(FU_LENOVO_ACCESSORY_IMPL(device),
+					       FU_LENOVO_DFU_EXIT_CODE_DFU_SUCCESS,
+					       error)) {
+		g_prefix_error_literal(error, "failed to exit: ");
+		return FALSE;
+	}
+	fu_device_add_flag(device, FWUPD_DEVICE_FLAG_WAIT_FOR_REPLUG);
+	return TRUE;
+}
+
+static gboolean
+fu_lenovo_accessory_ble_device_setup(FuDevice *device, GError **error)
+{
+	guint8 major = 0;
+	guint8 minor = 0;
+	guint8 micro = 0;
+	g_autofree gchar *version = NULL;
+
+	if (!fu_lenovo_accessory_impl_get_fwversion(FU_LENOVO_ACCESSORY_IMPL(device),
+						    &major,
+						    &minor,
+						    &micro,
+						    error))
+		return FALSE;
+	version = g_strdup_printf("%u.%u.%02u", major, minor, micro);
+	fu_device_set_version(device, version);
+	return TRUE;
+}
+
+static void
+fu_lenovo_accessory_ble_device_set_progress(FuDevice *device, FuProgress *progress)
+{
+	fu_progress_set_id(progress, G_STRLOC);
+	fu_progress_add_step(progress, FWUPD_STATUS_DECOMPRESSING, 0, "prepare-fw");
+	fu_progress_add_step(progress, FWUPD_STATUS_DEVICE_RESTART, 0, "detach");
+	fu_progress_add_step(progress, FWUPD_STATUS_DEVICE_WRITE, 99, "write");
+	fu_progress_add_step(progress, FWUPD_STATUS_DEVICE_RESTART, 1, "attach");
+	fu_progress_add_step(progress, FWUPD_STATUS_DEVICE_BUSY, 0, "reload");
+}
+
+static GByteArray *
+fu_lenovo_accessory_ble_device_read(FuLenovoAccessoryImpl *impl, GError **error)
+{
+	FuLenovoAccessoryBleDevice *self = FU_LENOVO_ACCESSORY_BLE_DEVICE(impl);
+	return fu_bluez_device_read(FU_BLUEZ_DEVICE(self), UUID_READ, error);
+}
+
+static gboolean
+fu_lenovo_accessory_ble_device_write(FuLenovoAccessoryImpl *impl, GByteArray *buf, GError **error)
+{
+	FuLenovoAccessoryBleDevice *self = FU_LENOVO_ACCESSORY_BLE_DEVICE(impl);
+	return fu_bluez_device_write(FU_BLUEZ_DEVICE(self), UUID_WRITE, buf, error);
+}
+
+static gboolean
+fu_lenovo_accessory_ble_device_poll_cb(FuDevice *device, gpointer user_data, GError **error)
+{
+	GByteArray *buf_rsp = (GByteArray *)user_data;
+	FuLenovoStatus status;
+	gsize offset = 0x0;
+	g_autoptr(GByteArray) buf = NULL;
+	g_autoptr(FuStructLenovoAccessoryCmd) st_cmd = NULL;
+
+	buf = fu_lenovo_accessory_ble_device_read(FU_LENOVO_ACCESSORY_IMPL(device), error);
+	if (buf == NULL)
+		return FALSE;
+	if (buf->len == 0) {
+		g_set_error_literal(error, FWUPD_ERROR, FWUPD_ERROR_READ, "received empty data");
+		return FALSE;
+	}
+	st_cmd = fu_struct_lenovo_accessory_cmd_parse(buf->data, buf->len, offset, error);
+	if (st_cmd == NULL)
+		return FALSE;
+	status = fu_struct_lenovo_accessory_cmd_get_target_status(st_cmd) & 0x0F;
+	if (status == FU_LENOVO_STATUS_COMMAND_BUSY) {
+		g_set_error_literal(error, FWUPD_ERROR, FWUPD_ERROR_BUSY, "command busy");
+		return FALSE;
+	}
+	if (status != FU_LENOVO_STATUS_COMMAND_SUCCESSFUL) {
+		g_set_error(error,
+			    FWUPD_ERROR,
+			    FWUPD_ERROR_WRITE,
+			    "command failed with status 0x%02x",
+			    status);
+		return FALSE;
+	}
+	offset += FU_STRUCT_LENOVO_ACCESSORY_CMD_SIZE;
+
+	/* success */
+	g_byte_array_append(buf_rsp, buf->data + offset, buf->len - offset);
+	return TRUE;
+}
+
+static GByteArray *
+fu_lenovo_accessory_ble_device_process(FuLenovoAccessoryImpl *impl, GByteArray *buf, GError **error)
+{
+	FuLenovoAccessoryBleDevice *self = FU_LENOVO_ACCESSORY_BLE_DEVICE(impl);
+	g_autoptr(GByteArray) buf_rsp = g_byte_array_new();
+
+	if (!fu_lenovo_accessory_ble_device_write(impl, buf, error)) {
+		g_prefix_error_literal(error, "failed to write cmd: ");
+		return NULL;
+	}
+	if (!fu_device_retry_full(FU_DEVICE(self),
+				  fu_lenovo_accessory_ble_device_poll_cb,
+				  50, /* count */
+				  10, /* ms */
+				  buf_rsp,
+				  error))
+		return NULL;
+	return g_steal_pointer(&buf_rsp);
+}
+
+static void
+fu_lenovo_accessory_ble_device_init(FuLenovoAccessoryBleDevice *self)
+{
+	fu_device_set_remove_delay(FU_DEVICE(self), 10000); /* ms */
+	fu_device_add_protocol(FU_DEVICE(self), "com.lenovo.accessory");
+	fu_device_set_install_duration(FU_DEVICE(self), 60);
+	fu_device_set_version_format(FU_DEVICE(self), FWUPD_VERSION_FORMAT_TRIPLET);
+	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_UPDATABLE);
+	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_UNSIGNED_PAYLOAD);
+	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_DUAL_IMAGE);
+	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_USABLE_DURING_UPDATE);
+}
+
+static void
+fu_lenovo_accessory_ble_device_impl_iface_init(FuLenovoAccessoryImplInterface *iface)
+{
+	iface->read = fu_lenovo_accessory_ble_device_read;
+	iface->write = fu_lenovo_accessory_ble_device_write;
+	iface->process = fu_lenovo_accessory_ble_device_process;
+}
+
+static void
+fu_lenovo_accessory_ble_device_class_init(FuLenovoAccessoryBleDeviceClass *klass)
+{
+	FuDeviceClass *device_class = FU_DEVICE_CLASS(klass);
+	device_class->write_firmware = fu_lenovo_accessory_ble_device_write_firmware;
+	device_class->set_progress = fu_lenovo_accessory_ble_device_set_progress;
+	device_class->setup = fu_lenovo_accessory_ble_device_setup;
+	device_class->attach = fu_lenovo_accessory_ble_device_attach;
+}
---
+++ plugins/lenovo-accessory/fu-lenovo-accessory-impl.c
@@ -0,0 +1,356 @@
+/*
+ * Copyright 2026 Yuchao Li <liyc44@lenovo.com>
+ * Copyright 2026 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#include "config.h"
+
+#include "fu-lenovo-accessory-impl.h"
+
+G_DEFINE_INTERFACE(FuLenovoAccessoryImpl, fu_lenovo_accessory_impl, G_TYPE_OBJECT)
+
+static void
+fu_lenovo_accessory_impl_default_init(FuLenovoAccessoryImplInterface *iface)
+{
+}
+
+#if 0
+static GByteArray *
+fu_lenovo_accessory_impl_read(FuLenovoAccessoryImpl *self, GError **error)
+{
+	FuLenovoAccessoryImplInterface *iface;
+
+	g_return_val_if_fail(FU_IS_LENOVO_ACCESSORY_IMPL(self), NULL);
+
+	iface = FU_LENOVO_ACCESSORY_IMPL_GET_IFACE(self);
+	if (iface->read == NULL) {
+		g_set_error_literal(error,
+				    FWUPD_ERROR,
+				    FWUPD_ERROR_INTERNAL,
+				    "iface->read not implemented");
+		return NULL;
+	}
+	return (*iface->read)(self, error);
+}
+#endif
+
+static gboolean
+fu_lenovo_accessory_impl_write(FuLenovoAccessoryImpl *self, GByteArray *buf, GError **error)
+{
+	FuLenovoAccessoryImplInterface *iface;
+
+	g_return_val_if_fail(FU_IS_LENOVO_ACCESSORY_IMPL(self), FALSE);
+
+	iface = FU_LENOVO_ACCESSORY_IMPL_GET_IFACE(self);
+	if (iface->write == NULL) {
+		g_set_error_literal(error,
+				    FWUPD_ERROR,
+				    FWUPD_ERROR_INTERNAL,
+				    "iface->write not implemented");
+		return FALSE;
+	}
+	return (*iface->write)(self, buf, error);
+}
+
+static GByteArray *
+fu_lenovo_accessory_impl_process(FuLenovoAccessoryImpl *self, GByteArray *buf, GError **error)
+{
+	FuLenovoAccessoryImplInterface *iface;
+
+	g_return_val_if_fail(FU_IS_LENOVO_ACCESSORY_IMPL(self), NULL);
+
+	iface = FU_LENOVO_ACCESSORY_IMPL_GET_IFACE(self);
+	if (iface->process == NULL) {
+		g_set_error_literal(error,
+				    FWUPD_ERROR,
+				    FWUPD_ERROR_INTERNAL,
+				    "iface->process not implemented");
+		return NULL;
+	}
+	return (*iface->process)(self, buf, error);
+}
+
+gboolean
+fu_lenovo_accessory_impl_get_fwversion(FuLenovoAccessoryImpl *self,
+				       guint8 *major,
+				       guint8 *minor,
+				       guint8 *micro,
+				       GError **error)
+{
+	g_autoptr(GByteArray) buf = NULL;
+	g_autoptr(FuStructLenovoAccessoryCmd) st_cmd = fu_struct_lenovo_accessory_cmd_new();
+	g_autoptr(FuStructLenovoFwVersionRsp) st_rsp = NULL;
+
+	fu_struct_lenovo_accessory_cmd_set_data_size(st_cmd, 0x03);
+	fu_struct_lenovo_accessory_cmd_set_command_class(
+	    st_cmd,
+	    FU_LENOVO_ACCESSORY_COMMAND_CLASS_DEVICE_INFORMATION);
+	fu_struct_lenovo_accessory_cmd_set_command_id(
+	    st_cmd,
+	    FU_LENOVO_ACCESSORY_INFO_ID_FIRMWARE_VERSION |
+		(FU_LENOVO_ACCESSORY_CMD_DIR_CMD_GET << 7));
+	buf = fu_lenovo_accessory_impl_process(self, st_cmd->buf, error);
+	if (buf == NULL)
+		return FALSE;
+	st_rsp = fu_struct_lenovo_fw_version_rsp_parse(buf->data, buf->len, 0x0, error);
+	if (st_rsp == NULL)
+		return FALSE;
+	if (major != NULL)
+		*major = fu_struct_lenovo_fw_version_rsp_get_major(st_rsp);
+	if (minor != NULL)
+		*minor = fu_struct_lenovo_fw_version_rsp_get_minor(st_rsp);
+	if (micro != NULL)
+		*micro = fu_struct_lenovo_fw_version_rsp_get_internal(st_rsp);
+	return TRUE;
+}
+
+#if 0
+gboolean
+fu_lenovo_accessory_impl_get_mode(FuLenovoAccessoryImpl *self, FuLenovoDeviceMode *mode, GError **error)
+{
+	g_autoptr(FuStructLenovoAccessoryCmd) st_cmd = fu_struct_lenovo_accessory_cmd_new();
+	g_autoptr(FuStructLenovoDevicemodeRsp) st_rsp = NULL;
+	g_autoptr(GByteArray) buf = NULL;
+
+	fu_struct_lenovo_accessory_cmd_set_data_size(st_cmd, 0x01);
+	fu_struct_lenovo_accessory_cmd_set_command_class(
+	    st_cmd,
+	    FU_LENOVO_ACCESSORY_COMMAND_CLASS_DEVICE_INFORMATION);
+	fu_struct_lenovo_accessory_cmd_set_command_id(
+	    st_cmd,
+	    FU_LENOVO_ACCESSORY_INFO_ID_DEVICE_MODE | (FU_LENOVO_ACCESSORY_CMD_DIR_CMD_SET << 7));
+	buf = fu_lenovo_accessory_impl_process(self, st_cmd->buf, error);
+	if (buf == NULL)
+		return FALSE;
+	st_rsp = fu_struct_lenovo_devicemode_rsp_parse(buf->data, buf->len, 0x0, error);
+	if (st_rsp == NULL)
+		return FALSE;
+	*mode = fu_struct_lenovo_devicemode_rsp_get_mode(st_rsp);
+
+	/* success */
+	return TRUE;
+}
+#endif
+
+gboolean
+fu_lenovo_accessory_impl_set_mode(FuLenovoAccessoryImpl *self,
+				  FuLenovoDeviceMode mode,
+				  GError **error)
+{
+	g_autoptr(FuStructLenovoAccessoryCmd) st_cmd = fu_struct_lenovo_accessory_cmd_new();
+	g_autoptr(FuStructLenovoDevicemodeReq) st_req = fu_struct_lenovo_devicemode_req_new();
+	g_autoptr(GByteArray) buf = NULL;
+
+	fu_struct_lenovo_accessory_cmd_set_data_size(st_cmd, 0x01);
+	fu_struct_lenovo_accessory_cmd_set_command_class(
+	    st_cmd,
+	    FU_LENOVO_ACCESSORY_COMMAND_CLASS_DEVICE_INFORMATION);
+	fu_struct_lenovo_accessory_cmd_set_command_id(
+	    st_cmd,
+	    FU_LENOVO_ACCESSORY_INFO_ID_DEVICE_MODE | (FU_LENOVO_ACCESSORY_CMD_DIR_CMD_SET << 7));
+	if (!fu_struct_lenovo_devicemode_req_set_cmd(st_req, st_cmd, error))
+		return FALSE;
+	fu_struct_lenovo_devicemode_req_set_mode(st_req, mode);
+	if (mode == FU_LENOVO_DEVICE_MODE_DFU_MODE)
+		return fu_lenovo_accessory_impl_write(self, st_req->buf, error);
+	buf = fu_lenovo_accessory_impl_process(self, st_req->buf, error);
+	if (buf == NULL)
+		return FALSE;
+
+	/* success */
+	return TRUE;
+}
+
+/* @exit_code: the exit status code (e.g., 0x00 for success/reboot) */
+gboolean
+fu_lenovo_accessory_impl_dfu_exit(FuLenovoAccessoryImpl *self,
+				  FuLenovoDfuExitCode exit_code,
+				  GError **error)
+{
+	g_autoptr(FuStructLenovoAccessoryCmd) st_cmd = fu_struct_lenovo_accessory_cmd_new();
+	g_autoptr(FuStructLenovoDfuExitReq) st_req = fu_struct_lenovo_dfu_exit_req_new();
+	g_autoptr(GError) error_local = NULL;
+
+	fu_struct_lenovo_accessory_cmd_set_data_size(st_cmd, 0x01);
+	fu_struct_lenovo_accessory_cmd_set_command_class(
+	    st_cmd,
+	    FU_LENOVO_ACCESSORY_COMMAND_CLASS_DFU_CLASS);
+	fu_struct_lenovo_accessory_cmd_set_command_id(
+	    st_cmd,
+	    FU_LENOVO_ACCESSORY_DFU_ID_DFU_EXIT | (FU_LENOVO_ACCESSORY_CMD_DIR_CMD_SET << 7));
+	if (!fu_struct_lenovo_dfu_exit_req_set_cmd(st_req, st_cmd, error))
+		return FALSE;
+	fu_struct_lenovo_dfu_exit_req_set_exit_code(st_req, exit_code);
+
+	/*
+	 * The device performs an immediate reset/reboot as soon as it receives the
+	 * DFU_EXIT command and therefore never sends back an ACK. The resulting
+	 * error (e.g., -EPIPE or -EIO) is expected and indicates that the reboot
+	 * was successfully triggered.
+	 */
+	if (!fu_lenovo_accessory_impl_write(self, st_req->buf, &error_local))
+		g_debug("ignoring: %s", error_local->message);
+
+	/* success */
+	return TRUE;
+}
+
+gboolean
+fu_lenovo_accessory_impl_dfu_attribute(FuLenovoAccessoryImpl *self,
+				       guint8 *major_ver,
+				       guint8 *minor_ver,
+				       guint16 *product_pid,
+				       guint8 *processor_id,
+				       guint32 *app_max_size,
+				       guint32 *page_size,
+				       GError **error)
+{
+	g_autoptr(FuStructLenovoAccessoryCmd) st_cmd = fu_struct_lenovo_accessory_cmd_new();
+	g_autoptr(FuStructLenovoDfuAttributeRsp) st_rsp = NULL;
+	g_autoptr(GByteArray) buf = NULL;
+
+	fu_struct_lenovo_accessory_cmd_set_data_size(st_cmd, 0x0D);
+	fu_struct_lenovo_accessory_cmd_set_command_class(
+	    st_cmd,
+	    FU_LENOVO_ACCESSORY_COMMAND_CLASS_DFU_CLASS);
+	fu_struct_lenovo_accessory_cmd_set_command_id(
+	    st_cmd,
+	    FU_LENOVO_ACCESSORY_DFU_ID_DFU_ATTRIBUTE | (FU_LENOVO_ACCESSORY_CMD_DIR_CMD_GET << 7));
+	buf = fu_lenovo_accessory_impl_process(self, st_cmd->buf, error);
+	if (buf == NULL)
+		return FALSE;
+	st_rsp = fu_struct_lenovo_dfu_attribute_rsp_parse(buf->data, buf->len, 0x0, error);
+	if (st_rsp == NULL)
+		return FALSE;
+
+	/* success */
+	if (major_ver != NULL)
+		*major_ver = fu_struct_lenovo_dfu_attribute_rsp_get_major_ver(st_rsp);
+	if (minor_ver != NULL)
+		*minor_ver = fu_struct_lenovo_dfu_attribute_rsp_get_minor_ver(st_rsp);
+	if (product_pid != NULL)
+		*product_pid = fu_struct_lenovo_dfu_attribute_rsp_get_product_pid(st_rsp);
+	if (processor_id != NULL)
+		*processor_id = fu_struct_lenovo_dfu_attribute_rsp_get_processor_id(st_rsp);
+	if (app_max_size != NULL)
+		*app_max_size = fu_struct_lenovo_dfu_attribute_rsp_get_app_max_size(st_rsp);
+	if (page_size != NULL)
+		*page_size = fu_struct_lenovo_dfu_attribute_rsp_get_page_size(st_rsp);
+	return TRUE;
+}
+
+gboolean
+fu_lenovo_accessory_impl_dfu_prepare(FuLenovoAccessoryImpl *self,
+				     FuLenovoDfuFileType file_type,
+				     guint32 start_address,
+				     guint32 end_address,
+				     guint32 crc32,
+				     GError **error)
+{
+	g_autoptr(FuStructLenovoAccessoryCmd) st_cmd = fu_struct_lenovo_accessory_cmd_new();
+	g_autoptr(FuStructLenovoDfuPrepareReq) st_req = fu_struct_lenovo_dfu_prepare_req_new();
+	g_autoptr(GByteArray) buf = NULL;
+
+	fu_struct_lenovo_accessory_cmd_set_data_size(st_cmd, 0x0D);
+	fu_struct_lenovo_accessory_cmd_set_command_class(
+	    st_cmd,
+	    FU_LENOVO_ACCESSORY_COMMAND_CLASS_DFU_CLASS);
+	fu_struct_lenovo_accessory_cmd_set_command_id(
+	    st_cmd,
+	    FU_LENOVO_ACCESSORY_DFU_ID_DFU_PREPARE | (FU_LENOVO_ACCESSORY_CMD_DIR_CMD_SET << 7));
+	if (!fu_struct_lenovo_dfu_prepare_req_set_cmd(st_req, st_cmd, error))
+		return FALSE;
+	fu_struct_lenovo_dfu_prepare_req_set_file_type(st_req, file_type);
+	fu_struct_lenovo_dfu_prepare_req_set_start_address(st_req, start_address);
+	fu_struct_lenovo_dfu_prepare_req_set_end_address(st_req, end_address);
+	fu_struct_lenovo_dfu_prepare_req_set_crc32(st_req, crc32);
+	buf = fu_lenovo_accessory_impl_process(self, st_req->buf, error);
+	if (buf == NULL)
+		return FALSE;
+
+	/* success */
+	return TRUE;
+}
+
+gboolean
+fu_lenovo_accessory_impl_dfu_file(FuLenovoAccessoryImpl *self,
+				  FuLenovoDfuFileType file_type,
+				  guint32 address,
+				  const guint8 *data,
+				  gsize datasz,
+				  GError **error)
+{
+	g_autoptr(FuStructLenovoAccessoryCmd) st_cmd = fu_struct_lenovo_accessory_cmd_new();
+	g_autoptr(FuStructLenovoDfuFwReq) st_req = fu_struct_lenovo_dfu_fw_req_new();
+	g_autoptr(GByteArray) buf = NULL;
+
+	fu_struct_lenovo_accessory_cmd_set_data_size(st_cmd, datasz + 5);
+	fu_struct_lenovo_accessory_cmd_set_command_class(
+	    st_cmd,
+	    FU_LENOVO_ACCESSORY_COMMAND_CLASS_DFU_CLASS);
+	fu_struct_lenovo_accessory_cmd_set_command_id(
+	    st_cmd,
+	    FU_LENOVO_ACCESSORY_DFU_ID_DFU_FILE | (FU_LENOVO_ACCESSORY_CMD_DIR_CMD_SET << 7));
+	if (!fu_struct_lenovo_dfu_fw_req_set_cmd(st_req, st_cmd, error))
+		return FALSE;
+	fu_struct_lenovo_dfu_fw_req_set_file_type(st_req, file_type);
+	fu_struct_lenovo_dfu_fw_req_set_offset_address(st_req, address);
+	if (!fu_struct_lenovo_dfu_fw_req_set_data(st_req, data, datasz, error))
+		return FALSE;
+	buf = fu_lenovo_accessory_impl_process(self, st_req->buf, error);
+	if (buf == NULL)
+		return FALSE;
+
+	/* success */
+	return TRUE;
+}
+
+gboolean
+fu_lenovo_accessory_impl_dfu_crc(FuLenovoAccessoryImpl *self, guint32 *crc32, GError **error)
+{
+	g_autoptr(FuStructLenovoAccessoryCmd) st_cmd = fu_struct_lenovo_accessory_cmd_new();
+	g_autoptr(FuStructLenovoDfuCrcRsp) st_rsp = NULL;
+	g_autoptr(GByteArray) buf = NULL;
+
+	fu_struct_lenovo_accessory_cmd_set_data_size(st_cmd, 0x05);
+	fu_struct_lenovo_accessory_cmd_set_command_class(
+	    st_cmd,
+	    FU_LENOVO_ACCESSORY_COMMAND_CLASS_DFU_CLASS);
+	fu_struct_lenovo_accessory_cmd_set_command_id(
+	    st_cmd,
+	    FU_LENOVO_ACCESSORY_DFU_ID_DFU_CRC | (FU_LENOVO_ACCESSORY_CMD_DIR_CMD_GET << 7));
+
+	buf = fu_lenovo_accessory_impl_process(self, st_cmd->buf, error);
+	if (buf == NULL)
+		return FALSE;
+	st_rsp = fu_struct_lenovo_dfu_crc_rsp_parse(buf->data, buf->len, 0x0, error);
+	if (st_rsp == NULL)
+		return FALSE;
+
+	/* success */
+	if (crc32 != NULL)
+		*crc32 = fu_struct_lenovo_dfu_crc_rsp_get_crc32(st_rsp);
+	return TRUE;
+}
+
+gboolean
+fu_lenovo_accessory_impl_dfu_entry(FuLenovoAccessoryImpl *self, GError **error)
+{
+	g_autoptr(FuStructLenovoAccessoryCmd) st_cmd = fu_struct_lenovo_accessory_cmd_new();
+	g_autoptr(GByteArray) buf = NULL;
+
+	fu_struct_lenovo_accessory_cmd_set_command_class(
+	    st_cmd,
+	    FU_LENOVO_ACCESSORY_COMMAND_CLASS_DFU_CLASS);
+	fu_struct_lenovo_accessory_cmd_set_command_id(
+	    st_cmd,
+	    FU_LENOVO_ACCESSORY_DFU_ID_DFU_ENTRY | (FU_LENOVO_ACCESSORY_CMD_DIR_CMD_SET << 7));
+	buf = fu_lenovo_accessory_impl_process(self, st_cmd->buf, error);
+	if (buf == NULL)
+		return FALSE;
+
+	/* success */
+	return TRUE;
+}
