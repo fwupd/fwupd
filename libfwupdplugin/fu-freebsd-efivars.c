a8ebdb7219d943f5c96b4dddbd6ffd8603a54a75
Daniel Schaefer <dhs@frame.work>
2026-01-13 23:35:33 +0800

    libfwupdplugin: Adapt for latest freebsd efivars
    See: https://github.com/freebsd/freebsd-ports/commit/164a3b4dd92d2201907be9fb12cd1dedeb2e4e5d
    
    Co-authored-by: decke <decke@FreeBSD.org>
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- libfwupdplugin/fu-freebsd-efivars.c
+++ libfwupdplugin/fu-freebsd-efivars.c
@@ -67,7 +67,7 @@ fu_freebsd_efivars_delete_with_glob(FuEfivars *efivars,
 
 	efi_str_to_guid(guid, &guid_to_delete);
 
-	while (efi_get_next_variable_name(&guidt, &name)) {
+	while (efi_get_next_variable_name(&guidt, &name) == 1) {
 		if (memcmp(&guid_to_delete, guidt, sizeof(guid_to_delete)) != 0)
 			continue;
 		if (!g_pattern_match_simple(name, name_glob))
@@ -87,7 +87,7 @@ fu_freebsd_efivars_exists_guid(const gchar *guid)
 	efi_guid_t test;
 
 	efi_str_to_guid(guid, &test);
-	while (efi_get_next_variable_name(&guidt, &name)) {
+	while (efi_get_next_variable_name(&guidt, &name) == 1) {
 		if (memcmp(&test, guidt, sizeof(test)) == 0)
 			return TRUE;
 	}
@@ -128,7 +128,7 @@ fu_freebsd_efivars_get_names(FuEfivars *efivars, const gchar *guid, GError **err
 	efi_str_to_guid(guid, &test);
 
 	/* find names with matching GUID */
-	while (efi_get_next_variable_name(&guidt, &name)) {
+	while (efi_get_next_variable_name(&guidt, &name) == 1) {
 		if (memcmp(&test, guidt, sizeof(test)) == 0)
 			g_ptr_array_add(names, g_strdup(name));
 	}
@@ -154,7 +154,7 @@ fu_freebsd_efivars_space_used(FuEfivars *efivars, GError **error)
 	efi_guid_t *guidt = NULL;
 	char *name = NULL;
 
-	while (efi_get_next_variable_name(&guidt, &name)) {
+	while (efi_get_next_variable_name(&guidt, &name) == 1) {
 		size_t size = 0;
 		if (efi_get_variable_size(*guidt, name, &size) < 0) {
 			g_set_error_literal(error,
@@ -182,7 +182,7 @@ fu_freebsd_efivars_set_data(FuEfivars *efivars,
 	efi_guid_t guidt;
 	efi_str_to_guid(guid, &guidt);
 
-	if (efi_set_variable(guidt, name, (guint8 *)data, sz, attr) != 0) {
+	if (efi_set_variable(guidt, name, (guint8 *)data, sz, attr, 0644) != 0) {
 		g_set_error(error,
 			    FWUPD_ERROR,
 			    FWUPD_ERROR_NOT_SUPPORTED,
