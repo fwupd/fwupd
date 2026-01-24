4fc43823a18e88c3874fa21cbd7981644a668f8e
Richard Hughes <richard@hughsie.com>
2026-01-24 16:47:00 +0000

    Split out some of the test code to new files
    Our largest self test source file is 8,000 lines -- which is clearly too big.

---
+++ libfwupd/fwupd-json-test.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2026 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#pragma once
+
+#include <glib.h>
+
+void
+fwupd_json_setup_tests(void);
