9e1fc06fcc2864c43a0da5657f1e35df0e43dc01
Richard Hughes <richard@hughsie.com>
2026-01-26 12:32:56 +0000

    lenovo-thinklmi: Do not create /tmp/fwupd-self-test without reason

--- plugins/lenovo-thinklmi/fu-self-test.c
+++ plugins/lenovo-thinklmi/fu-self-test.c
@@ -206,8 +206,6 @@ main(int argc, char **argv)
 	/* only critical and error are fatal */
 	g_log_set_fatal_mask(NULL, G_LOG_LEVEL_ERROR | G_LOG_LEVEL_CRITICAL);
 
-	g_assert_cmpint(g_mkdir_with_parents("/tmp/fwupd-self-test/var/lib/fwupd", 0755), ==, 0);
-
 	/* tests go here */
 	fu_test_self_init(self);
 	g_test_add_data_func("/fwupd/plugin{lenovo-think-lmi:bootorder-locked}",
