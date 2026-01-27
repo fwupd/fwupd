4b9c796581331aca554358d2d68e564705a16afd
Richard Hughes <richard@hughsie.com>
2026-01-27 09:27:41 +0000

    trivial: Split out fu-byte-array-test.c

--- libfwupdplugin/fu-self-test.c
+++ libfwupdplugin/fu-self-test.c
@@ -356,67 +356,6 @@ fu_common_bitwise_func(void)
 }
 
 static void
-fu_common_byte_array_safe_func(void)
-{
-	gboolean ret;
-	g_autoptr(GError) error = NULL;
-	g_autoptr(GByteArray) array = g_byte_array_new();
-	const guint8 buf[] = {0x01, 0x02, 0x03};
-
-	/* all buffer */
-	ret = fu_byte_array_append_safe(array, buf, sizeof(buf), 0, sizeof(buf), &error);
-	g_assert_no_error(error);
-	g_assert_true(ret);
-	g_assert_cmpint(array->len, ==, 3);
-
-	/* +1, -1 */
-	ret = fu_byte_array_append_safe(array, buf, sizeof(buf), 1, sizeof(buf) - 1, &error);
-	g_assert_no_error(error);
-	g_assert_true(ret);
-	g_assert_cmpint(array->len, ==, 3 + 2);
-
-	/* boom */
-	ret = fu_byte_array_append_safe(array, buf, sizeof(buf), 1, sizeof(buf), &error);
-	g_assert_error(error, FWUPD_ERROR, FWUPD_ERROR_READ);
-	g_assert_false(ret);
-}
-
-static void
-fu_common_byte_array_func(void)
-{
-	g_autofree gchar *str = NULL;
-	g_autoptr(GByteArray) array = g_byte_array_new();
-	g_autoptr(GByteArray) array2 = NULL;
-	g_autoptr(GByteArray) array3 = NULL;
-	g_autoptr(GError) error = NULL;
-
-	fu_byte_array_append_uint8(array, (guint8)'h');
-	fu_byte_array_append_uint8(array, (guint8)'e');
-	fu_byte_array_append_uint8(array, (guint8)'l');
-	fu_byte_array_append_uint8(array, (guint8)'l');
-	fu_byte_array_append_uint8(array, (guint8)'o');
-	g_assert_cmpint(array->len, ==, 5);
-	g_assert_cmpint(memcmp(array->data, "hello", array->len), ==, 0);
-
-	fu_byte_array_set_size(array, 10, 0x00);
-	g_assert_cmpint(array->len, ==, 10);
-	g_assert_cmpint(memcmp(array->data, "hello\0\0\0\0\0", array->len), ==, 0);
-
-	str = fu_byte_array_to_string(array);
-	g_assert_cmpstr(str, ==, "68656c6c6f0000000000");
-
-	array2 = fu_byte_array_from_string(str, &error);
-	g_assert_no_error(error);
-	g_assert_nonnull(array2);
-	g_assert_cmpint(array2->len, ==, 10);
-	g_assert_cmpint(memcmp(array2->data, "hello\0\0\0\0\0", array2->len), ==, 0);
-
-	array3 = fu_byte_array_from_string("ZZZ", &error);
-	g_assert_error(error, FWUPD_ERROR, FWUPD_ERROR_INVALID_DATA);
-	g_assert_null(array3);
-}
-
-static void
 fu_common_crc_func(void)
 {
 	guint8 buf[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
@@ -7365,8 +7304,6 @@ main(int argc, char **argv)
 	g_test_add_func("/fwupd/common{error-map}", fu_common_error_map_func);
 	g_test_add_func("/fwupd/common{align-up}", fu_common_align_up_func);
 	g_test_add_func("/fwupd/common{bitwise}", fu_common_bitwise_func);
-	g_test_add_func("/fwupd/common{byte-array}", fu_common_byte_array_func);
-	g_test_add_func("/fwupd/common{byte-array-safe}", fu_common_byte_array_safe_func);
 	g_test_add_func("/fwupd/common{crc}", fu_common_crc_func);
 	g_test_add_func("/fwupd/common{guid}", fu_common_guid_func);
 	g_test_add_func("/fwupd/common{string-append-kv}", fu_string_append_func);
