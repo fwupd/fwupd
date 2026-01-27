07ee642ed2285bb392e5f51114705fc1f7f1cd60
Richard Hughes <richard@hughsie.com>
2026-01-27 10:29:52 +0000

    trivial: Split out fu-kernel-test.c

--- libfwupdplugin/fu-self-test.c
+++ libfwupdplugin/fu-self-test.c
@@ -467,40 +467,6 @@ fu_smbios_func(void)
 }
 
 static void
-fu_kernel_cmdline_func(void)
-{
-	const gchar *buf = "key=val foo bar=\"baz baz baz\" tail\n";
-	g_autoptr(GHashTable) hash = NULL;
-
-	hash = fu_kernel_parse_cmdline(buf, strlen(buf));
-	g_assert_nonnull(hash);
-	g_assert_true(g_hash_table_contains(hash, "key"));
-	g_assert_cmpstr(g_hash_table_lookup(hash, "key"), ==, "val");
-	g_assert_true(g_hash_table_contains(hash, "foo"));
-	g_assert_cmpstr(g_hash_table_lookup(hash, "foo"), ==, NULL);
-	g_assert_true(g_hash_table_contains(hash, "bar"));
-	g_assert_cmpstr(g_hash_table_lookup(hash, "bar"), ==, "baz baz baz");
-	g_assert_true(g_hash_table_contains(hash, "tail"));
-	g_assert_false(g_hash_table_contains(hash, ""));
-}
-
-static void
-fu_kernel_config_func(void)
-{
-	const gchar *buf = "CONFIG_LOCK_DOWN_KERNEL_FORCE_NONE=y\n\n"
-			   "# CONFIG_LOCK_DOWN_KERNEL_FORCE_INTEGRITY is not set\n";
-	g_autoptr(GHashTable) hash = NULL;
-	g_autoptr(GError) error = NULL;
-
-	hash = fu_kernel_parse_config(buf, strlen(buf), &error);
-	g_assert_no_error(error);
-	g_assert_nonnull(hash);
-	g_assert_true(g_hash_table_contains(hash, "CONFIG_LOCK_DOWN_KERNEL_FORCE_NONE"));
-	g_assert_cmpstr(g_hash_table_lookup(hash, "CONFIG_LOCK_DOWN_KERNEL_FORCE_NONE"), ==, "y");
-	g_assert_false(g_hash_table_contains(hash, "CONFIG_LOCK_DOWN_KERNEL_FORCE_INTEGRITY"));
-}
-
-static void
 fu_smbios3_func(void)
 {
 	const gchar *str;
@@ -1574,35 +1540,6 @@ fu_plugin_quirks_device_func(void)
 	child2 = fu_device_get_child_by_logical_id(device, "SPI", &error);
 	g_assert_error(error, FWUPD_ERROR, FWUPD_ERROR_NOT_FOUND);
 	g_assert_null(child2);
-}
-
-static void
-fu_common_kernel_lockdown_func(void)
-{
-	gboolean ret;
-	g_autofree gchar *locked_dir = NULL;
-	g_autofree gchar *none_dir = NULL;
-	g_autofree gchar *old_kernel_dir = NULL;
-
-#ifndef __linux__
-	g_test_skip("only works on Linux");
-	return;
-#endif
-
-	old_kernel_dir = g_test_build_filename(G_TEST_DIST, "tests", "lockdown", NULL);
-	(void)g_setenv("FWUPD_SYSFSSECURITYDIR", old_kernel_dir, TRUE);
-	ret = fu_kernel_locked_down();
-	g_assert_false(ret);
-
-	locked_dir = g_test_build_filename(G_TEST_DIST, "tests", "lockdown", "locked", NULL);
-	(void)g_setenv("FWUPD_SYSFSSECURITYDIR", locked_dir, TRUE);
-	ret = fu_kernel_locked_down();
-	g_assert_true(ret);
-
-	none_dir = g_test_build_filename(G_TEST_DIST, "tests", "lockdown", "none", NULL);
-	(void)g_setenv("FWUPD_SYSFSSECURITYDIR", none_dir, TRUE);
-	ret = fu_kernel_locked_down();
-	g_assert_false(ret);
 }
 
 static gboolean
@@ -4778,7 +4715,6 @@ main(int argc, char **argv)
 	g_test_add_func("/fwupd/common{crc}", fu_common_crc_func);
 	g_test_add_func("/fwupd/common{guid}", fu_common_guid_func);
 	g_test_add_func("/fwupd/common{string-append-kv}", fu_string_append_func);
-	g_test_add_func("/fwupd/common{kernel-lockdown}", fu_common_kernel_lockdown_func);
 	g_test_add_func("/fwupd/common{strsafe}", fu_strsafe_func);
 	g_test_add_func("/fwupd/common{cpuid}", fu_cpuid_func);
 	g_test_add_func("/fwupd/efi-load-option", fu_efi_load_option_func);
@@ -4802,8 +4738,6 @@ main(int argc, char **argv)
 	g_test_add_func("/fwupd/context{udev-subsystems}", fu_context_udev_subsystems_func);
 	g_test_add_func("/fwupd/smbios", fu_smbios_func);
 	g_test_add_func("/fwupd/smbios3", fu_smbios3_func);
-	g_test_add_func("/fwupd/kernel{cmdline}", fu_kernel_cmdline_func);
-	g_test_add_func("/fwupd/kernel{config}", fu_kernel_config_func);
 	g_test_add_func("/fwupd/hid{descriptor}", fu_hid_descriptor_func);
 	g_test_add_func("/fwupd/hid{descriptor-container}", fu_hid_descriptor_container_func);
 	g_test_add_func("/fwupd/firmware", fu_firmware_func);
