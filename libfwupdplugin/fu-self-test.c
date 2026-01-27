60c802c18122ecdae91fbac9a40a2f2928cda56e
Richard Hughes <richard@hughsie.com>
2026-01-27 09:21:53 +0000

    trivial: Split out fu-temporary-directory-test.c

--- libfwupdplugin/fu-self-test.c
+++ libfwupdplugin/fu-self-test.c
@@ -7528,31 +7528,6 @@ fu_efi_load_option_func(void)
 	}
 }
 
-static void
-fu_temporary_directory_func(void)
-{
-	const gchar *tmpdir_path;
-	g_autofree gchar *tmpdir_fn = NULL;
-	g_autofree gchar *tmpdir_path_copy = NULL;
-	g_autoptr(FuTemporaryDirectory) tmpdir = NULL;
-	g_autoptr(GError) error = NULL;
-
-	tmpdir = fu_temporary_directory_new("foobar", &error);
-	g_assert_no_error(error);
-	g_assert_nonnull(tmpdir);
-
-	tmpdir_path = fu_temporary_directory_get_path(tmpdir);
-	g_assert_nonnull(tmpdir_path);
-	g_assert_true(g_strstr_len(tmpdir_path, -1, "foobar") != NULL);
-	g_assert_true(g_file_test(tmpdir_path, G_FILE_TEST_IS_DIR));
-
-	tmpdir_fn = fu_temporary_directory_build(tmpdir, "baz", NULL);
-	tmpdir_path_copy = g_strdup(tmpdir_path);
-	g_clear_object(&tmpdir);
-	g_assert_false(g_file_test(tmpdir_fn, G_FILE_TEST_EXISTS));
-	g_assert_false(g_file_test(tmpdir_path_copy, G_FILE_TEST_IS_DIR));
-}
-
 int
 main(int argc, char **argv)
 {
@@ -7763,6 +7738,5 @@ main(int argc, char **argv)
 	g_test_add_func("/fwupd/intel-me16-device", fu_intel_me16_device_func);
 	g_test_add_func("/fwupd/intel-me16-device{hap}", fu_intel_me16_device_hap_func);
 	g_test_add_func("/fwupd/intel-me18-device", fu_intel_me18_device_func);
-	g_test_add_func("/fwupd/temporary_directory", fu_temporary_directory_func);
 	return g_test_run();
 }
