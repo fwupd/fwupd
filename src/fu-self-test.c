dd60cff8d96d198d5e3df8fc2465f7333698daa2
Richard Hughes <richard@hughsie.com>
2026-01-30 09:19:08 +0000

    trivial: Use libfutest to remove some code duplication

--- src/fu-self-test.c
+++ src/fu-self-test.c
@@ -38,6 +38,7 @@
 #include "fu-remote-list.h"
 #include "fu-remote.h"
 #include "fu-security-attrs-private.h"
+#include "fu-test.h"
 #include "fu-usb-backend.h"
 #include "fu-util-common.h"
 
@@ -51,68 +52,6 @@ typedef struct {
 	FuPlugin *plugin;
 	FuContext *ctx;
 } FuTest;
-
-/* nocheck:static */
-static GMainLoop *_test_loop = NULL;
-static guint _test_loop_timeout_id = 0;
-
-static gboolean
-fu_test_hang_check_cb(gpointer user_data)
-{
-	g_main_loop_quit(_test_loop);
-	_test_loop_timeout_id = 0;
-	return G_SOURCE_REMOVE;
-}
-
-static void
-fu_test_loop_run_with_timeout(guint timeout_ms)
-{
-	g_assert_cmpint(_test_loop_timeout_id, ==, 0);
-	g_assert_null(_test_loop);
-	_test_loop = g_main_loop_new(NULL, FALSE);
-	_test_loop_timeout_id = g_timeout_add(timeout_ms, fu_test_hang_check_cb, NULL);
-	g_main_loop_run(_test_loop);
-}
-
-static void
-fu_test_loop_quit(void)
-{
-	if (_test_loop_timeout_id > 0) {
-		g_source_remove(_test_loop_timeout_id);
-		_test_loop_timeout_id = 0;
-	}
-	if (_test_loop != NULL) {
-		g_main_loop_quit(_test_loop);
-		g_main_loop_unref(_test_loop);
-		_test_loop = NULL;
-	}
-}
-
-static gboolean
-fu_test_compare_lines(const gchar *txt1, const gchar *txt2, GError **error)
-{
-	g_autofree gchar *output = NULL;
-
-	/* exactly the same */
-	if (g_strcmp0(txt1, txt2) == 0)
-		return TRUE;
-
-	/* matches a pattern */
-	if (g_pattern_match_simple(txt2, txt1))
-		return TRUE;
-
-	/* save temp files and diff them */
-	if (!g_file_set_contents("/tmp/a", txt1, -1, error))
-		return FALSE;
-	if (!g_file_set_contents("/tmp/b", txt2, -1, error))
-		return FALSE;
-	if (!g_spawn_command_line_sync("diff -urNp /tmp/b /tmp/a", &output, NULL, NULL, error))
-		return FALSE;
-
-	/* just output the diff */
-	g_set_error_literal(error, 1, 0, output);
-	return FALSE;
-}
 
 static void
 fu_self_test_mkroot(void)
