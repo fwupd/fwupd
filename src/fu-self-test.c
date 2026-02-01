b9d47f87670b3232a3c2403403704ac9f8c88b9c
Richard Hughes <richard@hughsie.com>
2026-02-01 13:11:52 +0000

    trivial: Split out fu-remote-list-test.c

--- src/fu-self-test.c
+++ src/fu-self-test.c
@@ -5599,59 +5599,6 @@ fu_remote_local_func(void)
 }
 
 static void
-fu_remote_list_repair_func(void)
-{
-	gboolean ret;
-	g_autofree gchar *testdatadir = NULL;
-	g_autoptr(FuPathStore) pstore = fu_path_store_new();
-	g_autoptr(FuRemoteList) remote_list = NULL;
-	g_autoptr(FuTemporaryDirectory) tmpdir = NULL;
-	g_autoptr(FwupdRemote) remote1 = NULL;
-	g_autoptr(FwupdRemote) remote2 = NULL;
-	g_autoptr(FwupdRemote) remote3 = NULL;
-	g_autoptr(GError) error = NULL;
-
-	/* set up test harness */
-	tmpdir = fu_temporary_directory_new("remote-list-repair", &error);
-	g_assert_no_error(error);
-	g_assert_nonnull(tmpdir);
-	testdatadir = g_test_build_filename(G_TEST_DIST, "tests", NULL);
-	fu_path_store_set_path(pstore, FU_PATH_KIND_DATADIR_PKG, testdatadir);
-	fu_path_store_set_tmpdir(pstore, FU_PATH_KIND_CACHEDIR_PKG, tmpdir);
-	fu_path_store_set_tmpdir(pstore, FU_PATH_KIND_LOCALSTATEDIR_METADATA, tmpdir);
-	remote_list = fu_remote_list_new(pstore);
-
-	fu_remote_list_set_lvfs_metadata_format(remote_list, "zst");
-	ret = fu_remote_list_load(remote_list, FU_REMOTE_LIST_LOAD_FLAG_FIX_METADATA_URI, &error);
-	g_assert_no_error(error);
-	g_assert_true(ret);
-
-	/* check .gz converted to .zst */
-	remote1 = fu_remote_list_get_by_id(remote_list, "legacy-lvfs", &error);
-	g_assert_no_error(error);
-	g_assert_nonnull(remote1);
-	g_assert_cmpstr(fwupd_remote_get_metadata_uri(remote1),
-			==,
-			"http://localhost/stable.xml.zst");
-
-	/* check .xz converted to .zst */
-	remote2 = fu_remote_list_get_by_id(remote_list, "legacy-lvfs-xz", &error);
-	g_assert_no_error(error);
-	g_assert_nonnull(remote2);
-	g_assert_cmpstr(fwupd_remote_get_metadata_uri(remote2),
-			==,
-			"http://localhost/stable.xml.zst");
-
-	/* check non-LVFS remote NOT .gz converted to .xz */
-	remote3 = fu_remote_list_get_by_id(remote_list, "legacy", &error);
-	g_assert_no_error(error);
-	g_assert_nonnull(remote3);
-	g_assert_cmpstr(fwupd_remote_get_metadata_uri(remote3),
-			==,
-			"http://localhost/stable.xml.gz");
-}
-
-static void
 fu_engine_integrity_func(void)
 {
 	gboolean ret;
@@ -6225,7 +6172,6 @@ main(int argc, char **argv)
 	g_test_add_func("/fwupd/remote/local", fu_remote_local_func);
 	g_test_add_func("/fwupd/remote/duplicate", fu_remote_duplicate_func);
 	g_test_add_func("/fwupd/remote/auth", fu_remote_auth_func);
-	g_test_add_func("/fwupd/remote-list/repair", fu_remote_list_repair_func);
 	g_test_add_func("/fwupd/plugin/module", fu_plugin_module_func);
 	g_test_add_func("/fwupd/release/compare", fu_release_compare_func);
 	g_test_add_func("/fwupd/release/uri-scheme", fu_release_uri_scheme_func);
