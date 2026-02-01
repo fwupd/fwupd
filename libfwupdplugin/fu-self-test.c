28eb4a27902ea0b1d3bb85887ff998c66cdd556c
Richard Hughes <richard@hughsie.com>
2026-02-01 10:32:58 +0000

    trivial: Split out fu-udev-device-test.c

--- libfwupdplugin/fu-self-test.c
+++ libfwupdplugin/fu-self-test.c
@@ -977,27 +977,6 @@ fu_plugin_quirks_device_func(void)
 	g_assert_null(child2);
 }
 
-static void
-fu_udev_device_func(void)
-{
-	g_autofree gchar *prop = NULL;
-	g_autofree gchar *sysfs_path = g_test_build_filename(G_TEST_DIST, "tests", NULL);
-	g_autoptr(FuContext) ctx = fu_context_new();
-	g_autoptr(FuUdevDevice) udev_device = fu_udev_device_new(ctx, sysfs_path);
-	g_autoptr(GError) error = NULL;
-	g_autoptr(GPtrArray) attrs = NULL;
-
-	prop = fu_udev_device_read_property(udev_device, "MODALIAS", &error);
-	g_assert_no_error(error);
-	g_assert_cmpstr(prop, ==, "hdaudio:v10EC0298r00100103a01");
-
-	/* list all the files in the directory */
-	attrs = fu_udev_device_list_sysfs(udev_device, &error);
-	g_assert_no_error(error);
-	g_assert_nonnull(attrs);
-	g_assert_cmpint(attrs->len, >, 10);
-}
-
 int
 main(int argc, char **argv)
 {
@@ -1030,6 +1009,5 @@ main(int argc, char **argv)
 	g_test_add_func("/fwupd/context/udev-subsystems", fu_context_udev_subsystems_func);
 	g_test_add_func("/fwupd/archive/invalid", fu_archive_invalid_func);
 	g_test_add_func("/fwupd/archive/cab", fu_archive_cab_func);
-	g_test_add_func("/fwupd/udev-device", fu_udev_device_func);
 	return g_test_run();
 }
