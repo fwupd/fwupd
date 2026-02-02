eda850d60371d6b29fc10ebfa3a3152044afad29
Richard Hughes <richard@hughsie.com>
2026-02-02 09:45:17 +0000

    trivial: Split out fu-engine-helper-test.c

--- src/fu-engine-test.c
+++ src/fu-engine-test.c
@@ -13,8 +13,6 @@
 #include "fu-context-private.h"
 #include "fu-device-private.h"
 #include "fu-drm-device-private.h"
-#include "fu-efivars-private.h"
-#include "fu-engine-helper.h"
 #include "fu-engine-requirements.h"
 #include "fu-engine.h"
 #include "fu-history.h"
@@ -3550,28 +3548,6 @@ fu_engine_modify_bios_settings_func(void)
 }
 
 static void
-fu_engine_integrity_func(void)
-{
-	gboolean ret;
-	g_autoptr(FuContext) ctx =
-	    fu_context_new_full(FU_CONTEXT_FLAG_NO_QUIRKS | FU_CONTEXT_FLAG_DUMMY_EFIVARS);
-	g_autofree gchar *str = NULL;
-	g_autoptr(GError) error = NULL;
-	g_autoptr(GHashTable) integrity = NULL;
-
-	/* build a plausible EFI system */
-	ret = fu_efivars_set_secure_boot(fu_context_get_efivars(ctx), TRUE, &error);
-	g_assert_no_error(error);
-	g_assert_true(ret);
-
-	integrity = fu_engine_integrity_new(ctx, &error);
-	g_assert_no_error(error);
-	g_assert_nonnull(integrity);
-	str = fu_engine_integrity_to_string(integrity);
-	g_debug("%s", str);
-}
-
-static void
 fu_engine_report_metadata_func(void)
 {
 	gboolean ret;
@@ -3631,73 +3607,6 @@ fu_engine_report_metadata_func(void)
 	}
 }
 
-static void
-fu_engine_error_array_func(void)
-{
-	g_autoptr(GPtrArray) arr1 = g_ptr_array_new_with_free_func((GDestroyNotify)g_error_free);
-	g_autoptr(GPtrArray) arr2 = g_ptr_array_new_with_free_func((GDestroyNotify)g_error_free);
-	g_autoptr(GPtrArray) arr3 = g_ptr_array_new_with_free_func((GDestroyNotify)g_error_free);
-	g_autoptr(GPtrArray) arr4 = g_ptr_array_new_with_free_func((GDestroyNotify)g_error_free);
-	g_autoptr(GError) error1 = NULL;
-	g_autoptr(GError) error2 = NULL;
-	g_autoptr(GError) error3 = NULL;
-	g_autoptr(GError) error4 = NULL;
-
-	/* fallback */
-	error1 = fu_engine_error_array_get_best(arr1);
-	g_assert_error(error1, FWUPD_ERROR, FWUPD_ERROR_NOT_FOUND);
-
-	/* get the most important single error */
-	g_ptr_array_add(arr2, g_error_new_literal(FWUPD_ERROR, FWUPD_ERROR_NOT_FOUND, ""));
-	error2 = fu_engine_error_array_get_best(arr2);
-	g_assert_error(error2, FWUPD_ERROR, FWUPD_ERROR_NOT_FOUND);
-
-	/* version same */
-	g_ptr_array_add(arr3, g_error_new_literal(FWUPD_ERROR, FWUPD_ERROR_VERSION_SAME, ""));
-	g_ptr_array_add(arr3, g_error_new_literal(FWUPD_ERROR, FWUPD_ERROR_VERSION_SAME, ""));
-	g_ptr_array_add(arr3, g_error_new_literal(FWUPD_ERROR, FWUPD_ERROR_NOT_FOUND, ""));
-	error3 = fu_engine_error_array_get_best(arr3);
-	g_assert_error(error3, FWUPD_ERROR, FWUPD_ERROR_NOTHING_TO_DO);
-
-	/* already have newer versions */
-	g_ptr_array_add(arr4, g_error_new_literal(FWUPD_ERROR, FWUPD_ERROR_VERSION_NEWER, ""));
-	g_ptr_array_add(arr4, g_error_new_literal(FWUPD_ERROR, FWUPD_ERROR_VERSION_NEWER, ""));
-	error4 = fu_engine_error_array_get_best(arr4);
-	g_assert_error(error4, FWUPD_ERROR, FWUPD_ERROR_NOTHING_TO_DO);
-}
-
-static void
-fu_engine_machine_hash_func(void)
-{
-	gsize sz = 0;
-	g_autofree gchar *buf = NULL;
-	g_autofree gchar *mhash1 = NULL;
-	g_autofree gchar *mhash2 = NULL;
-	g_autoptr(GError) error = NULL;
-
-	if (!g_file_test("/etc/machine-id", G_FILE_TEST_EXISTS)) {
-		g_test_skip("Missing /etc/machine-id");
-		return;
-	}
-	if (!g_file_get_contents("/etc/machine-id", &buf, &sz, &error)) {
-		g_test_skip("/etc/machine-id is unreadable");
-		return;
-	}
-
-	if (sz == 0) {
-		g_test_skip("Empty /etc/machine-id");
-		return;
-	}
-
-	mhash1 = fu_engine_build_machine_id("salt1", &error);
-	g_assert_no_error(error);
-	g_assert_cmpstr(mhash1, !=, NULL);
-	mhash2 = fu_engine_build_machine_id("salt2", &error);
-	g_assert_no_error(error);
-	g_assert_cmpstr(mhash2, !=, NULL);
-	g_assert_cmpstr(mhash2, !=, mhash1);
-}
-
 int
 main(int argc, char **argv)
 {
@@ -3720,10 +3629,7 @@ main(int argc, char **argv)
 	g_test_add_func("/fwupd/engine/history-success", fu_engine_history_func);
 	g_test_add_func("/fwupd/engine/history-verfmt", fu_engine_history_verfmt_func);
 	g_test_add_func("/fwupd/engine/history-error", fu_engine_history_error_func);
-	g_test_add_func("/fwupd/engine/machine-hash", fu_engine_machine_hash_func);
-	g_test_add_func("/fwupd/engine/error-array", fu_engine_error_array_func);
 	g_test_add_func("/fwupd/engine/report-metadata", fu_engine_report_metadata_func);
-	g_test_add_func("/fwupd/engine/integrity", fu_engine_integrity_func);
 	g_test_add_func("/fwupd/engine/require-hwid", fu_engine_require_hwid_func);
 	g_test_add_func("/fwupd/engine/requires-reboot", fu_engine_install_needs_reboot);
 	g_test_add_func("/fwupd/engine/history-inherit", fu_engine_history_inherit);
