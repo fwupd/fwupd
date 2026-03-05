2535bbfcd9ea03662e3ff17810d895f3706f7311
harris_tai <harris_tai@pixart.com>
2026-03-05 13:41:29 +0800

    feat:  support update 239 touchpad device.

--- plugins/pixart-tp/fu-pixart-tp-device.h
+++ plugins/pixart-tp/fu-pixart-tp-device.h
@@ -8,11 +8,61 @@
 #pragma once
 
 #include <fwupdplugin.h>
-
 #include "fu-pixart-tp-struct.h"
 
 #define FU_TYPE_PIXART_TP_DEVICE (fu_pixart_tp_device_get_type())
-G_DECLARE_FINAL_TYPE(FuPixartTpDevice, fu_pixart_tp_device, FU, PIXART_TP_DEVICE, FuHidrawDevice)
+
+G_DECLARE_DERIVABLE_TYPE(FuPixartTpDevice,
+			 fu_pixart_tp_device,
+			 FU,
+			 PIXART_TP_DEVICE,
+			 FuHidrawDevice)
+
+struct _FuPixartTpDeviceClass {
+	FuHidrawDeviceClass parent_class;
+};
+
+gboolean
+fu_pixart_tp_device_register_write(FuPixartTpDevice *self,
+				   FuPixartTpSystemBank bank,
+				   guint8 addr,
+				   guint8 val,
+				   GError **error) G_GNUC_NON_NULL(1);
+
+gboolean
+fu_pixart_tp_device_register_write_uint16(FuPixartTpDevice *self,
+					  FuPixartTpSystemBank bank,
+					  guint8 addr,
+					  guint16 val,
+					  GError **error) G_GNUC_NON_NULL(1);
+
+gboolean
+fu_pixart_tp_device_register_write_uint24(FuPixartTpDevice *self,
+					  FuPixartTpSystemBank bank,
+					  guint8 addr,
+					  guint32 val,
+					  GError **error) G_GNUC_NON_NULL(1);
+
+gboolean
+fu_pixart_tp_device_register_write_uint32(FuPixartTpDevice *self,
+					  FuPixartTpSystemBank bank,
+					  guint8 addr,
+					  guint32 val,
+					  GError **error) G_GNUC_NON_NULL(1);
+
+gboolean
+fu_pixart_tp_device_register_read(FuPixartTpDevice *self,
+				  FuPixartTpSystemBank bank,
+				  guint8 addr,
+				  guint8 *out_val,
+				  GError **error) G_GNUC_NON_NULL(1, 4);
+
+GByteArray *
+fu_pixart_tp_device_register_read_array(FuPixartTpDevice *self,
+					FuPixartTpSystemBank bank,
+					guint8 addr,
+					gsize len,
+					GError **error) G_GNUC_NON_NULL(1);
 
 gboolean
 fu_pixart_tp_device_register_user_write(FuPixartTpDevice *self,
@@ -20,3 +70,15 @@ fu_pixart_tp_device_register_user_write(FuPixartTpDevice *self,
 					guint8 addr,
 					guint8 val,
 					GError **error) G_GNUC_NON_NULL(1);
+
+gboolean
+fu_pixart_tp_device_register_burst_write(FuPixartTpDevice *self,
+					 const guint8 *buf,
+					 gsize bufsz,
+					 GError **error) G_GNUC_NON_NULL(1, 2);
+
+gboolean
+fu_pixart_tp_device_register_burst_read(FuPixartTpDevice *self,
+					guint8 *buf,
+					gsize bufsz,
+					GError **error) G_GNUC_NON_NULL(1, 2);
