c26e73dc71f9038e609bfb0e4fd7cb170adcc1b4
Peter Hutterer <peter.hutterer@who-t.net>
2026-03-18 13:43:02 +1000

    trivial: Ignore a missing dir when checking for existing fwupd binaries

--- contrib/build-venv.sh
+++ contrib/build-venv.sh
@@ -14,13 +14,15 @@ ninja -C ${BUILD} install
 # check whether we have an existing fwupd EFI binary in the host system to use
 EFI_PREFIX=$(pkg-config fwupd-efi --variable=prefix 2>/dev/null || echo "/usr")
 EFI_DIR=libexec/fwupd/efi
-BINARIES=$(find "${EFI_PREFIX}/${EFI_DIR}" -name "*.efi*" -type f -print || true)
-if [ -n "${BINARIES}" ]; then
-    mkdir -p ${DIST}/${EFI_DIR}
-    for i in ${BINARIES}; do
-        if [ -f "${DIST}/${EFI_DIR}/$(basename $i)" ]; then
-            continue
-        fi
-        ln -s $i "${DIST}/${EFI_DIR}/$(basename $i)"
-    done
+if [ -d "${EFI_PREFIX}/${EFI_DIR}" ]; then
+    BINARIES=$(find "${EFI_PREFIX}/${EFI_DIR}" -name "*.efi*" -type f -print || true)
+    if [ -n "${BINARIES}" ]; then
+        mkdir -p ${DIST}/${EFI_DIR}
+        for i in ${BINARIES}; do
+            if [ -f "${DIST}/${EFI_DIR}/$(basename $i)" ]; then
+                continue
+            fi
+            ln -s $i "${DIST}/${EFI_DIR}/$(basename $i)"
+        done
+    fi
 fi
