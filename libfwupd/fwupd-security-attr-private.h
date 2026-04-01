fwupd/fwupd

commit c8e9d8d3ca072ea10f3370e0f46f247a9db8c3f0

Add a new fwupd security check for HP Sure Start

libfwupd/fwupd-security-attr-private.h
@@ -402,6 +402,14 @@ G_BEGIN_DECLS
  * Since: 2.0.8
  **/
 #define FWUPD_SECURITY_ATTR_ID_UEFI_DB "org.fwupd.hsi.Uefi.Db"
+/**
+ * FWUPD_SECURITY_ATTR_HP_SURESTART:
+ *
+ * Host Security ID attribute for HP Surestart
+ *
+ * Since: 2.1.1
+ **/
+#define FWUPD_SECURITY_ATTR_ID_HP_SURESTART "org.fwupd.hsi.Hp.Surestart"
 
 FwupdSecurityAttr *
 fwupd_security_attr_copy(FwupdSecurityAttr *self) G_GNUC_NON_NULL(1);

docs/hsi-tests.d/org.fwupd.hsi.Hp.Surestart
@@ -1,0 +1,25 @@
+{
+  "id": "org.fwupd.hsi.Hp.Surestart",
+  "name": "HP SureStart",
+  "description": [
+    "HP SureStart is an advanced self-healing BIOS that automatically recovers the BIOS code if it becomes corrupted or is compromised by malicious software.",
+    "When enabled, SureStart monitors the BIOS for unauthorized changes and can automatically restore the BIOS to a known-good state.",
+    "This provides runtime intrusion prevention and detection for the firmware, protecting against both malware and physical attacks on the BIOS chip."
+  ],
+  "failure-impact": [
+    "Without HP SureStart enabled, the system firmware is vulnerable to persistent malware that could survive operating system reinstallation.",
+    "An attacker who compromises the BIOS could maintain persistent access to the system and potentially bypass security features like Secure Boot.",
+    "Physical attacks on the BIOS chip could result in permanent system compromise without detection."
+  ],
+  "failure-results": {
+    "not-enabled": "HP SureStart is disabled"
+  },
+  "success-results": {
+    "enabled": "HP SureStart is enabled"
+  },
+  "references": {
+    "https://h20195.www2.hp.com/v2/GetPDF.aspx/4aa6-4194enw.pdf": "HP SureStart White Paper"
+  },
+  "hsi-level": 2,
+  "fwupd-version": "2.1.1"
+}

libfwupdplugin/fu-bios-settings.c
@@ -610,6 +610,23 @@ fu_bios_settings_to_hash_kv(FuBiosSettings *self)
 }
 
 /**
+ * fu_bios_settings_is_supported:
+ * @self: a #FuBiosSettings
+ *
+ * Determines if any BIOS settings are supported on this system.
+ *
+ * Returns: %TRUE if any BIOS settings are available, %FALSE otherwise
+ *
+ * Since: 2.1.1
+ **/
+gboolean
+fu_bios_settings_is_supported(FuBiosSettings *self)
+{
+	g_return_val_if_fail(FU_IS_BIOS_SETTINGS(self), FALSE);
+	return self->attrs->len > 0;
+}
+
+/**
  * fu_bios_settings_new:
  *
  * Returns: #FuBiosSettings

libfwupdplugin/fu-bios-settings.h
@@ -19,3 +19,5 @@ fu_bios_settings_get_pending_reboot(FuBiosSettings *self, gboolean *result, GErr
     G_GNUC_NON_NULL(1);
 FwupdBiosSetting *
 fu_bios_settings_get_attr(FuBiosSettings *self, const gchar *val) G_GNUC_NON_NULL(1, 2);
+gboolean
+fu_bios_settings_is_supported(FuBiosSettings *self) G_GNUC_NON_NULL(1);

libfwupdplugin/fu-security-attrs.c
@@ -416,6 +416,7 @@ const struct {
     {FWUPD_SECURITY_ATTR_ID_AMD_SMM_LOCKED, FWUPD_SECURITY_ATTR_LEVEL_CRITICAL},
     {FWUPD_SECURITY_ATTR_ID_UEFI_MEMORY_PROTECTION, FWUPD_SECURITY_ATTR_LEVEL_IMPORTANT},
     {FWUPD_SECURITY_ATTR_ID_UEFI_DB, FWUPD_SECURITY_ATTR_LEVEL_IMPORTANT},
+    {FWUPD_SECURITY_ATTR_ID_HP_SURESTART, FWUPD_SECURITY_ATTR_LEVEL_IMPORTANT},
 };
 
 static void
@@ -490,6 +491,7 @@ const struct {
     {FWUPD_SECURITY_ATTR_ID_UEFI_MEMORY_PROTECTION, "2.0.7"},
     {FWUPD_SECURITY_ATTR_ID_UEFI_PK, "1.5.0"},
     {FWUPD_SECURITY_ATTR_ID_UEFI_SECUREBOOT, "1.5.0"},
+    {FWUPD_SECURITY_ATTR_ID_HP_SURESTART, "2.1.1"},
 };
 
 static void

plugins/hp-bioscfg/fu-hp-bioscfg-plugin.c
@@ -1,0 +1,85 @@
+/*
+ * Copyright 2026 Mario Limonciello <mario.limonciello@amd.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#include "config.h"
+
+#include "fu-hp-bioscfg-plugin.h"
+
+struct _FuHpBiosCfgPlugin {
+	FuPlugin parent_instance;
+};
+
+G_DEFINE_TYPE(FuHpBiosCfgPlugin, fu_hp_bioscfg_plugin, FU_TYPE_PLUGIN)
+
+#define BIOS_SETTING_SURESTART                                                                     \
+	"com.hp-bioscfg.Enhanced HP Firmware Runtime Intrusion Prevention and Detection"
+
+static gboolean
+fu_hp_bioscfg_plugin_startup(FuPlugin *plugin, FuProgress *progress, GError **error)
+{
+	const gchar *hwid =
+	    fu_context_get_hwid_value(fu_plugin_get_context(plugin), FU_HWIDS_KEY_MANUFACTURER);
+	if (g_strcmp0(hwid, "HP") != 0) {
+		g_set_error_literal(error,
+				    FWUPD_ERROR,
+				    FWUPD_ERROR_NOT_SUPPORTED,
+				    "unsupported manufacturer");
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+static void
+fu_hp_bioscfg_plugin_add_security_attrs(FuPlugin *plugin, FuSecurityAttrs *attrs)
+{
+	FwupdBiosSetting *bios_attr;
+	FuContext *ctx = fu_plugin_get_context(plugin);
+	g_autoptr(FwupdSecurityAttr) attr = NULL;
+	g_autoptr(FuBiosSettings) bios_settings = NULL;
+
+	attr = fu_plugin_security_attr_new(plugin, FWUPD_SECURITY_ATTR_ID_HP_SURESTART);
+	fu_security_attr_add_bios_target_value(attr, BIOS_SETTING_SURESTART, "Enable");
+	fwupd_security_attr_set_result_success(attr, FWUPD_SECURITY_ATTR_RESULT_ENABLED);
+	fu_security_attrs_append(attrs, attr);
+
+	/* no settings supported, hp-bioscfg is missing, we don't know if we have SureStart */
+	bios_settings = fu_context_get_bios_settings(ctx);
+	if (!fu_bios_settings_is_supported(bios_settings)) {
+		fwupd_security_attr_add_flag(attr, FWUPD_SECURITY_ATTR_FLAG_MISSING_DATA);
+		fwupd_security_attr_set_result(attr, FWUPD_SECURITY_ATTR_RESULT_UNKNOWN);
+		return;
+	}
+
+	/* hp-bioscfg found but didn't find SureStart, should be a failure */
+	bios_attr = fu_context_get_bios_setting(ctx, BIOS_SETTING_SURESTART);
+	if (bios_attr == NULL) {
+		fwupd_security_attr_set_result(attr, FWUPD_SECURITY_ATTR_RESULT_NOT_ENABLED);
+		return;
+	}
+
+	/* attribute found; show it's status */
+	if (g_strcmp0(fwupd_bios_setting_get_current_value(bios_attr), "Disable") == 0) {
+		fwupd_security_attr_add_flag(attr, FWUPD_SECURITY_ATTR_FLAG_ACTION_CONFIG_FW);
+		fwupd_security_attr_set_result(attr, FWUPD_SECURITY_ATTR_RESULT_NOT_ENABLED);
+		return;
+	}
+
+	fwupd_security_attr_add_flag(attr, FWUPD_SECURITY_ATTR_FLAG_SUCCESS);
+}
+
+static void
+fu_hp_bioscfg_plugin_init(FuHpBiosCfgPlugin *self)
+{
+}
+
+static void
+fu_hp_bioscfg_plugin_class_init(FuHpBiosCfgPluginClass *klass)
+{
+	FuPluginClass *plugin_class = FU_PLUGIN_CLASS(klass);
+	plugin_class->startup = fu_hp_bioscfg_plugin_startup;
+	plugin_class->add_security_attrs = fu_hp_bioscfg_plugin_add_security_attrs;
+}

plugins/hp-bioscfg/fu-hp-bioscfg-plugin.h
@@ -1,0 +1,11 @@
+/*
+ * Copyright 2026 Mario Limonciello <mario.limonciello@amd.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#pragma once
+
+#include <fwupdplugin.h>
+
+G_DECLARE_FINAL_TYPE(FuHpBiosCfgPlugin, fu_hp_bioscfg_plugin, FU, HP_BIOSCFG_PLUGIN, FuPlugin)

plugins/hp-bioscfg/fu-self-test.c
@@ -1,0 +1,197 @@
+/*
+ * Copyright 2026 Mario Limonciello <mario.limonciello@amd.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#include "config.h"
+
+#include <glib/gstdio.h>
+
+#include "fu-context-private.h"
+#include "fu-hp-bioscfg-plugin.h"
+#include "fu-plugin-private.h"
+#include "fu-security-attrs-private.h"
+
+typedef struct {
+	FuContext *ctx;
+	FuPlugin *plugin_hp_bioscfg;
+} FuTest;
+
+static void
+fu_test_self_init(FuTest *self)
+{
+	gboolean ret;
+	g_autoptr(FuContext) ctx = fu_context_new();
+	g_autoptr(FuProgress) progress = fu_progress_new(G_STRLOC);
+	g_autoptr(GError) error = NULL;
+
+	ret = fu_context_load_quirks(ctx,
+				     FU_QUIRKS_LOAD_FLAG_NO_CACHE | FU_QUIRKS_LOAD_FLAG_NO_VERIFY,
+				     &error);
+	g_assert_no_error(error);
+	g_assert_true(ret);
+	ret = fu_context_load_hwinfo(ctx, progress, FU_CONTEXT_HWID_FLAG_LOAD_CONFIG, &error);
+	g_assert_no_error(error);
+	g_assert_true(ret);
+	ret = fu_context_reload_bios_settings(ctx, &error);
+	g_assert_no_error(error);
+	g_assert_true(ret);
+
+	self->plugin_hp_bioscfg = fu_plugin_new_from_gtype(fu_hp_bioscfg_plugin_get_type(), ctx);
+	ret = fu_plugin_runner_startup(self->plugin_hp_bioscfg, progress, &error);
+	g_assert_no_error(error);
+	g_assert_true(ret);
+	self->ctx = fu_plugin_get_context(self->plugin_hp_bioscfg);
+}
+
+static void
+fu_plugin_hp_bioscfg_surestart_enabled(gconstpointer user_data)
+{
+	FuTest *self = (FuTest *)user_data;
+	gboolean ret;
+	g_autoptr(FuSecurityAttrs) attrs = fu_security_attrs_new();
+	g_autoptr(GError) error = NULL;
+	g_autoptr(FwupdSecurityAttr) attr = NULL;
+	g_autofree gchar *test_dir = g_test_build_filename(G_TEST_DIST,
+							   "tests",
+							   "firmware-attributes",
+							   "surestart-enabled",
+							   NULL);
+
+	(void)g_setenv("FWUPD_SYSFSFWATTRIBDIR", test_dir, TRUE);
+
+	ret = fu_context_reload_bios_settings(self->ctx, &error);
+	g_assert_no_error(error);
+	g_assert_true(ret);
+
+	fu_plugin_runner_add_security_attrs(self->plugin_hp_bioscfg, attrs);
+
+	/* check that SureStart attribute is present and has success status */
+	attr =
+	    fu_security_attrs_get_by_appstream_id(attrs, FWUPD_SECURITY_ATTR_ID_HP_SURESTART, NULL);
+	g_assert_nonnull(attr);
+	g_assert_cmpint(fwupd_security_attr_get_result(attr),
+			==,
+			FWUPD_SECURITY_ATTR_RESULT_ENABLED);
+	g_assert_true(fwupd_security_attr_has_flag(attr, FWUPD_SECURITY_ATTR_FLAG_SUCCESS));
+}
+
+static void
+fu_plugin_hp_bioscfg_surestart_disabled(gconstpointer user_data)
+{
+	FuTest *self = (FuTest *)user_data;
+	gboolean ret;
+	g_autoptr(FuSecurityAttrs) attrs = fu_security_attrs_new();
+	g_autoptr(GError) error = NULL;
+	g_autoptr(FwupdSecurityAttr) attr = NULL;
+	g_autofree gchar *test_dir = g_test_build_filename(G_TEST_DIST,
+							   "tests",
+							   "firmware-attributes",
+							   "surestart-disabled",
+							   NULL);
+
+	(void)g_setenv("FWUPD_SYSFSFWATTRIBDIR", test_dir, TRUE);
+
+	ret = fu_context_reload_bios_settings(self->ctx, &error);
+	g_assert_no_error(error);
+	g_assert_true(ret);
+
+	fu_plugin_runner_add_security_attrs(self->plugin_hp_bioscfg, attrs);
+
+	/* check that SureStart attribute is present and has failure status */
+	attr =
+	    fu_security_attrs_get_by_appstream_id(attrs, FWUPD_SECURITY_ATTR_ID_HP_SURESTART, NULL);
+	g_assert_nonnull(attr);
+	g_assert_cmpint(fwupd_security_attr_get_result(attr),
+			==,
+			FWUPD_SECURITY_ATTR_RESULT_NOT_ENABLED);
+	g_assert_true(
+	    fwupd_security_attr_has_flag(attr, FWUPD_SECURITY_ATTR_FLAG_ACTION_CONFIG_FW));
+	g_assert_false(fwupd_security_attr_has_flag(attr, FWUPD_SECURITY_ATTR_FLAG_SUCCESS));
+}
+
+static void
+fu_plugin_hp_bioscfg_surestart_not_available(gconstpointer user_data)
+{
+	FuTest *self = (FuTest *)user_data;
+	gboolean ret;
+	g_autoptr(FuSecurityAttrs) attrs = fu_security_attrs_new();
+	g_autoptr(GError) error = NULL;
+	g_autoptr(FwupdSecurityAttr) attr = NULL;
+	g_autofree gchar *test_dir = g_test_build_filename(G_TEST_DIST,
+							   "tests",
+							   "firmware-attributes",
+							   "surestart-not-available",
+							   NULL);
+
+	(void)g_setenv("FWUPD_SYSFSFWATTRIBDIR", test_dir, TRUE);
+
+	ret = fu_context_reload_bios_settings(self->ctx, &error);
+	g_assert_no_error(error);
+	g_assert_true(ret);
+
+	fu_plugin_runner_add_security_attrs(self->plugin_hp_bioscfg, attrs);
+
+	attr =
+	    fu_security_attrs_get_by_appstream_id(attrs, FWUPD_SECURITY_ATTR_ID_HP_SURESTART, NULL);
+	g_assert_nonnull(attr);
+	g_assert_cmpint(fwupd_security_attr_get_result(attr),
+			==,
+			FWUPD_SECURITY_ATTR_RESULT_NOT_ENABLED);
+	g_assert_false(fwupd_security_attr_has_flag(attr, FWUPD_SECURITY_ATTR_FLAG_SUCCESS));
+}
+
+static void
+fu_test_self_free(FuTest *self)
+{
+	if (self->plugin_hp_bioscfg != NULL)
+		g_object_unref(self->plugin_hp_bioscfg);
+	g_free(self);
+}
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-function"
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(FuTest, fu_test_self_free)
+#pragma clang diagnostic pop
+
+int
+main(int argc, char **argv)
+{
+	g_autofree gchar *confdir = NULL;
+	g_autofree gchar *test_dir = NULL;
+	g_autoptr(FuTest) self = g_new0(FuTest, 1);
+
+	(void)g_setenv("G_TEST_SRCDIR", SRCDIR, FALSE);
+	g_test_init(&argc, &argv, NULL);
+
+	/* starting bioscfg dir to make startup pass */
+	test_dir = g_test_build_filename(G_TEST_DIST,
+					 "tests",
+					 "firmware-attributes",
+					 "surestart-not-available",
+					 NULL);
+	(void)g_setenv("FWUPD_SYSFSFWATTRIBDIR", test_dir, TRUE);
+
+	/* to load fwupd.conf */
+	confdir = g_test_build_filename(G_TEST_DIST, "tests", "etc", "fwupd", NULL);
+	(void)g_setenv("CONFIGURATION_DIRECTORY", confdir, TRUE);
+
+	/* only critical and error are fatal */
+	g_log_set_fatal_mask(NULL, G_LOG_LEVEL_ERROR | G_LOG_LEVEL_CRITICAL);
+
+	g_assert_cmpint(g_mkdir_with_parents("/tmp/fwupd-self-test/var/lib/fwupd", 0755), ==, 0);
+
+	/* tests go here */
+	fu_test_self_init(self);
+	g_test_add_data_func("/fwupd/plugin{hp-bioscfg:surestart-enabled}",
+			     self,
+			     fu_plugin_hp_bioscfg_surestart_enabled);
+	g_test_add_data_func("/fwupd/plugin{hp-bioscfg:surestart-disabled}",
+			     self,
+			     fu_plugin_hp_bioscfg_surestart_disabled);
+	g_test_add_data_func("/fwupd/plugin{hp-bioscfg:surestart-not-available}",
+			     self,
+			     fu_plugin_hp_bioscfg_surestart_not_available);
+	return g_test_run();
+}

commit 773c0acbdd58eaa092b63676f68459e1a6e30ff5

trivial: Fix gtk-doc comment for a private attr
Fixes: c8e9d8d3ca07 ("Add a new fwupd security check for HP Sure Start")

libfwupd/fwupd-security-attr-private.h
@@ -411,7 +411,7 @@ G_BEGIN_DECLS
  **/
 #define FWUPD_SECURITY_ATTR_ID_UEFI_DB "org.fwupd.hsi.Uefi.Db"
 /**
- * FWUPD_SECURITY_ATTR_HP_SURESTART:
+ * FWUPD_SECURITY_ATTR_ID_HP_SURESTART:
  *
  * Host Security ID attribute for HP Surestart
  *