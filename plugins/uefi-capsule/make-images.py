fwupd/fwupd

commit 303b4e2a4a9e34b4e196013fef17b4203ed8a1d0

uefi-capsule: Port to FuZipFirmware

plugins/uefi-capsule/make-images.py
@@ -12,7 +12,7 @@
 import os
 import sys
 import argparse
-import tarfile
+import zipfile
 import math
 import io
 import struct
@@ -88,7 +88,9 @@ def _cairo_surface_write_to_bmp(img: cairo.ImageSurface) -> bytes:
 
 def main(args) -> int:
     # open output archive
-    with tarfile.open(args.out, "w:xz", preset=8) as tar:
+    with zipfile.ZipFile(
+        args.out, mode="w", compression=zipfile.ZIP_DEFLATED, allowZip64=False
+    ) as myzip:
         for lang in languages(args.podir):
             # these are the 1.6:1 of some common(ish) screen widths
             if lang == "en":
@@ -204,12 +206,8 @@ def main(args) -> int:
 
                 # convert to BMP and add to archive
                 with io.BytesIO() as io_bmp:
-                    io_bmp.write(_cairo_surface_write_to_bmp(img))
                     filename = f"fwupd-{lang}-{width}-{height}.bmp"
-                    tarinfo = tarfile.TarInfo(filename)
-                    tarinfo.size = io_bmp.tell()
-                    io_bmp.seek(0)
-                    tar.addfile(tarinfo, fileobj=io_bmp)
+                    myzip.writestr(filename, data=_cairo_surface_write_to_bmp(img))
 
     # success
     return 0

contrib/fwupd.spec.in
@@ -439,7 +439,7 @@ systemctl --no-reload preset fwupd-refresh.timer &>/dev/null || :
 %endif
 %if 0%{?have_uefi}
 %files plugin-uefi-capsule-data
-%{_datadir}/fwupd/uefi-capsule-ux.tar.xz
+%{_datadir}/fwupd/uefi-capsule-ux.zip
 %endif
 
 %files devel

plugins/uefi-capsule/fu-uefi-capsule-plugin.c
@@ -318,16 +318,19 @@ fu_uefi_capsule_plugin_get_splash_data(guint width, guint height, GError **error
 	const gchar *const *langs = g_get_language_names();
 	g_autofree gchar *filename_archive = NULL;
 	g_autofree gchar *langs_str = NULL;
-	g_autoptr(FuArchive) archive = NULL;
+	g_autoptr(FuFirmware) archive = fu_zip_firmware_new();
 	g_autoptr(GInputStream) stream_archive = NULL;
 
 	/* load archive */
-	filename_archive = fu_path_build(FU_PATH_KIND_DATADIR_PKG, "uefi-capsule-ux.tar.xz", NULL);
+	filename_archive = fu_path_build(FU_PATH_KIND_DATADIR_PKG, "uefi-capsule-ux.zip", NULL);
 	stream_archive = fu_input_stream_from_path(filename_archive, error);
 	if (stream_archive == NULL)
 		return NULL;
-	archive = fu_archive_new_stream(stream_archive, FU_FIRMWARE_PARSE_FLAG_NONE, error);
-	if (archive == NULL)
+	if (!fu_firmware_parse_stream(archive,
+				      stream_archive,
+				      0x0,
+				      FU_FIRMWARE_PARSE_FLAG_NONE,
+				      error))
 		return NULL;
 
 	/* find the closest locale match, falling back to `en` and `C` */
@@ -337,7 +340,7 @@ fu_uefi_capsule_plugin_get_splash_data(guint width, guint height, GError **error
 		if (g_str_has_suffix(langs[i], ".UTF-8"))
 			continue;
 		fn = g_strdup_printf("fwupd-%s-%u-%u.bmp", langs[i], width, height);
-		blob_tmp = fu_archive_lookup_by_fn(archive, fn, NULL);
+		blob_tmp = fu_firmware_get_image_by_id_bytes(archive, fn, NULL);
 		if (blob_tmp != NULL) {
 			g_debug("using UX image %s", fn);
 			return g_bytes_ref(blob_tmp);

plugins/uefi-capsule/meson.build
@@ -88,7 +88,7 @@ if get_option('plugin_uefi_capsule_splash')
       files('make-images.py'),
       ux_capsule_pofiles,
     ],
-    output: 'uefi-capsule-ux.tar.xz',
+    output: 'uefi-capsule-ux.zip',
     command: [
       python3.full_path(),
       files('make-images.py'),

commit fb299f28f6d9148a817cb78572a17841a3ce509b

uefi-capsule: Drop unused BytesIO contextmanager
writestr() is given bytes directly from _cairo_surface_write_to_bmp().

Fixes: 303b4e2a ("uefi-capsule: Port to FuZipFirmware")

plugins/uefi-capsule/make-images.py
@@ -205,9 +205,8 @@ def main(args) -> int:
                 img.flush()
 
                 # convert to BMP and add to archive
-                with io.BytesIO() as io_bmp:
-                    filename = f"fwupd-{lang}-{width}-{height}.bmp"
-                    myzip.writestr(filename, data=_cairo_surface_write_to_bmp(img))
+                filename = f"fwupd-{lang}-{width}-{height}.bmp"
+                myzip.writestr(filename, data=_cairo_surface_write_to_bmp(img))
 
     # success
     return 0