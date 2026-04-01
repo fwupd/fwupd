4f743f7570b1fb57f3a25cc88e37be5e65bb09b0
Peter Hutterer <peter.hutterer@who-t.net>
2026-04-01 15:13:30 +1000

    rust: Add a minimal fwupd-ffi crate
    This crate builds a static library we can then link into the rest of
    fwupd.

---
+++ rust/fwupd-ffi/src/lib.rs
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2026 Red Hat
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+//! C-compatible FFI bindings for the fwupd Rust implementations.
+//!
+//! This crate provides `#[no_mangle] extern "C"` wrappers around the pure-Rust
+//! `fwupd` crate, producing a static library that can be linked into
+//! `libfwupdplugin.so` to replace the C implementations.
+
