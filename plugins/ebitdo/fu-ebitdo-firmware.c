b2343992dfa5bb16662efbd01070d7d3b6a22078
Richard Hughes <richard@hughsie.com>
2026-04-07 09:30:18 +0100

    trivial: Do not match FuEbitdoFirmware when autodetecting firmware

--- plugins/ebitdo/fu-ebitdo-firmware.c
+++ plugins/ebitdo/fu-ebitdo-firmware.c
@@ -94,6 +94,7 @@ fu_ebitdo_firmware_init(FuEbitdoFirmware *self)
 	fu_firmware_add_image_gtype(FU_FIRMWARE(self), FU_TYPE_FIRMWARE);
 	fu_firmware_add_flag(FU_FIRMWARE(self), FU_FIRMWARE_FLAG_HAS_STORED_SIZE);
 	fu_firmware_add_flag(FU_FIRMWARE(self), FU_FIRMWARE_FLAG_ALLOW_LINEAR);
+	fu_firmware_add_flag(FU_FIRMWARE(self), FU_FIRMWARE_FLAG_NO_AUTO_DETECTION);
 }
 
 static void
