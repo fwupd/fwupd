0a853e11eef9694635dfa2eb4867acfc2e743d33
Richard Hughes <richard@hughsie.com>
2026-04-23 12:21:37 +0100

    Allow setting context flags from HWID matches
    This allows us to set the ignore-efivars-free-space for EDK2-based VMs.

--- libfwupdplugin/fu-context.rs
+++ libfwupdplugin/fu-context.rs
@@ -1,7 +1,7 @@
 // Copyright 2025 Richard Hughes <richard@hughsie.com>
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
-#[derive(Bitfield)]
+#[derive(Bitfield, FromString(enum))]
 enum FuContextFlags {
     None                    = 0,
     SaveEvents              = 1 << 0, // so that they can be replayed to emulate devices
