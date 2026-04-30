2a271437577fef2bfc82f5909aa668d7f4ad2a94
Richard Hughes <richard@hughsie.com>
2026-04-30 10:24:08 +0100

    acpi-sbom: Add a new plugin to read system SBOM data

--- libfwupdplugin/fu-uswid.rs
+++ libfwupdplugin/fu-uswid.rs
@@ -1,6 +1,7 @@
 // Copyright 2023 Richard Hughes <richard@hughsie.com>
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
+#[derive(ToString, FromString)]
 #[repr(u8)]
 enum FuUswidHeaderFlags {
     None = 0b0,
