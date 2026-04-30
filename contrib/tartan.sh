452fb07fa7b07554fedccffd08a474d6a43a3685
Thomas Mühlbacher <thomas.muehlbacher@br-automation.com>
2026-04-30 11:57:51 +0200

    trivial: specify tartan plugin location
    Add logic to find plugin on Debian-style multiarch systems.

--- contrib/tartan.sh
+++ contrib/tartan.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+set -eu
+
+plugin=/usr/lib64/tartan/18.1/libtartan.so
+[ -f "$plugin" ] || plugin="$(systemd-path system-library-arch)/tartan/18.1/libtartan.so"
+[ -f "$plugin" ] || {
+    echo "failed to find tartan plugin" 1>&2
+    exit 1
+}
+
 /usr/bin/scan-build-18 \
-    -load-plugin /usr/lib64/tartan/18.1/libtartan.so \
+    -load-plugin "$plugin" \
     -disable-checker core.CallAndMessage \
     -disable-checker core.NullDereference \
     -disable-checker deadcode.DeadStores \
