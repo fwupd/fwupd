e5dce63913b3fce011d537f2bb2a857a8bf5e2d5
Richard Hughes <richard@hughsie.com>
2026-05-15 15:13:16 +0100

    himax-tp: Use generic printf format types

--- plugins/himax-tp/fu-himax-tp-hid-device.c
+++ plugins/himax-tp/fu-himax-tp-hid-device.c
@@ -861,7 +861,7 @@ fu_himax_tp_hid_device_setup(FuDevice *device, GError **error)
 		return FALSE;
 
 	/* version format : cid(minor).tp_cfg_ver (decimal) */
-	version_str = g_strdup_printf("%hhu.%hhu",
+	version_str = g_strdup_printf("%u.%u",
 				      fu_struct_himax_tp_hid_info_get_cid(self->st_info) & 0xFF,
 				      fu_struct_himax_tp_hid_info_get_tp_cfg_ver(self->st_info));
 	fu_device_set_version(device, version_str);
