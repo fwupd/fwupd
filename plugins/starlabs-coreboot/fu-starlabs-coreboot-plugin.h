e94be3c7b9adc51c78ae1837e3ff4e52623926b9
Richard Hughes <richard@hughsie.com>
2026-05-17 20:44:47 +0100

    starlabs-coreboot: Warn Star Labs coreboot users before flashrom removal
    Based on a patch by Sean Rhodes <sean@starlabs.systems>, many thanks.

---
+++ plugins/starlabs-coreboot/fu-starlabs-coreboot-plugin.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2026 Richard Hughes <richard@hughsie.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#pragma once
+
+#include <fwupdplugin.h>
+
+G_DECLARE_FINAL_TYPE(FuStarlabsCorebootPlugin,
+		     fu_starlabs_coreboot_plugin,
+		     FU,
+		     STARLABS_COREBOOT_PLUGIN,
+		     FuPlugin)
