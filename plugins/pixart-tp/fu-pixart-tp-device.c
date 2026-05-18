3c43c96e607340d520002c0b80c6da9f08468209
Richard Hughes <richard@hughsie.com>
2026-05-18 12:34:20 +0100

    pixart-tp: Show a short easy-to-read string as the touchpad name
    PIXA3854:00 is not something the user is going to recognize.

--- plugins/pixart-tp/fu-pixart-tp-device.c
+++ plugins/pixart-tp/fu-pixart-tp-device.c
@@ -1546,7 +1546,7 @@ fu_pixart_tp_device_init(FuPixartTpDevice *self)
 	fu_device_set_version_format(FU_DEVICE(self), FWUPD_VERSION_FORMAT_HEX);
 	fu_device_set_remove_delay(FU_DEVICE(self), FU_DEVICE_REMOVE_DELAY_RE_ENUMERATE);
 	fu_device_add_protocol(FU_DEVICE(self), "com.pixart.tp");
-	fu_device_set_summary(FU_DEVICE(self), "Touchpad");
+	fu_device_set_name(FU_DEVICE(self), "Touchpad");
 	fu_device_add_icon(FU_DEVICE(self), "input-touchpad");
 	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_UPDATABLE);
 	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_UNSIGNED_PAYLOAD);
