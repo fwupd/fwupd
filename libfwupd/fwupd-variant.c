fwupd/fwupd

commit eee953ed0d7e5fb911895702a56b775ac4b11a0e

trivial: Add a safe getter for g_variant_get_string()

libfwupd/fwupd-variant.c
@@ -82,3 +82,24 @@ fwupd_variant_get_uint64(GVariant *value)
 		return g_variant_get_uint64(value);
 	return 0;
 }
+
+/**
+ * fwupd_variant_get_string:
+ * @value: a #GVariant
+ *
+ * Gets a string value from a variant.
+ *
+ * Returns: string, or %NULL if not valid
+ *
+ * Since: 2.1.4
+ **/
+const gchar *
+fwupd_variant_get_string(GVariant *value)
+{
+	g_return_val_if_fail(value != NULL, NULL);
+	if (g_variant_is_of_type(value, G_VARIANT_TYPE_STRING) ||
+	    g_variant_is_of_type(value, G_VARIANT_TYPE_OBJECT_PATH) ||
+	    g_variant_is_of_type(value, G_VARIANT_TYPE_SIGNATURE))
+		return g_variant_get_string(value, NULL); /* nocheck:blocked */
+	return NULL;
+}

contrib/ci/check-source.py
@@ -839,6 +839,7 @@ class Checker:
             "gnutls_malloc": "Use gnutls_calloc() instead",
             "g_variant_get_uint64": "Use fwupd_variant_get_uint64() instead",
             "g_variant_get_uint32": "Use fwupd_variant_get_uint32() instead",
+            "g_variant_get_string": "Use fwupd_variant_get_string() instead",
             "g_random_int_range": "Use a predicatable token instead",
             "g_assert": "Use g_set_error() or g_return_val_if_fail() instead",
             "HIDIOCSFEATURE": "Use fu_hidraw_device_set_feature() instead",

libfwupd/fwupd-bios-setting.c
@@ -900,27 +900,27 @@ fwupd_bios_setting_from_key_value(FwupdBiosSetting *self, const gchar *key, GVar
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_BIOS_SETTING_ID) == 0) {
-		fwupd_bios_setting_set_id(self, g_variant_get_string(value, NULL));
+		fwupd_bios_setting_set_id(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_NAME) == 0) {
-		fwupd_bios_setting_set_name(self, g_variant_get_string(value, NULL));
+		fwupd_bios_setting_set_name(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_FILENAME) == 0) {
-		fwupd_bios_setting_set_path(self, g_variant_get_string(value, NULL));
+		fwupd_bios_setting_set_path(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_BIOS_SETTING_CURRENT_VALUE) == 0) {
-		fwupd_bios_setting_set_current_value(self, g_variant_get_string(value, NULL));
+		fwupd_bios_setting_set_current_value(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_BIOS_SETTING_FILENAME) == 0) {
-		fwupd_bios_setting_set_filename(self, g_variant_get_string(value, NULL));
+		fwupd_bios_setting_set_filename(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_DESCRIPTION) == 0) {
-		fwupd_bios_setting_set_description(self, g_variant_get_string(value, NULL));
+		fwupd_bios_setting_set_description(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_BIOS_SETTING_POSSIBLE_VALUES) == 0) {

libfwupd/fwupd-client.c
@@ -623,36 +623,36 @@ fwupd_client_properties_changed_cb(GDBusProxy *proxy,
 		g_autoptr(GVariant) val = NULL;
 		val = g_dbus_proxy_get_cached_property(proxy, "DaemonVersion");
 		if (val != NULL)
-			fwupd_client_set_daemon_version(self, g_variant_get_string(val, NULL));
+			fwupd_client_set_daemon_version(self, fwupd_variant_get_string(val));
 	}
 	if (g_variant_dict_contains(dict, "HostBkc")) {
 		g_autoptr(GVariant) val = g_dbus_proxy_get_cached_property(proxy, "HostBkc");
 		if (val != NULL)
-			fwupd_client_set_host_bkc(self, g_variant_get_string(val, NULL));
+			fwupd_client_set_host_bkc(self, fwupd_variant_get_string(val));
 	}
 	if (g_variant_dict_contains(dict, "HostVendor")) {
 		g_autoptr(GVariant) val = NULL;
 		val = g_dbus_proxy_get_cached_property(proxy, "HostVendor");
 		if (val != NULL)
-			fwupd_client_set_host_vendor(self, g_variant_get_string(val, NULL));
+			fwupd_client_set_host_vendor(self, fwupd_variant_get_string(val));
 	}
 	if (g_variant_dict_contains(dict, "HostProduct")) {
 		g_autoptr(GVariant) val = NULL;
 		val = g_dbus_proxy_get_cached_property(proxy, "HostProduct");
 		if (val != NULL)
-			fwupd_client_set_host_product(self, g_variant_get_string(val, NULL));
+			fwupd_client_set_host_product(self, fwupd_variant_get_string(val));
 	}
 	if (g_variant_dict_contains(dict, "HostMachineId")) {
 		g_autoptr(GVariant) val = NULL;
 		val = g_dbus_proxy_get_cached_property(proxy, "HostMachineId");
 		if (val != NULL)
-			fwupd_client_set_host_machine_id(self, g_variant_get_string(val, NULL));
+			fwupd_client_set_host_machine_id(self, fwupd_variant_get_string(val));
 	}
 	if (g_variant_dict_contains(dict, "HostSecurityId")) {
 		g_autoptr(GVariant) val = NULL;
 		val = g_dbus_proxy_get_cached_property(proxy, "HostSecurityId");
 		if (val != NULL)
-			fwupd_client_set_host_security_id(self, g_variant_get_string(val, NULL));
+			fwupd_client_set_host_security_id(self, fwupd_variant_get_string(val));
 	}
 	if (g_variant_dict_contains(dict, "OnlyTrusted")) {
 		g_autoptr(GVariant) val = NULL;
@@ -1018,7 +1018,7 @@ fwupd_client_connect_get_proxy_cb(GObject *source, GAsyncResult *res, gpointer u
 			 self);
 	val = g_dbus_proxy_get_cached_property(priv->proxy, "DaemonVersion");
 	if (val != NULL)
-		fwupd_client_set_daemon_version(self, g_variant_get_string(val, NULL));
+		fwupd_client_set_daemon_version(self, fwupd_variant_get_string(val));
 	val2 = g_dbus_proxy_get_cached_property(priv->proxy, "Tainted");
 	if (val2 != NULL)
 		priv->tainted = g_variant_get_boolean(val2);
@@ -1030,19 +1030,19 @@ fwupd_client_connect_get_proxy_cb(GObject *source, GAsyncResult *res, gpointer u
 		priv->interactive = g_variant_get_boolean(val4);
 	val5 = g_dbus_proxy_get_cached_property(priv->proxy, "HostProduct");
 	if (val5 != NULL)
-		fwupd_client_set_host_product(self, g_variant_get_string(val5, NULL));
+		fwupd_client_set_host_product(self, fwupd_variant_get_string(val5));
 	val10 = g_dbus_proxy_get_cached_property(priv->proxy, "HostVendor");
 	if (val10 != NULL)
-		fwupd_client_set_host_vendor(self, g_variant_get_string(val10, NULL));
+		fwupd_client_set_host_vendor(self, fwupd_variant_get_string(val10));
 	val6 = g_dbus_proxy_get_cached_property(priv->proxy, "HostMachineId");
 	if (val6 != NULL)
-		fwupd_client_set_host_machine_id(self, g_variant_get_string(val6, NULL));
+		fwupd_client_set_host_machine_id(self, fwupd_variant_get_string(val6));
 	val7 = g_dbus_proxy_get_cached_property(priv->proxy, "HostSecurityId");
 	if (val7 != NULL)
-		fwupd_client_set_host_security_id(self, g_variant_get_string(val7, NULL));
+		fwupd_client_set_host_security_id(self, fwupd_variant_get_string(val7));
 	val8 = g_dbus_proxy_get_cached_property(priv->proxy, "HostBkc");
 	if (val8 != NULL)
-		fwupd_client_set_host_bkc(self, g_variant_get_string(val8, NULL));
+		fwupd_client_set_host_bkc(self, fwupd_variant_get_string(val8));
 	val9 = g_dbus_proxy_get_cached_property(priv->proxy, "OnlyTrusted");
 	if (val9 != NULL)
 		priv->only_trusted = g_variant_get_boolean(val9);

libfwupd/fwupd-device.c
@@ -2517,15 +2517,15 @@ fwupd_device_from_key_value(FwupdDevice *self, const gchar *key, GVariant *value
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_DEVICE_ID) == 0) {
-		fwupd_device_set_id(self, g_variant_get_string(value, NULL));
+		fwupd_device_set_id(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_PARENT_DEVICE_ID) == 0) {
-		fwupd_device_set_parent_id(self, g_variant_get_string(value, NULL));
+		fwupd_device_set_parent_id(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_COMPOSITE_ID) == 0) {
-		fwupd_device_set_composite_id(self, g_variant_get_string(value, NULL));
+		fwupd_device_set_composite_id(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_FLAGS) == 0) {
@@ -2571,54 +2571,58 @@ fwupd_device_from_key_value(FwupdDevice *self, const gchar *key, GVariant *value
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_NAME) == 0) {
-		fwupd_device_set_name(self, g_variant_get_string(value, NULL));
+		fwupd_device_set_name(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_VENDOR) == 0) {
-		fwupd_device_set_vendor(self, g_variant_get_string(value, NULL));
+		fwupd_device_set_vendor(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_VENDOR_ID) == 0) {
-		g_auto(GStrv) vendor_ids = NULL;
-		vendor_ids = g_strsplit(g_variant_get_string(value, NULL), "|", -1);
-		for (guint i = 0; vendor_ids[i] != NULL; i++)
-			fwupd_device_add_vendor_id(self, vendor_ids[i]);
+		const gchar *str = fwupd_variant_get_string(value);
+		if (str != NULL) {
+			g_auto(GStrv) strv = g_strsplit(str, "|", -1);
+			for (guint i = 0; strv[i] != NULL; i++)
+				fwupd_device_add_vendor_id(self, strv[i]);
+		}
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_SERIAL) == 0) {
-		fwupd_device_set_serial(self, g_variant_get_string(value, NULL));
+		fwupd_device_set_serial(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_SUMMARY) == 0) {
-		fwupd_device_set_summary(self, g_variant_get_string(value, NULL));
+		fwupd_device_set_summary(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_DETAILS_URL) == 0) {
-		fwupd_device_set_details_url(self, g_variant_get_string(value, NULL));
+		fwupd_device_set_details_url(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_BRANCH) == 0) {
-		fwupd_device_set_branch(self, g_variant_get_string(value, NULL));
+		fwupd_device_set_branch(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_CHECKSUM) == 0) {
-		const gchar *checksums = g_variant_get_string(value, NULL);
-		if (checksums != NULL) {
-			g_auto(GStrv) split = g_strsplit(checksums, ",", -1);
+		const gchar *str = fwupd_variant_get_string(value);
+		if (str != NULL) {
+			g_auto(GStrv) split = g_strsplit(str, ",", -1);
 			for (guint i = 0; split[i] != NULL; i++)
 				fwupd_device_add_checksum(self, split[i]);
 		}
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_PLUGIN) == 0) {
-		fwupd_device_set_plugin(self, g_variant_get_string(value, NULL));
+		fwupd_device_set_plugin(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_PROTOCOL) == 0) {
-		g_auto(GStrv) protocols = NULL;
-		protocols = g_strsplit(g_variant_get_string(value, NULL), "|", -1);
-		for (guint i = 0; protocols[i] != NULL; i++)
-			fwupd_device_add_protocol(self, protocols[i]);
+		const gchar *str = fwupd_variant_get_string(value);
+		if (str != NULL) {
+			g_auto(GStrv) strv = g_strsplit(str, "|", -1);
+			for (guint i = 0; strv[i] != NULL; i++)
+				fwupd_device_add_protocol(self, strv[i]);
+		}
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_ISSUES) == 0) {
@@ -2628,19 +2632,19 @@ fwupd_device_from_key_value(FwupdDevice *self, const gchar *key, GVariant *value
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_VERSION) == 0) {
-		fwupd_device_set_version(self, g_variant_get_string(value, NULL));
+		fwupd_device_set_version(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_VERSION_LOWEST) == 0) {
-		fwupd_device_set_version_lowest(self, g_variant_get_string(value, NULL));
+		fwupd_device_set_version_lowest(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_VERSION_HIGHEST) == 0) {
-		fwupd_device_set_version_highest(self, g_variant_get_string(value, NULL));
+		fwupd_device_set_version_highest(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_VERSION_BOOTLOADER) == 0) {
-		fwupd_device_set_version_bootloader(self, g_variant_get_string(value, NULL));
+		fwupd_device_set_version_bootloader(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_FLASHES_LEFT) == 0) {
@@ -2660,7 +2664,7 @@ fwupd_device_from_key_value(FwupdDevice *self, const gchar *key, GVariant *value
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_UPDATE_ERROR) == 0) {
-		fwupd_device_set_update_error(self, g_variant_get_string(value, NULL));
+		fwupd_device_set_update_error(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_UPDATE_STATE) == 0) {

libfwupd/fwupd-plugin.c
@@ -212,7 +212,7 @@ static void
 fwupd_plugin_from_key_value(FwupdPlugin *self, const gchar *key, GVariant *value)
 {
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_NAME) == 0) {
-		fwupd_plugin_set_name(self, g_variant_get_string(value, NULL));
+		fwupd_plugin_set_name(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_FLAGS) == 0) {

libfwupd/fwupd-release.c
@@ -1880,43 +1880,43 @@ fwupd_release_from_key_value(FwupdRelease *self, const gchar *key, GVariant *val
 {
 	FwupdReleasePrivate *priv = GET_PRIVATE(self);
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_REMOTE_ID) == 0) {
-		fwupd_release_set_remote_id(self, g_variant_get_string(value, NULL));
+		fwupd_release_set_remote_id(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_APPSTREAM_ID) == 0) {
-		fwupd_release_set_appstream_id(self, g_variant_get_string(value, NULL));
+		fwupd_release_set_appstream_id(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_RELEASE_ID) == 0) {
-		fwupd_release_set_id(self, g_variant_get_string(value, NULL));
+		fwupd_release_set_id(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_DETACH_CAPTION) == 0) {
-		fwupd_release_set_detach_caption(self, g_variant_get_string(value, NULL));
+		fwupd_release_set_detach_caption(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_DETACH_IMAGE) == 0) {
-		fwupd_release_set_detach_image(self, g_variant_get_string(value, NULL));
+		fwupd_release_set_detach_image(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_FILENAME) == 0) {
-		fwupd_release_set_filename(self, g_variant_get_string(value, NULL));
+		fwupd_release_set_filename(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_PROTOCOL) == 0) {
-		fwupd_release_set_protocol(self, g_variant_get_string(value, NULL));
+		fwupd_release_set_protocol(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_LICENSE) == 0) {
-		fwupd_release_set_license(self, g_variant_get_string(value, NULL));
+		fwupd_release_set_license(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_NAME) == 0) {
-		fwupd_release_set_name(self, g_variant_get_string(value, NULL));
+		fwupd_release_set_name(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_NAME_VARIANT_SUFFIX) == 0) {
-		fwupd_release_set_name_variant_suffix(self, g_variant_get_string(value, NULL));
+		fwupd_release_set_name_variant_suffix(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_SIZE) == 0) {
@@ -1928,15 +1928,15 @@ fwupd_release_from_key_value(FwupdRelease *self, const gchar *key, GVariant *val
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_SUMMARY) == 0) {
-		fwupd_release_set_summary(self, g_variant_get_string(value, NULL));
+		fwupd_release_set_summary(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_BRANCH) == 0) {
-		fwupd_release_set_branch(self, g_variant_get_string(value, NULL));
+		fwupd_release_set_branch(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_DESCRIPTION) == 0) {
-		fwupd_release_set_description(self, g_variant_get_string(value, NULL));
+		fwupd_release_set_description(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_CATEGORIES) == 0) {
@@ -1952,10 +1952,12 @@ fwupd_release_from_key_value(FwupdRelease *self, const gchar *key, GVariant *val
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_CHECKSUM) == 0) {
-		const gchar *checksums = g_variant_get_string(value, NULL);
-		g_auto(GStrv) split = g_strsplit(checksums, ",", -1);
-		for (guint i = 0; split[i] != NULL; i++)
-			fwupd_release_add_checksum(self, split[i]);
+		const gchar *str = fwupd_variant_get_string(value);
+		if (str != NULL) {
+			g_auto(GStrv) split = g_strsplit(str, ",", -1);
+			for (guint i = 0; split[i] != NULL; i++)
+				fwupd_release_add_checksum(self, split[i]);
+		}
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_LOCATIONS) == 0) {
@@ -1971,31 +1973,31 @@ fwupd_release_from_key_value(FwupdRelease *self, const gchar *key, GVariant *val
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_URI) == 0) {
-		fwupd_release_add_location(self, g_variant_get_string(value, NULL));
+		fwupd_release_add_location(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_HOMEPAGE) == 0) {
-		fwupd_release_set_homepage(self, g_variant_get_string(value, NULL));
+		fwupd_release_set_homepage(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_DETAILS_URL) == 0) {
-		fwupd_release_set_details_url(self, g_variant_get_string(value, NULL));
+		fwupd_release_set_details_url(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_SOURCE_URL) == 0) {
-		fwupd_release_set_source_url(self, g_variant_get_string(value, NULL));
+		fwupd_release_set_source_url(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_SBOM_URL) == 0) {
-		fwupd_release_set_sbom_url(self, g_variant_get_string(value, NULL));
+		fwupd_release_set_sbom_url(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_VERSION) == 0) {
-		fwupd_release_set_version(self, g_variant_get_string(value, NULL));
+		fwupd_release_set_version(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_VENDOR) == 0) {
-		fwupd_release_set_vendor(self, g_variant_get_string(value, NULL));
+		fwupd_release_set_vendor(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_TRUST_FLAGS) == 0) {
@@ -2011,11 +2013,11 @@ fwupd_release_from_key_value(FwupdRelease *self, const gchar *key, GVariant *val
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_UPDATE_MESSAGE) == 0) {
-		fwupd_release_set_update_message(self, g_variant_get_string(value, NULL));
+		fwupd_release_set_update_message(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_UPDATE_IMAGE) == 0) {
-		fwupd_release_set_update_image(self, g_variant_get_string(value, NULL));
+		fwupd_release_set_update_image(self, fwupd_variant_get_string(value));
 		return;
 	}
 	if (g_strcmp0(key, FWUPD_RESULT_KEY_METADATA) == 0) {

libfwupd/fwupd-remote.c
@@ -1723,7 +1723,7 @@ fwupd_remote_from_variant_iter(FwupdCodec *codec, GVariantIter *iter)
 	/* three passes, as we have to construct Id -> Url -> * */
 	while (g_variant_iter_loop(iter, "{&sv}", &key, &value)) {
 		if (g_strcmp0(key, FWUPD_RESULT_KEY_REMOTE_ID) == 0)
-			fwupd_remote_set_id(self, g_variant_get_string(value, NULL));
+			fwupd_remote_set_id(self, fwupd_variant_get_string(value));
 		if (g_strcmp0(key, "Type") == 0)
 			fwupd_remote_set_kind(self, fwupd_variant_get_uint32(value));
 		if (g_strcmp0(key, FWUPD_RESULT_KEY_FLAGS) == 0)
@@ -1731,27 +1731,27 @@ fwupd_remote_from_variant_iter(FwupdCodec *codec, GVariantIter *iter)
 	}
 	while (g_variant_iter_loop(iter2, "{&sv}", &key, &value)) {
 		if (g_strcmp0(key, FWUPD_RESULT_KEY_URI) == 0)
-			fwupd_remote_set_metadata_uri(self, g_variant_get_string(value, NULL));
+			fwupd_remote_set_metadata_uri(self, fwupd_variant_get_string(value));
 		if (g_strcmp0(key, "FilenameCache") == 0)
-			fwupd_remote_set_filename_cache(self, g_variant_get_string(value, NULL));
+			fwupd_remote_set_filename_cache(self, fwupd_variant_get_string(value));
 		if (g_strcmp0(key, "FilenameSource") == 0)
-			fwupd_remote_set_filename_source(self, g_variant_get_string(value, NULL));
+			fwupd_remote_set_filename_source(self, fwupd_variant_get_string(value));
 		if (g_strcmp0(key, "ReportUri") == 0)
-			fwupd_remote_set_report_uri(self, g_variant_get_string(value, NULL));
+			fwupd_remote_set_report_uri(self, fwupd_variant_get_string(value));
 	}
 	while (g_variant_iter_loop(iter3, "{&sv}", &key, &value)) {
 		if (g_strcmp0(key, "Username") == 0) {
-			fwupd_remote_set_username(self, g_variant_get_string(value, NULL));
+			fwupd_remote_set_username(self, fwupd_variant_get_string(value));
 		} else if (g_strcmp0(key, "Password") == 0) {
-			fwupd_remote_set_password(self, g_variant_get_string(value, NULL));
+			fwupd_remote_set_password(self, fwupd_variant_get_string(value));
 		} else if (g_strcmp0(key, "Title") == 0) {
-			fwupd_remote_set_title(self, g_variant_get_string(value, NULL));
+			fwupd_remote_set_title(self, fwupd_variant_get_string(value));
 		} else if (g_strcmp0(key, "PrivacyUri") == 0) {
-			fwupd_remote_set_privacy_uri(self, g_variant_get_string(value, NULL));
+			fwupd_remote_set_privacy_uri(self, fwupd_variant_get_string(value));
 		} else if (g_strcmp0(key, "Agreement") == 0) {
-			fwupd_remote_set_agreement(self, g_variant_get_string(value, NULL));
+			fwupd_remote_set_agreement(self, fwupd_variant_get_string(value));
 		} else if (g_strcmp0(key, FWUPD_RESULT_KEY_CHECKSUM) == 0) {
-			fwupd_remote_set_checksum_sig(self, g_variant_get_string(value, NULL));
+			fwupd_remote_set_checksum_sig(self, fwupd_variant_get_string(value));
 		} else if (g_strcmp0(key, "Priority") == 0) {
 			priv->priority = g_variant_get_int32(value);
 		} else if (g_strcmp0(key, "ModificationTime") == 0) {
@@ -1759,7 +1759,7 @@ fwupd_remote_from_variant_iter(FwupdCodec *codec, GVariantIter *iter)
 		} else if (g_strcmp0(key, "RefreshInterval") == 0) {
 			priv->refresh_interval = fwupd_variant_get_uint64(value);
 		} else if (g_strcmp0(key, "FirmwareBaseUri") == 0) {
-			fwupd_remote_set_firmware_base_uri(self, g_variant_get_string(value, NULL));
+			fwupd_remote_set_firmware_base_uri(self, fwupd_variant_get_string(value));
 		}
 	}
 }

commit 3a54de2e2df33ba24dfba9b0c9158cc32f09f97e

trivial: Fix gtk-doc for g_variant_get_string()
Fixes: eee953ed0d7e ("trivial: Add a safe getter for g_variant_get_string()")

libfwupd/fwupd-variant.c
@@ -174,7 +174,7 @@ fwupd_variant_get_strv(GVariant *value)
  *
  * Gets a double from a variant.
  *
- * Returns: boolean, or -1 if not valid
+ * Returns: double, or -1.0 if not valid
  *
  * Since: 2.1.4
  **/