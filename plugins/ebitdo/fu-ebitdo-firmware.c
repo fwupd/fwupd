070d31649d7a8a4221fd4ecbebb941be4dab4c54
Richard Hughes <richard@hughsie.com>
2026-05-29 14:13:59 +0100

    trivial: Set max firmware size for FuEbitdoFirmware

--- plugins/ebitdo/fu-ebitdo-firmware.c
+++ plugins/ebitdo/fu-ebitdo-firmware.c
@@ -99,6 +99,7 @@ fu_ebitdo_firmware_init(FuEbitdoFirmware *self)
 	fu_firmware_add_flag(FU_FIRMWARE(self), FU_FIRMWARE_FLAG_HAS_STORED_SIZE);
 	fu_firmware_add_flag(FU_FIRMWARE(self), FU_FIRMWARE_FLAG_ALLOW_LINEAR);
 	fu_firmware_add_flag(FU_FIRMWARE(self), FU_FIRMWARE_FLAG_NO_AUTO_DETECTION);
+	fu_firmware_set_size_max(FU_FIRMWARE(self), 16 * FU_MB);
 }
 
 static void
