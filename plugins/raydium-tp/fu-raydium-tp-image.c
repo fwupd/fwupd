3078c24e801894745ed65d295e1186eb4818eaf6
Richard Hughes <richard@hughsie.com>
2026-05-29 14:23:33 +0100

    trivial: Set max firmware size for FuRaydiumTpFirmware

--- plugins/raydium-tp/fu-raydium-tp-image.c
+++ plugins/raydium-tp/fu-raydium-tp-image.c
@@ -79,6 +79,7 @@ fu_raydium_tp_image_init(FuRaydiumTpImage *self)
 {
 	fu_firmware_add_flag(FU_FIRMWARE(self), FU_FIRMWARE_FLAG_HAS_CHECKSUM);
 	fu_firmware_add_flag(FU_FIRMWARE(self), FU_FIRMWARE_FLAG_NO_AUTO_DETECTION);
+	fu_firmware_set_size_max(FU_FIRMWARE(self), 1 * FU_MB);
 }
 
 static void
