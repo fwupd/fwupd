de6d370a0c720db653a4ce90ec3d45c15900b782
Rishabh Agrawal <rishabhagr@google.com>
2026-06-11 15:42:42 +0530

    Fix daemon runtime error 'no path set for datadir-pkg'

--- src/fu-binder-daemon.c
+++ src/fu-binder-daemon.c
@@ -1457,7 +1457,9 @@ fu_binder_daemon_setup(FuDaemon *daemon,
 				FU_ENGINE_LOAD_FLAG_REMOTES | FU_ENGINE_LOAD_FLAG_EXTERNAL_PLUGINS |
 				FU_ENGINE_LOAD_FLAG_BUILTIN_PLUGINS |
 				FU_ENGINE_LOAD_FLAG_ENSURE_CLIENT_CERT |
-				FU_ENGINE_LOAD_FLAG_DEVICE_HOTPLUG,
+				FU_ENGINE_LOAD_FLAG_PATH_STORE_DEFAULTS |
+				FU_ENGINE_LOAD_FLAG_DEVICE_HOTPLUG |
+				FU_ENGINE_LOAD_FLAG_HISTORY,
 			    fu_progress_get_child(progress),
 			    error)) {
 		g_prefix_error(error, "failed to load engine: ");
