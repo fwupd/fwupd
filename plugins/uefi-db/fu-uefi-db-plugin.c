7d3936703616e78927c992eaa3463b949ddceba3
Richard Hughes <richard@hughsie.com>
2026-07-03 22:57:31 +0100

    Do not allow db updates if the KEK is marked as external
    Fixes https://github.com/fwupd/fwupd/issues/10607

--- plugins/uefi-db/fu-uefi-db-plugin.c
+++ plugins/uefi-db/fu-uefi-db-plugin.c
@@ -11,6 +11,7 @@
 
 struct _FuUefiDbPlugin {
 	FuPlugin parent_instance;
+	FuDevice *device_kek;
 };
 
 G_DEFINE_TYPE(FuUefiDbPlugin, fu_uefi_db_plugin, FU_TYPE_PLUGIN)
@@ -63,9 +64,26 @@ fu_uefi_db_plugin_device_child_added(FuPlugin *plugin, FuDevice *device)
 }
 
 static void
+fu_uefi_db_plugin_ensure_external_locked(FuUefiDbPlugin *self, FuDevice *device_db)
+{
+	if (self->device_kek == NULL)
+		return;
+	if (fu_device_has_private_flag(self->device_kek, FU_UEFI_DEVICE_PRIVATE_FLAG_IS_EXTERNAL)) {
+		GPtrArray *device_children = fu_device_get_children(device_db);
+		for (guint i = 0; i < device_children->len; i++) {
+			FuDevice *device_tmp = g_ptr_array_index(device_children, i);
+			fu_device_add_problem(device_tmp, FWUPD_DEVICE_PROBLEM_FIRMWARE_LOCKED);
+		}
+	}
+}
+
+static void
 fu_uefi_db_plugin_device_added(FuPlugin *plugin, FuDevice *device)
 {
 	FuContext *ctx = fu_plugin_get_context(plugin);
+	FuUefiDbPlugin *self = FU_UEFI_DB_PLUGIN(plugin);
+
+	fu_uefi_db_plugin_ensure_external_locked(self, device);
 
 	if (!fu_plugin_get_config_value_boolean(plugin, "UpdateWindowsCA") &&
 	    !fu_context_has_flag(ctx, FU_CONTEXT_FLAG_DUAL_BOOT_WINDOWS)) {
@@ -78,10 +96,35 @@ fu_uefi_db_plugin_device_added(FuPlugin *plugin, FuDevice *device)
 }
 
 static void
+fu_uefi_db_plugin_device_registered(FuPlugin *plugin, FuDevice *device)
+{
+	FuUefiDbPlugin *self = FU_UEFI_DB_PLUGIN(plugin);
+	if (g_strcmp0(fu_device_get_plugin(device), "uefi_kek") == 0) {
+		GPtrArray *devices = fu_plugin_get_devices(plugin);
+		g_set_object(&self->device_kek, device);
+		for (guint i = 0; i < devices->len; i++) {
+			FuDevice *device_tmp = g_ptr_array_index(devices, i);
+			fu_uefi_db_plugin_ensure_external_locked(self, device_tmp);
+		}
+	}
+}
+
+static void
+fu_uefi_db_plugin_finalize(GObject *obj)
+{
+	FuUefiDbPlugin *self = FU_UEFI_DB_PLUGIN(obj);
+	g_clear_object(&self->device_kek);
+	G_OBJECT_CLASS(fu_uefi_db_plugin_parent_class)->finalize(obj);
+}
+
+static void
 fu_uefi_db_plugin_class_init(FuUefiDbPluginClass *klass)
 {
 	FuPluginClass *plugin_class = FU_PLUGIN_CLASS(klass);
+	GObjectClass *object_class = G_OBJECT_CLASS(klass);
+	object_class->finalize = fu_uefi_db_plugin_finalize;
 	plugin_class->constructed = fu_uefi_db_plugin_constructed;
 	plugin_class->modify_config = fu_uefi_db_plugin_modify_config;
 	plugin_class->device_added = fu_uefi_db_plugin_device_added;
+	plugin_class->device_registered = fu_uefi_db_plugin_device_registered;
 }
