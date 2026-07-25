efca0201afff2e4009113a198ab41c04930b560b
Peter Hutterer <peter.hutterer@who-t.net>
2026-07-25 12:52:41 +1000

    rust: Add a glib crate for FFI calls down to glib
    Currently provides GError and GString and a few helper function we
    already know we'll use shortly.

---
+++ rust/fwupd-ffi/src/glib/mod.rs
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2026 Red Hat
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+//! GLib FFI helpers
+
+mod gerror;
+mod gstring;
+
+pub use gerror::*;
+pub use gstring::*;
+
+/// GLib gboolean TRUE
+pub const GTRUE: i32 = 1;
+/// GLib gboolean FALSE
+pub const GFALSE: i32 = 0;
+
+pub const G_SEEK_CUR: i32 = 0;
+pub const G_SEEK_SET: i32 = 1;
+pub const G_SEEK_END: i32 = 2;
