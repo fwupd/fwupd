b49be66172f1eb6f83b69aaebb9d9b570c39a504
Richard Hughes <richard@hughsie.com>
2026-08-06 11:48:46 +0100

    Run the self tests under Android

--- contrib/ci/ubuntu-android.sh
+++ contrib/ci/ubuntu-android.sh
@@ -10,20 +10,21 @@ fi
 # check for NDK, SDK and Bionic runtime
 ./contrib/ci/android.sh
 
-# make aidl available
+# make aidl and exe_wrapper available
 export ANDROID_SDK_ROOT="/opt/android/sdk"
-export PATH="${PATH}:${ANDROID_SDK_ROOT}/cmdline-tools/bin:${ANDROID_SDK_ROOT}/build-tools/35.0.0"
+export PATH="${PWD}/contrib/android:${PATH}:${ANDROID_SDK_ROOT}/cmdline-tools/bin:${ANDROID_SDK_ROOT}/build-tools/35.0.0"
 
 root=$(pwd)
 export BUILD="${root}/build"
 rm -rf "${BUILD}"
-meson "${BUILD}" \
-    -Dman=false \
-    -Ddocs=enabled \
-    -Dlibxmlb:gtkdoc=false \
-    -Db_sanitize=address \
+meson setup "${BUILD}" \
+    -Db_coverage=true \
     --cross-file contrib/android/android_x86_64-cross-file.ini \
     --prefix="${root}/target"
 
 ninja -C "${BUILD}" -v
-meson test -C "${BUILD}" --print-errorlogs --verbose
+ninja -C "${BUILD}" test
+
+# generate coverage report using llvm-cov from the NDK
+export GCOV="/opt/android/android-ndk-r27d/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-cov gcov"
+./contrib/ci/coverage.sh
