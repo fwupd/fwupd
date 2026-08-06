76e33240b8716b61190ffbc1e8f6a358e650fd84
Peter Hutterer <peter.hutterer@who-t.net>
2026-08-06 11:25:00 +1000

    Drop fu_memory_input_stream_new() from the API
    This API was unused by any caller and it's quite pointless, it would
    merely create an empty stream that cannot be appended to. The only
    in-tree caller we had was a test case that can easily be switched
    to GBytes.

--- src/fu-engine-gtypes-test.c
+++ src/fu-engine-gtypes-test.c
@@ -31,7 +31,8 @@ fu_engine_plugin_device_gtype(FuContext *ctx, GType gtype, gboolean is_fake)
 	g_autoptr(GError) error = NULL;
 	g_autoptr(GHashTable) metadata_post = NULL;
 	g_autoptr(GHashTable) metadata_pre = NULL;
-	g_autoptr(FuInputStream) stream = fu_memory_input_stream_new();
+	g_autoptr(GBytes) data = g_bytes_new(NULL, 0);
+	g_autoptr(FuInputStream) stream = fu_memory_input_stream_new_from_bytes(data);
 
 	g_debug("loading %s", g_type_name(gtype));
 	device = g_object_new(gtype, "context", ctx, "physical-id", "/sys", NULL);
