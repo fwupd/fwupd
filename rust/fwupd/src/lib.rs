83a6ec24eeace35ec3b81f5556605384eabccf35
Peter Hutterer <peter.hutterer@who-t.net>
2026-08-06 23:20:59 +1000

    rust: Switch to thiserror@1
    Notably: this uses thiserror version 1 and not the current 2.0.19 due to
    this build error:
    
        error: environment variable `CARGO_PKG_VERSION_PATCH` not defined at compile time
        --> ../../subprojects/thiserror-impl-2.0.19/src/lib.rs:51:34
        |
        51 |             concat!("__private", env!("CARGO_PKG_VERSION_PATCH")),
        |                                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
        |
        = help: `CARGO_PKG_VERSION_PATCH` may not be available for the current Cargo target
        = help: Cargo sets build script variables at run time. Use `std::env::var("CARGO_PKG_VERSION_PATCH")` instead
    
        error: aborting due to 1 previous error
    
    thiserror requires CARGO_PKG_VERSION_PATCH to be set in the env, the
    value of which is used as private field in case there are multiple versions
    of thiserror in a workspace. cargo sets that variable by default but
    meson does not and it does not provide a way to pass that environment
    variable down to rustc.

--- rust/fwupd/src/lib.rs
+++ rust/fwupd/src/lib.rs
@@ -8,37 +8,60 @@
 
 mod bitflags;
 
+use thiserror::Error;
+
 pub use crate::bitflags::{BitflagIter, Bitflags};
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-define_error! {
+#[derive(Error, Debug)]
+pub enum Error {
     /// Error enum values matching the C `FwupdError` enum order from
     /// `fwupd-error.h`.
-    Error {
-    Internal        = 0  => "Internal error.",
-    VersionNewer    = 1  => "Installed newer firmware version.",
-    VersionSame     = 2  => "Installed same firmware version.",
-    AlreadyPending  = 3  => "Already set to be installed offline.",
-    AuthFailed      = 4  => "Failed to get authentication.",
-    Read            = 5  => "Failed to read from device.",
-    Write           = 6  => "Failed to write to the device.",
-    InvalidFile     = 7  => "Invalid file format.",
-    NotFound        = 8  => "No matching device exists.",
-    NothingToDo     = 9  => "Nothing to do.",
-    NotSupported    = 10 => "Action was not possible.",
-    SignatureInvalid = 11 => "Signature was invalid.",
-    AcPowerRequired = 12 => "AC power was required.",
-    PermissionDenied = 13 => "Permission was denied.",
-    BrokenSystem    = 14 => "User has configured their system in a broken way.",
-    BatteryLevelTooLow = 15 => "The system battery level is too low.",
-    NeedsUserAction = 16 => "User needs to do an action to complete the update.",
-    AuthExpired     = 17 => "Failed to get auth as credentials have expired.",
-    InvalidData     = 18 => "Invalid data.",
-    TimedOut        = 19 => "The request timed out.",
-    Busy            = 20 => "The device is busy.",
-    NotReachable    = 21 => "The network is not reachable.",
-    }
+    #[error("Internal error.")]
+    Internal = 0,
+    #[error("Installed newer firmware version.")]
+    VersionNewer = 1,
+    #[error("Installed same firmware version.")]
+    VersionSame = 2,
+    #[error("Already set to be installed offline.")]
+    AlreadyPending = 3,
+    #[error("Failed to get authentication.")]
+    AuthFailed = 4,
+    #[error("Failed to read from device.")]
+    Read = 5,
+    #[error("Failed to write to the device.")]
+    Write = 6,
+    #[error("Invalid file format.")]
+    InvalidFile = 7,
+    #[error("No matching device exists.")]
+    NotFound = 8,
+    #[error("Nothing to do.")]
+    NothingToDo = 9,
+    #[error("Action was not possible.")]
+    NotSupported = 10,
+    #[error("Signature was invalid.")]
+    SignatureInvalid = 11,
+    #[error("AC power was required.")]
+    AcPowerRequired = 12,
+    #[error("Permission was denied.")]
+    PermissionDenied = 13,
+    #[error("User has configured their system in a broken way.")]
+    BrokenSystem = 14,
+    #[error("The system battery level is too low.")]
+    BatteryLevelTooLow = 15,
+    #[error("User needs to do an action to complete the update.")]
+    NeedsUserAction = 16,
+    #[error("Failed to get auth as credentials have expired.")]
+    AuthExpired = 17,
+    #[error("Invalid data.")]
+    InvalidData = 18,
+    #[error("The request timed out.")]
+    TimedOut = 19,
+    #[error("The device is busy.")]
+    Busy = 20,
+    #[error("The network is not reachable.")]
+    NotReachable = 21,
 }
 
 impl From<Error> for u32 {
