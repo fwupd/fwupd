52d8bd746012e54e7c44a359f9abfc9a708a8ec5
Peter Hutterer <peter.hutterer@who-t.net>
2026-08-11 09:59:25 +1000

    rust: Typedef the glib booleans and seek types
    These are type aliases so the only effect is readability but that's not
    a bad thing to have.

--- rust/fwupd-ffi/src/glib/mod.rs
+++ rust/fwupd-ffi/src/glib/mod.rs
@@ -12,11 +12,23 @@ mod gstring;
 pub use gerror::*;
 pub use gstring::*;
 
-/// `GLib` gboolean TRUE
-pub const GTRUE: i32 = 1;
-/// `GLib` gboolean FALSE
-pub const GFALSE: i32 = 0;
+/// Typedef for a `gboolean`.
+///
+/// Boolean return values use `i32` (not Rust `bool`) to match `GLib`'s `gboolean`
+/// type which is `gint` (4 bytes), not C99 `_Bool` (1 byte).
+pub type GBoolean = i32;
 
-pub const G_SEEK_CUR: i32 = 0;
-pub const G_SEEK_SET: i32 = 1;
-pub const G_SEEK_END: i32 = 2;
+/// Typedef for a `goffset`
+pub type GOffset = i64;
+
+/// `GLib` gboolean TRUE
+pub const GTRUE: GBoolean = 1;
+/// `GLib` gboolean FALSE
+pub const GFALSE: GBoolean = 0;
+
+/// Typedef for `GLib`'s `GSeekType` i32 value.
+pub type GSeekType = i32;
+
+pub const G_SEEK_CUR: GSeekType = 0;
+pub const G_SEEK_SET: GSeekType = 1;
+pub const G_SEEK_END: GSeekType = 2;
