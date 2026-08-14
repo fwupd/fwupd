03ce19ec5e36cb82214fe76c8b02a3233f34ebdf
Peter Hutterer <peter.hutterer@who-t.net>
2026-08-14 12:39:26 +1000

    libfwupd: Add a new json parser loader from a FuInputStream
    Two new functions
    - libfwupd: fwupd_json_parser_load_from_stream_impl() which connects the
      Rust stream impl to the Rust json loader, skipping the C callbacks
      that we have in the normal json stream loader.
    
      This is a private method in that we only expect internal consumers
      (libfwupdplugin) to use it so the declaration is hidden in a private
      header file. It does need to be exported by the .so file though.
    
    - libfwupdplugin: fu_json_parser_load_from_stream() which takes
      a FuInputStream (which we know is Rust-backed) and loads that.
    
    With those two together we can now create a Rust JSON parser for a
    Rust-backed stream from our C code - once created the Rust bits
    talk to each other directly without FFI.
    
    Because of the GIR bindings we pass the stream_impl as gpointer, if we
    typedef those to FuRsStreamImpl like we do in libfwupdplugin we end up
    with an unhappy GIR compiler.

---
+++ libfwupdplugin/fu-json-parser.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2026 Red Hat
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#include "config.h"
+
+#include "fu-json-parser.h"
+
+/**
+ * fu_json_parser_load_from_stream: (skip):
+ * @self: a #FwupdJsonParser
+ * @stream: a #FuInputStream
+ * @flags: a #FwupdJsonLoadFlags
+ * @error: (nullable): optional return location for an error
+ *
+ * Loads JSON from a Rust-backed FuInputStream stream.
+ *
+ * Returns: (transfer full): a #FwupdJsonNode, or %NULL for error
+ *
+ * Since: 2.1.8
+ **/
+FwupdJsonNode *
+fu_json_parser_load_from_stream(FwupdJsonParser *self,
+				FuInputStream *stream,
+				FwupdJsonLoadFlags flags,
+				GError **error)
+{
+	FuRsStreamImpl *impl;
+
+	g_return_val_if_fail(FWUPD_IS_JSON_PARSER(self), NULL);
+	g_return_val_if_fail(FU_IS_INPUT_STREAM(stream), NULL);
+	g_return_val_if_fail(error == NULL || *error == NULL, NULL);
+
+	impl = fu_input_stream_get_stream_impl(stream);
+	if (impl == NULL) {
+		g_set_error_literal(error,
+				    FWUPD_ERROR,
+				    FWUPD_ERROR_NOT_SUPPORTED,
+				    "stream does not have a Rust implementation");
+		return NULL;
+	}
+
+	/* ownership of impl is transferred */
+	return fwupd_json_parser_load_from_stream_impl(self, impl, flags, error);
+}
