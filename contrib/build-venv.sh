f43940939f485acf61e8cfa0fd74f036b08d0bd0
Peter Hutterer <peter.hutterer@who-t.net>
2026-08-17 09:42:09 +1000

    trivial: Fix shellcheck issues in setup/build-fwupd/test-fwupd
    Disable shellcheck warnings for the nix.sh include (we source it so
    it'd have to be passed to the shellcheck command, easier to just disable
    the warning).
    
    Use read -r, the few questions we ask in setup don't care about
    backslash escaping anyway.
    
    The rest are all quotes, the exceptions are the mtd-self-test ENV usage
    which (given it's hardcoded anyway) is now single command over three
    lines instead of an ENV, and the EFI_PREFIX handling which splits
    fname into a separate variable to avoid nested quotes in basename.

--- contrib/build-venv.sh
+++ contrib/build-venv.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 set -e
 
+# shellcheck disable=SC1091
 . "$(dirname "$(readlink -f "$0")")/nix.sh"
 
-VENV=$(dirname "$0")/..
-BUILD=${VENV}/build
-DIST=${VENV}/dist
+VENV="$(dirname "$0")/.."
+BUILD="${VENV}/build"
+DIST="${VENV}/dist"
 EXTRA_ARGS="-Dlibxmlb:gtkdoc=false -Dsystemd=disabled"
 
 # NixOS: extract vendor_ids_dir from mesonFlags set by nix-shell
@@ -19,23 +20,25 @@ if [ -f "${VENV}/.nixos" ] && [ -n "$mesonFlags" ]; then
 fi
 
 #build and install
-if [ ! -d ${BUILD} ] || ! [ -e ${BUILD}/build.ninja ]; then
-    meson setup ${BUILD} --prefix=${DIST} ${EXTRA_ARGS} $@
+if [ ! -d "${BUILD}" ] || ! [ -e "${BUILD}/build.ninja" ]; then
+    # shellcheck disable=SC2086
+    meson setup "${BUILD}" --prefix="${DIST}" ${EXTRA_ARGS} "$@"
 fi
-ninja -C ${BUILD} install
+ninja -C "${BUILD}" install
 
 # check whether we have an existing fwupd EFI binary in the host system to use
-EFI_PREFIX=$(pkg-config fwupd-efi --variable=prefix 2>/dev/null || echo "/usr")
+EFI_PREFIX="$(pkg-config fwupd-efi --variable=prefix 2>/dev/null || echo "/usr")"
 EFI_DIR=libexec/fwupd/efi
 if [ -d "${EFI_PREFIX}/${EFI_DIR}" ]; then
     BINARIES=$(find "${EFI_PREFIX}/${EFI_DIR}" -name "*.efi*" -type f -print || true)
     if [ -n "${BINARIES}" ]; then
-        mkdir -p ${DIST}/${EFI_DIR}
+        mkdir -p "${DIST}/${EFI_DIR}"
         for i in ${BINARIES}; do
-            if [ -f "${DIST}/${EFI_DIR}/$(basename $i)" ]; then
+            FN=$(basename "$i")
+            if [ -f "${DIST}/${EFI_DIR}/${FN}" ]; then
                 continue
             fi
-            ln -s $i "${DIST}/${EFI_DIR}/$(basename $i)"
+            ln -s "$i" "${DIST}/${EFI_DIR}/${FN}"
         done
     fi
 fi
