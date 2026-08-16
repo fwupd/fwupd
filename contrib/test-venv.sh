200fa395ba31246daf4d5dcc5693c4fc08402a45
Peter Hutterer <peter.hutterer@who-t.net>
2026-08-17 09:42:09 +1000

    trivial: Fix shellcheck issues in setup/build-fwupd/test-fwupd
    Disable shellcheck warnings for the nix.sh include (we source it so
    it'd have to be passed to the shellcheck command, easier to just disable
    the warning).
    
    Use read -r, the few questions we ask in setup don't care about
    backslash escaping anyway.
    
    The rest are all quotes, the exceptions are the mtd-self-test ENV usage
    which (given it's hardcoded anyway) is now single command over three
    lines instead of an ENV, and the EFI_PREFIX handling which splits
    fname into a separate variable to avoid nested quotes in basename.

--- contrib/test-venv.sh
+++ contrib/test-venv.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 set -e
 
+# shellcheck disable=SC1091
 . "$(dirname "$(readlink -f "$0")")/nix.sh"
 
-VENV=$(dirname "$0")/..
-BUILD=${VENV}/build
-INSTALLED_TESTS=${VENV}/dist/share/installed-tests/fwupd
+VENV="$(dirname "$0")/.."
+BUILD="${VENV}/build"
+INSTALLED_TESTS="${VENV}/dist/share/installed-tests/fwupd"
 SUDO=$(which sudo)
-export G_TEST_BUILDDIR=${INSTALLED_TESTS}
-export G_TEST_SRCDIR=${INSTALLED_TESTS}
-export GI_TYPELIB_PATH=${BUILD}/libfwupd
-export LD_LIBRARY_PATH=${BUILD}/libfwupd
-export DAEMON_BUILDDIR=${BUILD}/src
-export PATH=${VENV}/bin:$PATH
+export G_TEST_BUILDDIR="${INSTALLED_TESTS}"
+export G_TEST_SRCDIR="${INSTALLED_TESTS}"
+export GI_TYPELIB_PATH="${BUILD}/libfwupd"
+export LD_LIBRARY_PATH="${BUILD}/libfwupd"
+export DAEMON_BUILDDIR="${BUILD}/src"
+export PATH="${VENV}/bin:$PATH"
 export PYTHONWARNINGS="ignore::DeprecationWarning:gi.events"
 
 echo "Build time test suite"
@@ -21,20 +22,21 @@ meson test -C "${BUILD}" "$@"
 # If we have arguments to pass to meson test, skip the other tests
 if [ $# -eq 0 ]; then
     echo "Testing mtd-self-test"
-    ${SUDO} modprobe mtdram
-    ENV="G_TEST_BUILDDIR=${G_TEST_BUILDDIR} \
-        LD_LIBRARY_PATH=${LD_LIBRARY_PATH} \
-        G_TEST_SRCDIR=${G_TEST_SRCDIR}"
-    ${SUDO} ${ENV} ${VENV}/dist/libexec/installed-tests/fwupd/mtd-self-test
+    "${SUDO}" modprobe mtdram
+    "${SUDO}" \
+        G_TEST_BUILDDIR="${G_TEST_BUILDDIR}" \
+        LD_LIBRARY_PATH="${LD_LIBRARY_PATH}" \
+        G_TEST_SRCDIR="${G_TEST_SRCDIR}" \
+        "${VENV}"/dist/libexec/installed-tests/fwupd/mtd-self-test
 
     echo "Testing fwupdtool.sh"
-    ${INSTALLED_TESTS}/fwupdtool.sh
+    "${INSTALLED_TESTS}"/fwupdtool.sh
 
     echo "Starting daemon"
-    G_DEBUG=fatal-criticals ${VENV}/bin/fwupd --verbose --no-timestamp >fwupd.txt 2>&1 &
+    G_DEBUG=fatal-criticals "${VENV}"/bin/fwupd --verbose --no-timestamp >fwupd.txt 2>&1 &
 
     echo "Testing fwupd.sh"
-    ${INSTALLED_TESTS}/fwupd.sh
+    "${INSTALLED_TESTS}"/fwupd.sh
 
     # artifacts from the test run
     rm -f fwupd.txt fwupdtool.txt
