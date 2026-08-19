fa69c9591eef7a2c0cb80c4af4e3a4b7525c5fb3
Richard Hughes <richard@hughsie.com>
2026-08-19 17:07:08 +0100

    trivial: Avoid using (gchar *) when we only need the const version
    (cherry picked from commit 9042b852c30c0e0eb3ae4cda1338de95369f4010)

--- plugins/tpm/fu-tpm-v2-device.c
+++ plugins/tpm/fu-tpm-v2-device.c
@@ -93,7 +93,7 @@ fu_tpm_v2_device_get_string(FuTpmV2Device *self, guint32 query, GError **error)
 	if (!fu_tpm_v2_device_get_uint32(self, query, &val_be, error))
 		return NULL;
 	val = GUINT32_FROM_BE(val_be);	  /* nocheck:blocked */
-	memcpy(result, (gchar *)&val, 4); /* nocheck:blocked */
+	memcpy(result, (const gchar *)&val, 4); /* nocheck:blocked */
 
 	/* convert non-ASCII into spaces */
 	for (guint i = 0; i < 4; i++) {
