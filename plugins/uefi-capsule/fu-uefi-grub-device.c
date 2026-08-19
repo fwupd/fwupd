6f85b6da416324d9b90128aee977cd2f14c1d7a5
Richard Hughes <richard@hughsie.com>
2026-08-19 13:41:31 +0100

    trivial: Emit a write-file signal from FuVolume
    (cherry picked from commit 1cf2cad7757c7dcb13f54e9e1a04b139b7180b8f)

--- plugins/uefi-capsule/fu-uefi-grub-device.c
+++ plugins/uefi-capsule/fu-uefi-grub-device.c
@@ -212,8 +212,6 @@ fu_uefi_grub_device_write_firmware(FuDevice *device,
 						   NULL);
 	if (fn == NULL)
 		return FALSE;
-	if (!fu_path_mkdir_parent(fn, error))
-		return FALSE;
 	fixed_fw = fu_uefi_capsule_device_fixup_firmware(self, fw, error);
 	if (fixed_fw == NULL)
 		return FALSE;
