fwupd/fwupd

commit a43aaa18aab374a0e6c9b99455f2e47fe4df81b0

Add fu_context_timeout_add_seconds() that uses the GMainContext for idle
At the moment we're using the default context in lots of places and although
that works now, it's only by accident.

src/fu-bluez-backend.c
@@ -133,7 +133,7 @@ typedef struct {
 	GMainLoop *loop;
 	GError **error;
 	GCancellable *cancellable;
-	guint timeout_id;
+	GSource *timeout_source;
 } FuBluezBackendHelper;
 
 static void
@@ -141,8 +141,10 @@ fu_bluez_backend_helper_free(FuBluezBackendHelper *helper)
 {
 	if (helper->object_manager != NULL)
 		g_object_unref(helper->object_manager);
-	if (helper->timeout_id != 0)
-		g_source_remove(helper->timeout_id);
+	if (helper->timeout_source != NULL) {
+		g_source_destroy(helper->timeout_source);
+		g_source_unref(helper->timeout_source);
+	}
 	g_cancellable_cancel(helper->cancellable);
 	g_main_loop_unref(helper->loop);
 	g_free(helper);
@@ -164,7 +166,7 @@ fu_bluez_backend_timeout_cb(gpointer user_data)
 {
 	FuBluezBackendHelper *helper = (FuBluezBackendHelper *)user_data;
 	g_cancellable_cancel(helper->cancellable);
-	helper->timeout_id = 0;
+	g_clear_pointer(&helper->timeout_source, g_source_unref);
 	return G_SOURCE_REMOVE;
 }
 
@@ -175,15 +177,20 @@ fu_bluez_backend_setup(FuBackend *backend,
 		       GError **error)
 {
 	FuBluezBackend *self = FU_BLUEZ_BACKEND(backend);
+	FuContext *ctx = fu_backend_get_context(backend);
+	GMainContext *main_ctx = fu_context_get_main_context(ctx);
 	g_autoptr(FuBluezBackendHelper) helper = g_new0(FuBluezBackendHelper, 1);
 
 	/* in some circumstances the bluez daemon will just hang... do not wait
 	 * forever and make fwupd startup also fail */
 	helper->error = error;
-	helper->loop = g_main_loop_new(NULL, FALSE);
+	helper->loop = g_main_loop_new(main_ctx, FALSE);
 	helper->cancellable = g_cancellable_new();
-	helper->timeout_id =
-	    g_timeout_add(FU_BLUEZ_BACKEND_TIMEOUT, fu_bluez_backend_timeout_cb, helper);
+	helper->timeout_source = fu_context_add_timeout(ctx,
+							FU_BLUEZ_BACKEND_TIMEOUT,
+							fu_bluez_backend_timeout_cb,
+							helper);
+	g_main_context_push_thread_default(main_ctx);
 	g_dbus_object_manager_client_new_for_bus(G_BUS_TYPE_SYSTEM,
 						 G_DBUS_OBJECT_MANAGER_CLIENT_FLAGS_NONE,
 						 "org.bluez",
@@ -194,6 +201,7 @@ fu_bluez_backend_setup(FuBackend *backend,
 						 helper->cancellable,
 						 fu_bluez_backend_connect_cb,
 						 helper);
+	g_main_context_pop_thread_default(main_ctx);
 	g_main_loop_run(helper->loop);
 	if (helper->object_manager == NULL)
 		return FALSE;

libfwupdplugin/fu-context.c
@@ -319,6 +319,62 @@ fu_context_get_main_context(FuContext *self)
 }
 
 /**
+ * fu_context_add_timeout_seconds:
+ * @self: a #FuContext
+ * @interval: timeout interval in seconds
+ * @function: (scope call): function to call
+ * @data: data to pass to @function
+ *
+ * Creates a new timeout source and attaches it to the main context.
+ *
+ * Returns: (transfer full): the #GSource, which must be removed with g_source_unref()
+ *
+ * Since: 2.1.8
+ **/
+GSource *
+fu_context_add_timeout_seconds(FuContext *self, guint interval, GSourceFunc function, gpointer data)
+{
+	FuContextPrivate *priv = GET_PRIVATE(self);
+	g_autoptr(GSource) source = g_timeout_source_new_seconds(interval);
+
+	g_return_val_if_fail(FU_IS_CONTEXT(self), NULL);
+	g_return_val_if_fail(interval != 0, NULL);
+	g_return_val_if_fail(function != NULL, NULL);
+
+	g_source_set_callback(source, function, data, NULL);
+	g_source_attach(source, priv->main_ctx);
+	return g_steal_pointer(&source);
+}
+
+/**
+ * fu_context_add_timeout:
+ * @self: a #FuContext
+ * @interval_ms: timeout interval in ms
+ * @function: (scope call): function to call
+ * @data: data to pass to @function
+ *
+ * Creates a new timeout source and attaches it to the main context.
+ *
+ * Returns: (transfer full): the #GSource, which must be removed with g_source_unref()
+ *
+ * Since: 2.1.8
+ **/
+GSource *
+fu_context_add_timeout(FuContext *self, guint interval_ms, GSourceFunc function, gpointer data)
+{
+	FuContextPrivate *priv = GET_PRIVATE(self);
+	g_autoptr(GSource) source = g_timeout_source_new(interval_ms);
+
+	g_return_val_if_fail(FU_IS_CONTEXT(self), NULL);
+	g_return_val_if_fail(interval_ms != 0, NULL);
+	g_return_val_if_fail(function != NULL, NULL);
+
+	g_source_set_callback(source, function, data, NULL);
+	g_source_attach(source, priv->main_ctx);
+	return g_steal_pointer(&source);
+}
+
+/**
  * fu_context_set_main_context:
  * @self: a #FuContext
  * @main_ctx: (nullable): a #GMainContext

libfwupdplugin/fu-context.h
@@ -160,6 +160,13 @@ gboolean
 fu_context_efivars_check_free_space(FuContext *self, gsize count, GError **error)
     G_GNUC_NON_NULL(1);
 
+GSource *
+fu_context_add_timeout(FuContext *self, guint interval_ms, GSourceFunc function, gpointer data)
+    G_GNUC_NON_NULL(1, 3);
+GSource *
+fu_context_add_timeout_seconds(FuContext *self, guint interval, GSourceFunc function, gpointer data)
+    G_GNUC_NON_NULL(1, 3);
+
 GPtrArray *
 fu_context_get_esp_files(FuContext *self, FuContextEspFileFlags flags, GError **error)
     G_GNUC_NON_NULL(1);

libfwupdplugin/fu-device.c
@@ -24,6 +24,7 @@
 #include "fu-input-stream.h"
 #include "fu-memory-input-stream.h"
 #include "fu-output-stream.h"
+#include "fu-progress-private.h"
 #include "fu-quirks.h"
 #include "fu-security-attr.h"
 #include "fu-string.h"
@@ -78,10 +79,11 @@ typedef struct {
 	guint event_idx;
 	guint remove_delay;    /* ms */
 	guint acquiesce_delay; /* ms */
+	guint poll_interval;   /* ms */
 	guint request_cnts[FWUPD_REQUEST_KIND_LAST];
 	gint order;
 	guint priority;
-	guint poll_id;
+	GSource *poll_source;
 	gint poll_locker_cnt;
 	gboolean done_probe;
 	gboolean done_setup;
@@ -791,6 +793,29 @@ fu_device_sleep_full(FuDevice *self, guint delay_ms, FuProgress *progress)
 }
 
 /**
+ * fu_device_sleep_idle:
+ * @self: a #FuDevice
+ * @duration_ms: duration in milliseconds
+ * @progress: a #FuProgress
+ *
+ * Sleeps, setting the device progress from 0..100% as time continues.
+ *
+ * Since: 2.1.8
+ **/
+void
+fu_device_sleep_idle(FuDevice *self, guint duration_ms, FuProgress *progress)
+{
+	FuDevicePrivate *priv = GET_PRIVATE(self);
+
+	g_return_if_fail(FU_IS_DEVICE(self));
+	g_return_if_fail(FU_IS_CONTEXT(priv->ctx));
+	g_return_if_fail(duration_ms < 1000000);
+	g_return_if_fail(FU_IS_PROGRESS(progress));
+
+	fu_progress_sleep_idle(progress, fu_context_get_main_context(priv->ctx), duration_ms);
+}
+
+/**
  * fu_device_set_contents:
  * @self: a #FuDevice
  * @filename: full path to a file
@@ -1279,10 +1304,36 @@ fu_device_poll_cb(gpointer user_data)
 
 	if (!fu_device_poll(self, &error_local)) {
 		g_warning("disabling polling: %s", error_local->message);
-		priv->poll_id = 0;
+		g_clear_pointer(&priv->poll_source, g_source_unref);
 		return G_SOURCE_REMOVE;
 	}
 	return G_SOURCE_CONTINUE;
+}
+
+static void
+fu_device_ensure_poll_interval(FuDevice *self)
+{
+	FuDevicePrivate *priv = GET_PRIVATE(self);
+
+	/* called from _init(); will call again from _constructed() */
+	if (priv->ctx == NULL)
+		return;
+
+	if (priv->poll_source != NULL) {
+		g_source_destroy(priv->poll_source);
+		g_clear_pointer(&priv->poll_source, g_source_unref);
+	}
+	if (priv->poll_interval == 0)
+		return;
+	if (priv->poll_interval % 1000 == 0) {
+		priv->poll_source = fu_context_add_timeout_seconds(priv->ctx,
+								   priv->poll_interval / 1000,
+								   fu_device_poll_cb,
+								   self);
+	} else {
+		priv->poll_source =
+		    fu_context_add_timeout(priv->ctx, priv->poll_interval, fu_device_poll_cb, self);
+	}
 }
 
 /**
@@ -1300,17 +1351,11 @@ void
 fu_device_set_poll_interval(FuDevice *self, guint interval)
 {
 	FuDevicePrivate *priv = GET_PRIVATE(self);
-
 	g_return_if_fail(FU_IS_DEVICE(self));
-
-	g_clear_handle_id(&priv->poll_id, g_source_remove);
-	if (interval == 0)
+	if (priv->poll_interval == interval)
 		return;
-	if (interval % 1000 == 0) {
-		priv->poll_id = g_timeout_add_seconds(interval / 1000, fu_device_poll_cb, self);
-	} else {
-		priv->poll_id = g_timeout_add(interval, fu_device_poll_cb, self);
-	}
+	priv->poll_interval = interval;
+	fu_device_ensure_poll_interval(self);
 }
 
 /**
@@ -5362,6 +5407,7 @@ fu_device_to_string_impl(FuDevice *self, guint idt, GString *str)
 	fwupd_codec_string_append(str, idt, "ProxyGuid", priv->proxy_guid);
 	fwupd_codec_string_append_int(str, idt, "RemoveDelay", priv->remove_delay);
 	fwupd_codec_string_append_int(str, idt, "AcquiesceDelay", priv->acquiesce_delay);
+	fwupd_codec_string_append_int(str, idt, "PollInterval", priv->poll_interval);
 	fwupd_codec_string_append(str, idt, "CustomFlags", priv->custom_flags);
 	if (priv->specialized_gtype != G_TYPE_INVALID)
 		fwupd_codec_string_append(str, idt, "GType", g_type_name(priv->specialized_gtype));
@@ -8377,6 +8423,14 @@ fu_device_from_json(FuDevice *self, FwupdJsonObject *json_obj, GError **error)
 }
 
 static void
+fu_device_constructed(GObject *obj)
+{
+	FuDevice *self = FU_DEVICE(obj);
+	fu_device_ensure_poll_interval(self);
+	G_OBJECT_CLASS(fu_device_parent_class)->constructed(obj);
+}
+
+static void
 fu_device_dispose(GObject *object)
 {
 	FuDevice *self = FU_DEVICE(object);
@@ -8457,6 +8511,7 @@ fu_device_class_init(FuDeviceClass *klass)
 	    FU_DEVICE_PRIVATE_FLAG_STRICT_EMULATION_ORDER,
 	};
 
+	object_class->constructed = fu_device_constructed;
 	object_class->dispose = fu_device_dispose;
 	object_class->finalize = fu_device_finalize;
 	object_class->get_property = fu_device_get_property;
@@ -8763,8 +8818,10 @@ fu_device_finalize(GObject *object)
 	}
 	if (priv->backend != NULL)
 		g_object_remove_weak_pointer(G_OBJECT(priv->backend), (gpointer *)&priv->backend);
-	if (priv->poll_id != 0)
-		g_source_remove(priv->poll_id);
+	if (priv->poll_source != NULL) {
+		g_source_destroy(priv->poll_source);
+		g_source_unref(priv->poll_source);
+	}
 	if (priv->metadata != NULL)
 		g_hash_table_unref(priv->metadata);
 	if (priv->inhibits != NULL)

libfwupdplugin/fu-device.h
@@ -1288,6 +1288,8 @@ void
 fu_device_sleep(FuDevice *self, guint delay_ms) G_GNUC_NON_NULL(1);
 void
 fu_device_sleep_full(FuDevice *self, guint delay_ms, FuProgress *progress) G_GNUC_NON_NULL(1);
+void
+fu_device_sleep_idle(FuDevice *self, guint duration_ms, FuProgress *progress) G_GNUC_NON_NULL(1);
 gboolean
 fu_device_bind_driver(FuDevice *self, const gchar *subsystem, const gchar *driver, GError **error)
     G_GNUC_WARN_UNUSED_RESULT G_GNUC_NON_NULL(1);

libfwupdplugin/fu-progress-private.h
@@ -10,3 +10,5 @@
 
 gdouble
 fu_progress_get_global_fraction(FuProgress *self) G_GNUC_NON_NULL(1);
+void
+fu_progress_sleep_idle(FuProgress *self, GMainContext *main_ctx, guint delay_ms) G_GNUC_NON_NULL(1);

libfwupdplugin/fu-progress-test.c
@@ -72,7 +72,7 @@ fu_progress_idle_func(void)
 
 	g_assert_cmpfloat_with_epsilon(fu_progress_get_duration(progress), 0.f, 0.001);
 
-	fu_progress_sleep_idle(progress, 500);
+	fu_progress_sleep_idle(progress, NULL, 500);
 	g_assert_cmpint(helper.last_percentage, ==, 0);
 
 	fu_test_loop_run_with_timeout(600);

libfwupdplugin/fu-progress.c
@@ -76,7 +76,7 @@ struct _FuProgress {
 	guint step_done;
 	guint step_scaling;
 	FuProgress *parent; /* no-ref */
-	guint sleep_timeout_id;
+	GSource *sleep_timeout_source;
 };
 
 enum { SIGNAL_PERCENTAGE_CHANGED, SIGNAL_STATUS_CHANGED, SIGNAL_LAST };
@@ -235,9 +235,10 @@ fu_progress_remove_flag(FuProgress *self, FuProgressFlags flag)
 static void
 fu_progress_sleep_idle_stop(FuProgress *self)
 {
-	if (self->sleep_timeout_id == 0)
-		return;
-	g_clear_handle_id(&self->sleep_timeout_id, g_source_remove);
+	if (self->sleep_timeout_source != NULL) {
+		g_source_destroy(self->sleep_timeout_source);
+		g_clear_pointer(&self->sleep_timeout_source, g_source_unref);
+	}
 }
 
 /**
@@ -1007,24 +1008,30 @@ fu_progress_sleep_idle_cb(gpointer user_data)
 /**
  * fu_progress_sleep_idle:
  * @self: a #FuProgress
+ * @main_ctx: a #GMainContext
  * @delay_ms: delay in milliseconds
  *
  * Sleeps, setting the device progress from 0..100% as time continues. If the parent progress
  * percentage is changed or finalized then the idle action will be cancelled.
  *
- * Since: 2.1.2
+ * Since: 2.1.8
  **/
 void
-fu_progress_sleep_idle(FuProgress *self, guint delay_ms)
+fu_progress_sleep_idle(FuProgress *self, GMainContext *main_ctx, guint delay_ms)
 {
+	g_autoptr(GSource) source = NULL;
+
 	g_return_if_fail(FU_IS_PROGRESS(self));
 	g_return_if_fail(delay_ms > 0);
 
 	fu_progress_sleep_idle_stop(self);
 	fu_progress_set_percentage(self, 0.0);
 	fu_progress_set_duration(self, (gdouble)delay_ms / 1000.f);
-	self->sleep_timeout_id =
-	    g_timeout_add(MAX(delay_ms / 1000, 1), fu_progress_sleep_idle_cb, self);
+
+	source = g_timeout_source_new(MAX(delay_ms / 100, 1));
+	g_source_set_callback(source, fu_progress_sleep_idle_cb, self, NULL);
+	g_source_attach(source, main_ctx);
+	self->sleep_timeout_source = g_steal_pointer(&source);
 }
 
 static void
@@ -1150,8 +1157,10 @@ fu_progress_finalize(GObject *object)
 	g_ptr_array_unref(self->children);
 	g_timer_destroy(self->timer);
 	g_timer_destroy(self->timer_child);
-	if (self->sleep_timeout_id != 0)
-		g_source_remove(self->sleep_timeout_id);
+	if (self->sleep_timeout_source != NULL) {
+		g_source_destroy(self->sleep_timeout_source);
+		g_source_unref(self->sleep_timeout_source);
+	}
 
 	G_OBJECT_CLASS(fu_progress_parent_class)->finalize(object);
 }

commit 2b573a06a28794b00a319d4ecfad71c5874b4dda

Bind the BlueZ signal subscriptions to the correct GMainContext
GDBusObjectManagerClient subscribes to the bus signals from ::init, and
g_dbus_connection_signal_subscribe() binds the subscription to the
thread-default main context of the calling thread.

The async constructor does not implement init_async, so GLib falls back to
the default GAsyncInitable implementation which runs ::init in a GTask
worker thread. That thread has no thread-default context, so the
subscription ends up bound to the global default context -- which nothing
iterates when a custom GMainContext is in use. Wrapping the async call in
g_main_context_push_thread_default() does not help, as that only affects
the context the completion callback is invoked in.

As a result no org.bluez.Device1 PropertiesChanged signal was ever
dispatched, ::g-properties-changed never fired, and a BLE device that
reboots and reconnects with the same D-Bus object path was never noticed --
so fu_device_list_wait_for_replug() timed out with 'failed to wait for
attach replug'. This regressed in a43aaa18a, before which both the backend
and the device list used the global default context, so they agreed.

Construct the object manager synchronously with the context pushed so that
everything is bound to the context we actually iterate. The startup timeout
from 4a1baae30 cannot stay a GSource, as nothing would be iterating the
context while the sync call blocks, so cancel from a watchdog thread
instead.

Fixes: a43aaa18a

src/fu-bluez-backend.c
@@ -128,46 +128,18 @@ fu_bluez_backend_object_removed_cb(GDBusObjectManager *manager,
 	fu_backend_device_removed(FU_BACKEND(self), device_tmp);
 }
 
-typedef struct {
-	GDBusObjectManager *object_manager;
-	GMainLoop *loop;
-	GError **error;
-	GCancellable *cancellable;
-	GSource *timeout_source;
-} FuBluezBackendHelper;
-
-static void
-fu_bluez_backend_helper_free(FuBluezBackendHelper *helper)
+/* the object manager has to be constructed synchronously to get the signal
+ * subscriptions bound to the correct main context, so the timeout cannot be a
+ * GSource -- nothing would be iterating the context while we block. Use a
+ * watchdog thread to cancel after the timeout. g_cancellable_cancel() is
+ * idempotent, so it's safe to call it even if the sync call already finished. */
+static gpointer
+fu_bluez_backend_watchdog_thread_cb(gpointer user_data)
 {
-	if (helper->object_manager != NULL)
-		g_object_unref(helper->object_manager);
-	if (helper->timeout_source != NULL) {
-		g_source_destroy(helper->timeout_source);
-		g_source_unref(helper->timeout_source);
-	}
-	g_cancellable_cancel(helper->cancellable);
-	g_main_loop_unref(helper->loop);
-	g_free(helper);
-}
-
-G_DEFINE_AUTOPTR_CLEANUP_FUNC(FuBluezBackendHelper, fu_bluez_backend_helper_free)
-
-static void
-fu_bluez_backend_connect_cb(GObject *source_object, GAsyncResult *res, gpointer user_data)
-{
-	FuBluezBackendHelper *helper = (FuBluezBackendHelper *)user_data;
-	helper->object_manager =
-	    g_dbus_object_manager_client_new_for_bus_finish(res, helper->error);
-	g_main_loop_quit(helper->loop);
-}
-
-static gboolean
-fu_bluez_backend_timeout_cb(gpointer user_data)
-{
-	FuBluezBackendHelper *helper = (FuBluezBackendHelper *)user_data;
-	g_cancellable_cancel(helper->cancellable);
-	g_clear_pointer(&helper->timeout_source, g_source_unref);
-	return G_SOURCE_REMOVE;
+	GCancellable *cancellable = G_CANCELLABLE(user_data);
+	g_usleep(FU_BLUEZ_BACKEND_TIMEOUT * 1000);
+	g_cancellable_cancel(cancellable);
+	return NULL;
 }
 
 static gboolean
@@ -179,33 +151,39 @@ fu_bluez_backend_setup(FuBackend *backend,
 	FuBluezBackend *self = FU_BLUEZ_BACKEND(backend);
 	FuContext *ctx = fu_backend_get_context(backend);
 	GMainContext *main_ctx = fu_context_get_main_context(ctx);
-	g_autoptr(FuBluezBackendHelper) helper = g_new0(FuBluezBackendHelper, 1);
+	GThread *watchdog_thread;
+	g_autoptr(GCancellable) cancellable = g_cancellable_new();
 
 	/* in some circumstances the bluez daemon will just hang... do not wait
 	 * forever and make fwupd startup also fail */
-	helper->error = error;
-	helper->loop = g_main_loop_new(main_ctx, FALSE);
-	helper->cancellable = g_cancellable_new();
-	helper->timeout_source = fu_context_add_timeout(ctx,
-							FU_BLUEZ_BACKEND_TIMEOUT,
-							fu_bluez_backend_timeout_cb,
-							helper);
+	watchdog_thread =
+	    g_thread_new("fu-bluez-watchdog", fu_bluez_backend_watchdog_thread_cb, cancellable);
+
+	/* GDBusObjectManagerClient subscribes to the bus signals from ::init, and
+	 * g_dbus_connection_signal_subscribe() binds the subscription to the
+	 * thread-default main context of the *calling* thread. The async constructor
+	 * runs ::init in a GTask worker thread, which has no thread-default context --
+	 * so the subscription would be bound to the global default context and the
+	 * device property changes would never be dispatched. Construct synchronously
+	 * with @main_ctx pushed so that everything is bound to the context we iterate. */
 	g_main_context_push_thread_default(main_ctx);
-	g_dbus_object_manager_client_new_for_bus(G_BUS_TYPE_SYSTEM,
-						 G_DBUS_OBJECT_MANAGER_CLIENT_FLAGS_NONE,
-						 "org.bluez",
-						 "/",
-						 NULL,
-						 NULL,
-						 NULL,
-						 helper->cancellable,
-						 fu_bluez_backend_connect_cb,
-						 helper);
+	self->object_manager =
+	    g_dbus_object_manager_client_new_for_bus_sync(G_BUS_TYPE_SYSTEM,
+							  G_DBUS_OBJECT_MANAGER_CLIENT_FLAGS_NONE,
+							  "org.bluez",
+							  "/",
+							  NULL,
+							  NULL,
+							  NULL,
+							  cancellable,
+							  error);
 	g_main_context_pop_thread_default(main_ctx);
-	g_main_loop_run(helper->loop);
-	if (helper->object_manager == NULL)
+
+	/* stop the watchdog (cancel is idempotent, harmless if already timed out) */
+	g_thread_join(watchdog_thread);
+
+	if (self->object_manager == NULL)
 		return FALSE;
-	self->object_manager = g_steal_pointer(&helper->object_manager);
 
 	if (flags & FU_BACKEND_SETUP_FLAG_USE_HOTPLUG) {
 		g_signal_connect(G_DBUS_OBJECT_MANAGER(self->object_manager),