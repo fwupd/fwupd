adb55d1ae398ea7b1c0abdd517e9f4ed915b9264
Marc-Antoine Perennou <ma.perennou@criteo.com>
2026-09-14 13:23:56 +0200

    redfish: Allow firmware blobs up to 512MiB
    Server firmware packages can be huge.
    Let's get a bigger margin than the basic 100MiB we generally use.
    
    Hit while attempting to udpate iDRAC on a Dell server:
    
    ╔══════════════════════════════════════════════════════════════════════════════╗
    ║ Upgrade iDRAC with Lifecycle Controller from 7.10.90.00 to 7.30.30.51?       ║
    ╠══════════════════════════════════════════════════════════════════════════════╣
    ║ The Integrated Dell Remote Access Controller (iDRAC) is designed to make     ║
    ║ server administrators more productive and improve the overall availability   ║
    ║ of Dell servers. iDRAC also provides an out-of-band mechanism for            ║
    ║ configuring the platform, applying firmware updates, saving or restoring a   ║
    ║ system backup, or deploying an operating system, either by using a GUI or a  ║
    ║ remote scripting language, such as Redfish or RACADM.                        ║
    ║                                                                              ║
    ║ iDRAC with Lifecycle Controller and all connected devices may not be usable  ║
    ║ while updating.                                                              ║
    ╚══════════════════════════════════════════════════════════════════════════════╝
    Perform operation? [Y|n]:
    Updating iDRAC with Lifecycle Controller…⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿▏
    Decompressing…           ▕⣤                                      ▏
    FuFirmware firmware is too large (241.9 MB, limit 104.9 MB)

--- plugins/redfish/fu-redfish-device.c
+++ plugins/redfish/fu-redfish-device.c
@@ -9,6 +9,7 @@
 #include "fu-redfish-backend.h"
 #include "fu-redfish-common.h"
 #include "fu-redfish-device.h"
+#include "fu-redfish-firmware.h"
 
 typedef struct {
 	FuRedfishBackend *backend;
@@ -1030,6 +1031,7 @@ static void
 fu_redfish_device_init(FuRedfishDevice *self)
 {
 	fu_device_set_summary(FU_DEVICE(self), "Redfish device");
+	fu_device_set_firmware_gtype(FU_DEVICE(self), FU_TYPE_REDFISH_FIRMWARE);
 	fu_device_add_protocol(FU_DEVICE(self), "org.dmtf.redfish");
 	fu_device_add_flag(FU_DEVICE(self), FWUPD_DEVICE_FLAG_REQUIRE_AC);
 	fu_device_add_private_flag(FU_DEVICE(self), FU_DEVICE_PRIVATE_FLAG_MD_SET_NAME);
--- plugins/redfish/fu-self-test.c
+++ plugins/redfish/fu-self-test.c
@@ -15,6 +15,7 @@
 #include "fu-plugin-private.h"
 #include "fu-redfish-backend.h"
 #include "fu-redfish-common.h"
+#include "fu-redfish-device.h"
 #include "fu-redfish-network.h"
 #include "fu-redfish-plugin.h"
 #include "fu-redfish-smc-device.h"
@@ -27,6 +28,19 @@ typedef struct {
 	FuPlugin *hpe_plugin;
 	FuPlugin *dell_plugin;
 } FuTest;
+
+static void
+fu_redfish_firmware_func(void)
+{
+	GType firmware_gtype;
+	g_autoptr(FuDevice) device = g_object_new(FU_TYPE_REDFISH_DEVICE, NULL);
+	g_autoptr(FuFirmware) firmware = NULL;
+
+	firmware_gtype = fu_device_get_firmware_gtype(device);
+	g_assert_cmpint(firmware_gtype, !=, G_TYPE_INVALID);
+	firmware = g_object_new(firmware_gtype, NULL);
+	g_assert_cmpuint(fu_firmware_get_size_max(firmware), ==, 512 * FU_MB);
+}
 
 static void
 fu_self_init(FuTest *self)
@@ -764,6 +778,7 @@ main(int argc, char **argv)
 	g_test_add_func("/redfish/backend/session-key/empty",
 			fu_redfish_backend_session_key_empty_func);
 	g_test_add_func("/redfish/common", fu_redfish_common_func);
+	g_test_add_func("/redfish/firmware", fu_redfish_firmware_func);
 	g_test_add_func("/redfish/common/version", fu_redfish_common_version_func);
 	g_test_add_func("/redfish/common/lenovo", fu_redfish_common_lenovo_func);
 	g_test_add_func("/redfish/network/mac_addr", fu_redfish_network_mac_addr_func);
