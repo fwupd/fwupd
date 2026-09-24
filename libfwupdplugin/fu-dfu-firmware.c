9b40a02e75dc2d5b33a0c7acb001a3e2984d4e20
Richard Hughes <richard@hughsie.com>
2026-09-24 12:02:38 +0100

    dfu: Fix the footer size check
    This broke the ColorHug+ device emulation -- although the DFU footer has to
    have specific fields, there's nothing that says it cannot be larger.
    
    This regressed in 52af789dc86965c37fd5bf23c1d7e48f2e10e520 -- thanks Claude.

--- libfwupdplugin/fu-dfu-firmware.c
+++ libfwupdplugin/fu-dfu-firmware.c
@@ -282,13 +282,11 @@ fu_dfu_firmware_parse_footer(FuDfuFirmware *self,
 			    (guint)bufsz);
 		return FALSE;
 	}
-
-	/* validate footer length matches the actual footer size */
-	if (priv->footer_len != FU_STRUCT_DFU_FTR_SIZE) {
+	if (priv->footer_len < FU_STRUCT_DFU_FTR_SIZE) {
 		g_set_error(error,
 			    FWUPD_ERROR,
 			    FWUPD_ERROR_INTERNAL,
-			    "reported footer size 0x%02x does not match expected size 0x%02x",
+			    "reported footer size 0x%02x < expected size 0x%02x",
 			    (guint)priv->footer_len,
 			    (guint)FU_STRUCT_DFU_FTR_SIZE);
 		return FALSE;
