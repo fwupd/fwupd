fwupd/fwupd

commit 66406db239daa5d6480e0a3e5f8e97744e6a9f4d

trivial: Record emulated ioctl errors
Save ioctl failures in device events and propagate them when replaying an
emulated device.

libfwupdplugin/fu-ioctl.c
@@ -247,6 +247,7 @@ fu_ioctl_execute(FuIoctl *self,
 		 GError **error)
 {
 	FuDeviceEvent *event = NULL;
+	g_autoptr(GError) error_local = NULL;
 	g_autoptr(GString) event_id = NULL;
 	gboolean emulated =
 	    fu_device_has_flag(FU_DEVICE(self->udev_device), FWUPD_DEVICE_FLAG_EMULATED);
@@ -272,6 +273,23 @@ fu_ioctl_execute(FuIoctl *self,
 		event = fu_device_load_event(FU_DEVICE(self->udev_device), event_id->str, error);
 		if (event == NULL)
 			return FALSE;
+		if (rc != NULL) {
+			gint64 rc_tmp = fu_device_event_get_i64(event, "Rc", NULL);
+			if (rc_tmp != G_MAXINT64) {
+				if (rc_tmp > G_MAXINT || rc_tmp < G_MININT) {
+					g_set_error_literal(error,
+							    FWUPD_ERROR,
+							    FWUPD_ERROR_INVALID_DATA,
+							    "rc oversize");
+					return FALSE;
+				}
+				*rc = (gint)rc_tmp;
+			}
+		}
+		if (!fu_device_event_check_error(event, &error_local)) {
+			g_propagate_error(error, g_steal_pointer(&error_local));
+			return FALSE;
+		}
 		if (self->fixups->len == 0) {
 			if ((flags & FU_IOCTL_FLAG_PTR_AS_INTEGER) == 0) {
 				if (!fu_device_event_copy_data(event,
@@ -296,11 +314,6 @@ fu_ioctl_execute(FuIoctl *self,
 						       error))
 				return FALSE;
 		}
-		if (rc != NULL) {
-			gint64 rc_tmp = fu_device_event_get_i64(event, "Rc", NULL);
-			if (rc_tmp != G_MAXINT64)
-				*rc = (gint)rc_tmp;
-		}
 		return TRUE;
 	}
 
@@ -323,8 +336,15 @@ fu_ioctl_execute(FuIoctl *self,
 				  rc,
 				  timeout,
 				  flags,
-				  error))
+				  &error_local)) {
+		if (event != NULL) {
+			if (rc != NULL)
+				fu_device_event_set_i64(event, "Rc", *rc);
+			fu_device_event_set_error(event, error_local);
+		}
+		g_propagate_error(error, g_steal_pointer(&error_local));
 		return FALSE;
+	}
 
 	/* save response */
 	if (event != NULL) {

commit d2586a69d11704355021093dd8b46160108d77ce

raydium-tp: Fix a regression when replaying old emulation versions
This regressed in 66406db239daa5d6480e0a3e5f8e97744e6a9f4d

libfwupdplugin/fu-ioctl.c
@@ -290,6 +290,18 @@ fu_ioctl_execute(FuIoctl *self,
 			g_propagate_error(error, g_steal_pointer(&error_local));
 			return FALSE;
 		}
+		/* prior to 66406db239daa5d6480e0a3e5f8e97744e6a9f4d we would not record the
+		 * emulation failure, and just not write a "DataOut" key -- and we can't use
+		 * fu_device_check_fwupd_version() as the FuDevice is not visible here */
+		if (fu_device_event_get_str(event, "DataOut", NULL) == NULL &&
+		    (flags & FU_IOCTL_FLAG_PTR_AS_INTEGER) == 0) {
+			g_set_error_literal(
+			    error,
+			    FWUPD_ERROR,
+			    FWUPD_ERROR_READ,
+			    "no DataOut so emulating read failure for old emulation");
+			return FALSE;
+		}
 		if (self->fixups->len == 0) {
 			if ((flags & FU_IOCTL_FLAG_PTR_AS_INTEGER) == 0) {
 				if (!fu_device_event_copy_data(event,